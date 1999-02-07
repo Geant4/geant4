3f36880b4c43d810e6f53278f5343f2959977245
johna <johna@cern.ch>
1999-02-07 17:00:24 +0000

    Removed GNU_GCC.
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@150 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/graphics_reps/src/G4Polymarker.cc
+++ source/graphics_reps/src/G4Polymarker.cc
@@ -5,20 +5,13 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: G4Polymarker.cc,v 1.1 1999-01-07 16:09:18 gunter Exp $
+// $Id: G4Polymarker.cc,v 1.2 1999-02-07 17:00:24 johna Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // 
 // John Allison  November 1996
 
 #include "G4Polymarker.hh"
-
-// Forward declaration for g++ linker.
-#ifdef GNU_GCC
-  #include <rw/tvvector.h>
-  #include "G4Point3D.hh"
-  template class RWTValVector<G4Point3D>;
-#endif
 
 G4Polymarker::G4Polymarker ():
 fMarkerType (line)
