baade8de0771bfc5c81dff05c75111c6031acc86
John Apostolakis <John.Apostolakis@cern.ch>
1999-02-15 11:14:01 +0000

    Changed kMaxVoxelNodes to 1000 to make its value consistent with Geant3.21
     But this change is 'hidden' because the new "smartless" quality sets an
    effective maximum = its_value * number_of_candidate_volumes.
    
       Change:  S. Giani
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@216 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/geometry/management/include/voxeldefs.hh
+++ source/geometry/management/include/voxeldefs.hh
@@ -5,7 +5,7 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: voxeldefs.hh,v 1.1 1999-01-07 16:07:19 gunter Exp $
+// $Id: voxeldefs.hh,v 1.2 1999-02-15 11:14:01 japost Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 
@@ -13,6 +13,7 @@
 // Voxel Optimisation Constants
 //
 // History:
+// 12.02.99 S.Giani made numerical values consistent with Geant3.21
 // 13.08.95 P.Kent Created separate file
 
 #ifndef VOXELDEFS_HH
@@ -21,7 +22,7 @@
 #include "globals.hh"
 
 // Hard limit on no. voxel nodes per given header
-const G4int kMaxVoxelNodes=2000;// Geant 3.21 uses 1000
+const G4int kMaxVoxelNodes=1000;  // PK chose 2000, Geant 3.21 used 1000
 
 const G4int kMinVoxelVolumesLevel1=2; // Only begin to make voxels if >=
 				      // this no of daughters
