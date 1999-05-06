0275e43ea337475b25e986fa381cf0dd4f4ccbd1
Peter Gumplinger <peter.gumplinger@cern.ch>
1999-05-06 00:23:37 +0000

    fixed typo - important error
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@799 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/electromagnetic/xrays/src/G4Scintillation.cc
+++ source/processes/electromagnetic/xrays/src/G4Scintillation.cc
@@ -5,7 +5,7 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: G4Scintillation.cc,v 1.1 1999-01-07 16:11:29 gunter Exp $
+// $Id: G4Scintillation.cc,v 1.2 1999-05-06 00:23:37 gum Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 ////////////////////////////////////////////////////////////////////////
@@ -170,7 +170,7 @@ G4Scintillation::PostStepDoIt(const G4Track& aTrack, const G4Step& aStep)
 		// Generate random photon direction
 
                 G4double cost = 1. - 2.*G4UniformRand();
-                G4double sint = sqrt((1.-cost)*(1.-cost));
+                G4double sint = sqrt((1.-cost)*(1.+cost));
 
 		G4double phi = 2*M_PI*G4UniformRand();
 		G4double sinp = sin(phi);
