9e2ac0fd96f146bca967e61c8c3e9b7414cc4755
Bill Lockman <lockman@cern.ch>
1999-05-06 05:14:15 +0000

    excplicit cast to char * in G3EleTable
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@803 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/g3tog4/src/G3EleTable.cc
+++ source/g3tog4/src/G3EleTable.cc
@@ -5,7 +5,7 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: G3EleTable.cc,v 1.1 1999-05-06 04:22:22 lockman Exp $
+// $Id: G3EleTable.cc,v 1.2 1999-05-06 05:14:15 lockman Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 
@@ -58,125 +58,125 @@ G3EleTable::parse(G4double& Z, char* name, char* sym, G4double& A){
 void
 G3EleTable::LoadUp(){
   int i=0;
-  _EleNames[i]="Hydrogen H 1.00794"; i++;
-  _EleNames[i]="Helium He 4.0026"; i++;
-  _EleNames[i]="Lithium Li 6.941"; i++;
-  _EleNames[i]="Beryllium Be 9.012182"; i++;
-  _EleNames[i]="Boron B 10.811"; i++;
-  _EleNames[i]="Carbon C 12.011"; i++;
-  _EleNames[i]="Nitrogen N 14.00674"; i++;
-  _EleNames[i]="Oxygen O 15.9994"; i++;
-  _EleNames[i]="Fluorine F 18.9984032"; i++;
-  _EleNames[i]="Neon Ne 20.1797"; i++;
+  _EleNames[i]=(char *)"Hydrogen H 1.00794"; i++;
+  _EleNames[i]=(char *)"Helium He 4.0026"; i++;
+  _EleNames[i]=(char *)"Lithium Li 6.941"; i++;
+  _EleNames[i]=(char *)"Beryllium Be 9.012182"; i++;
+  _EleNames[i]=(char *)"Boron B 10.811"; i++;
+  _EleNames[i]=(char *)"Carbon C 12.011"; i++;
+  _EleNames[i]=(char *)"Nitrogen N 14.00674"; i++;
+  _EleNames[i]=(char *)"Oxygen O 15.9994"; i++;
+  _EleNames[i]=(char *)"Fluorine F 18.9984032"; i++;
+  _EleNames[i]=(char *)"Neon Ne 20.1797"; i++;
 
-  _EleNames[i]="Sodium Na 22.989768"; i++;
-  _EleNames[i]="Magnesium Mg 24.3050"; i++;
-  _EleNames[i]="Aluminum Al 26.981539"; i++;
-  _EleNames[i]="Silicon Si 28.0855"; i++;
-  _EleNames[i]="Phosphorus P 30.973762"; i++;
-  _EleNames[i]="Sulfur S 32.066"; i++;
-  _EleNames[i]="Chlorine Cl 35.4527"; i++;
-  _EleNames[i]="Argon Ar 39.948"; i++;
-  _EleNames[i]="Potassium K 39.0983"; i++;
-  _EleNames[i]="Calcium Ca 40.078"; i++;
+  _EleNames[i]=(char *)"Sodium Na 22.989768"; i++;
+  _EleNames[i]=(char *)"Magnesium Mg 24.3050"; i++;
+  _EleNames[i]=(char *)"Aluminum Al 26.981539"; i++;
+  _EleNames[i]=(char *)"Silicon Si 28.0855"; i++;
+  _EleNames[i]=(char *)"Phosphorus P 30.973762"; i++;
+  _EleNames[i]=(char *)"Sulfur S 32.066"; i++;
+  _EleNames[i]=(char *)"Chlorine Cl 35.4527"; i++;
+  _EleNames[i]=(char *)"Argon Ar 39.948"; i++;
+  _EleNames[i]=(char *)"Potassium K 39.0983"; i++;
+  _EleNames[i]=(char *)"Calcium Ca 40.078"; i++;
 
-  _EleNames[i]="Scandium Sc 44.955910"; i++;
-  _EleNames[i]="Titanium Ti 47.867"; i++;
-  _EleNames[i]="Vanadium V 50.9415"; i++;
-  _EleNames[i]="Chromium Cr 51.9961"; i++;
-  _EleNames[i]="Manganese Mn 54.93805"; i++;
-  _EleNames[i]="Iron Fe 55.845"; i++;
-  _EleNames[i]="Cobalt Co 58.93320"; i++;
-  _EleNames[i]="Nickel Ni 58.6934"; i++;
-  _EleNames[i]="Copper Cu 63.546"; i++;
-  _EleNames[i]="Zinc Zn 65.39"; i++;
+  _EleNames[i]=(char *)"Scandium Sc 44.955910"; i++;
+  _EleNames[i]=(char *)"Titanium Ti 47.867"; i++;
+  _EleNames[i]=(char *)"Vanadium V 50.9415"; i++;
+  _EleNames[i]=(char *)"Chromium Cr 51.9961"; i++;
+  _EleNames[i]=(char *)"Manganese Mn 54.93805"; i++;
+  _EleNames[i]=(char *)"Iron Fe 55.845"; i++;
+  _EleNames[i]=(char *)"Cobalt Co 58.93320"; i++;
+  _EleNames[i]=(char *)"Nickel Ni 58.6934"; i++;
+  _EleNames[i]=(char *)"Copper Cu 63.546"; i++;
+  _EleNames[i]=(char *)"Zinc Zn 65.39"; i++;
 
-  _EleNames[i]="Gallium Ga 69.723"; i++;
-  _EleNames[i]="Germanium Ge 72.61"; i++;
-  _EleNames[i]="Arsenic As 74.92159"; i++;
-  _EleNames[i]="Selenium Se 78.96"; i++;
-  _EleNames[i]="Bromine Br 79.904"; i++;
-  _EleNames[i]="Krypton Kr 83.80"; i++;
-  _EleNames[i]="Rubidium Rb 85.4678"; i++;
-  _EleNames[i]="Strontium Sr 87.62"; i++;
-  _EleNames[i]="Yttrium Y 88.90585"; i++;
-  _EleNames[i]="Zirconium Zr 91.224"; i++;
+  _EleNames[i]=(char *)"Gallium Ga 69.723"; i++;
+  _EleNames[i]=(char *)"Germanium Ge 72.61"; i++;
+  _EleNames[i]=(char *)"Arsenic As 74.92159"; i++;
+  _EleNames[i]=(char *)"Selenium Se 78.96"; i++;
+  _EleNames[i]=(char *)"Bromine Br 79.904"; i++;
+  _EleNames[i]=(char *)"Krypton Kr 83.80"; i++;
+  _EleNames[i]=(char *)"Rubidium Rb 85.4678"; i++;
+  _EleNames[i]=(char *)"Strontium Sr 87.62"; i++;
+  _EleNames[i]=(char *)"Yttrium Y 88.90585"; i++;
+  _EleNames[i]=(char *)"Zirconium Zr 91.224"; i++;
 
-  _EleNames[i]="Niobium Nb 92.90638"; i++;
-  _EleNames[i]="Molybdenum Mo 95.94"; i++;
-  _EleNames[i]="Technetium Tc 97.907215"; i++;
-  _EleNames[i]="Ruthenium Ru 101.07"; i++;
-  _EleNames[i]="Rhodium Rh 102.90550"; i++;
-  _EleNames[i]="Palladium Pd 106.42"; i++;
-  _EleNames[i]="Silver Ag 107.8682"; i++;
-  _EleNames[i]="Cadmium Cd 112.41"; i++;
-  _EleNames[i]="Indium In 114.818"; i++;
-  _EleNames[i]="Tin Sn 118.710"; i++;
+  _EleNames[i]=(char *)"Niobium Nb 92.90638"; i++;
+  _EleNames[i]=(char *)"Molybdenum Mo 95.94"; i++;
+  _EleNames[i]=(char *)"Technetium Tc 97.907215"; i++;
+  _EleNames[i]=(char *)"Ruthenium Ru 101.07"; i++;
+  _EleNames[i]=(char *)"Rhodium Rh 102.90550"; i++;
+  _EleNames[i]=(char *)"Palladium Pd 106.42"; i++;
+  _EleNames[i]=(char *)"Silver Ag 107.8682"; i++;
+  _EleNames[i]=(char *)"Cadmium Cd 112.41"; i++;
+  _EleNames[i]=(char *)"Indium In 114.818"; i++;
+  _EleNames[i]=(char *)"Tin Sn 118.710"; i++;
 
-  _EleNames[i]="Antimony Sb 121.760"; i++;
-  _EleNames[i]="Tellurium Te 127.60"; i++;
-  _EleNames[i]="Iodine I 126.90447"; i++;
-  _EleNames[i]="Xenon Xe 131.29"; i++;
-  _EleNames[i]="Cesium Cs 132.90543"; i++;
-  _EleNames[i]="Barium Ba 137.27"; i++;
-  _EleNames[i]="Lanthanum La 138.9055"; i++;
-  _EleNames[i]="Cerium Ce 140.115"; i++;
-  _EleNames[i]="Praeseodymium Pr 140.90765"; i++;
-  _EleNames[i]="NeoDymium Nd 144.24"; i++;
+  _EleNames[i]=(char *)"Antimony Sb 121.760"; i++;
+  _EleNames[i]=(char *)"Tellurium Te 127.60"; i++;
+  _EleNames[i]=(char *)"Iodine I 126.90447"; i++;
+  _EleNames[i]=(char *)"Xenon Xe 131.29"; i++;
+  _EleNames[i]=(char *)"Cesium Cs 132.90543"; i++;
+  _EleNames[i]=(char *)"Barium Ba 137.27"; i++;
+  _EleNames[i]=(char *)"Lanthanum La 138.9055"; i++;
+  _EleNames[i]=(char *)"Cerium Ce 140.115"; i++;
+  _EleNames[i]=(char *)"Praeseodymium Pr 140.90765"; i++;
+  _EleNames[i]=(char *)"NeoDymium Nd 144.24"; i++;
   
-  _EleNames[i]="Promethium Pm 144.912745"; i++;
-  _EleNames[i]="Samarium Sm 150.36"; i++;
-  _EleNames[i]="Europium Eu 151.965"; i++;
-  _EleNames[i]="Gadolinium Gd 157.25"; i++;
-  _EleNames[i]="Terbium Tb 158.92534"; i++;
-  _EleNames[i]="Dysprosium Dy 162.50"; i++;
-  _EleNames[i]="Holmium Ho 164.93032"; i++;
-  _EleNames[i]="Erbium Er 167.26"; i++;
-  _EleNames[i]="Thulium Tm 168.93421"; i++;
-  _EleNames[i]="Ytterbium Yb 173.04"; i++;
+  _EleNames[i]=(char *)"Promethium Pm 144.912745"; i++;
+  _EleNames[i]=(char *)"Samarium Sm 150.36"; i++;
+  _EleNames[i]=(char *)"Europium Eu 151.965"; i++;
+  _EleNames[i]=(char *)"Gadolinium Gd 157.25"; i++;
+  _EleNames[i]=(char *)"Terbium Tb 158.92534"; i++;
+  _EleNames[i]=(char *)"Dysprosium Dy 162.50"; i++;
+  _EleNames[i]=(char *)"Holmium Ho 164.93032"; i++;
+  _EleNames[i]=(char *)"Erbium Er 167.26"; i++;
+  _EleNames[i]=(char *)"Thulium Tm 168.93421"; i++;
+  _EleNames[i]=(char *)"Ytterbium Yb 173.04"; i++;
 
-  _EleNames[i]="Lutetium Lu 174.967"; i++;
-  _EleNames[i]="Hafnium Hf 178.49"; i++;
-  _EleNames[i]="Tantalum Ta 180.9479"; i++;
-  _EleNames[i]="Tungsten W 183.84"; i++;
-  _EleNames[i]="Rhenium Re 186.207"; i++;
-  _EleNames[i]="Osmium Os 190.23"; i++;
-  _EleNames[i]="Iridium Ir 192.217"; i++;
-  _EleNames[i]="Platinum Pt 195.08"; i++;
-  _EleNames[i]="Gold Au 196.96654"; i++;
-  _EleNames[i]="Mercury Hg 200.59"; i++;
+  _EleNames[i]=(char *)"Lutetium Lu 174.967"; i++;
+  _EleNames[i]=(char *)"Hafnium Hf 178.49"; i++;
+  _EleNames[i]=(char *)"Tantalum Ta 180.9479"; i++;
+  _EleNames[i]=(char *)"Tungsten W 183.84"; i++;
+  _EleNames[i]=(char *)"Rhenium Re 186.207"; i++;
+  _EleNames[i]=(char *)"Osmium Os 190.23"; i++;
+  _EleNames[i]=(char *)"Iridium Ir 192.217"; i++;
+  _EleNames[i]=(char *)"Platinum Pt 195.08"; i++;
+  _EleNames[i]=(char *)"Gold Au 196.96654"; i++;
+  _EleNames[i]=(char *)"Mercury Hg 200.59"; i++;
 
-  _EleNames[i]="Thallium Tl 204.3833"; i++;
-  _EleNames[i]="Lead Pb 207.2"; i++;
-  _EleNames[i]="Bismuth Bi 208.98037"; i++;
-  _EleNames[i]="Polonium Po 208.982415"; i++;
-  _EleNames[i]="Astatine At 209.987131"; i++;
-  _EleNames[i]="Radon Rn 222.017570"; i++;
-  _EleNames[i]="Francium Fr 223.019731"; i++;
-  _EleNames[i]="Radium Ra 226.025402"; i++;
-  _EleNames[i]="Actinium Ac 227.027747"; i++;
-  _EleNames[i]="Thorium Th 232.0381"; i++;
+  _EleNames[i]=(char *)"Thallium Tl 204.3833"; i++;
+  _EleNames[i]=(char *)"Lead Pb 207.2"; i++;
+  _EleNames[i]=(char *)"Bismuth Bi 208.98037"; i++;
+  _EleNames[i]=(char *)"Polonium Po 208.982415"; i++;
+  _EleNames[i]=(char *)"Astatine At 209.987131"; i++;
+  _EleNames[i]=(char *)"Radon Rn 222.017570"; i++;
+  _EleNames[i]=(char *)"Francium Fr 223.019731"; i++;
+  _EleNames[i]=(char *)"Radium Ra 226.025402"; i++;
+  _EleNames[i]=(char *)"Actinium Ac 227.027747"; i++;
+  _EleNames[i]=(char *)"Thorium Th 232.0381"; i++;
 
-  _EleNames[i]="Protactinium Pa 231.03588"; i++;
-  _EleNames[i]="Uranium U 238.0289"; i++;
-  _EleNames[i]="Neptunium Np 237.048166"; i++;
-  _EleNames[i]="Plutonium Pu 244.064197"; i++;
-  _EleNames[i]="Americium Am 243.061372"; i++;
-  _EleNames[i]="Curium Cm 247.070346"; i++;
-  _EleNames[i]="Berkelium Bk 247.070298"; i++;
-  _EleNames[i]="Californium Cf 251.079579"; i++;
-  _EleNames[i]="Einsteinium Es 252.08297"; i++;
-  _EleNames[i]="Fermium Fm 257.095096"; i++;
+  _EleNames[i]=(char *)"Protactinium Pa 231.03588"; i++;
+  _EleNames[i]=(char *)"Uranium U 238.0289"; i++;
+  _EleNames[i]=(char *)"Neptunium Np 237.048166"; i++;
+  _EleNames[i]=(char *)"Plutonium Pu 244.064197"; i++;
+  _EleNames[i]=(char *)"Americium Am 243.061372"; i++;
+  _EleNames[i]=(char *)"Curium Cm 247.070346"; i++;
+  _EleNames[i]=(char *)"Berkelium Bk 247.070298"; i++;
+  _EleNames[i]=(char *)"Californium Cf 251.079579"; i++;
+  _EleNames[i]=(char *)"Einsteinium Es 252.08297"; i++;
+  _EleNames[i]=(char *)"Fermium Fm 257.095096"; i++;
 
-  _EleNames[i]="Mendelevium Md 258.098427"; i++;
-  _EleNames[i]="Nobelium No 259.1011"; i++;
-  _EleNames[i]="Lawrencium Lr 262.1098"; i++;
-  _EleNames[i]="Rutherfordium Rf 261.1089";  i++;
-  _EleNames[i]="Hahnium Ha 262.1144";  i++;
-  _EleNames[i]="Seaborgium Sg 263.1186";  i++;
-  _EleNames[i]="Nielsborium Ns 262.1231";  i++;
-  _EleNames[i]="Hassium Hs 265.1306";  i++;
-  _EleNames[i]="Meitnerium Mt 266.1378";  i++;
+  _EleNames[i]=(char *)"Mendelevium Md 258.098427"; i++;
+  _EleNames[i]=(char *)"Nobelium No 259.1011"; i++;
+  _EleNames[i]=(char *)"Lawrencium Lr 262.1098"; i++;
+  _EleNames[i]=(char *)"Rutherfordium Rf 261.1089";  i++;
+  _EleNames[i]=(char *)"Hahnium Ha 262.1144";  i++;
+  _EleNames[i]=(char *)"Seaborgium Sg 263.1186";  i++;
+  _EleNames[i]=(char *)"Nielsborium Ns 262.1231";  i++;
+  _EleNames[i]=(char *)"Hassium Hs 265.1306";  i++;
+  _EleNames[i]=(char *)"Meitnerium Mt 266.1378";  i++;
 
   // initialize element pointers to 0
   for (int j=0; j<i; j++) {
