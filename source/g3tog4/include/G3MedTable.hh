2193c58c27cb2b3c615efa5260e2e8c8907d5556
Bill Lockman <lockman@cern.ch>
1999-05-06 18:02:04 +0000

    Updated G3MedTable to use PtrOrderedVector instead of Hash dictionary
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@827 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/g3tog4/include/G3MedTable.hh
+++ source/g3tog4/include/G3MedTable.hh
@@ -8,19 +8,19 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: G3MedTable.hh,v 1.2 1999-05-06 04:20:25 lockman Exp $
+// $Id: G3MedTable.hh,v 1.3 1999-05-06 18:01:50 lockman Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 //
 // Maps G3 tracking media indices to G4Material*
 
-#include <rw/tphdict.h>
-#include "G4Material.hh"
+#include <rw/tpordvec.h>
+
+class G4Material;
 
 class G3MedTable {
 private:
-  RWTPtrHashDictionary<G4String,G4Material>* _Med;
-  char* MakeMedID(G4int id);
+  RWTPtrOrderedVector<G4Material>* _Med;
 public:
   G3MedTable();
   ~G3MedTable();
--- source/g3tog4/src/G3MedTable.cc
+++ source/g3tog4/src/G3MedTable.cc
@@ -5,48 +5,30 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: G3MedTable.cc,v 1.2 1999-05-06 04:22:47 lockman Exp $
+// $Id: G3MedTable.cc,v 1.3 1999-05-06 18:02:04 lockman Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 
-#include "G4strstreambuf.hh"
-#include "G4ios.hh"
 #include "globals.hh"
 #include "G3MedTable.hh"
+#include "G4Material.hh"
 
 G3MedTable::G3MedTable(){
   _Med = new 
-    RWTPtrHashDictionary<RWCString,G4Material>(RWCString::hash);
-}
+    RWTPtrOrderedVector<G4Material>;
+};
 
 G3MedTable::~G3MedTable(){
   G4cout << "Destructing G3MedTable." << endl;
   _Med->clearAndDestroy();
-}
+};
 
 G4Material*
 G3MedTable::get(G4int MedID){
-  // create a string from the MedID, retrieve pointer from hash dictionary
-  G4String Hash(MakeMedID(MedID));
-  G4Material* R = _Med->findValue(&Hash);
-  if (R == 0) G4cerr << "Arrggh! No G3gstmed found with key " << Hash << endl;
-  return R;
+  return (*_Med)[MedID];
 };
 
 void 
 G3MedTable::put(G4int MedID, G4Material* MatPT){
-  // create a string from the MedID, store in hash dictionary
-  G4String* Hash = new G4String(MakeMedID(MedID));
-  _Med->insertKeyAndValue(Hash, MatPT);
-}
-
-char*
-G3MedTable::MakeMedID(G4int MedID){
-  char buf[20];
-  ostrstream ostr(buf, sizeof buf);
-  ostr << "Med" << MedID << ends;
-  return buf;
-}
-
-
-
+  _Med->insertAt(MedID, MatPT);
+};
