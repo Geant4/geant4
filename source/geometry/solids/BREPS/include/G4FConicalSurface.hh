ece0b133b7ef9531aaa43136256e5fdc4dc3b731
Stefano Magni <magni@mi.infn.it>
1999-05-19 16:54:47 +0000

    S.Magni 19-5-99
    Corrected the following methods:
    - Intersect
    - HowNear
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@1025 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/geometry/solids/BREPS/include/G4FConicalSurface.hh
+++ source/geometry/solids/BREPS/include/G4FConicalSurface.hh
@@ -5,7 +5,7 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: G4FConicalSurface.hh,v 1.2 1999-01-27 16:11:57 broglia Exp $
+// $Id: G4FConicalSurface.hh,v 1.3 1999-05-19 16:54:47 magni Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 #ifndef __FCONIC_H
@@ -57,44 +57,7 @@ public:
     tan_angle = (large_radius-small_radius)/length;
   }
 	
-  // constructor utilized into G4BREPSolidPCone
-  G4FConicalSurface( const G4Point3D& o, const G4Vector3D& a,
-		     G4double l, G4double sr, G4double lr );
-
-  G4FConicalSurface( const G4FConicalSurface& c );
-  
-  ~G4FConicalSurface() {}
-
-  virtual G4Vector3D SurfaceNormal( const G4Point3D& p ) const;	
-
-  virtual int Inside( const G4Vector3D& x ) const;
-
-  G4String GetEntityType(){return G4String("FConical_Surface");}
-
-  // STEP additions
-  virtual char *Name() const { return "G4FConicalSurface"; }
-  virtual void PrintOn( ostream& os = G4cout ) const;
-
-  int operator==( const G4FConicalSurface& c );
-
-  int  Intersect( const G4Ray& ry ) ;
-  void CalcBBox();
-
-  // Add by L. Broglia
-  virtual G4double HowNear( const G4Vector3D& x ) const;
-  
-  virtual int WithinBoundary( const G4Vector3D& x ) const;
-  virtual G4double Scale() const;
-  virtual G4double Area() const;
-  virtual void resize( G4double l, G4double sr, G4double lr );
-  
-  G4double GetLength()      const { return length;       }
-  G4double GetSmallRadius() const { return small_radius; }
-  G4double GetLargeRadius() const { return large_radius; }
-  G4double GetTan_Angle()   const { return tan_angle; }
-
-//  Description of functions -----------------------------------------
-//
+// constructor utilized into G4BREPSolidPCone
 //  default constructor
 //----->G4FConicalSurface() : G4ConicalSurface() { length = 1.0;
 //----->		     small_radius = 0.0;
@@ -107,46 +70,89 @@ public:
 //                                          G4FConicalSurface
 //			 fifth argument is the large radius of the 
 //                                          G4FConicalSurface
-//----->G4FConicalSurface( const G4ThreeVec& o, const G4ThreeVec& a,
-//----->	        G4double l, G4double sr, G4double lr );
-//
-//  destructor 
-//----->virtual ~G4FConicalSurface() {}
-//
-//  copy constructor
-//----->G4FConicalSurface( const G4FConicalSurface& c );
+
+  G4FConicalSurface( const G4Point3D& o, const G4Vector3D& a,
+		     G4double l, G4double sr, G4double lr );
+
+  G4FConicalSurface( const G4FConicalSurface& c );
+  
+  ~G4FConicalSurface() {}
+
+  virtual G4Vector3D SurfaceNormal( const G4Point3D& p ) const;	
+
+// Return 0 if point x is outside G4ConicalSurface, 1 if Inside.
+  virtual int Inside( const G4Vector3D& x ) const;
+
+  G4String GetEntityType(){return G4String("FConical_Surface");}
+
+  // STEP additions
 //
 //  function to return class name
-//----->virtual char *NameOf() const { return "G4FConicalSurface"; }
-//
+  virtual char *Name() const { return "G4FConicalSurface"; }
 //  printing function
-//----->virtual void PrintOn( ostream& os = G4cout ) const;
-//
+  virtual void PrintOn( ostream& os = G4cout ) const;
+
 //  equality operator
-//----->int operator==( const G4FConicalSurface& c );
-//
+  int operator==( const G4FConicalSurface& c );
+
+// This function count the number of intersections of a 
+// bounded conical surface by a ray.
+// At first, calculates the intersections with the semi-infinite 
+// conical surfsace. After, count the intersections within the
+// finite conical surface boundaries, and set "distance" to the 
+// closest distance from the start point to the nearest intersection
+// If the point is on the surface it returns or the intersection with
+// the opposite surface or kInfinity
+// If no intersection is founded, set distance = kInfinity and
+// return 0
+
+  int  Intersect( const G4Ray& ry ) ;
+  void CalcBBox();
+
+  // Add by L. Broglia
+
+// Shortest distance from the point x to the G4FConicalSurface.
+// The distance will be positive always positive
+
+ virtual G4double HowNear( const G4Vector3D& x ) const;
+  
 //  function which returns true (1) if the point x is within the boundary
 //		returns false (0) otherwise
-//----->virtual int WithinBoundary( const G4ThreeVec& x ) const;
-//
+
+  virtual int WithinBoundary( const G4Vector3D& x ) const;
+
 //  function to return the size of a G4FConicalSurface.
 //		Used for Scale-invariant tests of surface thickness.
 //		If the small radius is zero, returns the large radius.
-//----->virtual G4double Scale() const;
-//
+
+  virtual G4double Scale() const;
+
 //  function to calculate the Area of a G4FConicalSurface
-//----->virtual G4double Area() const; 
-//
+  virtual G4double Area() const;
+
 //  function to change the radii and length of the G4FConicalSurface
 //		the first (input) argument is the new length
 //		the second (input) argument is the new small radius
 //		the third (input) argument is the new large radius
-//----->virtual void resize( G4double l, G4double sr, G4double lr );
-//
+
+  virtual void resize( G4double l, G4double sr, G4double lr );
+  
 //  functions to return the dimensions of the G4FConicalSurface
-//----->G4double GetLength() const { return length; }
-//----->G4double GetSmallRadius() const { return small_radius; }
-//----->G4double GetLargeRadius() const { return large_radius; }
+
+  G4double GetLength()      const { return length;       }
+
+//  functions to return the dimensions of the G4FConicalSurface
+
+  G4double GetSmallRadius() const { return small_radius; }
+
+//  functions to return the dimensions of the G4FConicalSurface
+
+  G4double GetLargeRadius() const { return large_radius; }
+
+//  functions to return the dimensions of the G4FConicalSurface
+
+  G4double GetTan_Angle()   const { return tan_angle; }
+
 };
 
 #endif
