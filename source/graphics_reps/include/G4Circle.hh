3942e1731e860bdf2b005731734fc70a39e92f78
johna <johna@cern.ch>
1999-06-05 15:13:12 +0000

    greps-01-00-08: Added virtual operator = and overrides to G4Circle and G4Square.
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@1332 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/graphics_reps/include/G4Circle.hh
+++ source/graphics_reps/include/G4Circle.hh
@@ -5,7 +5,7 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: G4Circle.hh,v 1.1 1999-01-07 16:09:09 gunter Exp $
+// $Id: G4Circle.hh,v 1.2 1999-06-05 15:13:10 johna Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // 
@@ -17,11 +17,22 @@
 #include "G4VMarker.hh"
 
 class G4Circle: public G4VMarker {
+
 public:
+
   G4Circle ();
   G4Circle (const G4Point3D& pos);
   G4Circle (const G4VMarker& marker);
-};
+  virtual ~G4Circle ();
+
+  //////////////////////////////////////////////////////
+  // Assignment...
+  virtual G4Visible&  operator = (const G4Visible& right);
+  virtual G4VVisPrim& operator = (const G4VVisPrim& right);
+  virtual G4VMarker&  operator = (const G4VMarker& right);
+  virtual G4Circle &  operator = (const G4Circle& right);
+
+ };
 
 #include "G4Circle.icc"
 
--- source/graphics_reps/include/G4Square.hh
+++ source/graphics_reps/include/G4Square.hh
@@ -5,7 +5,7 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: G4Square.hh,v 1.1 1999-01-07 16:09:12 gunter Exp $
+// $Id: G4Square.hh,v 1.2 1999-06-05 15:13:12 johna Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // 
@@ -17,10 +17,21 @@
 #include "G4VMarker.hh"
 
 class G4Square: public G4VMarker {
+
 public:
+
   G4Square ();
   G4Square (const G4Point3D& pos);
   G4Square (const G4VMarker& marker);
+  virtual ~G4Square ();
+
+  //////////////////////////////////////////////////////
+  // Assignment...
+  virtual G4Visible&  operator = (const G4Visible& right);
+  virtual G4VVisPrim& operator = (const G4VVisPrim& right);
+  virtual G4VMarker&  operator = (const G4VMarker& right);
+  virtual G4Square&   operator = (const G4Square& right);
+
 };
 
 #include "G4Square.icc"
