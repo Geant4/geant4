5d953c80a8ff7032c71d965e13fdac5d5bfb111d
Alessandra Forti <Alessandra.Forti@cern.ch>
1999-06-14 14:34:44 +0000

    new test14
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@1443 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- tests/test14/test14.cc
+++ tests/test14/test14.cc
@@ -5,82 +5,67 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: test14.cc,v 1.1 1999-05-29 14:12:02 stesting Exp $
+// $Id: test14.cc,v 1.2 1999-06-14 14:34:28 aforti Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
-// 
-// --------------------------------------------------------------
-//      GEANT 4 - TestTst14 
-//
-//      For information related to this code contact:
-//      CERN, IT Division, ASD Group
-// --------------------------------------------------------------
-// Comments
-//     
-//   
-// --------------------------------------------------------------
-
-#include "G4RunManager.hh"
-#include "G4UImanager.hh"
-#include "G4UIterminal.hh"
 
 #include "Tst14DetectorConstruction.hh"
-#include "Tst14PhysicsList.hh"
-#include "Tst14PrimaryGeneratorAction.hh"
 #include "Tst14RunAction.hh"
-#include "Tst14EventAction.hh"
+#include "Tst14PrimaryGeneratorAction.hh"
+#include "Tst14PhysicsList.hh"
 #include "Tst14SteppingAction.hh"
+#include "Tst14TrackingAction.hh"
 
-#ifdef GNU_GCC
-#include <rw/tpordvec.h>
-#include "Tst14CalorHit.hh"
-template class RWTPtrOrderedVector <Tst14CalorHit>;
-template class RWTPtrVector <Tst14CalorHit>;
-template class G4Allocator <Tst14CalorHit>;
-#endif
+#include "G4UImanager.hh"
+#include "G4UIterminal.hh"
+#include "G4RunManager.hh"
+
+#include "G4ios.hh"
 
 int main(int argc,char** argv) {
 
-  // Construct the default run manager
+  // Set the default random engine to RanecuEngine
+  RanecuEngine defaultEngine;
+  HepRandom::setTheEngine(&defaultEngine);
+
+  // Run manager
   G4RunManager * runManager = new G4RunManager;
 
-  // set mandatory initialization classes
-  Tst14DetectorConstruction* detector;
-  detector = new Tst14DetectorConstruction;
-  runManager->SetUserInitialization(detector);
-  runManager->SetUserInitialization(new Tst14PhysicsList(detector));
-  
-  // set user action classes
-  runManager->SetUserAction(new Tst14PrimaryGeneratorAction(detector));
-  Tst14RunAction* runaction = new Tst14RunAction;
-  runManager->SetUserAction(runaction);
+  // UserInitialization classes
+  runManager->SetUserInitialization(new Tst14DetectorConstruction);
+  runManager->SetUserInitialization(new Tst14PhysicsList);
 
-  Tst14EventAction* eventaction = new Tst14EventAction(runaction);
-  runManager->SetUserAction(eventaction);
+  // UserAction classes
+  runManager->SetUserAction(new Tst14RunAction);
+  runManager->SetUserAction(new Tst14PrimaryGeneratorAction);
+  //runManager->SetUserAction(new Tst14SteppingAction);
+  runManager->SetUserAction(new Tst14TrackingAction);
 
-  Tst14SteppingAction* steppingaction = new Tst14SteppingAction(detector,
-                                               eventaction, runaction);
-  runManager->SetUserAction(steppingaction);
-    
-  // get the pointer to the User Interface manager 
-    G4UImanager* UI = G4UImanager::GetUIpointer();  
- 
-  if (argc==1)   // Define UI terminal for interactive mode  
-    { 
-     G4UIsession * session = new G4UIterminal;
-     UI->ApplyCommand("/control/execute prerunTst14.mac");    
-     session->SessionStart();
-     delete session;
-    }
-  else           // Batch mode
-    { 
-     G4String command = "/control/execute ";
-     G4String fileName = argv[1];
-     UI->ApplyCommand(command+fileName);
-    }
-  // job termination
+  if(argc==1)
+  {
+    // G4UIterminal is a (dumb) terminal.
+    G4UIsession* session = new G4UIterminal;
+    session->SessionStart();
+    delete session;
+  }
+  else
+  {
+    G4UImanager* UImanager = G4UImanager::GetUIpointer();
+    G4String command = "/control/execute ";
+    G4String fileName = argv[1];
+    UImanager->ApplyCommand(command+fileName);
+  }
+
   delete runManager;
-
   return 0;
 }
+
+
+
+
+
+
+
+
+
 
