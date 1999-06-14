f34bb82a7954e6a95cbb57c9ec353dba57754afb
Alessandra Forti <Alessandra.Forti@cern.ch>
1999-06-14 15:09:51 +0000

    readding to the head
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@1445 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ tests/test14/src/Tst14TrackingAction.cc
@@ -0,0 +1,32 @@
+
+
+#include "Tst14TrackingAction.hh"
+#include "G4TrackingManager.hh"
+#include "G4Track.hh"
+#include "G4Gamma.hh"
+#include "globals.hh"
+
+void Tst14TrackingAction::PreUserTrackingAction(const G4Track* aTrack )
+{
+  G4TrackingManager* trackingManager = fpTrackingManager;
+  const G4DynamicParticle* aParticle = aTrack->GetDynamicParticle(); 
+
+  // Add polarization only for gamma
+  if ( aParticle->GetDefinition() == G4Gamma::Gamma() ){
+
+	// check polarization vector
+    // if polarization exists, leave as it is
+    if (aParticle->GetPolarization().mag() >0.0) return;
+
+    // set gamma polarization
+    //     Isotropic distribution of photons with transverse polarization 
+    //     with respect to gamma momentum
+    G4double  phi = twopi * G4UniformRand();
+    G4ThreeVector newPol(cos(phi),sin(phi),0.); 
+    G4ParticleMomentum aParticleDirection = aParticle->GetMomentumDirection(); 
+    newPol.rotateUz(aParticleDirection);
+    ((G4Track*)aTrack)->SetPolarization(newPol);
+  }
+}
+
+
