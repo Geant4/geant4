390a5390b83c46c204c6582170c9d5f89ec4faf7
Alessandra Forti <Alessandra.Forti@cern.ch>
1999-06-22 16:58:31 +0000

    primary generator was not properly initialized
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@1544 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- tests/test14/include/Tst14PrimaryGeneratorAction.hh
+++ tests/test14/include/Tst14PrimaryGeneratorAction.hh
@@ -5,29 +5,53 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: Tst14PrimaryGeneratorAction.hh,v 1.2 1999-06-14 14:28:34 aforti Exp $
+// $Id: Tst14PrimaryGeneratorAction.hh,v 1.3 1999-06-22 16:58:31 aforti Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
+// 
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
 
 #ifndef Tst14PrimaryGeneratorAction_h
 #define Tst14PrimaryGeneratorAction_h 1
 
 #include "G4VUserPrimaryGeneratorAction.hh"
+#include "globals.hh"
 
 class G4ParticleGun;
 class G4Event;
+class Tst14DetectorConstruction;
+//class Tst14PrimaryGeneratorMessenger;
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
 
 class Tst14PrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction
 {
   public:
-    Tst14PrimaryGeneratorAction();
-    ~Tst14PrimaryGeneratorAction();
+    Tst14PrimaryGeneratorAction(Tst14DetectorConstruction*);    
+   ~Tst14PrimaryGeneratorAction();
 
   public:
-    void GeneratePrimaries(G4Event* anEvent);
+    void GeneratePrimaries(G4Event*);
+    void SetRndmFlag(G4String val) { rndmFlag = val;}
+    void Setxvertex(G4double x) ;
+    void Setyvertex(G4double y) ;
+    void Setzvertex(G4double z) ;
+
+    static G4String GetPrimaryName() ;                
 
   private:
-    G4ParticleGun* particleGun;
+    G4ParticleGun*                particleGun;	//pointer a to G4 service class
+    Tst14DetectorConstruction*      Tst14Detector; //pointer to the geometry
+    
+  //  Tst14PrimaryGeneratorMessenger* gunMessenger; //messenger of this class
+    G4String                      rndmFlag;	//flag for a random impact point       
+
+    static G4String thePrimaryParticleName ;
+    G4double xvertex,yvertex,zvertex;
+    G4bool vertexdefined ;
+
 };
 
 #endif
