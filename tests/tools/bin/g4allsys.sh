f14e0e1f94392ca76cfc55db144f59c6d6c4bf13
Geant4 Testing <geant4.testing@cern.ch>
1999-07-19 09:10:57 +0000

    Removed use of g4sbr.o.sh and .d.sh.
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@1736 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- tests/tools/bin/g4allsys.sh
+++ tests/tools/bin/g4allsys.sh
@@ -21,13 +21,16 @@ do
 	REF=ref
     fi
 
-    sym_debopt=`echo $debopt|cut -c 1`
-    if [ X$sym_debopt = Xo ]
-    then
-	g4sbr=/afs/cern.ch/sw/geant4/stt/$REF/src/geant4/tests/tools/bin/g4sbr.o.sh
-    else
-	g4sbr=/afs/cern.ch/sw/geant4/stt/$REF/src/geant4/tests/tools/bin/g4sbr.d.sh
-    fi
+    g4sbr=/afs/cern.ch/sw/geant4/stt/$REF/src/geant4/tests/tools/bin/g4sbr.sh
+
+# This was intended to help the g4status command.  Let's think of another way.
+#    sym_debopt=`echo $debopt|cut -c 1`
+#    if [ X$sym_debopt = Xo ]
+#    then
+#	g4sbr=/afs/cern.ch/sw/geant4/stt/$REF/src/geant4/tests/tools/bin/g4sbr.o.sh
+#    else
+#	g4sbr=/afs/cern.ch/sw/geant4/stt/$REF/src/geant4/tests/tools/bin/g4sbr.d.sh
+#    fi
 
     command="rsh -l $account $host $g4sbr $devprod $debopt $tag $action $actarg1 $actarg2 $actarg3 $nonincremental"
     if [ X$host = X\# ]
