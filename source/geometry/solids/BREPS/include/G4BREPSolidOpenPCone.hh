a83a6fb5ff6d3eaba288d2b9eed3acf7302d6da4
John Apostolakis <John.Apostolakis@cern.ch>
1999-10-28 09:14:01 +0000

    Added new implementation of Open PCone.  Modified GNUmakefile to include Boolean for it (temporarily?)
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@2230 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/geometry/solids/BREPS/include/G4BREPSolidOpenPCone.hh
@@ -0,0 +1,34 @@
+// This code implementation is the intellectual property of
+// the RD44 GEANT4 collaboration.
+//
+// By copying, distributing or modifying the Program (or any work
+// based on the Program) you indicate your acceptance of this statement,
+// and all its terms.
+//
+#ifndef G4BREPSolidOpenPCone_hh
+#define G4BREPSolidOpenPCone_hh
+
+#include "G4IntersectionSolid.hh"
+
+class G4BREPSolidOpenPCone : public G4IntersectionSolid {
+
+public:
+  
+  G4BREPSolidOpenPCone ( G4String name,
+                    const G4double  start_angle,
+                    const G4double  opening_angle,                 
+                    const int       num_z_planes, // sections,
+                    const G4double  z_start,               
+                    const G4double  z_values[],
+                    const G4double  RMIN[],
+                    const G4double  RMAX[]
+                  );
+
+  void DescribeYourselfTo (G4VGraphicsScene& scene) const;
+
+private:
+  
+};
+
+
+#endif
