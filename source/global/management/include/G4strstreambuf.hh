d1a2cc166f0d4011f3e73db2433bc087810e5f77
Yoshida Hajime <yhajime@cern.ch>
1999-10-28 04:20:20 +0000

    streambuf only version
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@2222 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/global/management/include/G4strstreambuf.hh
+++ source/global/management/include/G4strstreambuf.hh
@@ -13,61 +13,13 @@
 class G4strstreambuf;
 extern G4strstreambuf G4coutbuf;
 extern G4strstreambuf G4cerrbuf;
-#ifndef G4STREAM_STREAMBUF_IMPLEMENTATION
-
-
-class G4strstreambuf : public strstreambuf {
-public:
-    G4strstreambuf() {
-      destination = NULL;
-    }
-    void SetDestination(G4coutDestination * value) {
-      destination = value;
-    }
-    int sync() {
-        G4String stringToSend;
-        int c;
-        int result;
-	result = EOF;
-        while (( c= sbumpc() ) != EOF) {
-            stringToSend += (char) c;
-        }
-        if(this == & G4coutbuf && destination != NULL) {
-            result =  destination->ReceiveG4cout(stringToSend);
-        } else
-        if(this == & G4cerrbuf && destination != NULL) {
-            result =  destination->ReceiveG4cerr(stringToSend);
-        } else
-        if(this == & G4coutbuf && destination == NULL) {
-            cout << stringToSend << flush;
-            result =0;
-        } else 
-        if(this == & G4cerrbuf && destination == NULL) {
-            cerr << stringToSend << flush;
-            result =0;
-        }
-        return result;
-    };
-
-private:
-  G4coutDestination *destination;
-};
-
-// 
-
-#else // G4STREAM_STREAMBUF_IMPLEMENTATION :
-
-//  On NT the upper implementation involves that
-// first character is lost when sending to destination !
-//  Below implementation is correct on this platform.
-
 
 class G4strstreambuf : public streambuf {
 public:
   G4strstreambuf() {
     destination = NULL;
     count = 0;
-    size = 127;
+    size = 4095;
     buffer = new char[size+1];
   }
   ~G4strstreambuf() {
@@ -125,8 +77,6 @@ private:
   char* buffer;
   int count,size;
 };
-
-#endif   // G4STREAM_STREAMBUF_IMPLEMENTATION :
 
 
  
