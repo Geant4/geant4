28d432690e1e355657246c8d32fc84ad0e0b33b1
masayasu <masayasu@cern.ch>
1999-11-08 04:14:10 +0000

    STL vector used, documentation update
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@2379 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/interfaces/GAG/include/G4UIGAG.hh
+++ source/interfaces/GAG/include/G4UIGAG.hh
@@ -5,7 +5,7 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: G4UIGAG.hh,v 1.2 1999-04-13 01:26:18 yhajime Exp $
+// $Id: G4UIGAG.hh,v 1.3 1999-11-08 04:14:10 masayasu Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // G4UIGAG.hh
@@ -21,24 +21,43 @@
 #include "G4UIsession.hh"
 #include "G4UImanager.hh"
 #include <fstream.h>
+#include <g4std/vector>
 
 enum UImode { terminal_mode , java_mode, tcl_mode };
 enum ChangeOfTree  { notChanged=0, added, deleted, addedAndDeleted };
 
+// class description:
+//
+// This class inherits the class G4UIsession.
+// This is the class which is the front-end to GAG (Geant4 Adaptive GUI).
+// Its usage is quite similar to G4UIterminal.
+
 class G4UIGAG : public G4UIsession
 {
-  public:
+      public: // with description
       G4UIGAG();
       ~G4UIGAG();
 
       G4UIsession * SessionStart();
+
+      // A GAG session  "gagSession" is instantiated.
+      // G4cout stream is redirected by default to the constructed instance.
+      // Usage:  G4UIsession * gagSession = new G4UIGAG;
+      // "gagSession" is started.
+      // Usage: gagSession->SessionStart();
+      // "gagSession"  is deleted.
+      // Usage: delete gagSession;
+      //
       void PauseSessionStart(G4String);
-      void Prompt(G4String);
-      G4String GetCommand();
       G4int ReceiveG4cout(G4String);
       G4int ReceiveG4cerr(G4String);         
+      // These methods are implementation of the virtual methods of
+      // G4UIsession class. 
+      //
       void SessionTerminate();
-
+      void Prompt(G4String);
+      G4String GetCommand();
+      // These methods are not for users.
   private:
       G4String prefix;
       G4UImanager * UI;
@@ -82,12 +101,12 @@ class G4UIGAG : public G4UIsession
     void GetNewTreeStructure( G4UIcommandTree*,int recursiveLevel);
     void GetNewTreeValues( G4UIcommandTree*,int recursiveLevel);
     
-    RWTValOrderedVector<G4String> previousTreeCommands;
-    RWTValOrderedVector<G4String> newTreeCommands;
-    RWTValOrderedVector<G4String> previousTreeParams;
-    RWTValOrderedVector<G4String> newTreeParams;
-    RWTValOrderedVector<G4UIcommand*> previousTreePCP;
-    RWTValOrderedVector<G4UIcommand*> newTreePCP;
+    G4std::vector<G4String> previousTreeCommands;
+    G4std::vector<G4String> newTreeCommands;
+    G4std::vector<G4String> previousTreeParams;
+    G4std::vector<G4String> newTreeParams;
+    G4std::vector<G4UIcommand*> previousTreePCP;
+    G4std::vector<G4UIcommand*> newTreePCP;
 
 };
 
