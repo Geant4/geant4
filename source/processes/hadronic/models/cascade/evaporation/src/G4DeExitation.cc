e8b593a2f944dde5e0afe54ca538af17f658bfda
Aatos Heikkinen <miheikki@cern.ch>
1999-11-11 14:14:59 +0000

    File Added
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@2550 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/processes/hadronic/models/cascade/evaporation/src/G4DeExitation.cc
@@ -0,0 +1,51 @@
+//## begin module%382AC8D797F0.cm preserve=no
+//	  %X% %Q% %Z% %W%
+//## end module%382AC8D797F0.cm
+
+//## begin module%382AC8D797F0.cp preserve=no
+//## end module%382AC8D797F0.cp
+
+//## Module: G4DeExitation%382AC8D797F0; Subprogram body
+//## Subsystem: evaporation::src_evap%382AC8A21C18
+//## Source file: /tmp/miheikki/geant4/source/processes/hadronic/models/cascade/evaporation/src/G4DeExitation.cc
+
+//## begin module%382AC8D797F0.additionalIncludes preserve=no
+#include "G4DeExitation.hh"
+//## end module%382AC8D797F0.additionalIncludes
+
+//## begin module%382AC8D797F0.includes preserve=yes
+//## end module%382AC8D797F0.includes
+
+//## begin module%382AC8D797F0.declarations preserve=no
+//## end module%382AC8D797F0.declarations
+
+//## begin module%382AC8D797F0.additionalDeclarations preserve=yes
+//## end module%382AC8D797F0.additionalDeclarations
+
+
+// Class G4DeExitation 
+
+G4DeExitation::G4DeExitation()
+  //## begin G4DeExitation::G4DeExitation%.hasinit preserve=no
+  //## end G4DeExitation::G4DeExitation%.hasinit
+  //## begin G4DeExitation::G4DeExitation%.initialization preserve=yes
+  //## end G4DeExitation::G4DeExitation%.initialization
+{
+  //## begin G4DeExitation::G4DeExitation%.body preserve=yes
+  //## end G4DeExitation::G4DeExitation%.body
+}
+
+
+G4DeExitation::~G4DeExitation()
+{
+  //## begin G4DeExitation::~G4DeExitation%.body preserve=yes
+  //## end G4DeExitation::~G4DeExitation%.body
+}
+
+
+// Additional Declarations
+  //## begin G4DeExitation%382AC71DC230.declarations preserve=yes
+  //## end G4DeExitation%382AC71DC230.declarations
+
+//## begin module%382AC8D797F0.epilog preserve=yes
+//## end module%382AC8D797F0.epilog
