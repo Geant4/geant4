dbb2fe201b6713e4b9df7eec19d13ef637e0a5cf
Johannes-Peter Wellisch <hpw@cern.ch>
1999-11-16 16:55:18 +0000

    fixing a memory leak found by Hisaya
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@2625 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/management/src/G4HadronicProcess.cc
+++ source/processes/hadronic/management/src/G4HadronicProcess.cc
@@ -5,7 +5,7 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: G4HadronicProcess.cc,v 1.3 1999-11-16 16:49:01 hpw Exp $
+// $Id: G4HadronicProcess.cc,v 1.4 1999-11-16 16:55:18 hpw Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
  // HPW to implement the choosing of an element for scattering.
@@ -119,6 +119,9 @@
     theIsoPC.SetProducer("WaitingForTypeidToBeAvailableInCompilers");
     
     delete anIsoResult;
+
+//--- Clear secondary list of aResult     
+    aResult->Clear();
     
     return &theIsoPC;
   }
