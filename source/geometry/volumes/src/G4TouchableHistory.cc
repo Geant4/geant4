78f79acc8943b59f4cb9679263606f78710e2e37
John Apostolakis <John.Apostolakis@cern.ch>
1999-11-26 18:51:27 +0000

    Moved GetTranslation and GetRotation from inline file to source file -- as they use static variables in the methods themselves.
       As these methods are virtual anyway,  this should not create a real penalty
    for most of their uses.
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@2821 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/geometry/volumes/src/G4TouchableHistory.cc
+++ source/geometry/volumes/src/G4TouchableHistory.cc
@@ -5,7 +5,7 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: G4TouchableHistory.cc,v 1.1 1999-01-07 16:08:49 gunter Exp $
+// $Id: G4TouchableHistory.cc,v 1.2 1999-11-26 18:51:27 japost Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // 
@@ -15,5 +15,33 @@
 
 G4TouchableHistory::~G4TouchableHistory()
 {
+}
+const G4ThreeVector& G4TouchableHistory::GetTranslation(G4int depth) const
+{
+   // The value returned will change at the next call
+   //  Copy it if you want to use it!
+   //   
+   static G4ThreeVector currTranslation;
+   if(depth==0.0) {
+      return ftlate;
+   }else{
+      currTranslation= fhistory.GetTransform(CalculateHistoryIndex(depth)).NetTranslation();
+      return currTranslation;
+   }
+}
+
+const G4RotationMatrix* G4TouchableHistory::GetRotation(G4int depth) const
+{
+   // The value returned will change at the next call
+   //  Copy it if you want to use it!
+   //
+   static G4RotationMatrix rotM;
+
+   if(depth==0.0) {
+      return &frot;
+   }else{
+      rotM= fhistory.GetTransform(CalculateHistoryIndex(depth)).NetRotation();
+      return &rotM;
+   }
 }
 
