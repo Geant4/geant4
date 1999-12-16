5e450db4b81d387032e2c5b3e50fb76140bcf873
johna <johna@cern.ch>
1999-12-16 14:12:34 +0000

    Added replica volumes.
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@3136 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/visualization/test/src/MyDetectorConstruction.cc
+++ source/visualization/test/src/MyDetectorConstruction.cc
@@ -5,7 +5,7 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: MyDetectorConstruction.cc,v 1.4 1999-12-15 14:54:34 gunter Exp $
+// $Id: MyDetectorConstruction.cc,v 1.5 1999-12-16 14:12:34 johna Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // 
@@ -31,6 +31,7 @@
 #include "G4ThreeVector.hh"
 #include "G4Transform3D.hh"
 #include "G4PVPlacement.hh"
+#include "G4PVReplica.hh"
 #include "G4SDManager.hh"
 #include "G4VisAttributes.hh"
 #include "G4Colour.hh"
@@ -139,17 +140,102 @@ G4VPhysicalVolume* MyDetectorConstruction::Construct()
 
   //-------------------------------------------- Boolean solids
 
+  G4Tubs* cylinder1 = new G4Tubs("Cylinder #1",20*cm,50*cm,30*cm,0,2*M_PI);
   G4Box* box1 = new G4Box("Box #1",20*cm,30*cm,40*cm);
-  G4Tubs* Cylinder1 = new G4Tubs("Cylinder #1",20*cm,50*cm,30*cm,0,2*M_PI);
-  G4RotationMatrix* rm = new G4RotationMatrix;
-  rm->rotateZ(40*deg);
-  G4UnionSolid* b1UnionC1 = new G4UnionSolid("Box+Cylinder", Cylinder1, box1,
-					     rm,G4ThreeVector(30.*cm,0.,0.));
+  G4Box* box2 = new G4Box("Box #2",10*cm,20*cm,30*cm);
+  G4RotationMatrix* rm1 = new G4RotationMatrix;
+  rm1->rotateZ(20*deg);
+  G4RotationMatrix* rm2 = new G4RotationMatrix;
+  rm2->rotateZ(60*deg);
+
+  G4UnionSolid* cyl1Ubox1 =
+    new G4UnionSolid("cylinder1-union-box1", cylinder1, box1,
+		     rm1,G4ThreeVector(30.*cm,0.,0.));
+  G4UnionSolid* cyl1Ubox1Ubox2 =
+    new G4UnionSolid("cylinder1-union-box1-union-box2", cyl1Ubox1, box2,
+		     rm2,G4ThreeVector(0.,40*cm,0.));
   G4LogicalVolume * union_log
-    = new G4LogicalVolume(b1UnionC1,Ar,"union_L",0,0,0);
+    = new G4LogicalVolume(cyl1Ubox1Ubox2,Ar,"union_L",0,0,0);
   new G4PVPlacement(0,G4ThreeVector(200.*cm,-50*cm,0.*cm),
                     "union_phys",union_log,experimantalHall_phys,
                     false,0);
+
+  //----------- Tubes, replicas(!?) and daughter boxes
+
+  const G4double eps = 10 * mm;
+  const G4double alp = 10 * mrad;
+  G4int iCopy;
+
+  G4Tubs* tube = new G4Tubs("tube",20*cm,50*cm,30*cm,0,2*M_PI);
+  G4LogicalVolume * tube_log
+    = new G4LogicalVolume(tube,Ar,"tube_L",0,0,0);
+  new G4PVPlacement(0,G4ThreeVector(-200.*cm,0.,0.*cm),
+                    "tube_phys",tube_log,experimantalHall_phys,
+                    false,0);
+
+  G4Tubs* divided_tube = new G4Tubs
+    ("divided_tube",
+     20*cm,50*cm,30*cm,0,M_PI/3.);
+  G4LogicalVolume * divided_tube_log
+    = new G4LogicalVolume(divided_tube,Ar,"divided_tube_L",0,0,0);
+  /************ 
+  new G4PVReplica("divided_tube_phys",divided_tube_log,tube_log,
+  		  kPhi,6,M_PI/3.);
+  *********/
+  for (iCopy = 0; iCopy < 6; iCopy++) {
+    new G4PVPlacement
+      (G4Transform3D(G4RotationMatrix().rotateZ(iCopy*M_PI/3.),
+		     G4ThreeVector()),
+       divided_tube_log,"divided_tube_phys",tube_log,
+       false,iCopy);
+  }
+
+  G4Tubs* divided_tube_inset = new G4Tubs
+    ("divided_tube_inset",
+     20*cm+2.*eps,50*cm-2.*eps,30*cm-2.*eps,2.*alp,M_PI/3.-4.*alp);
+  G4LogicalVolume * divided_tube_inset_log
+    = new G4LogicalVolume(divided_tube_inset,Ar,"divided_tube_inset_L",0,0,0);
+  new G4PVPlacement(0,G4ThreeVector(),
+                    divided_tube_inset_log,"divided_tube_inset_phys",
+		    divided_tube_log,false,0);
+
+  G4Tubs* sub_divided_tube = new G4Tubs
+    ("sub_divided_tube",
+     20*cm+2.*eps,50*cm-2.*eps,30*cm-2.*eps,0.,M_PI/12.-alp);
+  G4LogicalVolume * sub_divided_tube_log
+    = new G4LogicalVolume(sub_divided_tube,Ar,"sub_divided_tube_L",0,0,0);
+  /************ 
+  new G4PVReplica("sub_divided_tube_phys",
+  		  sub_divided_tube_log,divided_tube_inset_log,
+		  kPhi,4,M_PI/12.-alp);
+  ************/
+  for (iCopy = 0; iCopy < 4; iCopy++) {
+    new G4PVPlacement
+      (G4Transform3D(G4RotationMatrix().rotateZ(iCopy*(M_PI/12.-alp)+2.*alp),
+		     G4ThreeVector()),
+       sub_divided_tube_log,"sub_divided_tube_phys",divided_tube_inset_log,
+       false,iCopy);
+  }
+  
+  G4Box* daughter_box = new G4Box("daughter_box",4.*cm,3.*cm,25.*cm);
+  G4LogicalVolume * daughter_box_log
+    = new G4LogicalVolume(daughter_box,Ar,"daughter_box_L",0,0,0);
+  G4Box* grand_daughter_box = new G4Box("grand_daughter_box",1*cm,2*cm,5*cm);
+  G4LogicalVolume * grand_daughter_box_log
+    = new G4LogicalVolume(grand_daughter_box,Ar,"grand_daughter_box_L",0,0,0);
+  G4Box* grand_daughter_box2 = new G4Box("grand_daughter_box2",1*cm,2*cm,5*cm);
+  G4LogicalVolume * grand_daughter_box_log2
+    = new G4LogicalVolume
+    (grand_daughter_box2,Ar,"grand_daughter_box2_L",0,0,0);
+  new G4PVPlacement(0,G4ThreeVector(-2*cm,0.,0.),
+                    grand_daughter_box_log,"grand_daughter_box_phys",
+		    daughter_box_log,false,0);
+  new G4PVPlacement(0,G4ThreeVector(2*cm,0.,0.),
+                    grand_daughter_box_log2,"grand_daughter_box_phys2",
+		    daughter_box_log,false,0);
+  new G4PVPlacement(0,G4ThreeVector(40*cm,5.*cm,0.),
+                    daughter_box_log,"daughter_box_phys",
+		    sub_divided_tube_log,false,0);
 
   //------------------------------------------------ sensitive detectors
 
