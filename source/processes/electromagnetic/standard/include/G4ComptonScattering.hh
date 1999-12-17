1b947922065146e8c05ee3557cb5f6d24411d9dc
Michel Maire <Michel.Maire@cern.ch>
1999-12-17 15:48:41 +0000

    documentation
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@3158 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/electromagnetic/standard/include/G4ComptonScattering.hh
+++ source/processes/electromagnetic/standard/include/G4ComptonScattering.hh
@@ -5,21 +5,12 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: G4ComptonScattering.hh,v 1.2 1999-12-15 14:51:45 gunter Exp $
+// $Id: G4ComptonScattering.hh,v 1.3 1999-12-17 15:48:41 maire Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
-// 
-// ------------------------------------------------------------
-//      GEANT 4 class header file --- Copyright CERN 1995
-//      CERN Geneva Switzerland
-//
-//      For information related to this code contact:
-//      CERN, CN Division, ASD group
-//      History: first implementation, based on object model of
-//      2nd December 1995, G.Cosmo
 //      ------------ G4ComptonScattering physics process ------
 //                   by Michel Maire, April 1996
-// ************************************************************
+//
 // 10-06-96, updated by M.Maire 
 // 21-06-96, SetCuts implementation, M.Maire
 // 06-01-97, crossection table + meanfreepath table, M.Maire
@@ -28,6 +19,13 @@
 // 12-03-97, new physics scheme again
 // 13-08-98, new methods SetBining()  PrintInfo() 
 // ------------------------------------------------------------
+
+// class description
+//
+// inherit from G4VDiscreteProcess
+//
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 #ifndef G4ComptonScattering_h
 #define G4ComptonScattering_h 1
@@ -43,34 +41,59 @@
 #include "G4Electron.hh"
 #include "G4Step.hh"
 
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 class G4ComptonScattering : public G4VDiscreteProcess
  
 { 
-  public:
+  public:  // with description
  
      G4ComptonScattering(const G4String& processName ="compt");
  
     ~G4ComptonScattering();
 
      G4bool IsApplicable(const G4ParticleDefinition&);
+       // true for Gamma only.
      
      void SetPhysicsTableBining(G4double lowE, G4double highE, G4int nBins);
+       // Allows to define the binning of the PhysicsTables, 
+       // before to build them.
      
      void BuildPhysicsTable(const G4ParticleDefinition& GammaType);
+       // It builds the total CrossSectionPerAtom table, for Gamma,
+       // and for every element contained in the elementTable.
+       // It builds the MeanFreePath table, for Gamma,
+       // and for every material contained in the materialTable.       
+       // This function overloads a virtual function of the base class.
+       // It is invoked by the G4ParticleWithCuts::SetCut() method. 
      
-     void PrintInfoDefinition(); 
+     void PrintInfoDefinition();
+       // Print few lines of informations about the process: validity range,
+       // origine ..etc..
+       // Invoked by BuildPhysicsTable(). 
      
      G4double GetMeanFreePath(const G4Track& aTrack,
                               G4double previousStepSize,
                               G4ForceCondition* condition);
-
+       // It returns the MeanFreePath of the process for the current track :
+       // (energy, material)
+       // The previousStepSize and G4ForceCondition* are not used.
+       // This function overloads a virtual function of the base class.		      
+       // It is invoked by the ProcessManager of the Particle.
+       
      G4double GetMicroscopicCrossSection(G4DynamicParticle* aDynamicGamma,
-                                         G4Element*         anElement); 
+                                         G4Element*         anElement);
+       // It returns the total CrossSectionPerAtom of the process, 
+       // for the current DynamicGamma (energy), in anElement.					 
+					  
 
      G4VParticleChange* PostStepDoIt(const G4Track& aTrack,
                                     const G4Step& aStep);
-
+       // It computes the final state of the process (at end of step),
+       // returned as a ParticleChange object.			    
+       // This function overloads a virtual function of the base class.
+       // It is invoked by the ProcessManager of the Particle.
+               
   protected:
 
      virtual G4double ComputeCrossSectionPerAtom(G4double GammaEnergy, 
