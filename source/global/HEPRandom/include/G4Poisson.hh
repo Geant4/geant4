d6f41cd53877c0f82016abb3f0025f4914e1c0cc
Michel Maire <Michel.Maire@cern.ch>
2000-01-06 14:13:08 +0000

    correction for troncated G4long: value
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@3169 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/global/HEPRandom/include/G4Poisson.hh
+++ source/global/HEPRandom/include/G4Poisson.hh
@@ -5,7 +5,7 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: G4Poisson.hh,v 1.4 1999-11-16 17:31:35 gcosmo Exp $
+// $Id: G4Poisson.hh,v 1.5 2000-01-06 14:13:08 maire Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // 
@@ -50,7 +50,7 @@ inline G4long G4Poisson(G4double mean)
   t = sqrt(-2*log(RandFlat::shoot()));
   y = twopi*RandFlat::shoot();
   t *= cos(y);
-  value = mean + t*sqrt(mean);
+  value = mean + t*sqrt(mean) + 0.5;
   if(value <= 0) {return 0;}
   if(value >= limit) { return G4long(limit);}
   return G4long(value);
