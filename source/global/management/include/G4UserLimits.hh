400912f190c496427926ac9b42cf0d4618bddc7a
Hisaya Kurashige <kurasige@phys.sci.kobe-u.ac.jp>
2000-02-16 14:08:02 +0000

    Added a new member of fType  by H.Kurashige
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@3448 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/global/management/include/G4UserLimits.hh
+++ source/global/management/include/G4UserLimits.hh
@@ -5,7 +5,7 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: G4UserLimits.hh,v 1.4 1999-11-23 15:00:04 gcosmo Exp $
+// $Id: G4UserLimits.hh,v 1.5 2000-02-16 14:08:02 kurasige Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 // 
 //
@@ -20,6 +20,7 @@
 // 
 // 01-11-97: change GetMaxAllowedStep(), Hisaya Kurashige
 // 08-04-98: new data members, mma
+// 02-16-00: add a member of Type, Hisaya Kurashige 
 //
 #ifndef G4USERLIMITS_HH
 #define G4USERLIMITS_HH
@@ -33,6 +34,12 @@ class G4UserLimits
 public:  // with description
 
   G4UserLimits(G4double ustepMax = DBL_MAX,
+               G4double utrakMax = DBL_MAX,
+               G4double utimeMax = DBL_MAX,
+	       G4double uekinMin = 0.,
+	       G4double urangMin = 0. );
+  G4UserLimits(const G4String& type = "base",
+	       G4double ustepMax = DBL_MAX,
                G4double utrakMax = DBL_MAX,
                G4double utimeMax = DBL_MAX,
 	       G4double uekinMin = 0.,
@@ -57,6 +64,15 @@ public:  // with description
   virtual void SetUserMaxTime(G4double utimeMax);
   virtual void SetUserMinEkine(G4double uekinMin);
   virtual void SetUserMinRange(G4double urangMin);
+
+  const G4String & GetType() const;
+  void  SetType(const G4String& type);
+  // Set/Get type name for UserLimits
+  // This type member is supposed to be used to check
+  // real class type for each object of UserLimits
+  // In other words, users who uses special their UserLimits class
+  // derived from this base class should name their class by using
+  // this type member.  
  
 protected:  // with description
 
@@ -65,6 +81,8 @@ protected:  // with description
   G4double fMaxTime;        // max time
   G4double fMinEkine;       // min kinetic energy  (only for charged particles)
   G4double fMinRange;       // min remaining range (only for charged particles)
+
+  G4String fType;           // type name
 };
 
 #include "G4UserLimits.icc"
