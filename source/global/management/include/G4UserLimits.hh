a08352e6949c052a6a0c63cbf187057ce89881c8
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2000-02-17 16:13:08 +0000

    - Removed default argument in constructor for fType. - Organised comments for reference manual.
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@3457 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/global/management/include/G4UserLimits.hh
+++ source/global/management/include/G4UserLimits.hh
@@ -5,7 +5,7 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: G4UserLimits.hh,v 1.5 2000-02-16 14:08:02 kurasige Exp $
+// $Id: G4UserLimits.hh,v 1.6 2000-02-17 16:13:06 gcosmo Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 // 
 //
@@ -20,7 +20,7 @@
 // 
 // 01-11-97: change GetMaxAllowedStep(), Hisaya Kurashige
 // 08-04-98: new data members, mma
-// 02-16-00: add a member of Type, Hisaya Kurashige 
+// 02-16-00: add fType member and accessors, Hisaya Kurashige 
 //
 #ifndef G4USERLIMITS_HH
 #define G4USERLIMITS_HH
@@ -38,7 +38,7 @@ public:  // with description
                G4double utimeMax = DBL_MAX,
 	       G4double uekinMin = 0.,
 	       G4double urangMin = 0. );
-  G4UserLimits(const G4String& type = "base",
+  G4UserLimits(const G4String& type,
 	       G4double ustepMax = DBL_MAX,
                G4double utrakMax = DBL_MAX,
                G4double utimeMax = DBL_MAX,
@@ -47,13 +47,12 @@ public:  // with description
   virtual ~G4UserLimits();
 
 public:  // with description
-
-  // If a Logical Volume has a G4UserLimits object, 
-  // the Step length should be limited as shorter 
-  // than MaxAllowedStep in the volume.
-  // In the current design, the other limits are irrelavant in tracking
   
   virtual G4double GetMaxAllowedStep(const G4Track&);  
+  // If a Logical Volume has a G4UserLimits object, the Step length should
+  // be limited as shorter than MaxAllowedStep in the volume.
+  // In the current design, the other limits are irrelevant in tracking
+
   virtual G4double GetUserMaxTrackLength(const G4Track&) ;
   virtual G4double GetUserMaxTime (const G4Track&);
   virtual G4double GetUserMinEkine(const G4Track&);
@@ -67,12 +66,11 @@ public:  // with description
 
   const G4String & GetType() const;
   void  SetType(const G4String& type);
-  // Set/Get type name for UserLimits
-  // This type member is supposed to be used to check
-  // real class type for each object of UserLimits
-  // In other words, users who uses special their UserLimits class
-  // derived from this base class should name their class by using
-  // this type member.  
+  // Set/Get type name for UserLimits.
+  // This type member is supposed to be used to check real class types for
+  // each concrete instantiation of G4UserLimits. In other words, users who
+  // use special classes derived from this base class should name their class
+  // with a proper identifier.  
  
 protected:  // with description
 
