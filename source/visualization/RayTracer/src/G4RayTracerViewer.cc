25b8691bb9de9a5ccc81dd87b7a9723b3c4500d6
johna <johna@cern.ch>
2000-02-24 12:21:16 +0000

    Corrected for zoom, etc.
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@3522 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/visualization/RayTracer/src/G4RayTracerViewer.cc
+++ source/visualization/RayTracer/src/G4RayTracerViewer.cc
@@ -5,7 +5,7 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: G4RayTracerViewer.cc,v 1.1 2000-02-23 16:03:53 johna Exp $
+// $Id: G4RayTracerViewer.cc,v 1.2 2000-02-24 12:21:16 johna Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 
 #include "G4RayTracerViewer.hh"
@@ -27,24 +27,34 @@ G4RayTracerViewer::~G4RayTracerViewer() {}
 void G4RayTracerViewer::SetView() {
   G4RayTracer* theTracer = 
     (G4RayTracer*) fSceneHandler.GetGraphicsSystem();
+
   if (fVP.GetFieldHalfAngle() == 0.) { // Orthogonal (parallel) projection.
     G4cout << 
       "G4RayTracerViewer::SetView: orthogonal projection not yet implemented."
 	   << G4endl;
     return;
   }
-  G4double bRadius =  // See G4ViewParameters for following procedure.
-    fSceneHandler.GetScene() -> GetExtent().GetExtentRadius();
-  if(bRadius<=0.) bRadius = 1.;
-  G4double cDistance = fVP.GetCameraDistance(bRadius);
-  G4ThreeVector eyePosition =
-    fVP.GetCurrentTargetPoint() + cDistance * fVP.GetViewpointDirection();
-  theTracer ->
-    SetViewSpan(200. * fVP.GetFieldHalfAngle() / theTracer -> GetNColumn());
-  theTracer -> SetTargetPosition(fVP.GetCurrentTargetPoint());
-  theTracer -> SetEyePosition(eyePosition);
-  theTracer -> SetHeadAngle(0.);  // Still to be figured out?????????
-  theTracer -> SetLightDirection(-fVP.GetActualLightpointDirection());
+
+  // Get radius of scene, etc.  (See G4OpenGLViewer::SetView().)
+  // Note that this procedure properly takes into account zoom, dolly and pan.
+  const G4Point3D& targetPoint = fVP.GetCurrentTargetPoint();
+  G4double radius =  // See G4ViewParameters for following procedure.
+    fSceneHandler.GetScene()->GetExtent().GetExtentRadius();
+  if(radius<=0.) radius = 1.;
+  const G4double cameraDistance = fVP.GetCameraDistance(radius);
+  const G4Point3D cameraPosition =
+    targetPoint + cameraDistance * fVP.GetViewpointDirection().unit();
+  const G4double nearDistance  = fVP.GetNearDistance(cameraDistance,radius);
+  const G4double frontHalfHeight = fVP.GetFrontHalfHeight(nearDistance,radius);
+  const G4double frontHalfAngle = atan(frontHalfHeight / nearDistance);
+
+  // Calculate and set ray tracer parameters.
+  theTracer->
+    SetViewSpan(200. * frontHalfAngle / theTracer->GetNColumn());
+  theTracer->SetTargetPosition(targetPoint);
+  theTracer->SetEyePosition(cameraPosition);
+  theTracer->SetHeadAngle(0.);  // Still to be figured out?????????
+  theTracer->SetLightDirection(-fVP.GetActualLightpointDirection());
 }
 
 
@@ -58,5 +68,5 @@ void G4RayTracerViewer::DrawView() {
   G4std::ostrstream ost(fileName, 100);
   ost << "g4RayTracer." << fShortName << '_' << fFileCount++ << ".jpeg"
       << G4std::ends;
-  theTracer -> Trace(fileName);
+  theTracer->Trace(fileName);
 }
