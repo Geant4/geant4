5774ab5faab05c7ac9cfe4a8cb070f7088dc5a9e
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2000-02-25 15:57:43 +0000

    Created dummy implementation
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@3557 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/geometry/solids/BREPS/src/G4BSplineSurfaceWithKnots.cc
@@ -0,0 +1,12 @@
+// This code implementation is the intellectual property of
+// the GEANT4 collaboration.
+//
+// By copying, distributing or modifying the Program (or any work
+// based on the Program) you indicate your acceptance of this statement,
+// and all its terms.
+//
+// $Id: G4BSplineSurfaceWithKnots.cc,v 1.1 2000-02-25 15:57:43 gcosmo Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+#include "G4BSplineSurfaceWithKnots.hh"
+
