05ac5114ee665f5931c19fff32ed5cf9597507f3
Vladimir Grichine <Vladimir.Grichine@cern.ch>
2000-02-25 16:57:15 +0000

    cosmetics to prevent /0. and sqrt(<0.)
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@3566 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/geometry/magneticfield/include/G4LineSection.hh
+++ source/geometry/magneticfield/include/G4LineSection.hh
@@ -5,13 +5,16 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: G4LineSection.hh,v 1.2 1999-12-15 14:49:47 gunter Exp $
+// $Id: G4LineSection.hh,v 1.3 2000-02-25 16:57:02 grichine Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 //
 //  A utility class that calculates the distance of a point from a 
 //   line section.
 //
+// History:
+// ~1996 J. Apostolakis, first version
+// 25.02.00 V. Grichine, cosmetics against /0. and sqrt(<0.)
 //  
 
 #include "globals.hh" 
--- source/geometry/magneticfield/src/G4LineSection.cc
+++ source/geometry/magneticfield/src/G4LineSection.cc
@@ -5,10 +5,11 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: G4LineSection.cc,v 1.2 1999-12-15 14:49:49 gunter Exp $
+// $Id: G4LineSection.cc,v 1.3 2000-02-25 16:57:15 grichine Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // typedef double    G4double;  
+
 #include "G4LineSection.hh" 
 
 G4LineSection::G4LineSection( const G4ThreeVector& PntA, 
@@ -16,8 +17,15 @@ G4LineSection::G4LineSection( const G4ThreeVector& PntA,
 { 
   EndpointA= PntA; 
   VecAtoB=PntB-PntA; 
-  
-  inverse_square_distAB=1.0 / VecAtoB.mag2();
+  G4double distABsquared = VecAtoB.mag2() ;  
+  if ( distABsquared == 0.0)
+  {
+    G4Exception("Equal points in G4LineSection::G4LineSection: line->point ?") ;
+  }
+  else
+  {
+    inverse_square_distAB=1.0 / distABsquared ;
+  }
 }
 
 G4double G4LineSection::Dist( G4ThreeVector OtherPnt ) const
@@ -36,23 +44,31 @@ G4double G4LineSection::Dist( G4ThreeVector OtherPnt ) const
   unit_projection= inner_prod * InvsqDistAB();
 
   if( (0. <= unit_projection ) && (unit_projection <= 1.0 ) )
-     dist_sq= sq_VecAZ -  unit_projection * inner_prod;
-  else{
+  {
+     dist_sq= sq_VecAZ -  unit_projection * inner_prod ;
+  }
+  else
+ {
      //  The perpendicular from the point to the line AB meets the line
      //   in a point outside the line segment!
-     //
-     if( unit_projection < 0. ) {
+     
+     if( unit_projection < 0. ) 
+     {
         // A is the closest point
         dist_sq= sq_VecAZ;  
-     }else{
+     }
+     else
+     {
         // B is the closest point
+
 	G4ThreeVector   EndpointB = EndpointA + VecAtoB;
         G4ThreeVector   VecBZ =     OtherPnt - EndpointB;
         dist_sq =  VecBZ.mag2();
      }
   }
+  if( dist_sq < 0.0 ) dist_sq = 0.0 ;
 
-  return sqrt(dist_sq);  
+  return sqrt(dist_sq) ;  
 }
 
 G4double G4LineSection::Distline( const G4ThreeVector& OtherPnt, 
