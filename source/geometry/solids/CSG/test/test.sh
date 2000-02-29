d256bb7cdec98a61adb4eca06e05baa20556efa3
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2000-02-29 13:26:38 +0000

    Moved to sh shell and use G4WORKDIR, G4TARGET
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@3615 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/geometry/solids/CSG/test/test.sh
+++ source/geometry/solids/CSG/test/test.sh
@@ -1,19 +1,10 @@
-#!/usr/local/bin/bash
+#!/bin/sh
 echo "Run on `hostname`, which is a `uname -a` machine" | tee -a test.out
 echo `date` >> test.out
-k=$TESTTARGET
-ln -s ../../../../../bin  .
 for i in *.cc
 do
   j=`basename $i .cc`
-  TESTTARGET=$j
-  export TESTTARGET
-  gmake
-  echo Test output for $TESTTARGET... >>test.out
-  ./bin/$G4SYSTEM/$TESTTARGET >>test.out 2>&1;
-  # ../../../../bin/$G4SYSTEM/$TESTTARGET >>test.out 2>&1;
+  gmake G4TARGET=$j
+  echo Test output for $j... >> test.out
+  $G4WORKDIR/bin/$G4SYSTEM/$j >> test.out 2>&1;
 done
-TESTTARGET=$k
-export TESTTARGET
-
-
