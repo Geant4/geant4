0816baef2aaddc38b28b39b34a0028e1b08a2765
Michel Maire <Michel.Maire@cern.ch>
2000-02-29 12:01:52 +0000

    previous version commited by error. 1.6 is equivalent to 1.4
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@3610 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/novice/N02/include/ExN02PhysicsList.hh
+++ examples/novice/N02/include/ExN02PhysicsList.hh
@@ -5,46 +5,82 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: ExN02PhysicsList.hh,v 1.5 2000-02-28 17:58:48 maire Exp $
+// $Id: ExN02PhysicsList.hh,v 1.6 2000-02-29 12:01:52 maire Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
-
+// 
+// ------------------------------------------------------------
+//	GEANT 4 class header file 
+//
+//      This class is a class derived from G4VUserPhysicsList
+//      for constructing all particles and processes.
+//
+//	History
+//        first version              10  Jan. 1998 by H.Kurashige
+// ------------------------------------------------------------
 #ifndef ExN02PhysicsList_h
 #define ExN02PhysicsList_h 1
 
 #include "G4VUserPhysicsList.hh"
 #include "globals.hh"
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+class G4PhotoElectricEffect;
+class G4ComptonScattering;
+class G4GammaConversion;
+
+class G4MultipleScattering;
+
+class G4eIonisation;
+class G4eBremsstrahlung;
+class G4eplusAnnihilation;
+
+class G4MuIonisation;
+class G4MuBremsstrahlung;
+class G4MuPairProduction;
+
+class G4hIonisation;
 
 class ExN02PhysicsList: public G4VUserPhysicsList
 {
   public:
     ExN02PhysicsList();
-   ~ExN02PhysicsList();
+    virtual ~ExN02PhysicsList();
 
   protected:
     // Construct particle and physics
-    void ConstructParticle();
-    void ConstructProcess();
- 
-    void SetCuts();
+    virtual void ConstructParticle();
+    virtual void ConstructProcess();
+
+    // 
+    virtual void SetCuts();
 
    
   protected:
     // these methods Construct particles 
-    void ConstructBosons();
-    void ConstructLeptons();
-    void ConstructMesons();
-    void ConstructBaryons();
+    virtual void ConstructBosons();
+    virtual void ConstructLeptons();
+    virtual void ConstructMesons();
+    virtual void ConstructBaryons();
 
   protected:
   // these methods Construct physics processes and register them
-    void ConstructGeneral();
-    void ConstructEM();
+    virtual void ConstructGeneral();
+    virtual void ConstructEM();
+    
+  private:
+
+    G4PhotoElectricEffect* thePhotoElectricEffect;
+    G4ComptonScattering*   theComptonScattering;
+    G4GammaConversion*     theGammaConversion;
+    
+    G4MultipleScattering*  theeminusMultipleScattering;
+    G4eIonisation*         theeminusIonisation;
+    G4eBremsstrahlung*     theeminusBremsstrahlung;
+    
+    G4MultipleScattering*  theeplusMultipleScattering;
+    G4eIonisation*         theeplusIonisation;
+    G4eBremsstrahlung*     theeplusBremsstrahlung;
+    G4eplusAnnihilation*   theeplusAnnihilation;
 };
 
 #endif
