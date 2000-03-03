9ac69e1887babaf04347488003ed768a9ab29bf4
Vladimir Grichine <Vladimir.Grichine@cern.ch>
2000-03-03 09:16:15 +0000

    approximation for generation of X-ray TR in radiator
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@3656 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ examples/extended/electromagnetic/TestEm8/include/Em8RegularXrayTRmodel.hh
@@ -0,0 +1,47 @@
+// This code implementation is the intellectual property of
+// the RD44 GEANT4 collaboration.
+//
+// By copying, distributing or modifying the Program (or any work
+// based on the Program) you indicate your acceptance of this statement,
+// and all its terms.
+//
+// $Id: Em8RegularXrayTRmodel.hh,v 1.1 2000-03-03 09:15:21 grichine Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+// 
+///////////////////////////////////////////////////////////////////////////
+// 
+// Model describing a radiator of X-ray transition radiation.  
+// Thicknesses of plates and gas gaps are fixed.
+// We suppose that:
+// formation zone ~ mean thickness << absorption length
+// for each material and in the range 1-100 keV. This allows us to simplify
+// interference effects in radiator stack (GetStackFactor method).
+// 
+// 
+// History:
+// 10.02.00 V. Grichine, first version 
+//
+
+
+#ifndef Em8RegularXrayTRmodel_h
+#define Em8RegularXrayTRmodel_h 1
+
+#include "G4VFastSimulationModel.hh"
+#include "G4ForwardXrayTR.hh"
+
+#include "Em8XrayTRmodel.hh"
+
+class Em8RegularXrayTRmodel : public Em8XrayTRmodel
+{
+public:
+
+   Em8RegularXrayTRmodel (G4LogicalVolume *anEnvelope,G4double,G4double);
+  ~Em8RegularXrayTRmodel ();
+
+  // Pure virtual function from base class
+
+  G4double GetStackFactor( G4double energy, G4double gamma, G4double varAngle);
+};
+
+#endif
