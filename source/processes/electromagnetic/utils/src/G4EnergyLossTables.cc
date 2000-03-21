7e668041992da682e4f049f1d03823bdb59392e7
Laszlo Urban <Laszlo.Urban@cern.ch>
2000-03-21 11:16:44 +0000

    changes in lowenergy extrapolation
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@3741 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/electromagnetic/utils/src/G4EnergyLossTables.cc
+++ source/processes/electromagnetic/utils/src/G4EnergyLossTables.cc
@@ -5,7 +5,7 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: G4EnergyLossTables.cc,v 1.10 1999-12-15 14:52:04 gunter Exp $
+// $Id: G4EnergyLossTables.cc,v 1.11 2000-03-21 11:16:44 urban Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // ------------------------------------------------------------------- 
@@ -80,7 +80,10 @@ void G4EnergyLossTables::Register(
 
   if (scaledKineticEnergy<t.theLowestKineticEnergy) {
 
-     dEdx = (*dEdxTable)(materialIndex)->GetValue(
+    // dEdx = (*dEdxTable)(materialIndex)->GetValue(
+    //          t.theLowestKineticEnergy,isOut);
+     dEdx = sqrt(scaledKineticEnergy/t.theLowestKineticEnergy)
+            *(*dEdxTable)(materialIndex)->GetValue(
               t.theLowestKineticEnergy,isOut);
 
   } else if (scaledKineticEnergy>t.theHighestKineticEnergy) {
@@ -128,7 +131,10 @@ void G4EnergyLossTables::Register(
 
   if (scaledKineticEnergy<t.theLowestKineticEnergy) {
 
-    Range = scaledKineticEnergy/t.theLowestKineticEnergy*
+   // Range = scaledKineticEnergy/t.theLowestKineticEnergy*
+   //         (*rangeTable)(materialIndex)->GetValue(
+   //           t.theLowestKineticEnergy,isOut);
+    Range = sqrt(scaledKineticEnergy/t.theLowestKineticEnergy)*
             (*rangeTable)(materialIndex)->GetValue(
               t.theLowestKineticEnergy,isOut);
 
