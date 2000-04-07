08b33d132db7de4d8b780a96f9d8f5067ba4b7eb
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2000-04-07 11:07:19 +0000

    Copied from CSG and adapted
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@3833 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/geometry/solids/specific/test/test.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "Run on `hostname`, which is a `uname -a` machine" | tee -a test.out
+echo `date` >> test.out
+for i in *.cc
+do
+  j=`basename $i .cc`
+  gmake G4TARGET=$j
+  echo Test output for $j... >> test.out
+  $G4WORKDIR/bin/$G4SYSTEM/$j >> test.out 2>&1;
+done
