2b7bf35c534e296592ad2620e79b80b458cc4571
John Apostolakis <John.Apostolakis@cern.ch>
2000-04-12 18:30:03 +0000

    New test output (corrected & in line with 1-4) for Helical stepper (choice 5)
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@3921 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/geometry/magneticfield/test/testPropagateMagField.cc
+++ source/geometry/magneticfield/test/testPropagateMagField.cc
@@ -5,7 +5,7 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: testPropagateMagField.cc,v 1.2 1999-12-15 14:49:50 gunter Exp $
+// $Id: testPropagateMagField.cc,v 1.3 2000-04-12 18:30:03 japost Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 //  
