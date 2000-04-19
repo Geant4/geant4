e2995719a5acd9ea05660bcf2d90dd366972ca0c
Veronique Lefebure <Veronique.Lefebure@cern.ch>
2000-04-19 13:20:53 +0000

    first continuous energly loss implementation.
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@3957 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/electromagnetic/lowenergy/include/G4LowEnergyBremsstrahlung.hh
+++ source/processes/electromagnetic/lowenergy/include/G4LowEnergyBremsstrahlung.hh
@@ -5,7 +5,7 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: G4LowEnergyBremsstrahlung.hh,v 1.10 2000-04-04 13:47:34 lefebure Exp $
+// $Id: G4LowEnergyBremsstrahlung.hh,v 1.11 2000-04-19 13:20:53 lefebure Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // 
@@ -17,19 +17,18 @@
 //      CERN, IT Division, ASD group
 //      ------------ G4LowEnergyBremsstrahlung physics process ------
 //                     by A.Forti  1999/03/27 19:18:13
+//
+// 18.04.2000 V.Lefebure
+// - First implementation of continuous energy loss.
+//
+//
 // ************************************************************
 
 #ifndef G4LowEnergyBremsstrahlung_h
 #define G4LowEnergyBremsstrahlung_h 1
 
-// Base Class Headers
-//#include "G4VDiscreteProcess.hh"
 #include "G4eLowEnergyLoss.hh"
-
-// Contained Variables Headers
 #include "G4LowEnergyUtilities.hh"
-#include "G4Electron.hh"
-#include "G4Positron.hh"
 
 class G4LowEnergyBremsstrahlung : public G4eLowEnergyLoss{
  
@@ -42,8 +41,6 @@ public:
   G4bool IsApplicable(const G4ParticleDefinition&);
 
   void SetCutForLowEnSecPhotons(G4double);
-
-  void SetPhysicsTableBining(G4double lowE, G4double highE, G4int nBins);
   
   void PrintInfoDefinition();
   
@@ -58,35 +55,34 @@ public:
   G4VParticleChange* PostStepDoIt(const G4Track& track,         
 				  const G4Step&  step);                 
   
-  G4double GetLambda(G4double KineticEnergy,G4Material* material);
   
+  G4double GetEnergyLossWithCut(const G4double AtomicNumber,
+                                const G4double KineticEnergy,
+                                const G4double Tcut) ;
   
-protected:
+private:
   
   void BuildCrossSectionTable();
   void BuildMeanFreePathTable();
   void BuildATable();
   void BuildBTable();
   void BuildZVec();
+  void BuildLambdaTable(const G4ParticleDefinition& aParticleType);
 
-  void ComputePartialSumSigma(G4double KineticEnergy, 
-			      const G4Material* aMaterial);
+  void ComputePartialSumSigma(const G4double KineticEnergy, 
+			      const G4Material* aMaterial,
+			      const G4double threshold);
   
 private:
   
-  G4double ComputeA(G4int Z, G4double ElectKinEnergy); // interpolation
-  G4double ComputeB(G4int Z, G4double ElectKinEnergy); // parametrized formula
-  
-  G4double ComputeBremLoss(G4double Z, G4double natom, G4double T,
-			   G4double Cut, G4double x);
-  
-  G4double ComputeXYPolynomial(G4double x,G4double y,G4int xSize,
-			       G4int ySize,const G4double coeff[]);
-  
-  G4double ComputePositronCorrFactorLoss(G4double AtomicNumber,
-					 G4double KineticEnergy,
-					 G4double GammaEnergyCut);
-    
+  G4double ComputeA(const G4int Z,const  G4double ElectKinEnergy); // interpolation
+  G4double ComputeB(const G4int Z,const  G4double ElectKinEnergy); // parametrized formula
+  G4double GetCrossSection(const G4double AtomicNumber,
+                           const G4double KineticEnergy) ;
+  G4double GetCrossSectionWithCut(const G4double AtomIndex,
+				  const G4double IncEnergy,
+		   	 	  const G4double CutEnergy);
+     
   G4Element* SelectRandomAtom(G4Material* aMaterial) const;
 
   
