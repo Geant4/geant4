4c0d147040038cd2417157dd209122b2c98581b3
Maria Grazia Pia <Maria.Grazia.Pia@cern.ch>
2000-05-07 17:34:54 +0000

    First version of G4VhEnergyLossModel.hh
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@4087 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/processes/electromagnetic/lowenergy/include/G4VhEnergyLossModel.hh
@@ -0,0 +1,63 @@
+// This code implementation is the intellectual property of
+// the GEANT4 collaboration.
+//
+// By copying, distributing or modifying the Program (or any work
+// based on the Program) you indicate your acceptance of this statement,
+// and all its terms.
+//
+// -------------------------------------------------------------------
+//
+// GEANT4 Class file
+//
+// For information related to this code contact:
+// Geant4 Collaboration
+//
+// File name:     G4VhEnergyLossModel
+//
+// Author:        Maria Grazia Pia (MariaGrazia.Pia@ge.infn.it)
+// 
+// Creation date: 7 May 2000
+//
+// Modifications: 
+//          
+// Class Description: 
+//
+// Abstract base class for hadron energy loss model
+//
+// Class Description: End 
+//
+// -------------------------------------------------------------------
+//
+
+#ifndef G4VHENERGYLOSSMODEL_HH
+#define G4VHENERGYLOSSMODEL_HH
+
+#include "globals.hh"
+
+class G4ParticleDefinition;
+
+class G4VhEnergyLossModel 
+{
+
+public:
+
+  G4VhEnergyLossModel();
+
+  virtual ~G4VhEnergyLossModel();
+
+  virtual G4double EnergyLoss() const = 0;
+
+  virtual G4double LowEnergyLimit() const = 0;
+ 
+  virtual G4double HighEnergyLimit() const = 0;
+
+  virtual G4bool IsInCharge(G4double energy, 
+			    const G4ParticleDefinition* partDef) const = 0;
+
+protected:
+
+
+private:
+
+
+#endif
