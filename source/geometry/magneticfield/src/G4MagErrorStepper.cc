5691eee6c1b42e1f319b3c670a415a31a909a414
John Apostolakis <John.Apostolakis@cern.ch>
2000-05-09 11:41:00 +0000

    Modified DistChord to check whether the chord endpoint are the same point.
       Also fixed comments.
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@4095 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/geometry/magneticfield/src/G4MagErrorStepper.cc
+++ source/geometry/magneticfield/src/G4MagErrorStepper.cc
@@ -5,7 +5,7 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: G4MagErrorStepper.cc,v 1.6 1999-12-15 14:49:49 gunter Exp $
+// $Id: G4MagErrorStepper.cc,v 1.7 2000-05-09 11:41:00 japost Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 #include "G4MagErrorStepper.hh"
@@ -61,11 +61,26 @@ G4MagErrorStepper::Stepper( const G4double yInput[],
 G4double
 G4MagErrorStepper::DistChord()   const 
 {
-  // Soon: must check whether h/R > 2 pi  !!
-  //  Method below is good only for < 2 pi
+  // Estimate the maximum distance from the curve to the chord
+  //
+  //  We estimate this using the distance of the midpoint to 
+  //  chord (the line between 
+  // 
+  //  Method below is good only for angle deviations < 2 pi, 
+  //   This restriction should not a problem for the Runge cutta methods, 
+  //   which generally cannot integrate accurately for large angle deviations.
+  G4double distLine, distChord; 
 
-  return G4LineSection::Distline( fMidPoint, fInitialPoint, fFinalPoint );
-  // This is a class method that gives distance of Mid 
-  //  from the Chord between the Initial and Final points.
+  if (fInitialPoint != fFinalPoint) {
+     distLine= G4LineSection::Distline( fMidPoint, fInitialPoint, fFinalPoint );
+     // This is a class method that gives distance of Mid 
+     //  from the Chord between the Initial and Final points.
+
+     distChord = distLine;
+  }else{
+     distChord = (fMidPoint-fInitialPoint).mag();
+  }
+
+  return distChord;
 }
  
