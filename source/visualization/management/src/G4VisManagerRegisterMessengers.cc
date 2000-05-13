f71332b7f8654e2944ff2950415410976bb35c48
johna <johna@cern.ch>
2000-05-13 07:34:11 +0000

    Expanded comments considerably.
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@4163 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/visualization/management/src/G4VisManagerRegisterMessengers.cc
+++ source/visualization/management/src/G4VisManagerRegisterMessengers.cc
@@ -5,7 +5,7 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: G4VisManagerRegisterMessengers.cc,v 1.13 2000-05-04 08:52:10 johna Exp $
+// $Id: G4VisManagerRegisterMessengers.cc,v 1.14 2000-05-13 07:34:11 johna Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // 
@@ -45,228 +45,402 @@ void G4VisManager::RegisterMessengers () {
 
 (Extract for working note.)
 
+* means "not implemented yet".
+
+
 Introduction
 ============
 
-This note defines the concepts of scene, scene handler and viewers.
+This note defines the concepts of scene, scene handler and viewer.
+
+A scene handler can handle only one scene at any one time but can have
+any number of viewers.
+
+A scene can be attached to any number of scene handlers.
+
+It is important to realise that there is also the concept of a "current"
+scene, "current" scene handler and "current" viewer which are maintained
+by the G4VisManager.  It is usually the last object created or selected
+or operated upon.
+
+Note that there is also a concept of a "standard view" which is that which
+comfortably includes all components of the scene.  From this follow the
+concept of standard target point, etc.  It is the responsibility of
+each viewer to apply its view parameters relatively, taking into account
+the scene it represents.  This is a dynamic operation and the scene handler
+and its viewers must be smart enough to know when to recalculate
+graphics-system-dependent quantities when the scene or the view parameters
+change.
+
 
 Scenes
 ======
 
-We introduce the concept of a graphics-system-independent scene, which
-is a list of visualizable objects, such as detector components
-(G4PhysicalVolumeModels), hits, trajectories.  The G4VisManager
-manages a list of scenes.
+A scene is a list of visualizable objects, such as detector components,
+hits, trajectories, axes, etc.
 
-* means "not implemented yet".
+Scenes are graphics-system-independent.
+
+The G4VisManager has a list of scenes.
 
 /vis/scene/create [<scene-name>]
-/vis/scene/list [-l] [<scene-name>]
+  default       auto-generated name
+  This scene becomes current.
+
+/vis/scene/list [<scene-name>] [<verbosity>]
+  default:           all             0
+  Current scene remains current.
+
 /vis/scene/select [<scene-name>]
+  default:      current scene name
+  This scene becomes current.
+
 * /vis/scene/edit
+
 /vis/scene/remove <scene-name>
+  Current scene can change or become invalid.
+
 /vis/scene/add/ghosts [<particle>]
+  default:                 all
+  Adds to current scene.
+
 * /vis/scene/add/ghost [<particle>] [<physical-volume-name>]
 *                      [<copy-no>] [<depth>]
+
 /vis/scene/add/logicalVolume <logical-volume-name> [<depth>] 
-/vis/scene/add/volume [<physical-volume-name>] [<copy-no>] [<depth>] 
-/vis/scene/include/hits [<sensitive-volume-name>] (argument not impl'd yet.)
-/vis/scene/include/trajectories [<sensitive-volume-name>] (do.)
+  default:                                             1
+  Adds to current scene.
+
+/vis/scene/add/volume [<physical-volume-name>] [<copy-no>] [<depth>]
+  default:               <world-volume-name>        0         -1
+  Adds to current scene.
+
+/vis/scene/include/hits [<sensitive-volume-name>]
+  default:              (argument not impl'd yet.)
+  Causes hits, if any, to be drawn at the end of processiing an event.
+  Includes in current scene.
+
+/vis/scene/include/trajectories [<sensitive-volume-name>]
+  default:                      (argument not impl'd yet.)
+  Causes trajectories, if any, to be drawn at the end of processiing an event.
+  Includes in current scene.
+
 * /vis/scene/include/transientObjects
+  Includes in current scene.
+
 * /vis/scene/set/hitOption accumulate|byEvent
+  Affects current scene.
+
 * /vis/scene/set/notifyOption immediate|delayed
+  Affects current scene.
+
 * /vis/scene/set/modelingStyle [<modeling-style>]
+  Affects current scene.
+
 /vis/scene/notifyHandlers
+  Refreshes all viewers of current scene.
+  Does not issue "show" (see /vis/viewer/show).
+
 * /vis/scene/add/axes
+  Adds to current scene.
+
 * /vis/scene/add/text
+  Adds to current scene.
+
 
 Scene Handlers
 ==============
 
-A scene handler is a graphics-system-specific thing that turns a scene
-into something that the graphics system can understand.
+A scene handler is an object which knows how to interpret a scene for a
+specific graphics system.
 
-The commands would look something like:
+Each scene handler handles one scene and has, in general, any number of
+viewers.
+
+The G4VisManager has a list of scene handlers.
 
 /vis/sceneHandler/create
-       <graphics-system> [<scene-handler-name>] [<scene-name>]
+       <graphics-system> [<scene-handler-name>]   [<scene-name>]
+  default:                auto-generated name   current scene name
+  This scene handler becomes current.
+
 /vis/sceneHandler/attach [<scene-name>]
-/vis/sceneHandler/list
+  default:             current scene name
+  Attaches scene to current scene handler.
+
+/vis/sceneHandler/list [<scene-handler-name>] [<verbosity>]
+  default:                     all                  0
+  Current scene handler remains current.
+
 /vis/sceneHandler/select [<scene-handler-name>]
+  default:            current scene handler name
+  This scene handler becomes current.
+
 /vis/sceneHandler/remove <scene-handler-name>
+  Current scene handler can change or become invalid.
+
 * /vis/sceneHandler/processScene
+  Refreshes all viewers of current scene handler.
+  Does not issue "show" (see /vis/viewer/show).
+
 * /vis/sceneHandler/notifyEndOfProcessing
+  Issues "show" for each viewer of current scene handler.
 
 
 Viewers
 =======
 
-A viewer opens windows and draws to the screen, hardcopy, etc.  It can
-be dumb (a non-interactive window) or intelligent (respond to mouse
-clicks, spawn other windows, change viewpoint, etc.).
+A viewer opens windows and draws to the screen or writes to file for
+off-line viewing or hardcopy, etc.  It can be dumb (a non-interactive
+window) or intelligent (respond to mouse clicks, spawn other windows,
+change viewpoint, etc.).
+
+Most viewer commands respond to the viewer "short name", which is the
+name up to the first space character, if any.  Thus, a viewer name can
+contain spaces but must be unique up to the first space.
 
 * /vis/viewer/clear
+  Affects current viewer.
 
-/vis/viewer/create [<scene-handler>]           [<viewer-name>]
-  default:     current scene handler name    auto-generated name
+* /vis/viewer/clone
+  Clone becomes current viewer.
+
+/vis/viewer/create [<scene-handler>]           [<viewer-name>]     [<pixels>]
+  default:     current scene handler name    auto-generated name      600
+  Pixel size of square window (hint only).
+  This viewer becomes current.
 
 /vis/viewer/list   [<viewer-name>]    [<verbosity>]
   default:             all                  0
+  Current viewer remains current.
 
 /vis/viewer/select <viewer-name>
   default:          no default
+  This viewer becomes current.
 
 /vis/viewer/refresh [<viewer-name>]
   default:       current viewer name
+  This viewer becomes current.
 
 /vis/viewer/remove <viewer-name>
   default:          no default
+  Current viewer can change or become invalid.
 
 /vis/viewer/reset [<viewer-name>]
   default:      current viewer name
+  Resets view parameters to defaults.
+  This viewer becomes current.
+
+(Now follow some viewer/set commands.  A possible syntax is:
+
+* /vis/viewer/set <parameter-name> <parameter-value> [<parameter-value>]...
+
+but this would mean re-inventing parameter parsing which is already
+contained in many G4UIcommand subclasses.  To re-use this code we have
+to know the type of the parameter, so we need separate commands.)
+
+* /vis/viewer/set/all <from-viewer-name>
+  Copies view parameters from from-viewer to current viewer.
+  Affects current viewer.
 
 * /vis/viewer/set/style wireframe|surface
 *or* /vis/viewer/set/surface   ) ( opposites, i.e., one
 *or* /vis/viewer/set/wireframe ) ( negates the other.
+* The first is preferred.  We need parameter name completion.
+* If someone gives a wrong parameter, G4UIparameter should be more informative
+* about possible values.
+  Affects current viewer.
 
-* /vis/viewer/set/edge true|false
+* /vis/viewer/set/edge [true|false]
+  default:              true
+  Affects current viewer.
 
-* /vis/viewer/set/hiddenLine true|false
+* /vis/viewer/set/hiddenEdge  [true|false]
+  default:                       true
+  Affects current viewer.
 
-From /vis/set/:
+(Here follow some culling commands.  A possible (preferred) syntax is:
 
-* /vis/viewer/set/cull
-*or* /vis/viewer/cull
+* /vis/viewer/set/culling global|coveredDaughters|invisible|density [true|false]
+  default:                                                           true
+  Affects current viewer.
+
+Again, this would benefit from parameter name completion.)
+
+* /vis/viewer/set/cull  [true|false]
+*or* /vis/viewer/cull  [true|false]
+  default:              true
   Global culling flag.  Does not change specific culling flags.
+  Affects current viewer.
 
-* /vis/viewer/set/cull_covered_daughters
-*or* /vis/viewer/cull_covered_daughters
-   Cull (i.e., do not Draw) daughters covered by opaque mothers.
+* /vis/viewer/set/cullCoveredDaughters
+*or* /vis/viewer/cullCoveredDaughters  [true|false]
+  default:                                true
+  Cull (i.e., do not Draw) daughters covered by opaque mothers.
+  Affects current viewer.
 
-* /vis/viewer/set/cull_invisible_objects
-*or* /vis/viewer/cull_invisible_objects
-   Cull (i.e., do not Draw) "invisible" objects.
+* /vis/viewer/set/cullInvisibleObjects
+*or* /vis/viewer/cullInvisibleObjects  [true|false]
+  default:                             true
+  Cull (i.e., do not Draw) "invisible" objects.
+  Affects current viewer.
 
-* /vis/viewer/set/cull_by_density
-*or* /vis/viewer/cull_by_density
-  Cull (i.e., do not Draw) geometry objects with density less than
-  specified - on/off and density (g / cm3).
+* /vis/viewer/set/cullByDensity
+*or* /vis/viewer/cullByDensity  [true|false]
+  default:                       true
+  Cull (i.e., do not Draw) geometry objects with density less than specified.
+  Affects current viewer.
 
-* /vis/viewer/set/marker_choices
-  Choices for marker drawing (hidden by surfaces, etc.).
+* /vis/viewer/set/hiddenMarker  [true|false]
+  default:                       true
+  Affects current viewer.
 
-* /vis/viewer/set/section_plane
+* /vis/viewer/set/sectionPlane ...
   Set plane for drawing section (DCUT).  Specify plane by x y z units nx ny nz,
   e.g., for a y-z plane at x = 1 cm:
   /vis/viewer/set/section_plane on 1 0 0 cm 1 0 0
+  Affects current viewer.
 
-* /vis/viewer/set/sides
-  Number of sides per circle in polygon approximation.
+* /vis/viewer/set/cutawayPlane ...
+  Set plane(s) for cutaway views.
+  Affects current viewer.
 
-From /vis/camera/:
+* /vis/viewer/set/circleApproximationParameter [<number-of-sides-per-circle>]
+*or*              lineSegmentsPerCircle [<number-of-sides-per-circle>]
+  default:                                         24
+  Number of sides per circle in polygon/polyhedron graphical representation
+    of objects with curved lines/surfaces.
+  Affects current viewer.
 
-* /vis/viewer/set/dollyBy
-*or* /vis/viewer/dollyBy
-  Moves the camera in by this distance.  Reset with /vis/viewer/reset.
+* /vis/viewer/set/dolly [<increment>]
+*or* /vis/viewer/dolly [<increment>]
+  default:                  0
+  Moves the camera incrementally in by this distance.
+  Affects current viewer.
 
-* /vis/viewer/set/dollyTo
-*or* /vis/viewer/dollyTo
-  Moves the camera to this distance from standard position..  Reset with
-  /vis/viewer/reset.
+* /vis/viewer/set/dollyTo [<distance>]
+*or* /vis/viewer/dollyTo [<distance>]
+  default:                    0
+  Moves the camera in this distance relative to standard target point.
+  Affects current viewer.
 
 *special* /vis/viewer/set/orbit
   Orbits the scene about the up-vector, lights fixed to the scene.  Draws N
-   frames, the camera rotated Delta-beta about the up-vector each frame.
+    frames, the camera rotated Delta-beta about the up-vector each frame.
+  Affects current viewer.
 
-* /vis/viewer/set/panBy
-*or*  /vis/viewer/panBy
-  Moves the camera incrementatly right and up by these amounts.  Reset with
-  /vis/viewer/reset.
+* /vis/viewer/set/pan [<x-increment>] [<y-increment>]
+*or*  /vis/viewer/pan [<x-increment>] [<y-increment>]
+  default:                 0           0
+  Moves the camera incrementatly right and up by these amounts.
+  Affects current viewer.
 
-* /vis/viewer/set/panTo
-*or*  /vis/viewer/panTo
-  Moves the camera to this position right and up relative to standard position
-  point.  Reset with /vis/viewer/reset.
+* /vis/viewer/set/panTo [<x>] [<y>]
+*or*  /vis/viewer/panTo [<x>] [<y>]
+  default:                0     0
+  Moves the camera to this position right and up relative to standard target
+    point.
+  Affects current viewer.
 
-* /vis/viewer/set/projection_style orthogonal|perspective [<field-angle>]
+* /vis/viewer/set/projection_style orthogonal|perspective [<field-angle>] [deg|rad]
+  default:                                                     30          deg
 *or* /vis/viewer/set/orthgonal
 *or* /vis/viewer/set/perspective [<field-angle>] [deg|rad]
-  default:                        30          deg
+  default:                           30          deg
+  Affects current viewer.
 
 *special* /vis/viewer/set/spin
   Spins the scene about the up-vector, lights fixed to the camera.  Draws N
   frames, the scene rotated Delta-beta about the up-vector each frame.
+  Affects current viewer.
 
-* /vis/viewer/set/viewpoint
+* /vis/viewer/set/viewpoint <theta> <phi> [deg|rad]
+  default:                     0     0     deg
+*or* /vis/viewer/set/viewpointVector [<x>] [<y>] [<z>]
+  default:                             0     0     1
   Set direction from target to camera.  Also changes lightpoint direction if
   lights are set to move with camera.
-*or* /vis/viewer/set/viewpointThetaPhi <theta> <phi> [deg|rad]
-*or* /vis/viewer/set/viewpointVector [<x>] [<y>] [<z>]
-  default:                         0     0     1
+  Affects current viewer.
 
-* /vis/viewer/set/window_size_hint
-  Size in pixels (Square window `size x size').  Affects future viewer/create
-  commands (hint only).
+* /vis/viewer/set/zoom [<factor>]
+  default:                 1
+  Multiplies magnification by this factor.
+  Affects current viewer.
 
-* /vis/viewer/set/zoomBy
-  Multiplies magnification by this factor.  Reset with /vis/viewwer/reset.
-
-* /vis/viewer/set/zoomTo
-  Magnifies by this factor relative to standard view.  Reset with
-  /vis/viewer/reset.
-
-From /vis/lights:
+* /vis/viewer/set/zoomTo [<factor>]
+  default:                   1
+  Magnifies by this factor relative to standard view.
+  Affects current viewer.
 
 * /vis/viewer/set/lights move-with-camera|move-with-object
 *or* /vis/viewer/set/lightsMoveWithCamera ) ( Opposites, i.e., one
 *or* /vis/viewer/set/lightsMoveWithObject ) ( negates the other.
+  Affects current viewer.
 
-* /vis/viewer/set/lightsThetaPhi <theta> <phi> [deg|rad]
+* /vis/viewer/set/lights <theta> <phi> [deg|rad]
+  default:                   0     0    deg
 * /vis/viewer/set/lightsVector [<x>] [<y>] [<z>]
-  default:                   0     0     1
+  default:                       0     0     1
+  Affects current viewer.
 
-* /vis/viewer/notifyOption immediate|delayed
+* /vis/viewer/notifyOption immediate|delayed ?Issue of "show" after "set"?
 
-* /vis/viewer/notifyHandler
-
-* /vis/viewer/clone
+* /vis/viewer/notifyHandler ??
 
 /vis/viewer/show [<viewer-name>]
   default:     current viewer name
+  This viewer becomes current.
 
 /vis/viewer/update - synonym for /vis/viewer/show.
+
+
+Attributes (nothing implemented yet)
+==========
+
+The G4VisManager also keeps a list of visualization attributes which can
+be created and changed and attributed to visualizable objects.
+
+* /vis/attributes/create [<vis-attributes-name>]
+  default:                 auto-generated name
+  These attributes become current.
+
+* /vis/attributes/set/colour [<vis-attributes-name>] [<r>]  [<g>] [<b>] [<o>]
+* /vis/attributes/set/color  [<vis-attributes-name>] [<r>]  [<g>] [<b>] [<o>]
+  default:                   current attributes name   1      1     1     1
+  Sets colour (red, green, blue, opacity).
+  Affects current vis attributes.
+
+* /vis/scene/set/attributes <logical-volume-name>
+  Associates current vis attributes with logical volume.  (Do we need to
+    provide possibility of resetting to original attributes?)
+  (Move to scene when implemented.)
 
 
 Global Commands
 ===============
 
-* /vis/verbose
-* /vis/copy/sceneAndView <from-viewer-name> <to-viewer-name>
+* /vis/verbose [<verbosity-integer>]
+  default:               0
 
 
 Compound Commands
 =================
 
-* /vis/create/view OGLSXM
+* /vis/open  OGLSXM                   /vis/sceneHandler/create $1
+                                      /vis/viewer/create
 
-would be
+* /vis/draw Calorimiter               /vis/scene/create
+                                      /vis/scene/add/volume $1
+                                      /vis/sceneHandler/attach
+                                      /vis/viewer/refresh
+                                      /vis/viewer/show
 
-/vis/scene/create
-/vis/sceneHandler/create $1
-/vis/sceneHandler/attach
-/vis/viewer/create
-
-and
-
-* /vis/draw Calorimiter
-
-would be
-
-/vis/scene/add/volume $1
-/vis/scene/notifyHandlers
-/vis/viewer/show
-
-or some such.
+* /vis/specify <logical-volume-name>  /vis/scene/create
+                                      /vis/scene/add/logicalVolume $1
+                                      /vis/sceneHandler/attach
+                                      /vis/viewer/refresh
+                                      /vis/viewer/show
+                                      /geometry/print $1
 
   ******************************************************************/
 
