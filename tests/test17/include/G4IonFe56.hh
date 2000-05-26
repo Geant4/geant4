90c6e5e087291807621b636cf8ba185f847d9488
Maria Grazia Pia <Maria.Grazia.Pia@cern.ch>
2000-05-26 15:49:18 +0000

    Added, the responsible forgot to commit them
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@4378 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ tests/test17/include/G4IonFe56.hh
@@ -0,0 +1,82 @@
+// This code implementation is the intellectual property of
+// the GEANT4 collaboration.
+//
+// By copying, distributing or modifying the Program (or any work
+// based on the Program) you indicate your acceptance of this statement,
+// and all its terms.
+//
+// $Id: G4IonFe56.hh,v 1.1 2000-05-26 15:48:51 pia Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+// 
+// ------------------------------------------------------------
+//      GEANT 4 class header file
+//
+//      For information related to this code contact:
+//      CERN, CN Division, ASD group
+//      History: first implementation, based on object model of
+//      4-th April 1996, G.Cosmo
+// ****************************************************************
+//  Added particle definitions, H.Kurashige, 19 April 1996
+//  Revised, G.Cosmo, 6 June 1996
+//  Added not static GetEnergyCuts() and GetLengthCuts(), G.Cosmo, 11 July 1996
+// ----------------------------------------------------------------
+
+// Class Description:
+// The new ion Fe56+ is defined as G4VIon.
+// Each class inheriting from G4VIon
+// corresponds to a particle type; one and only one
+// instance for each class is guaranteed.
+// Class Description - end
+
+#ifndef G4IonFe56_h
+#define G4IonFe56_h 1
+
+#include "globals.hh"
+#include "G4ios.hh"
+#include "G4VIon.hh"
+
+// ######################################################################
+// ###                          IonFe56                                 ###
+// ######################################################################
+
+class G4IonFe56 : public G4VIon
+{
+ private:
+   static G4IonFe56 theIonFe56;
+   static G4double  theIonFe56LengthCut;
+   static G4double* theIonFe56KineticEnergyCuts;
+
+public: // Without description
+
+   G4IonFe56(
+       const G4String&     aName,        G4double            mass,
+       G4double            width,        G4double            charge,   
+       G4int               iSpin,        G4int               iParity,    
+       G4int               iConjugation, G4int               iIsospin,   
+       G4int               iIsospin3,    G4int               gParity,
+       const G4String&     pType,        G4int               lepton,      
+       G4int               baryon,       G4int               encoding,
+       G4bool              stable,       G4double            lifetime,
+       G4DecayTable        *decaytable
+   );
+   virtual ~G4IonFe56();
+  
+   static G4IonFe56*    IonFe56Definition();
+   static G4IonFe56*    IonFe56() {return &theIonFe56;}
+   static G4double GetCuts() {return theIonFe56LengthCut;}   
+   static G4double* GetCutsInEnergy() {return theIonFe56KineticEnergyCuts;};
+
+   void SetCuts(G4double aCut); 
+};
+
+inline void G4IonFe56::SetCuts(G4double aCut)
+{
+  CalcEnergyCuts(aCut);
+  theIonFe56LengthCut = theCutInMaxInteractionLength;  
+  theIonFe56KineticEnergyCuts = theKineticEnergyCuts;
+  
+}
+
+#endif
+
---
+++ tests/test17/src/G4IonFe56.cc
@@ -0,0 +1,76 @@
+// This code implementation is the intellectual property of
+// the GEANT4 collaboration.
+//
+// By copying, distributing or modifying the Program (or any work
+// based on the Program) you indicate your acceptance of this statement,
+// and all its terms.
+//
+// $Id: G4IonFe56.cc,v 1.1 2000-05-26 15:49:18 pia Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+// 
+// ----------------------------------------------------------------------
+//      GEANT 4 class implementation file
+//
+//      For information related to this code contact:
+//      CERN, CN Division, ASD Group
+//      History: first implementation, based on object model of
+//      4th April 1996, G.Cosmo
+// **********************************************************************
+//  Added by J.L.Chuma, TRIUMF, 27 June 1996
+//  Added  G4IonFe56::IonFe56Definition() by H.Kurashige 27 June 1996
+//  Fixed  parameters by H.Kurashige, 25 Apr 1998
+// ----------------------------------------------------------------------
+
+#include "g4std/fstream"
+#include "g4std/iomanip"
+
+#include "G4IonFe56.hh"
+
+// ######################################################################
+// ###                           IonFe56                                ###
+// ######################################################################
+
+G4IonFe56::G4IonFe56(
+       const G4String&     aName,        G4double            mass,
+       G4double            width,        G4double            charge,   
+       G4int               iSpin,        G4int               iParity,    
+       G4int               iConjugation, G4int               iIsospin,   
+       G4int               iIsospin3,    G4int               gParity,
+       const G4String&     pType,        G4int               lepton,      
+       G4int               baryon,       G4int               encoding,
+       G4bool              stable,       G4double            lifetime,
+       G4DecayTable        *decaytable )
+ : G4VIon( aName,mass,width,charge,iSpin,iParity,
+           iConjugation,iIsospin,iIsospin3,gParity,pType,
+           lepton,baryon,encoding,stable,lifetime,decaytable )
+{
+}
+
+G4IonFe56::~G4IonFe56()
+{
+}
+
+// ......................................................................
+// ...                 static member definitions                      ...
+// ......................................................................
+//     
+//    Arguments for constructor are as follows
+//               name             mass          width         charge
+//             2*spin           parity  C-conjugation
+//          2*Isospin       2*Isospin3       G-parity
+//               type    lepton number  baryon number   PDG encoding
+//             stable         lifetime    decay table 
+
+G4IonFe56 G4IonFe56::theIonFe56(
+              "IonFe56",    52.0308*GeV,       0.0*MeV,  +26.0*eplus, 
+		    0,              +1,             0,          
+		    0,               0,             0,             
+	    "nucleus",               0,            +56,           0,
+		 true,            -1.0,          NULL
+);
+
+G4IonFe56* G4IonFe56::IonFe56Definition(){return &theIonFe56;}
+// initialization for static cut values
+G4double   G4IonFe56::theIonFe56LengthCut;
+G4double*  G4IonFe56::theIonFe56KineticEnergyCuts;
