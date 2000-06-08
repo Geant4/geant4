cfce207f9898b46b6a3b4c9e91ae565c0328ce40
Guy Barrand <barrand@cern.ch>
2000-06-08 12:55:31 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@4490 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- tests/test201/test201.cc
+++ tests/test201/test201.cc
@@ -1,11 +1,12 @@
 // This code implementation is the intellectual property of
+
 // the GEANT4 collaboration.
 //
 // By copying, distributing or modifying the Program (or any work
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: test201.cc,v 1.7 2000-06-08 12:51:44 barrand Exp $
+// $Id: test201.cc,v 1.8 2000-06-08 12:55:31 barrand Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // 
