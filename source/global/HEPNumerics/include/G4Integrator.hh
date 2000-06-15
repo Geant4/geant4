5971c23b930ca603caeb4b6872e360efbfc6cdb4
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2000-06-15 17:31:23 +0000

    Changed implementation of G4Integrator class to be a template class. Needed for porting on Solaris CC compiler which does not support templated member functions.
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@4609 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/global/HEPNumerics/include/G4Integrator.hh
+++ source/global/HEPNumerics/include/G4Integrator.hh
@@ -5,17 +5,17 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: G4Integrator.hh,v 1.3 1999-11-16 17:30:58 gcosmo Exp $
+// $Id: G4Integrator.hh,v 1.4 2000-06-15 17:31:23 gcosmo Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // Class description:
 //
-// Class collecting template integrator methods for generic funtions.
+// Template class collecting integrator methods for generic funtions.
 
 // History:
 //
 // 04.09.99 V.Grichine, first implementation based on G4SimpleIntegration class
-//                      and H.-P. Wellisch, G.Cosmo, and E.Cherniaev advises
+//                      H.P.Wellisch, G.Cosmo, and E.Cherniaev advises
 // 08.09.99 V.Grichine, methods involving orthogonal polynomials
 // 
 
@@ -26,133 +26,84 @@
 #include "globals.hh"
 #include <math.h>
 
+template <class T, class F>
 class G4Integrator
 {
-  public:
+  public:  // with description
 
    G4Integrator(){;}
-
   ~G4Integrator(){;}
 
-  // Simpson integration method
-
- template <class T, class F> G4double 
- Simpson( T& typeT, F f, G4double a, G4double b, G4int n ) ;
-
- template <class T, class F> G4double 
- Simpson( T* ptrT, F f, G4double a, G4double b, G4int n ) ;
-
+ G4double Simpson( T& typeT, F f, G4double a, G4double b, G4int n ) ;
+ G4double Simpson( T* ptrT, F f, G4double a, G4double b, G4int n ) ;
  G4double Simpson( G4double (*f)(G4double), 
                    G4double a, G4double b, G4int n ) ;
+  // Simpson integration method
 
-  // Adaptive Gauss method
-
- template <class T, class F> G4double 
- AdaptiveGauss( T& typeT, F f, G4double a, G4double b, G4double e ) ;
-
- template <class T, class F> G4double 
- AdaptiveGauss( T* ptrT, F f, G4double a, G4double b, G4double e ) ;
-
+ G4double AdaptiveGauss( T& typeT, F f, G4double a, G4double b, G4double e ) ;
+ G4double AdaptiveGauss( T* ptrT, F f, G4double a, G4double b, G4double e ) ;
  G4double AdaptiveGauss( G4double (*f)(G4double), 
                          G4double a, G4double b, G4double e ) ;
+  // Adaptive Gauss method
+
 
   // Integration methods involving orthogohol polynomials
 
+ G4double Legendre( T& typeT, F f, G4double a, G4double b, G4int n) ;
+ G4double Legendre( T* ptrT, F f, G4double a, G4double b, G4int n) ;
+ G4double Legendre( G4double (*f)(G4double), G4double a, G4double b, G4int n) ;
+  //
   // Methods involving Legendre polynomials  
 
-
- template <class T, class F>    
- G4double Legendre( T& typeT, F f, G4double a, G4double b, G4int n) ;
-
- template <class T, class F>    
- G4double Legendre( T* ptrT, F f, G4double a, G4double b, G4int n) ;
-
- G4double Legendre( G4double (*f)(G4double), G4double a, G4double b, G4int n) ;
-
-  // Legendre10 is very fast and accurate anough
-
- template <class T, class F>  
  G4double Legendre10( T& typeT, F f,G4double a, G4double b) ;
-
- template <class T, class F>  
  G4double Legendre10( T* ptrT, F f,G4double a, G4double b) ;
-
  G4double Legendre10( G4double (*f)(G4double), G4double a, G4double b) ;
+  //
+  // Legendre10 is very fast and accurate enough
 
+ G4double Legendre96( T& typeT, F f,G4double a, G4double b) ;
+ G4double Legendre96( T* ptrT, F f,G4double a, G4double b) ;
+ G4double Legendre96( G4double (*f)(G4double), G4double a, G4double b) ;
+  //
   // Legendre96 is very accurate and fast enough
 
- template <class T, class F>  
- G4double Legendre96( T& typeT, F f,G4double a, G4double b) ;
-
- template <class T, class F>  
- G4double Legendre96( T* ptrT, F f,G4double a, G4double b) ;
-
- G4double Legendre96( G4double (*f)(G4double), G4double a, G4double b) ;
-
+ G4double Chebyshev( T& typeT, F f, G4double a, G4double b, G4int n) ;
+ G4double Chebyshev( T* ptrT, F f, G4double a, G4double b, G4int n) ;
+ G4double Chebyshev( G4double (*f)(G4double), G4double a, G4double b, G4int n) ;
+  //
   // Methods involving Chebyshev  polynomials  
                           
- template <class T, class F>    
- G4double Chebyshev( T& typeT, F f, G4double a, G4double b, G4int n) ;
-
- template <class T, class F>    
- G4double Chebyshev( T* ptrT, F f, G4double a, G4double b, G4int n) ;
-
- G4double Chebyshev( G4double (*f)(G4double), G4double a, G4double b, G4int n) ;
-
+ G4double Laguerre( T& typeT, F f, G4double alpha, G4int n) ;
+ G4double Laguerre( T* ptrT, F f, G4double alpha, G4int n) ;
+ G4double Laguerre( G4double (*f)(G4double), G4double alpha,  G4int n) ;
+  //
   // Method involving Laguerre polynomials
 
- template <class T, class F>    
- G4double Laguerre( T& typeT, F f, G4double alpha, G4int n) ;
-
- template <class T, class F>    
- G4double Laguerre( T* ptrT, F f, G4double alpha, G4int n) ;
-
- G4double Laguerre( G4double (*f)(G4double), G4double alpha,  G4int n) ;
-
+ G4double Hermite( T& typeT, F f, G4int n) ;
+ G4double Hermite( T* ptrT, F f, G4int n) ;
+ G4double Hermite( G4double (*f)(G4double), G4int n) ;
+  //
   // Method involving Hermite polynomials
 
- template <class T, class F>    
- G4double Hermite( T& typeT, F f, G4int n) ;
-
- template <class T, class F>    
- G4double Hermite( T* ptrT, F f, G4int n) ;
-
- G4double Hermite( G4double (*f)(G4double), G4int n) ;
-
-  // Method involving Jacobi polynomials
-
- template <class T, class F>    
  G4double Jacobi( T& typeT, F f, G4double alpha, G4double beta, G4int n) ;
-
- template <class T, class F>    
  G4double Jacobi( T* ptrT, F f, G4double alpha, G4double beta, G4int n) ;
-
  G4double Jacobi( G4double (*f)(G4double), G4double alpha, 
                                            G4double beta, G4int n) ;
+  // Method involving Jacobi polynomials
 
 
  protected:
 
   // Auxiliary function for adaptive Gauss method
 
- template <class T, class F> G4double 
- Gauss( T& typeT, F f, G4double a, G4double b ) ;
-
- template <class T, class F> G4double 
- Gauss( T* ptrT, F f, G4double a, G4double b ) ;
-
+ G4double Gauss( T& typeT, F f, G4double a, G4double b ) ;
+ G4double Gauss( T* ptrT, F f, G4double a, G4double b ) ;
  G4double Gauss( G4double (*f)(G4double), G4double a, G4double b) ;
 
-  //
-
- template <class T, class F> void
- AdaptGauss( T& typeT, F f, G4double a, G4double b, 
-                            G4double e, G4double& sum, G4int& n) ;
-
- template <class T, class F> void 
- AdaptGauss( T* typeT, F f, G4double a, G4double b, 
-                            G4double e, G4double& sum, G4int& n ) ;
-
+ void AdaptGauss( T& typeT, F f, G4double a, G4double b, 
+                                 G4double e, G4double& sum, G4int& n) ;
+ void AdaptGauss( T* typeT, F f, G4double a, G4double b, 
+                                 G4double e, G4double& sum, G4int& n ) ;
  void AdaptGauss( G4double (*f)(G4double), G4double a, G4double b, 
                   G4double e, G4double& sum, G4int& n ) ;
 
