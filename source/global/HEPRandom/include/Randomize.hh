e4486d23620b54089ec277cc06d5aeabbc8e186a
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2000-08-02 10:48:21 +0000

    Added new distributions, until header in CLHEP is updated
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@4945 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/global/HEPRandom/include/Randomize.hh
+++ source/global/HEPRandom/include/Randomize.hh
@@ -5,13 +5,22 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: Randomize.hh,v 1.2 1999-11-16 17:31:35 gcosmo Exp $
+// $Id: Randomize.hh,v 1.3 2000-08-02 10:48:21 gcosmo Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 #ifndef randomize_h
 #define randomize_h 1
 
 #include <CLHEP/Random/Randomize.h>
+
+// Additional distributions ...
+//
+#include <CLHEP/Random/RandGaussQ.h>
+#include <CLHEP/Random/RandGaussT.h>
+#include <CLHEP/Random/RandPoissonQ.h>
+#include <CLHEP/Random/RandPoissonT.h>
+#include <CLHEP/Random/RandLandau.h>
+#include <CLHEP/Random/RandBit.h>
 
 #define G4UniformRand() HepRandom::getTheEngine()->flat()
 
