33be2a6a749f8de1371472d25eee7db9e45a7fcb
Michel Maire <Michel.Maire@cern.ch>
2000-08-16 12:38:56 +0000

    G4ionIonisation removed. Use G4hIonisation instead
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@5049 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- tests/test02/src/Tst02PhysicsList.cc
+++ tests/test02/src/Tst02PhysicsList.cc
@@ -5,7 +5,7 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: Tst02PhysicsList.cc,v 1.4 1999-12-15 14:54:37 gunter Exp $
+// $Id: Tst02PhysicsList.cc,v 1.5 2000-08-16 12:38:56 maire Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // 
@@ -124,7 +124,6 @@ void Tst02PhysicsList::ConstructProcess()
 #include "G4MuPairProduction.hh"
 
 #include "G4hIonisation.hh"
-#include "G4ionIonisation.hh"
 
 void Tst02PhysicsList::ConstructEM()
 {
@@ -205,7 +204,7 @@ void Tst02PhysicsList::ConstructEM()
      pmanager->SetProcessOrdering(aPairProduction, idxPostStep, 4);
      
     } else if( particleName == "GenericIon" ) {
-     G4VProcess* aionIonization = new G4ionIonisation;
+     G4VProcess* aionIonization = new G4hIonisation;
      G4VProcess* aMultipleScattering = new G4MultipleScattering();
      pmanager->AddProcess(aionIonization);
      pmanager->AddProcess(aMultipleScattering);
