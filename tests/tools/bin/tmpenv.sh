2784ae287fdbd50da888beda1c473bd6133fc5df
Geant4 Testing <geant4.testing@cern.ch>
2000-11-01 12:51:09 +0000

    put compiler workspace TMPDIR on local /tmp disk for dxplus
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@5420 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- tests/tools/bin/tmpenv.sh
+++ tests/tools/bin/tmpenv.sh
@@ -10,38 +10,48 @@ fi
 fi
 
 if [ `uname -n | grep sunasd1` ]; then
-echo "Nothing set!"
+  echo "Nothing set for sunasd"
 fi
 
 if [ `uname -n | grep hpplus` ]; then
-echo "Nothing set for limit!"
+  echo "Nothing set for limit on hpplus"
 # there is a pretty useless looking file from april in
 # the directory below - I bet we are screwed if anything goes there
 # with concurrent tests.
-if [ X$REFTREE = Xprod ]; then
-export TMPDIR=/afs/cern.ch/sw/geant4/stt/tmpdir
-else
-export TMPDIR=/afs/cern.ch/sw/geant4/stt/tmpdir
-fi
+  if [ X$REFTREE = Xprod ]; then
+    export TMPDIR=/afs/cern.ch/sw/geant4/stt/tmpdir
+  else
+    export TMPDIR=/afs/cern.ch/sw/geant4/stt/tmpdir
+  fi
 fi
 
 if [ `uname -n | grep axcnsi` ]; then
-ulimit -d 200000
-ulimit -s 20000
-echo "Nothing set for TMPDIR!"
+  ulimit -d 200000
+  ulimit -s 20000
+  echo "axcnsi ulimit -d 200000 ulimit -s 20000"
+  echo "Nothing set for TMPDIR on axcnsi!"
 fi
 
 if [ `uname -n | grep sgmedia` ]; then
-ulimit -s 200000
-ulimit -m 400000
-if [ X$REFTREE = Xprod ]; then
-export TMPDIR=/afs/cern.ch/sw/geant4/stt/tmpdir
-else
-export TMPDIR=/afs/cern.ch/sw/geant4/stt/tmpdir
+  ulimit -s 200000
+  ulimit -m 400000
+  if [ X$REFTREE = Xprod ]; then
+    export TMPDIR=/afs/cern.ch/sw/geant4/stt/tmpdir
+  else
+    export TMPDIR=/afs/cern.ch/sw/geant4/stt/tmpdir
+  fi
 fi
-fi
-
+  
 if [ `uname -n | grep dxplus` ]; then
-echo "I don't know about limits here :)...ooops...already know :("
-ulimit -d 400000
+  ulimit -d 400000
+# (current understanding) TMPDIR is compiler loader  workspace (few 100 MBytes)
+#                         G4TMP  are dependency files, object files  (up to Gbytes)
+  if [ X$REFTREE = Xprod ]; then
+    if [ ! -d /tmp/geant4tmp ]; then
+      mkdir /tmp/geant4tmp
+    fi
+    export TMPDIR=/tmp/geant4tmp
+#  else
+#  export TMPDIR=/afs/cern.ch/sw/geant4/stt/tmpdir
+  fi
 fi
