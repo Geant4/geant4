39b5d1c8e9853a80776407d8adcc6a4c53639926
Michel Maire <Michel.Maire@cern.ch>
2000-11-09 15:52:24 +0000

    add UI subdirectory eLoss
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@5529 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/electromagnetic/utils/include/G4EnergyLossMessenger.hh
+++ source/processes/electromagnetic/utils/include/G4EnergyLossMessenger.hh
@@ -5,9 +5,23 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: G4EnergyLossMessenger.hh,v 1.2 2000-10-30 06:49:46 urban Exp $
+// $Id: G4EnergyLossMessenger.hh,v 1.3 2000-11-09 15:52:23 maire Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
+//
+// Class Description:
+//  This is a messenger class to interface to exchange information
+//  between G4VEnergyLoss and UI.
+//        
+//  /process/eLoss/   directory
+//
+//   Commands :
+//
+//    rndmStep *     Randomize the proposed step by eLoss (false/true)
+//    fluct *        Switch true/false the energy loss fluctuations
+//    subsec *       Switch true/false the subcutoff generation
+//    minsubsec *    Set the min. cut for subcutoff delta in range
+//    StepFunction * Set the energy loss step limitation parameters
 // 
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
@@ -19,6 +33,7 @@
 #include "globals.hh"
 #include "G4UImessenger.hh"
 
+class G4UIdirectory;
 class G4UIcommand;
 class G4UIcmdWithABool;
 class G4UIcmdWithADoubleAndUnit;
@@ -27,7 +42,7 @@ class G4UIcmdWithADoubleAndUnit;
 
 class G4EnergyLossMessenger: public G4UImessenger
 {
-  public:
+  public:   // with description
   
     G4EnergyLossMessenger();
    ~G4EnergyLossMessenger();
@@ -35,11 +50,12 @@ class G4EnergyLossMessenger: public G4UImessenger
     void SetNewValue(G4UIcommand*, G4String);
     
   private:
-      
+
+    G4UIdirectory*             eLossDirectory;     
     G4UIcmdWithABool*          RndmStepCmd;
     G4UIcmdWithABool*          EnlossFlucCmd;
     G4UIcmdWithABool*          SubSecCmd;
-    G4UIcmdWithADoubleAndUnit* MinDeltaCutInRangeCmd;
+    G4UIcmdWithADoubleAndUnit* MinSubSecCmd;
     G4UIcommand*               StepFuncCmd;
 };
 
--- source/processes/electromagnetic/utils/src/G4EnergyLossMessenger.cc
+++ source/processes/electromagnetic/utils/src/G4EnergyLossMessenger.cc
@@ -5,7 +5,7 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: G4EnergyLossMessenger.cc,v 1.2 2000-10-30 06:50:49 urban Exp $
+// $Id: G4EnergyLossMessenger.cc,v 1.3 2000-11-09 15:52:24 maire Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // 
@@ -17,6 +17,7 @@
 
 #include "G4VEnergyLoss.hh"
 
+#include "G4UIdirectory.hh"
 #include "G4UIcommand.hh"
 #include "G4UIparameter.hh"
 #include "G4UIcmdWithABool.hh"
@@ -27,47 +28,54 @@
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
 
 G4EnergyLossMessenger::G4EnergyLossMessenger()
-{       
+{
+  eLossDirectory = new G4UIdirectory("/process/eLoss/");
+  eLossDirectory->SetGuidance("Commands for G4VEnergyLoss.");
+         
   RndmStepCmd = new G4UIcmdWithABool("/process/eLoss/rndmStep",this);
-  RndmStepCmd->SetGuidance("Randomize the proposed step for eLoss.");
+  RndmStepCmd->SetGuidance("Randomize the proposed step by eLoss.");
   RndmStepCmd->SetParameterName("choice",true);
   RndmStepCmd->SetDefaultValue(false);
   RndmStepCmd->AvailableForStates(Idle);
   
   EnlossFlucCmd = new G4UIcmdWithABool("/process/eLoss/fluct",this);
-  EnlossFlucCmd->SetGuidance("Switch on/off the energy loss fluctuation.");
+  EnlossFlucCmd->SetGuidance("Switch true/false the energy loss fluctuations.");
   EnlossFlucCmd->SetParameterName("choice",true);
   EnlossFlucCmd->SetDefaultValue(true);
   EnlossFlucCmd->AvailableForStates(Idle);
 
   SubSecCmd = new G4UIcmdWithABool("/process/eLoss/subsec",this);
-  SubSecCmd->SetGuidance("Switch on/off the subcutoff generation.");
+  SubSecCmd->SetGuidance("Switch true/false the subcutoff generation.");
   SubSecCmd->SetParameterName("choice",true);
   SubSecCmd->SetDefaultValue(true);
   SubSecCmd->AvailableForStates(Idle);
 
-  MinDeltaCutInRangeCmd = new G4UIcmdWithADoubleAndUnit("/process/eLoss/mindeltacutinrange",this);
-  MinDeltaCutInRangeCmd->SetGuidance("Set the min. cut for subcutoff delta in range .");
-  MinDeltaCutInRangeCmd->SetParameterName("rcmin",true);
-  MinDeltaCutInRangeCmd->AvailableForStates(Idle);
+  MinSubSecCmd = new G4UIcmdWithADoubleAndUnit("/process/eLoss/minsubsec",this);
+  MinSubSecCmd->SetGuidance("Set the min. cut for subcutoff delta in range.");
+  MinSubSecCmd->SetParameterName("rcmin",true);
+  MinSubSecCmd->AvailableForStates(Idle);
 
   StepFuncCmd = new G4UIcommand("/process/eLoss/StepFunction",this);
   StepFuncCmd->SetGuidance("Set the energy loss step limitation parameters.");
   StepFuncCmd->SetGuidance("  dRoverR   : max Range variation per step");
   StepFuncCmd->SetGuidance("  finalRange: range for final step");
+  
   G4UIparameter* dRoverRPrm = new G4UIparameter("dRoverR",'d',false);
   dRoverRPrm->SetGuidance("max Range variation per step (fractional number)");
   dRoverRPrm->SetParameterRange("dRoverR>0. && dRoverR<=1.");
   StepFuncCmd->SetParameter(dRoverRPrm);
+  
   G4UIparameter* finalRangePrm = new G4UIparameter("finalRange",'d',false);
   finalRangePrm->SetGuidance("range for final step");
   finalRangePrm->SetParameterRange("finalRange>0.");
   StepFuncCmd->SetParameter(finalRangePrm);
+  
   G4UIparameter* unitPrm = new G4UIparameter("unit",'s',true);
   unitPrm->SetGuidance("unit of finalRange");
   unitPrm->SetDefaultValue("mm");
   G4String unitCandidates = G4UIcommand::UnitsList(G4UIcommand::CategoryOf("mm"));
   unitPrm->SetParameterCandidates(unitCandidates);
+  
   StepFuncCmd->SetParameter(unitPrm);
   StepFuncCmd->AvailableForStates(Idle);
 }
@@ -79,8 +87,9 @@ G4EnergyLossMessenger::~G4EnergyLossMessenger()
   delete RndmStepCmd;
   delete EnlossFlucCmd;
   delete SubSecCmd;
-  delete MinDeltaCutInRangeCmd;
+  delete MinSubSecCmd;
   delete StepFuncCmd;
+  delete eLossDirectory;
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
@@ -99,8 +108,8 @@ void G4EnergyLossMessenger::SetNewValue(G4UIcommand* command,G4String newValue)
    { G4VEnergyLoss::SetSubSec(SubSecCmd->GetNewBoolValue(newValue));
    }
 
-  if (command == MinDeltaCutInRangeCmd)
-   { G4VEnergyLoss::SetMinDeltaCutInRange(MinDeltaCutInRangeCmd->GetNewDoubleValue(newValue));
+  if (command == MinSubSecCmd)
+   { G4VEnergyLoss::SetMinDeltaCutInRange(MinSubSecCmd->GetNewDoubleValue(newValue));
    }
 
   if (command == StepFuncCmd)
