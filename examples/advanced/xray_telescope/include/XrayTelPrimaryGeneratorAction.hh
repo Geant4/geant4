0fba7c8bbb98eacd72a36672b085c7f3f4e8b848
Ramon Nartallo <nartallo@cern.ch>
2000-11-16 09:29:44 +0000

    complete-analysis
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@5616 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/advanced/xray_telescope/include/XrayTelPrimaryGeneratorAction.hh
+++ examples/advanced/xray_telescope/include/XrayTelPrimaryGeneratorAction.hh
@@ -12,15 +12,20 @@
 // * MODULE:            XrayTelPrimaryGeneratorAction.hh                *
 // * -------                                                            *
 // *                                                                    *
-// * Version:           0.4                                             *
-// * Date:              06/11/00                                        *
-// * Author:            R Nartallo                                      *
-// * Organisation:      ESA/ESTEC, Noordwijk, THe Netherlands           *
+// * Version:           0.5                                             *
+// * Date:              15/11/00                                        *
+// * Author:            F.Lei                                           *
+// * Organisation:      DERA, UK                                        *
 // *                                                                    *
 // **********************************************************************
 // 
 // CHANGE HISTORY
 // --------------
+//
+// 15.11.2000 F.Lei
+// - New version of PrimaryGeneratorAction using the GPS insead of the
+//   standard particle gun.  
+// - The PrimaryGeneratorMessenger is no longer needed.
 //
 // 06.11.2000 R.Nartallo
 // - First implementation of X-ray Telescope advanced example.
@@ -33,49 +38,25 @@
 #define XrayTelPrimaryGeneratorAction_h 1
 
 #include "G4VUserPrimaryGeneratorAction.hh"
-#include "globals.hh"
-#include "Randomize.hh"
-#include "G4ThreeVector.hh"
-#include "CLHEP/Random/RandGeneral.h"
 
-class G4ParticleGun;
+class G4GeneralParticleSource;
 class G4Event;
-class XrayTelDetectorConstruction;
-class XrayTelPrimaryGeneratorMessenger;
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
 
 class XrayTelPrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction
 {
 public:
-  XrayTelPrimaryGeneratorAction( XrayTelDetectorConstruction* );    
+  XrayTelPrimaryGeneratorAction();
   ~XrayTelPrimaryGeneratorAction();
 
 public:
-  void GeneratePrimaries(G4Event*);
-  void SetRndmFlag(G4String val) { rndmFlag = val;}
-  void SetErndmFlag(G4String val) { erndmFlag = val;}
-  void SetRmin ( G4double r ) { Rmin = r; }
-  void SetRmax ( G4double r ) { Rmax = r; }
-  void SetTmax ( G4double t ) { Tmax = t; }
+  void GeneratePrimaries(G4Event* anEvent);
 
 private:
-  G4ParticleGun* particleGun;	          // pointer a to G4 service class   
-  XrayTelDetectorConstruction* XrayTelDetector;  //pointer to the geometry
-  XrayTelPrimaryGeneratorMessenger* gunMessenger; //messenger of this class
-  G4String rndmFlag;	              //flag for a random impact point    
-  G4String erndmFlag;	              //flag for a random energy selection    
-  G4double Rmin;
-  G4double Rmax;
-  G4double Tmax;
-
-  G4ThreeVector GetRandomRingPosition (G4double minRad, G4double maxRad);   
-  // returns a random position vector in a ring defined by minRad, maxRad
-  G4ThreeVector GetRandomDirection(G4double maxT);
-  // returns a random direction vector in the space
-  HepJamesRandom* engine;
+  G4GeneralParticleSource* particleGun;
+  
 };
 
 #endif
+
 
 
