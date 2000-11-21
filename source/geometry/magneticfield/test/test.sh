e4e9188caa84b33369566646a18431a79b1276df
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2000-11-21 13:00:06 +0000

    Fixes and new ref-ouputs and ref-errors
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@5710 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/geometry/magneticfield/test/test.sh
+++ source/geometry/magneticfield/test/test.sh
@@ -1,14 +1,50 @@
+#!/bin/sh
 #
+# A simple script to run all the tests in this directory and check
+# their results against the expected (previous) results
 #
-foreach target ( testPropagateMagField testPropagateSpin testProPerpSpin testProElectroMagField )
-   gmake G4TARGET=$target
-end
+# $Id: test.sh,v 1.2 2000-11-21 13:00:04 gcosmo Exp $
+# $Name: not supported by cvs2svn $
+#
+
+echo "Running on `hostname`, which is a `uname -a` machine" 
+host=`hostname`
+
+target=testPropagateMagField
+echo  "Compiling $target ... "
+gmake G4TARGET=$target
+echo  "Executing $target ..."
+for n in 1 2 3 4 5 6 7 8
+do
+  echo "Executing with stepper choice $n .. \c"
+  $G4WORKDIR/bin/$G4SYSTEM/$target $n > $target.newout$n \
+                                     2> $target.newerr$n
+  echo  ".. difference from expected output: "
+  diff -wb $target.out$n $target.newout$n
+  sleep 1;
+  echo  ".. difference from expected error: "
+  diff -wb $target.err$n $target.newerr$n
+  sleep 1;
+  echo  " "
+done
+
+#exit
+
+for i in *Spin.cc testProElectroMagField.cc
+do
+  target=`basename $i .cc`
+  echo  "Compiling $target ... "
+  gmake G4TARGET=$target
+  echo  "Executing $target ..."
+  $G4WORKDIR/bin/$G4SYSTEM/$target > $target.newout \
+                                  2> $target.newerr
+  echo  ".. difference from expected output: "
+  diff -wb $target.out $target.newout
+  sleep 1;
+  echo  ".. difference from expected error: "
+  diff -wb $target.err $target.newerr
+  sleep 1;
+  echo  " "
+done
 
 exit
-
-#
-gmake G4TARGET=testPropagateMagField
-   320  19:10   gmake G4TARGET=testPropagateSpin
-   321  19:11   gmake G4TARGET=testProPerpSpin
-   322  19:11   gmake G4TARGET=testProElectroMagField
-
