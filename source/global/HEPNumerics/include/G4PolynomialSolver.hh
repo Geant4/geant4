4e2eb11c97a4362428d0e8a6647f2df579ec7b10
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2001-01-29 09:49:55 +0000

    Cleaned up useless dependencies
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@6113 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/global/HEPNumerics/include/G4PolynomialSolver.hh
+++ source/global/HEPNumerics/include/G4PolynomialSolver.hh
@@ -5,39 +5,33 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-//
+// $Id: G4PolynomialSolver.hh,v 1.2 2001-01-29 09:49:54 gcosmo Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
 // 
 // class G4PolynomialSolver
 //
-// Implementation
-//
-// 19.12.00 E.Medernach, First implementation
-//
-
 // Class description:
 //
-//   This class allows the user to solve a polynomial equation
+//   G4PolynomialSolver allows the user to solve a polynomial equation
 //   with a great precision. This is used by Implicit Equation solver.
 //
-//   For the moment, we use a Bezier clipping method to solve the polynomial.
+//   The Bezier clipping method is used to solve the polynomial.
 //
-
 // How to use it:
-//
-//   You have to create a class that is the function to be solved.
-//   This class could have internal parameters to allow you to change
+//   Create a class that is the function to be solved.
+//   This class could have internal parameters to allow to change
 //   the equation to be solved without recreating a new one.
 //
-//   You define a Polynomial solver like this one:
+//   Define a Polynomial solver, example:
 //   G4PolynomialSolver<MyFunctionClass,G4double(MyFunctionClass::*)(G4double)>
 //     PolySolver (&MyFunction,
 //                 &MyFunctionClass::Function,
 //                 &MyFunctionClass::Derivative,
 //                 precision);
 //
-//   The precision is relative to the function you solve.
+//   The precision is relative to the function to solve.
 //
-//   In MyFunctionClass, you have to provide the function to solve and its derivative:
+//   In MyFunctionClass, provide the function to solve and its derivative:
 //   Example of function to provide :
 //
 //   x,y,z,dx,dy,dz,Rmin,Rmax are internal variables of MyFunctionClass
@@ -72,38 +66,44 @@
 //     return result;
 //   }
 //   
-//   Then to have a root inside an interval [IntervalMin,IntervalMax] you have to do the following:
+//   Then to have a root inside an interval [IntervalMin,IntervalMax] do the
+//   following:
+//
 //   MyRoot = PolySolver.solve(IntervalMin,IntervalMax);
 //
 
-#include  "globals.hh"
-     
+// History:
+//
+// - 19.12.00 E.Medernach, First implementation
+//
+
 #ifndef G4POL_SOLVER_HH
 #define G4POL_SOLVER_HH
 
-
+#include  "globals.hh"
 
 template <class T, class F>
 class G4PolynomialSolver 
 {
-public:
+public:  // with description
   
-  G4PolynomialSolver(T* typeF,F func,F deriv,G4double precision);  
+  G4PolynomialSolver(T* typeF, F func, F deriv, G4double precision);  
   ~G4PolynomialSolver();
   
 
-  G4double solve (G4double IntervalMin,G4double IntervalMax);
+  G4double solve (G4double IntervalMin, G4double IntervalMax);
   
 private:
-  //General Newton method with Bezier Clipping
-  G4double Newton (G4double IntervalMin,G4double IntervalMax);
-  
+
+  G4double Newton (G4double IntervalMin, G4double IntervalMax);
+    //General Newton method with Bezier Clipping
+
   // Works for polynomial of order less or equal than 4.
   // But could be changed to work for polynomial of any order providing
   // that we find the bezier control points.
 
-  //   This is just one iteration of Bezier Clipping
-  int BezierClipping(G4double *IntervalMin,G4double *IntervalMax);
+  G4int BezierClipping(G4double *IntervalMin, G4double *IntervalMax);
+    //   This is just one iteration of Bezier Clipping
 
 
   T* FunctionClass ;
--- source/global/HEPNumerics/test/testG4PolynomialSolver.cc
+++ source/global/HEPNumerics/test/testG4PolynomialSolver.cc
@@ -1,3 +1,13 @@
+// This code implementation is the intellectual property of
+// the GEANT4 collaboration.
+//
+// By copying, distributing or modifying the Program (or any work
+// based on the Program) you indicate your acceptance of this statement,
+// and all its terms.
+//
+// $Id: testG4PolynomialSolver.cc,v 1.2 2001-01-29 09:49:55 gcosmo Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
 #include "G4PolynomialSolver.hh"
 
 class MyFunctionClass 
@@ -40,7 +50,7 @@ private:
 
 
 
-int main (void)
+G4int main (void)
 {
   MyFunctionClass MyFunction(1.0);
 
@@ -66,6 +76,3 @@ int main (void)
   
   return 0;  
 }
-
-
-
