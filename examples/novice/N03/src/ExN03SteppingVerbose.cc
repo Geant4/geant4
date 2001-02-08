00e0e0545ea2ff3d1e4c7a8211fae8a27612467e
Takashi Sasaki <takashi.sasaki@kek.jp>
2001-02-08 08:33:31 +0000

    STL migration of tracking:entries->size
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@6245 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/novice/N03/src/ExN03SteppingVerbose.cc
+++ examples/novice/N03/src/ExN03SteppingVerbose.cc
@@ -5,7 +5,7 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: ExN03SteppingVerbose.cc,v 1.4 2001-01-22 17:04:47 maire Exp $
+// $Id: ExN03SteppingVerbose.cc,v 1.5 2001-02-08 08:33:01 tsasaki Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo.....
@@ -87,12 +87,12 @@ void ExN03SteppingVerbose::StepInfo()
 	       << ",Along=" << G4std::setw(2) << fN2ndariesAlongStepDoIt
 	       << ",Post="  << G4std::setw(2) << fN2ndariesPostStepDoIt
 	       << "), "
-	       << "#SpawnTotal=" << G4std::setw(3) << (*fSecondary).entries()
+	       << "#SpawnTotal=" << G4std::setw(3) << (*fSecondary).size()
 	       << " ---------------"
 	       << G4endl;
 
-	for(G4int lp1=(*fSecondary).entries()-tN2ndariesTot; 
-                        lp1<(*fSecondary).entries(); lp1++){
+	for(G4int lp1=(*fSecondary).size()-tN2ndariesTot; 
+                        lp1<(*fSecondary).size(); lp1++){
 	  G4cout << "    : "
 		 << G4std::setw(6)
 		 << G4BestUnit((*fSecondary)[lp1]->GetPosition().x(),"Length")
