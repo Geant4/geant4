6b95baf7a11a21824588265498ab8105a8e7b7ab
Johannes-Peter Wellisch <hpw@cern.ch>
2001-02-26 17:09:14 +0000

    suppressing non-elastic cross-section for ion under 6 MeV
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@6423 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/cross_sections/src/G4HadronCrossSections.cc
+++ source/processes/hadronic/cross_sections/src/G4HadronCrossSections.cc
@@ -5,7 +5,7 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: G4HadronCrossSections.cc,v 1.2 1999-12-15 14:52:06 gunter Exp $
+// $Id: G4HadronCrossSections.cc,v 1.3 2001-02-26 17:09:14 hpw Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 //
@@ -1235,6 +1235,7 @@ G4HadronCrossSections::CalcScatteringCrossSections(
       sigin = 49.*term*term;
    // Convert cross section from mb to default units
       siginelastic = sigin*millibarn;
+      if(aParticle->GetKineticEnergy() < 6*MeV) siginelastic = 0;
       sigelastic = 0.;
       return;
    }
