690a058eded01fef4b0ca7a88bca58be74f8e554
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2001-03-05 09:31:33 +0000

    Migration to STL vectors for G4PhysicsTable
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@6460 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/global/management/include/G4PhysicsTable.hh
+++ source/global/management/include/G4PhysicsTable.hh
@@ -5,68 +5,89 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: G4PhysicsTable.hh,v 1.6 2001-02-24 05:37:52 kurasige Exp $
+// $Id: G4PhysicsTable.hh,v 1.7 2001-03-05 09:31:32 gcosmo Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // 
 // ------------------------------------------------------------
 //      GEANT 4 class header file
 //
-//	History: first implementation, based on object model of
-//	2nd December 1995, G.Cosmo
+// Class description:
 //
-//      Modified 01 March 1996, K. Amako
+// G4PhysicsTable is an utility class for storage of pointers
+// to G4PhysicsVector containers. It derives all functionalities
+// of STL vector containers with the addition of few methods for
+// compatibility with previous implementation based on Rogue-Wave
+// pointer collections.
+// The constructor given the 'capacity' of the table, pre-allocates
+// memory for the specified value by invoking the STL's reserve()
+// function, in order to avoid reallocation during insertions.
+
 // ------------------------------------------------------------
-//      STL migration Feb 2001, H.Kurashige
+//
+// History:
+// -------
+// - First implementation, based on object model of
+//   2nd December 1995. G.Cosmo
+// - 1st March 1996, modified. K.Amako
+// - 24th February 2001, migration to STL vectors. H.Kurashige
+//
+// ------------------------------------------------------------
 
 #ifndef G4PhysicsTable_h
 #define G4PhysicsTable_h 1
 
 #include "g4std/vector"
 #include "globals.hh"
-class     G4PhysicsVector;
 
+class G4PhysicsVector;
 
-class G4PhysicsTable: public G4std::vector<G4PhysicsVector*> 
+class G4PhysicsTable : public G4std::vector<G4PhysicsVector*> 
 {
  public: // with description
 
   G4PhysicsTable();
-  // Deafult constructor.
+    // Deafult constructor.
 
   G4PhysicsTable(size_t capacity);
-  // Constructor with capacity 
-
+    // Constructor with capacity. Reserves memory for the
+    // specified capacity.
 
   virtual ~G4PhysicsTable();
-  // Destructor
+    // Destructor.
+    // Does not invoke deletion of contained pointed collections.
 
- private:
- // Private copy constructor and assignment operator.
-  G4PhysicsTable(const G4PhysicsTable&);
-  // Copy constructor  
-
-  G4PhysicsTable& operator=(const G4PhysicsTable&);
-  // assignment operator
-
- public:
   G4PhysicsVector*& operator()(size_t);
   G4PhysicsVector* const& operator()(size_t) const;
+    // Access operators.
 
   void clearAndDestroy();
-  // Remove all items and delete them at the same time
+    // Removes all items and deletes them at the same time.
 
   void   insert (G4PhysicsVector*);
+    // Pushes new element to collection.
+
   void   insertAt (size_t, G4PhysicsVector*); 
+    // Inserts element at the specified position in the collection.
 
   size_t entries() const;
   size_t length() const;
+    // Return collection's size.
+
   G4bool isEmpty() const;
- 
+    // Flags if collection is empty or not.
+
+ private:
+
+  G4PhysicsTable(const G4PhysicsTable&);
+  G4PhysicsTable& operator=(const G4PhysicsTable&);
+    // Private copy constructor and assignment operator.
+
 };
 
 typedef G4PhysicsTable::iterator G4PhysicsTableIterator;
 
 #include "G4PhysicsVector.hh"
 #include "G4PhysicsTable.icc"
+
 #endif
--- source/global/management/src/G4PhysicsTable.cc
+++ source/global/management/src/G4PhysicsTable.cc
@@ -1,3 +1,21 @@
+// This code implementation is the intellectual property of
+// the GEANT4 collaboration.
+//
+// By copying, distributing or modifying the Program (or any work
+// based on the Program) you indicate your acceptance of this statement,
+// and all its terms.
+//
+// $Id: G4PhysicsTable.cc,v 1.2 2001-03-05 09:31:33 gcosmo Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+// 
+// ------------------------------------------------------------
+//      GEANT 4 class implementation
+//
+//	G4PhysicsTable
+//
+// ------------------------------------------------------------
+
 #include "G4PhysicsVector.hh"
 #include "G4PhysicsTable.hh"
 
@@ -19,9 +37,11 @@ G4PhysicsTable::G4PhysicsTable(const G4PhysicsTable& right)
 
 G4PhysicsTable& G4PhysicsTable::operator=(const G4PhysicsTable& right)
 {
-  if (this != &right) {
+  if (this != &right)
+  {
     G4std::vector<G4PhysicsVector*>::const_iterator itr;
-    for (itr=right.begin(); itr!=right.end(); ++itr){
+    for (itr=right.begin(); itr!=right.end(); ++itr)
+    {
       G4std::vector<G4PhysicsVector*>::push_back(*itr);
     }
   }
