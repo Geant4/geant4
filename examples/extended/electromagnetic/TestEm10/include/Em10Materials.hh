b0d75f358e3bb6b146af8cbd4f334389d58d90a8
Vladimir Grichine <Vladimir.Grichine@cern.ch>
2001-03-16 15:20:38 +0000

    file consisting materials typical for XTR experiments
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@6572 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ examples/extended/electromagnetic/TestEm10/include/Em10Materials.hh
@@ -0,0 +1,385 @@
+// File for definition of materials used in X-ray TR experiments
+
+#ifndef Em10Materials_h
+#define Em10Materials_h 1
+
+
+
+#include "G4Material.hh"
+
+
+
+
+
+G4String name, symbol ;             //a=mass of a mole;
+G4double a, z, density ;            //z=mean number of protons;  
+G4int iz, n, nel ;                       //iz=number of protons  in an isotope; 
+                                   // n=number of nucleons in an isotope;
+
+G4int ncomponents, natoms;
+G4double abundance, fractionmass;
+G4double temperature, pressure;
+
+//
+// define Elements
+//
+
+  a = 1.01*g/mole;
+  G4Element* elH  = new G4Element(name="Hydrogen",symbol="H" , z= 1., a);
+
+  a = 6.94*g/mole;
+  G4Element* elLi  = new G4Element(name="Lithium",symbol="Li" , z= 3., a);
+
+  a = 9.01*g/mole;
+  G4Element* elBe  = new G4Element(name="Beryllium",symbol="Be" , z= 4., a);
+
+  a = 6.01*g/mole;
+  G4Element* elC = new G4Element(name="Carbon", symbol="C", z=6., a);
+
+  a = 14.01*g/mole;
+  G4Element* elN  = new G4Element(name="Nitrogen",symbol="N" , z= 7., a);
+
+  a = 16.00*g/mole;
+  G4Element* elO  = new G4Element(name="Oxygen"  ,symbol="O" , z= 8., a);
+
+  a = 39.948*g/mole;
+  G4Element* elAr = new G4Element(name="Argon", symbol="Ar", z=18., a);
+
+  a = 131.29*g/mole;
+  G4Element* elXe = new G4Element(name="Xenon", symbol="Xe", z=54., a);
+  
+  a = 19.00*g/mole;
+  G4Element* elF  = new G4Element(name="Fluorine", symbol="F", z=9., a);
+
+
+//
+// define simple materials
+//
+
+    
+
+  density = 1.848*g/cm3;
+  a = 9.01*g/mole;
+  G4Material* Be = new G4Material(name="Beryllium", z=4., a, density);
+
+
+  density = 1.390*g/cm3;
+  a = 39.95*g/mole;
+  G4Material* lAr = new G4Material(name="liquidArgon", z=18., a, density);
+
+  density = 7.870*g/cm3;
+  a = 55.85*g/mole;
+  G4Material* Fe = new G4Material(name="Iron"   , z=26., a, density);
+
+  density = 8.960*g/cm3;
+  a = 63.55*g/mole;
+  G4Material* Cu = new G4Material(name="Copper"   , z=29., a, density);
+
+  density = 19.32*g/cm3;
+  a =196.97*g/mole;
+  G4Material* Au = new G4Material(name="Gold"   , z=79., a, density);
+
+  density = 11.35*g/cm3;
+  a = 207.19*g/mole;
+  G4Material* Pb = new G4Material(name="Lead"     , z=82., a, density);
+
+//
+// define a material from elements.   case 1: chemical molecule
+//
+
+  density = 1.000*g/cm3;
+  G4Material* H2O = new G4Material(name="Water", density, ncomponents=2);
+  H2O->AddElement(elH, natoms=2);
+  H2O->AddElement(elO, natoms=1);
+
+
+  // Silicon as detector material
+
+  density = 2.330*g/cm3;
+  a = 28.09*g/mole;
+  G4Material* Si = new G4Material(name="Silicon", z=14., a, density);
+
+/////////////////////////////////////////////////////////////////
+//
+// Detector windows, electrodes 
+
+  // Al for electrodes
+
+  density = 2.700*g/cm3;
+  a = 26.98*g/mole;
+  G4Material* Al = new G4Material(name="Aluminium", z=13., a, density);
+
+
+
+////////////////////////////////////////////////////////////////////////////
+//
+// Materials for popular X-ray TR radiators
+//
+
+  // TRT_CH2
+      
+  density = 0.935*g/cm3;
+  G4Material* TRT_CH2 = new G4Material(name="TRT_CH2",density, nel=2);
+  TRT_CH2->AddElement(elC,1);
+  TRT_CH2->AddElement(elH,2);
+
+  // Radiator
+
+  density = 0.059*g/cm3;
+  G4Material* Radiator = new G4Material(name="Radiator",density, nel=2);
+  Radiator->AddElement(elC,1);
+  Radiator->AddElement(elH,2);
+
+  // Carbon Fiber
+
+  density = 0.145*g/cm3;
+  G4Material* CarbonFiber = new G4Material(name="CarbonFiber",density, nel=1);
+  CarbonFiber->AddElement(elC,1);
+
+  // Lithium
+
+  density = 0.534*g/cm3;
+  G4Material* Li = new G4Material(name="Li",density, nel=1);
+  Li->AddElement(elLi,1);
+
+  // Beryllium
+
+  density = 1.848*g/cm3;
+  G4Material* Be = new G4Material(name="Be",density, nel=1);
+  Be->AddElement(elBe,1);
+
+  // Mylar
+
+  density = 1.39*g/cm3;
+  G4Material* Mylar = new G4Material(name="Mylar", density, nel=3);
+  Mylar->AddElement(elO,2);
+  Mylar->AddElement(elC,5);
+  Mylar->AddElement(elH,4);
+
+  // Kapton (polyimide) ??? since = Mylar C5H4O2
+
+  density = 1.39*g/cm3;
+  G4Material* Kapton = new G4Material(name="Kapton", density, nel=3);
+  Kapton->AddElement(elO,2);
+  Kapton->AddElement(elC,5);
+  Kapton->AddElement(elH,4);
+
+  // Polypropelene
+
+  G4Material* CH2 = new G4Material ("Polypropelene" , 0.91*g/cm3, 2);
+  CH2->AddElement(elH,2);
+  CH2->AddElement(elC,1);
+
+
+  density = 1.290*mg/cm3;  // old air from elements
+  G4Material* air = new G4Material(name="air"  , density, ncomponents=2);
+  Air->AddElement(elN, fractionmass=0.7);
+  Air->AddElement(elO, fractionmass=0.3);
+
+
+  density = 1.25053*mg/cm3 ;       // STP
+  a = 14.01*g/mole ;       // get atomic weight !!!
+  //  a = 28.016*g/mole;
+  G4Material* N2  = new G4Material(name="Nitrogen", z= 7.,a,density) ;
+
+  density = 1.25053*mg/cm3 ;       // STP
+  G4Material* anotherN2 = new G4Material(name="anotherN2", density,ncomponents=2);
+  anotherN2->AddElement(elN, 1);
+  anotherN2->AddElement(elN, 1);
+
+
+
+//////////////////////////////////////////////////////////////////////////
+//
+// Noble gases , STP conditions
+
+  // Helium as detector gas, STP
+
+  density = 0.178*mg/cm3 ;
+  a = 4.0026*g/mole ;
+  G4Material* He  = new G4Material(name="He",z=2., a, density );
+
+  // Neon as detector gas, STP
+
+  density = 0.900*mg/cm3 ;
+  a = 20.179*g/mole ;
+  G4Material* Ne  = new G4Material(name="Ne",z=10., a, density );
+
+  // Argon as detector gas, STP
+
+  density = 1.7836*mg/cm3 ;       // STP
+  G4Material* Argon = new G4Material(name="Argon"  , density, ncomponents=1);
+  Argon->AddElement(elAr, 1);
+
+  // Krypton as detector gas, STP
+
+  density = 3.700*mg/cm3 ;
+  a = 83.80*g/mole ;
+  G4Material* Kr  = new G4Material(name="Kr",z=36., a, density );
+
+  // Xenon as detector gas, STP
+
+  density = 5.858*mg/cm3 ;
+  a = 131.29*g/mole ;
+  G4Material* Xe  = new G4Material(name="Xenon",z=54., a, density );
+
+/////////////////////////////////////////////////////////////////////////////
+//
+// Hydrocarbones, metane and others
+
+  // Metane, STP
+
+  density = 0.7174*mg/cm3 ;
+  G4Material* metane = new G4Material(name="CH4",density,nel=2) ;
+  metane->AddElement(elC,1) ;
+  metane->AddElement(elH,4) ;
+
+  // Propane, STP
+
+  density = 2.005*mg/cm3 ;
+  G4Material* propane = new G4Material(name="C3H8",density,nel=2) ;
+  propane->AddElement(elC,3) ;
+  propane->AddElement(elH,8) ;
+
+  // iso-Butane (methylpropane), STP
+
+  density = 2.67*mg/cm3 ;
+  G4Material* isobutane = new G4Material(name="isoC4H10",density,nel=2) ;
+  isobutane->AddElement(elC,4) ;
+  isobutane->AddElement(elH,10) ;
+
+///////////////////////////////////////////////////////////////////////////
+//
+// Molecular gases
+
+  // Carbon dioxide
+
+  density = 1.977*mg/cm3;
+  G4Material* CO2 = new G4Material(name="CO2", density, nel=2,
+				       kStateGas,273.15*kelvin,1.*atmosphere);
+  CO2->AddElement(elC,1);
+  CO2->AddElement(elO,2);
+
+  // Carbon dioxide, STP
+
+  density = 1.977*mg/cm3;
+  G4Material* CarbonDioxide = new G4Material(name="CO2", density, nel=2);
+  CarbonDioxide->AddElement(elC,1);
+  CarbonDioxide->AddElement(elO,2);
+
+
+  // Nitrogen, STP
+
+  density = 1.25053*mg/cm3 ;       // STP
+  G4Material* Nitrogen = new G4Material(name="N2"  , density, ncomponents=1);
+  Nitrogen->AddElement(elN, 2);
+
+ // Oxygen, STP
+
+  density = 1.4289*mg/cm3 ;       // STP
+  G4Material* Oxygen = new G4Material(name="O2"  , density, ncomponents=1);
+  Oxygen->AddElement(elO, 2);
+
+  // Dry air (average composition), STP
+
+  density = 1.2928*mg/cm3 ;       // STP
+  G4Material* Air = new G4Material(name="Air"  , density, ncomponents=3);
+  Air->AddMaterial( Nitrogen, fractionmass = 0.7557 ) ;
+  Air->AddMaterial( Oxygen,   fractionmass = 0.2315 ) ;
+  Air->AddMaterial( Argon,    fractionmass = 0.0128 ) ;
+
+////////////////////////////////////////////////////////////////////////////
+//
+// MWPC mixtures
+
+  // 93% Ar + 7% CH4, STP
+
+  density = 1.709*mg/cm3 ;      
+  G4Material* Ar7CH4 = new G4Material(name="Ar7CH4"  , density, 
+                                                          ncomponents=2);
+  Ar7CH4->AddMaterial( Argon,    fractionmass = 0.971 ) ;
+  Ar7CH4->AddMaterial( metane,   fractionmass = 0.029 ) ;
+
+  // 93% Kr + 7% CH4, STP
+
+  density = 3.491*mg/cm3 ;      
+  G4Material* Kr7CH4 = new G4Material(name="Kr7CH4"  , density, 
+                                                  ncomponents=2);
+  Kr7CH4->AddMaterial( Kr,       fractionmass = 0.986 ) ;
+  Kr7CH4->AddMaterial( metane,   fractionmass = 0.014 ) ;
+
+  // ATLAS TRT gas mixture
+
+  G4double TRT_Xe_density = 5.485*mg/cm3;
+  G4Material* TRT_Xe = new G4Material(name="TRT_Xe", TRT_Xe_density, nel=1,
+				      kStateGas,293.15*kelvin,1.*atmosphere);
+  TRT_Xe->AddElement(elXe,1);
+
+  G4double TRT_CO2_density = 1.842*mg/cm3;
+  G4Material* TRT_CO2 = new G4Material(name="TRT_CO2", TRT_CO2_density, nel=2,
+				       kStateGas,293.15*kelvin,1.*atmosphere);
+  TRT_CO2->AddElement(elC,1);
+  TRT_CO2->AddElement(elO,2);
+
+  G4double TRT_CF4_density = 3.9*mg/cm3;
+  G4Material* TRT_CF4 = new G4Material(name="TRT_CF4", TRT_CF4_density, nel=2,
+                                       kStateGas,293.15*kelvin,1.*atmosphere);
+  TRT_CF4->AddElement(elC,1);
+  TRT_CF4->AddElement(elF,4);
+
+  // ATLAS TRT straw tube gas mixture (20 C, 1 atm)
+
+  G4double XeCO2CF4_density = 4.76*mg/cm3;
+  G4Material* XeCO2CF4 = new G4Material(name="XeCO2CF4", XeCO2CF4_density,
+					ncomponents=3,
+					kStateGas,293.15*kelvin,1.*atmosphere);
+  XeCO2CF4->AddMaterial(TRT_Xe,0.807);
+  XeCO2CF4->AddMaterial(TRT_CO2,0.039);
+  XeCO2CF4->AddMaterial(TRT_CF4,0.154);
+
+  // 80% Ar + 20% CO2, STP
+
+  density = 1.8223*mg/cm3 ;      
+  G4Material* Ar_80CO2_20 = new G4Material(name="ArCO2"  , density, 
+                                   ncomponents=2);
+  Ar_80CO2_20->AddMaterial( Argon,           fractionmass = 0.783 ) ;
+  Ar_80CO2_20->AddMaterial( CarbonDioxide,   fractionmass = 0.217 ) ;
+
+  // 80% Xe + 20% CO2, STP
+
+  density = 5.0818*mg/cm3 ;      
+  G4Material* Xe20CO2 = new G4Material(name="Xe20CO2"  , density, ncomponents=2);
+  Xe20CO2->AddMaterial( Xe,              fractionmass = 0.922 ) ;
+  Xe20CO2->AddMaterial( CarbonDioxide,   fractionmass = 0.078 ) ;
+
+  // 80% Kr + 20% CO2, STP
+
+  density = 3.601*mg/cm3 ;      
+  G4Material* Kr20CO2 = new G4Material(name="Kr20CO2", density, 
+                                                       ncomponents=2);
+  Kr20CO2->AddMaterial( Kr,              fractionmass = 0.89 ) ;
+  Kr20CO2->AddMaterial( CarbonDioxide,   fractionmass = 0.11 ) ;
+
+  // NIM A294 (1990) 465-472; STP
+
+  G4double density = 1.963*mg/cm3;
+  G4Material* Xe55He15CH4 = new G4Material(name="Xe55He15CH4",density,
+					                      ncomponents=3);
+  Xe55He15CH4->AddMaterial(Xe, 0.895);
+  Xe55He15CH4->AddMaterial(He, 0.050);
+  Xe55He15CH4->AddMaterial(metane,0.055);
+
+  // 90% Xe + 10% CH4, STP ; NIM A248 (1986) 379-388
+
+  density = 5.344*mg/cm3 ;      
+  G4Material* Xe10CH4 = new G4Material(name="Xe10CH4"  , density, 
+                                                  ncomponents=2);
+  Xe10CH4->AddMaterial( Xe,       fractionmass = 0.987 ) ;
+  Xe10CH4->AddMaterial( metane,   fractionmass = 0.013 ) ;
+
+
+#endif
+
+//
+//
+//////////////////////////////////////////////////////////////////////////
