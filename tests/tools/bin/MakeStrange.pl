364983fa297b5158938af9ee9136d154406ad209
Geant4 Testing <geant4.testing@cern.ch>
2001-05-03 21:54:34 +0000

    Corrected dubbling of strings :(
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@6880 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- tests/tools/bin/MakeStrange.pl
+++ tests/tools/bin/MakeStrange.pl
@@ -20,6 +20,7 @@ while ($line=<GMAKE>) {
     if ( $line =~ m#Compiling (\w+)\.# && $flag_comp eq 0) {
 	 print "$line\n";
 	 $flag_comp = 1;
+	 $flag_warn = 0;
 	 open(TMP,">>./buffer1.txt");
 	 print TMP "$line\n";
 	 close TMP;
@@ -30,28 +31,29 @@ while ($line=<GMAKE>) {
 	 open(TMP,">>./buffer2.txt");
 	 close TMP;
 	 system("cat ./buffer1.txt >> ./buffer2.txt");
-	 open(TMP,">>./buffer1.txt");
+	 open(TMP,">./buffer1.txt");
 	 print TMP "$line\n";
 	 close TMP;
 	 next;
      }
     if ( $line !~ m#Compiling (\w+)\.# && $line !~ m#Creating/replacing object files in .*\/(\w+\.a)\s*# && $flag_comp eq 1) {
 	 $flag_warn = 1;
+	 $flag_lib  = 1;
 	 open(TMP,">>./buffer1.txt");
 	 print TMP "$line\n";
 	 close TMP;
 	 next;
      }
     if ( $line =~ m#Compiling (\w+)\.# && $flag_comp eq 1 && $flag_warn eq 0) {
-	 system("rm ./buffer1.txt");
-	 open(TMP,">>./buffer1.txt");
+	 open(TMP,">./buffer1.txt");
 	 print TMP "$line\n";
 	 close TMP;
 	 next;
      }
-    if ( $line =~ m#Creating/replacing object files in .*\/(\w+\.a)\s*# && $flag_comp eq 1 && $flag_warn eq 1) {
+    if ( $line =~ m#Creating/replacing object files in .*\/(\w+\.a)\s*# && $flag_lib eq 1) {
 	 $flag_comp = 0;
 	 $flag_warn = 0;
+	 $flag_lib  = 0;
 	 open(TMP,">>./buffer2.txt");
 	 close TMP;
 	 system("cat ./buffer1.txt >> ./buffer2.txt");
@@ -59,8 +61,10 @@ while ($line=<GMAKE>) {
 	 system("mv ./buffer2.txt ./$1.txt");
 	 next;
      }
-    if ( $line =~ m#Creating/replacing object files in .*\/(\w+\.a)\s*# && $flag_comp eq 1 && $flag_warn eq 0) {
+    if ( $line =~ m#Creating/replacing object files in .*\/(\w+\.a)\s*# && $flag_lib eq 0) {
 	 $flag_comp = 0;
+	 $flag_warn = 0;
+	 $flag_lib  = 0;
 	 system("rm ./buffer1.txt");
 	 next;
      }
