098f325f5ba2d3f73aadf711158a00e844f9b540
Johannes-Peter Wellisch <hpw@cern.ch>
2001-05-18 12:42:47 +0000

    now we are done, and ok
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@6960 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/models/neutron_hp/test/NeutronDataTest.cc
+++ source/processes/hadronic/models/neutron_hp/test/NeutronDataTest.cc
@@ -5,6 +5,10 @@
 main()
 {
    G4Element* theElement = new G4Element("carbon", "C", 6, 12.0*g/mole);
+//   G4Element* theElement = new G4Element("Plutonium", "Pu", 1);
+//   G4Isotope * theIso = new G4Isotope("Pu140", 94., 240, 240.0*g/mole);
+//   theElement->AddIsotope(theIso, 100.*perCent);
+
    G4NeutronHPElasticData anElasticDataSet; 
    G4ParticleDefinition* theParticleDefinition = G4Neutron::NeutronDefinition();
 
@@ -13,17 +17,26 @@ main()
    temp *= kelvin;
 //   G4double ekin = 0.5E+04*eV;
    G4double ekin = 1.E-5*eV;
+//   G4double ekin = 1.E-3*eV;
    G4DynamicParticle* theDynamicParticle;
    G4int counter = -1;
    G4int points = -1; 
-   while (++points<30)
+   G4int hpw=0;
+   while (++points<20)
+//   while (++points<500)
    {
 //     ekin +=0.002E+04*eV;
-     ekin *=3.333333333;
+     ekin *=3.333333333; 
+//     if(ekin<0.8*eV) ekin *=1.2;
+//     else if(ekin<12*eV) ekin*=1.05;
+//     else ekin*=1.005;
      theDynamicParticle = new G4DynamicParticle(theParticleDefinition,
                                               G4ParticleMomentum(1.,0.,0.), ekin);
-     while(++counter<100)
+     while(++counter<50)
+//     while(++counter<1)
      {
+       hpw++;
+       if(hpw == 1*(hpw/1)) G4cerr << "point number "<<hpw<<" being processed"<<endl;
        cout << ekin/MeV 
             << " " 
             << anElasticDataSet.GetCrossSection(theDynamicParticle, theElement, temp)/millibarn
