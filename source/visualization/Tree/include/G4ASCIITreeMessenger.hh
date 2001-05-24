2c92f2aa548ca1104252bd97687bde5d7eea3c06
johna <johna@cern.ch>
2001-05-24 13:21:15 +0000

    Ooops! Forgot to add!
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@7055 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/visualization/Tree/include/G4ASCIITreeMessenger.hh
@@ -0,0 +1,38 @@
+// This code implementation is the intellectual property of
+// the GEANT4 collaboration.
+//
+// By copying, distributing or modifying the Program (or any work
+// based on the Program) you indicate your acceptance of this statement,
+// and all its terms.
+//
+// $Id: G4ASCIITreeMessenger.hh,v 1.1 2001-05-24 13:21:13 johna Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+// 
+// John Allison  5th April 2001
+// A scene handler to dump geometry hierarchy to standard output as
+//   ASCII stream.
+// Based on a provisional G4ASCIITreeGraphicsScene (was in modeling).
+
+#ifndef G4ASCIITREEMESSENGER_HH
+#define G4ASCIITREEMESSENGER_HH
+
+#include "G4UImessenger.hh"
+
+class G4UIcommand;
+class G4UIcmdWithAnInteger;
+class G4ASCIITree;
+
+class G4ASCIITreeMessenger: public G4UImessenger {
+public:
+  G4ASCIITreeMessenger(G4ASCIITree*);
+  virtual ~G4ASCIITreeMessenger();
+  G4String GetCurrentValue (G4UIcommand* command);
+  void SetNewValue (G4UIcommand* command, G4String newValue);
+private:
+  G4ASCIITree* fpASCIITree;
+  G4UIcommand* fpDirectory;
+  G4UIcmdWithAnInteger* fpCommandVerbose;
+};
+
+#endif
---
+++ source/visualization/Tree/src/G4ASCIITreeMessenger.cc
@@ -0,0 +1,53 @@
+// This code implementation is the intellectual property of
+// the GEANT4 collaboration.
+//
+// By copying, distributing or modifying the Program (or any work
+// based on the Program) you indicate your acceptance of this statement,
+// and all its terms.
+//
+// $Id: G4ASCIITreeMessenger.cc,v 1.1 2001-05-24 13:21:15 johna Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+// 
+// John Allison  5th April 2001
+// A scene handler to dump geometry hierarchy.
+// Based on a provisional G4ASCIITreeGraphicsScene (was in modeling).
+
+#include "G4ASCIITreeMessenger.hh"
+
+#include "G4ASCIITree.hh"
+#include "G4UIcommand.hh"
+#include "G4UIdirectory.hh"
+#include "G4UIcmdWithAnInteger.hh"
+
+G4ASCIITreeMessenger::G4ASCIITreeMessenger
+(G4ASCIITree* ASCIITree):
+  fpASCIITree(ASCIITree) {
+  G4bool omitable;
+  fpDirectory = new G4UIdirectory ("/vis/ASCIITree/");
+  fpDirectory -> SetGuidance ("Commands for ASCIITree control.");
+  fpCommandVerbose = new G4UIcmdWithAnInteger ("/vis/ASCIITree/verbose", this);
+  fpCommandVerbose -> SetGuidance ("/vis/ASCIITree/verbose [<verbosity>]");
+  fpCommandVerbose -> SetGuidance
+    ("0 (default) mimimum - 10 maximum printing.");
+  fpCommandVerbose -> SetParameterName ("verbosity", omitable = true);
+}
+
+G4ASCIITreeMessenger::~G4ASCIITreeMessenger() {
+  delete fpCommandVerbose;
+  delete fpDirectory;
+}
+
+G4String G4ASCIITreeMessenger::GetCurrentValue
+(G4UIcommand* command) {
+  return "0";
+}
+
+void G4ASCIITreeMessenger::SetNewValue
+(G4UIcommand* command, G4String newValue) {
+  fpASCIITree->SetVerbosity
+    (fpCommandVerbose->GetNewIntValue(newValue));
+  G4cout << "G4ASCIITree verbosity now "
+	 << fpASCIITree->GetVerbosity()
+	 << G4endl;  
+}
