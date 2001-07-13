229f03c2b6e227ed649ddafaa0b17e584a4647a1
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2001-07-13 11:50:57 +0000

    Removed double-inserted disclaimer
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@7430 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/event/src/G4GeneralParticleSource.cc
+++ source/event/src/G4GeneralParticleSource.cc
@@ -20,28 +20,6 @@
 // * statement, and all its terms.                                    *
 // ********************************************************************
 //
-//
-// ********************************************************************
-// * DISCLAIMER                                                       *
-// *                                                                  *
-// * The following disclaimer summarizes all the specific disclaimers *
-// * of contributors to this software. The specific disclaimers,which *
-// * govern, are listed with their locations in:                      *
-// *   http://cern.ch/geant4/license                                  *
-// *                                                                  *
-// * Neither the authors of this software system, nor their employing *
-// * institutes,nor the agencies providing financial support for this *
-// * work  make  any representation or  warranty, express or implied, *
-// * regarding  this  software system or assume any liability for its *
-// * use.                                                             *
-// *                                                                  *
-// * This  code  implementation is the  intellectual property  of the *
-// * GEANT4 collaboration.                                            *
-// * By copying,  distributing  or modifying the Program (or any work *
-// * based  on  the Program)  you indicate  your  acceptance of  this *
-// * statement, and all its terms.                                    *
-// ********************************************************************
-//
 ///////////////////////////////////////////////////////////////////////////////
 //
 // MODULE:       G4GeneralParticleSource.cc
@@ -60,7 +38,7 @@
 //       Technical Note (TN) on the physics and algorithms
 //
 ///////////////////////////////////////////////////////////////////////////////
-// $Id: G4GeneralParticleSource.cc,v 1.10 2001-07-11 09:58:52 gunter Exp $
+// $Id: G4GeneralParticleSource.cc,v 1.11 2001-07-13 11:50:57 gcosmo Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 ///////////////////////////////////////////////////////////////////////////////
 //
