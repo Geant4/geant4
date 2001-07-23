14dc6ff29240b35edbcf5c5c6f5f0a1c401e6b82
Vladimir Grichine <Vladimir.Grichine@cern.ch>
2001-07-23 16:24:28 +0000

    new class for description of quasi reflections in terms of G3
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@7527 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/geometry/solids/Boolean/include/G4ReflectedSolid.hh
@@ -0,0 +1,150 @@
+// This code implementation is the intellectual property of
+// the GEANT4 collaboration.
+//
+// By copying, distributing or modifying the Program (or any work
+// based on the Program) you indicate your acceptance of this statement,
+// and all its terms.
+//
+// $Id: G4ReflectedSolid.hh,v 1.1 2001-07-23 16:24:13 grichine Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+//
+// class G4ReflectedSolid
+//
+// Class description:
+//
+// A Reflected solid is a solid that has been shifted from its original
+// frame of reference to a new one. It is meant to be used only for
+// simplifying the implementation of "Boolean solids". 
+
+// History:
+//
+// 23.07.01 V.Grichine, creation according John/Gabriele recommendations
+
+
+#ifndef G4ReflectedSolid_HH
+#define G4ReflectedSolid_HH
+
+#include "G4VSolid.hh"
+#include "G4RotationMatrix.hh"
+#include "G4ThreeVector.hh"
+#include "G4Transform3D.hh"
+#include "G4AffineTransform.hh"
+
+class G4ReflectedSolid : public G4VSolid
+{
+  public:  // with description
+
+    G4ReflectedSolid( const G4String& pName,
+                            G4VSolid* pSolid ,
+                      const G4Transform3D& transform  ) ;
+
+  //  G4ReflectedSolid( const G4String& pName,
+  //                          G4VSolid* pSolid ,
+  //                          G4RotationMatrix* rotMatrix,
+  //                    const G4ThreeVector& transVector  ) ;
+
+  //   G4ReflectedSolid( const G4String& pName,
+  //                           G4VSolid* pSolid ,
+  //	      const G4AffineTransform directTransform );
+      // For use in instantiating a transient instance from a persistent one.
+
+    virtual ~G4ReflectedSolid() ;
+
+  public:  // without description 
+
+    // It also has all the methods that a solid requires, eg.
+
+    EInside Inside( const G4ThreeVector& p ) const ; 
+
+    G4bool CalculateExtent(const EAxis pAxis,
+			   const G4VoxelLimits& pVoxelLimit,
+			   const G4AffineTransform& pTransform,
+				 G4double& pMin, G4double& pMax) const ;
+
+    G4ThreeVector SurfaceNormal( const G4ThreeVector& p ) const ;
+
+    G4double DistanceToIn( const G4ThreeVector& p,
+                           const G4ThreeVector& v  ) const ;
+
+    G4double DistanceToIn( const G4ThreeVector& p) const ;
+
+    G4double DistanceToOut( const G4ThreeVector& p,
+			    const G4ThreeVector& v,
+			    const G4bool calcNorm=false,
+			    G4bool *validNorm=0,
+			    G4ThreeVector *n=0      ) const ;
+
+    G4double DistanceToOut( const G4ThreeVector& p ) const ;
+
+
+    void ComputeDimensions( G4VPVParameterisation* p,
+	                    const G4int n,
+                            const G4VPhysicalVolume* pRep ) ;
+                                   
+  public:  // with description 
+
+    virtual G4GeometryType  GetEntityType() const;
+
+    virtual const G4ReflectedSolid* GetReflectedSolidPtr() const   ;
+    virtual       G4ReflectedSolid* GetReflectedSolidPtr();
+
+      // If the Solid is a "G4ReflectedSolid",
+      // return a self pointer else return 0.
+
+    G4VSolid*                GetConstituentMovedSolid() const;
+
+    G4AffineTransform        GetTransform() const; 
+    void       SetTransform(G4AffineTransform& ); 
+
+    G4AffineTransform        GetDirectTransform() const; 
+    void       SetDirectTransform(G4AffineTransform&);
+ 
+    // Access/Set methods.
+
+    G4RotationMatrix         GetFrameRotation() const;
+    void  SetFrameRotation(const G4RotationMatrix&);
+
+    G4ThreeVector            GetFrameTranslation() const; 
+    void  SetFrameTranslation(const G4ThreeVector&); 
+
+      // Get/Set the rotation/translation, as applied to the frame of reference.
+
+    G4RotationMatrix         GetObjectRotation() const;
+    void  SetObjectRotation(const G4RotationMatrix&);
+
+    G4ThreeVector            GetObjectTranslation() const; 
+    void  SetObjectTranslation(const G4ThreeVector&); 
+
+    // Get/Set the rotation/translation, as applied to the object.
+
+  public:  // without description
+
+    void DescribeYourselfTo ( G4VGraphicsScene& scene ) const ;
+    G4Polyhedron* CreatePolyhedron () const ;
+    G4NURBS*      CreateNURBS      () const ;
+
+    // For creating graphical representations (ie for visualisation).
+
+  protected:
+
+    G4VSolid*          fPtrSolid        ;
+    G4AffineTransform* fPtrTransform    ;
+    G4AffineTransform* fDirectTransform ;
+
+    G4Transform3D*     fPtrTransform3D    ;
+    G4Transform3D*     fDirectTransform3D ;
+
+  private:
+
+    G4ReflectedSolid(const G4ReflectedSolid&);
+    G4ReflectedSolid& operator=(const G4ReflectedSolid&);
+
+      // Private copy constructor and assignment operator.
+} ;
+
+#endif
+
+
+
+
---
+++ source/geometry/solids/Boolean/src/G4ReflectedSolid.cc
@@ -0,0 +1,363 @@
+// This code implementation is the intellectual property of
+// the GEANT4 collaboration.
+//
+// By copying, distributing or modifying the Program (or any work
+// based on the Program) you indicate your acceptance of this statement,
+// and all its terms.
+//
+// $Id: G4ReflectedSolid.cc,v 1.1 2001-07-23 16:24:28 grichine Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+// Implementation for G4ReflectedSolid class for boolean 
+// operations between other solids
+//
+// History:
+//
+// 23.07.01 V.Grichine, creation according John/Gabriele recommendations
+
+#include "G4ReflectedSolid.hh"
+#include "G4Point3D.hh"
+#include "G4Normal3D.hh"
+
+
+#include "G4VoxelLimits.hh"
+#include "G4AffineTransform.hh"
+
+#include "G4VPVParameterisation.hh"
+
+#include "G4VGraphicsScene.hh"
+#include "G4Polyhedron.hh"
+#include "G4NURBS.hh"
+#include "G4NURBSbox.hh"
+
+
+/////////////////////////////////////////////////////////////////////////////////
+//
+// Constructor using HepTransform3D, in fact HepReflect3D
+
+G4ReflectedSolid::G4ReflectedSolid( const G4String& pName,
+                                          G4VSolid* pSolid ,
+			            const G4Transform3D& transform  ) :
+  G4VSolid(pName)
+{
+  fPtrSolid = pSolid ;
+  fDirectTransform = new G4AffineTransform(transform.getRotation().inverse(),
+                                           transform.getTranslation()) ;
+
+  fPtrTransform    = new G4AffineTransform(transform.getRotation().inverse(),
+                                           transform.getTranslation()) ;
+  fPtrTransform->Invert() ;
+
+  fDirectTransform3D = new G4Transform3D(transform) ;
+
+  fPtrTransform3D    = new G4Transform3D(transform.inverse()) ;
+ 
+}
+
+/* **************************************************************
+
+////////////////////////////////////////////////////////////////
+//
+// Constractor for transformation like rotation of frame then translation 
+// in new frame. It is similar to 1st constractor in G4PVPlacement
+
+G4ReflectedSolid::
+G4ReflectedSolid( const G4String& pName,
+                     G4VSolid* pSolid ,
+                     G4RotationMatrix* rotMatrix,
+               const G4ThreeVector& transVector    )
+   : G4VSolid(pName)
+{
+  fPtrSolid = pSolid ;
+  fPtrTransform = new G4AffineTransform(rotMatrix,transVector) ;
+  fPtrTransform->Invert() ;
+  fDirectTransform = new G4AffineTransform(rotMatrix,transVector) ;
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+//  Constructor for use with creation of Transient object from Persistent object
+//
+
+G4ReflectedSolid::G4ReflectedSolid( const G4String& pName,
+                                    G4VSolid* pSolid ,
+			      const G4AffineTransform directTransform ) :
+  G4VSolid(pName)
+{
+  fPtrSolid = pSolid ;
+  fDirectTransform = new G4AffineTransform( directTransform );
+  fPtrTransform    = new G4AffineTransform( directTransform.Inverse() ) ; 
+}
+
+********************************************************** */
+
+
+///////////////////////////////////////////////////////////////////
+//
+
+G4ReflectedSolid::~G4ReflectedSolid() 
+{
+  if(fPtrTransform)
+  {
+   delete fPtrTransform ;
+   delete fDirectTransform;
+  }
+}
+
+G4GeometryType G4ReflectedSolid::GetEntityType() const 
+{
+   return G4String("G4ReflectedSolid");
+}
+
+const G4ReflectedSolid* G4ReflectedSolid::GetReflectedSolidPtr() const   
+{ return this; }
+
+      G4ReflectedSolid* G4ReflectedSolid::GetReflectedSolidPtr() 
+{ return this; }
+
+G4VSolid* G4ReflectedSolid::GetConstituentMovedSolid() const
+{ 
+  return fPtrSolid; 
+} 
+
+/////////////////////////////////////////////////////////////////////////////
+
+G4AffineTransform  G4ReflectedSolid::GetTransform() const
+{
+   G4AffineTransform aTransform = *fPtrTransform;
+   return aTransform;
+}
+
+void G4ReflectedSolid::SetTransform(G4AffineTransform& transform) 
+{
+   fPtrTransform = &transform ;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+G4AffineTransform  G4ReflectedSolid::GetDirectTransform() const
+{
+   G4AffineTransform aTransform= *fDirectTransform;
+   return aTransform;
+}
+
+void G4ReflectedSolid::SetDirectTransform(G4AffineTransform& transform) 
+{
+   fDirectTransform = &transform ;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+G4RotationMatrix G4ReflectedSolid::GetFrameRotation() const
+{
+   G4RotationMatrix InvRotation= fDirectTransform->NetRotation();
+   return InvRotation;
+}
+
+void G4ReflectedSolid::SetFrameRotation(const G4RotationMatrix& matrix)
+{
+   fDirectTransform->SetNetRotation(matrix);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+G4ThreeVector  G4ReflectedSolid::GetFrameTranslation() const
+{
+   return fPtrTransform->NetTranslation();
+}
+
+void G4ReflectedSolid::SetFrameTranslation(const G4ThreeVector& vector)
+{
+  fPtrTransform->SetNetTranslation(vector);
+}
+
+///////////////////////////////////////////////////////////////
+
+G4RotationMatrix G4ReflectedSolid::GetObjectRotation() const
+{
+   G4RotationMatrix Rotation= fPtrTransform->NetRotation();
+   return Rotation;
+}
+
+void G4ReflectedSolid::SetObjectRotation(const G4RotationMatrix& matrix)
+{
+   fPtrTransform->SetNetRotation(matrix);
+}
+
+///////////////////////////////////////////////////////////////////////
+
+G4ThreeVector  G4ReflectedSolid::GetObjectTranslation() const
+{
+   return fDirectTransform->NetTranslation();
+}
+
+void G4ReflectedSolid::SetObjectTranslation(const G4ThreeVector& vector)
+{
+  fDirectTransform->SetNetTranslation(vector);
+}
+
+///////////////////////////////////////////////////////////////
+//
+//
+     
+G4bool 
+G4ReflectedSolid::CalculateExtent( const EAxis pAxis,
+			           const G4VoxelLimits& pVoxelLimit,
+			           const G4AffineTransform& pTransform,
+				         G4double& pMin, 
+                                         G4double& pMax           ) const 
+{
+  G4AffineTransform sumTransform ;
+  sumTransform.Product(*fDirectTransform,pTransform) ;
+  return fPtrSolid->CalculateExtent(pAxis,pVoxelLimit,sumTransform,
+                                    pMin,pMax) ;
+}
+ 
+/////////////////////////////////////////////////////
+//
+// 
+
+EInside G4ReflectedSolid::Inside(const G4ThreeVector& p) const
+{
+  // G4ThreeVector newPoint = fPtrTransform->TransformPoint(p) ;
+
+  G4Point3D newPoint = (*fDirectTransform3D)*G4Point3D(p) ;
+  return fPtrSolid->Inside(G4ThreeVector(newPoint.x(),newPoint.y(),newPoint.z())) ; 
+}
+
+//////////////////////////////////////////////////////////////
+//
+//
+
+G4ThreeVector 
+G4ReflectedSolid::SurfaceNormal( const G4ThreeVector& p ) const 
+{
+  G4Point3D newPoint = (*fDirectTransform3D)*G4Point3D(p) ;
+  G4ThreeVector normal = fPtrSolid->SurfaceNormal(G4ThreeVector(newPoint.x(),
+                                                                newPoint.y(),
+                                                                newPoint.z() ) ) ;
+  G4Point3D newN = (*fDirectTransform3D)*G4Point3D(normal) ;
+  newN.unit() ;
+
+  return G4ThreeVector(newN.x(),newN.y(),newN.z()) ;
+    
+}
+
+/////////////////////////////////////////////////////////////
+//
+// The same algorithm as in DistanceToIn(p)
+
+G4double 
+G4ReflectedSolid::DistanceToIn( const G4ThreeVector& p,
+                                   const G4ThreeVector& v  ) const 
+{    
+  G4Point3D newPoint     = (*fDirectTransform3D)*G4Point3D(p) ;
+  G4Point3D newDirection = (*fDirectTransform3D)*G4Point3D(v) ;
+  newDirection.unit() ;
+  return fPtrSolid->DistanceToIn(
+       G4ThreeVector(newPoint.x(),newPoint.y(),newPoint.z()),
+       G4ThreeVector(newDirection.x(),newDirection.y(),newDirection.z())) ;   
+}
+
+////////////////////////////////////////////////////////
+//
+// Approximate nearest distance from the point p to the intersection of
+// two solids
+
+G4double 
+G4ReflectedSolid::DistanceToIn( const G4ThreeVector& p) const 
+{
+  G4Point3D newPoint = (*fDirectTransform3D)*G4Point3D(p) ;
+  return fPtrSolid->DistanceToIn(
+                    G4ThreeVector(newPoint.x(),newPoint.y(),newPoint.z())) ;   
+}
+
+//////////////////////////////////////////////////////////
+//
+// The same algorithm as DistanceToOut(p)
+
+G4double 
+G4ReflectedSolid::DistanceToOut( const G4ThreeVector& p,
+			            const G4ThreeVector& v,
+			            const G4bool calcNorm,
+			            G4bool *validNorm,
+			            G4ThreeVector *n      ) const 
+{
+  G4ThreeVector solNorm ; 
+
+  G4Point3D newPoint     = (*fDirectTransform3D)*G4Point3D(p) ;
+  G4Point3D newDirection = (*fDirectTransform3D)*G4Point3D(v) ;
+  newDirection.unit() ;
+
+  G4double dist = fPtrSolid->DistanceToOut(
+                  G4ThreeVector(newPoint.x(),newPoint.y(),newPoint.z()),
+                  G4ThreeVector(newDirection.x(),newDirection.y(),newDirection.z()),
+                                           calcNorm,validNorm,&solNorm) ;
+  if(calcNorm)
+  { 
+    G4Point3D newN = (*fDirectTransform3D)*G4Point3D(solNorm) ;
+    newN.unit() ;
+    *n = G4ThreeVector(newN.x(),newN.y(),newN.z()) ;
+  }
+  return dist ;  
+}
+
+//////////////////////////////////////////////////////////////
+//
+// Inverted algorithm of DistanceToIn(p)
+
+G4double 
+G4ReflectedSolid::DistanceToOut( const G4ThreeVector& p ) const 
+{
+  G4Point3D newPoint = (*fDirectTransform3D)*G4Point3D(p) ;
+  return fPtrSolid->DistanceToOut(
+                    G4ThreeVector(newPoint.x(),newPoint.y(),newPoint.z())) ;   
+}
+
+//////////////////////////////////////////////////////////////
+//
+//
+
+void 
+G4ReflectedSolid::ComputeDimensions( G4VPVParameterisation* p,
+	                                const G4int n,
+                                        const G4VPhysicalVolume* pRep ) 
+{
+  // fPtrSolid->ComputeDimensions(p,n,pRep);
+
+  G4Exception("ERROR: ComputeDimensions has no meaning for a G4ReflectedSolid. It cannot be called.");
+}
+
+/////////////////////////////////////////////////
+//
+//                    
+
+void 
+G4ReflectedSolid::DescribeYourselfTo ( G4VGraphicsScene& scene ) const 
+{
+  scene.AddThis (*this);
+}
+
+////////////////////////////////////////////////////
+//
+//
+
+G4Polyhedron* 
+G4ReflectedSolid::CreatePolyhedron () const 
+{
+  G4Polyhedron* polyhedron = fPtrSolid->CreatePolyhedron();
+  polyhedron->Transform
+    (G4Transform3D(GetObjectRotation(),GetObjectTranslation()));
+  return polyhedron;
+}
+
+/////////////////////////////////////////////////////////
+//
+//
+
+G4NURBS*      
+G4ReflectedSolid::CreateNURBS      () const 
+{
+  // Take into account local transformation - see CreatePolyhedron.
+  // return fPtrSolid->CreateNURBS() ;
+  return 0;
+}
