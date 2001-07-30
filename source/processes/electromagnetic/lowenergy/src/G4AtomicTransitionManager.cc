0895dac919bb37a69f40133ba966f0d03d17d925
Elena Gianolio <elena@cern.ch>
2001-07-30 13:30:36 +0000

    the source file for the class which manager the shells
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@7567 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/electromagnetic/lowenergy/src/G4AtomicTransitionManager.cc
+++ source/processes/electromagnetic/lowenergy/src/G4AtomicTransitionManager.cc
@@ -35,215 +35,391 @@
 
 #include "G4AtomicTransitionManager.hh"
 #include "G4LowEnergyUtilities.hh"
+#include "G4EnergyLossTables.hh"
+#include "G4Element.hh"
+#include "G4ios.hh"
+#include "g4std/fstream"
 
 //----------..................-------------------....................
 
 // constructor
 
-G4AtomicTransitionManager::G4AtomicTransitionManager() {
+G4AtomicTransitionManager::G4AtomicTransitionManager( )
+  :ZVector(0),
+   tableLimit(0)
+{
+  G4int tableLimit = 5;
 
-  BuildTable();
+  G4SecondLevel* theBindingEnergyTable=BuildBindingEnergiesTable();
+  
+  G4ThirdLevel* theTransitionTable= BuildTransitionTable();
+  
+  G4int ShellIndex=0;
+  
+  
+  for (G4int index=0; index <(ZVector.size()); index++){  
 
-}
-
-//----------..................-------------------....................
-// this is destructor, private, because from outside i can only see instance()
-
-G4AtomicTransitionManager::~G4AtomicTransitionManager() {
-
-  if (instance) {
-
-    G4std::map <G4int, G4std::vector<G4AtomicShell> >::iterator cur;
-    for(cur = shellTable.begin(); cur != shellTable.end(); ++cur) {
-
-      shellTable.erase(cur);
+ G4std::vector<G4AtomicShell*> vectorOfShells;  
+    
+    G4int Z = ZVector[index];
+    G4FirstLevel* theBindEnVec = (*theBindingEnergyTable)[Z-1];
+    
+    G4int size = ((*theBindEnVec)[0])->size();
+   
+    for (ShellIndex=0; ShellIndex<size; ShellIndex++){
+      
+      G4int  id = (G4int) (*(*theBindEnVec)[0])[ShellIndex];
+      
+       G4DataVector finalId = 0;
+    
+       G4DataVector prob = 0;
+    
+       G4DataVector energies = 0;
+      
+      G4double energy = ((*(*theBindEnVec)[1])[ShellIndex])*MeV;
+      
+      G4SecondLevel* oneAtomTrans = (*theTransitionTable)[index];
+      
+      G4int FinalShellIndex = 0;
+      
+      G4int ShellCol = 0,   ProbCol = 1, EnergyCol = 2;
+      
+      G4int SizeOneAtomTrans = oneAtomTrans->size();
+      
+      for (FinalShellIndex=0; FinalShellIndex<SizeOneAtomTrans; FinalShellIndex++){
+	
+	G4int sizeOneFinalShellVec = (*(*oneAtomTrans)[FinalShellIndex]).size();
+	
+	
+	for(G4int k = 0; k<sizeOneFinalShellVec; k++){
+	  
+	  G4int tmpId = (G4int) (*(*(*oneAtomTrans)[FinalShellIndex])[ShellCol])[k];  
+	  
+	  if (id == tmpId){
+	    
+	    prob.push_back((*(*(*oneAtomTrans)[FinalShellIndex])[ProbCol])[k]);
+	    
+	    G4double ppo =((*(*(*oneAtomTrans)[FinalShellIndex])[ProbCol])[k]);
+	    
+	    energies.push_back((*(*(*oneAtomTrans)[FinalShellIndex])[EnergyCol])[k]);
+	    
+	    finalId.push_back((*(*(*oneAtomTrans)[FinalShellIndex])[ShellCol])[0]);
+	    
+	  }
+	  else{ 
+	    
+	  }
+	  
+	} 
+	
+      }  
+      //no radiative transition allowed
+     
+      while (prob.size()<=ShellIndex)
+	{
+	  prob.push_back(0.);
+	  
+	  energies.push_back(0.);
+	  
+	  finalId.push_back(0.);
+	  
+	}
+      
+      G4AtomicShell *shell = new G4AtomicShell (id,energy,prob,energies,finalId);
+      
+  vectorOfShells.push_back(shell);
+      
     }
-
-    instance = 0;
-
+  shellTable.insert(std::make_pair(Z, vectorOfShells));
+    
   }
   
-
-}
-
-//....................000000000000000...................000000000000............00000000000000
-
-G4int G4AtomicTransitionManager::NumberOfShells(G4int Z) {
-
-  return shellTable[Z].size(); 
-
-}
-
-//....................000000000000000...................000000000000............00000000000000
-
-// This returns the probability a fluorescence photon is emitted. It is the sum of the probabilities
-// of emission for all possible transitions of the selected subshell
-
-G4double G4AtomicTransitionManager::TotalRadiativeTransitionProbability(G4int Z, G4int subShellId) {
+  delete theBindingEnergyTable;
+  delete theTransitionTable;
   
-  G4double theRadTransProb(0);
- 
-  G4std::vector<G4int> subShellVect = Shell(Z,subShellId)->TransSubShellIdentifiers();
+}
 
-  for(G4int transId =0; transId != subShellVect.size(); transId++) {
+G4AtomicTransitionManager::~G4AtomicTransitionManager()
 
-    theRadTransProb += Shell(Z,subShellId)->TransitionProbability(transId);
+{ G4std::map<G4int,G4std::vector<G4AtomicShell*>,std::less<G4int> >::iterator pos;
+
+ for (pos = shellTable.begin(); pos != shellTable.end(); pos++){
+   
+   G4std::vector< G4AtomicShell*>vec = pos->second;
+
+     G4int vecSize=vec.size();
+   
+   for (G4int i=0; i< vecSize; i++){
+     
+     delete vec[i]; 
   }
 
-  return theRadTransProb;
-
+ }
+   
 }
-
-//....................000000000000000...................000000000000............00000000000000
-
-// since no auger effect is simulated yet, and first of all because probabilities r normalized 
-// to one on all the transitions (radiative and non-radiative), total non radiative transition probability
-// is computed as 1-(total radiative tarnsition probability)
-
-G4double G4AtomicTransitionManager::TotalNonRadiativeTransitionProbability(G4int Z, G4int subShellId) {
-
-  G4double nonRadProb = (1.0)-(TotalRadiativeTransitionProbability(Z, subShellId));
-  return nonRadProb;
-
-}
-//....................000000000000000...................000000000000............00000000000000
-
-
-G4AtomicShell* G4AtomicTransitionManager::Shell(G4int Z, G4int subShellId) {
-
-  G4AtomicShell* tmp;
-  *tmp = shellTable[Z][subShellId];
-
-  return tmp;
-
-}
-
-//....................000000000000000...................000000000000............00000000000000
-
-// this is a trick to get one only object of this type to exist at the same time
 
 G4AtomicTransitionManager* G4AtomicTransitionManager::instance = 0;
 
-G4AtomicTransitionManager* G4AtomicTransitionManager::Instance() {
-  if (instance = 0) {
-
-    instance = new G4AtomicTransitionManager;
-  }
+G4AtomicTransitionManager* G4AtomicTransitionManager::Instance()
+{
+  if (instance==0)
+    {
+      instance = new G4AtomicTransitionManager;
+     
+    }
   return instance;
 }
 
-//....................000000000000000...................000000000000............00000000000000
+  //build the table for the binding energies.
+//theBindingEnergyTable has three degrees of fredom (consequently three indexes):
+//the first ranges from 0 to the size of ZVector and is used to choose the element.
+//its value os the index of the position the Z of the choosen element occupies in ZVector
+//the second index is used to discriminate between shells[0] and binding energies[0]
+//the third index is used to choose a shell and its binding energy
 
-// this function read from EADL the binding energies of any subshell of the atom, and creates 
-// the objects "shells" to be put in a map where to every Z is associated the vector of the corresponding shells
-
-void G4AtomicTransitionManager::BuildTable() {
-
-  G4int Z; 
-
-  G4int dataNum = 2;
+G4SecondLevel* G4AtomicTransitionManager::BuildBindingEnergiesTable()
+{
   
-  G4SecondLevel* theBindingEnergyTable = util.BuildSecondLevelTables(0,dataNum,"fluor/binding");
-
-  for (Z = 1; Z<=99; Z++) {
-
+  G4int dataNum = 2;
+  G4SecondLevel*theBindingEnergyTable = util.BuildSecondLevelTables(0,dataNum,"fluor/binding");
+  
+return theBindingEnergyTable;
+}
+//this function builds the table for the radiative transitions
+//they have four degrees of fredoom (four indexes):
+//the first ranges from 0 to the size of ZVector and is used to choose the element.
+//the second is used to select the values relative to all the radiative transition
+//towards a given final shell
+//the third is used to disctiminate between identities of the shells [0], transition 
+//probabilities[1] and transition energies[2]
+//the fourth is used to select a given starting shell for the transition
+G4ThirdLevel* G4AtomicTransitionManager::BuildTransitionTable()
+{
  
-    dataNum=3;
-    G4SecondLevel* theAtomShellFL = util.BuildSecondLevelTables(Z, dataNum, "fluor/fl-tr-pr-");
+  BuildZVec();
+ 
+   G4ThirdLevel* theTransitionTable = new G4ThirdLevel();
+  
+  G4int dataNumb = 3;
+  
+ for(G4int tableInd = 0; tableInd <ZVector.size()  ; tableInd++){
 
+    G4int atomInd =  ZVector[tableInd];
 
-    //Remember first position in vector is position 0
-
-    G4FirstLevel* theBindEnVec = (*theBindingEnergyTable)[Z-1];
-    
-    G4std::vector<G4AtomicShell> zShellVec;
-
-    G4std::vector<G4int> finalTransitionSubShellIdVect(0);
-    G4std::vector<G4double> transitionProbabilities(0);
-    G4std::vector<G4double> transitionEnergies(0);
-    
-    
-    for(G4int subShellId = 0; subShellId < (*theBindEnVec)[0]->size(); subShellId++) {
-
-      G4int shellId = (G4int) (*(*theBindEnVec)[0])[subShellId];
-
-      G4double theBindEnergy=((*(*theBindEnVec)[1])[subShellId])*MeV;
-
-      G4FirstLevel* theSubShellFL = (*theAtomShellFL)[subShellId];
-
-
-      // Fluorescence data r present only for Z > 5
-
-      G4AtomicShell subShell;
-
-      if(Z > 5){
-      
-
-	// theSubShellFL is a table that has, 4 each shell (in wich ther is a vacancy) of the atom three columns: 
-	// the subshell availables, the probability that this transition occurs, 
-	// and the energy of the gamma emitted
-
-	const G4int SubShellCol = 0, ProbCol = 1, EnergyCol = 2;
-
-	// Transnum is the index of the loop and of the table of transition. it starts from 1
-	// because the first element of the data table is the primary shell id number and not a
-	// transition probability.
-
-	for(G4int TransNum = 1; TransNum < (*theSubShellFL)[ProbCol]->size(); TransNum++) {
-
-	  G4int finalTrSubShell = (*(*theSubShellFL)[SubShellCol])[TransNum];
-	  G4double transProb = (G4double) (*(*theSubShellFL)[ProbCol])[TransNum];
-	  G4double gammaTransEnergy = (G4double) ((*(*theSubShellFL)[EnergyCol])[TransNum])*MeV;
-	
-	  // finTranSubShellId is a vector where there r the possible destination subshells' Ids
-	  // transProbabilities is a vector where is stored the probabilities of the above destinations occurs
-	  // transEnergies is a vector containing the energy of the gamma emitted by above transitions.
-
-	  finalTransitionSubShellIdVect.push_back(finalTrSubShell); 
-	  transitionProbabilities.push_back(transProb); 
-	  transitionEnergies.push_back(gammaTransEnergy);
-	}
-
-
-	// Now I have all the data needed to create a subshell object, and i do it:
-
-	// G4AtomicShell subShell = new G4AtomicShell(shellId,theBindEnergy,finalTransitionSubShellIdVect,
-	//					   transitionProbabilities,transitionEnergies);
-      }
-       
-    
-      // describing what happens if Z<=5: subshell is created but data regarding transitions are null
-      /*
-      else {
-
-	finalTransitionSubShellIdVect = 0; 
-	transitionProbabilities = 0.0; 
-	transitionEnergies = 0.0;
-
-	// G4AtomicShell subShell = new G4AtomicShell(shellId,theBindEnergy,finalTransitionSubShellIdVect,
-	// transitionProbabilities,transitionEnergies);
-	}*/
-
-      subShell =  G4AtomicShell(shellId,theBindEnergy,finalTransitionSubShellIdVect,
-				transitionProbabilities,transitionEnergies);
-
-      // a shell vector for the atom Z has been created: now I put it to the map of the atom's shells
-
-      zShellVec.push_back(subShell);
+    if(atomInd > tableLimit){
+  
+      G4SecondLevel* oneAtomTransProb = util.BuildSecondLevelTables(atomInd, dataNumb, "fluor/fl-tr-pr-");
+     
+      theTransitionTable->push_back(oneAtomTransProb);
     }
-    /*
-    for (G4int sShell = 0; sShell< zShellVec.size(); sShell++) {
-
-      shellTable[Z][sShell] = zShellVec[sShell];
-    }
-    */
-
-
-	     
-    shellTable[Z].operator=(zShellVec); 
+    else{
+      ZVector.erase(remove( ZVector.begin(), ZVector.end(),atomInd),ZVector.end()) ;
+     
+       tableInd=tableInd-1;
+    } 
+   
   }
 
+   return theTransitionTable;
 }
 
+void G4AtomicTransitionManager::BuildZVec(){
+  
+  const G4MaterialTable* theMaterialTable=G4Material::GetMaterialTable();
+  G4int numOfMaterials = theMaterialTable->length();
+   
+   for (G4int J=0 ; J < numOfMaterials; J++){ 
+    
+    const G4Material* material= (*theMaterialTable)[J];        
+    const G4ElementVector* theElementVector = material->GetElementVector();
+    const G4int NumberOfElements = material->GetNumberOfElements() ;
+    
+    for (G4int iel=0; iel<NumberOfElements; iel++ ){
+      
+      G4double Zel = (*theElementVector)(iel)->GetZ();
+      
+      G4int ZVectorSize = ZVector.size();
+      
+      G4int k = 0;
+      
+      for( G4int i = 0; i<ZVectorSize; i++){
+	
+	if (Zel !=ZVector[i]){k=k+1;}
+	
+	else{}
+	
+      }
+      
+      if ( k==ZVectorSize){
+      
+	ZVector.push_back(Zel);
+	
+      } 
+      else
+	{
+	  continue;
+	  
+	}
+    }
+    
+   }
+   
+}
 
+const G4AtomicShell* G4AtomicTransitionManager::Shell(G4int z, G4int shellIdentifier)
+{ 
+  G4std::map<G4int,G4std::vector<G4AtomicShell*>,std::less<G4int> >::iterator pos;
+
+  pos = shellTable.find(z);
+
+  if (pos!= shellTable.end()){
+
+    G4std::vector<G4AtomicShell*> v = pos->second;
+
+G4int index =v.size();
+ 
+    for (G4int i=0; i<v.size(); i++){
+
+      G4int tmpId = (v[i])->ShellId();
+      if (tmpId==shellIdentifier)
+	{index = i;}
+      else{}
+    }  
+  
+ if (index< v.size()){
+  
+  
+ return (v[index]);
+      
+    }
+    else {
+      G4Exception("shell not found");
+	
+    }
+  }
+  else{
+    G4Exception("Z not found");
+	
+  } 
+}
+
+G4int G4AtomicTransitionManager::NumberOfShells (G4int z)
+
+{
+
+G4std::map<G4int,G4std::vector<G4AtomicShell*>,std::less<G4int> >::iterator pos;
+
+  pos = shellTable.find(z);
+
+  if (pos!= shellTable.end()){
+
+    G4std::vector<G4AtomicShell*> v = pos->second;
+
+    return v.size();
+  }
+
+  else{
+    G4Exception( "Z not found" );
+  } 
+}
+
+G4double G4AtomicTransitionManager::TotalRadiativeTransitionProbability(G4int z, G4int shellId)
+
+{
+
+G4std::map<G4int,G4std::vector<G4AtomicShell*>,std::less<G4int> >::iterator pos;
+
+  pos = shellTable.find(z);
+
+  if (pos!= shellTable.end()){
+
+    G4std::vector<G4AtomicShell*> v = pos->second;
+  
+    G4int index = v.size();
+
+    for (G4int i=0; i<v.size(); i++){
+
+      G4int tmpId = (v[i])->ShellId();
+      
+      if (tmpId==shellId)
+	
+	{index = i;}
+      
+      else{}
+    }  
+  
+ if (index< v.size()){
+  
+   G4AtomicShell* shell=v[index];
+
+   G4DataVector transProb = shell->TransitionProbabilities();
+
+   G4double totalRadTransProb = 0;
+
+   for(G4int j = 0; j<transProb.size(); j++){
+
+     totalRadTransProb = totalRadTransProb + transProb[j];
+
+   }
+
+   return totalRadTransProb;   
+      
+    }
+ else {
+   G4Exception( "shell not found" );
+	
+ }
+  }
+  else{
+    G4Exception( "Z not found");
+  } 
+}
+
+G4double G4AtomicTransitionManager::TotalNonRadiativeTransitionProbability(G4int z, G4int shellId)
+
+{
+
+  G4std::map<G4int,G4std::vector<G4AtomicShell*>,std::less<G4int> >::iterator pos;
+  
+  pos = shellTable.find(z);
+  
+  if (pos!= shellTable.end()){
+    
+    G4std::vector<G4AtomicShell*> v = pos->second;
+    
+    G4int index = v.size();
+    
+    for (G4int i=0; i<v.size(); i++){
+      
+      G4int tmpId=(v[i])->ShellId();
+      if (tmpId==shellId)
+	{index = i;}
+      else{}
+    }  
+    
+    if (index< v.size()){
+      
+      G4AtomicShell* shell=v[index];
+      
+      G4DataVector transProb = shell->TransitionProbabilities();
+      
+      G4double totalRadTransProb = 0;
+      
+      for(G4int j = 0; j<transProb.size(); j++){
+
+	totalRadTransProb = totalRadTransProb + transProb[j];
+      }
+      
+      G4double totalNonRadTransProb= (1 - totalRadTransProb);
+      
+      return totalNonRadTransProb;    }
+    
+    else {
+      G4Exception( "shell not found");
+    }
+  }
+  else{
+    G4Exception("Z not found");
+  } 
+}
 
