e92b3a030445b9e4f6ca95becf955b4f31132207
Elena Gianolio <elena@cern.ch>
2001-07-30 13:44:58 +0000

    the source file of the class that creates a shell
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@7574 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/electromagnetic/lowenergy/src/G4AtomicShell.cc
+++ source/processes/electromagnetic/lowenergy/src/G4AtomicShell.cc
@@ -1,29 +1,15 @@
+// This code implementation is the intellectual property of
+// the GEANT4 collaboration.
 //
-// ********************************************************************
-// * DISCLAIMER                                                       *
-// *                                                                  *
-// * The following disclaimer summarizes all the specific disclaimers *
-// * of contributors to this software. The specific disclaimers,which *
-// * govern, are listed with their locations in:                      *
-// *   http://cern.ch/geant4/license                                  *
-// *                                                                  *
-// * Neither the authors of this software system, nor their employing *
-// * institutes,nor the agencies providing financial support for this *
-// * work  make  any representation or  warranty, express or implied, *
-// * regarding  this  software system or assume any liability for its *
-// * use.                                                             *
-// *                                                                  *
-// * This  code  implementation is the  intellectual property  of the *
-// * GEANT4 collaboration.                                            *
-// * By copying,  distributing  or modifying the Program (or any work *
-// * based  on  the Program)  you indicate  your  acceptance of  this *
-// * statement, and all its terms.                                    *
-// ********************************************************************
-//
+// By copying, distributing or modifying the Program (or any work
+// based on the Program) you indicate your acceptance of this statement,
+// and all its terms.
 //
 // -------------------------------------------------------------------
 //      GEANT 4 class implementation file 
 //
+//      For information related to this code contact:
+//      Geant4 Collaboration
 //
 //      File name:     G4AtomicShell.cc
 //
