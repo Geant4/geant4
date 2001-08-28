7439dba352bfd26892627062d06750a54a99e20e
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2001-08-28 13:07:22 +0000

    Restored old version... only Raytracer can be supported here!
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@7799 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- tests/test05/src/Tst05VisManager.cc
+++ tests/test05/src/Tst05VisManager.cc
@@ -21,7 +21,7 @@
 // ********************************************************************
 //
 //
-// $Id: Tst05VisManager.cc,v 1.4 2001-07-11 10:09:39 gunter Exp $
+// $Id: Tst05VisManager.cc,v 1.5 2001-08-28 13:07:22 gcosmo Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 
@@ -34,58 +34,8 @@
 
 // Supported drivers...
 
-#ifdef G4VIS_USE_ASCIITREE
-#include "G4ASCIITree.hh"
-#endif
-
-#ifdef G4VIS_USE_DAWN
-#include "G4FukuiRenderer.hh"
-#endif
-
-#ifdef G4VIS_USE_DAWNFILE
-#include "G4DAWNFILE.hh"
-#endif
-
-#ifdef G4VIS_USE_OPACS
-#include "G4Wo.hh"
-#include "G4Xo.hh"
-#endif
-
-#ifdef G4VIS_USE_OPENGLX
-#include "G4OpenGLImmediateX.hh"
-#include "G4OpenGLStoredX.hh"
-#endif
-
-#ifdef G4VIS_USE_OPENGLWIN32
-#include "G4OpenGLImmediateWin32.hh"
-#include "G4OpenGLStoredWin32.hh"
-#endif
-
-#ifdef G4VIS_USE_OPENGLXM
-#include "G4OpenGLImmediateXm.hh"
-#include "G4OpenGLStoredXm.hh"
-#endif
-
-#ifdef G4VIS_USE_OIX
-#include "G4OpenInventorX.hh"
-#endif
-
-#ifdef G4VIS_USE_OIWIN32
-#include "G4OpenInventorWin32.hh"
-#endif
-
 #ifdef G4VIS_USE_RAYTRACER
 #include "G4RayTracer.hh"
-#endif
-
-#ifdef G4VIS_USE_VRML
-#include "G4VRML1.hh"
-#include "G4VRML2.hh"
-#endif
-
-#ifdef G4VIS_USE_VRMLFILE
-#include "G4VRML1File.hh"
-#include "G4VRML2File.hh"
 #endif
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
@@ -96,58 +46,8 @@ Tst05VisManager::Tst05VisManager () {}
 
 void Tst05VisManager::RegisterGraphicsSystems () {
 
-#ifdef G4VIS_USE_ASCIITREE
-  RegisterGraphicsSystem (new G4ASCIITree);
-#endif
-
-#ifdef G4VIS_USE_DAWN
-  RegisterGraphicsSystem (new G4FukuiRenderer);
-#endif
-
-#ifdef G4VIS_USE_DAWNFILE
-  RegisterGraphicsSystem (new G4DAWNFILE);
-#endif
-
-#ifdef G4VIS_USE_OPACS
-  RegisterGraphicsSystem (new G4Wo);
-  RegisterGraphicsSystem (new G4Xo);
-#endif
-
-#ifdef G4VIS_USE_OPENGLX
-  RegisterGraphicsSystem (new G4OpenGLImmediateX);
-  RegisterGraphicsSystem (new G4OpenGLStoredX);
-#endif
-
-#ifdef G4VIS_USE_OPENGLWIN32
-  RegisterGraphicsSystem (new G4OpenGLImmediateWin32);
-  RegisterGraphicsSystem (new G4OpenGLStoredWin32);
-#endif
-
-#ifdef G4VIS_USE_OPENGLXM
-  RegisterGraphicsSystem (new G4OpenGLImmediateXm);
-  RegisterGraphicsSystem (new G4OpenGLStoredXm);
-#endif
-
-#ifdef G4VIS_USE_OIX
-  RegisterGraphicsSystem (new G4OpenInventorX);
-#endif
-
-#ifdef G4VIS_USE_OIWIN32
-  RegisterGraphicsSystem (new G4OpenInventorWin32);
-#endif
-
 #ifdef G4VIS_USE_RAYTRACER
   RegisterGraphicsSystem (new G4RayTracer);
-#endif
-
-#ifdef G4VIS_USE_VRML
-  RegisterGraphicsSystem (new G4VRML1);
-  RegisterGraphicsSystem (new G4VRML2);
-#endif
-
-#ifdef G4VIS_USE_VRMLFILE
-  RegisterGraphicsSystem (new G4VRML1File);
-  RegisterGraphicsSystem (new G4VRML2File);
 #endif
 
   if (fVerbose > 0) {
