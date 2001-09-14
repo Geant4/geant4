7a70010001552183f2c519a7243817adb2fa910a
Vladimir Ivantchenko <Vladimir.Ivantchenko@cern.ch>
2001-09-14 08:17:13 +0000

    design iteration
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@7905 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/electromagnetic/lowenergy/src/G4BremsstrahlungParameters.cc
+++ source/processes/electromagnetic/lowenergy/src/G4BremsstrahlungParameters.cc
@@ -21,7 +21,7 @@
 // ********************************************************************
 //
 //
-// $Id: G4BremsstrahlungParameters.cc,v 1.1 2001-08-20 16:37:37 pia Exp $
+// $Id: G4BremsstrahlungParameters.cc,v 1.2 2001-09-14 08:17:13 vnivanch Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // Author: Maria Grazia Pia (Maria.Grazia.Pia@cern.ch)
@@ -29,15 +29,19 @@
 // History:
 // -----------
 // 31 Jul 2001   MGP        Created
+// 13.09.01 V.Ivanchenko  Add paramA map
 //
 // -------------------------------------------------------------------
 
 #include "G4BremsstrahlungParameters.hh"
 #include "G4VEMDataSet.hh"
 #include "G4EMDataSet.hh"
-#include "G4CompositeEMDataSet.hh"
+#include "G4LogLogInterpolation.hh"
+#include "G4MaterialTable.hh"
 #include "g4std/fstream"
 #include "g4std/strstream"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
 
 G4BremsstrahlungParameters:: G4BremsstrahlungParameters(G4int minZ, G4int maxZ)
   : zMin(minZ), zMax(maxZ)
@@ -45,20 +49,51 @@ G4BremsstrahlungParameters:: G4BremsstrahlungParameters(G4int minZ, G4int maxZ)
   LoadData();
 }
 
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
 G4BremsstrahlungParameters::~G4BremsstrahlungParameters()
-{ }
+{ 
+  // Reset the map of data sets: remove the data sets from the map 
+  G4std::map<G4int,G4VEMDataSet*,G4std::less<G4int> >::iterator pos;
+
+  for (pos = paramA.begin(); pos != paramA.end(); pos++)
+    {
+      G4VEMDataSet* dataSet = pos->second;
+      paramA.erase(pos);
+      delete dataSet;
+    }
+
+  activeZ.clear();
+  delete interpolation;
+  paramB0.clear();
+  paramB1.clear();
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
 
 G4double G4BremsstrahlungParameters::ParameterA(G4int Z, G4double energy) const
 {
-  // To be implemented
   G4double value = 0.;
+  G4std::map<G4int,G4VEMDataSet*,G4std::less<G4int> >::const_iterator pos;
+  pos = paramA.find(Z);
+  if (pos!= paramA.end()) {
+    G4VEMDataSet* dataSet = pos->second;
+    value = dataSet->FindValue(energy);
+  } else {
+    G4String ex = "G4BremsstrahlungParameters::ParameterA - wrong Z=" + Z; 
+    G4Exception(ex);
+  }
   return value;
 }
 
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
 G4double G4BremsstrahlungParameters::ParameterB(G4int Z, G4int id) const
 {
-  if (Z < (zMin-1) || Z > (zMax-1))
-    G4Exception("G4BremsstrahlungParameters::FindValue - index outside boundaries");
+  if (Z < (zMin-1) || Z > (zMax-1)) {
+    G4String ex = "G4BremsstrahlungParameters::ParameterB - wrong Z=" + Z; 
+    G4Exception(ex);
+  }
 
   G4double value;
 
@@ -73,13 +108,111 @@ G4double G4BremsstrahlungParameters::ParameterB(G4int Z, G4int id) const
   return value;
 }
 
-
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
 
 void G4BremsstrahlungParameters::LoadData()
 {
-  // Partial implementation, to be completed
-
   // Build the complete string identifying the file with the data set
+
+  if(!interpolation) interpolation = new G4LogLogInterpolation();
+
+  // define active elements
+
+  const G4MaterialTable* materialTable = G4Material::GetMaterialTable();
+  if (materialTable == 0)
+     G4Exception("G4CrossSectionHandler: no MaterialTable found)");
+
+  G4int nMaterials = materialTable->length();
+  
+  for (G4int m=0; m<nMaterials; m++) {
+
+    const G4Material* material= (*materialTable)[m];        
+    const G4ElementVector* elementVector = material->GetElementVector();
+    const G4int nElements = material->GetNumberOfElements();
+      
+    for (G4int iEl=0; iEl<nElements; iEl++) {
+      G4Element* element = (*elementVector)[iEl];
+      G4double Z = element->GetZ();
+      if (!(activeZ.contains(Z)) && Z > 0 && Z < zMax) {
+	 activeZ.push_back(Z);
+      }
+    }
+  }
+
+  // Read parameters A
+   
+  char* path = getenv("G4LEDATA");
+  if (!path)
+    { 
+      G4String excep = G4String("G4BremsstrahlungParameters - G4LEDATA") +
+                     + G4String("environment variable not set");
+      G4Exception(excep);
+    }
+
+  G4String pathString_a(path);
+  G4String name_a = pathString_a + "/brem/br-co-a.dat";  
+  G4std::ifstream file_a(name_a);
+  G4std::filebuf* lsdp_a = file_a.rdbuf();
+  
+  if (! (lsdp_a->is_open()) ) {
+     G4String excep = G4String("G4BremsstrahlungParameters: cannot open file ")
+                    + name_a;
+     G4Exception(excep);
+  }
+
+  // The file is organized into two columns:
+  // 1st column is the energy
+  // 2nd column is the corresponding value
+  // The file terminates with the pattern: -1   -1
+  //                                       -2   -2
+
+  G4DataVector* energies;
+  G4DataVector* data;
+  G4double e = 0;
+  G4double a = 0;
+  size_t nZ  = activeZ.size();
+  energies   = new G4DataVector(); 
+  data       = new G4DataVector(); 
+  G4int z    = 0;
+
+  do {
+    file_a >> e >> a;
+
+    // End of the next element
+    if (e == -1) {
+
+      z++;
+      G4bool used = false;
+      for (size_t i=0; i<nZ; i++) {
+    
+	// fill map
+        if(z == (G4int)activeZ[i]) {
+          G4VEMDataSet* dataSet = 
+                        new G4EMDataSet(z, energies, data, interpolation);
+          paramA[z] = dataSet;
+          used = true;
+	}
+      }
+      if(!used) {
+        energies->clear();
+        data->clear();
+      }
+  
+      if (z > 98) break;
+
+      energies   = new G4DataVector(); 
+      data       = new G4DataVector(); 
+      
+    } else {
+
+      energies->push_back(e);
+      data->push_back(a);
+    }
+  } while (e != -2);
+  
+  file_a.close();
+
+  // Read parameters B
   
   char nameChar[100] = {""};
   G4std::ostrstream ost(nameChar, 100, G4std::ios::out);
@@ -88,14 +221,7 @@ void G4BremsstrahlungParameters::LoadData()
   ost << fileName  << ".dat";
   
   G4String name(nameChar);
-  
-  char* path = getenv("G4LEDATA");
-  if (!path)
-    { 
-      G4String excep = "G4BremsstrahlungParameters - G4LEDATA environment variable not set";
-      G4Exception(excep);
-    }
-  
+    
   G4String pathString(path);
   G4String dirFile = pathString + "/" + name;
   G4std::ifstream file(dirFile);
@@ -108,7 +234,7 @@ void G4BremsstrahlungParameters::LoadData()
 	}
 
   G4int nParam = 2;
-  G4double a = 0;
+  a = 0;
   G4int k = 1;
 
   do
@@ -132,20 +258,50 @@ void G4BremsstrahlungParameters::LoadData()
 	}
     } while (a != -2); // end of file
   
+  zMax = paramB0.size();
   file.close();
 }
 
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
 void G4BremsstrahlungParameters::PrintData() const
 {
-  // To be completed
+  
+  G4cout << G4endl;
+  G4cout << "===== G4BremsstrahlungParameters =====" << G4endl;
+  G4cout << G4endl;
+  G4cout << "===== Parameter A =====" << G4endl;
+  G4cout << G4endl;
+
+  size_t nZ = activeZ.size();
+  G4std::map<G4int,G4VEMDataSet*,G4std::less<G4int> >::const_iterator pos;
+
+  for (G4int j=0; j<nZ; j++) {
+    G4int Z = (G4int)activeZ[j];   
+    pos = paramA.find(Z);
+    if (pos!= paramA.end()) {
+      G4cout << "===== Z= " << Z << " =====" << G4endl;
+      G4VEMDataSet* dataSet = pos->second;
+      dataSet->PrintData();
+    }
+  }
+
+  G4cout << G4endl;
+  G4cout << "===== Parameter B =====" << G4endl;
+  G4cout << G4endl;
+
   for (G4int i=zMin-1; i<zMax-1; i++)
     {
       G4double b0 = paramB0[i];
-      G4double b1 = paramB1[i] ;
+      G4double b1 = paramB1[i];
       G4cout << "Z = " << i << " - Parameter B: "
 	     << b0
 	     << ",  "
 	     << b1
 	     << G4endl; 
     }
+  G4cout << "==========================================" << G4endl;
 }
+
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
--- source/processes/electromagnetic/lowenergy/src/G4eIonisationParameters.cc
+++ source/processes/electromagnetic/lowenergy/src/G4eIonisationParameters.cc
@@ -21,7 +21,7 @@
 // ********************************************************************
 //
 //
-// $Id: G4eIonisationParameters.cc,v 1.2 2001-08-20 17:05:10 pia Exp $
+// $Id: G4eIonisationParameters.cc,v 1.3 2001-09-14 08:17:12 vnivanch Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // Author: Maria Grazia Pia (Maria.Grazia.Pia@cern.ch)
@@ -29,41 +29,239 @@
 // History:
 // -----------
 // 31 Jul 2001   MGP        Created
+// 12.09.01 V.Ivanchenko    Add param and interpolation of parameters  
 //
 // -------------------------------------------------------------------
 
 #include "G4eIonisationParameters.hh"
+#include "G4VEMDataSet.hh"
+#include "G4EMDataSet.hh"
+#include "G4ShellEMDataSet.hh"
+#include "G4SemiLogInterpolation.hh"
+#include "G4MaterialTable.hh"
 #include "G4DataVector.hh"
 #include "g4std/fstream"
 #include "g4std/strstream"
 
-G4IonisationParameters:: G4IonisationParameters(G4int minZ, G4int maxZ)
-  : zMin(minZ), zMax(maxZ)
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+G4eIonisationParameters:: G4eIonisationParameters(G4int minZ, G4int maxZ)
+  : zMin(minZ), zMax(maxZ),
+  length(16),
+  interpolation(0)
 {
   LoadData();
 }
 
-G4IonisationParameters::~G4IonisationParameters()
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+G4eIonisationParameters::~G4eIonisationParameters()
 { }
 
-G4double G4IonisationParameters::Parameter(G4int Z, G4int shellIndex, G4int parameterIndex) const
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+G4double G4eIonisationParameters::Parameter(G4int Z, G4int shellIndex, 
+                                                     G4int parameterIndex, 
+                                                     G4double e) const
 {
-  // To be implemented
   G4double value = 0.;
+  G4int id = Z*20 + parameterIndex;
+  G4std::map<G4int,G4VEMDataSet*,G4std::less<G4int> >::const_iterator pos;
+
+  pos = param.find(id);
+  if (pos!= param.end()) {
+    G4VEMDataSet* dataSet = pos->second;
+    size_t nShells = dataSet->NumberOfComponents();
+
+    if(shellIndex < nShells) { 
+      const G4VEMDataSet* comp = dataSet->GetComponent(shellIndex);
+      const G4DataVector ener = comp->GetEnergies(0);
+      G4double ee = G4std::max(ener.front(),G4std::min(ener.back(),e));
+      G4double value = comp->FindValue(ee);
+
+    } else {
+      G4cout << "WARNING: G4IonisationParameters::FindParameters "
+             << "has no parameters for shell= " << shellIndex 
+             << "; Z= " << Z
+             << G4endl;
+    }
+  } else {
+    G4cout << "WARNING: G4IonisationParameters::FindValue "
+           << "did not find ID = "
+           << index << G4endl;
+  }
+
   return value;
 }
 
-//const G4DataVector& G4IonisationParameters::Parameters(G4int Z, G4int shellIndex) const
-//{
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+const G4DataVector& G4eIonisationParameters::Parameters(G4int Z, 
+                                                       G4int shellIndex,
+                                                       G4double e) const
+{
 // To be implemented
-//}
-
-void G4IonisationParameters::LoadData()
-{
-  // To be implemented
+  G4DataVector* p = new G4DataVector();
+  return *p;
 }
 
-void G4IonisationParameters::PrintData() const
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+void G4eIonisationParameters::LoadData()
 {
-  // To be implemented
+  if(!interpolation)  interpolation  = new G4SemiLogInterpolation();
+  // define active elements
+
+  const G4MaterialTable* materialTable = G4Material::GetMaterialTable();
+  if (materialTable == 0)
+     G4Exception("G4CrossSectionHandler: no MaterialTable found)");
+
+  G4int nMaterials = materialTable->length();
+  
+  for (G4int m=0; m<nMaterials; m++) {
+
+    const G4Material* material= (*materialTable)[m];        
+    const G4ElementVector* elementVector = material->GetElementVector();
+    const G4int nElements = material->GetNumberOfElements();
+      
+    for (size_t iEl=0; iEl<nElements; iEl++) {
+      G4Element* element = (*elementVector)[iEl];
+      G4double Z = element->GetZ();
+      if (!(activeZ.contains(Z)) && Z > 0 && Z < zMax) {
+	 activeZ.push_back(Z);
+      }
+    }
+  }
+  char* path = getenv("G4LEDATA");
+  if (!path)
+    { 
+      G4String excep = "G4EMDataSet - G4LEDATA environment variable not set";
+      G4Exception(excep);
+    }
+  G4String pathString(path);
+  pathString += "/ioni/io-co-";  
+
+  G4double energy;
+  G4DataVector* e;
+  G4VEMDataSet* set;
+  G4std::vector<G4DataVector*> a;
+  G4std::vector<G4ShellEMDataSet*> p;
+  a.resize(length);
+  p.resize(length);
+
+  size_t nZ = activeZ.size();
+
+  for (G4int i=0; i<nZ; i++) {
+
+    G4int Z = (G4int)activeZ[i];
+    char nameChar[100] = {""};
+    G4std::ostrstream ost(nameChar, 100, G4std::ios::out);
+    ost << pathString << Z << ".dat";
+    G4String name(nameChar);  
+
+    G4std::ifstream file(name);
+    G4std::filebuf* lsdp = file.rdbuf();
+  
+    if (! (lsdp->is_open()) ) {
+      G4String excep = "G4IonisationParameters - data file: " 
+                     + name + " not found";
+      G4Exception(excep);
+    }
+
+    // The file is organized into two columns:
+    // 1st column is the energy
+    // 2nd column is the corresponding value
+    // The file terminates with the pattern: -1   -1
+    //                                       -2   
+
+    e   = new G4DataVector();
+    for(size_t j=0; j<length; j++) {
+      a[j] = new G4DataVector();
+    } 
+    for(size_t k=0; k<length; k++) {
+      G4int id = Z*20 + k;
+      p[k] = new G4ShellEMDataSet(id, interpolation, 1., 1.);
+    }
+
+    do {
+      file >> energy;
+      if (energy == -2) break;
+      if (energy >  -1) e->push_back(energy);
+
+      G4double x;
+      for (size_t j=0; j<length; j++) {
+          file >> x;    
+          if(energy > -1) a[j]->push_back(x);
+      }
+
+      // fill map
+      if(energy < 0) {
+
+        G4int id;
+        for(G4int k=0; k<length; k++) {
+            set = new G4EMDataSet(k, e, a[k], interpolation, 1., 1.);
+            p[k]->AddComponent(set);
+            G4int id = Z*20 + k;
+        } 
+
+	// clear vectors
+        e   = new G4DataVector();
+        for(size_t j2=0; j2<length; j2++) {
+          a[j2] = new G4DataVector();
+        } 
+      }
+    } while (energy > -2);
+
+    file.close();
+
+    for(size_t kk=0; kk<length; kk++) {
+      G4int id = Z*20 + kk;
+      param[id] = p[kk];
+    }
+
+    // remove last vectors because they are not used
+    delete e;
+    for(size_t j1=0; j1<length; j1++) {
+      delete  a[j1];
+    } 
+  }
 }
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+void G4eIonisationParameters::PrintData() const
+{
+  G4cout << G4endl;
+  G4cout << "===== G4eIonisationParameters =====" << G4endl;
+  G4cout << G4endl;
+
+  size_t nZ = activeZ.size();
+  G4std::map<G4int,G4VEMDataSet*,G4std::less<G4int> >::const_iterator pos;
+
+  for (G4int i=0; i<nZ; i++) {
+    G4int Z = (G4int)activeZ[i];      
+
+    for (G4int j=0; j<length; j++) {
+    
+      G4int index = Z*20 + j;
+
+      pos = param.find(index);
+      if (pos!= param.end()) {
+        G4VEMDataSet* dataSet = pos->second;
+        size_t nShells = dataSet->NumberOfComponents();
+
+        for (G4int k=0; k<nShells; k++) {
+
+          G4cout << "===== Z= " << Z << " shell= " << k 
+                 << " parameter[" << j << "]  =====" 
+                 << G4endl;
+          const G4VEMDataSet* comp = dataSet->GetComponent(k);
+          comp->PrintData();
+	}
+      }
+    }
+  }
+  G4cout << "====================================" << G4endl;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
