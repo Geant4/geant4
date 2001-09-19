0a83f3d1d0f16d92fb82987b54160d2d220715a8
Elena Gianolio <elena@cern.ch>
2001-09-19 13:44:38 +0000

    added messenger to PrimaryGeneratorAction
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@8027 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/processes/electromagnetic/lowenergy/test/fluoTest/include/FluoTestPrimaryGeneratorMessenger.hh
@@ -0,0 +1,57 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+//
+//
+// $Id: FluoTestPrimaryGeneratorMessenger.hh,v 1.1 2001-09-19 13:44:38 elena Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+// 
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+#ifndef FluoTestPrimaryGeneratorMessenger_h
+#define FluoTestPrimaryGeneratorMessenger_h 1
+
+#include "G4UImessenger.hh"
+#include "globals.hh"
+
+class FluoTestPrimaryGeneratorAction;
+class G4UIcmdWithAString;
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+class FluoTestPrimaryGeneratorMessenger: public G4UImessenger
+{
+  public:
+    FluoTestPrimaryGeneratorMessenger(FluoTestPrimaryGeneratorAction*);
+   ~FluoTestPrimaryGeneratorMessenger();
+    
+    void SetNewValue(G4UIcommand*, G4String);
+    
+  private:
+    FluoTestPrimaryGeneratorAction* FluoTestAction; 
+    G4UIcmdWithAString*          RndmCmd;
+};
+
+#endif
+
---
+++ source/processes/electromagnetic/lowenergy/test/fluoTest/src/FluoTestPrimaryGeneratorMessenger.cc
@@ -0,0 +1,67 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+//
+//
+// $Id: FluoTestPrimaryGeneratorMessenger.cc,v 1.1 2001-09-19 13:44:02 elena Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+// 
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+#include "FluoTestPrimaryGeneratorMessenger.hh"
+
+#include "FluoTestPrimaryGeneratorAction.hh"
+#include "G4UIcmdWithAString.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+FluoTestPrimaryGeneratorMessenger::FluoTestPrimaryGeneratorMessenger(FluoTestPrimaryGeneratorAction* FluoTestGun)
+:FluoTestAction(FluoTestGun)
+{ 
+  RndmCmd = new G4UIcmdWithAString("/gun/random",this);
+  RndmCmd->SetGuidance("Shoot randomly the incident particle.");
+  RndmCmd->SetGuidance("  Choice : on(default), off");
+  RndmCmd->SetParameterName("choice",true);
+  RndmCmd->SetDefaultValue("on");
+  RndmCmd->SetCandidates("on off");
+  RndmCmd->AvailableForStates(PreInit,Idle);
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+FluoTestPrimaryGeneratorMessenger::~FluoTestPrimaryGeneratorMessenger()
+{
+  delete RndmCmd;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+void FluoTestPrimaryGeneratorMessenger::SetNewValue(G4UIcommand * command,G4String newValue)
+{ 
+  if( command == RndmCmd )
+   { FluoTestAction->SetRndmFlag(newValue);}
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
