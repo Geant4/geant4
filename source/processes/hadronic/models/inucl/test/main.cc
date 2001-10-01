91f5ae8a02203de3922525fa490b69e7b2f6df5b
Aatos Heikkinen <miheikki@cern.ch>
2001-10-01 13:07:02 +0000

    adding G4 style main test for inucl++
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@8152 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/processes/hadronic/models/inucl/test/main.cc
@@ -0,0 +1,133 @@
+//#define CHECK_MOMC
+
+#include "G4Collider.hh"
+#include "G4InuclCollider.hh"
+#include "G4IntraNucleiCascader.hh"
+#include "G4NonEquilibriumEvaporator.hh"
+#include "G4EquilibriumEvaporator.hh"
+#include "G4Fissioner.hh"
+#include "G4BigBanger.hh"
+#include "G4ElementaryParticleCollider.hh"
+#include "G4InuclParticle.hh"
+#include "G4InuclElementaryParticle.hh"
+#include "G4InuclNuclei.hh"
+#include "G4CollisionOutput.hh"
+#include "G4Analyser.hh"
+#include "G4WatcherGun.hh"
+
+#include "vector"
+
+int main() {
+// test program to run inucl stand alone
+
+const G4int to_report = 100;
+
+G4int nrain = 100000; // number of interactions to be generated
+
+// auxiliarly stuff for ugly analysis
+G4Analyser* analyser = new Analyser;
+G4WatcherGun* gun = new WatcherGun;
+gun->setWatchers();
+analyser->setWatchers(gun->getWatchers());
+analyser->setInelCsec(1760.0, true);
+
+//  colliders initialisation
+G4ElementaryParticleCollider* colep = new G4ElementaryParticleCollider;
+G4IntraNucleiCascader* incas = new G4IntraNucleiCascader;
+G4NonEquilibriumEvaporator* noneq = new G4NonEquilibriumEvaporator;
+G4EquilibriumEvaporator* eqil = new G4EquilibriumEvaporator;
+G4Fissioner* fiss = new G4Fissioner;
+G4BigBanger* bigb = new G4BigBanger;
+G4InuclCollider* inucl = new G4InuclCollider(colep, incas, noneq, eqil, fiss, bigb);
+
+// Bullet / Target initialisation
+
+// Bullet could be nucleon or pion or nuclei
+
+// 0.8 GeV proton with momentum along Z axis
+G4InuclParticle* bull = new G4InuclElementaryParticle(0.8, 1); 
+
+/*
+// neutron  with the momentum defined by the vector momb(4)
+vector<G4double> momb(4);
+momb[1] = 0.0; 
+momb[2] = 0.0; 
+momb[3] = 2.;
+G4InuclParticle* bull = new G4InuclElementaryParticle(mom, 2);
+*/
+
+/*
+// He4 nuclei with momentum momb
+G4InuclParticle* bull = new G4InuclNuclei(momb, 4.0, 2.0);
+((G4InuclNuclei*)bull)->setEnergy(); // for nuclei mom[4] has to be set specially !!! :::
+*/
+ 
+// Target could be nucleon or nuclei (more precise, in case when
+// particle-particle interaction will be generated, at least one particle
+// has to be nucleon)
+
+// Au197 target at rest
+G4InuclParticle* targ = new G4InuclNuclei(0.0, 197.0, 79.0);
+
+/*
+// Neutron with momentum momta
+vector<G4double> momta(4);
+momta[1] = -0.3; 
+momta[2] = 0.2; 
+momta[3] = -0.2; 
+G4InuclParticle* targ = new G4InuclElementaryParticle(momta, 2);
+*/
+
+/*
+// C12 nuclei with momentum momta
+G4InuclParticle* targ = new G4InuclNuclei(momta, 12.0, 6.0);
+((G4InuclNuclei*)targ)->setEnergy();
+*/
+
+#ifdef CHECK_MOMC
+vector<G4double> total_mom_in = bull->getMomentum();
+vector<G4double> momt = targ->getMomentum();
+for(G4int i = 0; i < 4; i++) total_mom_in[i] += momt[i];
+vector<G4double> total_mom_out;
+bull->printParticle();
+targ->printParticle();
+G4cout << " tot in mom: px " << total_mom_in[1] << " py " << total_mom_in[2]
+ << " pz " << total_mom_in[3] << " e " << total_mom_in[0] << G4endl;
+#endif
+
+for(G4int i = 0; i < nrain; i++) {
+  if((i + 1) % to_report == 0)  
+    G4cout << " ---- EVENT ------ " << i+1 << G4endl;
+    G4CollisionOutput output = inucl->collide(bull, targ); // standard method
+//  auxiliarly method to get more information about the different stages 
+//  of interaction  
+//  G4CollisionOutput output = inucl->collideAndTest(bull, targ, analyser);
+//
+//  G4cout << " ++++++++++++++++++++++++++++++++++++++++++++++++++ " << G4endl;
+//  G4cout << " Global output " << G4endl;
+//  output.printCollisionOutput();
+//
+//
+#ifdef CHECK_MOMC
+  total_mom_out = output.getTotalOutputMomentum();
+  G4cout << " 4 - momentum conservation check " << G4endl
+    << " dE " << total_mom_out[0] - total_mom_in[0] << 
+     " dPx " << total_mom_out[1] - total_mom_in[1] <<
+     " dPy " << total_mom_out[2] - total_mom_in[2] <<
+     " dPz " << total_mom_out[3] - total_mom_in[3] << G4endl;
+#endif
+  analyser->analyse(output);
+};
+analyser->printResults();
+
+return 0;
+        
+}   
+
+
+
+
+
+
+
+
