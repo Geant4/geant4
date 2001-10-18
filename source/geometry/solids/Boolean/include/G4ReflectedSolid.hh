28096b87debc69dc6ddc54d497121e94cf3b0987
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2001-10-18 10:07:50 +0000

    Bug fix in CreatePolyhedron()
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@8400 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/geometry/solids/Boolean/include/G4ReflectedSolid.hh
+++ source/geometry/solids/Boolean/include/G4ReflectedSolid.hh
@@ -1,11 +1,27 @@
-// This code implementation is the intellectual property of
-// the GEANT4 collaboration.
 //
-// By copying, distributing or modifying the Program (or any work
-// based on the Program) you indicate your acceptance of this statement,
-// and all its terms.
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
 //
-// $Id: G4ReflectedSolid.hh,v 1.1 2001-07-23 16:24:13 grichine Exp $
+//
+// $Id: G4ReflectedSolid.hh,v 1.2 2001-10-18 10:07:49 gcosmo Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 //
@@ -14,13 +30,11 @@
 // Class description:
 //
 // A Reflected solid is a solid that has been shifted from its original
-// frame of reference to a new one. It is meant to be used only for
-// simplifying the implementation of "Boolean solids". 
+// frame of reference to a new one.
 
 // History:
 //
 // 23.07.01 V.Grichine, creation according John/Gabriele recommendations
-
 
 #ifndef G4ReflectedSolid_HH
 #define G4ReflectedSolid_HH
@@ -38,22 +52,14 @@ class G4ReflectedSolid : public G4VSolid
     G4ReflectedSolid( const G4String& pName,
                             G4VSolid* pSolid ,
                       const G4Transform3D& transform  ) ;
-
-  //  G4ReflectedSolid( const G4String& pName,
-  //                          G4VSolid* pSolid ,
-  //                          G4RotationMatrix* rotMatrix,
-  //                    const G4ThreeVector& transVector  ) ;
-
-  //   G4ReflectedSolid( const G4String& pName,
-  //                           G4VSolid* pSolid ,
-  //	      const G4AffineTransform directTransform );
-      // For use in instantiating a transient instance from a persistent one.
+      // For use in instantiating a transient instance.
 
     virtual ~G4ReflectedSolid() ;
+      // Virtual destructor.
 
   public:  // without description 
 
-    // It also has all the methods that a solid requires, eg.
+    // Includes all the methods that a solid requires.
 
     EInside Inside( const G4ThreeVector& p ) const ; 
 
@@ -86,45 +92,37 @@ class G4ReflectedSolid : public G4VSolid
 
     virtual G4GeometryType  GetEntityType() const;
 
-    virtual const G4ReflectedSolid* GetReflectedSolidPtr() const   ;
+    virtual const G4ReflectedSolid* GetReflectedSolidPtr() const;
     virtual       G4ReflectedSolid* GetReflectedSolidPtr();
-
       // If the Solid is a "G4ReflectedSolid",
       // return a self pointer else return 0.
 
     G4VSolid*                GetConstituentMovedSolid() const;
-
     G4AffineTransform        GetTransform() const; 
-    void       SetTransform(G4AffineTransform& ); 
-
+    void       SetTransform(G4AffineTransform&);
     G4AffineTransform        GetDirectTransform() const; 
     void       SetDirectTransform(G4AffineTransform&);
- 
-    // Access/Set methods.
+      // Accessors methods.
 
     G4RotationMatrix         GetFrameRotation() const;
     void  SetFrameRotation(const G4RotationMatrix&);
-
     G4ThreeVector            GetFrameTranslation() const; 
     void  SetFrameTranslation(const G4ThreeVector&); 
-
-      // Get/Set the rotation/translation, as applied to the frame of reference.
+      // Get/Set the rotation/translation, as applied to the
+      // frame of reference.
 
     G4RotationMatrix         GetObjectRotation() const;
     void  SetObjectRotation(const G4RotationMatrix&);
-
     G4ThreeVector            GetObjectTranslation() const; 
     void  SetObjectTranslation(const G4ThreeVector&); 
-
-    // Get/Set the rotation/translation, as applied to the object.
+      // Get/Set the rotation/translation, as applied to the object.
 
   public:  // without description
 
     void DescribeYourselfTo ( G4VGraphicsScene& scene ) const ;
     G4Polyhedron* CreatePolyhedron () const ;
     G4NURBS*      CreateNURBS      () const ;
-
-    // For creating graphical representations (ie for visualisation).
+      // For creating graphical representations (ie for visualisation).
 
   protected:
 
@@ -139,12 +137,7 @@ class G4ReflectedSolid : public G4VSolid
 
     G4ReflectedSolid(const G4ReflectedSolid&);
     G4ReflectedSolid& operator=(const G4ReflectedSolid&);
-
       // Private copy constructor and assignment operator.
 } ;
 
 #endif
-
-
-
-
--- source/geometry/solids/Boolean/src/G4ReflectedSolid.cc
+++ source/geometry/solids/Boolean/src/G4ReflectedSolid.cc
@@ -1,24 +1,37 @@
-// This code implementation is the intellectual property of
-// the GEANT4 collaboration.
 //
-// By copying, distributing or modifying the Program (or any work
-// based on the Program) you indicate your acceptance of this statement,
-// and all its terms.
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
 //
-// $Id: G4ReflectedSolid.cc,v 1.2 2001-09-17 14:15:59 grichine Exp $
+//
+// $Id: G4ReflectedSolid.cc,v 1.3 2001-10-18 10:07:50 gcosmo Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // Implementation for G4ReflectedSolid class for boolean 
 // operations between other solids
 //
-// History:
-//
-// 23.07.01 V.Grichine, creation according John/Gabriele recommendations
+// Author: Vladimir Grichine, 23.07.01  (Vladimir.Grichine@cern.ch)
 
 #include "G4ReflectedSolid.hh"
 #include "G4Point3D.hh"
 #include "G4Normal3D.hh"
-
 
 #include "G4VoxelLimits.hh"
 #include "G4AffineTransform.hh"
@@ -356,8 +369,8 @@ G4Polyhedron*
 G4ReflectedSolid::CreatePolyhedron () const 
 {
   G4Polyhedron* polyhedron = fPtrSolid->CreatePolyhedron();
-  polyhedron->Transform
-    (G4Transform3D(GetObjectRotation(),GetObjectTranslation()));
+  polyhedron->Transform(*fDirectTransform3D);
+
   return polyhedron;
 }
 
