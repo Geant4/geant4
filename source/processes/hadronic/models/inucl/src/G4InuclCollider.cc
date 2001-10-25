0523348d371187aaf96fcb5d8dbade46227276cd
Aatos Heikkinen <miheikki@cern.ch>
2001-10-25 14:15:10 +0000

    adding G4 naming style
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@8617 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/models/inucl/src/G4InuclCollider.cc
+++ source/processes/hadronic/models/inucl/src/G4InuclCollider.cc
@@ -1,58 +1,58 @@
 //#define DEBUG
 
-#include "InuclCollider.h"
-#include "InuclElementaryParticle.h"
-#include "LorentzConvertor.h"
-#include "ParticleLargerEkin.h"
+#include "G4InuclCollider.hh"
+#include "G4InuclElementaryParticle.hh"
+#include "G4LorentzConvertor.hh"
+#include "G4ParticleLargerEkin.hh"
 #include "algorithm"
 
-typedef vector<InuclElementaryParticle>::iterator particleIterator;
-typedef vector<InuclNuclei>::iterator nucleiIterator;
+typedef vector<G4InuclElementaryParticle>::iterator particleIterator;
+typedef vector<G4InuclNuclei>::iterator nucleiIterator;
 	 
-CollisionOutput InuclCollider::collide(InuclParticle* bullet,
-                     InuclParticle* target) {
+G4CollisionOutput G4InuclCollider::collide(G4InuclParticle* bullet,
+                     G4InuclParticle* target) {
 
-  const int itry_max = 1000;
+  const G4int itry_max = 1000;
   		     
-  CollisionOutput globalOutput;
-  InuclElementaryParticle* particle1 =
-               dynamic_cast<InuclElementaryParticle*>(bullet);
-  InuclElementaryParticle* particle2 =
-               dynamic_cast<InuclElementaryParticle*>(target);
+  G4CollisionOutput globalOutput;
+  G4InuclElementaryParticle* particle1 =
+               dynamic_cast<G4InuclElementaryParticle*>(bullet);
+  G4InuclElementaryParticle* particle2 =
+               dynamic_cast<G4InuclElementaryParticle*>(target);
   
   if(particle1 && particle2) { // particle + particle 
 #ifdef DEBUG
     particle1->printParticle();
     particle2->printParticle();
 #endif  
-    globalOutput = theElementaryParticleCollider->collide(bullet,target);
+    globalOutput = theElementaryParticleCollider->collide(bullet, target);
 
   }
    else { // needs to call all machinery    	
 
-     LorentzConvertor convertToTargetRestFrame;
+     G4LorentzConvertor convertToTargetRestFrame;
 
-     InteractionCase interCase = bulletTargetSetter(bullet,target);
-     int intcase = interCase.getInterCase();
+     G4InteractionCase interCase = bulletTargetSetter(bullet, target);
+     G4int intcase = interCase.getInterCase();
      
      if(intcase > 0) { // ok
-       InuclNuclei* ntarget =
-                   dynamic_cast<InuclNuclei*>(interCase.getTarget());
+       G4InuclNuclei* ntarget =
+                   dynamic_cast<G4InuclNuclei*>(interCase.getTarget());
        convertToTargetRestFrame.setTarget(ntarget->getMomentum(),
 	                                           ntarget->getMass());
-       int btype;
-       double ab;
-       double zb;
-       double at = ntarget->getA();
-       double zt = ntarget->getZ();
+       G4int btype;
+       G4double ab;
+       G4double zb;
+       G4double at = ntarget->getA();
+       G4double zt = ntarget->getZ();
        
        if(intcase == 1) { // particle with nuclei
-         InuclElementaryParticle* pbullet = 
-	   dynamic_cast<InuclElementaryParticle*>(interCase.getBullet());
+         G4InuclElementaryParticle* pbullet = 
+	   dynamic_cast<G4InuclElementaryParticle*>(interCase.getBullet());
          
 	 if(pbullet->photon()) {
-	   cout << " InuclCollider -> can not collide with photon " << endl;
-	   globalOutput.trivialise(bullet,target);
+	   G4cout << " InuclCollider -> can not collide with photon " << G4endl;
+	   globalOutput.trivialise(bullet, target);
 	   return globalOutput;
 	 }
 	  else {
@@ -62,7 +62,7 @@ CollisionOutput InuclCollider::collide(InuclParticle* bullet,
 	 }; 
        }
         else { // nuclei with nuclei
-         InuclNuclei* nbullet = 
+         G4InuclNuclei* nbullet = 
 	       dynamic_cast<InuclNuclei*>(interCase.getBullet());
          convertToTargetRestFrame.setBullet(nbullet->getMomentum(),
 	                           nbullet->getMass());   
@@ -70,85 +70,85 @@ CollisionOutput InuclCollider::collide(InuclParticle* bullet,
 	 zb = nbullet->getZ();
        };
        	
-       double ekin = convertToTargetRestFrame.getKinEnergyInTheTRS();
+       G4double ekin = convertToTargetRestFrame.getKinEnergyInTheTRS();
 #ifdef DEBUG
-       cout << " ekin in trs " << ekin << endl;
+       G4cout << " ekin in trs " << ekin << G4endl;
 #endif
-       if(inelasticInteractionPossible(bullet,target,ekin)) {
+       if(inelasticInteractionPossible(bullet, target, ekin)) {
          convertToTargetRestFrame.toTheTargetRestFrame();
 #ifdef DEBUG
-	 cout << " degenerated? " << convertToTargetRestFrame.trivial() << endl;
+	 G4cout << " degenerated? " << convertToTargetRestFrame.trivial() << G4endl;
 #endif
-	 vector<double> bmom(4,0.);
+	 vector<G4double> bmom(4, 0.0);
 	 bmom[3] = convertToTargetRestFrame.getTRSMomentum();
-	 InuclNuclei ntarget(at,zt);
-	 vector<double> tmom(4,0.);
+	 InuclNuclei ntarget(at, zt);
+	 vector<G4double> tmom(4, 0.0);
 	 ntarget.setMomentum(tmom);
 	 ntarget.setEnergy();
          theIntraNucleiCascader->setInteractionCase(intcase);
 	 
-         bool bad = true;
-	 int itry = 0;
+         G4bool bad = true;
+	 G4int itry = 0;
 	 
 	 while (bad && itry < itry_max) {
-           CollisionOutput TRFoutput;
-           CollisionOutput output;
+           G4CollisionOutput TRFoutput;
+           G4CollisionOutput output;
 	   itry++;
 	   if(intcase == 1) {
-	     InuclElementaryParticle pbullet(bmom,btype);
-	     output = theIntraNucleiCascader->collide(&pbullet,&ntarget);
+	     G4InuclElementaryParticle pbullet(bmom, btype);
+	     output = theIntraNucleiCascader->collide(&pbullet, &ntarget);
 	   }
 	    else {
-	     InuclNuclei nbullet(ab,zb);
+	     G4InuclNuclei nbullet(ab, zb);
 	     nbullet.setMomentum(bmom);
 	     nbullet.setEnergy();
-	     output = theIntraNucleiCascader->collide(&nbullet,&ntarget);
+	     output = theIntraNucleiCascader->collide(&nbullet, &ntarget);
 	   };   
 
 #ifdef DEBUG
-           cout << " After Cascade " << endl;
+           G4cout << " After Cascade " << G4endl;
            output.printCollisionOutput();
-           cout << " ++++++++++++++++++++++++++++++++++++++++++++++++++ " << endl;
+           G4cout << " ++++++++++++++++++++++++++++++++++++++++++++++++++ " <<G4 endl;
 #endif
 //             the rest, if any
            TRFoutput.addOutgoingParticles(output.getOutgoingParticles());
 	   if(output.numberOfNucleiFragments() == 1) { // there is smth. after
                
-	     InuclNuclei cascad_rec_nuclei = output.getNucleiFragments()[0];
+	     G4InuclNuclei cascad_rec_nuclei = output.getNucleiFragments()[0];
 
              if(explosion(&cascad_rec_nuclei)) {
-               cout << " big bang after cascade " << endl;
+               G4cout << " big bang after cascade " << G4endl;
 	       output = theBigBanger->collide(0,&cascad_rec_nuclei);
                TRFoutput.addOutgoingParticles(output.getOutgoingParticles());
 	     }
 	      else {
-               output = theNonEquilibriumEvaporator->collide(0,&cascad_rec_nuclei);
+               output = theNonEquilibriumEvaporator->collide(0, &cascad_rec_nuclei);
 #ifdef DEBUG
-              cout << " After NonEquilibriumEvaporator " << endl;
+              G4cout << " After NonEquilibriumEvaporator " << G4endl;
               output.printCollisionOutput();
-              cout << " ++++++++++++++++++++++++++++++++++++++++++++++++++ " << endl;
+              G4cout << " ++++++++++++++++++++++++++++++++++++++++++++++++++ " << G4endl;
 #endif
                TRFoutput.addOutgoingParticles(output.getOutgoingParticles());
 
-               InuclNuclei exiton_rec_nuclei = output.getNucleiFragments()[0];
-               output = theEquilibriumEvaporator->collide(0,&exiton_rec_nuclei);
+               G4InuclNuclei exiton_rec_nuclei = output.getNucleiFragments()[0];
+               output = theEquilibriumEvaporator->collide(0, &exiton_rec_nuclei);
 #ifdef DEBUG
-               cout << " After EquilibriumEvaporator " << endl;
+               G4cout << " After EquilibriumEvaporator " << G4endl;
                output.printCollisionOutput();
-               cout << " ++++++++++++++++++++++++++++++++++++++++++++++++++ " << endl;
+               G4cout << " ++++++++++++++++++++++++++++++++++++++++++++++++++ " << G4endl;
 #endif
                TRFoutput.addOutgoingParticles(output.getOutgoingParticles());  
                TRFoutput.addTargetFragments(output.getNucleiFragments());         
 	     };
 	   };
 //             convert to the LAB       
-           bool withReflection = convertToTargetRestFrame.reflectionNeeded();       
-           vector<InuclElementaryParticle> particles = 
+           G4bool withReflection = convertToTargetRestFrame.reflectionNeeded();       
+           vector<G4InuclElementaryParticle> particles = 
 	             TRFoutput.getOutgoingParticles();
 	   if(!particles.empty()) { 
 	     particleIterator ipart;
 	     for(ipart = particles.begin(); ipart != particles.end(); ipart++) {
-	       vector<double> mom = ipart->getMomentum();
+	       vector<G4double> mom = ipart->getMomentum();
 	       if(withReflection) mom[3] = -mom[3];
 	       mom = convertToTargetRestFrame.rotate(mom);
 	       ipart->setMomentum(mom); 
@@ -158,12 +158,12 @@ CollisionOutput InuclCollider::collide(InuclParticle* bullet,
 	     sort(particles.begin(), particles.end(), ParticleLargerEkin());
            };
            
-	   vector<InuclNuclei> nucleus = 
+	   vector<G4InuclNuclei> nucleus = 
 	             TRFoutput.getNucleiFragments();
 	   if(!nucleus.empty()) { 
 	     nucleiIterator inuc;
 	     for(inuc = nucleus.begin(); inuc != nucleus.end(); inuc++) {
-	       vector<double> mom = inuc->getMomentum(); 
+	       vector<G4double> mom = inuc->getMomentum(); 
 	       if(withReflection) mom[3] = -mom[3];
 	       mom = convertToTargetRestFrame.rotate(mom);
 	       inuc->setMomentum(mom);
@@ -175,7 +175,7 @@ CollisionOutput InuclCollider::collide(InuclParticle* bullet,
            };
            globalOutput.addOutgoingParticles(particles);
 	   globalOutput.addTargetFragments(nucleus);
-	   globalOutput.setOnShell(bullet,target);
+	   globalOutput.setOnShell(bullet, target);
 	   if(globalOutput.acceptable()) {
 	     return globalOutput;
            }
@@ -184,23 +184,23 @@ CollisionOutput InuclCollider::collide(InuclParticle* bullet,
 	   }; 
 	 };
 #ifdef DEBUG
-	 cout << " InuclCollider -> can not generate acceptable inter. after " 
-	      << itry_max << " attempts " << endl;
+	 G4cout << " InuclCollider -> can not generate acceptable inter. after " 
+	      << itry_max << " attempts " << G4endl;
 #endif
-	 globalOutput.trivialise(bullet,target);
+	 globalOutput.trivialise(bullet, target);
 	 return globalOutput;        
        }
         else {
 #ifdef DEBUG
-	 cout << " InuclCollider -> inelastic interaction is impossible " <<
-	   endl << " due to the coulomb barirer " << endl;
+	 G4cout << " InuclCollider -> inelastic interaction is impossible " <<
+	   endl << " due to the coulomb barirer " << G4endl;
 #endif
 	 globalOutput.trivialise(bullet,target);
 	 return globalOutput;
        };	
      }
       else {
-       cout << " InuclCollider -> inter case " << intcase << endl;
+       G4cout << " InuclCollider -> inter case " << intcase << G4endl;
      };       
   };
 
@@ -209,38 +209,38 @@ return globalOutput;
 
 }
 		     
-bool InuclCollider::inelasticInteractionPossible(InuclParticle* bullet,
-          InuclParticle* target, double ekin) const {
+G4bool G4InuclCollider::inelasticInteractionPossible(G4InuclParticle* bullet,
+          G4InuclParticle* target, G4double ekin) const {
 
-const double coeff = 0.001*1.2;
-const double one_third = 1./3.;
+const G4double coeff = 0.001 * 1.2;
+const G4double one_third = 1.0 / 3.0;
 
-bool possible = true;
-double at;
-double zt;
-double ab;
-double zb;
+G4bool possible = true;
+G4double at;
+G4double zt;
+G4double ab;
+G4double zb;
 
-if(InuclNuclei* nuclei_target = dynamic_cast<InuclNuclei*>(target)) {
+if(G4InuclNuclei* nuclei_target = dynamic_cast<G4InuclNuclei*>(target)) {
   at = nuclei_target->getA();
   zt = nuclei_target->getZ(); 
-  if(InuclNuclei* nuclei_bullet = dynamic_cast<InuclNuclei*>(bullet)) {
+  if(G4InuclNuclei* nuclei_bullet = dynamic_cast<G4InuclNuclei*>(bullet)) {
     ab = nuclei_bullet->getA();
     zb = nuclei_bullet->getZ();     
   }
    else {
-    InuclElementaryParticle* particle =
-               dynamic_cast<InuclElementaryParticle*>(bullet);
+    G4InuclElementaryParticle* particle =
+               dynamic_cast<G4InuclElementaryParticle*>(bullet);
     ab = 1;
     zb = particle->getCharge();
   }; 
 }
  else {
-  if(InuclNuclei* nuclei_bullet = dynamic_cast<InuclNuclei*>(bullet)) {
+  if(G4InuclNuclei* nuclei_bullet = dynamic_cast<G4InuclNuclei*>(bullet)) {
     ab = nuclei_bullet->getA();
     zb = nuclei_bullet->getZ();     
-    InuclElementaryParticle* particle =
-               dynamic_cast<InuclElementaryParticle*>(target);
+    G4InuclElementaryParticle* particle =
+               dynamic_cast<G4InuclElementaryParticle*>(target);
     at = 1;
     zt = particle->getCharge();    
   }
@@ -249,7 +249,7 @@ if(InuclNuclei* nuclei_target = dynamic_cast<InuclNuclei*>(target)) {
   };  
 }; 
 
-double VCOL = coeff*zt*zb/(pow(at,one_third) + pow(ab,one_third));
+G4double VCOL = coeff * zt * zb / (pow(at, one_third) + pow(ab, one_third));
 possible = VCOL < ekin;
 return possible;
 
