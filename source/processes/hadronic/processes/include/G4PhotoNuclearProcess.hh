dabf2d164983d76d368f617bd6fb55ec4f8aa304
Johannes-Peter Wellisch <hpw@cern.ch>
2001-10-26 13:24:03 +0000

    adding gamma nuclear process
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@8664 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/processes/hadronic/processes/include/G4PhotoNuclearProcess.hh
@@ -0,0 +1,56 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * authors in the GEANT4 collaboration.                             *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+//
+//
+//
+ // Hadronic Process: Ion Inelastic Process
+ // J.P. Wellisch, CERN, Apr. 14 2000
+ // Last modified: 03-Apr-1997
+
+#ifndef G4PhotoNuclearProcess_h
+#define G4PhotoNuclearProcess_h 1
+ 
+// Class Description
+// Process for Ion Inelastic scattering; 
+// to be used in your physics list in case you need this physics.
+// Class Description - End
+
+#include "G4HadronInelasticProcess.hh"
+#include "G4Gamma.hh"
+ 
+
+ class G4PhotoNuclearProcess : public G4HadronInelasticProcess
+ {
+ public:
+    
+    G4PhotoNuclearProcess(
+     const G4String& processName = "PhotonInelastic" ) :
+
+      G4HadronInelasticProcess( processName, G4Gamma:Gamma() )
+    { }
+        
+    ~G4PhotoNuclearProcess()
+    { }
+ };
+
+#endif
+
