3660733c9feb9690ae4e046140e3f17c4f12e12b
griccard <griccard@cern.ch>
2001-11-28 18:21:44 +0000

    Some changes in the README file and a new Python example script for analysis
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@9368 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ examples/advanced/gammaray_telescope/gammaraytel.py
@@ -0,0 +1,32 @@
+# tell HistoManger where to find the histograms:
+# change the name to access hbook files different from the first run
+hm.selectStore("gammaraytel0.hbook")
+
+# ... and load them into memory:
+hE    = hm.load1D(1)
+hPl   = hm.load1D(2)
+hXZ   = hm.load2D(3)
+hYZ   = hm.load2D(4)
+
+# set plotter to 3*2 zones
+pl.zone(2,2)
+# ... and plot the histograms
+hplot(hE)
+hplot(hPl)
+hplot(hXZ)
+hplot(hYZ)
+
+# get the primary ntuple from the NtupleManager
+nt1 = ntm.findNtuple("gammaraytel0.hbook::1" )
+
+# see which attributes there are:
+nt1.listAttributes()
+
+
+
+
+
+
+
+
+
