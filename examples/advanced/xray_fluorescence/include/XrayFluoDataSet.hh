a6ab89b30eeedeff95bc15919b949c292bbd1c18
Elena Gianolio <elena@cern.ch>
2001-11-28 16:06:00 +0000

    added files for data analysis
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@9341 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ examples/advanced/xray_fluorescence/include/XrayFluoDataSet.hh
@@ -0,0 +1,90 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+//
+//
+// $Id: XrayFluoDataSet.hh
+// GEANT4 tag $Name: xray_fluo-V03-02-00
+//
+// Author: Elena Guardincerri (Elena.Guardincerri@ge.infn.it)
+//
+// History:
+// -----------
+//  28 Nov 2001  Elena Guardincerri   Created
+//
+// -------------------------------------------------------------------
+
+//for the documentation of this class see also G4EMDataSet class
+
+#ifndef FluoDataSet_hh
+#define FluoDataSet_hh 1
+
+#include "globals.hh"
+#include "G4DataVector.hh"
+#include "G4VEMDataSet.hh"
+
+class G4VDataSetAlgorithm;
+
+class XrayFluoDataSet : public G4VEMDataSet {
+
+public:
+
+  XrayFluoDataSet(G4int Z,
+		  G4DataVector* points, 
+	      G4DataVector* values,
+	      const G4VDataSetAlgorithm* interpolation,
+	      G4double unitE = MeV, G4double unitData = barn);
+  
+  XrayFluoDataSet(G4int Z,
+		  const G4String& dataFile,
+	      const G4VDataSetAlgorithm* interpolation,
+	      G4double unitE = MeV, G4double unitData = barn);
+
+  ~XrayFluoDataSet();
+ 
+  //find the value corresponding to the energy e in the set
+  //identified by id
+  G4double FindValue(G4double e, G4int id = 0) const;
+  
+  void PrintData() const;
+
+  const G4DataVector& GetEnergies(G4int i) const { return *energies; }
+  const G4DataVector& GetData(G4int i) const { return *data; }
+
+private:
+
+  void LoadData(const G4String& dataFile);
+  G4int z;
+  G4int FindBinLocation(G4double energy) const;
+
+  G4DataVector* energies; // Owned pointer
+  G4DataVector* data;     // Owned pointer
+
+  const G4VDataSetAlgorithm* algorithm; // Not owned pointer 
+  
+  G4double unit1;
+  G4double unit2;
+
+  size_t numberOfBins;
+
+};
+ 
+#endif
---
+++ examples/advanced/xray_fluorescence/src/XrayFluoNormalization.cc
@@ -0,0 +1,88 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+//
+//
+// $Id: XrayFluoAnalysisMessenger.cc
+// GEANT4 tag $Name: xray_fluo-V03-02-00
+//
+// Author: Elena Guardincerri (Elena.Guardincerri@ge.infn.it)
+//
+// History:
+// -----------
+// 28 Nov 2001 Elena Guardincerri     Created
+//
+// -------------------------------------------------------------------
+
+#include "XrayFluoNormalization.hh"
+#include "G4VDataSetAlgorithm.hh"
+#include "G4LogLogInterpolation.hh"
+#include "XrayFluoDataSet.hh"
+
+XrayFluoNormalization::XrayFluoNormalization()
+
+{ }
+
+XrayFluoNormalization::~XrayFluoNormalization()
+
+{ }
+
+const XrayFluoDataSet* XrayFluoNormalization::Normalize(G4double minIntExtreme, G4double maxIntExtreme, G4int nBins, G4String fileName)
+{
+ 
+ G4VDataSetAlgorithm* interpolation = new G4LogLogInterpolation();
+ XrayFluoDataSet* dataSet = 
+   new XrayFluoDataSet(1,fileName,interpolation,keV,1);
+
+ G4double integral = Integrate(minIntExtreme, maxIntExtreme, nBins, dataSet);
+
+ G4VDataSetAlgorithm* interpolation2 = new G4LogLogInterpolation();
+
+ XrayFluoDataSet* finalDataSet = new XrayFluoDataSet(1,fileName,interpolation2,keV,1/(integral/keV));
+ return finalDataSet;
+}
+
+G4double XrayFluoNormalization::Integrate(G4double minIntExtreme, G4double maxIntExtreme, G4int nBins, XrayFluoDataSet* dataSet)
+{
+ G4double partialHeight = 0;
+ G4double id = 0;
+ G4double lenghtOfBins = (maxIntExtreme - minIntExtreme)/nBins;
+ 
+ for (G4int i = 0; i<nBins; i++)
+   {
+     G4double midPoint = minIntExtreme + i*lenghtOfBins+lenghtOfBins/2;
+    
+     G4double heightOfRectangle = dataSet->FindValue(midPoint,id);
+    
+     partialHeight += heightOfRectangle;
+   
+   }
+
+ G4double integral = lenghtOfBins * partialHeight;
+ 
+ delete dataSet;
+ dataSet = 0;
+ return integral;
+}
+
+
+
+
