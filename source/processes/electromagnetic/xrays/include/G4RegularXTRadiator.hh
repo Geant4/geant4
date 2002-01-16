171b1d076ab8b280cf536276375367972600ca4d
Vladimir Grichine <Vladimir.Grichine@cern.ch>
2002-01-16 16:57:16 +0000

    first version of regular XTR radiator process
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@9820 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/processes/electromagnetic/xrays/include/G4RegularXTRadiator.hh
@@ -0,0 +1,61 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+//
+//
+// $Id: G4RegularXTRadiator.hh,v 1.1 2002-01-16 16:57:16 grichine Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+// 
+///////////////////////////////////////////////////////////////////////////
+// 
+// Process describing a radiator of X-ray transition radiation.  
+// Thicknesses of plates and gas gaps are fixed.
+// We suppose that:
+// formation zone ~ mean thickness << absorption length
+// for each material and in the range 1-100 keV. This allows us to simplify
+// interference effects in radiator stack (GetStackFactor method).
+// 
+// 
+// History:
+// 16.01.02 V. Grichine, first version 
+//
+
+
+#ifndef G4RegularXTRadiator_h
+#define G4RegularXTRadiator_h 1
+
+#include "G4VXTRenergyLoss.hh"
+
+class G4RegularXTRadiator : public G4VXTRenergyLoss
+{
+public:
+
+   G4RegularXTRadiator (G4LogicalVolume *anEnvelope,G4double,G4double,G4int,
+                        const G4String & processName = "XTRegularRadiator");
+  ~G4RegularXTRadiator ();
+
+  // Pure virtual function from base class
+
+  G4double GetStackFactor( G4double energy, G4double gamma, G4double varAngle);
+};
+
+#endif
---
+++ source/processes/electromagnetic/xrays/src/G4RegularXTRadiator.cc
@@ -0,0 +1,122 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+//
+//
+// $Id: G4RegularXTRadiator.cc,v 1.1 2002-01-16 16:57:16 grichine Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+
+#include "g4std/complex"
+
+#include "G4RegularXTRadiator.hh"
+#include "Randomize.hh"
+
+#include "G4Gamma.hh"
+
+////////////////////////////////////////////////////////////////////////////
+//
+// Constructor, destructor
+
+G4RegularXTRadiator::G4RegularXTRadiator(G4LogicalVolume *anEnvelope, 
+                                         G4double a, G4double b, G4int n,
+                                         const G4String& processName) :
+  G4VXTRenergyLoss(anEnvelope,a,b,n,processName)
+{
+  G4cout<<"Regular X-ray TR dE/dx radiator model is called"<<G4endl ;
+
+  // Build energy and angular integral spectra of X-ray TR photons from
+  // a radiator
+
+  BuildTable() ;
+}
+
+///////////////////////////////////////////////////////////////////////////
+
+G4RegularXTRadiator::~G4RegularXTRadiator()
+{
+  ;
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////
+//
+// Approximation for radiator interference factor for the case of
+// fully Regular radiator. The plate and gas gap thicknesses are fixed .
+// The mean values of the plate and gas gap thicknesses 
+// are supposed to be about XTR formation zones but much less than 
+// mean absorption length of XTR photons in coresponding material.
+
+G4double 
+G4RegularXTRadiator::GetStackFactor( G4double energy, 
+                                         G4double gamma, G4double varAngle )
+{
+  G4double result, Qa, Qb, Q, aZa, bZb, aMa, bMb, D ;
+  
+  aZa = fPlateThick/GetPlateFormationZone(energy,gamma,varAngle) ;
+  bZb = fGasThick/GetGasFormationZone(energy,gamma,varAngle) ;
+
+  aMa = fPlateThick*GetPlateLinearPhotoAbs(energy) ;
+  bMb = fGasThick*GetGasLinearPhotoAbs(energy) ;
+
+  Qa = exp(-aMa) ;
+  Qb = exp(-bMb) ;
+  Q  = Qa*Qb ;
+
+  G4complex Ha( exp(-0.5*aMa)*cos(aZa),
+               -exp(-0.5*aMa)*sin(aZa)   ) ; 
+ 
+  G4complex Hb( exp(-0.5*bMb)*cos(bZb),
+               -exp(-0.5*bMb)*sin(bZb)    ) ;
+
+  G4complex H  = Ha*Hb ;
+
+  G4complex Hs = G4std::conj(H) ;
+
+  D            = 1.0 /( (1 - sqrt(Q))*(1 - sqrt(Q)) + 
+                  4*sqrt(Q)*sin(0.5*(aZa+bZb))*sin(0.5*(aZa+bZb)) ) ;
+
+  G4complex F1 = (1.0 - Ha)*(1.0 - Hb)*(1.0 - Hs)
+                 * G4double(fPlateNumber)*D ;
+
+  G4complex F2 = (1.0-Ha)*(1.0-Ha)*Hb*(1.0-Hs)*(1.0-Hs)
+                 * (1.0 - G4std::pow(H,fPlateNumber)) * D*D ;
+
+  G4complex R  = (F1 + F2)*OneInterfaceXTRdEdx(energy,gamma,varAngle) ;
+
+  result       = 2.0*G4std::real(R) ;
+ 
+  return      result ;
+}
+
+
+//
+//
+////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
