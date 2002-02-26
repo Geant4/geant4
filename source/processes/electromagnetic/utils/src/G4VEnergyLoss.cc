e8acca2dc0f35d903e88bdf999a160fa6f6ce642
Michel Maire <Michel.Maire@cern.ch>
2002-02-26 12:21:51 +0000

    finalRange=1mm
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@10017 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/electromagnetic/utils/src/G4VEnergyLoss.cc
+++ source/processes/electromagnetic/utils/src/G4VEnergyLoss.cc
@@ -21,7 +21,7 @@
 // ********************************************************************
 //
 //
-// $Id: G4VEnergyLoss.cc,v 1.31 2002-02-14 10:53:30 gcosmo Exp $
+// $Id: G4VEnergyLoss.cc,v 1.32 2002-02-26 12:21:51 maire Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 
@@ -38,6 +38,7 @@
 //  08.11.01 some static methods,data members are not static L.Urban
 //  11.02.02 subSecFlag = false --> No sucutoff generation (mma) 
 //  14.02.02 initial value of data member finalRange has been changed L.Urban
+//  26.02.02 initial value of data member finalRange = 1 mm (mma)
 // 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
@@ -57,8 +58,7 @@ G4bool*      G4VEnergyLoss::LowerLimitForced        = 0;
 G4bool	     G4VEnergyLoss::setMinDeltaCutInRange = false;
 
 G4double     G4VEnergyLoss::dRoverRange    = 20*perCent;
-// G4double     G4VEnergyLoss::finalRange     = 200*micrometer;
-G4double     G4VEnergyLoss::finalRange     = 1000*mm;
+G4double     G4VEnergyLoss::finalRange     = 1*mm;
 G4double     G4VEnergyLoss::c1lim = dRoverRange;
 G4double     G4VEnergyLoss::c2lim = 2.*(1.-dRoverRange)*finalRange;
 G4double     G4VEnergyLoss::c3lim = -(1.-dRoverRange)*finalRange*finalRange;
