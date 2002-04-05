3ce33f8efe95d6335f17179417890c45400bb6b5
Micheal Dressel <dressel@cern.ch>
2002-04-05 09:25:41 +0000

    G4PMapPtkTallys.cc update
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@10230 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/geometry/biasing/src/G4PMapPtkTallys.cc
+++ source/geometry/biasing/src/G4PMapPtkTallys.cc
@@ -3,7 +3,7 @@
 #include "G4Sigma.hh"
 
 
-ostream& operator<<(ostream &out, const G4PMapNameTally &tallys){
+G4std::ostream& operator<<(G4std::ostream &out, const G4PMapNameTally &tallys){
   for (G4PMapNameTally::const_iterator it = tallys.begin();
        it != tallys.end(); it++) {
     out << it->first << "\n";
@@ -11,7 +11,7 @@ ostream& operator<<(ostream &out, const G4PMapNameTally &tallys){
   }
   return out;
 }
-ostream& operator<<(ostream &out, const G4PMapPtkTallys &tallystore){
+G4std::ostream& operator<<(G4std::ostream &out, const G4PMapPtkTallys &tallystore){
   for (G4PMapPtkTallys::const_iterator it = tallystore.begin();
        it != tallystore.end(); it++) {
     out << it->first << "\n";
