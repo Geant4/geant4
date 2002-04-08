c1e233704c0d7099cb8f01c46180f3010ee33cdb
Micheal Dressel <dressel@cern.ch>
2002-04-08 07:49:54 +0000

    change ->first to (*).first for HP
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@10238 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/geometry/biasing/src/G4PMapPtkTallys.cc
+++ source/geometry/biasing/src/G4PMapPtkTallys.cc
@@ -6,16 +6,16 @@
 G4std::ostream& operator<<(G4std::ostream &out, const G4PMapNameTally &tallys){
   for (G4PMapNameTally::const_iterator it = tallys.begin();
        it != tallys.end(); it++) {
-    out << it->first << "\n";
-    out << it->second;
+    out << (*it).first << "\n";
+    out << (*it).second;
   }
   return out;
 }
 G4std::ostream& operator<<(G4std::ostream &out, const G4PMapPtkTallys &tallystore){
   for (G4PMapPtkTallys::const_iterator it = tallystore.begin();
        it != tallystore.end(); it++) {
-    out << it->first << "\n";
-    out << it->second;
+    out << (*it).first << "\n";
+    out << (*it).second;
   }
   return out;
 }
