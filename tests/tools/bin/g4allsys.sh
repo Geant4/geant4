53eead6e0f1e77a9b294716b86b7e06726cf7384
Geant4 Testing <geant4.testing@cern.ch>
2002-04-18 16:28:35 +0000

    Ssh instead of rsh.
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@10309 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- tests/tools/bin/g4allsys.sh
+++ tests/tools/bin/g4allsys.sh
@@ -10,7 +10,8 @@ do
 
     g4sbr=/afs/cern.ch/sw/geant4/stt/$devprod/testtools/geant4/tests/tools/bin/g4sbr.sh
 
-    command="rsh -l $account $host $g4sbr $devprod $debopt $tag $action $actarg1 $actarg2 $actarg3 $nonincremental"
+#    command="rsh -l $account $host $g4sbr $devprod $debopt $tag $action $actarg1 $actarg2 $actarg3 $nonincremental"
+    command="ssh -l $account $host $g4sbr $devprod $debopt $tag $action $actarg1 $actarg2 $actarg3 $nonincremental"
     if [ X$host = X ]
     then
         exit # end of requests on command line
@@ -27,6 +28,8 @@ do
 	echo $PATH >>  $host.$devprod.$debopt.log 2>&1 
 	echo "______________________" >>  $host.$devprod.$debopt.log 2>&1 
 	echo $command >>  $host.$devprod.$debopt.log 2>&1 
+# Sequential run!
 	$command >> $host.$devprod.$debopt.log 2>&1 &
+#	$command >> $host.$devprod.$debopt.log 2>&1 
     fi	
 done
