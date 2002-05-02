3adccaf86de4e064defa0c97bb44082ff8d36ff0
Michel Maire <Michel.Maire@cern.ch>
2002-05-02 14:18:51 +0000

    visualisation
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@10421 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ examples/novice/N04/include/ExN04SteppingVerbose.hh
@@ -0,0 +1,57 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+//
+//
+// $Id: ExN04SteppingVerbose.hh,v 1.1 2002-05-02 14:18:51 maire Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+//   This class manages the verbose outputs in G4SteppingManager. 
+//   It inherits from G4SteppingVerbose.
+//   It shows how to extract informations during the tracking of a particle.
+//
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+class ExN04SteppingVerbose;
+
+#ifndef ExN04SteppingVerbose_h
+#define ExN04SteppingVerbose_h 1
+
+#include "G4SteppingVerbose.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+class ExN04SteppingVerbose : public G4SteppingVerbose 
+{
+ public:
+   
+  ExN04SteppingVerbose();
+ ~ExN04SteppingVerbose();
+
+  void StepInfo();
+  void TrackingStarted();
+
+};
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+#endif
--- examples/novice/N04/src/ExN04EventAction.cc
+++ examples/novice/N04/src/ExN04EventAction.cc
@@ -58,17 +58,35 @@ void ExN04EventAction::BeginOfEventAction(const G4Event*)
     calorimeterCollID = SDman->GetCollectionID(colNam="calCollection");
     muonCollID = SDman->GetCollectionID(colNam="muonCollection");
   }
-  G4VVisManager* pVVisManager = G4VVisManager::GetConcreteInstance();
-  if(pVVisManager)
-  {
-    G4UImanager::GetUIpointer()->ApplyCommand("/vis/draw/current");
-  }
+///  G4VVisManager* pVVisManager = G4VVisManager::GetConcreteInstance();
+///   if(pVVisManager)
+///   {
+///     G4UImanager::GetUIpointer()->ApplyCommand("/vis/draw/current");
+///   }
 }
 
 void ExN04EventAction::EndOfEventAction(const G4Event* evt)
 {
   G4cout << ">>> Event " << evt->GetEventID() << G4endl;
-
+  
+  // get number of stored trajectories
+  //
+  G4TrajectoryContainer* trajectoryContainer = evt->GetTrajectoryContainer();
+  G4int n_trajectories = 0;
+  if (trajectoryContainer) n_trajectories = trajectoryContainer->entries();
+  
+  // extract the trajectories and draw them
+  //
+  if (G4VVisManager::GetConcreteInstance())
+    {
+     for (G4int i=0; i<n_trajectories; i++) 
+        { G4Trajectory* trj = (G4Trajectory*)
+	                            ((*(evt->GetTrajectoryContainer()))[i]);
+          trj->DrawTrajectory(50);
+        }
+    }
+    
+    
   if(trackerCollID<0||calorimeterCollID<0||muonCollID<0) return;
 
   G4HCofThisEvent * HCE = evt->GetHCofThisEvent();
@@ -106,14 +124,14 @@ void ExN04EventAction::EndOfEventAction(const G4Event* evt)
          << " hits are stored in ExN04MuonHitsCollection." << G4endl;
   }
 
-  G4VVisManager* pVVisManager = G4VVisManager::GetConcreteInstance();
-  if(pVVisManager)
-  {
-    if(THC) THC->DrawAllHits();
-    if(CHC) CHC->DrawAllHits();
-    if(MHC) MHC->DrawAllHits();
-    G4UImanager::GetUIpointer()->ApplyCommand("/vis/show/view");
-  }
+///  G4VVisManager* pVVisManager = G4VVisManager::GetConcreteInstance();
+///  if(pVVisManager)
+///  {
+///    if(THC) THC->DrawAllHits();
+///    if(CHC) CHC->DrawAllHits();
+///    if(MHC) MHC->DrawAllHits();
+///    G4UImanager::GetUIpointer()->ApplyCommand("/vis/show/view");
+///  }
 }
 
 
