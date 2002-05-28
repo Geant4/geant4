b62edc1eddf3f075195a8fb5d1194c7e80a60c7c
Koichi Murakami <murakami@cern.ch>
2002-05-28 13:52:40 +0000

    add (rename)
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@10577 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ examples/extended/eventgenerator/HepMC/HepMCEx01/include/HepMCG4AsciiReader.hh
@@ -0,0 +1,86 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+
+// ====================================================================
+//
+//   HepMCG4AsciiReader.hh
+//   $Id: HepMCG4AsciiReader.hh,v 1.1 2002-05-28 13:52:40 murakami Exp $
+//
+// ====================================================================
+#ifndef HEPMC_G4_ASCII_READER_H
+#define HEPMC_G4_ASCII_READER_H
+
+#include "HepMCG4Interface.hh"
+#include "HepMC/IO_Ascii.h"
+
+class HepMCG4AsciiReaderMessenger;
+
+class HepMCG4AsciiReader : public HepMCG4Interface {
+protected:
+  G4String filename;
+  HepMC::IO_Ascii* asciiInput;
+
+  G4int verbose;
+  HepMCG4AsciiReaderMessenger* messenger;
+
+  virtual HepMC::GenEvent* GenerateHepMCEvent();
+
+public:
+  HepMCG4AsciiReader();
+  ~HepMCG4AsciiReader();
+
+  // set/get methods
+  void SetFileName(G4String name);
+  G4String GetFileName() const;
+
+  void SetVerboseLevel(G4int i);
+  G4int GetVerboseLevel() const; 
+
+  // methods...
+  void Initialize();
+};
+
+// ====================================================================
+// inline functions
+// ====================================================================
+
+inline void HepMCG4AsciiReader::SetFileName(G4String name)
+{
+  filename= name;
+}
+
+inline G4String HepMCG4AsciiReader::GetFileName() const
+{
+  return filename;
+}
+
+inline void HepMCG4AsciiReader::SetVerboseLevel(G4int i)
+{
+  verbose= i;
+}
+
+inline G4int HepMCG4AsciiReader::GetVerboseLevel() const
+{
+  return verbose;
+}
+
+#endif
---
+++ examples/extended/eventgenerator/HepMC/HepMCEx01/include/HepMCG4AsciiReaderMessenger.hh
@@ -0,0 +1,56 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+
+// ====================================================================
+//
+//   HepMCG4AsciiReaderMessenger.hh
+//   $Id: HepMCG4AsciiReaderMessenger.hh,v 1.1 2002-05-28 13:52:40 murakami Exp $
+//
+// ====================================================================
+#ifndef HEPMC_G4_ASCII_READER_MESSENGER_H
+#define HEPMC_G4_ASCII_READER_MESSENGER_H
+
+#include "G4UImessenger.hh"
+
+class HepMCG4AsciiReader;
+class G4UIdirectory;
+class G4UIcmdWithoutParameter;
+class G4UIcmdWithAString;
+class G4UIcmdWithAnInteger;
+
+class HepMCG4AsciiReaderMessenger : public G4UImessenger {
+private:
+  HepMCG4AsciiReader* gen;
+
+  G4UIdirectory* dir;
+  G4UIcmdWithAnInteger* verbose;
+  G4UIcmdWithAString* open;
+
+public:
+  HepMCG4AsciiReaderMessenger(HepMCG4AsciiReader* agen);
+  ~HepMCG4AsciiReaderMessenger();
+
+  void SetNewValue(G4UIcommand* command, G4String newValues);
+  G4String GetCurrentValue(G4UIcommand* command);
+};
+
+#endif
