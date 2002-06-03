85a673cb050078475ebe25320a094952748af124
radoone <radoone@cern.ch>
2002-06-03 12:09:36 +0000

    Added G4 Disclaimer
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@10724 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/gdml/libgdml/processes/src/defineProcess.cc
+++ examples/extended/gdml/libgdml/processes/src/defineProcess.cc
@@ -1,3 +1,35 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+//
+//
+// $Id: defineProcess.cc,v 1.3 2002-06-03 12:09:32 radoone Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+// 
+// --------------------------------------------------------------
+// Comments
+//
+// --------------------------------------------------------------
+//
 #include "ProcessingConfigurator.hh"
 #include "ProcessingContext.hh"
 #include "SAXProcessor.hh"
--- examples/extended/gdml/sample/src/gogdmlDetectorConstruction.cc
+++ examples/extended/gdml/sample/src/gogdmlDetectorConstruction.cc
@@ -1,13 +1,34 @@
-// This code implementation is the intellectual property of
-// the GEANT4 collaboration.
 //
-// By copying, distributing or modifying the Program (or any work
-// based on the Program) you indicate your acceptance of this statement,
-// and all its terms.
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
 //
-// $Id: gogdmlDetectorConstruction.cc,v 1.1.1.1 2002-05-31 00:34:43 radoone Exp $
+//
+// $Id: gogdmlDetectorConstruction.cc,v 1.2 2002-06-03 12:09:36 radoone Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
+// 
+// --------------------------------------------------------------
+// Comments
+//
+// --------------------------------------------------------------
 
 #include "gogdmlDetectorConstruction.hh"
 
