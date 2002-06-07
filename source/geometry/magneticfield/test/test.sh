90741cb19e4643ab20d9507c54b47a16ed70e72d
John Apostolakis <John.Apostolakis@cern.ch>
2002-06-07 18:31:40 +0000

    more tests for spin: using different steppers
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@10803 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/geometry/magneticfield/test/test.sh
+++ source/geometry/magneticfield/test/test.sh
@@ -3,7 +3,7 @@
 # A simple script to run all the tests in this directory and check
 # their results against the expected (previous) results
 #
-# $Id: test.sh,v 1.4 2002-06-06 17:30:34 japost Exp $
+# $Id: test.sh,v 1.5 2002-06-07 18:31:40 japost Exp $
 # $Name: not supported by cvs2svn $
 #
 
@@ -46,7 +46,29 @@ do
   echo  " "
 done
 
-#exit
+target=testProPerpSpin
+for i in *Spin.cc
+do
+  target=`basename $i .cc`
+  echo  "Compiling $target ... "
+  gmake G4TARGET=$target
+  echo  "Executing $target ..."
+  for n in 0 1 2 3 4
+  do
+    echo "Executing with stepper choice $n .. \c"
+    $G4WORKDIR/bin/$G4SYSTEM/$target $n > $target.newout$n \
+				       2> $target.newerr$n
+    echo  ".. difference from expected output: "
+    diff -wb $target.out$n $target.newout$n
+    sleep 1;
+    echo  ".. difference from expected error: "
+    diff -wb $target.err$n $target.newerr$n
+    sleep 1;
+    echo  " "
+  done
+done
+
+exit
 
 for i in *Spin.cc
 do
