5a5dd51b1b9970804bf77f0271f7d799940594b4
Vladimir Ivantchenko <Vladimir.Ivantchenko@cern.ch>
2002-06-10 15:45:21 +0000

    status 10.06
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@10823 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ tests/test31/src/test31StepCut.cc
@@ -0,0 +1,64 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+//
+//---------------------------------------------------------------------------
+//
+//---------------------------------------------------------------------------
+//
+// ClassName:   test31StepCut
+//  
+// Description: Cut on step length of charged particles
+//
+// Authors:   08.04.01  V.Ivanchenko 
+//
+// Modified:
+//
+//----------------------------------------------------------------------------
+//
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+#include "test31StepCut.hh"
+
+#include "G4Step.hh"
+#include "G4UserLimits.hh"
+#include "G4VParticleChange.hh"
+#include "G4EnergyLossTables.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+test31StepCut::test31StepCut(const G4String& aName, const test31PhysicsList* list)
+ :G4VDiscreteProcess(aName),
+  thePhysics(list)
+{
+  if (verboseLevel>0) {
+     G4cout << GetProcessName() << " is created "<< G4endl;
+  }
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+test31StepCut::~test31StepCut()
+{}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
