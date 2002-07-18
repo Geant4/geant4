768a45e57318ef4c30e480efbe458a6e984b2a07
Johannes-Peter Wellisch <jwellisc@cern.ch>
2002-07-18 11:01:11 +0000

    adding fix by Alex
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@11217 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/cross_sections/src/G4PhotoNuclearCrossSection.cc
+++ source/processes/hadronic/cross_sections/src/G4PhotoNuclearCrossSection.cc
@@ -21,7 +21,7 @@
 // ********************************************************************
 //
 //
-// $Id: G4PhotoNuclearCrossSection.cc,v 1.7 2002-05-29 07:16:39 mkossov Exp $
+// $Id: G4PhotoNuclearCrossSection.cc,v 1.8 2002-07-18 11:01:11 jwellisc Exp $
 // The lust update: M.V. Kossov, CERN/ITEP(Moscow) 17-June-02
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
@@ -131,6 +131,8 @@ G4double G4PhotoNuclearCrossSection::GetCrossSection(const G4DynamicParticle* aP
 #endif
         colN.push_back(targN);
         colZ.push_back(targZ);
+        GDR.push_back(lastGDR);                // added GDR, found by AH 10/7/02
+        HEN.push_back(lastHEN);                // added HEN, found by AH 10/7/02
         eTH.push_back(lastTH);                // Threshold Energy
         spA.push_back(lastSP);                // Pomeron Shadowing
 	  } // End of creation of the new set of parameters
