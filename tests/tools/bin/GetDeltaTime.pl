53082aba8f844c46c35542c2e1d01547f8aca0e3
Geant4 Testing <geant4.testing@cern.ch>
2002-07-31 11:01:31 +0000

    Correction for 'deltatime'.
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@11298 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- tests/tools/bin/GetDeltaTime.pl
+++ tests/tools/bin/GetDeltaTime.pl
@@ -1,7 +1,18 @@
 #!/usr/local/bin/perl
 
 
-$SttDir = "$ENV{'G4WORKDIR'}/stt/$ENV{'G4SYSTEM'}/";
+$SttDir = "$ENV{'G4WORKDIR'}/stt.geant4-04-01-ref-00+tags949/$ENV{'G4SYSTEM'}/";
+$SttDir2 = "$ENV{'G4WORKDIR'}/stt.geant4-04-01-ref-00+tags935/$ENV{'G4SYSTEM'}/";
+
+
+#$SttDir2 = $SttDir;
+#$SttDir2 =~ s|prod|dev1|;
+
+print "SttDir  = $SttDir\n";
+print "SttDir2 = $SttDir2\n";
+
+my $deltatime1 ;
+my $deltatime2 ;
 
 opendir(TT,"$SttDir") || die "Failed to opendir  $SttDir $!";
 @Tests = grep(m/\.err$/,readdir(TT));
@@ -18,9 +29,16 @@ print "TestName = $TestName\n";
 # Get the size of $TestName.err 
 #
     $filename = "$ENV{'G4WORKDIR'}/stt/$ENV{'G4SYSTEM'}/$TestName";
+    $filename2 = "$SttDir2/$TestName";
+
     $errsize = (stat($filename))[7];
 
+print "filename = $filename\n";
+print "filename2 = $filename2\n";
+
     open(ERRFILE,"$filename") || die "Failed to open read $filename $!";
+    open(ERRFILE2,"$filename2") || die "Failed to open read $filename $!";
+
     
     while(<ERRFILE>) {
 	if ($ENV{'G4SYSTEM'} eq 'Linux-g++' || $ENV{'G4SYSTEM'} eq 'Linux-egcs') {
@@ -35,7 +53,7 @@ print "TestName = $TestName\n";
 #		print "usertime=$usertime\n";
 #		print "usertime-1=",$usertime-1,"\n";
 	    } else {
-		$usertime=0;
+#		$usertime=0;
 	    }
 
 	    if (/(\d+)\.(\d+)system/) {
@@ -45,27 +63,92 @@ print "TestName = $TestName\n";
 #		print "systemtime=$systemtime\n";
 #		print "systemtime-1=",$systemtime-1,"\n";
 	    } else {
-		$systemtime=0;
+#		$systemtime=0;
 	    }
 	    
-	    $deltatime = $usertime + $systemtime;
-#	    print "$deltatime\n";
-	    if ( $deltatime ne 0) {
-		print "$TestName: deltatime = ",$deltatime,"\n";
+	    $deltatime1 = $usertime + $systemtime;
+#	    print "$deltatime1\n";
+	    if ( $deltatime1 ne 0) {
+		print "$TestName: deltatime1 = ",$deltatime1,"\n";
 	    }
 
 	} else {
 	    if (/^real(\s+)(\d+)\.(\d+)/) {
 		
-		$deltatime = "$2.$3";
+		$deltatime1 = "$2.$3";
 		
-#		print "deltatime=$deltatime\n";
+#		print "deltatime1=$deltatime1\n";
 	    }
 	}
     }
-    
-    close(ERRFILE);
 
+close(ERRFILE);
+
+    while(<ERRFILE2>) {
+	if ($ENV{'G4SYSTEM'} eq 'Linux-g++' || $ENV{'G4SYSTEM'} eq 'Linux-egcs') {
+	    
+#	    print "OK\n";
+# It's very dependent on CPU load! Need to use user+system.
+#	    if (/(\d+)\:(\d+)\.(\d+)elapsed/) {
+	    if (/(\d+)\.(\d+)user/) {
+		
+		$usertime = "$1.$2";
+		
+#		print "usertime=$usertime\n";
+#		print "usertime-1=",$usertime-1,"\n";
+	    } else {
+#		$usertime=0;
+	    }
+
+	    if (/(\d+)\.(\d+)system/) {
+		
+		$systemtime = "$1.$2";
+		
+#		print "systemtime=$systemtime\n";
+#		print "systemtime-1=",$systemtime-1,"\n";
+	    } else {
+#		$systemtime=0;
+	    }
+	    
+	    $deltatime2 = $usertime + $systemtime;
+#	    print "$deltatime2\n";
+	    if ( $deltatime2 ne 0) {
+		print "$TestName: deltatime2 = ",$deltatime2,"\n";
+	    }
+
+	} else {
+	    if (/^real(\s+)(\d+)\.(\d+)/) {
+		
+		$deltatime2 = "$2.$3";
+		
+#		print "deltatime2=$deltatime2\n";
+	    }
+	}
+#		print "$TestName: deltatime2 = ",$deltatime2,"\n";
+    }
+    
+    close(ERRFILE2);
+
+
+
+no integer;
+$i = 0.29;
+$j = 0.35;
+$testNumber = $i/$j;
+
+print "testNumber = $testNumber\n";
+  
+#$ratio = $deltatime1/$deltatime2;
+
+		print "$TestName: deltatime1 = ",$deltatime1,"\n";
+		print "$TestName: deltatime2 = ",$deltatime2,"\n";
+
+$TestName =~ s/.err//;
+$Ratio = ($deltatime1*100)/$deltatime2;
+
+use integer;
+$IntRatio = $Ratio*1;
+print "Ratio($TestName): new/release = \t\t",$IntRatio,"\n";
 
 }
     
