4ff26daaa34e55b637a65fc496368e7323e912a6
Alexander Howard <alexander.howard@cern.ch>
2002-10-01 16:54:16 +0000

    Corrected physics lists
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@11529 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/advanced/lAr_calorimeter/include/FCALPhysicsList.hh
+++ examples/advanced/lAr_calorimeter/include/FCALPhysicsList.hh
@@ -1,111 +1,68 @@
-//----------------------------------------------------------------------------
+// This code implementation is the intellectual property of
+// the GEANT4 collaboration.
 //
-//   Name of file:      FCALFrontVolumes.hh
-//   Author:            Mathieu Fontaine           Rachid Mazini
-//                      fontaine@lps.umontreal.ca  Rachid.Mazini@cern.ch
-//   Language:          C++
-//   Tested on:         g++
-//   Prerequisites:     None
-//   Purpose:           Header file for FCALFrontVolume.cc, which defines
-//                      the  volumes in the testbeam front.
-//   Developped:        10-March-2000   M.F.
+// By copying, distributing or modifying the Program (or any work
+// based on the Program) you indicate your acceptance of this statement,
+// and all its terms.
 //
-//----------------------------------------------------------------------------
+// $Id: FCALPhysicsList.hh,v 1.2 2002-10-01 16:54:16 ahoward Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+// 
 
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
 
-#ifndef FCALTestbeamSetup_h
-#define FCALTestbeamSetup_h 1
+#ifndef FCALPhysicsList_h
+#define FCALPhysicsList_h 1
 
-#include "G4VUserDetectorConstruction.hh"
+#include "G4VUserPhysicsList.hh"
 #include "globals.hh"
-#include "FCALTestbeamSetupSD.hh"
 
-//class FCALFrontVolumes;
-//class FCALTailVolumes;
-//class FCALCryostatVolumes;
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
 
-class G4LogicalVolume;
-class G4VPhysicalVolume;
-
-class FCALTestbeamSetup : public G4VUserDetectorConstruction
+class FCALPhysicsList: public G4VUserPhysicsList
 {
+  public:
+    FCALPhysicsList();
+   ~FCALPhysicsList();
 
-public:
+  protected:
+    // Construct particle and physics
+    void ConstructParticle();
+    void ConstructProcess();
+ 
+    void SetCuts();
 
-  FCALTestbeamSetup();
-  ~FCALTestbeamSetup();
+  public:
+    // Set/Get cut values 
+    void      SetCutForGamma(G4double);
+    void      SetCutForElectron(G4double);
+    void      SetCutForProton(G4double);           
+    G4double  GetCutForGamma() const;
+    G4double  GetCutForElectron() const;
+    G4double  GetCutForProton() const;
+    
+  protected:
+    // these methods Construct particles 
+    void ConstructBosons();
+    void ConstructLeptons();
+    void ConstructMesons();
+    void ConstructBaryons();
 
-public:
+  protected:
+  // these methods Construct physics processes and register them
+    void ConstructGeneral();
+    void ConstructEM();
 
-  G4VPhysicalVolume* Construct();
-
-private:
-
-  G4double MotherSizeX, MotherSizeY, MotherSizeZ;
-
-  G4double MWPCSizeX, MWPCSizeY, MWPCSizeZ;
-  G4double MWPCPosX, MWPCPosY, MWPCPosZ[5];
-
-  G4double ScintS1andS3SizeX, ScintS1andS3SizeY, ScintS1andS3SizeZ;
-  G4double ScintS2SizeX, ScintS2SizeY, ScintS2SizeZ;
-  G4double ScintS1_S3PosX, ScintS1_S3PosY, ScintS1PosZ,ScintS2PosZ, ScintS3PosZ;
-
-  G4double HoleCntrSizeX, HoleCntrSizeY, HoleCntrScintSizeZ, HoleCntrAbsrbSizeZ;
-  G4double HoleCntrScintPosX, HoleCntrScintPosY, HoleCntrScintPosZ;
-  G4double HoleCntrPbPosX, HoleCntrPbPosY, HoleCntrPbPosZ;
-  G4double HoleCntrAlPosX, HoleCntrAlPosY, HoleCntrAlPosZ;
-  G4double ScintHoleRmin, ScintHoleRmax, ScintHoleLenght;
-  G4double AbsrbHoleRmin, AbsrbHoleRmax, AbsrbHoleLenght;
-  G4double HoleStartPhi, HoleDPhi;
-  G4double HolePosX, HolePosY, HolePosZ;
-
-  G4double LeadWallSizeX, LeadWallSizeY, LeadWallSizeZ;
-  G4double LeadWallSlitSizeX, LeadWallSlitSizeY, LeadWallSlitSizeZ;
-  G4double LeadWallPosX,LeadWallPosY, LeadWallPosZ;
-
-  G4double IronWallSizeX, IronWallSizeY, IronWallSizeZ;
-  G4double IronWallSlitSizeX, IronWallSlitSizeY, IronWallSlitSizeZ;
-  G4double IronWallPosX,IronWallPosY, IronWallPosZ;
-
-  G4int NBigScint, NSmallScint, NBigIron, NSmallIron;
-  G4double BigScintSizeX, BigScintSizeY, SmallScintSizeX, SmallScintSizeY, ScintSizeZ;
-  G4double ScintPosX, ScintPosY, ScintPosZ[7];
-  G4double BigIronSizeX, BigIronSizeY, SmallIronSizeX, SmallIronSizeY, IronSizeZ;
-  G4double IronPosX, IronPosY, IronPosZ[6];
-
-  G4double ConcWallSizeX, ConcWallSizeY, ConcWallSizeZ; 
-  G4double ConcWallPosX, ConcWallPosY, ConcWallAPosZ, ConcWallBPosZ;
-  G4double ConcWallInsSizeX, ConcWallInsSizeY, ConcWallInsSizeZ;
-  G4double ConcWallInsPosX, ConcWallInsPosY, ConcWallInsPosZ;
-
-  G4double MuCntrSIzeX, MuCntrSIzeY, MuCntrSIzeZ;
-  G4double MuCntrPosX, MuCntrPosY, MuCntrPosZ;
-  
-
-  G4double CryostatPosX, CryostatPosY, CryostatPosZ;
-
-
-  FCALTestbeamSetupSD* FcalTBSetupSD;  // Senstive detector
-
-  /* 
-  G4double TailPosX;
-  G4double TailPosY;
-  G4double TailPosZ;
-
-  G4double FrontPosX;
-  G4double FrontPosY;
-  G4double FrontPosZ;
-  */
-  /*
-  G4double EMModulePosX;
-  G4double EMModulePosY;
-  G4double EMModulePosZ;
-
-  G4double HadModulePosX;
-  G4double HadModulePosY;
-  G4double HadModulePosZ;
-  */
-
+  private:
+    G4double cutForGamma;
+    G4double cutForElectron; 
+    G4double cutForProton;
+    G4double currentDefaultCut;
 };
 
-#endif   /* FCALTestbeamSetup.hh */
+#endif
+
+
+
