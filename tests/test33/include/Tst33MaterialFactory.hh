87247a3cf1d35df0c0ef5bfcd3e03b0f280f8602
Micheal Dressel <dressel@cern.ch>
2002-10-29 16:37:10 +0000

    added DISCLAIMERs
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@11804 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- tests/test33/include/Tst33MaterialFactory.hh
+++ tests/test33/include/Tst33MaterialFactory.hh
@@ -1,3 +1,39 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+//
+//
+// $Id: Tst33MaterialFactory.hh,v 1.2 2002-10-29 16:37:09 dressel Exp $
+// GEANT4 tag 
+//
+// ----------------------------------------------------------------------
+// Class Tst33MaterialFactory
+//
+// Class description:
+//
+// ...
+
+// Author: Michael Dressel (Michael.Dressel@cern.ch)
+// ----------------------------------------------------------------------
+
 #ifndef Tst33MaterialFactory_hh
 #define Tst33MaterialFactory_hh Tst33MaterialFactory_hh
 
--- tests/test33/src/Tst33ParallelGeometry.cc
+++ tests/test33/src/Tst33ParallelGeometry.cc
@@ -1,3 +1,36 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+//
+//
+// $Id: Tst33ParallelGeometry.cc,v 1.2 2002-10-29 16:37:10 dressel Exp $
+// GEANT4 tag 
+//
+// ----------------------------------------------------------------------
+// GEANT 4 class source file
+//
+// Tst33ParallelGeometry.cc
+//
+// ----------------------------------------------------------------------
+
 #include "Tst33ParallelGeometry.hh"
 #include "G4Tubs.hh"
 #include "G4LogicalVolume.hh"
