62ea1143d97037152774aabda737d0aeacf3bf2a
Aatos Heikkinen <miheikki@cern.ch>
2002-11-05 12:35:23 +0000

    preliminary working  testing environment
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@11917 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/processes/hadronic/models/cascade/test/cascade.C
@@ -0,0 +1,45 @@
+{
+// automated analysis for g4 cascade simulation
+// root -b -q cascade.C
+
+#include <iostream.h>
+
+printf("::: Reading cascade data ...\n");
+
+gROOT->Reset();
+
+ifstream in;
+// we assume a file basic.dat in the current directory
+// this file has 3 columns of float data
+in.open("basic.dat", ios::in);
+
+Float_t x,y,z;
+Int_t nlines = 0;
+TFile *f = new TFile("basic.root","RECREATE");
+TH1F *h1 = new TH1F("h1","x distribution",100,-4,4);
+TNtuple *ntuple = new TNtuple("ntuple","data from ascii file","x:y:z");
+
+while (1) {
+  in >> x >> y >> z;
+  if (!in.good()) break;
+  if (nlines < 5) printf("x=%8f, y=%8f, z=%8f\n",x,y,z);
+  h1->Fill(x);
+  ntuple->Fill(x,y,z);
+  nlines++;
+}
+printf(" found %d points\n",nlines);
+
+in.close();
+
+f->Write();
+
+printf("::: Writing ps files ...\n");
+
+TPostScript* ps = new TPostScript("cascade.ps",-111);
+   ps->Off();
+   ps->On();
+   ps->NewPage();
+   c1->Update();
+   ps->Off();
+   ps->Close();
+}
