921cf9e1cce51b1a3b673e79590ba08e3daa9910
Micheal Dressel <dressel@cern.ch>
2002-11-07 13:47:59 +0000

    example for mass geometry with importance and scoring
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@11971 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/biasing/B01/exampleB01.cc
+++ examples/extended/biasing/B01/exampleB01.cc
@@ -21,7 +21,7 @@
 // ********************************************************************
 //
 //
-// $Id: exampleB01.cc,v 1.14 2002-10-22 14:09:02 dressel Exp $
+// $Id: exampleB01.cc,v 1.15 2002-11-07 13:47:59 dressel Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // 
@@ -31,36 +31,81 @@
 // --------------------------------------------------------------
 // Comments
 //
+// This example intends to show how to use importance sampling and scoring
+// in the mass (tracking) geometry.
+// A simple geometry consisting of a 180 cm high concrete cylinder
+// divided into 18 slabs of 10cm each is created. 
+// Importance values are assigned to the 18 concrete slabs in the
+// detector construction class for simplicity.
+// Pairs of G4GeometryCell and importance values are stored in
+// the importance store.
+// The G4Scorer is used for the scoring. This is a top level
+// class using the frame work provided for scoring.
 // 
+
 // --------------------------------------------------------------
 
-#include "CLHEP/Random/Random.h"
-#include "G4UImanager.hh"
-#include "G4UIterminal.hh"
-#include "G4UItcsh.hh"
-#include "G4UIsession.hh"
+#include "g4std/iostream"
 
-#include "B01AppStarter.hh"
+#include "G4VPhysicalVolume.hh"
+#include "G4RunManager.hh"
+#include "G4UImanager.hh"
+
+#include "B01DetectorConstruction.hh"
+#include "B01PhysicsList.hh"
+#include "B01PrimaryGeneratorAction.hh"
+
+// Files specific for biasing and scoring
+#include "G4Scorer.hh"
+#include "G4MassGeometrySampler.hh"
+#include "G4IStore.hh"
+
+// a score table
+#include "G4ScoreTable.hh"
+
 
 int main(int argc, char **argv)
 {  
+  G4std::ostream *myout = &G4cout;
+  G4int numberOfEvent = 100;
 
-  HepRandom::setTheSeed(345354);
+  G4long myseed = 345354;
 
-  B01AppStarter *appstarter = new B01AppStarter;
+  HepRandom::setTheSeed(myseed);
 
-  if (argc!=2) {
-    G4UIsession * session = 0;
-#ifdef G4UI_USE_TCSH
-    session = new G4UIterminal(new G4UItcsh);      
-#else
-    session = new G4UIterminal();
-#endif    
-    session->SessionStart();
-  }
-  else {
-    G4UImanager::GetUIpointer()->
-      ApplyCommand(G4String("/control/execute ") + G4String(argv[1]));
-  }
-  delete appstarter;
+  G4RunManager *runManager = new G4RunManager;
+  
+  // create the detector      ---------------------------
+  B01DetectorConstruction *detector = new B01DetectorConstruction();
+  runManager->SetUserInitialization(detector);
+  //  ---------------------------------------------------
+  runManager->SetUserInitialization(new B01PhysicsList);
+  runManager->SetUserAction(new B01PrimaryGeneratorAction);
+  runManager->Initialize();
+
+  // the IStore is filled during detector construction
+  G4IStore &aIstore = *detector->GetIStore();
+
+  // create the importance and scoring sampler for biasing and scoring 
+  // in the tracking world
+
+  G4Scorer scorer; 
+
+  G4MassGeometrySampler mgs("neutron");
+  mgs.PrepareScoring(&scorer);
+  mgs.PrepareImportanceSampling(&aIstore, 0);
+  mgs.Configure();
+
+  G4UImanager* UI;
+
+  UI = G4UImanager::GetUIpointer();
+  UI->ApplyCommand("/control/execute init.mac");   
+
+  runManager->BeamOn(numberOfEvent);
+
+  // print a table of the scores
+  G4ScoreTable sp(&aIstore);
+  sp.Print(scorer.GetMapGeometryCellCellScorer(), myout);
+
+  return 0;
 }
