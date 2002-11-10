1b92a477926c7866cf2896663672d18c269f924d
Aatos Heikkinen <miheikki@cern.ch>
2002-11-10 18:11:46 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@12054 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/processes/hadronic/models/cascade/test/benchmarks.cc
@@ -0,0 +1,126 @@
+//#define CHECK_MOMC
+
+#include "globals.hh"
+//#include "Randomize.hh"
+
+#include "G4Collider.hh"
+#include "G4InuclCollider.hh"
+#include "G4IntraNucleiCascader.hh"
+#include "G4NonEquilibriumEvaporator.hh"
+#include "G4EquilibriumEvaporator.hh"
+#include "G4Fissioner.hh"
+#include "G4BigBanger.hh"
+#include "G4ElementaryParticleCollider.hh"
+#include "G4InuclParticle.hh"
+#include "G4InuclElementaryParticle.hh"
+#include "G4InuclNuclei.hh"
+#include "G4CollisionOutput.hh"
+#include "G4Analyser.hh"
+#include "G4WatcherGun.hh"
+
+#include "vector"
+
+G4int benchmarkAll();
+
+// Banchmarking. 
+// Uses permanent datafile banchmarks.data
+int main() {
+
+  benchmarkAll(); // Run all models in tandem
+
+  return 0;       
+};
+
+G4int benchmarkAll()  {
+
+  G4int verboseLevel = 1; // For benchmarking  quals 1.
+  
+  // General benchmark test program
+
+  const G4int to_report = 1;
+  G4int    nrain = 100;  // number of interactions to be generated
+  G4double eMin  = 0.1;  // minimun energy for bullet
+  G4double eMax  = 10.0; // maximum energy for bullet
+  G4int    eBins = 10;   // bullet energy bins
+  G4double eStep = (eMax-eMin)/eBins;
+
+  for(G4int e = 0; e < eBins; e++) { // Scan with different energy 
+    // Auxiliarly stuff for ugly analysis
+    G4Analyser* analyser = new G4Analyser();
+    G4WatcherGun* gun = new G4WatcherGun;
+    gun->setWatchers();
+    analyser->setWatchers(gun->getWatchers());
+    analyser->setInelCsec(1760.0, true);
+
+    // Colliders initialisation
+    G4ElementaryParticleCollider*   colep = new G4ElementaryParticleCollider;
+    G4IntraNucleiCascader*        cascade = new G4IntraNucleiCascader; // the actual cascade
+    G4NonEquilibriumEvaporator*     noneq = new G4NonEquilibriumEvaporator;
+    G4EquilibriumEvaporator*         eqil = new G4EquilibriumEvaporator;
+    G4Fissioner*                     fiss = new G4Fissioner;
+    G4BigBanger*                     bigb = new G4BigBanger;
+    G4InuclCollider*             collider = new G4InuclCollider(colep, cascade, noneq, eqil, fiss, bigb);
+
+    // Bullet / Target initialisation
+    // Bullet could be nucleon or pion or nuclei
+    // proton momentum in Z-direction [GeV]
+    G4double bulletEnergy = eMin + eStep * e;
+ 
+    if (verboseLevel > 1) {
+      G4cout << "Bullet E =" << bulletEnergy << " GeV" << endl;
+    };
+
+    G4InuclParticle* bull = new G4InuclElementaryParticle(bulletEnergy, 1); 
+
+    G4InuclParticle* targ = NULL;
+
+    switch (e) {
+     // ::: add standard H,Be, Cu, Lb, U
+      case 1:
+     targ = new G4InuclNuclei(0.0, 197.0, 79.0);     // Au197 target at rest
+
+     default:
+      targ = new G4InuclNuclei(0.0, 208.0, 82.0); // Pb
+    }
+
+#ifdef CHECK_MOMC
+    vector<G4double> total_mom_in = bull->getMomentum();
+    vector<G4double> momt = targ->getMomentum();
+    for(G4int i = 0; i < 4; i++) total_mom_in[i] += momt[i];
+    vector<G4double> total_mom_out;
+    bull->printParticle();
+    targ->printParticle();
+    if (verboseLevel > 1) {
+      G4cout << " tot in mom: px " << total_mom_in[1] << " py " << total_mom_in[2] << " pz " << total_mom_in[3] << " e " << total_mom_in[0] << G4endl;
+    }
+
+#endif
+    for(G4int i = 0; i < nrain; i++) {
+      if((i + 1) % to_report == 0) 
+
+	if (verboseLevel > 1) {
+	  G4cout << " Event " << i+1 <<":" << G4endl;
+      	}
+
+      G4CollisionOutput cascadeParticles = collider->collide(bull, targ); // standard method
+
+#ifdef CHECK_MOMC
+      total_mom_out = cascadeParticles.getTotalOutputMomentum();
+      G4cout << " 4 - momentum conservation check " << G4endl
+	     << " dE " << total_mom_out[0] - total_mom_in[0] << 
+	" dPx " << total_mom_out[1] - total_mom_in[1] <<
+	" dPy " << total_mom_out[2] - total_mom_in[2] <<
+	" dPz " << total_mom_out[3] - total_mom_in[3] << G4endl;
+#endif
+      analyser->analyse(cascadeParticles);
+    };
+    //  analyser->printResults();
+    //  analyser->printResultsSimple();
+    analyser->printResultsNtuple();
+  }
+
+  return 0;
+};
+
+
+
