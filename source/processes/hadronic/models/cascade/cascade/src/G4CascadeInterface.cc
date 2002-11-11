4920b2afaa2bff46905e2ca64cde9b1535fc014b
Aatos Heikkinen <miheikki@cern.ch>
2002-11-11 10:11:11 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@12060 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/models/cascade/cascade/src/G4CascadeInterface.cc
+++ source/processes/hadronic/models/cascade/cascade/src/G4CascadeInterface.cc
@@ -30,31 +30,31 @@ G4CascadeInterface::G4CascadeInterface()
 };
    
 G4ReactionProductVector* G4CascadeInterface::Propagate(G4KineticTrackVector* theSecondaries, 
-						       G4V3DNucleus* theNucleus) 
-{
+						       G4V3DNucleus* theNucleus) {
   return NULL;
-}
+};
 
 #define debug_G4CascadeInterface
 
-G4VParticleChange* G4CascadeInterface::
-ApplyYourself(const G4Track& aTrack, G4Nucleus& theNucleus)
-{
+G4VParticleChange* G4CascadeInterface::ApplyYourself(const G4Track& aTrack, 
+						     G4Nucleus& theNucleus) {
 #ifdef debug_G4CascadeInterface
   static G4int counter(0);
   counter++;
   G4cerr << "Reaction number "<< counter << G4endl;
 #endif
   
-  if (verboseLevel > 3) 
-    {
-      G4cout << " >>> G4CascadeInterface::ApplyYourself" << G4endl;
-    }
+  if (verboseLevel > 3) {
+    G4cout << " >>> G4CascadeInterface::ApplyYourself" << G4endl;
+  };
 
-  // Make conversion between native Geant4 and Bertini cascade classes.
   theResult.Initialize(aTrack);
 
+  // Make conversion between native Geant4 and Bertini cascade classes.
+  // NOTE: Geant4 units are MeV = 1 and GeV = 1000. Cascade code by default use GeV = 1.
+
   enum particleType { proton = 1, neutron = 2, pionPlus = 3, pionMinus = 5, pionZero = 7 };
+
   G4int bulletType = 0;
 
   // Coding particles 
@@ -64,11 +64,9 @@ ApplyYourself(const G4Track& aTrack, G4Nucleus& theNucleus)
   if(aTrack.GetDefinition() == G4PionMinus::PionMinus() ) bulletType = pionMinus;
   if(aTrack.GetDefinition() ==  G4PionZero::PionZero()  ) bulletType = pionZero;
 
-  // NOTE: Geant4 units are MeV = 1 and GeV = 1000. Cascade code by default use GeV = 1.
-
   // Code momentum and energy.
   G4std::vector<G4double> momentumBullet(4);
-  momentumBullet[0] =aTrack.GetDynamicParticle()->Get4Momentum().e() / GeV;
+  momentumBullet[0] =aTrack.GetDynamicParticle()->Get4Momentum().e()  / GeV;
   momentumBullet[1] =aTrack.GetDynamicParticle()->Get4Momentum().px() / GeV;
   momentumBullet[2] =aTrack.GetDynamicParticle()->Get4Momentum().py() / GeV;
   momentumBullet[3] =aTrack.GetDynamicParticle()->Get4Momentum().pz() / GeV;
@@ -85,7 +83,7 @@ ApplyYourself(const G4Track& aTrack, G4Nucleus& theNucleus)
 
   G4CollisionOutput output;
 
-  G4bool runOnlyINC = true; // Flag for running only INC or all models.
+  G4bool runOnlyINC = false; // Flag for running only INC or all models.
 
   if (runOnlyINC) { // Run only INC
   
@@ -103,17 +101,20 @@ ApplyYourself(const G4Track& aTrack, G4Nucleus& theNucleus)
     // Colliders initialisation
     G4ElementaryParticleCollider*   colep = new G4ElementaryParticleCollider;
     G4IntraNucleiCascader*            inc = new G4IntraNucleiCascader; // the actual cascade
+    inc->setInteractionCase(1); // Interaction type is particle with nuclei.
+
     G4NonEquilibriumEvaporator*     noneq = new G4NonEquilibriumEvaporator;
     G4EquilibriumEvaporator*         eqil = new G4EquilibriumEvaporator;
     G4Fissioner*                     fiss = new G4Fissioner;
     G4BigBanger*                     bigb = new G4BigBanger;
+
     G4InuclCollider*             collider = new G4InuclCollider(colep, inc, noneq, eqil, fiss, bigb);
 
     output = collider->collide(bullet, target); 
   }
 
   if (verboseLevel > 1) {
-    G4cout << " Bertini cascade: " << G4endl;
+    G4cout << " Cascade output: " << G4endl;
     output.printCollisionOutput();
   };
 
@@ -135,22 +136,27 @@ ApplyYourself(const G4Track& aTrack, G4Nucleus& theNucleus)
       G4DynamicParticle* cascadeParticle = NULL;
 
       switch(outgoingParticle) {
+
       case proton: 
 	cascadeParticle = 
 	  new G4DynamicParticle(G4Proton::ProtonDefinition(), aMom, ekin);
 	break; 
+
       case neutron: 
 	cascadeParticle = 
 	  new G4DynamicParticle(G4Neutron::NeutronDefinition(), aMom, ekin);
 	break;
+
       case pionPlus: 
 	cascadeParticle = 
 	  new G4DynamicParticle(G4PionPlus::PionPlusDefinition(), aMom, ekin);
 	break;
+
       case pionMinus:
 	cascadeParticle = 
 	  new G4DynamicParticle(G4PionMinus::PionMinusDefinition(), aMom, ekin);
 	break;
+
       case pionZero: 
 	cascadeParticle = 
 	  new G4DynamicParticle(G4PionZero::PionZeroDefinition(), aMom, ekin);
