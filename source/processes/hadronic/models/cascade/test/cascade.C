385cf53260f861324dc99d7d3f7dbaf6e20db160
Aatos Heikkinen <miheikki@cern.ch>
2002-11-12 14:33:27 +0000

    toward minimalistic style
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@12101 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/models/cascade/test/cascade.C
+++ source/processes/hadronic/models/cascade/test/cascade.C
@@ -1,8 +1,9 @@
 {
 // Batch analysis for g4 cascade simulation: root -b -q cascade.C
 
-#include <iostream.h>
+// Gray shades 10...19, 0 (from flack to white) 40-49 shades of red
 
+//#include <iostream.h>
 
 gROOT->Reset();
 
@@ -34,39 +35,46 @@ in.close();
 
 f->Write();
 
-c1 = new TCanvas("c1","The Ntuple canvas",200,10,700,780);
+c1 = new TCanvas("c1","Analysis from cascade.cc ",200,10,700,780);
+c1->SetFillColor(0);
+c1->SetBorderMode(0);
 
-pad1 = new TPad("pad1","This is pad1",0.02,0.52,0.48,0.98,0); // 0 equals white 1 = black
+pad1 = new TPad("pad1","This is pad1",0.02,0.52,0.48,0.98,0); 
 pad2 = new TPad("pad2","This is pad2",0.52,0.52,0.98,0.98,0);
 pad3 = new TPad("pad3","This is pad3",0.02,0.02,0.48,0.48,0);
 pad4 = new TPad("pad4","This is pad4",0.52,0.02,0.98,0.48,0);
+
+   pad1->SetBorderMode(0);
+pad2->SetBorderMode(0);
+   pad3->SetBorderMode(0);
+   pad4->SetBorderMode(0);
 pad1->Draw();
 pad2->Draw();
 pad3->Draw();
 pad4->Draw();
 
-// Change default style for the statistics box
+// Change default style 
+//gStyle->SetPalette(,) 
 gStyle->SetStatW(0.30);
 gStyle->SetStatH(0.20);
 gStyle->SetStatColor(0);
-
+gStyle->SetOptStat(10);
 // Proton energy
 pad1->cd();
 pad1->SetGrid();
 pad1->SetLogy();
-pad1->GetFrame()->SetFillColor(0);
 ntuple->SetLineColor(1);
-ntuple->SetFillColor(0);
+ntuple->SetFillColor(17);
 
 ntuple->Draw("eKin", "typePart==1");
 c1->Update();
-c1->SetTitle("ssss");
 
 // Neutron energy
 pad2->cd();
 pad2->SetGrid();
 pad2->SetLogy();
-pad2->GetFrame()->SetFillColor(0);
+pad2->GetFrame()->SetFillColor(3);
+ntuple->SetFillColor(18);
 ntuple->Draw("eKin", "typePart==2");
 
 c1->Update();
@@ -78,9 +86,12 @@ pad3->SetGrid();
 pad3->SetLogy();
 pad3->GetFrame()->SetFillColor(0);
 
-ntuple->SetFillStyle(1001);
+ntuple->SetFillStyle(4000);
+
 ntuple->Draw("eKin", "typePart==3");
+
 ntuple->Draw("eKin", "typePart==5", "same");
+
 ntuple->Draw("eKin", "typePart==7", "same");
 c1->Update();
 
@@ -88,14 +99,14 @@ pad4->cd();
 pad4->SetGrid();
 pad4->SetLogy();
 ntuple->SetLineColor(1);
-
+ntuple->SetFillColor(19);
 //ntuple->Draw("eKin", "typePart==10");
 ntuple->Draw("eKin", "typePart==0");
 c1->Update();
 
 c1->cd();
 c1->Update();
-gStyle->SetStatColor(0);
+gStyle->SetStatColor(5);
 
 printf(" ::: Writing ps files ...\n");
 c1->Print("cascade.ps");
