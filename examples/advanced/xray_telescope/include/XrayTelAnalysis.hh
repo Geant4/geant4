11e6203cb956641e3f9f6c76990089313d56444f
Giovanni Santin <Giovanni.Santin@cern.ch>
2002-11-13 15:57:27 +0000

    only one analysis version with the AIDA interfaces
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@12143 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ examples/advanced/xray_telescope/include/XrayTelAnalysis.hh
@@ -0,0 +1,140 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+//
+//
+// $Id: XrayTelAnalysis.hh,v 1.5 2002-11-13 15:57:27 santin Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+// Author: A. Pfeiffer (Andreas.Pfeiffer@cern.ch) 
+//         (copy of his UserAnalyser class)
+//
+// History:
+// -----------
+//  7 Nov 2001   MGP  Implemented according to A. Pfeiffer's instructions
+//
+// -------------------------------------------------------------------
+// Class description:
+// Example of analysis in a simulation application (histograms, ntuples etc.)
+// This class follows the singleton design pattern; 
+// it is responsible for the analysis management and algorithms 
+// Histograms are compliant with AIDA 1.0 (the header files are
+// copies from the Anaphe Interfaces directory), except for the usage of
+// IHistoManager which is Anaphe/Lizard-specific
+// For ntuples, for which the AIDA interfaces and compliant implementations
+// are still in progress at the time of the current Geant4 release, 
+// an implementation with Anaphe/Lizard is shown
+// Other implementations specific to an analysis system are possible too
+// (see, for instance, JAS and OpenScientist documentation from the links
+// in http://aida.freehep.org/)
+// The implementation of the usage of ntuples shown in this example
+// is expected to change in future Geant4 releases, when AIDA interfaces 
+// and related implementations would be available
+// Further documentation is available from: http://www.ge.infn.it/geant4/lowE/
+//                                          http://aida.freehep.org/
+//                                          http://cern.ch/anaphe/
+
+// -------------------------------------------------------------------
+
+#ifndef G4PROCESSTESTANALYSIS_HH
+#define G4PROCESSTESTANALYSIS_HH
+
+#include "globals.hh"
+
+#ifdef G4ANALYSIS_USE
+#include "AIDA/IAnalysisFactory.h"
+
+#include "AIDA/ITreeFactory.h"
+#include "AIDA/ITree.h"
+
+#include "AIDA/IHistogramFactory.h"
+#include "AIDA/IHistogram1D.h"
+#include "AIDA/IHistogram2D.h"
+#include "AIDA/IHistogram3D.h"
+
+#include "AIDA/IPlotterFactory.h"
+#include "AIDA/IPlotterRegion.h"
+#include "AIDA/IPlotter.h"
+
+#include "AIDA/ITupleFactory.h"
+#include "AIDA/ITuple.h"
+
+#include "AIDA/IManagedObject.h"
+#endif
+
+// Histogramming from AIDA 
+class IAnalysisFactory;
+class ITree;
+class IHistogramFactory;
+class ITupleFactory;
+class ITuple;
+
+class G4Track;
+class NTuple;
+
+class XrayTelAnalysis
+{
+public:
+
+  ~XrayTelAnalysis();
+
+  void book();
+  
+  void finish();
+  
+  void analyseStepping(const G4Track& track, G4bool entering);
+
+  static XrayTelAnalysis* getInstance();
+
+private:
+
+  XrayTelAnalysis();
+
+#ifdef G4ANALYSIS_USE_PLOTTER
+  void plotAll();
+#endif
+  static XrayTelAnalysis* instance;
+
+#ifdef G4ANALYSIS_USE
+  AIDA::IAnalysisFactory  *analysisFactory;
+  AIDA::ITree             *tree;
+  AIDA::IHistogramFactory *histoFactory;
+  AIDA::ITupleFactory     *tupleFactory;
+#ifdef G4ANALYSIS_USE_PLOTTER
+  AIDA::IPlotterFactory   *plotterFactory;
+  AIDA::IPlotter          *plotter;
+#endif
+#endif
+  // Quantities for the ntuple
+  G4double eKin;
+  G4double x;
+  G4double y;
+  G4double z;
+  G4double dirX;
+  G4double dirY;
+  G4double dirZ;
+
+  G4String histFileName;
+  G4String histFileType;
+
+};
+
+#endif 
---
+++ examples/advanced/xray_telescope/src/XrayTelAnalysis.cc
@@ -0,0 +1,266 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+//
+//
+// $Id: XrayTelAnalysis.cc,v 1.5 2002-11-13 15:56:52 santin Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+// Author:  A. Pfeiffer (Andreas.Pfeiffer@cern.ch) 
+//         (copied from his UserAnalyser class)
+//
+// History:
+// -----------
+//  8 Nov 2002   GS         Migration to AIDA 3
+//  7 Nov 2001   MGP        Implementation
+//
+// -------------------------------------------------------------------
+
+#include "XrayTelAnalysis.hh"
+#include "globals.hh"
+#include "G4Track.hh"
+#include "G4SteppingManager.hh"
+#include "G4ThreeVector.hh"
+
+
+XrayTelAnalysis* XrayTelAnalysis::instance = 0;
+
+XrayTelAnalysis::XrayTelAnalysis()
+  : analysisFactory(0)
+  , tree(0)
+  , histoFactory(0)
+  , tupleFactory(0)
+#ifdef G4ANALYSIS_USE_PLOTTER
+  , plotterFactory(0)
+  , plotter(0)
+#endif
+{
+#ifdef G4ANALYSIS_USE
+  histFileName = "xraytel";
+  histFileType = "hbook";
+#endif
+}
+
+XrayTelAnalysis::~XrayTelAnalysis()
+{ 
+#ifdef G4ANALYSIS_USE
+
+#ifdef G4ANALYSIS_USE_PLOTTER
+  if (plotterFactory) delete plotterFactory;
+  plotterFactory = 0;
+#endif
+
+  if (tupleFactory) delete tupleFactory;
+  tupleFactory = 0;
+
+  if (histoFactory) delete histoFactory;
+  histoFactory = 0;
+
+  if (tree) delete tree;
+  tree = 0;
+
+  if (analysisFactory) delete analysisFactory;
+  analysisFactory = 0;
+#endif
+}
+
+XrayTelAnalysis* XrayTelAnalysis::getInstance()
+{
+#ifdef G4ANALYSIS_USE
+  if (instance == 0) instance = new XrayTelAnalysis;
+  return instance;
+#endif
+}
+
+
+void XrayTelAnalysis::book()
+{
+#ifdef G4ANALYSIS_USE
+  //build up  the  factories
+  analysisFactory = AIDA_createAnalysisFactory();
+  if(analysisFactory) {
+    //parameters for the TreeFactory
+    G4bool fileExists = false;
+    G4bool readOnly   = false;
+    AIDA::ITreeFactory* treeFactory = analysisFactory->createTreeFactory();
+    if(treeFactory) {
+      //  histoManager->selectStore("XrayTel.his");
+      G4String histFileNameComplete; 
+      histFileNameComplete = histFileName+".hbook";
+      tree = treeFactory->create(histFileNameComplete, "hbook", readOnly, fileExists);
+      G4cout << " Histogramfile: " << histFileNameComplete << G4endl;
+      
+      if (tree) {
+	G4cout << "Tree store : " << tree->storeName() << G4endl;
+	G4cout << "Booked Hbook File " << G4endl;
+
+	//HistoFactory and TupleFactory depend on theTree
+	histoFactory = analysisFactory->createHistogramFactory ( *tree );
+	tupleFactory = analysisFactory->createTupleFactory     ( *tree );
+
+	// Book histograms
+	histoFactory->createHistogram1D("1","Energy, all /keV",  100,0.,100.);
+	histoFactory->createHistogram2D("2","y-z, all /mm", 100,-500.,500.,100,-500.,500.);
+	histoFactory->createHistogram1D("3","Energy, entering detector /keV", 500,0.,500.);
+	histoFactory->createHistogram2D("4","y-z, entering detector /mm", 200,-50.,50.,200,-50.,50.);
+
+	// Divide the plot into 4 zones to show the 4 histograms during execution
+	//  plotter->zone(2,2,0,0);
+
+	// Book ntuples
+	AIDA::ITuple* ntuple10 = tupleFactory->create( "10", "Track ntuple", 
+						       "double energy,x,y,z,dirx,diry,dirz" );
+	assert(ntuple10);
+
+      }
+      delete treeFactory;
+    }
+  }
+#endif
+}
+
+void XrayTelAnalysis::finish()
+{
+#ifdef G4ANALYSIS_USE
+  if (tree) {
+    // Committing the transaction with the tree
+    G4std::cout << "Committing..." << G4std::endl;
+    // write all histograms to file
+    tree->commit();
+
+    G4std::cout << "Closing the tree..." << G4std::endl;
+
+    // close (will again commit)
+    tree->close();
+  }
+
+  // extra delete as objects are created in book() method rather than during
+  // initialisation of class
+  //   delete plotterFactory;
+   
+  if (tupleFactory)    delete tupleFactory;
+  if (histoFactory)    delete histoFactory;
+  if (tree)            delete tree;
+  if (analysisFactory) delete analysisFactory;
+#endif
+}
+
+void XrayTelAnalysis::analyseStepping(const G4Track& track, G4bool entering)
+{
+#ifdef G4ANALYSIS_USE
+  eKin = track.GetKineticEnergy()/keV;
+  G4ThreeVector pos = track.GetPosition()/mm;
+  y = pos.y();
+  z = pos.z();
+  G4ThreeVector dir = track.GetMomentumDirection();
+  dirX = dir.x();
+  dirY = dir.y();
+  dirZ = dir.z();
+
+  // Fill histograms, all tracks
+  AIDA::IHistogram1D* h1 = dynamic_cast<AIDA::IHistogram1D *> ( tree->find("1") );
+  h1->fill(eKin);  // fill(x,y,weight)
+  AIDA::IHistogram2D* h2 = dynamic_cast<AIDA::IHistogram2D *> ( tree->find("2") );
+  h2->fill(y,z);
+
+  // Fill histograms and ntuple, tracks entering the detector
+  if (entering) {
+    // Fill and plot histograms
+    AIDA::IHistogram1D* h3 = dynamic_cast<AIDA::IHistogram1D *> ( tree->find("3") );
+    h3->fill(eKin);
+    AIDA::IHistogram2D* h4 = dynamic_cast<AIDA::IHistogram2D *> ( tree->find("4") );
+    h4->fill(y,z);
+#ifdef G4ANALYSIS_USE_PLOTTER
+    plotAll();
+#endif
+  }
+
+  // Fill ntuple
+  if (entering) {
+    
+    AIDA::ITuple * ntuple = dynamic_cast<AIDA::ITuple *> ( tree->find("10") );
+    if (ntuple) {
+      // Fill the secondaries ntuple
+      ntuple->fill( ntuple->findColumn( "energy" ), (G4double) eKin );
+      ntuple->fill( ntuple->findColumn( "x"      ), (G4double) x    );
+      ntuple->fill( ntuple->findColumn( "y"      ), (G4double) y    );
+      ntuple->fill( ntuple->findColumn( "z"      ), (G4double) z    );
+      ntuple->fill( ntuple->findColumn( "dirx"   ), (G4double) dirX );
+      ntuple->fill( ntuple->findColumn( "diry"   ), (G4double) dirY );
+      ntuple->fill( ntuple->findColumn( "dirz"   ), (G4double) dirZ );
+
+      ntuple->addRow(); // check for returning true ...
+    } else {
+      G4cout << "Ntuple not found" << G4endl;
+    }
+  }
+
+#endif
+}
+
+#ifdef G4ANALYSIS_USE_PLOTTER
+void XrayTelAnalysis::plotAll()
+{
+  if (!plotter) {
+    AIDA::IPlotterFactory* plotterFactory = 
+      analysisFactory->createPlotterFactory();
+    if(plotterFactory) {
+      G4cout << "Creating the Plotter" << G4endl;
+      plotter = plotterFactory->create();
+      if(plotter) {
+	// Map the plotter on screen :
+	G4cout << "Showing the Plotter on screen" << G4endl;
+	plotter->show();
+	// Set the page title :
+	G4cout << "Setting the Plotter title" << G4endl;
+	plotter->setParameter("pageTitle","XrayTel");
+	// Have multiple plotting regions (two columns, two rows).
+	// G4cout << "Dividing the Plotter in multiple regions" << G4endl;
+	// plotter->createRegions(2,2); 
+      } else {
+	G4cout << "XrayTelAnalysis::plotAll: WARNING: Plotter not created" << G4endl;
+      }
+      delete plotterFactory;
+    } else {
+      G4cout << "XrayTelAnalysis::plotAll: WARNING: Plotter Factory not created" << G4endl;
+    }
+  }
+
+  if (plotter) {
+    AIDA::IHistogram1D* hp = dynamic_cast<AIDA::IHistogram1D *> ( tree->find("1") );
+    AIDA::IHistogram1D& h  = *hp;  
+    (plotter->currentRegion()).plot(h);
+    // plotter->next();
+    hp = dynamic_cast<AIDA::IHistogram1D *> ( tree->find("2") );
+    h  = *hp;
+    (plotter->currentRegion()).plot(h);
+    // plotter->next();
+    hp = dynamic_cast<AIDA::IHistogram1D *> ( tree->find("3") );
+    h  = *hp;
+    (plotter->currentRegion()).plot(h);
+    // plotter->next();
+    hp = dynamic_cast<AIDA::IHistogram1D *> ( tree->find("4") );
+    h  = *hp;
+    (plotter->currentRegion()).plot(h);
+  }
+}
+#endif
+
