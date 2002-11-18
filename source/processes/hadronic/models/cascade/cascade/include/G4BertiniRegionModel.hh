bc1d1b2245ea5bfc5c6371767ee663eef413f2c1
Aatos Heikkinen <miheikki@cern.ch>
2002-11-18 11:36:58 +0000

    Replased // with /// for Doxygen documentation.
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@12240 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/models/cascade/cascade/include/G4BertiniRegionModel.hh
+++ source/processes/hadronic/models/cascade/cascade/include/G4BertiniRegionModel.hh
@@ -11,24 +11,24 @@
 
 typedef G4std::vector<G4double>::const_iterator iterator; 
 
-class G4BertiniRegionModel //:public G4VRegionModel
+class G4BertiniRegionModel /// :public G4VRegionModel
 {
 public:
   G4BertiniRegionModel(const G4int numberOfLayers, const G4int A, const G4int Z);
   ~G4BertiniRegionModel();
 
-  //instead of A and Z outer radius of the nucleus?
-  //void Init(const G4int numberOfLayers, const G4int A, const G4int Z);
+  /// instead of A and Z outer radius of the nucleus?
+  /// void Init(const G4int numberOfLayers, const G4int A, const G4int Z);
   G4double GetDensity(G4double radius);
   G4double GetPotentialEnergy(G4double r, G4int particle);
   G4double GetMaximumNucleonMomentum(G4double radius, G4int nucleon);
-  // G4double NumberOfRegions();   
+  /// G4double NumberOfRegions();   
 
 private:
 
   G4int massNumber;
   G4int protonNumber;
-  G4std::vector<G4double> radius; //contains the outer radiuses of the shells
+  G4std::vector<G4double> radius; /// contains the outer radiuses of the shells
   G4std::vector<G4double> density;
   G4std::vector<G4double> protonFermiEnergy;
   G4std::vector<G4double> neutronFermiEnergy;
@@ -40,7 +40,7 @@ private:
 
   static const G4double radius0; 
   static const G4double BE;
-  //static const G4double pi = 3.141592;
+  /// static const G4double pi = 3.141592;
 
   G4double GetFermiMomentum(G4double density, G4double mass);
   G4double GetFermiEnergy(G4double density, G4double mass);
