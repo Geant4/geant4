d709d9a417593f5ea42c542cfc5180792782e032
Aatos Heikkinen <miheikki@cern.ch>
2002-11-21 19:51:04 +0000

    Timing pad added.
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@12423 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/models/cascade/test/data.C
+++ source/processes/hadronic/models/cascade/test/data.C
@@ -24,7 +24,11 @@ for (Int_t j = 1; j < 100; j++) {
 }
 
 c1 = new TCanvas("c1","A Simple Graph Example",200,10,700,500);
-
+c1->SetFillColor(0);
+c1->SetBorderMode(0);
+c1->Divide(2,2);
+  c1->cd(1);
+  gPad->SetBorderMode(0);
 c1->SetFillColor(11);
 c1->SetGrid();
 
@@ -52,7 +56,82 @@ gr->GetHistogram()->SetXTitle("GeV");
 gr->GetHistogram()->SetYTitle("mb");
 c1->Modified();
 
-//printf(" ::: Writing ps file ...\n");
+// ------------------------------------------
+Double_t t[5][5] = 
+{{0.5, 0.51, 2.37, 2.97, 4.83 },
+ {1.0, 0.38, 2.70, 3.87, 6.95 },
+ {2.0, 0.54, 3.02, 5.12, 10.50},
+ {4.0, 0.46, 3.78, 6.07, 14.91},
+ {8.0, 0.43, 4.12, 7.06, 18.95}};
+
+c1->cd(2);
+  gPad->SetBorderMode(0);
+// gPad->SetLogy();
+c1->SetFillColor(0);
+c1->SetGrid();
+
+const Int_t n = 5;
+Double_t x[n], tH[n], tAl[n], tFe[n], tPb[n];
+
+for (Int_t i=0; i < n; i++) {
+    x[i] = t[i][0];
+   tH[i] = t[i][1];
+  tAl[i] = t[i][2];
+  tFe[i] = t[i][3];
+  tPb[i] = t[i][4];
+  printf(" e =  %i %f %f \n", i, x[i], tH[i]);
+}
+
+
+gr0 = new TGraph(n, x, tPb);
+gr1 = new TGraph(n, x, tFe);
+gr2 = new TGraph(n, x, tAl);
+gr3 = new TGraph(n, x, tH);
+
+
+gr0->SetLineColor(1);
+gr0->SetLineWidth(1);
+
+gr1->SetLineColor(1);
+gr1->SetLineWidth(1);
+
+gr2->SetLineColor(1);
+gr2->SetLineWidth(1);
+
+gr3->SetLineColor(1);
+gr3->SetLineWidth(1);
+
+gr0->SetMarkerColor(16);
+gr1->SetMarkerColor(16);
+gr2->SetMarkerColor(16);
+gr3->SetMarkerColor(16);
+
+gr0->SetMarkerStyle(21);
+gr1->SetMarkerStyle(21);
+gr2->SetMarkerStyle(21);
+gr3->SetMarkerStyle(21);
+
+gr0->SetTitle("Simulation time of one INC");
+
+gr0->Draw("AC");
+gr1->Draw("C");
+gr2->Draw("C");
+gr3->Draw("C");
+TLatex l;
+l->DrawLatex(0.5,  19, "Geant4 INC model speed");
+l->DrawLatex(5,  18, "Pb_{82}^{208}");
+l->DrawLatex(5, 7.5, "Fe_{26}^{56}");
+l->DrawLatex(5, 4.5, "Al_{13}^{26}");
+
+c1->Update();
+c1->GetFrame()->SetFillColor(0);
+c1->GetFrame()->SetBorderSize(0);
+
+gr0->GetHistogram()->SetXTitle("Bullet particle kinetic energy [GeV]");
+gr0->GetHistogram()->SetYTitle("Timing for one INC [ms] ");
+
+c1->Modified();
+
 c1->Print("data.eps");
 
 printf(" ::: Done.\n");
