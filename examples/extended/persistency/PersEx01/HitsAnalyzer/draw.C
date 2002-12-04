3eb36ae39f58fa7e161120723424a30fe9b73441
Youhei Morita <morita@cern.ch>
2002-12-04 18:34:45 +0000

    comment out hist5
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@12709 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/persistency/PersEx01/HitsAnalyzer/draw.C
+++ examples/extended/persistency/PersEx01/HitsAnalyzer/draw.C
@@ -18,9 +18,9 @@
   hist4->SetMarkerStyle(21);
   hist4->SetMarkerColor(kGreen);
 
-  hist5->SetFillColor(kYellow);
-  hist5->SetMarkerStyle(21);
-  hist5->SetMarkerColor(kYellow);
+  // hist5->SetFillColor(kYellow);
+  // hist5->SetMarkerStyle(21);
+  // hist5->SetMarkerColor(kYellow);
 
   // hist6->SetFillColor(7);
   // hist6->SetMarkerStyle(21);
@@ -29,7 +29,7 @@
   // TCanvas *c1 = new TCanvas("c1","stacked hists",10,10,750,600);
   TCanvas *c1 = new TCanvas("c1","stacked hists",10,10,1000,800);
   c1->SetFillColor(41);
-  c1->Divide(3,2);
+  c1->Divide(2,2);
 
   // in top left pad, draw the stack with defaults
   c1->cd(1);
@@ -45,6 +45,6 @@
   c1->cd(4);
   hist4->Draw();
 
-  c1->cd(5);
-  hist5->Draw();
+  // c1->cd(5);
+  // hist5->Draw();
 }
