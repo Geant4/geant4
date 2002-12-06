ba4898ffe7a7d0e235ae3231f81557ff1c62e231
Mark Donszelmann <mark.donszelmann@cern.ch>
2002-12-06 17:02:22 +0000

    streamer was split from the g4 files in freehep so the copy commands have changed
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@12804 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/visualization/HepRep/copyfreehep.bat
+++ source/visualization/HepRep/copyfreehep.bat
@@ -1,7 +1,8 @@
 set HEPREP=%FREEHEP%\hep\graphics\heprep
 copy %HEPREP%\org\freehep\jni\FreeHepTypes.h include
-copy %HEPREP%\geant4\*.h include
-copy %HEPREP%\geant4\*.hh include
+copy %HEPREP%\geant4\include\*.hh include
+copy %HEPREP%\streamer-c++\*.h include
 copy %HEPREP%\include\HEPREP\*.h include\HEPREP
 copy %HEPREP%\geant4\*.cc src
+copy %HEPREP%\streamer-c++\*.cc src
 
