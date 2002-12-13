1798a972dbee6f90e1a5a768e73840b06bbf0744
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2002-12-13 09:06:26 +0000

    Correction after migration....
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@12966 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/management/include/G4HadLeadBias.hh
+++ source/processes/hadronic/management/include/G4HadLeadBias.hh
@@ -19,6 +19,14 @@
 // * based  on  the Program)  you indicate  your  acceptance of  this *
 // * statement, and all its terms.                                    *
 // ********************************************************************
+//
+// $Id: G4HadLeadBias.hh,v 1.3 2002-12-13 09:06:26 gcosmo Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+// --------------------------------------------------------------------
+#ifndef G4HadLeadBias_h
+#define G4HadLeadBias_h
+
 #include "G4VLeadingParticleBiasing.hh"
 #include "g4std/vector"
 #include "G4VParticleChange.hh"
--- source/processes/hadronic/management/include/G4VLeadingParticleBiasing.hh
+++ source/processes/hadronic/management/include/G4VLeadingParticleBiasing.hh
@@ -19,6 +19,14 @@
 // * based  on  the Program)  you indicate  your  acceptance of  this *
 // * statement, and all its terms.                                    *
 // ********************************************************************
+//
+// $Id: G4VLeadingParticleBiasing.hh,v 1.3 2002-12-13 09:06:26 gcosmo Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+// --------------------------------------------------------------------
+#ifndef G4VLeadingParticleBiasing_h
+#define G4VLeadingParticleBiasing_h
+
 class G4VParticleChange;
 
 class G4VLeadingParticleBiasing 
