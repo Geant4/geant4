085f3aa807ed5f943336012e7d3b2a3420836691
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2002-12-13 10:56:57 +0000

    Corrections after headers migration...
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@12973 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/models/cascade/cascade/include/G4BertiniData.hh
+++ source/processes/hadronic/models/cascade/cascade/include/G4BertiniData.hh
@@ -19,6 +19,9 @@
 // * based  on  the Program)  you indicate  your  acceptance of  this *
 // * statement, and all its terms.                                    *
 // ********************************************************************
+#ifndef G4BertiniData_h
+#define G4BertiniData_h 1
+
 #include "globals.hh"
 
 class G4BertiniData //: public G4VIntraNuclearTransportModel 
