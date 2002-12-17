e2a72e65d1de2904182d2f440ac37c3577c90808
Patricia Mendez Lorenzo <Patricia.Mendez@cern.ch>
2002-12-17 15:45:42 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@13052 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/advanced/lAr_calorimeter/src/FCALRunAction.cc
+++ examples/advanced/lAr_calorimeter/src/FCALRunAction.cc
@@ -23,7 +23,7 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: FCALRunAction.cc,v 1.2 2002-12-12 19:16:33 gunter Exp $
+// $Id: FCALRunAction.cc,v 1.3 2002-12-17 15:45:42 pmendez Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // 
@@ -31,12 +31,19 @@
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
 
+#ifdef G4ANALYSIS_USE
+
 #include "FCALRunAction.hh"
 
 #include "G4Run.hh"
+#include "G4RunManager.hh"
 #include "G4UImanager.hh"
 #include "G4VVisManager.hh"
 #include "G4ios.hh"
+
+#include "Randomize.hh"
+
+#include <AIDA/AIDA.h>
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
 
@@ -47,7 +54,66 @@ FCALRunAction::FCALRunAction()
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
 
 FCALRunAction::~FCALRunAction()
-{}
+{
+
+  cleanHisto();
+
+}
+
+
+void FCALRunAction::bookHisto(){
+
+  AIDA::IAnalysisFactory* af = AIDA_createAnalysisFactory();
+ 
+ // Creating the tree factory
+  AIDA::ITreeFactory* tf = af->createTreeFactory();
+
+ // Creating a tree mapped to an hbook file.
+  tree = tf->create("fcal.paw", "hbook", false, true);
+
+ // Creating a histogram factory, whose histograms will be handled by the tree
+  AIDA::IHistogramFactory* hf = af->createHistogramFactory(*tree);
+
+ // Creating a ntuple factory.
+
+  AIDA::ITupleFactory* nf = af->createTupleFactory(*tree);
+
+ // Creating ntuples
+
+  ntuple[1] = nf->create("100","Number Out of World","float OutOfWorld, i, j");
+   
+  ntuple[2] = nf->create("200","Secondary Info","float Secondary, i, j");
+  
+  ntuple[3] = nf->create("300","Energy Deposits","float EmEdep, HadEdep");
+
+  // Creating Histograms
+  
+  histo[1] = hf->createHistogram1D("1","Number of OutOfWorld",100, 0., 100.);
+  histo[2] = hf->createHistogram1D("2","Number of Secondaries",100, 0., 100.);
+  histo[3] = hf->createHistogram1D("3","Electromagnetic Energy / MeV",100, 0., 100.);
+  histo[4] = hf->createHistogram1D("4","Hadronic Energy / MeV",100, 0., 100.);
+
+  delete hf;
+  delete tf;
+  delete af;     
+  delete nf;
+ 
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void FCALRunAction::cleanHisto()
+{
+
+  tree->commit();       // Writing the histograms to the file
+  tree->close();        // and closing the tree (and the file)
+  
+  delete tree;
+ 
+
+}
+
+
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
 
@@ -55,6 +121,8 @@ void FCALRunAction::BeginOfRunAction(const G4Run* aRun)
 {
  
   G4cout << "### Run " << aRun->GetRunID() << " start." << G4endl;
+
+  if (aRun->GetRunID() == 0) bookHisto();
 
   if (G4VVisManager::GetConcreteInstance())
     {
@@ -72,4 +140,13 @@ void FCALRunAction::EndOfRunAction(const G4Run* )
   }
 }
 
+#endif
+
+
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+
+
+
+
+
