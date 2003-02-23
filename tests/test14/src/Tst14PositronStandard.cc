506e562a9e07f01aa66fa2f28c8e6c09715a731d
Maria Grazia Pia <Maria.Grazia.Pia@cern.ch>
2003-02-23 17:47:05 +0000

    Work for modular Physics List
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@13568 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ tests/test14/src/Tst14PositronStandard.cc
@@ -0,0 +1,70 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+//
+// $Id: Tst14PositronStandard.cc,v 1.1 2003-02-23 17:47:05 pia Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+// Author: Maria.Grazia.Pia@cern.ch
+//
+// History:
+// -----------
+// 22 Feb 2003 MGP          Designed for modular Physics List
+//
+// -------------------------------------------------------------------
+
+#include "Tst14ElectronStandard.hh"
+
+#include "G4ProcessManager.hh"
+#include "G4Gamma.hh"
+#include "G4ParticleDefinition.hh"
+#include "G4MultipleScattering.hh"
+#include "G4eIonisation.hh"
+#include "G4eBremsstrahlung.hh"
+#include "G4eplusAnnihilation.hh"
+
+Tst14ElectronStandard::Tst14ElectronStandard(const G4String& name): G4VPhysicsConstructor(name)
+{ }
+
+Tst14ElectronStandard::~Tst14ElectronStandard()
+{ }
+
+void Tst14ElectronStandard::ConstructProcess()
+{
+  // Add standard processes for electrons
+  
+  theParticleIterator->reset();
+
+  while( (*theParticleIterator)() )
+    {
+      G4ParticleDefinition* particle = theParticleIterator->value();
+      G4ProcessManager* manager = particle->GetProcessManager();
+      G4String particleName = particle->GetParticleName();
+     
+      if (particleName == "e+") 
+	{
+	  manager->AddProcess(new G4MultipleScattering, -1, 1,1);
+	  manager->AddProcess(new G4eIonisation,        -1, 2,2);
+	  manager->AddProcess(new G4eBremsstrahlung,    -1,-1,3);
+	  manager->AddProcess(new G4eplusAnnihilation,   0,-1,4);
+	}   
+    }
+}
