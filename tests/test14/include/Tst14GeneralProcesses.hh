776bb65a8d9a80679270c004a363bd2dd7fc78ac
Maria Grazia Pia <Maria.Grazia.Pia@cern.ch>
2003-02-23 10:42:43 +0000

    General processes for PhysicsList
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@13561 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ tests/test14/include/Tst14GeneralProcesses.hh
@@ -0,0 +1,69 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+//
+//
+// $Id: Tst14GeneralProcesses.hh,v 1.1 2003-02-23 10:42:43 pia Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+// Author: Maria Grazia Pia (Maria.Grazia.Pia@cern.ch)
+//
+// History:
+// -----------
+// 22 Feb 2003 MGP          Created
+//
+// -------------------------------------------------------------------
+
+// Class description:
+// System test for e/gamma, general processes for PhysicsList
+// Further documentation available from http://www.ge.infn.it/geant4/lowE
+
+// -------------------------------------------------------------------
+
+#ifndef TST14GENERALPROCESSES_HH
+#define TST14GENERALPROCESSES_HH 1
+
+#include "G4VPhysicsConstructor.hh"
+#include "globals.hh"
+
+class Tst14GeneralProcesses : public G4VPhysicsConstructor {
+
+public: 
+
+  Tst14GeneralProcesses(const G4String& name = "general");
+  
+  virtual ~Tst14GeneralProcesses();
+  
+  // This method is dummy for physics
+  virtual void ConstructParticle() {};
+  
+  virtual void ConstructProcess();
+};
+
+#endif
+
+
+
+
+
+
+
+
