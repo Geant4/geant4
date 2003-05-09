f40f4815a46b8f86f00bccf9a4e66f18cdfe8123
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2003-05-09 13:21:20 +0000

    Some fixes on pointers and cleanup...
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@14252 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/advanced/brachytherapy/src/BrachyDetectorConstructionIr.cc
+++ examples/advanced/brachytherapy/src/BrachyDetectorConstructionIr.cc
@@ -19,7 +19,6 @@
 // * based  on  the Program)  you indicate  your  acceptance of  this *
 // * statement, and all its terms.                                    *
 // ********************************************************************
-// ********************************************************************
 
 //
 // --------------------------------------------------------------
@@ -35,9 +34,10 @@
 //    *                                      *
 //    ****************************************
 //
-// $Id: BrachyDetectorConstructionIr.cc,v 1.3 2002-12-12 19:16:18 gunter Exp $
+// $Id: BrachyDetectorConstructionIr.cc,v 1.4 2003-05-09 13:21:20 gcosmo Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
+#include "globals.hh"
 #include "BrachyDetectorConstructionIr.hh"
 #include "G4CSGSolid.hh"
 #include "G4Sphere.hh"
@@ -52,7 +52,6 @@
 #include "G4LogicalVolume.hh"
 #include "G4ThreeVector.hh"
 #include "G4PVPlacement.hh"
-#include "globals.hh"
 #include "G4MaterialTable.hh"
 #include "Randomize.hh"  
 #include "G4RunManager.hh"
@@ -68,136 +67,86 @@
 #include "G4VisAttributes.hh"
 #include "G4Colour.hh"
 
-BrachyDetectorConstructionIr::BrachyDetectorConstructionIr():
-Capsule(0),CapsuleLog(0),CapsulePhys(0),
-CapsuleTip(0),CapsuleTipLog(0),CapsuleTipPhys(0),
-IridiumCore(0),IridiumCoreLog(0),IridiumCorePhys(0)
-
-{ 
-pMat= new BrachyMaterial() ;
-
+BrachyDetectorConstructionIr::BrachyDetectorConstructionIr()
+ : Capsule(0),CapsuleLog(0),CapsulePhys(0),
+   CapsuleTip(0),CapsuleTipLog(0),CapsuleTipPhys(0),
+   IridiumCore(0),IridiumCoreLog(0),IridiumCorePhys(0)
+{
+  pMat= new BrachyMaterial();
 }
 
-
 //....
-
 BrachyDetectorConstructionIr::~BrachyDetectorConstructionIr()
 { 
-
-delete pMat; 
+  delete pMat; 
 }
 
 //....
 void BrachyDetectorConstructionIr::ConstructIridium(G4VPhysicalVolume* mother)
 {
+  G4Colour  red     (1.0, 0.0, 0.0) ;
+  G4Colour  magenta (1.0, 0.0, 1.0) ; 
 
-G4Colour  white   (1.0, 1.0, 1.0) ;
-G4Colour  grey    (0.5, 0.5, 0.5) ;
-G4Colour  lgrey   (.75, .75, .75) ;
-G4Colour  red     (1.0, 0.0, 0.0) ;
-G4Colour  blue    (0.0, 0.0, 1.0) ;
-G4Colour  cyan    (0.0, 1.0, 1.0) ;
-G4Colour  magenta (1.0, 0.0, 1.0) ; 
-G4Colour  yellow  (1.0, 1.0, 0.0) ;
-G4Colour  lblue   (0.0, 0.0, .75);
+  G4Material* CapsuleMat=pMat->GetMat("Stainless steel");
+  G4Material*IridiumMat=pMat->GetMat("Iridium");
 
-G4Material* CapsuleMat=pMat->GetMat("Stainless steel");
-G4Material*IridiumMat=pMat->GetMat("Iridium");
+  // Capsule main body
 
-// Capsule main body
+  Capsule =
+     new G4Tubs("Capsule",0,0.55*mm,3.725*mm,0.*deg,360.*deg);
+  CapsuleLog =
+     new G4LogicalVolume(Capsule,CapsuleMat,"CapsuleLog");
+  CapsulePhys =
+     new G4PVPlacement(0,G4ThreeVector(0,0,-1.975),"CapsulePhys",CapsuleLog,mother,false,0);
 
+  // Capsule tip
 
-G4Tubs* Capsule = new G4Tubs("Capsule",0,0.55*mm,3.725*mm,0.*deg,360.*deg);
-G4LogicalVolume* CapsuleLog = new G4LogicalVolume(Capsule,CapsuleMat,"CapsuleLog");
-G4VPhysicalVolume* CapsulePhys = new G4PVPlacement
-      (0,G4ThreeVector(0,0,-1.975),"CapsulePhys",CapsuleLog,mother,false,0);
+  CapsuleTip =
+     new G4Sphere("CapsuleTipIridium",0.*mm,0.55*mm,0.*deg,360.*deg,0.*deg,90.*deg); 
+  CapsuleTipLog =
+     new G4LogicalVolume(CapsuleTip,CapsuleMat,"CapsuleTipIridumLog");
+  CapsuleTipPhys =
+     new G4PVPlacement(0,G4ThreeVector(0.,0.,1.75*mm),"CapsuleTipIridiumPhys",CapsuleTipLog,mother,false,0);
 
-// Capsule tip
+  // Iridium core
 
-G4Sphere* CapsuleTip = new G4Sphere("CapsuleTipIridium",0.*mm,0.55*mm,0.*deg,360.*deg,0.*deg,90.*deg); 
+  IridiumCore =
+     new G4Tubs("IrCore",0,0.30*mm,1.75*mm,0.*deg,360.*deg);
+  IridiumCoreLog =
+     new G4LogicalVolume(IridiumCore,IridiumMat,"IridiumCoreLog");
+  IridiumCorePhys =
+     new G4PVPlacement(0,G4ThreeVector(),"IridiumCorePhys",IridiumCoreLog,CapsulePhys,false,0);
 
-G4LogicalVolume* CapsuleTipLog = new G4LogicalVolume(CapsuleTip,CapsuleMat,"CapsuleTipIridumLog");
-G4VPhysicalVolume* CapsuleTipPhys = new G4PVPlacement(0,G4ThreeVector(0.,0.,1.75*mm),"CapsuleTipIridiumPhys",CapsuleTipLog,mother,false,0);
+  simpleCapsuleVisAtt= new G4VisAttributes(red);
+  simpleCapsuleVisAtt->SetVisibility(true);  
+  simpleCapsuleVisAtt->SetForceWireframe(true);
+  CapsuleLog->SetVisAttributes(simpleCapsuleVisAtt);
 
-// Iridium core
+  simpleCapsuleTipVisAtt= new G4VisAttributes(red);
+  simpleCapsuleTipVisAtt->SetVisibility(true);  
+  simpleCapsuleTipVisAtt->SetForceSolid(true);
+  CapsuleTipLog->SetVisAttributes(simpleCapsuleTipVisAtt);
 
-G4Tubs* IridiumCore = new G4Tubs("IrCore",0,0.30*mm,1.75*mm,0.*deg,360.*deg);
-G4LogicalVolume* IridiumCoreLog = new G4LogicalVolume(IridiumCore,IridiumMat,"IridiumCoreLog");
-G4VPhysicalVolume* IridiumCorePhys = new G4PVPlacement(0,G4ThreeVector(),"IridiumCorePhys",IridiumCoreLog,CapsulePhys,false,0);
-
-simpleIridiumVisAtt= new G4VisAttributes(magenta);
-simpleIridiumVisAtt->SetVisibility(true);
-simpleIridiumVisAtt->SetForceWireframe(true);
-IridiumCoreLog->SetVisAttributes(simpleIridiumVisAtt);
-
-simpleCapsuleVisAtt= new G4VisAttributes(red);
-simpleCapsuleVisAtt->SetVisibility(true);  
-simpleCapsuleVisAtt->SetForceWireframe(true);
-CapsuleLog->SetVisAttributes( simpleCapsuleVisAtt);
-
-simpleCapsuleTipVisAtt= new G4VisAttributes(red);
-simpleCapsuleTipVisAtt->SetVisibility(true);  
-simpleCapsuleTipVisAtt->SetForceSolid(true);
-CapsuleTipLog->SetVisAttributes( simpleCapsuleTipVisAtt);
+  simpleIridiumVisAtt= new G4VisAttributes(magenta);
+  simpleIridiumVisAtt->SetVisibility(true);
+  simpleIridiumVisAtt->SetForceWireframe(true);
+  IridiumCoreLog->SetVisAttributes(simpleIridiumVisAtt);
 }
 
 void BrachyDetectorConstructionIr::CleanIridium()
 { 
-G4VPhysicalVolume* toDieCapsule =  CapsulePhys; 
-G4VPhysicalVolume* toDieTip =CapsuleTipPhys;
-G4VPhysicalVolume* toDieCore = IridiumCorePhys;
+  delete simpleIridiumVisAtt; simpleIridiumVisAtt=0;
+  delete IridiumCorePhys; IridiumCorePhys=0;
+  delete IridiumCore; IridiumCore=0;
+  delete IridiumCoreLog; IridiumCoreLog=0 ;
 
+  delete simpleCapsuleTipVisAtt; simpleCapsuleTipVisAtt=0;
+  delete CapsuleTipPhys; CapsuleTipPhys=0;
+  delete CapsuleTip; CapsuleTip=0;
+  delete CapsuleTipLog; CapsuleTipLog=0;
 
-
-if ( toDieTip != 0 ){delete toDieTip;}
-if ( toDieCore !=0){delete toDieCore;}
-if(toDieCapsule !=0){delete toDieCapsule;}
-
-if(CapsuleTipLog !=0)
-{G4VSolid* solToDieTip =CapsuleTipLog->GetSolid();
-if (solToDieTip  !=0)
- {delete solToDieTip;}
-delete CapsuleTipLog;
-CapsuleTipLog=0; }
-
-if( CapsuleLog !=0)
-{G4VSolid* solToDieCapsule =CapsuleLog->GetSolid();
-if (solToDieCapsule  !=0)
- {delete solToDieCapsule;}
-delete CapsuleLog; 
-CapsuleLog=0;}
-
-if (IridiumCoreLog !=0)
-{G4VSolid* solToDieCore =IridiumCoreLog->GetSolid();
-if (solToDieCore  !=0)
- {delete solToDieCore;}
-delete IridiumCoreLog;
-IridiumCoreLog=0 ;}
-
-if( simpleIridiumVisAtt !=0){delete simpleIridiumVisAtt;}
-if (simpleCapsuleVisAtt !=0){ delete simpleCapsuleVisAtt;}
-if  (simpleCapsuleTipVisAtt !=0) {delete simpleCapsuleTipVisAtt;}
-
+  delete simpleCapsuleVisAtt; simpleCapsuleVisAtt=0;
+  delete CapsulePhys; CapsulePhys=0;
+  delete Capsule; Capsule=0;
+  delete CapsuleLog; CapsuleLog=0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
