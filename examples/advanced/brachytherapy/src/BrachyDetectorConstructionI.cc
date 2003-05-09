58a38f44a18cbf9fbf7a7b36c05c57c00d14140b
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2003-05-09 17:36:41 +0000

    General cleanup...
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@14258 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/advanced/brachytherapy/src/BrachyDetectorConstructionI.cc
+++ examples/advanced/brachytherapy/src/BrachyDetectorConstructionI.cc
@@ -33,9 +33,10 @@
 //    *                                      *
 //    ****************************************
 //
-// $Id: BrachyDetectorConstructionI.cc,v 1.3 2003-05-09 16:52:07 gcosmo Exp $
+// $Id: BrachyDetectorConstructionI.cc,v 1.4 2003-05-09 17:36:41 gcosmo Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
+#include "globals.hh"
 #include "BrachyDetectorConstructionI.hh"
 #include "G4CSGSolid.hh"
 #include "G4Sphere.hh"
@@ -50,7 +51,6 @@
 #include "G4LogicalVolume.hh"
 #include "G4ThreeVector.hh"
 #include "G4PVPlacement.hh"
-#include "globals.hh"
 #include "G4MaterialTable.hh"
 #include "Randomize.hh"  
 #include "G4RunManager.hh"
@@ -66,128 +66,113 @@
 #include "G4VisAttributes.hh"
 #include "G4Colour.hh"
 
-BrachyDetectorConstructionI::BrachyDetectorConstructionI():
-DefaultTub(0), DefaultTub_log(0),DefaultTub_Phys(0),
-Capsule(0),CapsuleLog(0),CapsulePhys(0),
-CapsuleTip(0),CapsuleTipLog(0),CapsuleTipPhys1(0),CapsuleTipPhys2(0),
-IodiumCore(0),ICoreLog(0),ICorePhys(0),
-Marker(0),MarkerLog(0),MarkerPhys(0)
-
+BrachyDetectorConstructionI::BrachyDetectorConstructionI()
+: DefaultTub(0), DefaultTub_log(0),DefaultTub_Phys(0),
+  Capsule(0),CapsuleLog(0),CapsulePhys(0),
+  CapsuleTip(0),CapsuleTipLog(0),CapsuleTipPhys1(0),CapsuleTipPhys2(0),
+  IodiumCore(0),ICoreLog(0),ICorePhys(0),
+  Marker(0),MarkerLog(0),MarkerPhys(0)
 { 
-pMat= new BrachyMaterial() ;
-
+  pMat= new BrachyMaterial();
 }
 
-
 //....
-
 BrachyDetectorConstructionI::~BrachyDetectorConstructionI()
 { 
-
-delete pMat; 
+  delete pMat; 
 }
 
 //....
 void BrachyDetectorConstructionI::ConstructIodium(G4VPhysicalVolume* mother)
 {
-// Volumes
+  // Volumes
 
-G4cout << "Creation of Iodium ..." << G4endl;
-G4cout << "# of daughters of world - "
-       << mother->GetMother()->GetLogicalVolume()->GetNoDaughters() << G4endl;
-G4cout << "# of daughters of phantom - "
-       << mother->GetLogicalVolume()->GetNoDaughters() << G4endl;
+  G4cout << "Creation of Iodium ..." << G4endl;
+  G4cout << "# of daughters of world - "
+         << mother->GetMother()->GetLogicalVolume()->GetNoDaughters() << G4endl;
+  G4cout << "# of daughters of phantom - "
+         << mother->GetLogicalVolume()->GetNoDaughters() << G4endl;
 
-G4Material* titanium=pMat->GetMat("titanium");
-G4Material* air =pMat->GetMat("Air");
-G4Material* Iodio=pMat->GetMat("Iodium");
-G4Material* Gold=pMat->GetMat("gold");
+  G4Material* titanium=pMat->GetMat("titanium");
+  G4Material* air =pMat->GetMat("Air");
+  G4Material* Iodio=pMat->GetMat("Iodium");
+  G4Material* Gold=pMat->GetMat("gold");
 
-G4Colour  red     (1.0, 0.0, 0.0) ;
-G4Colour  magenta (1.0, 0.0, 1.0) ; 
-G4Colour  lblue   (0.0, 0.0, .75);
+  G4Colour  red     (1.0, 0.0, 0.0) ;
+  G4Colour  magenta (1.0, 0.0, 1.0) ; 
+  G4Colour  lblue   (0.0, 0.0, .75);
 
-// Rotation Matrix
-G4RotationMatrix*  rotateMatrix=new G4RotationMatrix(); //=new G4RotationMatrix() ;
-rotateMatrix->rotateX(180.0*deg);
+  DefaultTub = new G4Tubs("DefaultTub",0.*mm,0.40*mm,1.84*mm,0.*deg,360.*deg);
+  DefaultTub_log = new G4LogicalVolume(DefaultTub,air,"DefaultTub_Log");
+  DefaultTub_Phys = new G4PVPlacement(0,G4ThreeVector(),"DefaultTub_Phys",
+                                      DefaultTub_log,mother,false,0); 
 
-DefaultTub=new G4Tubs("DefaultTub",
-			   0.*mm,
-			   0.40*mm,
-			   1.84*mm,
-			   0.*deg,
-			   360.*deg);
-DefaultTub_log = new G4LogicalVolume(DefaultTub,air,"DefaultTub_Log");
-DefaultTub_Phys = new G4PVPlacement(0,G4ThreeVector(),"DefaultTub_Phys",DefaultTub_log,
-		     mother,false,0); 
+  G4cout << "Created DefaultTub_Phys" << G4endl;
+  G4cout << "# of daughters - " << mother->GetLogicalVolume()->GetNoDaughters() << G4endl;
 
-G4cout << "Created DefaultTub_Phys" << G4endl;
-G4cout << "# of daughters - " << mother->GetLogicalVolume()->GetNoDaughters() << G4endl;
+  //  Capsule main body
 
-//  Capsule main body
-G4double CapsuleR=0.35*mm;
-Capsule = new G4Tubs("Capsule",
-			  CapsuleR,//raggio interno
-			  0.40*mm,//raggio esterno
-			  1.84*mm,//meta' lunghezza 
-			  0.*deg,//angolo di partenza
-			  360.*deg);//angolo di rotazione
+  G4double CapsuleR=0.35*mm;
+  Capsule = new G4Tubs("Capsule", CapsuleR,0.40*mm,1.84*mm,0.*deg,360.*deg);
+  CapsuleLog = new G4LogicalVolume(Capsule,titanium,"CapsuleLog");
+  CapsulePhys = new G4PVPlacement(0,G4ThreeVector(),"CapsulePhys",
+                                  CapsuleLog,DefaultTub_Phys,false,0);
 
-CapsuleLog = new G4LogicalVolume(Capsule,titanium,"CapsuleLog");
-CapsulePhys = new G4PVPlacement
-		    (0,G4ThreeVector(),"CapsulePhys",CapsuleLog,
-		    DefaultTub_Phys,false,0);
+  G4cout << "Created CapsulePhys" << G4endl;
+  G4cout << "# of daughters - " << mother->GetLogicalVolume()->GetNoDaughters() << G4endl;
 
-G4cout << "Created CapsulePhys" << G4endl;
-G4cout << "# of daughters - " << mother->GetLogicalVolume()->GetNoDaughters() << G4endl;
+  // Capsule tips
 
-// Capsule tip
+  CapsuleTip = new G4Sphere("CapsuleTip",0.*mm,0.40*mm,0.*deg,360.*deg,0.*deg,90.*deg);
+  CapsuleTipLog = new G4LogicalVolume(CapsuleTip,titanium,"CapsuleTipLog");
+  CapsuleTipPhys1 = new G4PVPlacement(0,G4ThreeVector(0.,0.,1.84*mm),
+                                      "CapsuleTipPhys1",CapsuleTipLog,mother,false,0);
 
-CapsuleTip = new G4Sphere("CapsuleTip",0.*mm,0.40*mm,0.*deg,360.*deg,0.*deg,90.*deg);
-CapsuleTipLog = new G4LogicalVolume(CapsuleTip,titanium,"CapsuleTipLog");
-CapsuleTipPhys1 = new G4PVPlacement
-	  (0,G4ThreeVector(0.,0.,1.84*mm), "CapsuleTipPhys1",CapsuleTipLog,mother,false,0);
+  G4cout << "Created CapsuleTipPhys1" << G4endl;
+  G4cout << "# of daughters - " << mother->GetLogicalVolume()->GetNoDaughters() << G4endl;
 
-G4cout << "Created CapsuleTipPhys1" << G4endl;
-G4cout << "# of daughters - " << mother->GetLogicalVolume()->GetNoDaughters() << G4endl;
+  // Rotated tip
 
-CapsuleTipPhys2 = new G4PVPlacement(rotateMatrix, G4ThreeVector(0,0,-1.84*mm),"CapsuleTipPhys2",CapsuleTipLog,mother,false,0);
+  G4RotationMatrix* rotateMatrix = new G4RotationMatrix();
+  rotateMatrix->rotateX(180.0*deg);
+  CapsuleTipPhys2 = new G4PVPlacement(rotateMatrix, G4ThreeVector(0,0,-1.84*mm),
+                                      "CapsuleTipPhys2",CapsuleTipLog,mother,false,0);
 
-G4cout << "Created CapsuleTipPhys2" << G4endl;
-G4cout << "# of daughters - " << mother->GetLogicalVolume()->GetNoDaughters() << G4endl;
+  G4cout << "Created CapsuleTipPhys2" << G4endl;
+  G4cout << "# of daughters - " << mother->GetLogicalVolume()->GetNoDaughters() << G4endl;
 
-IodiumCore = new G4Tubs("ICore",0.085*mm,0.35*mm,1.75*mm,0.*deg,360.*deg);
-ICoreLog = new G4LogicalVolume(IodiumCore,Iodio,"IodiumCoreLog");
-ICorePhys = new G4PVPlacement(0,G4ThreeVector(0.,0.,0.),
-		    "IodiumCorePhys",ICoreLog,DefaultTub_Phys,false,0);
+  IodiumCore = new G4Tubs("ICore",0.085*mm,0.35*mm,1.75*mm,0.*deg,360.*deg);
+  ICoreLog = new G4LogicalVolume(IodiumCore,Iodio,"IodiumCoreLog");
+  ICorePhys = new G4PVPlacement(0,G4ThreeVector(0.,0.,0.),
+                                "IodiumCorePhys",ICoreLog,DefaultTub_Phys,false,0);
 
-G4cout << "Created ICorePhys" << G4endl;
-G4cout << "# of daughters - " << mother->GetLogicalVolume()->GetNoDaughters() << G4endl;
+  G4cout << "Created ICorePhys" << G4endl;
+  G4cout << "# of daughters - " << mother->GetLogicalVolume()->GetNoDaughters() << G4endl;
 
-Marker=new G4Tubs("GoldenMarker",0.*mm,0.085*mm,1.75*mm,0.*deg,360.*deg);
-MarkerLog = new G4LogicalVolume(Marker,Gold,"MarkerLog");
-MarkerPhys=new G4PVPlacement(0,G4ThreeVector(0.,0.,0.),"MarkerPhys",MarkerLog,DefaultTub_Phys,false,0);
+  Marker=new G4Tubs("GoldenMarker",0.*mm,0.085*mm,1.75*mm,0.*deg,360.*deg);
+  MarkerLog = new G4LogicalVolume(Marker,Gold,"MarkerLog");
+  MarkerPhys=new G4PVPlacement(0,G4ThreeVector(0.,0.,0.),
+                               "MarkerPhys",MarkerLog,DefaultTub_Phys,false,0);
 
-G4cout << "Created MarkerPhys" << G4endl;
-G4cout << "# of daughters - " << mother->GetLogicalVolume()->GetNoDaughters() << G4endl;
+  G4cout << "Created MarkerPhys" << G4endl;
+  G4cout << "# of daughters - " << mother->GetLogicalVolume()->GetNoDaughters() << G4endl;
 
+  G4VisAttributes* simpleMarkerVisAtt= new G4VisAttributes(lblue);
+  simpleMarkerVisAtt->SetVisibility(true);
+  simpleMarkerVisAtt->SetForceSolid(true);
 
-G4VisAttributes* simpleMarkerVisAtt= new G4VisAttributes(lblue);
-simpleMarkerVisAtt->SetVisibility(true);
-simpleMarkerVisAtt->SetForceSolid(true);
+  G4VisAttributes* simpleIodiumVisAtt= new G4VisAttributes(magenta);
+  simpleIodiumVisAtt->SetVisibility(true);
+  simpleIodiumVisAtt->SetForceWireframe(true);
+  ICoreLog->SetVisAttributes(simpleIodiumVisAtt);
 
-G4VisAttributes* simpleIodiumVisAtt= new G4VisAttributes(magenta);
-simpleIodiumVisAtt->SetVisibility(true);
-simpleIodiumVisAtt->SetForceWireframe(true);
-ICoreLog->SetVisAttributes(simpleIodiumVisAtt);
+  G4VisAttributes* simpleCapsuleVisAtt= new G4VisAttributes(red);
+  simpleCapsuleVisAtt->SetVisibility(true);  
+  simpleCapsuleVisAtt->SetForceWireframe(true);
+  CapsuleLog->SetVisAttributes( simpleCapsuleVisAtt);
 
-G4VisAttributes* simpleCapsuleVisAtt= new G4VisAttributes(red);
-simpleCapsuleVisAtt->SetVisibility(true);  
-simpleCapsuleVisAtt->SetForceWireframe(true);
-CapsuleLog->SetVisAttributes( simpleCapsuleVisAtt);
-
-G4VisAttributes* simpleCapsuleTipVisAtt= new G4VisAttributes(red);
-simpleCapsuleTipVisAtt->SetVisibility(true); 
-simpleCapsuleTipVisAtt->SetForceSolid(true);
-CapsuleTipLog->SetVisAttributes( simpleCapsuleTipVisAtt);
+  G4VisAttributes* simpleCapsuleTipVisAtt= new G4VisAttributes(red);
+  simpleCapsuleTipVisAtt->SetVisibility(true); 
+  simpleCapsuleTipVisAtt->SetForceSolid(true);
+  CapsuleTipLog->SetVisAttributes( simpleCapsuleTipVisAtt);
 }
