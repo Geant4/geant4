4ce63b109bca68327af2e575cfeeda65c5620544
Vladimir Ivantchenko <Vladimir.Ivantchenko@cern.ch>
2003-06-03 10:42:04 +0000

    fix warnings
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@14676 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/electromagnetic/TestEm4/src/Em4RunAction.cc
+++ examples/extended/electromagnetic/TestEm4/src/Em4RunAction.cc
@@ -21,7 +21,7 @@
 // ********************************************************************
 //
 //
-// $Id: Em4RunAction.cc,v 1.16 2003-06-02 18:24:54 vnivanch Exp $
+// $Id: Em4RunAction.cc,v 1.17 2003-06-03 10:42:04 vnivanch Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // 
@@ -81,22 +81,22 @@ void Em4RunAction::bookHisto()
 
  // Creating a histogram factory, whose histograms will be handled by the tree
  AIDA::IHistogramFactory* hf = af->createHistogramFactory(*tree);
- 
+
  // Creating the histogram
  histo[0]=hf->createHistogram1D("1","total energy deposit in C6F6(MeV)",100,0.,10.);
- 
+
  delete hf;
  delete tf;
  delete af;
-#endif      
+#endif
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 void Em4RunAction::BeginOfRunAction(const G4Run* aRun)
-{  
+{
   G4cout << "### Run " << aRun->GetRunID() << " start." << G4endl;
-  
+
   // save Rndm status
   //  G4RunManager::GetRunManager()->SetRandomNumberStore(true);
   HepRandom::showEngineStatus();
@@ -107,8 +107,8 @@ void Em4RunAction::BeginOfRunAction(const G4Run* aRun)
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-void Em4RunAction::EndOfRunAction(const G4Run* aRun)
-{  
+void Em4RunAction::EndOfRunAction(const G4Run*)
+{
   if (G4VVisManager::GetConcreteInstance())
     G4UImanager::GetUIpointer()->ApplyCommand("/vis/viewer/update");
 
