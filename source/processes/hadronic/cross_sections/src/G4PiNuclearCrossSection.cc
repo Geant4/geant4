49f42129f44625813c2391d6666432c47d0538ab
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2003-06-25 13:56:06 +0000

    Fixed usage of arguments to pow for porting on .NET compiler
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@15152 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/cross_sections/src/G4PiNuclearCrossSection.cc
+++ source/processes/hadronic/cross_sections/src/G4PiNuclearCrossSection.cc
@@ -236,8 +236,8 @@ G4double G4PiNuclearCrossSection::
 Interpolate(G4int Z1, G4int Z2, G4int Z, G4double x1, G4double x2)
 {
   G4double result = 0;
-  G4double r1 = x1/pow(Z1, 2./3.)*pow(Z, 2./3.);
-  G4double r2 = x2/pow(Z2, 2./3.)*pow(Z, 2./3.);
+  G4double r1 = x1/pow(G4double(Z1), 2./3.)*pow(G4double(Z), 2./3.);
+  G4double r2 = x2/pow(G4double(Z2), 2./3.)*pow(G4double(Z), 2./3.);
   result = (r1+r2)/2.;
   return result;
 }
