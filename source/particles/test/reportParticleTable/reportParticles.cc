0a1830bf1102c18c97fcaef870e00776dcfaf036
Hisaya Kurashige <kurasige@phys.sci.kobe-u.ac.jp>
2003-09-21 19:38:04 +0000

    Modify for new scheme by Hisaya
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@15673 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/particles/test/reportParticleTable/reportParticles.cc
+++ source/particles/test/reportParticleTable/reportParticles.cc
@@ -21,7 +21,7 @@
 // ********************************************************************
 //
 //
-// $Id: reportParticles.cc,v 1.7 2003-06-16 16:58:52 gunter Exp $
+// $Id: reportParticles.cc,v 1.8 2003-09-21 19:38:03 kurasige Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // 
@@ -29,12 +29,13 @@
 #include "G4ios.hh"
 #include "globals.hh"
 #include "tst2ParticleConstructor.hh"
-#include "tst2ParticleContainer.hh"
-#include "tst2SimpleReporter.hh"
-#include "tst2HtmlReporter.hh"
+#include "G4ParticlePropertyTable.hh"
+#include "G4SimplePPReporter.hh"
+#include "G4HtmlPPReporter.hh"
 #include <fstream>
 #include <iomanip>
 
+#define USE_HTML_REPORTER 1
 
 int main(int argc,char** argv) {
   // create all particles
@@ -42,17 +43,28 @@ int main(int argc,char** argv) {
   pConstructor.ConstructParticle();
 
   // particleContainer 
-  tst2ParticleContainer* leptonContainer = new tst2ParticleContainer();
-  tst2ParticleContainer* mesonContainer = new tst2ParticleContainer();
-  tst2ParticleContainer* baryonContainer = new tst2ParticleContainer();
-  tst2ParticleContainer* ionContainer = new tst2ParticleContainer();
-  tst2ParticleContainer* quarkContainer = new tst2ParticleContainer();
-  tst2ParticleContainer* otherContainer = new tst2ParticleContainer();
-
+  G4VParticlePropertyReporter* aPPR;
+#ifdef USE_HTML_REPORTER
+  G4VParticlePropertyReporter* pLepton = new G4HtmlPPReporter();
+  G4VParticlePropertyReporter* pMeson  = new G4HtmlPPReporter();
+  G4VParticlePropertyReporter* pBaryon = new G4HtmlPPReporter();
+  G4VParticlePropertyReporter* pIon    = new G4HtmlPPReporter();
+  G4VParticlePropertyReporter* pQuark  = new G4HtmlPPReporter();
+  G4VParticlePropertyReporter* pOther  = new G4HtmlPPReporter();
+#else
+  G4VParticlePropertyReporter* pLepton = new G4SimplePPReporter();
+  G4VParticlePropertyReporter* pMeson  = new G4SimplePPReporter();
+  G4VParticlePropertyReporter* pBaryon = new G4SimplePPReporter();
+  G4VParticlePropertyReporter* pIon    = new G4SimplePPReporter();
+  G4VParticlePropertyReporter* pQuark  = new G4SimplePPReporter();
+  G4VParticlePropertyReporter* pOther  = new G4SimplePPReporter();
+#endif
   // pointer to the particle table
   G4ParticleTable* theParticleTable = G4ParticleTable::GetParticleTable();
   G4ParticleTable::G4PTblDicIterator* theParticleIterator;
   theParticleIterator = theParticleTable->GetIterator();
+
+  G4ParticlePropertyTable* thePPTable = G4ParticlePropertyTable::GetParticlePropertyTable();
 
   // loop over all particles in G4ParticleTable 
   theParticleIterator->reset();
@@ -60,35 +72,32 @@ int main(int argc,char** argv) {
     G4ParticleDefinition* particle = theParticleIterator->value();
     G4String type = particle->GetParticleType();
     if  (type=="lepton") {
-      leptonContainer->Insert(particle); 
+      aPPR = pLepton;
     } else  if  (type=="meson") {
-      mesonContainer->Insert(particle); 
-    } else  if  (type=="baryon") {
-      baryonContainer->Insert(particle); 
-    } else  if  (type=="nucleus") {
-      ionContainer->Insert(particle); 
-    } else  if  ((type=="quarks")||(type=="diquarks")||(type=="gluons")) {
-      quarkContainer->Insert(particle); 
-    } else    {
-      otherContainer->Insert(particle); 
-    } 
-  }
+      aPPR = pMeson;
 
-  // tst2SimpleReporter reporter;
-  //reporter.Print(*quarkContainer);
-  //reporter.Print(*leptonContainer);
-  //reporter.Print(*mesonContainer);
-  //reporter.Print(*baryonContainer);
-  //reporter.Print(*ionContainer);
-  //reporter.Print(*otherContainer);
-  
-  tst2HtmlReporter html;
-  html.Print( *quarkContainer,  "quarks  20/Nov/02");
-  html.Print( *leptonContainer, "leptons 20/Nov/02");
-  html.Print( *mesonContainer,  "mesons 20/Nov/02");
-  html.Print( *baryonContainer, "baryons 20/Nov/02");
-  html.Print( *ionContainer,    "ions 20/Nov/02");
-  html.Print( *otherContainer,  "others 20/Nov/02");
+    } else  if  (type=="baryon") {
+      aPPR = pBaryon;
+
+    } else  if  (type=="nucleus") {
+      aPPR = pIon;
+
+    } else  if  ((type=="quarks")||(type=="diquarks")||(type=="gluons")) {
+      aPPR = pQuark;
+
+    } else    {
+      aPPR = pOther;
+    } 
+    // add property data to PPDcontainer
+    aPPR->FillList(particle->GetParticleName()); 
+}
+  pLepton->Print("leptons");
+  pMeson->Print("mesons");
+  pBaryon->Print("baryons");
+  pIon->Print("ions");
+  pQuark->Print("quarks");
+  pOther->Print("others");
+
   return EXIT_SUCCESS;
 }
 
