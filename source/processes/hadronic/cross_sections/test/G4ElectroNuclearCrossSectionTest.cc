ddb97e2b353d390702223a532cff86ffebc9a7b2
Mikhail Kossov <mkossov@cern.ch>
2003-09-29 15:37:21 +0000

    bug in high energy electronuclear interactions is fixed and the result is tested
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@15740 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/cross_sections/test/G4ElectroNuclearCrossSectionTest.cc
+++ source/processes/hadronic/cross_sections/test/G4ElectroNuclearCrossSectionTest.cc
@@ -20,6 +20,9 @@
 // * statement, and all its terms.                                    *
 // ********************************************************************
 //
+#define debug
+#define qedebug
+
 #include "G4ios.hh"
 #include "G4Electron.hh"
 #include "G4Element.hh"
@@ -30,18 +33,21 @@ int main()
   G4ElectroNuclearCrossSection eACrossSection;
   const G4int nN=6;                          // A#of plots (nuclei in the test)
   const G4int nC=227;                        // A#of points in the plots (energy values)
-  ofstream fileH1("h1_e.out", ios::out);
-  fileH1.setf( ios::scientific, ios::floatfield );
-  ofstream fileH2("h2_e.out", ios::out);
-  fileH2.setf( ios::scientific, ios::floatfield );
-  ofstream fileC12("c12_e.out", ios::out);
-  fileC12.setf( ios::scientific, ios::floatfield );
-  ofstream fileAl27("al27_e.out", ios::out);
-  fileAl27.setf( ios::scientific, ios::floatfield );
-  ofstream fileCu("cu_e.out", ios::out);
-  fileCu.setf( ios::scientific, ios::floatfield );
-  ofstream filePb("pb_e.out", ios::out);
-  filePb.setf( ios::scientific, ios::floatfield );
+#ifdef qedebug
+  const G4int nP=10;                         // A#of points in nu/Q2 for each eE (energy values)
+#endif
+  std::ofstream fileH1("h1_e.out", ios::out);
+  fileH1.setf( std::ios::scientific, std::ios::floatfield );
+  std::ofstream fileH2("h2_e.out", std::ios::out);
+  fileH2.setf( std::ios::scientific, std::ios::floatfield );
+  std::ofstream fileC12("c12_e.out", std::ios::out);
+  fileC12.setf( std::ios::scientific, std::ios::floatfield );
+  std::ofstream fileAl27("al27_e.out", std::ios::out);
+  fileAl27.setf( std::ios::scientific, std::ios::floatfield );
+  std::ofstream fileCu("cu_e.out", std::ios::out);
+  fileCu.setf( std::ios::scientific, std::ios::floatfield );
+  std::ofstream filePb("pb_e.out", std::ios::out);
+  filePb.setf( std::ios::scientific, std::ios::floatfield );
   G4double low[nN]={137. , 2.22, 12.5 , 8.3 , 7.  , 7.35};
   G4double high[nN]={4.e7, 4.e7, 4.e7, 4.e7, 4.e7, 4.e7};
   G4Element* theElement[nN]={new G4Element("Hydrogen", "H", 1, 1.*g/mole),
@@ -58,7 +64,9 @@ int main()
 	G4double lekin = log(low[n]);
 	G4double dlekin= exp((log(log(high[n]))-log(lekin))/(nC-1));
 	lekin /= dlekin;
-	//G4cout<<"n="<<n<<", low="<<low[n]<<", high="<<high[n]<<", np="<<nC<<", d="<<dlekin<<G4endl;
+#ifdef debug
+	G4cout<<"G4ElNucCSTest:>>>>>>n="<<n<<",l="<<low[n]<<",h="<<high[n]<<",n="<<nC<<",d="<<dlekin<<G4endl;
+#endif
 	for(G4int ll=0; ll<nC; ll++)
 	{
 	  lekin*=dlekin;
@@ -67,7 +75,18 @@ int main()
 												 G4ParticleMomentum(1.,0.,0.), ekin*MeV);
 	  sig = eACrossSection.GetCrossSection(theDynamicParticle,theElement[n])/millibarn;
 	  delete theDynamicParticle;
-      //G4cout<<"n="<<n<<", e="<<ekin<<", sigma="<<sig<<G4endl;
+#ifdef debug
+      G4cout<<"G4ElNucCSTest: n="<<n<<", e="<<ekin<<", sigma="<<sig<<G4endl;
+#endif
+#ifdef qedebug
+	  for(G4int qe=0; qe<nP; qe++)
+	  {
+	    G4double phE = eACrossSection.GetEquivalentPhotonEnergy();
+	    G4double phQ2= eACrossSection.GetEquivalentPhotonQ2(phE);
+	    G4double x = phQ2/1878./phE;
+        G4cout<<"G4ElNucCSTest:............eE="<<ekin<<", phE="<<phE<<", phQ2="<<phQ2<<", x="<<x<<G4endl;
+	  }
+#endif
       if(!n) fileH1<<ekin<<" "<<sig<<G4endl;
       else if(n==1) fileH2<<ekin<<" "<<sig<<G4endl;
       else if(n==2) fileC12<<ekin<<" "<<sig<<G4endl;
