a9c15610dc60e2eb06fe274b7026db8bb1e11646
Vladimir Ivantchenko <Vladimir.Ivantchenko@cern.ch>
2003-11-12 14:51:54 +0000

    first add
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@16618 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ tests/test26/include/Tst26PhysListEm52.hh
@@ -0,0 +1,75 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+//
+//
+// $Id: Tst26PhysListEm52.hh,v 1.1 2003-11-12 14:51:54 vnivanch Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+/////////////////////////////////////////////////////////////////////////
+//
+// test26: Cut per region physics
+//
+// Created: 31.01.03 V.Ivanchenko
+//
+// Modified:
+//
+////////////////////////////////////////////////////////////////////////
+//
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+#ifndef Tst26PhysListEm52_h
+#define Tst26PhysListEm52_h 1
+
+#include "G4VPhysicsConstructor.hh"
+#include "globals.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+class Tst26PhysListEm52 : public G4VPhysicsConstructor
+{
+  public: 
+    Tst26PhysListEm52(const G4String& name = "model");
+    virtual ~Tst26PhysListEm52();
+
+  public: 
+    // This method is dummy for physics. 
+    virtual void ConstructParticle() {};
+ 
+    // This method will be invoked in the Construct() method.
+    // each physics process will be instantiated and
+    // registered to the process manager of each particle type 
+    virtual void ConstructProcess();
+};
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+#endif
+
+
+
+
+
+
+
+
