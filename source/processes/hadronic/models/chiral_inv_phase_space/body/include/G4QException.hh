1f5e7f835e295ab118e6db8e25d94dce3a021205
Johannes-Peter Wellisch <hpw@cern.ch>
2003-11-13 17:26:12 +0000

    fixing the missing throw specifier
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@16675 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/models/chiral_inv_phase_space/body/include/G4QException.hh
+++ source/processes/hadronic/models/chiral_inv_phase_space/body/include/G4QException.hh
@@ -21,7 +21,7 @@
 // ********************************************************************
 //
 //
-// $Id: G4QException.hh,v 1.2 2003-11-13 14:40:45 mkossov Exp $
+// $Id: G4QException.hh,v 1.3 2003-11-13 17:26:12 hpw Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 //      ---------------- G4QException ----------------
@@ -40,6 +40,7 @@ class G4QException : public std::exception
 {
   public:
   G4QException(G4String message);
+  virtual ~G4QException() throw() {}
   const G4String& GetMessage();
   private:
   G4String theMessage;
