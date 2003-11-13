59a9568b40eb1eb00ed972fd369ea9d274b7cf4f
Johannes-Peter Wellisch <hpw@cern.ch>
2003-11-13 19:01:13 +0000

    sort utility
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@16690 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/util/include/G4HadTmpUtil.hh
+++ source/processes/hadronic/util/include/G4HadTmpUtil.hh
@@ -2,6 +2,7 @@
 #define G4HadTmpUtil_hh 1
 
 #include "globals.hh"
+#include <vector>
 
 // Waiting for permission to use namespaces in geant4.....
 // namespace G4Had_Tmp_Util
@@ -9,6 +10,26 @@
 G4int G4lrint(double ad);
 G4int G4lint(double ad);
 G4int G4rint(double ad);
+G4String G4inttostring(int ai);
+  template <class A> class G4SortHelperPtr
+  {
+    public:
+    G4SortHelperPtr(A * aA){theA = aA;}
+    G4bool operator<(G4SortHelperPtr<A> right) const
+    { return *theA < (*right); }
+    A & operator * () {return *theA;}
+    private:
+    A * theA;
+  };
+#include <algorithm>
+template<class A> void G4PtrSort(std::vector<A *> * aList)
+{
+  std::vector<G4SortHelperPtr<A> > helper;
+  for(size_t i=0; i<aList->size(); i++) helper.push_back(aList->operator[](i));
+  std::sort(helper.begin(), helper.end());
+  for(size_t i=0; i<helper.size(); i++) aList->operator[](i)=&(*helper[i]);
+};
+
 // }
 
 #endif
--- source/processes/hadronic/util/src/G4HadTmpUtil.cc
+++ source/processes/hadronic/util/src/G4HadTmpUtil.cc
@@ -1,4 +1,5 @@
 #include "G4HadTmpUtil.hh"
+#include <strstream>
 
 G4int G4lrint(double ad)
   {
@@ -14,3 +15,12 @@ G4int G4rint(double ad)
   {
     return (ad>0) ? static_cast<int>(ad+1) : static_cast<int>(ad);
   }
+
+G4String G4inttostring(int ai)
+{
+  char the[100] = {""};
+  std::ostrstream ost(the, 100, std::ios::out);
+  ost << ai;
+  G4String result(the);
+  return result;
+}
