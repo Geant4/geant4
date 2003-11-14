b5aa491ce37760b86ac02c9849afb5bf8478267d
Gunter Folger <Gunter.Folger@cern.ch>
2003-11-14 15:07:24 +0000

    first version
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@16720 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ tests/test27/src/Tst27DetectorMessenger.cc
@@ -0,0 +1,64 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+//
+//
+// $Id: Tst27DetectorMessenger.cc,v 1.1 2003-11-14 15:07:24 gunter Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+
+#include "Tst27DetectorMessenger.hh"
+
+#include "Tst27DetectorConstruction.hh"
+#include "G4UIdirectory.hh"
+#include "G4UIcmdWithAString.hh"
+#include "globals.hh"
+
+#include "G4ios.hh"
+
+Tst27DetectorMessenger::Tst27DetectorMessenger(Tst27DetectorConstruction * myDC)
+:myDetector(myDC)
+{
+  G4String defParam;
+
+  mydetDir = new G4UIdirectory("/mydet/");
+  mydetDir->SetGuidance("Detector setup commands.");
+
+  selMatCmd = new G4UIcmdWithAString("/mydet/SelectMaterial",this);
+  selMatCmd->SetGuidance("Select Material of the SimpleBox.");
+  selMatCmd->SetGuidance("  Choice : H, Si, Cu, U (default)");
+  selMatCmd->SetParameterName("choice",true);
+  selMatCmd->SetDefaultValue("U");
+  selMatCmd->SetCandidates("H Si Cu U");
+  selMatCmd->AvailableForStates(G4State_PreInit,G4State_Idle);
+
+  myDetector->SelectMaterial(defParam="U");
+}
+
+void Tst27DetectorMessenger::SetNewValue(G4UIcommand * command,G4String newValues)
+{
+  if( command == selMatCmd )
+  {
+    myDetector->SelectMaterial(newValues);
+  }
+  return;
+}
+
