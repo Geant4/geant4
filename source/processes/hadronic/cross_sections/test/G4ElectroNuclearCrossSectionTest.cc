9189ff3c501370cee36cc7e5d14076547f53b8a7
Mikhail Kossov <mkossov@cern.ch>
2003-11-19 11:08:42 +0000

    cleaning and better bug report in G4ElectroNuclearCrossSection.cc
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@16863 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/cross_sections/test/G4ElectroNuclearCrossSectionTest.cc
+++ source/processes/hadronic/cross_sections/test/G4ElectroNuclearCrossSectionTest.cc
@@ -23,7 +23,10 @@
 #define debug
 #define qedebug
 
+//#include "G4UIterminal.hh"
 #include "G4ios.hh"
+//#include "G4ProcessManager.hh"
+//#include "G4DynamicParticle.hh"
 #include "G4Electron.hh"
 #include "G4Element.hh"
 #include "G4ElectroNuclearCrossSection.hh"
@@ -36,7 +39,7 @@ int main()
 #ifdef qedebug
   const G4int nP=10;                         // A#of points in nu/Q2 for each eE (energy values)
 #endif
-  std::ofstream fileH1("h1_e.out", ios::out);
+  std::ofstream fileH1("h1_e.out", std::ios::out);
   fileH1.setf( std::ios::scientific, std::ios::floatfield );
   std::ofstream fileH2("h2_e.out", std::ios::out);
   fileH2.setf( std::ios::scientific, std::ios::floatfield );
@@ -49,25 +52,27 @@ int main()
   std::ofstream filePb("pb_e.out", std::ios::out);
   filePb.setf( std::ios::scientific, std::ios::floatfield );
   G4double low[nN]={137. , 2.22, 12.5 , 8.3 , 7.  , 7.35};
-  G4double high[nN]={4.e7, 4.e7, 4.e7, 4.e7, 4.e7, 4.e7};
-  G4Element* theElement[nN]={new G4Element("Hydrogen", "H", 1, 1.*g/mole),
-							 new G4Element("Deuterium", "D", 1, 2.*g/mole),
-							 new G4Element("Carbon", "C", 6, 12.*g/mole),
-							 new G4Element("Aluminum", "Al", 13, 27.*g/mole),
-							 new G4Element("Copper", "Cu", 29, 64.*g/mole),
-							 new G4Element("Lead", "Pb", 82, 208.*g/mole)};
+  G4double high[nN]={2.e7, 2.e7, 2.e7, 2.e7, 2.e7, 2.e7};
+  G4int zA[nN]  ={1, 1,  6, 13, 29,  82};
+  G4int aA[nN]  ={1, 2, 12, 27, 64, 208};
+  G4Element* theElement[nN]={new G4Element("Hydrogen",  "H" , zA[0], aA[0]*g/mole),
+							 new G4Element("Deuterium", "D" , zA[1], aA[1]*g/mole),
+							 new G4Element("Carbon",    "C" , zA[2], aA[2]*g/mole),
+							 new G4Element("Aluminum",  "Al", zA[3], aA[3]*g/mole),
+							 new G4Element("Copper",    "Cu", zA[4], aA[4]*g/mole),
+							 new G4Element("Lead",      "Pb", zA[5], aA[5]*g/mole)};
   G4ParticleDefinition* theParticleDefinition = G4Electron::ElectronDefinition();
   G4DynamicParticle* theDynamicParticle;
   G4double sig=0.;
-  for(G4int n=0; n<6; n++)
+  for(G4int n=0; n<nN; n++)                 // >>>> LOOP over Elements
   {
 	G4double lekin = log(low[n]);
 	G4double dlekin= exp((log(log(high[n]))-log(lekin))/(nC-1));
 	lekin /= dlekin;
 #ifdef debug
-	G4cout<<"G4ElNucCSTest:>>>>>>n="<<n<<",l="<<low[n]<<",h="<<high[n]<<",n="<<nC<<",d="<<dlekin<<G4endl;
+	G4cout<<"G4ElNucCSTest:>>>>n="<<n<<",l="<<low[n]<<",h="<<high[n]<<",n="<<nC<<",d="<<dlekin<<G4endl;
 #endif
-	for(G4int ll=0; ll<nC; ll++)
+	for(G4int ll=0; ll<nC; ll++)            // >>>> LOOP over electron energies with the log-step
 	{
 	  lekin*=dlekin;
       G4double ekin=exp(lekin);
@@ -76,24 +81,25 @@ int main()
 	  sig = eACrossSection.GetCrossSection(theDynamicParticle,theElement[n])/millibarn;
 	  delete theDynamicParticle;
 #ifdef debug
-      G4cout<<"G4ElNucCSTest: n="<<n<<", e="<<ekin<<", sigma="<<sig<<G4endl;
+      G4cout<<"G4ElNucCSTest:****>>> Z="<<zA[n]<<", A="<<aA[n]<<", eE="<<ekin<<", sigma="<<sig<<G4endl;
 #endif
 #ifdef qedebug
-	  for(G4int qe=0; qe<nP; qe++)
+	  if(sig>0.)for(G4int qe=0; qe<nP; qe++) // >>>> LOOP over equiv. photon energies for particular eE
 	  {
 	    G4double phE = eACrossSection.GetEquivalentPhotonEnergy();
 	    G4double phQ2= eACrossSection.GetEquivalentPhotonQ2(phE);
 	    G4double x = phQ2/1878./phE;
         G4cout<<"G4ElNucCSTest:.......eE="<<ekin<<", phE="<<phE<<", phQ2="<<phQ2<<", x="<<x<<G4endl;
-	  }
+	  } // End of the equivalent photon LOOP
 #endif
+      // Fill the result for the electro-nuclear cross section plot
       if(!n) fileH1<<ekin<<" "<<sig<<G4endl;
       else if(n==1) fileH2<<ekin<<" "<<sig<<G4endl;
       else if(n==2) fileC12<<ekin<<" "<<sig<<G4endl;
       else if(n==3) fileAl27<<ekin<<" "<<sig<<G4endl;
       else if(n==4) fileCu<<ekin<<" "<<sig<<G4endl;
       else if(n==5) filePb<<ekin<<" "<<sig<<G4endl;
-    } // End of the point LOOP
-  } // End of the plot LOOP
+    } // End of the LOOP over electron energies
+  } // End of the LOOP over elements
   return EXIT_SUCCESS;
 }
