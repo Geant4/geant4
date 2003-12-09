6d56c021c0ed9ca10a0f4ece4be0f1865fcb80ed
sussi <sussi@cern.ch>
2003-12-09 14:13:24 +0000

    comitting purging magnet example
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@17389 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ examples/advanced/purging_magnet/include/PurgMagTrackingAction.hh
@@ -0,0 +1,58 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+// ********************************************************************
+// Code developed by:
+//  S.Larsson
+//
+//    **********************************
+//    *                                *
+//    *    PurgMagTrackingAction.hh    *
+//    *                                *
+//    **********************************
+//
+// $Id: PurgMagTrackingAction.hh,v 1.1 2003-12-09 14:13:23 sussi Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+
+#ifndef PurgMagTrackingAction_h
+#define PurgMagTrackingAction_h 1
+
+#include "G4UserTrackingAction.hh"
+
+class PurgMagRunAction;
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+class PurgMagTrackingAction : public G4UserTrackingAction 
+{
+
+  public:  
+    PurgMagTrackingAction(PurgMagRunAction*);
+   ~PurgMagTrackingAction() {};
+   
+    void PostUserTrackingAction(const G4Track*);
+    
+  private:
+    PurgMagRunAction* PurgMagRun;  
+};
+
+#endif
