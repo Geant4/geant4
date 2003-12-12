8c1088ced59828a764532cec8f8fbaf7762e37c7
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2003-12-12 13:12:14 +0000

    Removed useless const qualifier to return type in GetAccuracyForWarning/Exception()
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@17463 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/track/src/G4VParticleChange.cc
+++ source/track/src/G4VParticleChange.cc
@@ -21,7 +21,7 @@
 // ********************************************************************
 //
 //
-// $Id: G4VParticleChange.cc,v 1.15 2003-06-19 14:45:12 gunter Exp $
+// $Id: G4VParticleChange.cc,v 1.16 2003-12-12 13:12:14 gcosmo Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // 
@@ -325,12 +325,12 @@ G4bool G4VParticleChange::CheckSecondary(G4Track& aTrack)
 }
 
 
-const G4double G4VParticleChange::GetAccuracyForWarning() const
+G4double G4VParticleChange::GetAccuracyForWarning() const
 {
   return accuracyForWarning;
 }
 
-const G4double G4VParticleChange::GetAccuracyForException() const
+G4double G4VParticleChange::GetAccuracyForException() const
 {
   return accuracyForException;
 }
