ecf3395c94a872b010273077b0c8fc44d8b5dbea
Vladimir Grichine <Vladimir.Grichine@cern.ch>
2004-01-30 14:56:08 +0000

    first version for writing MSU data
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@17656 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/processes/hadronic/models/verification/hadronic_data_set/example/src/G4MSUHadData.cc
@@ -0,0 +1,109 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+//
+
+#include "G4MSUHadData.hh"
+#include "G4DataVector.hh"
+#include "G4Material.hh"
+#include "G4Element.hh"
+#include "G4Isotope.hh"
+#include "G4ElementVector.hh"
+
+#include <fstream>
+#include <strstream>
+#include "globals.hh"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+//sim
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include "G4HadFileFinder.hh"
+//end sim
+
+G4String G4MSUHadData::fMSUenergy = "MeV";
+G4String G4MSUHadData::fMSUangle = "degree";
+G4String G4MSUHadData::fMSUddXsc = "barn/sr/MeV";
+
+G4MSUHadData::G4MSUHadData( G4String&  inputFn, G4HadFileSpec& filetowrite):
+  G4VHadDataWriting()
+{
+  SetInputFileName(inputFn);
+  SetEnergyUnit(fMSUenergy);
+  SetAngleUnit(fMSUangle);
+  SetDdXscUnit(fMSUddXsc);
+
+  WriteDataFile(filetowrite);
+}
+
+G4MSUHadData::~G4MSUHadData()
+{ 
+
+}
+
+
+
+///////////////////////////////////////////////////////////////
+//
+//
+
+void G4MSUHadData::WriteDataFile( G4HadFileSpec& filetowrite) 
+{
+
+  G4String name(filetowrite.G4HDSFilename());
+  G4String pathString(filetowrite.G4HDSFilepath());
+  
+  G4String dirFile = pathString + name;  
+
+  char* doIhaverights = getenv("G4HADWRITTINGRIGHTS");
+
+  int doI=strcmp(doIhaverights,"0");
+
+  if ( ( !doIhaverights) || ( doI == 0 ) )
+    {
+      G4String excep = "G4HADWRITTINGRIGHTS environment variable is not set or equals 0. ";
+      excep += "You don't have the permission to write in the G4HDS";
+      G4Exception(excep);
+      
+    }
+
+
+  G4cout << "Filling file in position " << dirFile << G4endl;
+
+  FillDoubleDiffXSC(dirFile);
+
+}
+
+
+
+
+
+
+
+
+
+
+
