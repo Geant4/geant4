3c78049fa7a1b204483496fb7b8e1e2ef6eedbfd
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2004-05-14 17:40:21 +0000

    Added facilities for explicitely deallocate storage
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@18511 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/global/management/include/G4Allocator.hh
+++ source/global/management/include/G4Allocator.hh
@@ -21,7 +21,7 @@
 // ********************************************************************
 //
 //
-// $Id: G4Allocator.hh,v 1.13 2003-04-01 10:58:49 gcosmo Exp $
+// $Id: G4Allocator.hh,v 1.14 2004-05-14 17:40:21 gcosmo Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // 
@@ -60,6 +60,14 @@ class G4Allocator
     inline void FreeSingle(Type* anElement);
       // Malloc and Free methods to be used when overloading
       // new and delete operators in the client <Type> object
+
+    inline void ResetStorage();
+      // Returns allocated storage to the free store, resets
+      // allocator and page sizes.
+      // Note: contents in memory are lost using this call !
+
+    inline int GetAllocatedSize();
+      // Returns the size of the total memory allocated
 
   private:
 
@@ -145,29 +153,26 @@ Type* G4Allocator<Type>::MallocSingle()
 template <class Type>
 void G4Allocator<Type>::FreeSingle(Type* anElement)
 {
-  if (anElement)
+  G4AllocatorUnit<Type> * fUnit;
+
+  // The gcc-3.1 compiler will complain and not correctly handle offsets
+  // computed from non-POD types. Pointers to member data should be used
+  // instead.  This C++ feature seems not to work on earlier versions of
+  // the same compiler.
+  //
+  #if (__GNUC__==3) && (__GNUC_MINOR__>0)
+    Type G4AllocatorUnit<Type>::*pOffset = &G4AllocatorUnit<Type>::fElement;
+    fUnit = (G4AllocatorUnit<Type> *) ((char *)anElement - size_t(pOffset));
+  #else
+    fUnit = (G4AllocatorUnit<Type> *)
+            ((char *) anElement - offsetof(G4AllocatorUnit<Type>, fElement));
+  #endif
+
+  if (fUnit->deleted == Allocated)
   {
-    G4AllocatorUnit<Type> * fUnit;
-
-    // The gcc-3.1 compiler will complain and not correctly handle offsets
-    // computed from non-POD types. Pointers to member data should be used
-    // instead. This advanced C++ feature seems not to work on earlier
-    // versions of the same compiler.
-    //
-    #if (__GNUC__==3) && (__GNUC_MINOR__>0)
-      Type G4AllocatorUnit<Type>::*pOffset = &G4AllocatorUnit<Type>::fElement;
-      fUnit = (G4AllocatorUnit<Type> *) ((char *)anElement - size_t(pOffset));
-    #else
-      fUnit = (G4AllocatorUnit<Type> *)
-              ((char *) anElement - offsetof(G4AllocatorUnit<Type>, fElement));
-    #endif
-
-    if (fUnit->deleted == Allocated)
-    {
-      fUnit->deleted = Deleted;
-      fUnit->fNext = fFreeList;
-      fFreeList = fUnit;
-    }
+    fUnit->deleted = Deleted;
+    fUnit->fNext = fFreeList;
+    fFreeList = fUnit;
   }
 }
 
@@ -195,6 +200,54 @@ void G4Allocator<Type>::AddNewPage()
   }
   aPage->fUnits[unit_no].fNext = fFreeList;
   fFreeList = &aPage->fUnits[0];
+}
+
+// ************************************************************
+// ResetStorage
+// ************************************************************
+//
+template <class Type>
+void G4Allocator<Type>::ResetStorage()
+{
+  // Clear all allocated storage and return it to the free store
+  //
+  G4AllocatorPage<Type> * aPage;
+  G4AllocatorPage<Type> * aNextPage;
+
+  aPage = fPages;
+  while (aPage != 0)
+  {
+    aNextPage = aPage->fNext;
+    free(aPage->fUnits);
+    delete aPage;
+    aPage = aNextPage;
+  }
+
+  // Reset unit&page size and allocate a single page
+  //
+  fPages = 0;
+  fFreeList = 0;
+  fUnitSize = sizeof(G4AllocatorUnit<Type>);
+  fPageSize = ( (fUnitSize < 512) ? 1024 : (fUnitSize*10) );
+  AddNewPage();
+}
+
+// ************************************************************
+// GetAllocatedSize
+// ************************************************************
+//
+template <class Type>
+int G4Allocator<Type>::GetAllocatedSize()
+{
+  G4AllocatorPage<Type> * aPage = fPages;
+  int count = 0;
+
+  while (aPage != 0)
+  {
+    aPage = aPage->fNext;
+    count++;
+  }
+  return count*fPageSize;
 }
 
 // ************************************************************
