5c4c7bf8ee4a3065e1bf808955c5980e270fb28e
Alberto Ribon <Alberto.Ribon@cern.ch>
2004-05-28 11:19:19 +0000

    Ported to PI light. No changes in the code were necessary.
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@18722 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ examples/advanced/Rich/setupAidaPi.csh
@@ -0,0 +1,11 @@
+#------------------------------------------------------------------
+# C-shell script to be run before building/executing this example.
+#------------------------------------------------------------------
+#
+# --- Geant4 specific ---
+setenv G4ANALYSIS_USE 1
+#
+# --- Aida / PI ---
+eval `aida-config --runtime csh`
+#
+
---
+++ examples/advanced/Rich/setupAidaPi.sh
@@ -0,0 +1,12 @@
+#------------------------------------------------------------------
+# Bash-shell script to be run before building/executing this example.
+#------------------------------------------------------------------
+#
+# --- Geant4 specific ---
+export G4ANALYSIS_USE=1
+#
+# --- Aida / PI ---
+eval `aida-config --runtime sh`
+#
+
+
