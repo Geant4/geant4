736c1e49e7aa21161dccbf21859e1d20aba5e7c7
John Apostolakis <John.Apostolakis@cern.ch>
2004-06-09 13:48:45 +0000

    Added - counting of zero steps - constants for # steps to 'act' or abandon
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@18979 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/geometry/navigation/include/G4Navigator.hh
+++ source/geometry/navigation/include/G4Navigator.hh
@@ -21,7 +21,7 @@
 // ********************************************************************
 //
 //
-// $Id: G4Navigator.hh,v 1.5 2004-03-10 18:21:15 gcosmo Exp $
+// $Id: G4Navigator.hh,v 1.6 2004-06-09 13:48:45 japost Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 //
@@ -359,11 +359,23 @@ class G4Navigator
                                 // of current path through geomtrical
                                 // hierarchy
 
+  //  Count zero steps - as one or two can occur due to changing momentum at a boundary
+  //                       or at an edge common between volumes
+  //                   - several are a sign of a problem in the geometry description
+  //                       or the Navigator  
+  //
   G4bool  fLastStepWasZero;
                                 // Whether the last ComputeStep moved Zero
                                 //  Used to check for edges.
   G4bool  fLocatedOnEdge;       
                                 // Whether the Navigator has detected an edge
+  G4int fNumberZeroSteps;
+                                // Number of preceding ComputeStep moves that were Zero
+                                //  Reset to 0 after finite step
+  G4int fActionThreshold_NoZeroSteps;  
+                                // After this many failed/zero steps, act (push etc) 
+  G4int fAbandonThreshold_NoZeroSteps; 
+                                // After this many failed/zero steps, abandon track
 
   G4ThreeVector  fPreviousSftOrigin;
   G4double       fPreviousSafety; 
