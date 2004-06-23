c7b466aff3efc2f573639ebb29849a92f7a4444f
Gunter Folger <Gunter.Folger@cern.ch>
2004-06-23 15:06:26 +0000

    adding/correcting disclaimer
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@19140 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/models/util/src/G4WilsonRadius.cc
+++ source/processes/hadronic/models/util/src/G4WilsonRadius.cc
@@ -1,3 +1,35 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// *                                                                  *
+// * Parts of this code which have been  developed by QinetiQ Ltd     *
+// * under contract to the European Space Agency (ESA) are the	      *
+// * intellectual property of ESA. Rights to use, copy, modify and    *
+// * redistribute this software for general public use are granted    *
+// * in compliance with any licensing, distribution and development   *
+// * policy adopted by the Geant4 Collaboration. This code has been   *
+// * written by QinetiQ Ltd for the European Space Agency, under ESA  *
+// * contract 17191/03/NL/LvH (Aurora Programme). 		      *
+// *                                                                  *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+//
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 //
 // MODULE:              G4WilsonRadius.cc
@@ -19,38 +51,6 @@
 //
 // 15 March 2004, P R Truscott, QinetiQ Ltd, UK
 // Beta release
-//
-// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-//
-// ********************************************************************
-// * DISCLAIMER                                                       *
-// *                                                                  *
-// * The following disclaimer summarizes all the specific disclaimers *
-// * of contributors to this software. The specific disclaimers,which *
-// * govern, are listed with their locations in:                      *
-// *   http://cern.ch/geant4/license                                  *
-// *                                                                  *
-// * Neither the authors of this software system, nor their employing *
-// * institutes,nor the agencies providing financial support for this *
-// * work  make  any representation or  warranty, express or implied, *
-// * regarding  this  software system or assume any liability for its *
-// * use.                                                             *
-// *                                                                  *
-// * This  code  implementation  is  the  intellectual   property  of *
-// * QinetiQ.   Rights  to use  this  software  are  granted  to  the *
-// * European Space Agency  under  the  provisions  of  Clause 39  of *
-// * the    Standard    Conditions   for  Contract.   ESA    contract *
-// * 17191/03/NL/LvH  provides  the  rights to distribute source code *
-// * through  the  Geant4 Collaboration Agreement for general  public *
-// * use.  Some elements of this source code may be derived from code *
-// * developed   by   other   member   institutes   of   the   Geant4 *
-// * Collaboration, and the provision of  their code under the Geant4 *
-// * Collaboration is acknowledged.                                   *
-// *                                                                  *
-// * By copying,  distributing  or modifying the Program (or any work *
-// * based  on  the Program)  you indicate  your  acceptance of  this *
-// * statement, and all its terms.                                    *
-// ********************************************************************
 //
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 //
