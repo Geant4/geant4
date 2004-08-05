f9eb4c5cc8ecf13f8a036dd6cbfdeb1f00714a96
Michel Maire <Michel.Maire@cern.ch>
2004-08-05 14:11:04 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@19490 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/electromagnetic/TestEm5/src/RunAction.cc
+++ examples/extended/electromagnetic/TestEm5/src/RunAction.cc
@@ -20,7 +20,7 @@
 // * statement, and all its terms.                                    *
 // ********************************************************************
 //
-// $Id: RunAction.cc,v 1.11 2004-08-05 12:25:47 maire Exp $
+// $Id: RunAction.cc,v 1.12 2004-08-05 14:11:04 maire Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -143,7 +143,7 @@ void RunAction::EndOfRunAction(const G4Run* aRun)
   
   G4EmCalculator emCalculator;
   G4double dEdxTable = 0.;
-  if (charge != 0.) dEdxTable = emCalculator.GetDEDX(particle,material,energy);
+  ///if (charge != 0.) dEdxTable = emCalculator.GetDEDX(particle,material,energy);
   G4double stopTable = dEdxTable/density;
   
   //Stopping Power from simulation.
