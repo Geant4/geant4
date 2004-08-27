80472d18015d5355c6c1f2118c59ca026ca8eef0
Alberto Ribon <Alberto.Ribon@cern.ch>
2004-08-27 15:39:41 +0000

    Fix the setting
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@19594 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/models/verification/full_setups/StatAccepTest/env.csh
+++ source/processes/hadronic/models/verification/full_setups/StatAccepTest/env.csh
@@ -1,11 +1,18 @@
 #
 # --- Setup the Geant4 kernel environment ---
-source ~/ExtraSpace/geant4/env.csh-3.23-nodeb
+# ***LOOKHERE*** IT IS ASSUMED THAT YOU HAVE ALREADY YOUR 
+#                COMPILER g++ 3.23 AND THE GEANT4 ENVIRONMENTAL 
+#                VARIABLES PROPERLY SET.
+###source ~/ExtraSpace/geant4/env.csh-3.23-nodeb
 #
 # --- Setup your Geant4 application environment ---
 unsetenv G4TMP
 unsetenv G4BIN
-setenv G4WORKDIR ~/ExtraSpace/StatAccepTest
+#
+#***LOOKHERE*** SET HERE YOUR WORKSPACE: BY DEFAULT, IT IS
+#               ASSUMED TO BE THE CURRENT DIRECTORY.
+setenv G4WORKDIR $PWD
+#
 setenv PATH ${PATH}:${G4WORKDIR}/bin/${G4SYSTEM}
 setenv G4ANALYSIS_USE 1
 #
--- source/processes/hadronic/models/verification/full_setups/StatAccepTest/env.sh
+++ source/processes/hadronic/models/verification/full_setups/StatAccepTest/env.sh
@@ -1,11 +1,18 @@
 #
 # --- Setup the Geant4 kernel environment ---
-. ~/ExtraSpace/geant4/env.sh-3.23-nodeb
+# ***LOOKHERE*** IT IS ASSUMED THAT YOU HAVE ALREADY YOUR 
+#                COMPILER g++ 3.23 AND THE GEANT4 ENVIRONMENTAL 
+#                VARIABLES PROPERLY SET.
+###. ~/ExtraSpace/geant4/env.sh-3.23-nodeb
 #
 # --- Setup your Geant4 application environment ---
 unset G4TMP
 unset G4BIN
-export G4WORKDIR=~/ExtraSpace/StatAccepTest
+#
+#***LOOKHERE*** SET HERE YOUR WORKSPACE: BY DEFAULT, IT IS
+#               ASSUMED TO BE THE CURRENT DIRECTORY.
+export G4WORKDIR=$PWD
+#
 export PATH=$PATH:$G4WORKDIR/bin/$G4SYSTEM
 export G4ANALYSIS_USE=1
 #
@@ -14,10 +21,6 @@ export PI_DIR=/afs/cern.ch/sw/lcg/app/releases/PI/PI_1_2_3/rh73_gcc323
 export PATH=$PI_DIR/bin:$PATH
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PI_DIR/lib
 eval `aida-config --runtime sh`
-#
-#***LOOKHERE*** TEMPORARY LORENZO FIX: CLOUD CACHE SETS TO 10,000 .
-export LD_LIBRARY_PATH=~/ExtraSpace/myPiDir/lib:$LD_LIBRARY_PATH
-#export LD_LIBRARY_PATH=~/ExtraSpace/myPiDir/lib_dbg:$LD_LIBRARY_PATH
 #
 # --- GSL : this is needed only for  dirStat/pvalue.cpp ---
 export GSL_DIR=/afs/cern.ch/sw/lcg/external/GSL/1.4/rh73_gcc323
