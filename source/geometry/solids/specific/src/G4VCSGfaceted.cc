c5711ed22424646927fa9e3a7c76f00883149f00
David Williams <davidw@cern.ch>
2004-09-01 23:40:21 +0000

    For the (hopefully rare) anomalous situation in which DistanceToOut(p,v) will report kInfinity, make an additional check for the point p on the surface, in which case report zero.
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@19621 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/geometry/solids/specific/src/G4VCSGfaceted.cc
+++ source/geometry/solids/specific/src/G4VCSGfaceted.cc
@@ -26,7 +26,7 @@
 // based on the Program) you indicate your acceptance of this statement,
 // and all its terms.
 //
-// $Id: G4VCSGfaceted.cc,v 1.10 2003-06-16 16:54:01 gunter Exp $
+// $Id: G4VCSGfaceted.cc,v 1.11 2004-09-01 23:40:21 davidw Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // 
@@ -318,7 +318,8 @@ G4double G4VCSGfaceted::DistanceToOut( const G4ThreeVector &p,
     }
   }
   else
-  {
+  { 
+    if (Inside(p) == kSurface) distance = 0;
     if (calcNorm) *validNorm = false;
   }
 
