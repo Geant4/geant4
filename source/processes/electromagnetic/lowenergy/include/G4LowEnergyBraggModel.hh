83bbc7d0538c56de7e189ed3597c82f799445e87
Vladimir Ivantchenko <Vladimir.Ivantchenko@cern.ch>
2004-10-11 08:45:34 +0000

    return back
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@19856 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/processes/electromagnetic/lowenergy/include/G4LowEnergyBraggModel.hh
@@ -0,0 +1,162 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+// 
+// $Id: G4LowEnergyBraggModel.hh,v 1.4 2004-10-11 08:41:28 vnivanch Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+// -------------------------------------------------------------------
+//
+// GEANT4 Class header file
+//
+//
+// File name:     G4LowEnergyBraggModel
+//
+// Author:        Vladimir Ivanchenko
+//
+// Creation date: 17.04.2004
+//
+// Modifications:
+//
+//
+// Class Description:
+//
+// Implementation of energy loss and delta-electron production
+// by heavy slow charged particles using eveluated data
+
+// -------------------------------------------------------------------
+//
+
+#ifndef G4LowEnergyBraggModel_h
+#define G4LowEnergyBraggModel_h 1
+
+#include "G4VEmModel.hh"
+
+class G4hParametrisedLossModel;
+
+class G4LowEnergyBraggModel : public G4VEmModel
+{
+
+public:
+
+  G4LowEnergyBraggModel(const G4ParticleDefinition* p = 0, const G4String& nam = "LEBragg");
+
+  ~G4LowEnergyBraggModel();
+
+  void Initialise(const G4ParticleDefinition*, const G4DataVector&);
+
+  G4double HighEnergyLimit(const G4ParticleDefinition*);
+
+  G4double LowEnergyLimit(const G4ParticleDefinition*);
+
+  void SetHighEnergyLimit(G4double e) {highKinEnergy = e;};
+
+  void SetLowEnergyLimit(G4double e) {lowKinEnergy = e;};
+
+  G4double MinEnergyCut(const G4ParticleDefinition*,
+                        const G4MaterialCutsCouple*);
+
+  G4bool IsInCharge(const G4ParticleDefinition*);
+
+  G4double ComputeDEDX(const G4MaterialCutsCouple*,
+                       const G4ParticleDefinition*,
+                             G4double kineticEnergy,
+                             G4double cutEnergy);
+
+  G4double CrossSection(const G4MaterialCutsCouple*,
+                        const G4ParticleDefinition*,
+                              G4double kineticEnergy,
+                              G4double cutEnergy,
+                              G4double maxEnergy);
+
+  G4DynamicParticle* SampleSecondary(
+                                const G4MaterialCutsCouple*,
+                                const G4DynamicParticle*,
+                                      G4double tmin,
+                                      G4double maxEnergy);
+
+  std::vector<G4DynamicParticle*>* SampleSecondaries(
+                                const G4MaterialCutsCouple*,
+                                const G4DynamicParticle*,
+                                      G4double tmin,
+                                      G4double maxEnergy);
+
+  G4double MaxSecondaryEnergy(const G4DynamicParticle*);
+
+  void SetParameterization(G4hParametrisedLossModel* p);
+
+protected:
+
+  G4double MaxSecondaryEnergy(const G4ParticleDefinition*,
+                                    G4double kinEnergy);
+
+private:
+
+  void SetParticle(const G4ParticleDefinition* p);
+
+  // hide assignment operator
+  G4LowEnergyBraggModel & operator=(const  G4LowEnergyBraggModel &right);
+  G4LowEnergyBraggModel(const  G4LowEnergyBraggModel&);
+
+  const G4ParticleDefinition* particle;
+  G4hParametrisedLossModel* parameterization;
+  G4double mass;
+  G4double spin;
+  G4double chargeSquare;
+  G4double massRate;
+  G4double ratio;
+  G4double highKinEnergy;
+  G4double lowKinEnergy;
+  G4double lowestKinEnergy;
+
+  G4bool   isIon;
+};
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+inline G4double G4LowEnergyBraggModel::MaxSecondaryEnergy(
+          const G4ParticleDefinition* pd,
+                G4double kinEnergy)
+{
+  if(isIon) SetParticle(pd);
+  G4double tau  = kinEnergy/mass;
+  G4double tmax = 2.0*electron_mass_c2*tau*(tau + 2.) /
+                  (1. + 2.0*(tau + 1.)*ratio + ratio*ratio);
+  return tmax;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+inline G4double G4LowEnergyBraggModel::MaxSecondaryEnergy(const G4DynamicParticle* dp)
+{
+  if(isIon) {
+    mass =  dp->GetMass();
+    ratio = electron_mass_c2/mass;
+  }
+  G4double tau  = dp->GetKineticEnergy()/mass;
+  G4double tmax = 2.0*electron_mass_c2*tau*(tau + 2.) /
+                  (1. + 2.0*(tau + 1.)*ratio + ratio*ratio);
+  return tmax;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+#endif
