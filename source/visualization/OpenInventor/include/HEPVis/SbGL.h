ab5289874d930c79bdd81ddaa4392a8e84623cd5
Guy Barrand <gbarrand@cern.ch>
2004-11-08 17:30:50 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@20021 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/visualization/OpenInventor/include/HEPVis/SbGL.h
@@ -0,0 +1,16 @@
+#ifndef HEPVis_SbGL_h
+#define HEPVis_SbGL_h 
+
+#ifdef WIN32
+#include <windows.h>
+#endif
+
+#ifdef APPLE_GL
+#include <gl.h>
+#include <glu.h>
+#else
+#include <GL/gl.h>
+#include <GL/glu.h>
+#endif
+
+#endif
