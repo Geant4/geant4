ae9e3ac1c62d0959b862eb9e4bf9996a8b2e64e8
Guy Barrand <gbarrand@cern.ch>
2004-11-09 09:13:33 +0000

    This commit was generated by cvs2svn to compensate for changes in r20032, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@20033 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/visualization/OpenInventor/include/HEPVis/actions/SoGL2PSAction.h
@@ -0,0 +1,36 @@
+#ifndef HEPVis_SoGL2PSAction_h
+#define HEPVis_SoGL2PSAction_h 
+
+#include <Inventor/actions/SoGLRenderAction.h>
+
+/**
+ *  SoGL2PSAction inherits Inventor/SoGLRenderAction.
+ * It uses the gl2ps software to produce vector PostScript of a scene graph.
+ *  See applications/DetectorTreeKit, Polyhedron for examples.
+ */
+
+#define SoGL2PSAction Geant4_SoGL2PSAction
+
+class SoGL2PSAction : public SoGLRenderAction {
+  SO_ACTION_HEADER(SoGL2PSAction);
+public:
+  SoGL2PSAction(const SbViewportRegion&);
+  void setFileName(const char*);
+  void enableFileWriting();
+  void disableFileWriting();
+  SbBool fileWritingEnabled() const;
+  SbBool addPixmap(float,float,int,int,float*,SbBool);
+  SbBool addBitmap(int,int,float=0,float=0,float=0,float=0);
+  void beginViewport();
+  void endViewport();
+public: /*SoINTERNAL*/
+  static void initClass();
+protected:
+  virtual void beginTraversal(SoNode*);
+private:
+  SbString fFileName;
+  SbBool fEnable;
+};
+
+#endif
+
