a7d6fc5d1cdaed7b82433558447ebe472a08774e
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2004-11-19 16:17:26 +0000

    Remove inclusion of windows.h, no longer supported on .NET
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@20255 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/global/management/src/G4Timer.cc
+++ source/global/management/src/G4Timer.cc
@@ -21,7 +21,7 @@
 // ********************************************************************
 //
 //
-// $Id: G4Timer.cc,v 1.11 2003-06-06 16:17:17 gcosmo Exp $
+// $Id: G4Timer.cc,v 1.12 2004-11-19 16:17:26 gcosmo Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // 
@@ -29,7 +29,7 @@
 // class G4Timer
 //
 // Implementation
-// 29.04.97 G.Cosmo Added timings for Windows/NT
+// 29.04.97 G.Cosmo Added timings for Windows systems
 
 #include "G4Timer.hh"
 #include "G4ios.hh"
@@ -47,7 +47,6 @@ void G4Exception(const char* s=0);
 
 #ifdef WIN32
 #  include <sys/types.h>
-#  include <windows.h>
 
    // extract milliseconds time unit
    int sysconf(int a){
