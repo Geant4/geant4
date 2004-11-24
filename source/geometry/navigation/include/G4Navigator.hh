a484fe6c12f83507813e579a94da2438d7597529
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2004-11-24 08:27:19 +0000

    cosmetics ...
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@20334 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/geometry/navigation/include/G4Navigator.hh
+++ source/geometry/navigation/include/G4Navigator.hh
@@ -21,7 +21,7 @@
 // ********************************************************************
 //
 //
-// $Id: G4Navigator.hh,v 1.9 2004-11-19 14:21:21 japost Exp $
+// $Id: G4Navigator.hh,v 1.10 2004-11-24 08:27:19 gcosmo Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 //
@@ -83,8 +83,6 @@
 
 // History:
 // - Created. Paul Kent,         July 95/96
-// - Maintained  J. Apostolakis, Oct 1996/2004
-// - Maintained  G. Cosmo,       Oct 2000/2004
 // ********************************************************************
 
 #ifndef G4NAVIGATOR_HH
@@ -202,14 +200,12 @@ class G4Navigator
     // LocateGlobalPointAndSetup(). Then use the volume found and its
     // navigation history to update the touchable.
 
-  // #ifDEF OBSOLETE_LOCATE_NO_DIRECTION
   inline void LocateGlobalPointAndUpdateTouchable(
                 const G4ThreeVector&       position,
                       G4VTouchable*        touchableToUpdate,
                 const G4bool               RelativeSearch = true);
     // Old version (missing direction).
     // Not recommended replace with newer version above.
-  // #endif
 
   inline void SetGeometricallyLimitedStep();
     // Inform the navigator that the previous Step calculated
@@ -273,11 +269,11 @@ class G4Navigator
     // as needed by LocalGlobalPointAndSetup.
     // [Does not perform clears, resizes, or reset fLastLocatedPointLocal]
 
-  inline G4int SeverityOfZeroStepping( G4int *noZeroSteps ) const; 
+  inline G4int SeverityOfZeroStepping( G4int* noZeroSteps ) const; 
     // Report on severity of error and number of zero steps,
-    //   - in case Navigator is stuck and is returning zero steps.
-    //  Values: 1 (small problem),  5 ('pushing'), 
-    //          9 (ready to abandon), 10 (abandoned)
+    // in case Navigator is stuck and is returning zero steps.
+    // Values: 1 (small problem),  5 (correcting), 
+    //         9 (ready to abandon), 10 (abandoned)
 
  protected:  // with description
 
