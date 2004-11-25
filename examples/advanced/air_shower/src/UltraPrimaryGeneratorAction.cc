f5aa83ba6ed2fe61c73d7f42fad05b3973c12c99
Bernardo Neto Gomes Baptista Tome <tomezb@cern.ch>
2004-11-25 13:22:34 +0000

    Migrated to new tree-structured command format for G4GeneralParticleSource
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@20381 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/advanced/air_shower/src/UltraPrimaryGeneratorAction.cc
+++ examples/advanced/air_shower/src/UltraPrimaryGeneratorAction.cc
@@ -39,12 +39,13 @@
 //    incidence is used. The source parameters can be set directly in this class
 //    or through the GeneralParticleSource  messenger class.
 //
-#include <math.h>
 #include "UltraPrimaryGeneratorAction.hh"
 
-#include "globals.hh"
 #include "G4Event.hh"
 #include "G4GeneralParticleSource.hh"
+#include "G4SPSAngDistribution.hh"
+#include "G4SPSEneDistribution.hh"
+#include "G4SPSPosDistribution.hh"
 #include "G4ParticleTable.hh"
 #include "G4ParticleDefinition.hh"
 #include "G4ThreeVector.hh"
@@ -55,7 +56,7 @@ UltraPrimaryGeneratorAction::UltraPrimaryGeneratorAction()
 {
 
   particleGun = new G4GeneralParticleSource();
-  
+
 
   // Define here the user default properties for the General Particle Source (GPS)
   // Can be modified through the GPS Messenger (/gps/... commands)
@@ -80,14 +81,16 @@ UltraPrimaryGeneratorAction::UltraPrimaryGeneratorAction()
   G4ThreeVector Polarization = G4ThreeVector(1.,1.,0.) ;
   particleGun->SetParticlePolarization(Polarization);
   
-  // GENERATES A MONO-ENERGETIC SOURCE
-  particleGun->SetEnergyDisType("Mono");
-  particleGun->SetMonoEnergy(3.0*eV);
+  // DEFINE A MONO-ENERGETIC SOURCE
+  G4SPSEneDistribution *eneDist = particleGun->GetCurrentSource()->GetEneDist() ;
+  eneDist->SetEnergyDisType("Mono");
+  eneDist->SetMonoEnergy(3.0*eV);
 
-  // SET POSITION DISTRIBUTION TYPE
-  particleGun->SetPosDisType("Plane");
-  particleGun->SetPosDisShape("Circle");
-  particleGun->SetRadius(10.0*cm);
+  // SET POSITION DISTRIBUTION 
+  G4SPSPosDistribution *posDist = particleGun->GetCurrentSource()->GetPosDist() ;
+  posDist->SetPosDisType("Plane");
+  posDist->SetPosDisShape("Circle");
+  posDist->SetRadius(10.0*cm);
 
 #ifdef ULTRA_MIRROR_USE
 #define ULTRA_REFLECTION_USE
@@ -97,15 +100,17 @@ UltraPrimaryGeneratorAction::UltraPrimaryGeneratorAction()
 #define ULTRA_REFLECTION_USE
 #endif
 
+  G4SPSAngDistribution *angDist = particleGun->GetCurrentSource()->GetAngDist() ;
+
 #ifdef ULTRA_REFLECTION_USE
-  particleGun->SetParticleMomentumDirection(G4ThreeVector(0.0,-1.0,0.0)) ;
-  particleGun->SetPosRot1(G4ThreeVector(1.,0.,0.));
-  particleGun->SetPosRot2(G4ThreeVector(0.,0.,-1.));
-  particleGun->SetCentreCoords(G4ThreeVector(0.0*cm,90.0*cm,150.0*cm));
+  angDist->SetParticleMomentumDirection(G4ThreeVector(0.0,-1.0,0.0)) ;
+  posDist->SetPosRot1(G4ThreeVector(1.,0.,0.));
+  posDist->SetPosRot2(G4ThreeVector(0.,0.,-1.));
+  posDist->SetCentreCoords(G4ThreeVector(0.0*cm,90.0*cm,150.0*cm));
 
 #else
-  particleGun->SetParticleMomentumDirection(G4ThreeVector(0.0,0.0,-1.0)) ;
-  particleGun->SetCentreCoords(G4ThreeVector(0.0*cm,0.0*cm,150.0*cm));
+  angDist->SetParticleMomentumDirection(G4ThreeVector(0.0,0.0,-1.0)) ;
+  posDist->SetCentreCoords(G4ThreeVector(0.0*cm,0.0*cm,150.0*cm));
 
 #endif
 
@@ -126,9 +131,9 @@ void UltraPrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent)
   G4int iEvent = anEvent->GetEventID() ;
   if ( iEvent == 0 ){
 
-    G4cout << particleGun->GetParticleDefinition()->GetParticleName() << " " ;
-    G4cout << particleGun->GetEnergyDisType() << " " ;
-    G4cout << particleGun->GetPosDisType() << G4endl ;
+    G4cout << particleGun->GetParticleDefinition()->GetParticleName()           << " " ;
+    G4cout << particleGun->GetCurrentSource()->GetEneDist()->GetEnergyDisType() << " " ;
+    G4cout << particleGun->GetCurrentSource()->GetPosDist()->GetPosDisType()    << G4endl ;
 
 }
   particleGun->GeneratePrimaryVertex(anEvent);
