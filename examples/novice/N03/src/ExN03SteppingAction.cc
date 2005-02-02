5d799902cc1be38facf58f7d23ba59d9f06b00fa
Michel Maire <Michel.Maire@cern.ch>
2005-02-02 17:11:11 +0000

    sum stepl even if edep=0.
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@21113 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/novice/N03/src/ExN03SteppingAction.cc
+++ examples/novice/N03/src/ExN03SteppingAction.cc
@@ -21,7 +21,7 @@
 // ********************************************************************
 //
 //
-// $Id: ExN03SteppingAction.cc,v 1.8 2003-09-15 15:38:18 maire Exp $
+// $Id: ExN03SteppingAction.cc,v 1.9 2005-02-02 17:11:11 maire Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // 
@@ -64,10 +64,8 @@ void ExN03SteppingAction::UserSteppingAction(const G4Step* aStep)
   if (track->GetDefinition()->GetPDGCharge() != 0.)
     stepl = aStep->GetStepLength();
       
-  if ((edep!=0.)) {
-    if (volume == detector->GetAbsorber()) eventaction->AddAbs(edep,stepl);
-    if (volume == detector->GetGap())      eventaction->AddGap(edep,stepl);
-  }
+  if (volume == detector->GetAbsorber()) eventaction->AddAbs(edep,stepl);
+  if (volume == detector->GetGap())      eventaction->AddGap(edep,stepl);
        
  // save the random number seed of this event, under condition
  //// if(condition) G4RunManager::GetRunManager()->rndmSaveThisEvent();
