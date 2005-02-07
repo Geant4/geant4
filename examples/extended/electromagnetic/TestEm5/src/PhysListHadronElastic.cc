0d6307b4551e8c0db6b99f6c229bb6c7912c8e57
Michel Maire <Michel.Maire@cern.ch>
2005-02-07 16:12:17 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@21125 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/electromagnetic/TestEm5/src/PhysListHadronElastic.cc
+++ examples/extended/electromagnetic/TestEm5/src/PhysListHadronElastic.cc
@@ -20,7 +20,7 @@
 // * statement, and all its terms.                                    *
 // ********************************************************************
 //
-// $Id: PhysListHadronElastic.cc,v 1.4 2004-06-21 10:57:14 maire Exp $
+// $Id: PhysListHadronElastic.cc,v 1.5 2005-02-07 16:12:17 maire Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -29,7 +29,7 @@
 #include "PhysListHadronElastic.hh"
 #include "G4ParticleDefinition.hh"
 #include "G4ProcessManager.hh"
-#include "G4LElastic.hh"   
+#include "G4LElastic.hh"
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
