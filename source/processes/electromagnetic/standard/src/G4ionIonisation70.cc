4f14cf6cf4c1a242bd8a98c65d37a110cf36be1d
Vladimir Ivantchenko <Vladimir.Ivantchenko@cern.ch>
2005-02-26 22:01:07 +0000

    add h and ion ionisation70
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@21265 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/processes/electromagnetic/standard/src/G4ionIonisation70.cc
@@ -0,0 +1,147 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+//
+// $Id: G4ionIonisation70.cc,v 1.1 2005-02-26 22:01:07 vnivanch Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+// -------------------------------------------------------------------
+//
+// GEANT4 Class file
+//
+//
+// File name:     G4ionIonisation70
+//
+// Author:        Vladimir Ivanchenko
+//
+// Creation date: 07.05.2002
+//
+// Modifications:
+//
+// 23-12-02 Change interface in order to move to cut per region (V.Ivanchenko)
+// 26-12-02 Secondary production moved to derived classes (V.Ivanchenko)
+// 13-02-03 SubCutoff regime is assigned to a region (V.Ivanchenko)
+// 18-04-03 Use IonFluctuations (V.Ivanchenko)
+// 03-08-03 Add effective charge (V.Ivanchenko)
+// 12-11-03 G4EnergyLossSTD -> G4EnergyLossProcess (V.Ivanchenko)
+// 27-05-04 Set integral to be a default regime (V.Ivanchenko)
+// 08-11-04 Migration to new interface of Store/Retrieve tables (V.Ivantchenko)
+//
+//
+// -------------------------------------------------------------------
+//
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+#include "G4ionIonisation70.hh"
+#include "G4Electron.hh"
+#include "G4Proton.hh"
+#include "G4GenericIon.hh"
+#include "G4BraggModel.hh"
+#include "G4BraggIonModel.hh"
+#include "G4BetheBlochModel70.hh"
+#include "G4IonFluctuations.hh"
+#include "G4UniversalFluctuation.hh"
+#include "G4UnitsTable.hh"
+#include "G4LossTableManager.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+using namespace std;
+
+G4ionIonisation70::G4ionIonisation70(const G4String& name)
+  : G4VEnergyLossProcess(name),
+    theParticle(0),
+    theBaseParticle(0),
+    isInitialised(false),
+    subCutoff(false)
+{
+  SetDEDXBinning(120);
+  SetLambdaBinning(120);
+  SetMinKinEnergy(0.1*keV);
+  SetMaxKinEnergy(100.0*TeV);
+  SetVerboseLevel(0);
+  corr = G4LossTableManager::Instance()->EmCorrections();  
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+G4ionIonisation70::~G4ionIonisation70()
+{}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+void G4ionIonisation70::InitialiseEnergyLossProcess(const G4ParticleDefinition* part,
+                                                  const G4ParticleDefinition* bpart)
+{
+  if(isInitialised) return;
+
+  theParticle = part;
+
+  if(part == bpart || part == G4GenericIon::GenericIon()) theBaseParticle = 0;
+  else if(bpart == 0) theBaseParticle = G4GenericIon::GenericIon();
+  else                theBaseParticle = bpart;
+
+  SetBaseParticle(theBaseParticle);
+  SetSecondaryParticle(G4Electron::Electron());
+
+  flucModel = new G4IonFluctuations();
+  //  eth = 10.0*MeV/4.0026;
+  eth = 2.0*MeV;
+  G4VEmModel* em = new G4BraggIonModel();
+  //G4VEmModel* em = new G4BraggModel();
+  em->SetLowEnergyLimit(0.1*keV);
+  em->SetHighEnergyLimit(eth);
+  AddEmModel(1, em, flucModel);
+  G4VEmModel* em1 = new G4BetheBlochModel70();
+  em1->SetLowEnergyLimit(eth);
+  em1->SetHighEnergyLimit(100.0*TeV);
+  AddEmModel(2, em1, flucModel);
+
+  SetLinearLossLimit(0.15);
+  SetStepLimits(0.1, 0.1*mm);
+
+  isInitialised = true;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+void G4ionIonisation70::PrintInfoDefinition()
+{
+  G4VEnergyLossProcess::PrintInfoDefinition();
+
+  G4cout << "      Scaling relation is used to proton dE/dx and range"
+         << G4endl
+         << "      Bether-Bloch model for Escaled > " << eth << " MeV, ICRU49 "
+         << "parametrisation of Bragg peak below."
+         << G4endl;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+void G4ionIonisation70::SetSubCutoff(G4bool val)
+{
+  subCutoff = val;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+
