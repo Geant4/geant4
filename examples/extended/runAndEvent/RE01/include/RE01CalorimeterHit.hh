b28a5bbf7ce69c152edc4847fe8ad37b43e6e016
Joseph Perl <perl@slac.stanford.edu>
2005-06-02 21:30:50 +0000

    Add G4Atts to hits
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@22230 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/runAndEvent/RE01/include/RE01CalorimeterHit.hh
+++ examples/extended/runAndEvent/RE01/include/RE01CalorimeterHit.hh
@@ -20,7 +20,7 @@
 // * statement, and all its terms.                                    *
 // ********************************************************************
 //
-// $Id: RE01CalorimeterHit.hh,v 1.1 2004-11-26 07:37:39 asaim Exp $
+// $Id: RE01CalorimeterHit.hh,v 1.2 2005-06-02 21:30:50 perl Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 
@@ -38,6 +38,9 @@
 #include "G4RotationMatrix.hh"
 #include "RE01TrackInformation.hh"
 
+class G4AttDef;
+class G4AttValue;
+
 class RE01CalorimeterHit : public G4VHit
 {
   public:
@@ -53,6 +56,8 @@ class RE01CalorimeterHit : public G4VHit
       inline void operator delete(void *aHit);
 
       void Draw();
+      virtual const std::map<G4String,G4AttDef>* GetAttDefs() const;
+      virtual std::vector<G4AttValue>* CreateAttValues() const;
       void Print();
 
   private:
