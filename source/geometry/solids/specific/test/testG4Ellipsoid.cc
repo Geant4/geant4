0d6bf25183d365815507029ed5098dbc2aaeb196
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2005-07-04 08:32:53 +0000

    Added unit test for G4Ellipsoid by D.Anninos
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@22557 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/geometry/solids/specific/test/testG4Ellipsoid.cc
@@ -0,0 +1,123 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+//
+//
+// $Id: testG4Ellipsoid.cc
+// GEANT4 tag $Name: 
+//
+// testG4Ellipsoid
+//
+//  Test file for class G4Ellipsoid
+//
+//             Ensure asserts are compiled in
+
+#include <assert.h>
+#include <cmath>
+
+#include "globals.hh"
+#include "geomdefs.hh"
+
+#include "G4ThreeVector.hh"
+#include "G4Ellipsoid.hh"
+#include "G4RotationMatrix.hh"
+#include "G4AffineTransform.hh"
+#include "G4VoxelLimits.hh"
+
+
+G4bool testG4Ellipsoid()
+{
+
+    //test full ellipsoid
+
+    G4ThreeVector pzero(0,0,0);
+    G4ThreeVector pout ( 200*cm,200*cm,200*cm) ;
+    G4ThreeVector dir = pzero-pout ;
+    dir *= 1/dir.mag();
+
+    G4Ellipsoid t1("Solid Ellipsoid #1",
+		   20*cm,       // xSemiAxis
+		   10*cm,       // ySemiAxis
+		   15*cm,       // zSemiAxis
+		   -1*cm,       // set default cut to get full solid
+		   -1*cm) ;     // set default cut to get full solid
+
+    G4double dist = t1.DistanceToIn(pout,dir) ;
+    G4cout << "distance = " << dist << G4endl ;
+
+    // Check Inside
+
+    G4cout << "Test Inside(p) " << G4endl << G4endl ;
+
+    G4cout << "pzero : " << t1.Inside(pzero) << G4endl ;
+   
+    //test the name
+    G4cout << "The name is" << t2.GetName() << G4endl ; 
+
+    // testing the volume
+
+    G4double volume = t1.GetCubicVolume() ;
+    G4cout << G4endl << G4endl ;
+    G4cout << "Test Ellipsoid #1 Volume = " << volume / cm / cm /cm << " cm^3" << G4endl ;
+
+ 
+    //test cut ellipsoid
+    G4ThreeVector onTop(0*cm, 0*cm, 50*cm);
+    G4ThreeVector axial = onTop - pzero;
+    axial *= 1/axial.mag() ; 
+
+    G4Ellipsoid t2("Solid Ellipsoid #2",
+		   20*cm,
+		   10*cm,
+		   15*cm,
+		   10*cm,
+		   -8*cm) ;
+    
+    dist = t2.DistanceToIn(onTop,axial) ;
+    G4cout << "distance = " << dist << G4endl ;
+
+    // Check Inside
+
+    G4cout << "Test Inside(p) " << G4endl << G4endl ;
+
+    G4cout << "pzero : " << t2.Inside(pzero) << G4endl ;
+    
+    //test the name
+    G4cout << "The name is" << t2.GetName() << G4endl ; 
+
+    // testing the volume 
+
+    G4double volume = t2.GetCubicVolume() ;
+    G4cout << G4endl << G4endl ;
+    G4cout << "Test Ellipsoid #2 Volume = " << volume / cm / cm /cm << " cm^3" << G4endl ;
+
+    return true;
+}
+
+int main() 
+{ 
+#ifdef NDEBUG 
+    G4Exception("FAIL: *** Assertions must be compiled in! ***");
+#endif 
+    assert(testG4Ellipsoid()); 
+    return 0; 
+}
+
