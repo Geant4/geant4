6f08f4143f28eb8241ef544f0290937223c3d5e0
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2005-08-04 11:26:03 +0000

    Added disclaimer...
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@22742 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/geometry/solids/test/testSolidComparisons.cc
+++ source/geometry/solids/test/testSolidComparisons.cc
@@ -1,3 +1,39 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+//
+//
+// $Id: testSolidComparisons.cc,v 1.2 2005-08-04 11:26:03 gcosmo Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+// 
+// --------------------------------------------------------------
+// 
+// Test for comparison of solids of different type but similar topology
+// Returns 0 if there no inconsistencies in the answers provided by the
+// compared solids.
+//
+// Author: Dionisyos Anninos
+//
+// --------------------------------------------------------------
 #include <assert.h>
 #include <cmath>
 
@@ -57,8 +93,8 @@ G4bool compareEllipsoidtoOrb(G4int N)
     yin  = RandFlat::shoot(-10.0*cm,10.0*cm);
     zin  = RandFlat::shoot(-10.0*cm,10.0*cm);
     
-    pin  = G4ThreeVector(xin,  yin,  zin );
-    pout = G4ThreeVector(xout, yout, zout);
+    pin  = (xin,  yin,  zin );
+    pout = (xout, yout, zout);
     
     dir  = pin - pout;
     dir /= dir.mag();
