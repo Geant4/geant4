55440c48debf1a615b0e3fe91e5ca499ecfdc27f
danninos <danninos@cern.ch>
2005-08-16 10:09:20 +0000

    Added  unit test for G4EllipticalCone
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@22780 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/geometry/solids/specific/test/testG4EllipticalCone.cc
@@ -0,0 +1,155 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+//
+//
+// $Id: testG4EllipticalCone.cc
+// GEANT4 tag $Name: 
+//
+// testG4EllipticalCone
+//
+//  Test file for class G4EllipticalCone
+//
+//             Ensure asserts are compiled in
+
+#include <assert.h>
+#include <cmath>
+
+#include "globals.hh"
+#include "geomdefs.hh"
+
+#include "G4ThreeVector.hh"
+#include "G4EllipticalCone.hh"
+#include "G4RotationMatrix.hh"
+#include "G4AffineTransform.hh"
+#include "G4VoxelLimits.hh"
+
+
+G4bool testG4EllipticalCone()
+{
+
+    G4ThreeVector pzero(0,0,0);
+    G4ThreeVector pout ( 0*cm, 0*cm, 5*cm ) ;
+    G4ThreeVector dir = pzero-pout ;
+    dir *= 1/dir.mag();
+    
+    G4EllipticalCone t1("Solid EllipticalCone #1",
+			2*cm,       // xSemiAxis
+			1*cm,       // ySemiAxis
+			15*cm,      // zheight   
+			10*cm) ;    // zTopCut
+		           
+    G4double dist = t1.DistanceToOut(pout,dir) ; 
+    G4cout << "*********** Testing DistanceToOut method *************** "<<G4endl;
+    G4cout << "Distance = " << dist << G4endl <<G4endl;  
+
+    // Check Inside 
+ 
+    G4cout << "************ Test Inside(p) ****************" << G4endl ;
+    G4cout << "pzero : " << t1.Inside(pzero) << G4endl <<G4endl ; 
+    
+    //test the name
+    G4cout << "The name is : " << t1.GetName() << G4endl ;  
+
+    // testing the volume 
+ 
+    G4double volume = t1.GetCubicVolume() ; 
+    G4cout << G4endl ;
+    G4cout << "Solid EllipticalCone #1 has Volume = " << volume / cm / cm /cm << " cm^3"  
+	   << G4endl << G4endl; 
+
+    return true; 
+}
+
+// 
+//  This test generates a random point on the surface of the solid and  
+//  checks the distance from a point outside in the direction of the 
+//  line between the two points 
+//
+G4bool testDistanceToIn() 
+{    
+  G4EllipticalCone t1("Solid EllipticalCone #1", 
+		      2*cm,       // xSemiAxis
+		      1*cm,       // ySemiAxis  
+		      15*cm,      // zheight
+		      10*cm) ;    // zTopCut 
+  
+  G4int N = 10000;
+  G4int n = 0;
+  
+  for(G4int i=0; i<N; i++)
+  {
+    G4ThreeVector point = t1.GetPointOnSurface();
+    point.setX(std::fabs(point.x()));
+    point.setY(std::fabs(point.y()));
+    
+    G4ThreeVector out (100*cm, 100*cm, 100*cm);
+    
+    G4ThreeVector dir = point - out;
+    G4double dist2 = dir.mag();
+    dir /= dist2;
+    
+    G4double dist1 = t1.DistanceToIn(point,dir);
+    G4double diff = std::fabs(dist1 - dist2);
+    
+    if(diff < 2.*kCarTolerance)
+      n++;      
+  }
+
+  G4cout <<" ************   For testG4EllipticalCone   ******************"<<G4endl<<G4endl;
+  G4cout <<" Number of inconsistencies for testDistanceToIn was: "<< n <<" ..."<<G4endl
+	 <<" ... For a total of "<<N<<" trials."<< G4endl <<G4endl;
+
+  return true;
+}
+
+ 
+int main() 
+{ 
+
+  G4cout << G4endl;
+  G4cout << "*********************************************************************" <<G4endl;
+  G4cout << "****************** UNIT TEST FOR ELLIPTICAL CONE ********************" <<G4endl;
+  G4cout << "*********************************************************************" <<G4endl;
+  G4cout << G4endl;
+
+
+
+
+#ifdef NDEBUG 
+  G4Exception("FAIL: *** Assertions must be compiled in! ***");
+#endif 
+  assert(testG4EllipticalCone()); 
+    
+  G4bool what;
+  what = testDistanceToIn();
+
+  G4cout <<" Note that there is also a test for the GetPointOnSurface() method "<<G4endl
+	 << " .../solids/tests directory."<< G4endl << G4endl; 
+    
+  G4cout << G4endl;
+  G4cout << "*********************************************************************" <<G4endl;
+  G4cout << "******************* END OF TEST - THANK YOU!!! **********************" <<G4endl;
+  G4cout << "*********************************************************************" <<G4endl;
+  G4cout << G4endl;
+
+  return 0;
+}
