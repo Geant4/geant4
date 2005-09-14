e08af6aa7e9185592c49607469a374af29e6f69f
Vladimir Ivantchenko <Vladimir.Ivantchenko@cern.ch>
2005-09-14 09:04:26 +0000

    fix problem of changing cuts btwen runs
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@22968 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/electromagnetic/TestEm9/include/DetectorConstruction.hh
+++ examples/extended/electromagnetic/TestEm9/include/DetectorConstruction.hh
@@ -21,7 +21,7 @@
 // ********************************************************************
 //
 //
-// $Id: DetectorConstruction.hh,v 1.3 2004-05-27 13:43:17 vnivanch Exp $
+// $Id: DetectorConstruction.hh,v 1.4 2005-09-14 09:04:26 vnivanch Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 //
@@ -112,7 +112,6 @@ private:
   G4Region*   muonRegion;
 
   DetectorMessenger* detectorMessenger;  //pointer to the Messenger
-
 };
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo.....
