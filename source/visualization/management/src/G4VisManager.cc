97aa11e29ff1875631d480236b4e1cb7158c14d5
John Allison <John.Allison@cern.ch>
2005-10-13 18:17:40 +0000

    Migrated to <sstream>.
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@23283 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/visualization/management/src/G4VisManager.cc
+++ source/visualization/management/src/G4VisManager.cc
@@ -21,7 +21,7 @@
 // ********************************************************************
 //
 //
-// $Id: G4VisManager.cc,v 1.65 2005-10-13 18:14:31 allison Exp $
+// $Id: G4VisManager.cc,v 1.66 2005-10-13 18:17:40 allison Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // 
