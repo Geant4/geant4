64e035a1dcedf39d9660ae63b4774cfd7ae0d8a9
Vladimir Ivantchenko <Vladimir.Ivantchenko@cern.ch>
2005-11-11 11:13:07 +0000

    reorganisation of EM builders
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@23612 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- physics_lists/hadronic/Packaging/include/G4EmStandardBuilder.hh
+++ physics_lists/hadronic/Packaging/include/G4EmStandardBuilder.hh
@@ -20,53 +20,46 @@
 // * statement, and all its terms.                                    *
 // ********************************************************************
 //
-// $Id: G4EMBuilder.hh,v 1.2 2005-11-11 11:13:07 vnivanch Exp $
+//
+// $Id: G4EmStandardBuilder.hh,v 1.1 2005-11-11 11:12:01 vnivanch Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 //---------------------------------------------------------------------------
 //
-// ClassName:   G4EMBuilder
+// ClassName:   G4EmStandardBuilder
 //
-// Author: 2002 J.P. Wellisch
+// Author:      V.Ivanchenko 09.11.2009
 //
 // Modified:
-// 10.11.2005 V.Ivanchenko edit to provide a standard
 //
 //----------------------------------------------------------------------------
 //
 
-#ifndef G4EMBuilder_h
-#define G4EMBuilder_h 1
+#ifndef G4EmStandardBuilder_h
+#define G4EmStandardBuilder_h 1
 
-#include "G4EMTailorer.hh"
+#include "G4VPhysicsConstructor.hh"
 #include "globals.hh"
 
-class G4SynchrotronRadiation;
-class G4ElectroNuclearBuilder;
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-class G4EMBuilder
+class G4EmStandardBuilder : public G4VPhysicsConstructor
 {
 public:
-  G4EMBuilder();
-  virtual ~G4EMBuilder();
+  G4EmStandardBuilder(const G4String& name = "EMstandard");
+  virtual ~G4EmStandardBuilder();
 
-  void Build();
-
-  void Synch(G4String & aState);
-  void GammaNuclear(G4String & aState);
-
-private:
-  G4bool wasActivated;
-  G4bool synchOn;
-  G4bool gammNucOn;
-
-  G4EMTailorer * theT;
-  G4SynchrotronRadiation*  theElectronSynch;
-  G4SynchrotronRadiation*  thePositronSynch;
-  G4ElectroNuclearBuilder* theGNPhysics;
+public:
+  virtual void ConstructParticle();
+  virtual void ConstructProcess();
 };
 
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
 #endif
+
+
+
 
 
 
