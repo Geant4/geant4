fe0de7f813379953ffb673f85e57176739bd87b9
John Apostolakis <John.Apostolakis@cern.ch>
2005-11-19 02:23:16 +0000

    First version of interface
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@23807 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/geometry/management/include/G4VVolumeMaterialScanner.hh
@@ -0,0 +1,53 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+//
+//
+// $Id: G4VVolumeMaterialScanner.hh,v 1.1 2005-11-19 02:23:16 japost Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+// class G4VVolumeMaterialScanner
+//
+// Class description:
+//
+// Interface for repeated volumes or parameterisations that are able to 
+//  tabulate their materials.
+
+// History:
+//  18.11.05  J.Apostolakis  First version
+
+#ifndef G4VVOLUMEMATERIALSCANNER_HH
+#define G4VVOLUMEMATERIALSCANNER_HH
+class G4Material;
+// #include "G4Material.hh"
+
+class G4VVolumeMaterialScanner
+{
+  public:
+    G4VVolumeMaterialScanner() {}
+    virtual ~G4VVolumeMaterialScanner() {}
+
+    virtual G4int       GetNumberOfMaterials() const =0;
+    virtual G4Material* GetMaterial(G4int idx) const =0;
+};
+   
+#endif
+ 
