dbc872a7409705f320bb1d6222f0b7a5c0b79b01
Vladimir Ivantchenko <Vladimir.Ivantchenko@cern.ch>
2005-12-02 09:56:24 +0000

    add DecayPhysics
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@24286 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ physics_lists/hadronic/Packaging/include/G4DecayPhysics.hh
@@ -0,0 +1,75 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+//
+// $Id: G4DecayPhysics.hh,v 1.1 2005-12-02 09:56:24 vnivanch Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+//---------------------------------------------------------------------------
+//
+// ClassName:   G4DecayPhysics
+//
+// Author: 2002 J.P. Wellisch
+//
+// Modified:
+// 10.11.2005 V.Ivanchenko edit to provide a standard
+//
+//----------------------------------------------------------------------------
+//
+
+#ifndef G4DecayPhysics_h
+#define G4DecayPhysics_h 1
+
+#include "globals.hh"
+#include "G4VPhysicsConstructor.hh"
+
+class G4Decay;
+
+class G4DecayPhysics : public G4VPhysicsConstructor
+{
+  public: 
+    G4DecayPhysics(const G4String& name = "decay");
+    virtual ~G4DecayPhysics();
+
+  public: 
+    // This method will be invoked in the Construct() method. 
+    // each particle type will be instantiated
+  virtual void ConstructParticle();
+ 
+    // This method will be invoked in the Construct() method.
+    // each physics process will be instantiated and
+    // registered to the process manager of each particle type 
+  virtual void ConstructProcess();
+
+private:
+  G4Decay* fDecayProcess;
+  G4bool   wasActivated;
+};
+
+#endif
+
+
+
+
+
+
+
+
---
+++ physics_lists/hadronic/Packaging/src/G4DecayPhysics.cc
@@ -0,0 +1,109 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+//
+// $Id: G4DecayPhysics.cc,v 1.1 2005-12-02 09:56:24 vnivanch Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+//---------------------------------------------------------------------------
+//
+// ClassName:   G4DecayPhysics
+//
+// Author: 2002 J.P. Wellisch
+//
+// Modified:
+// 10.11.2005 V.Ivanchenko edit to provide a standard
+//
+//----------------------------------------------------------------------------
+//
+
+#include "G4DecayPhysics.hh"
+
+#include "G4Decay.hh"
+
+#include "G4ParticleDefinition.hh"
+#include "G4ProcessManager.hh"
+
+#include "G4BosonConstructor.hh"
+#include "G4LeptonConstructor.hh"
+#include "G4MesonConstructor.hh"
+#include "G4BosonConstructor.hh"
+#include "G4BaryonConstructor.hh"
+#include "G4IonConstructor.hh"
+#include "G4ShortLivedConstructor.hh"
+
+
+G4DecayPhysics::G4DecayPhysics(const G4String& name)
+  :  G4VPhysicsConstructor(name), wasActivated(false)
+{}
+
+G4DecayPhysics::~G4DecayPhysics()
+{
+  if(wasActivated) delete fDecayProcess;
+}
+
+void G4DecayPhysics::ConstructParticle()
+{
+
+ G4cout << "G4DecayPhysics::ConstructParticle" << G4endl;
+  G4BosonConstructor  pBosonConstructor; 
+  pBosonConstructor.ConstructParticle();
+
+  G4LeptonConstructor pLeptonConstructor;
+  pLeptonConstructor.ConstructParticle();
+
+  G4MesonConstructor pMesonConstructor;
+  pMesonConstructor.ConstructParticle();
+
+  G4BaryonConstructor pBaryonConstructor;
+  pBaryonConstructor.ConstructParticle();
+
+  G4IonConstructor pIonConstructor;
+  pIonConstructor.ConstructParticle();
+
+  G4ShortLivedConstructor pShortLivedConstructor;
+  pShortLivedConstructor.ConstructParticle();  
+}
+
+void G4DecayPhysics::ConstructProcess()
+{
+  wasActivated = true;
+
+  // Add Decay Process
+  fDecayProcess = new G4Decay();
+  theParticleIterator->reset();
+  G4ParticleDefinition* particle=0;
+  G4ProcessManager* pmanager=0;
+
+  while( (*theParticleIterator)() )
+  {
+    particle = theParticleIterator->value();
+    pmanager = particle->GetProcessManager();
+    if( fDecayProcess->IsApplicable(*particle) ) 
+    { 
+      pmanager -> AddProcess(fDecayProcess);
+      pmanager -> SetProcessOrdering(fDecayProcess, idxPostStep);
+      pmanager -> SetProcessOrdering(fDecayProcess, idxAtRest);
+    }
+  }
+}
+
+
