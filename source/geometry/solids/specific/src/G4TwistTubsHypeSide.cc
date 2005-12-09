8c4d93c3c3582add2c09cb33d0cfd014d730a116
Oliver Link <link@cern.ch>
2005-12-09 13:38:15 +0000

    Fixed visibility of soft edges.
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@24522 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/geometry/solids/specific/src/G4TwistTubsHypeSide.cc
+++ source/geometry/solids/specific/src/G4TwistTubsHypeSide.cc
@@ -21,7 +21,7 @@
 // ********************************************************************
 //
 //
-// $Id: G4TwistTubsHypeSide.cc,v 1.3 2005-12-06 09:22:13 gcosmo Exp $
+// $Id: G4TwistTubsHypeSide.cc,v 1.4 2005-12-09 13:38:13 link Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // 
@@ -963,10 +963,11 @@ void G4TwistTubsHypeSide::GetFacets( G4int m, G4int n, G4double xyz[][3],
         
         nface = GetFace(i,j,m,n,iside) ;
 
-        faces[nface][0] = GetNode(i  ,j  ,m,n,iside)+1 ;  
-        faces[nface][1] = GetNode(i+1,j  ,m,n,iside)+1 ;
-        faces[nface][2] = GetNode(i+1,j+1,m,n,iside)+1 ;
-        faces[nface][3] = GetNode(i  ,j+1,m,n,iside)+1 ;
+	faces[nface][0] = GetEdgeVisibility(i,j,m,n,0,1) * ( GetNode(i  ,j  ,m,n,iside)+1) ;  
+	faces[nface][1] = GetEdgeVisibility(i,j,m,n,1,1) * ( GetNode(i+1,j  ,m,n,iside)+1) ;
+	faces[nface][2] = GetEdgeVisibility(i,j,m,n,2,1) * ( GetNode(i+1,j+1,m,n,iside)+1) ;
+	faces[nface][3] = GetEdgeVisibility(i,j,m,n,3,1) * ( GetNode(i  ,j+1,m,n,iside)+1) ;
+
       }
     }
   }
