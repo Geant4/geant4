d04a0b4682beafb395f65e379f309a0df99cfb0d
Alberto Ribon <Alberto.Ribon@cern.ch>
2005-12-12 10:21:32 +0000

    Added in unpack.py information by calorimeter type, beam particle type, and beam energy.
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@24546 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/models/verification/full_setups/StatAccepTest/dirObservables/unpack.py
+++ source/processes/hadronic/models/verification/full_setups/StatAccepTest/dirObservables/unpack.py
@@ -1,5 +1,5 @@
 #-------------------------------------------------------------------
-# Last update: 6-Dec-2005
+# Last update: 12-Dec-2005
 #
 # This script should be run in the directory which has, as immediate
 # subdirectories, the results of the Grid validation testing,
@@ -10,8 +10,9 @@
 # the tar-ball, keep a statistics of the files that should be contained
 # in such tar-ball, and look if the jobs have started running but not
 # terminated normally.
-# At the end, it prints out the above information, and also produces
-# the following files:
+# At the end, it prints out the above information, both as overall
+# statistics and also by calorimeter, by beam particle type, and
+# by beam energy, and also produces the following files:
 #   o  listDirFailed.txt : a list of the directories whose tar-ball
 #                          do not have the expected files.
 #   o  listDirRunCrashed.txt : a list of directories in which the
@@ -31,6 +32,92 @@
 import os
 import sys
 import string
+
+# The following dictionaries keep track, calorimeter by calorimeter,
+# beam particle by beam particle, and beam energy by beam energy,
+# of the number of jobs which succeeded (i.e. there is the file
+# "outputPvalues.log-..."), the corresponding fractions, the number
+# of .PS files which are produced, and their fractions.
+
+dictCalorimetersOK = { 'FeSci':0,
+                       'CuSci':0,
+                       'CuLAr':0,
+                       'WLAr':0,
+                       'PbSci':0,
+                       'PbLAr':0,
+                       'PbWO4':0 }
+
+dictBeamParticlesOK = { 'e-':0,
+                        'pi+':0,
+                        'pi-':0,
+                        'k+':0,
+                        'k-':0,
+                        'k0L':0,
+                        'p':0,
+                        'n':0 }
+
+dictBeamEnergiesOK = { '1GeV':0,
+                       '2GeV':0,
+                       '3GeV':0,
+                       '4GeV':0,
+                       '5GeV':0,
+                       '6GeV':0,
+                       '7GeV':0,
+                       '8GeV':0,
+                       '9GeV':0,
+                       '10GeV':0,
+                       '20GeV':0,
+                       '30GeV':0,
+                       '50GeV':0,
+                       '60GeV':0,
+                       '80GeV':0,
+                       '100GeV':0,
+                       '120GeV':0,
+                       '150GeV':0,
+                       '180GeV':0,
+                       '200GeV':0,
+                       '250GeV':0,
+                       '300GeV':0 }
+
+dictCalorimetersPS = { 'FeSci':0,
+                       'CuSci':0,
+                       'CuLAr':0,
+                       'WLAr':0,
+                       'PbSci':0,
+                       'PbLAr':0,
+                       'PbWO4':0 }
+
+dictBeamParticlesPS = { 'e-':0,
+                        'pi+':0,
+                        'pi-':0,
+                        'k+':0,
+                        'k-':0,
+                        'k0L':0,
+                        'p':0,
+                        'n':0 }
+
+dictBeamEnergiesPS = { '1GeV':0,
+                       '2GeV':0,
+                       '3GeV':0,
+                       '4GeV':0,
+                       '5GeV':0,
+                       '6GeV':0,
+                       '7GeV':0,
+                       '8GeV':0,
+                       '9GeV':0,
+                       '10GeV':0,
+                       '20GeV':0,
+                       '30GeV':0,
+                       '50GeV':0,
+                       '60GeV':0,
+                       '80GeV':0,
+                       '100GeV':0,
+                       '120GeV':0,
+                       '150GeV':0,
+                       '180GeV':0,
+                       '200GeV':0,
+                       '250GeV':0,
+                       '300GeV':0 }
 
 listDirFailed = open( "listDirFailed.txt", "w" )
 listDirRunCrashed = open( "listDirRunCrashed.txt", "w" )
@@ -54,7 +141,7 @@ for dir in listDir :
     currentDir = os.getcwd()
     #print " I am in directory: ", currentDir
 
-    os.system( "tar xvfz *.tgz" )       #***LOOKHERE**
+    #os.system( "tar xvfz *.tgz" )       #***LOOKHERE**
 
     os.system( "ls -1 > listFiles.txt" )
     listFiles = open( "listFiles.txt", "r" )
@@ -84,10 +171,28 @@ for dir in listDir :
             foundNtuples = 1
         if ( iFile.find( "outputPvalues.log" ) > -1 ) :
             foundPvalues = 1
+            for iDetector in dictCalorimetersOK.keys() :
+                if ( iFile.find( "-" + iDetector + "-" ) > -1 ) :
+                    dictCalorimetersOK[ iDetector ] += 1
+            for iParticle in dictBeamParticlesOK.keys() :
+                if ( iFile.find( "-" + iParticle + "-" ) > -1 ) :
+                    dictBeamParticlesOK[ iParticle ] += 1
+            for iEnergy in dictBeamEnergiesOK.keys() :
+                if ( iFile.find( "-" + iEnergy + "-" ) > -1 ) :
+                    dictBeamEnergiesOK[ iEnergy ] += 1
         if ( iFile.find( "plot.ps" ) > -1 ) :
             foundPS = 1
             countPS += 1
             listPS.write( currentDir + "/" + iFile )
+            for iDetector in dictCalorimetersPS.keys() :
+                if ( iFile.find( "-" + iDetector + "-" ) > -1 ) :
+                    dictCalorimetersPS[ iDetector ] += 1
+            for iParticle in dictBeamParticlesPS.keys() :
+                if ( iFile.find( "-" + iParticle + "-" ) > -1 ) :
+                    dictBeamParticlesPS[ iParticle ] += 1
+            for iEnergy in dictBeamEnergiesPS.keys() :
+                if ( iFile.find( "-" + iEnergy + "-" ) > -1 ) :
+                    dictBeamEnergiesPS[ iEnergy ] += 1
     if ( foundRunCrashed ) :
         countDirWithRunCrashes += 1
         listDirRunCrashed.write( currentDir + "\n" )        
@@ -111,6 +216,37 @@ print " Number of directories with Ntuples = ", countDirWithNtuples
 print " Number of directories with p-values = ", countDirWithPvalues
 print " Number of directories with .PS = ", countDirWithPS
 print " Number of .PS = ", countPS
+print " "
+print " Number of OK jobs and PS files for calorimeter types:"
+for iDetector in dictCalorimetersOK.keys() :
+    print "  calorimeter=", iDetector, \
+          "  #OK=", dictCalorimetersOK[ iDetector ], " (", \
+          int( dictCalorimetersOK[ iDetector ]*100.0 /
+               float( countDirWithPvalues ) ), "%)", \
+          "  #PS=", dictCalorimetersPS[ iDetector ], " (", \
+          int( dictCalorimetersPS[ iDetector ]*100.0 /
+               float( countPS ) ), "%)"
+print " "
+print " Number of OK jobs and PS files for beam particles:"
+for iParticle in dictBeamParticlesOK.keys() :
+    print "  particle=", iParticle, \
+          "  #OK=", dictBeamParticlesOK[ iParticle ], " (", \
+          int( dictBeamParticlesOK[ iParticle ]*100.0 /
+               float( countDirWithPvalues ) ), "%)", \
+          "  #PS=", dictBeamParticlesPS[ iParticle ], " (", \
+          int( dictBeamParticlesPS[ iParticle ]*100.0 /
+               float( countPS ) ), "%)"
+print " "
+print " Number of OK jobs and PS files for beam energies:"
+for iEnergy in dictBeamEnergiesOK.keys() :
+    print "  energy=", iEnergy, \
+          "  #OK=", dictBeamEnergiesOK[ iEnergy ], " (", \
+          int( dictBeamEnergiesOK[ iEnergy ]*100.0 /
+               float( countDirWithPvalues ) ), "%)", \
+          "  #PS=", dictBeamEnergiesPS[ iEnergy ], " (", \
+          int( dictBeamEnergiesPS[ iEnergy ]*100.0 /
+               float( countPS ) ), "%)"
+print " "
 
 listDir.close()
 listDirFailed.close()
