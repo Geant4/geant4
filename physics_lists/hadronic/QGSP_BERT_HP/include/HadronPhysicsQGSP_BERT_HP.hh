4613d67915619f96a1ea0cf6d2967cfbf617f98b
Gunter Folger <Gunter.Folger@cern.ch>
2005-12-15 20:33:01 +0000

    ported to 8.0
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@24613 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- physics_lists/hadronic/QGSP_BERT_HP/include/HadronPhysicsQGSP_BERT_HP.hh
+++ physics_lists/hadronic/QGSP_BERT_HP/include/HadronPhysicsQGSP_BERT_HP.hh
@@ -1,3 +1,39 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * * the following disclaimer summarizes all * the specific disclaimers *
+// * of contributors to this software. * the specific disclaimers,which *
+// * govern, are listed with * their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither * the authors of this software system, nor * their employing *
+// * institutes,nor * the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is * the  intellectual property  of * the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying * the Program (or any work *
+// * based  on  * the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+//
+// $Id: HadronPhysicsQGSP_BERT_HP.hh,v 1.2 2005-12-15 20:33:01 gunter Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+//---------------------------------------------------------------------------
+//
+// ClassName:   HadronPhysicsQGSP_BERT_HP
+//
+// Author: 2002 J.P. Wellisch
+//
+// Modified:
+// 15.12.2005 G.Folger: migration to non static particles
+//
+//----------------------------------------------------------------------------
+//
 #ifndef HadronPhysicsQGSP_BERT_HP_h
 #define HadronPhysicsQGSP_BERT_HP_h 1
 
@@ -5,7 +41,6 @@
 #include "G4ios.hh"
 
 #include "G4VPhysicsConstructor.hh"
-#include "G4HadronQEDBuilder.hh"
 #include "G4StoppingHadronBuilder.hh"
 #include "G4MiscLHEPBuilder.hh"
 
@@ -36,25 +71,25 @@ class HadronPhysicsQGSP_BERT_HP : public G4VPhysicsConstructor
     virtual void ConstructProcess();
 
   private:
-    G4NeutronBuilder theNeutrons;
-    G4LEPNeutronBuilder theLEPNeutron;
-    G4QGSPNeutronBuilder theQGSPNeutron;
-    G4BertiniNeutronBuilder theBertiniNeutron;
-    G4NeutronHPBuilder theHPNeutron;
+    void CreateModels();
+    G4NeutronBuilder * theNeutrons;
+    G4LEPNeutronBuilder * theLEPNeutron;
+    G4QGSPNeutronBuilder * theQGSPNeutron;
+    G4BertiniNeutronBuilder * theBertiniNeutron;
+    G4NeutronHPBuilder * theHPNeutron;
     
-    G4PiKBuilder thePiK;
-    G4LEPPiKBuilder theLEPPiK;
-    G4QGSPPiKBuilder theQGSPPiK;
-    G4BertiniPiKBuilder theBertiniPiK;
+    G4PiKBuilder * thePiK;
+    G4LEPPiKBuilder * theLEPPiK;
+    G4QGSPPiKBuilder * theQGSPPiK;
+    G4BertiniPiKBuilder * theBertiniPiK;
     
-    G4ProtonBuilder thePro;
-    G4LEPProtonBuilder theLEPPro;
-    G4QGSPProtonBuilder theQGSPPro; 
-    G4BertiniProtonBuilder theBertiniPro;
+    G4ProtonBuilder * thePro;
+    G4LEPProtonBuilder * theLEPPro;
+    G4QGSPProtonBuilder * theQGSPPro; 
+    G4BertiniProtonBuilder * theBertiniPro;
     
-    G4MiscLHEPBuilder theMiscLHEP;
-    G4StoppingHadronBuilder theStoppingHadron;
-    G4HadronQEDBuilder theHadronQED;
+    G4MiscLHEPBuilder * theMiscLHEP;
+    G4StoppingHadronBuilder * theStoppingHadron;
 };
 
 // 2002 by J.P. Wellisch
--- physics_lists/hadronic/QGSP_BERT_HP/src/HadronPhysicsQGSP_BERT_HP.cc
+++ physics_lists/hadronic/QGSP_BERT_HP/src/HadronPhysicsQGSP_BERT_HP.cc
@@ -1,3 +1,39 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+//
+// $Id: HadronPhysicsQGSP_BERT_HP.cc,v 1.2 2005-12-15 20:33:01 gunter Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+//---------------------------------------------------------------------------
+//
+// ClassName:   HadronPhysicsQGSP_BERT_HP
+//
+// Author: 2002 J.P. Wellisch
+//
+// Modified:
+// 15.12.2005 G.Folger: migration to non static particles
+//
+//----------------------------------------------------------------------------
+//
 #include "HadronPhysicsQGSP_BERT_HP.hh"
 
 #include "globals.hh"
@@ -12,34 +48,64 @@
 
 HadronPhysicsQGSP_BERT_HP::HadronPhysicsQGSP_BERT_HP(const G4String& name)
                     :  G4VPhysicsConstructor(name) 
-{
-  theNeutrons.RegisterMe(&theQGSPNeutron);
-  theNeutrons.RegisterMe(&theLEPNeutron);
-  theNeutrons.RegisterMe(&theBertiniNeutron);
-  theNeutrons.RegisterMe(&theHPNeutron);
-  theLEPNeutron.SetMaxInelasticEnergy(25*GeV);
-  theLEPNeutron.SetMinEnergy(19.9*MeV);
-  theLEPNeutron.SetMinInelasticEnergy(9.5*GeV);
-  theBertiniNeutron.SetMaxEnergy(9.9*GeV);
-  theBertiniNeutron.SetMinEnergy(19.9*MeV);
+{}
 
-  thePro.RegisterMe(&theQGSPPro);
-  thePro.RegisterMe(&theLEPPro);
-  thePro.RegisterMe(&theBertiniPro);
-  theLEPPro.SetMaxEnergy(25*GeV);
-  theLEPPro.SetMinEnergy(9.5*GeV);
-  theBertiniPro.SetMaxEnergy(9.9*GeV);
+void HadronPhysicsQGSP_BERT_HP::CreateModels()
+{
+  theNeutrons=new G4NeutronBuilder;
+  theNeutrons->RegisterMe(theQGSPNeutron=new G4QGSPNeutronBuilder);
+  theNeutrons->RegisterMe(theLEPNeutron=new G4LEPNeutronBuilder);
+  theLEPNeutron->SetMinEnergy(19.9*MeV);
+  theLEPNeutron->SetMinInelasticEnergy(9.5*GeV);
+  theLEPNeutron->SetMaxInelasticEnergy(25*GeV);  
+
+  theNeutrons->RegisterMe(theBertiniNeutron=new G4BertiniNeutronBuilder);
+  theBertiniNeutron->SetMinEnergy(19.9*MeV);
+  theBertiniNeutron->SetMaxEnergy(9.9*GeV);
+
+  theNeutrons->RegisterMe(theHPNeutron=new G4NeutronHPBuilder);
+
+  thePro=new G4ProtonBuilder;
+  thePro->RegisterMe(theQGSPPro=new G4QGSPProtonBuilder);
+  thePro->RegisterMe(theLEPPro=new G4LEPProtonBuilder);
+  theLEPPro->SetMinEnergy(9.5*GeV);
+  theLEPPro->SetMaxEnergy(25*GeV);
+
+  thePro->RegisterMe(theBertiniPro=new G4BertiniProtonBuilder);
+  theBertiniPro->SetMaxEnergy(9.9*GeV);
+
+  thePiK=new G4PiKBuilder;
+  thePiK->RegisterMe(theQGSPPiK=new G4QGSPPiKBuilder);
+  thePiK->RegisterMe(theLEPPiK=new G4LEPPiKBuilder);
+  theLEPPiK->SetMaxEnergy(25*GeV);
+  theLEPPiK->SetMinPionEnergy(9.5*GeV);
+
+  thePiK->RegisterMe(theBertiniPiK=new G4BertiniPiKBuilder);
+  theBertiniPiK->SetMaxEnergy(9.9*GeV);
+
   
-  thePiK.RegisterMe(&theQGSPPiK);
-  thePiK.RegisterMe(&theLEPPiK);
-  thePiK.RegisterMe(&theBertiniPiK);
-  theLEPPiK.SetMaxEnergy(25*GeV);
-  theLEPPiK.SetMinPionEnergy(9.5*GeV);
-  theBertiniPiK.SetMaxEnergy(9.9*GeV);
-  
+  theMiscLHEP=new G4MiscLHEPBuilder;
+  theStoppingHadron=new G4StoppingHadronBuilder;
+
 }
 
-HadronPhysicsQGSP_BERT_HP::~HadronPhysicsQGSP_BERT_HP() {}
+HadronPhysicsQGSP_BERT_HP::~HadronPhysicsQGSP_BERT_HP() 
+{
+   delete theStoppingHadron;
+   delete theMiscLHEP;
+   delete theQGSPNeutron;
+   delete theLEPNeutron;
+   delete theBertiniNeutron;
+   delete theHPNeutron;
+   delete theQGSPPro;
+   delete theLEPPro;
+   delete thePro;
+   delete theBertiniPro;
+   delete theQGSPPiK;
+   delete theLEPPiK;
+   delete theBertiniPiK;
+   delete thePiK;
+}
 
 void HadronPhysicsQGSP_BERT_HP::ConstructParticle()
 {
@@ -56,11 +122,11 @@ void HadronPhysicsQGSP_BERT_HP::ConstructParticle()
 #include "G4ProcessManager.hh"
 void HadronPhysicsQGSP_BERT_HP::ConstructProcess()
 {
-  theNeutrons.Build();
-  thePro.Build();
-  thePiK.Build();
-  theMiscLHEP.Build();
-  theStoppingHadron.Build();
-  theHadronQED.Build();
+  CreateModels();
+  theNeutrons->Build();
+  thePro->Build();
+  thePiK->Build();
+  theMiscLHEP->Build();
+  theStoppingHadron->Build();
 }
 // 2002 by J.P. Wellisch
