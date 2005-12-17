66ac4ed7c7efd65c1f0e847d2646ef2086fea432
Alberto Ribon <Alberto.Ribon@cern.ch>
2005-12-17 11:16:40 +0000

    Removed obsolete code for visualization.
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@24646 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/models/verification/full_setups/StatAccepTest/src/StatAccepTestRunAction.cc
+++ source/processes/hadronic/models/verification/full_setups/StatAccepTest/src/StatAccepTestRunAction.cc
@@ -2,10 +2,6 @@
 #include "globals.hh"
 #include "G4Run.hh"
 
-#include "G4UImanager.hh"
-#include "G4VVisManager.hh"
-#include "G4ios.hh"
-
 #include "StatAccepTestAnalysis.hh"
 
 
@@ -16,30 +12,14 @@ StatAccepTestRunAction::~StatAccepTestRunAction() {
 
 
 void StatAccepTestRunAction::BeginOfRunAction(const G4Run* aRun) {
-
   G4cout << "### Run " << aRun->GetRunID() << " start." << G4endl;
-
   // Reset the histograms/ntuples of the eventual previous run.
   StatAccepTestAnalysis::getInstance()->init();
-
-  // For the visualization of the event.
-  if ( G4VVisManager::GetConcreteInstance() ) {
-    G4UImanager* UI = G4UImanager::GetUIpointer(); 
-    UI->ApplyCommand("/vis/scene/notifyHandlers");
-  } 
-  
 }
 
 
 void StatAccepTestRunAction::EndOfRunAction(const G4Run* ) {
-
   // Commit the histograms/ntuples.
   StatAccepTestAnalysis::getInstance()->finish();
-
-  // For the visualization of the event.
-  if (G4VVisManager::GetConcreteInstance()) {
-     G4UImanager::GetUIpointer()->ApplyCommand("/vis/viewer/update");
-  }
-
 }   
 
