8b4583fb953cad4515f8356b60d5c70e5995ce14
John Apostolakis <John.Apostolakis@cern.ch>
2006-02-06 16:14:59 +0000

    Added StartTracking() method, which enables correction of a problem with suspended tracks.
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@25020 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/transportation/include/G4Transportation.hh
+++ source/processes/transportation/include/G4Transportation.hh
@@ -21,7 +21,7 @@
 // ********************************************************************
 //
 //
-// $Id: G4Transportation.hh,v 1.10 2004-11-23 17:45:43 japost Exp $
+// $Id: G4Transportation.hh,v 1.11 2006-02-06 16:14:59 japost Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // 
@@ -128,6 +128,9 @@ class G4Transportation : public G4VProcess
                              const G4Step&
                             ) {return 0;};
        // No operation in  AtRestDoIt.
+
+  void StartTracking(G4Track* aTrack);
+       // Reset state for new (potentially resumed) track 
 
   protected:
 
