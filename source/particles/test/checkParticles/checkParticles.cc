a43bf7a44479e00774f0f07c606454b6053a872b
Hisaya Kurashige <kurasige@phys.sci.kobe-u.ac.jp>
2006-02-28 10:19:11 +0000

    Add SetReadiness by Hisaya
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@25145 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/particles/test/checkParticles/checkParticles.cc
+++ source/particles/test/checkParticles/checkParticles.cc
@@ -21,7 +21,7 @@
 // ********************************************************************
 //
 //
-// $Id: checkParticles.cc,v 1.6 2004-12-02 08:12:17 kurasige Exp $
+// $Id: checkParticles.cc,v 1.7 2006-02-28 10:19:11 kurasige Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // 
@@ -29,6 +29,7 @@
 #include "G4ios.hh"
 #include "globals.hh"
 #include "tstParticleConstructor.hh"
+#include "G4ParticleTable.hh"
 #include <fstream>
 #include <iomanip>
 
@@ -39,7 +40,7 @@ int main(int argc,char** argv) {
   // PDG computer-readble file for particle properties
   //  see http://www-pdg.lbl.gov/computer_read.html
   std::ifstream pdgFile;
-  G4String pdgFileName = "mass_width_02.mc";
+  G4String pdgFileName = "mass_width_2004.mc";
   if (argc > 1) pdgFileName =  argv[1];
  
   // open the pdg file
@@ -48,6 +49,9 @@ int main(int argc,char** argv) {
     G4cerr << "pdg data file <" << pdgFileName << "> could not open." << G4endl;
 	exit(1);
   }
+
+  // set Readiness
+  G4ParticleTable::GetParticleTable()->SetReadiness();
 
   // create all particles
   tstParticleConstructor pConstructor;
