a2d8e43b44134f590b7a73d59fd0ba7b1ea76a56
Alberto Ribon <Alberto.Ribon@cern.ch>
2006-04-06 16:34:09 +0000

    Fixed overcounting of neutrons tracks with _HP Physics Lists, due to the suspensions of these tracks, and the fact that the method  G4UserTrackingAction::PreUserTrackingAction  is called not only once at the creation of a track, but also each time a suspended track is resumed.
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@25481 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/models/verification/full_setups/StatAccepTest/src/StatAccepTestTrackingAction.cc
+++ source/processes/hadronic/models/verification/full_setups/StatAccepTest/src/StatAccepTestTrackingAction.cc
@@ -1,5 +1,6 @@
 #include "StatAccepTestTrackingAction.hh"
 #include "StatAccepTestAnalysis.hh"
+#include "G4Track.hh"
 
 
 StatAccepTestTrackingAction::StatAccepTestTrackingAction() {}
@@ -9,7 +10,19 @@ StatAccepTestTrackingAction::~StatAccepTestTrackingAction() {}
 
 
 void StatAccepTestTrackingAction::PreUserTrackingAction( const G4Track* aTrack ) {
-  StatAccepTestAnalysis::getInstance()->infoTrack( aTrack );
+
+  // This method is called not only once when a particle is created, 
+  // but also each time it is resumed, in the case the track gets
+  // suspended, as it happens in the case of neutrons with _HP
+  // Physics Lists. To be sure that we collect information about
+  // a track one and only once, we require that the current step
+  // be the first one.
+  if ( aTrack->GetCurrentStepNumber() == 0 ) {
+    StatAccepTestAnalysis::getInstance()->infoTrack( aTrack );
+  } else {
+    //G4cout << " aTrack->GetCurrentStepNumber() = " 
+    //	     <<  aTrack->GetCurrentStepNumber() << G4endl;  //***DEBUG***
+  }
 }
 
 
