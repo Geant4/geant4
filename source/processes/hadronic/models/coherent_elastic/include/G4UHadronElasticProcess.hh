3f43f7ac3dd813e3fdf488d4e1c632d0fe5221f8
Vladimir Ivantchenko <Vladimir.Ivantchenko@cern.ch>
2006-04-24 11:06:04 +0000

    add process
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@25622 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/processes/hadronic/models/coherent_elastic/include/G4UHadronElasticProcess.hh
@@ -0,0 +1,89 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+//
+//
+//
+// Geant4 Hadron Elastic Scattering Process -- header file
+// 
+// Created 21 April 2006 V.Ivanchenko
+//  
+// Modified:
+//
+
+// Class Description
+// Process for hadron nuclear elastic scattering using optimal 
+// combination of Geant4 models
+// Class Description - End
+
+#ifndef G4UHadronElasticProcess_h
+#define G4UHadronElasticProcess_h 1
+ 
+#include "globals.hh"
+#include "G4HadronicProcess.hh"
+#include "G4Nucleus.hh"
+
+class G4VQCrossSection;
+class G4ParticleDefinition;
+class G4CrossSectionDataStore;
+
+class G4UHadronElasticProcess : public G4HadronicProcess
+{
+public:
+
+  G4UHadronElasticProcess(const G4String& processName = "hElastic", G4bool fl = true);
+
+  virtual ~G4UHadronElasticProcess();
+ 
+  virtual G4VParticleChange* PostStepDoIt(const G4Track& aTrack, 
+					  const G4Step& aStep);
+
+  virtual G4bool IsApplicable(const G4ParticleDefinition& aParticleType);
+
+  virtual void BuildPhysicsTable(const G4ParticleDefinition& aParticleType);
+
+  virtual void DumpPhysicsTable(const G4ParticleDefinition& aParticleType);
+
+  virtual G4double GetMeanFreePath(const G4Track&, G4double, G4ForceCondition*);
+
+  virtual G4double GetMicroscopicCrossSection(const G4DynamicParticle* aParticle,
+					      const G4Element* anElement,
+					      G4double aTemp);
+
+private:
+
+  G4VQCrossSection*           qCManager;
+  const G4ParticleDefinition* theProton;
+  const G4ParticleDefinition* theNeutron;
+  const G4ParticleDefinition* theParticle;
+
+  G4CrossSectionDataStore* store;
+  G4Nucleus                targetNucleus;
+
+  G4double        xsec[40];
+  G4double        xsecH[2];
+  G4double        cross;
+
+  G4int    pPDG;
+  G4bool   flagHP;
+};
+
+#endif
---
+++ source/processes/hadronic/models/coherent_elastic/src/G4UHadronElasticProcess.cc
@@ -0,0 +1,244 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+//
+//
+// Geant4 Hadron Elastic Scattering Process -- header file
+// 
+// Created 21 April 2006 V.Ivanchenko
+//  
+// Modified:
+//
+//
+
+#include "G4UHadronElasticProcess.hh"
+#include "G4CrossSectionDataStore.hh"
+#include "G4HadronElasticDataSet.hh"
+#include "G4VQCrossSection.hh"
+#include "G4QElasticCrossSection.hh"
+#include "G4Element.hh"
+#include "G4ElementVector.hh"
+#include "G4IsotopeVector.hh"
+#include "G4Neutron.hh"
+#include "G4Proton.hh"
+#include "G4NeutronHPElasticData.hh"
+ 
+G4UHadronElasticProcess::G4UHadronElasticProcess(const G4String& processName, G4bool fl)
+  : G4HadronicProcess(processName), flagHP(fl)
+{
+  AddDataSet(new G4HadronElasticDataSet);
+  store = G4HadronicProcess::GetCrossSectionDataStore();
+  qCManager   = G4QElasticCrossSection::GetPointer();
+  theProton = G4Proton::Proton();
+  theNeutron = G4Neutron::Neutron();
+}
+
+G4UHadronElasticProcess::~G4UHadronElasticProcess()
+{
+}
+ 
+void G4UHadronElasticProcess::
+BuildPhysicsTable(const G4ParticleDefinition& aParticleType)
+{
+  theParticle = &aParticleType;
+  pPDG = theParticle->GetPDGEncoding();
+
+  if(theParticle == theNeutron && flagHP) 
+    AddDataSet(new G4NeutronHPElasticData());
+
+  G4HadronicProcess::GetCrossSectionDataStore()
+    ->BuildPhysicsTable(aParticleType);
+}
+
+G4double G4UHadronElasticProcess::GetMeanFreePath(const G4Track& track, 
+						  G4double, 
+						  G4ForceCondition*)
+{
+  const G4DynamicParticle* dp = track.GetDynamicParticle();
+  const G4Material* material = track.GetMaterial();
+  cross = 0.0;
+  G4double x;
+  const G4ElementVector* theElementVector = material->GetElementVector();
+  const G4double* theAtomNumDensityVector = material->GetVecNbOfAtomsPerVolume();
+  G4double temp = material->GetTemperature();
+  G4int nelm   = material->GetNumberOfElements();
+  xsecH[0] = 0.0;
+  xsecH[1] = 0.0;
+  for (G4int i=0; i<nelm; i++) {
+    const G4Element* elm = (*theElementVector)[i];
+    G4int iz = G4int(elm->GetZ());
+
+    // CHIPS cross sections
+    if(iz <= 2 && theParticle == theProton) {
+      G4double momentum = dp->GetTotalMomentum();
+      if(iz == 1) {
+	G4IsotopeVector* isv = elm->GetIsotopeVector(); 
+        G4int ni = 0;
+	if(isv) ni = isv->size();
+        if(ni > 0) {
+	  G4double* ab = elm->GetRelativeAbundanceVector();
+	  x = 0.0;
+	  for(G4int j=0; j<ni; j++) {
+            G4int N = elm->GetIsotope(j)->GetN() - 1;
+            if(N == 0 || N == 1) {
+	      G4double y = ab[j]*
+		qCManager->GetCrossSection(true,momentum,1,N,pPDG);
+	      xsecH[N] += y;
+	      x += y;
+	    }
+	  }
+	} else {
+	  x = qCManager->GetCrossSection(true,momentum,1,0,pPDG);
+	  xsecH[0] += x;
+	}
+      } else {
+	x = qCManager->GetCrossSection(true,momentum,2,2,pPDG);
+      }
+    } else {
+      x = GetMicroscopicCrossSection(dp, elm, temp);
+    }
+    cross += theAtomNumDensityVector[i]*x;
+    xsec[i] = cross;
+  }
+  if(cross > DBL_MIN) x = 1./cross;
+  else x = DBL_MAX;
+
+  return x;
+}
+
+G4double G4UHadronElasticProcess::GetMicroscopicCrossSection(
+                                  const G4DynamicParticle* aParticle, 
+				  const G4Element* anElement, 
+				  G4double aTemp)
+{
+  // gives the microscopic cross section in GEANT4 internal units
+  return store->GetCrossSection(aParticle, anElement, aTemp);
+}
+
+G4VParticleChange* G4UHadronElasticProcess::PostStepDoIt(
+				  const G4Track& track, 
+				  const G4Step& step)
+{
+  aParticleChange.Initialize(track);
+  G4double kineticEnergy = track.GetKineticEnergy();
+  G4Material* material = track.GetMaterial();
+
+  // Select element
+  const G4ElementVector* theElementVector = material->GetElementVector();
+  G4Element* elm = (*theElementVector)[0];
+  G4int nelm = material->GetNumberOfElements() - 1;
+  if (nelm > 0) {
+    G4double x = G4UniformRand()*cross;
+    G4int i = -1;
+    do {i++;} while (x > xsec[i] && i < nelm);
+    elm = (*theElementVector)[i];
+  }
+  G4double Z = elm->GetZ();
+  G4double A = elm->GetA();
+  G4int iz = G4int(Z);
+
+  // Select isotope
+  G4IsotopeVector* isv = elm->GetIsotopeVector(); 
+  G4int ni = 0;
+  if(isv) ni = isv->size();
+  if(ni == 1) { 
+    A = G4double(elm->GetIsotope(0)->GetN());
+  } else if(ni > 1) {
+    if(iz == 1 && theParticle == theProton) {
+      A = 1.;
+      if(G4UniformRand()*(xsecH[0] + xsec[1]) > xsec[0]) A = 2.;
+    } else {
+      G4double* ab = elm->GetRelativeAbundanceVector();
+      G4double y = G4UniformRand();
+      G4int j = -1;
+      ni--;
+      do {
+	j++;
+	y -= ab[j];
+      } while (y > 0.0 && j < ni);
+      A = G4double(elm->GetIsotope(j)->GetN());
+    }
+  }
+
+  G4HadronicInteraction* hadi = 
+		ChooseHadronicInteraction( kineticEnergy, material, elm );
+
+  // Initialize the hadronic projectile from the track
+  G4HadProjectile thePro(track);
+  targetNucleus.SetParameters(A, Z);
+
+  aParticleChange.Initialize(track);
+  G4HadFinalState* result = hadi->ApplyYourself(thePro, targetNucleus);
+
+  aParticleChange.ProposeEnergy(result->GetEnergyChange());
+  aParticleChange.ProposeMomentumDirection(result->GetMomentumChange()) ;
+  if(result->GetNumberOfSecondaries() > 0) {
+    aParticleChange.SetNumberOfSecondaries(1);
+    G4DynamicParticle* p = result->GetSecondary(0)->GetParticle();
+    aParticleChange.AddSecondary(p);
+  } else {
+    aParticleChange.SetNumberOfSecondaries(0);
+    aParticleChange.ProposeLocalEnergyDeposit(result->GetLocalEnergyDeposit());
+  }
+  result->Clear();
+
+  return G4VDiscreteProcess::PostStepDoIt(track,step);
+}
+
+G4bool G4UHadronElasticProcess::
+IsApplicable(const G4ParticleDefinition& aParticleType)
+{
+   return (aParticleType == *(G4PionPlus::PionPlus()) ||
+           aParticleType == *(G4PionZero::PionZero()) ||
+           aParticleType == *(G4PionMinus::PionMinus()) ||
+           aParticleType == *(G4KaonPlus::KaonPlus()) ||
+           aParticleType == *(G4KaonZeroShort::KaonZeroShort()) ||
+           aParticleType == *(G4KaonZeroLong::KaonZeroLong()) ||
+           aParticleType == *(G4KaonMinus::KaonMinus()) ||
+           aParticleType == *(G4Proton::Proton()) ||
+           aParticleType == *(G4AntiProton::AntiProton()) ||
+           aParticleType == *(G4Neutron::Neutron()) ||
+           aParticleType == *(G4AntiNeutron::AntiNeutron()) ||
+           aParticleType == *(G4Lambda::Lambda()) ||
+           aParticleType == *(G4AntiLambda::AntiLambda()) ||
+           aParticleType == *(G4SigmaPlus::SigmaPlus()) ||
+           aParticleType == *(G4SigmaZero::SigmaZero()) ||
+           aParticleType == *(G4SigmaMinus::SigmaMinus()) ||
+           aParticleType == *(G4AntiSigmaPlus::AntiSigmaPlus()) ||
+           aParticleType == *(G4AntiSigmaZero::AntiSigmaZero()) ||
+           aParticleType == *(G4AntiSigmaMinus::AntiSigmaMinus()) ||
+           aParticleType == *(G4XiZero::XiZero()) ||
+           aParticleType == *(G4XiMinus::XiMinus()) ||
+           aParticleType == *(G4AntiXiZero::AntiXiZero()) ||
+           aParticleType == *(G4AntiXiMinus::AntiXiMinus()) ||
+           aParticleType == *(G4Deuteron::Deuteron()) ||
+           aParticleType == *(G4Triton::Triton()) ||
+           aParticleType == *(G4Alpha::Alpha()) ||
+           aParticleType == *(G4OmegaMinus::OmegaMinus()) ||
+           aParticleType == *(G4AntiOmegaMinus::AntiOmegaMinus()));
+}
+
+void G4UHadronElasticProcess::
+DumpPhysicsTable(const G4ParticleDefinition& aParticleType)
+{
+  G4HadronicProcess::GetCrossSectionDataStore()
+    ->DumpPhysicsTable(aParticleType);
+}
