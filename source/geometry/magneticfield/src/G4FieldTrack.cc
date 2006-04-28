80d010509a30b1eb73548db6b6248adf6d6068e1
John Apostolakis <John.Apostolakis@cern.ch>
2006-04-28 10:51:02 +0000

    Moved existing constructor.
     Created new one that takes charge, magnetic moment.
    
     New method to set all charges -- needs fix for memory issue.
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@25698 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/geometry/magneticfield/src/G4FieldTrack.cc
+++ source/geometry/magneticfield/src/G4FieldTrack.cc
@@ -21,12 +21,13 @@
 // ********************************************************************
 //
 //
-// $Id: G4FieldTrack.cc,v 1.7 2004-01-13 12:27:05 japost Exp $
+// $Id: G4FieldTrack.cc,v 1.8 2006-04-28 10:51:02 japost Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // -------------------------------------------------------------------
 
 #include "G4FieldTrack.hh"
+#include "G4ChargeState.hh"
 
 std::ostream& operator<<( std::ostream& os, const G4FieldTrack& SixVec)
 {
@@ -40,3 +41,70 @@ std::ostream& operator<<( std::ostream& os, const G4FieldTrack& SixVec)
      os << " ) ";
      return os;
 }
+
+G4FieldTrack::G4FieldTrack( const G4ThreeVector& pPosition, 
+			          G4double       LaboratoryTimeOfFlight,
+			    const G4ThreeVector& pMomentumDirection,
+			          G4double       kineticEnergy,
+			          G4double       restMass_c2,
+		                  G4double       charge, 
+			    const G4ThreeVector* pSpin,
+			          G4double       magnetic_dipole_moment,
+			          G4double       curve_length )
+ : fKineticEnergy(kineticEnergy),
+   fRestMass_c2(restMass_c2),
+   fLabTimeOfFlight(LaboratoryTimeOfFlight), 
+   // fProperTimeOfFlight(0.0),
+   fMomentumDir(pMomentumDirection)
+{
+  G4double momentum  = std::sqrt(kineticEnergy*kineticEnergy
+                            +2.0*restMass_c2*kineticEnergy);
+
+  G4ThreeVector pMomentum= momentum * pMomentumDirection; 
+  SetCurvePnt( pPosition, pMomentum, curve_length );
+  InitialiseSpin( pSpin ); 
+
+  fpChargeState = new G4ChargeState( charge, magnetic_dipole_moment ); 
+}
+
+G4FieldTrack::G4FieldTrack( const G4ThreeVector& pPosition, 
+                            const G4ThreeVector& pMomentumDirection,    
+                                  G4double       curve_length, 
+                                  G4double       kineticEnergy,
+                            const G4double       restMass_c2,
+                                  G4double,   // velocity
+                                  G4double       pLaboratoryTimeOfFlight,
+                                  G4double       pProperTimeOfFlight,
+                            const G4ThreeVector* pSpin)
+ : fKineticEnergy(kineticEnergy),
+   fRestMass_c2(restMass_c2),
+   fLabTimeOfFlight(pLaboratoryTimeOfFlight), 
+   fProperTimeOfFlight(pProperTimeOfFlight),
+   fMomentumDir(pMomentumDirection)
+{
+  G4double momentum  = std::sqrt(kineticEnergy*kineticEnergy
+                            +2.0*restMass_c2*kineticEnergy);
+  G4ThreeVector pMomentum= momentum * pMomentumDirection; 
+
+  SetCurvePnt( pPosition, pMomentum, curve_length );
+  InitialiseSpin( pSpin ); 
+
+  fpChargeState = new G4ChargeState( DBL_MAX );     //  charge not yet set !!
+}
+
+void G4FieldTrack::
+     SetChargeAndMoments(G4double charge, 
+			 G4double magnetic_dipole_moment, // default= DBL_MAX - do not change
+			 G4double electric_dipole_moment, //   ditto
+			 G4double magnetic_charge )       //   ditto
+{
+  fpChargeState= new G4ChargeState(  charge, magnetic_dipole_moment, 
+				     electric_dipole_moment, magnetic_charge
+                                  ); 
+
+  // TO-DO: fix the memory leak that this creates 
+  //   -- the old one (which may be shared by other copies) is
+  //      left to hang loose in this alpha implementation  ---    JA, 28 Apr 2006
+}
+
+
