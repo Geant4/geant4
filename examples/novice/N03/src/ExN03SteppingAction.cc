b90d60a5ea0ad04bee2c2fd20b20ac72cbec1d45
Michel Maire <Michel.Maire@cern.ch>
2006-05-24 15:18:21 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@26225 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/novice/N03/src/ExN03SteppingAction.cc
+++ examples/novice/N03/src/ExN03SteppingAction.cc
@@ -21,7 +21,7 @@
 // ********************************************************************
 //
 //
-// $Id: ExN03SteppingAction.cc,v 1.11 2006-05-23 18:41:50 vnivanch Exp $
+// $Id: ExN03SteppingAction.cc,v 1.12 2006-05-24 15:18:21 maire Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // 
@@ -34,7 +34,7 @@
 #include "ExN03DetectorConstruction.hh"
 #include "ExN03EventAction.hh"
 
-#include "G4Track.hh"
+#include "G4Step.hh"
 
 ////#include "G4RunManager.hh"
 
@@ -54,18 +54,21 @@ ExN03SteppingAction::~ExN03SteppingAction()
 
 void ExN03SteppingAction::UserSteppingAction(const G4Step* aStep)
 {
-  const G4Track* track = aStep->GetTrack();
+  // get volume of the current step
   G4VPhysicalVolume* volume = aStep->GetPreStepPoint()->GetPhysicalVolume();
   
   // collect energy and track length step by step
   G4double edep = aStep->GetTotalEnergyDeposit();
   
   G4double stepl = 0.;
-  if (track->GetDefinition()->GetPDGCharge() != 0.)
+  if (aStep->GetTrack()->GetDefinition()->GetPDGCharge() != 0.)
     stepl = aStep->GetStepLength();
       
   if (volume == detector->GetAbsorber()) eventaction->AddAbs(edep,stepl);
   if (volume == detector->GetGap())      eventaction->AddGap(edep,stepl);
+  
+  //example of saving random number seed of this event, under condition
+  //// if (condition) G4RunManager::GetRunManager()->rndmSaveThisEvent(); 
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
