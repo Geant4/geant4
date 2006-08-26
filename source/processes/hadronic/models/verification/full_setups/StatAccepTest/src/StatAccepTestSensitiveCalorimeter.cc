85c0938fa727a75a35e6a217adde0c354fe2cbc3
Alberto Ribon <Alberto.Ribon@cern.ch>
2006-08-26 13:25:57 +0000

    Added the possibility to simulate the electron recombination effect in liquid Argon for ATLAS, using the Birks' law. Added also a printing at the first call in the case of Birks' activation.
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@27345 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/models/verification/full_setups/StatAccepTest/src/StatAccepTestSensitiveCalorimeter.cc
+++ source/processes/hadronic/models/verification/full_setups/StatAccepTest/src/StatAccepTestSensitiveCalorimeter.cc
@@ -47,11 +47,14 @@ ProcessHits( G4Step* aStep, G4TouchableHistory* ) {
   if ( edep < 0.001*eV ) return false;
 
   // -----------------------------------------------------------------
-  // 6-Apr-2006 : In the case of Scintillator as active medium,
-  //              implement by hand Birks' law, using the expression
-  //              and the coefficients taken from the paper 
-  //              NIM 80 (1970) 239-244 for the organic scintillator
-  //              NE-102:
+  //                        BIRKS' LAW
+  //                        ==========
+  //
+  // 6-Apr-2006 : In the case of Scintillator as active medium, we can
+  //              describe the quenching effects with the Birks' law, 
+  //              using the expression and the coefficients taken from 
+  //              the paper NIM 80 (1970) 239-244 for the organic 
+  //              scintillator NE-102:
   //                                     S*dE/dr 
   //                    dL/dr = -----------------------------------
   //                               1 + C1*(dE/dr) + C2*(dE/dr)^2 
@@ -65,32 +68,115 @@ ProcessHits( G4Step* aStep, G4TouchableHistory* ) {
   //              by uncommenting one of the lines below.
   //              To get the "dE/dr" that appears in the formula,
   //              which has the dimensions
-  //                   [ dE/dr ] = MeV * cm^2 / g
+  //                    [ dE/dr ] = MeV * cm^2 / g
   //              we have to divide the energy deposit in MeV by the
   //              product of the step length (in cm) and the density
-  //              of the scintillator, which is 1.032*g/cm3 .
+  //              of the scintillator:
+  //                    rho_scintillator = 1.032  g/cm3 .
   //              Of course, in our case we use only the denominator
   //              of the Birks' formula above, because we do not have
   //              digitization, i.e. we only have energy deposit but
   //              not conversion to photons.
   //              If you do not want to apply Birks' law, simply set
   //              isBirksOn = false.
-  bool isBirksOn = false;  //***LOOKHERE***
+  //
+  // 26-Aug-2006: In the case of LAr as active medium, we can also
+  //              take into consideration the electron recombination 
+  //              in liquid argon with a Birks' law, using the 
+  //              expression taken from the paper: NIM A 523 (2004) 275-286  
+  //                                      Q0
+  //                    Q  = A * -------------------------
+  //                              1 + (k/E_field)*(dE/dx)  
+  //              with:
+  //                    A  adimensional constant (fitteed on the data);
+  //                    k  [kV/cm] is constant (fitted on the data);
+  //                    E_field  [kV/cm] is the electric field.
+  //              In the case of ATLAS LAr :
+  //                    E_field = 10 kV/cm
+  //              so it is a much higher field than the one discussed
+  //              in the paper ( 0.1 < E_field < 1.0 kV/cm ) so we 
+  //              cannot use directly the values of A and k reported 
+  //              there. However, they discuss also the results reported
+  //              by a previous analysis (Scalettar et al., 
+  //              Phys Rev. A 25 (1882) 2419), which was done for higher
+  //              fields, up to 10 kV/cm. In this analysis, they discuss
+  //              the recombination effect as a function of the electric
+  //              field only, and not as a function of the particle
+  //              stopping power dE/dx, using the phenomenological
+  //              formula:             Q0
+  //                         Q = ----------------
+  //                              1 + k_E/E_field
+  //              where  k_E = 0.53 kV/cm .
+  //              Now, we could use our formula above, where dE/dx
+  //              appears, by identifying:
+  //                    A = 1 
+  //                    k = k_E/(dE/dx)
+  //              where as  dE/dx  we have to use the average stopping
+  //              power for the case considered in that analysis, i.e.
+  //              364 keV electrons (from a 113Sn conversion electron
+  //              source). This correspond, in liquid argon:
+  //                    dE/dx = 2.4 MeV/(g/cm^2)
+  //              which gives:
+  //                    k = 0.22 (kV/cm)((g/cm^2)/MeV)
+  //              In conclusion, to study the electron recombination
+  //              for ATLAS LAr we can use the same formula as for
+  //              the quenching in scintillator, with the following
+  //              coefficients:
+  //                    C1 = k/E_field = 0.22/10,0 = 0.022 (g/cm^2)/MeV
+  //                    C2 = 0.0
+  //              and with the following density:
+  //                    rho_lAr = 1.396  g/cm^3
+  //
+  //--------------------------------------------------------------------
+  bool isBirksOn = false;         //***LOOKHERE***
+  bool isScintillator = true;     //              true = Quenching in Scintillator 
+                                  //              false = Recombination in LAr.
   if ( isBirksOn ) {
-    double C1 = 1.29e-2;                                  // [g*cm^-2*MeV^-1]  
-    double C2 = 9.59e-6;                                  // [g^2*cm^-4*MeV^-2] 
+
+    // --- Quenching in Scintillator ---
+    double C1_scintillator = 1.29e-2;                    // [g*cm^-2*MeV^-1]  
+    double C2_scintillator = 9.59e-6;                    // [g^2*cm^-4*MeV^-2] 
     // Uncomment one line below if you want to try different
-    // values for the Birks' coefficient C1, with C2 = 0.
-    //C1 = 1.31e-2; C2 = 0.0;  // Standard Birks law.
-    //C1 = 8.5e-3;  C2 = 0.0;  // Zeus SCSN38, lower limit (-35%)
-    //C1 = 1.59e-2; C2 = 0.0;  // Pilot B (same paper), upper limit (+21%)
-    double rho = 1.032;                                   // [g/cm^3]
+    // values for the scintillator Birks' coefficient C1, with C2 = 0.
+    //C1_scintillator = 1.31e-2; C2_scintillator = 0.0;  // Standard Birks law.
+    //C1_scintillator = 8.5e-3;  C2_scintillator = 0.0;  // Zeus SCSN38, lower limit (-35%)
+    //C1_scintillator = 1.59e-2; C2_scintillator = 0.0;  // Pilot B (same paper), upper limit (+21%)
+    double rho_scintillator = 1.032;                     // [g/cm^3]
+
+    // --- Recombination in LiquidArgon --
+    double C1_lAr = 0.022;                               // [g*cm^-2*MeV^-1]  
+    double C2_lAr = 0.0;
+    double rho_lAr = 1.396;                              // [g/cm^3]
+
+    double C1, C2, rho;
+    if ( isScintillator ) {
+      C1 = C1_scintillator;
+      C2 = C2_scintillator;
+      rho = rho_scintillator;
+    } else {
+      C1 = C1_lAr;
+      C2 = C2_lAr;
+      rho = rho_lAr;
+    }
+
     double stepLength_cm = aStep->GetStepLength() / cm ;  // [cm] 
     if ( stepLength_cm > 1.0e-8 ) {
       double dedx = edep / ( rho * stepLength_cm );       // [MeV*cm^2/g]
       double birksFactor = 1.0 / ( 1.0 + C1*dedx + C2*dedx*dedx );
-      //std::cout << " birksFactor=" << birksFactor << std::endl; //***DEBUG***
+      //G4cout << " birksFactor=" << birksFactor << G4endl; //***DEBUG***
       edep = edep * birksFactor;
+    }
+
+    static bool isFirstCall = true;
+    if ( isFirstCall ) {
+      G4cout << "\t *** StatAccepTestSensitiveCalorimeter::ProcessHits *** " << G4endl
+	     << "\t isBirksOn = " << isBirksOn << G4endl
+	     << "\t isScintillator = " << isScintillator << G4endl
+	     << "\t C1 = " << C1 << G4endl
+             << "\t C2 = " << C2 << G4endl
+	     << "\t rho = " << rho << G4endl
+             << "\t ******************* " << G4endl; 
+      isFirstCall = false;
     }
   }
   // -----------------------------------------------------------------
