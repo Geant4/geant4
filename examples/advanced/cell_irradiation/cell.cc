7f7846556ba2ed517b3930a88f0ec9f94d43d215
Barbara Mascialino <mascial@cern.ch>
2006-10-17 17:57:24 +0000

    added files
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@27719 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ examples/advanced/cell_irradiation/cell.cc
@@ -0,0 +1,121 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+//
+//
+// $Id: cell.cc,v 1.32 2006/09/20 15:30:15 guatelli
+//
+// 
+
+#include "globals.hh"
+#include "G4ios.hh"
+#include "G4RunManager.hh"
+#include "G4UImanager.hh"
+#include "G4UIterminal.hh"
+#include "G4UItcsh.hh"
+#include "CellDetectorConstruction.hh"
+#include "CellPhysicsList.hh"
+#include "CellPrimaryGeneratorAction.hh"
+#include "CellRunAction.hh"
+#include "CellEventAction.hh"
+#include "CellSteppingAction.hh"
+#include "CellSteppingVerbose.hh"
+#include "CellAnalysisManager.hh"
+#ifdef G4VIS_USE
+#include "G4VisExecutive.hh"
+#endif
+
+int main(int argc,char** argv) {
+ 	
+  CLHEP::HepRandom::setTheEngine(new CLHEP::RanecuEngine);   
+  G4int seed = time(0);
+  CLHEP::HepRandom ::setTheSeed(seed);
+ 
+  G4RunManager * runManager = new G4RunManager;
+
+  CellDetectorConstruction* cellDetector = new CellDetectorConstruction();
+  runManager->SetUserInitialization(cellDetector);  
+
+  CellPhysicsList* cellPhysics = new CellPhysicsList();
+  runManager->SetUserInitialization(cellPhysics);
+
+#ifdef G4VIS_USE 
+  G4VisManager* visManager = new G4VisExecutive;
+  visManager->Initialize();
+#endif
+
+  CellPrimaryGeneratorAction* cellPrimaryParticle = new CellPrimaryGeneratorAction(); 
+  runManager->SetUserAction(cellPrimaryParticle);
+
+  CellRunAction* cellRun=new CellRunAction(); 
+  runManager->SetUserAction(cellRun);  
+
+  CellEventAction *cellEventAction = new CellEventAction();
+ 
+  runManager->SetUserAction(cellEventAction);
+     
+  CellSteppingAction* cellSteppingAction = new CellSteppingAction(cellPrimaryParticle,
+                                                                cellRun,
+                                                                cellDetector);
+  runManager->SetUserAction(cellSteppingAction);
+
+  CellAnalysisManager* analysis = CellAnalysisManager::getInstance();
+ 
+  // Set the name of the generated .hbk file
+  analysis -> book("cell");
+  
+  //get the pointer to the User Interface manager 
+  G4UImanager * UI = G4UImanager::GetUIpointer();  
+  
+  if (argc == 1)
+    // Define (G)UI terminal for interactive mode  
+    { 
+      // G4UIterminal is a (dumb) terminal.
+      G4UIsession * session = 0;
+
+#ifdef G4UI_USE_TCSH
+      session = new G4UIterminal(new G4UItcsh);      
+#else
+      session = new G4UIterminal();
+#endif    
+      UI->ApplyCommand("/control/execute default.mac");
+      session->SessionStart();
+      delete session;
+    }
+  else
+    // Batch mode
+    {     
+      G4String command =("/control/execute ");
+      G4String fileName = argv[1];
+      G4cout <<"macro --> "<< fileName << G4endl;
+      UI->ApplyCommand(command+fileName);
+    }
+
+  analysis->finish();
+
+#ifdef G4VIS_USE
+  delete visManager;
+#endif
+
+   delete runManager;
+
+  return 0;
+}
