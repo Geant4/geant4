5d699c3f420ed9f87c4b811592f7ff99d36b5ad2
Michel Maire <Michel.Maire@cern.ch>
2006-10-24 16:54:16 +0000

    ¨¨
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@27841 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/electromagnetic/TestEm14/src/PhysicsList.cc
+++ examples/extended/electromagnetic/TestEm14/src/PhysicsList.cc
@@ -24,7 +24,7 @@
 // ********************************************************************
 //
 // 
-// $Id: PhysicsList.cc,v 1.3 2006-06-29 16:45:53 gunter Exp $
+// $Id: PhysicsList.cc,v 1.4 2006-10-24 16:54:16 maire Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -34,7 +34,6 @@
 #include "PhysicsListMessenger.hh"
  
 #include "PhysListEmStandard.hh"
-#include "PhysListEmG4v52.hh"
 
 #include "PhysListEmLivermore.hh"
 #include "PhysListEmPenelope.hh"
@@ -204,12 +203,7 @@ void PhysicsList::AddPhysicsList(const G4String& name)
     emName = name;
     delete emPhysicsList;
     emPhysicsList = new PhysListEmStandard(name);
-    
-  } else if (name == "g4v52") {
-    emName = name;
-    delete emPhysicsList;
-    emPhysicsList = new PhysListEmG4v52(name);
-    
+       
   } else if (name == "livermore") {
     emName = name;
     delete emPhysicsList;
