71c274b4d574d929fe6b8f5c29f1a3d5802e2148
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2006-11-02 15:00:17 +0000

    Changed date for 8.1.p02
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@28015 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/global/management/include/G4Version.hh
+++ source/global/management/include/G4Version.hh
@@ -24,7 +24,7 @@
 // ********************************************************************
 //
 //
-// $Id: G4Version.hh,v 1.5 2006-07-20 16:02:52 gcosmo Exp $
+// $Id: G4Version.hh,v 1.6 2006-11-02 15:00:17 gcosmo Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // Version information
@@ -46,7 +46,7 @@
 //     |--> patch number
 
 #ifndef G4VERSION_NUMBER
-#define G4VERSION_NUMBER  811
+#define G4VERSION_NUMBER  812
 #endif
 
 #ifndef G4VERSION_TAG
@@ -58,6 +58,6 @@
 #include "G4String.hh"
 
 static const G4String G4Version = "$Name: not supported by cvs2svn $";
-static const G4String G4Date    = "(27-July-2006)";
+static const G4String G4Date    = "(10-November-2006)";
 
 #endif
