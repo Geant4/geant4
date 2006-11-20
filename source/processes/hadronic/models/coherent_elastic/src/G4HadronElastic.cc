a23aef174d7f60e86cc5e920d221e7620d99400d
Vladimir Ivantchenko <Vladimir.Ivantchenko@cern.ch>
2006-11-20 13:51:37 +0000

    restore the head
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@28626 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/models/coherent_elastic/src/G4HadronElastic.cc
+++ source/processes/hadronic/models/coherent_elastic/src/G4HadronElastic.cc
@@ -23,7 +23,7 @@
 // * acceptance of all terms of the Geant4 Software license.          *
 // ********************************************************************
 //
-// $Id: G4HadronElastic.cc,v 1.33 2006-11-20 13:50:58 vnivanch Exp $
+// $Id: G4HadronElastic.cc,v 1.34 2006-11-20 13:51:37 vnivanch Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 //
@@ -79,8 +79,8 @@ G4HadronElastic::G4HadronElastic(G4double, G4double, G4double)
   SetMaxEnergy( 100.*TeV );
   verboseLevel= 0;
   lowEnergyRecoilLimit = 100.*keV;  
-  //  lowEnergyLimitHE = 0.4*GeV;  
-  lowEnergyLimitHE = DBL_MAX;  
+  lowEnergyLimitHE = 0.4*GeV;  
+  //lowEnergyLimitHE = DBL_MAX;  
   lowestEnergyLimit= 10.0*keV;  
   plabLowLimit     = 20.0*MeV;
 
