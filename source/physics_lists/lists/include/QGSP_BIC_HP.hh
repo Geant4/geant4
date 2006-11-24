9633f36b49fef5517a8468528e7a8ca22ff4ad57
Gunter Folger <Gunter.Folger@cern.ch>
2006-11-24 16:31:35 +0000

    adding QGSP_BIC_HP
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@28767 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/physics_lists/lists/include/QGSP_BIC_HP.hh
@@ -0,0 +1,69 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+// $Id: QGSP_BIC_HP.hh,v 1.1 2006-11-24 16:31:35 gunter Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+//---------------------------------------------------------------------------
+//
+// ClassName:   QGSP_BIC_HP
+//
+// Author: 2006 G.Folger
+//
+// based on QGSP_BIC
+// Modified:
+//
+//----------------------------------------------------------------------------
+//
+#ifndef TQGSP_BIC_HP_h
+#define TQGSP_BIC_HP_h 1
+
+#include "G4VModularPhysicsList.hh"
+#include "globals.hh"
+#include "CompileTimeConstraints.hh"
+
+template<class T>
+class TQGSP_BIC_HP: public T
+{
+public:
+  TQGSP_BIC_HP(G4int ver = 1);
+  virtual ~TQGSP_BIC_HP();
+  
+public:
+  // SetCuts() 
+  virtual void SetCuts();
+
+private:
+  enum {ok = CompileTimeConstraints::IsA<T, G4VModularPhysicsList>::ok };
+};
+#include "QGSP_BIC_HP.icc"
+typedef TQGSP_BIC_HP<G4VModularPhysicsList> QGSP_BIC_HP;
+
+// 2002 by J.P. Wellisch
+
+#endif
+
+
+
