6340daaef767151015ac70145c6aff2794e5f1c1
John Allison <John.Allison@cern.ch>
2007-01-05 16:52:47 +0000

    - Moved LoadAtts to vis management.
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@29073 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/visualization/OpenInventor/include/G4OpenInventorSceneHandler.hh
+++ source/visualization/OpenInventor/include/G4OpenInventorSceneHandler.hh
@@ -24,7 +24,7 @@
 // ********************************************************************
 //
 //
-// $Id: G4OpenInventorSceneHandler.hh,v 1.31 2006-11-02 12:01:20 allison Exp $
+// $Id: G4OpenInventorSceneHandler.hh,v 1.32 2007-01-05 16:52:47 allison Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // 
@@ -47,7 +47,6 @@
 class G4OpenInventor;
 class SoSeparator;
 class Geant4_SoStyleCache;
-class G4AttHolder;
 
 // Base class for various OpenInventorScene classes.
 class G4OpenInventorSceneHandler: public G4VSceneHandler {
@@ -85,7 +84,6 @@ public:
 
 private:
 
-  void LoadAtts(const G4Visible&, G4AttHolder*);
   static G4int fSceneIdCount;   // static counter for OpenInventor scenes.
   enum G4OIMarker {G4OICircle, G4OISquare};
   void AddCircleSquare (G4OIMarker markerType, const G4VMarker&);
