3b31cfe4fdf1784dd5a9d7d1bea476ff0996c936
John Allison <John.Allison@cern.ch>
2007-01-05 16:07:02 +0000

    o Correct treatment of ForcedLineSegmentsPerCircle when clipping.
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@29058 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/visualization/modeling/src/G4PhysicalVolumeModel.cc
+++ source/visualization/modeling/src/G4PhysicalVolumeModel.cc
@@ -24,7 +24,7 @@
 // ********************************************************************
 //
 //
-// $Id: G4PhysicalVolumeModel.cc,v 1.57 2006-11-16 12:11:30 allison Exp $
+// $Id: G4PhysicalVolumeModel.cc,v 1.58 2007-01-05 16:07:02 allison Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // 
@@ -545,9 +545,13 @@ void G4PhysicalVolumeModel::DescribeSolid
     // Clipping, etc., performed by Boolean operations on polyhedron objects.
 
     // First, get polyhedron for current solid...
-    G4Polyhedron::SetNumberOfRotationSteps (fpMP->GetNoOfSides());
+    if (pVisAttribs->IsForceLineSegmentsPerCircle())
+      G4Polyhedron::SetNumberOfRotationSteps
+	(pVisAttribs->GetForcedLineSegmentsPerCircle());
+    else
+      G4Polyhedron::SetNumberOfRotationSteps(fpMP->GetNoOfSides());
     G4Polyhedron* pOriginal = pSol->GetPolyhedron();
-    G4Polyhedron::ResetNumberOfRotationSteps ();
+    G4Polyhedron::ResetNumberOfRotationSteps();
     if (!pOriginal) {
 	if (fpMP->IsWarning())
 	  G4cout <<
