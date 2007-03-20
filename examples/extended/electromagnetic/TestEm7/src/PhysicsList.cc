3b3445a5681ddbfc32d877b20478d0adec5d73b3
Vladimir Ivantchenko <Vladimir.Ivantchenko@cern.ch>
2007-03-20 18:34:17 +0000

    fix elastic
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@29653 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/electromagnetic/TestEm7/src/PhysicsList.cc
+++ examples/extended/electromagnetic/TestEm7/src/PhysicsList.cc
@@ -23,7 +23,7 @@
 // * acceptance of all terms of the Geant4 Software license.          *
 // ********************************************************************
 //
-// $Id: PhysicsList.cc,v 1.21 2006-12-11 20:13:53 vnivanch Exp $
+// $Id: PhysicsList.cc,v 1.22 2007-03-20 18:34:17 vnivanch Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -38,6 +38,7 @@
 #include "PhysListEmPenelope.hh"
 #include "G4EmStandardPhysics.hh"
 #include "G4EmStandardPhysics71.hh"
+#include "G4EmStandardPhysics72.hh"
 
 #include "G4HadronElasticPhysics.hh"
 #include "G4HadronHElasticPhysics.hh"
@@ -215,6 +216,12 @@ void PhysicsList::AddPhysicsList(const G4String& name)
     delete emPhysicsList;
     emPhysicsList = new G4EmStandardPhysics71(name);
 
+  } else if (name == "G4standard_exp") {
+
+    emName = name;
+    delete emPhysicsList;
+    emPhysicsList = new G4EmStandardPhysics72(name);
+
   } else if (name == "standardSS") {
 
     emName = name;
@@ -236,11 +243,11 @@ void PhysicsList::AddPhysicsList(const G4String& name)
     helIsRegisted = true;
 
   } else if (name == "HElastic" && !helIsRegisted) {
-    hadronPhys.push_back( new G4HadronElasticPhysics(name));
+    hadronPhys.push_back( new G4HadronHElasticPhysics(name));
     helIsRegisted = true;
 
   } else if (name == "QElastic" && !helIsRegisted) {
-    hadronPhys.push_back( new G4HadronElasticPhysics(name));
+    hadronPhys.push_back( new G4HadronQElasticPhysics(name));
     helIsRegisted = true;
 
   } else if (name == "binary" && !bicIsRegisted) {
