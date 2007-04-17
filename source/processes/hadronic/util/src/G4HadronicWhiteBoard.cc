128be9416274e4b3fbe2085a65217fb8a1ae9dff
Dennis Herbert Wright <dennis@cern.ch>
2007-04-17 14:50:04 +0000

    add set model and set process name methods
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@29836 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/util/src/G4HadronicWhiteBoard.cc
+++ source/processes/hadronic/util/src/G4HadronicWhiteBoard.cc
@@ -32,50 +32,74 @@ G4HadronicWhiteBoard & G4HadronicWhiteBoard::Instance()
   return theInstance;
 }
 
-  const G4HadProjectile * G4HadronicWhiteBoard::GetProjectile() {return theProjectile;}
-  const G4Nucleus & G4HadronicWhiteBoard::GetTargetNucleus() { return theTarget; }
-  G4ParticleDefinition * G4HadronicWhiteBoard::GetPDef() {return theDef;}
-  G4String G4HadronicWhiteBoard::GetParticleName() {return theName;}
-  G4double G4HadronicWhiteBoard::GetEnergy() {return theE;}
-  G4double G4HadronicWhiteBoard::GetPx(){return thePx;}
-  G4double G4HadronicWhiteBoard::GetPy(){return thePy;}
-  G4double G4HadronicWhiteBoard::GetPz(){return thePz;}
-  G4double G4HadronicWhiteBoard::GetA(){return theA;}
-  G4double G4HadronicWhiteBoard::GetZ(){return theZ;}
+const G4HadProjectile * G4HadronicWhiteBoard::GetProjectile() 
+{
+  return theProjectile;
+}
+
+const G4Nucleus & G4HadronicWhiteBoard::GetTargetNucleus() 
+{ 
+  return theTarget;
+}
+
+G4ParticleDefinition * G4HadronicWhiteBoard::GetPDef() {return theDef;}
+G4String G4HadronicWhiteBoard::GetParticleName() {return theName;}
+G4double G4HadronicWhiteBoard::GetEnergy() {return theE;}
+G4double G4HadronicWhiteBoard::GetPx(){return thePx;}
+G4double G4HadronicWhiteBoard::GetPy(){return thePy;}
+G4double G4HadronicWhiteBoard::GetPz(){return thePz;}
+G4double G4HadronicWhiteBoard::GetA(){return theA;}
+G4double G4HadronicWhiteBoard::GetZ(){return theZ;}
+
   
-  void G4HadronicWhiteBoard::SetProjectile(const G4HadProjectile & aProjectile)
-  {
-    theProjectile = const_cast<G4HadProjectile*>(& aProjectile);
-    theDef = const_cast<G4ParticleDefinition*>(theProjectile->GetDefinition());
-    theName = const_cast<char *>(theDef->GetParticleName().c_str() );
-    theE = theProjectile->Get4Momentum().t();
-    thePx = theProjectile->Get4Momentum().vect().x();
-    thePy = theProjectile->Get4Momentum().vect().y();
-    thePz = theProjectile->Get4Momentum().vect().z();
-  }
+void G4HadronicWhiteBoard::SetProjectile(const G4HadProjectile & aProjectile)
+{
+  theProjectile = const_cast<G4HadProjectile*>(& aProjectile);
+  theDef = const_cast<G4ParticleDefinition*>(theProjectile->GetDefinition());
+  theName = const_cast<char *>(theDef->GetParticleName().c_str() );
+  theE = theProjectile->Get4Momentum().t();
+  thePx = theProjectile->Get4Momentum().vect().x();
+  thePy = theProjectile->Get4Momentum().vect().y();
+  thePz = theProjectile->Get4Momentum().vect().z();
+}
+
     
-  void G4HadronicWhiteBoard::SetTargetNucleus(const G4Nucleus & aTarget) 
-  {
-    theTarget = aTarget;
-    theA = theTarget.GetN();
-    theZ = theTarget.GetZ();
-  }
+void G4HadronicWhiteBoard::SetTargetNucleus(const G4Nucleus & aTarget) 
+{
+  theTarget = aTarget;
+  theA = theTarget.GetN();
+  theZ = theTarget.GetZ();
+}
 
 
-  void G4HadronicWhiteBoard::Dump()
-  {
-    std::cerr << std::endl;
-    std::cerr << "*** Geant4 Hadronic Reaction Information ***" 
-              << std::endl;
-    std::cerr << "    Nucleus A, Z = " << theA << " " << theZ 
-              << std::endl;
-    std::cerr << "    Projectile was a " << theName 
-              << std::endl;
-    std::cerr << "    projectile momentum (px, py, pz) = (" << thePx << ", " 
-              << thePy << ", " << thePz << ")" << std::endl;
-    std::cerr << "    Projectile energy = "<< theE 
-              << std::endl;
-    std::cerr << "*** End of Geant4 Hadronic Reaction Information ***"
-              << std::endl;
-    G4Exception("G4HadronicProcess", "001", FatalException, "segmentation fault");
-  }
+void G4HadronicWhiteBoard::SetProcessName(const G4String& aProcessName) 
+{
+  theProcessName = aProcessName;
+}
+
+
+void G4HadronicWhiteBoard::SetModelName(const G4String& aModelName) 
+{
+  theModelName = aModelName;
+}
+
+
+void G4HadronicWhiteBoard::Dump()
+{
+  std::cerr << std::endl;
+  std::cerr << "*** Geant4 Hadronic Reaction Information ***" 
+            << std::endl;
+  std::cerr << "    Process: " << theProcessName << " , Model: " 
+            << theModelName << std::endl;
+  std::cerr << "    Nucleus A, Z = " << theA << " " << theZ 
+            << std::endl;
+  std::cerr << "    Projectile was a " << theName 
+            << std::endl;
+  std::cerr << "    projectile momentum (px, py, pz) = (" << thePx << ", " 
+            << thePy << ", " << thePz << ")" << std::endl;
+  std::cerr << "    Projectile energy = "<< theE 
+            << std::endl;
+  std::cerr << "*** End of Geant4 Hadronic Reaction Information ***"
+            << std::endl;
+  G4Exception(theModelName, "001", FatalException, "segmentation fault");
+}
