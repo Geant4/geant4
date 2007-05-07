b5647650e33bacb90607e9acd94ad64994c39858
Gunter Folger <Gunter.Folger@cern.ch>
2007-05-07 14:34:29 +0000

    add QGSP_BERT_TRV
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@30071 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/physics_lists/lists/include/QGSP_BERT_TRV.hh
@@ -0,0 +1,69 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+// $Id: QGSP_BERT_TRV.hh,v 1.1 2007-05-07 14:34:29 gunter Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+//---------------------------------------------------------------------------
+//
+// ClassName:   QGSP_BERT_TRV
+//
+// Author: 2007 G.Folger
+//       Created from QGSP_BERT
+//
+// Modified:
+//
+//----------------------------------------------------------------------------
+//
+#ifndef TQGSP_BERT_TRV_h
+#define TQGSP_BERT_TRV_h 1
+
+#include "G4VModularPhysicsList.hh"
+#include "globals.hh"
+#include "CompileTimeConstraints.hh"
+
+template<class T>
+class TQGSP_BERT_TRV: public T
+{
+public:
+  TQGSP_BERT_TRV(G4int ver = 1);
+  virtual ~TQGSP_BERT_TRV();
+  
+public:
+  // SetCuts() 
+  virtual void SetCuts();
+
+private:
+  enum {ok = CompileTimeConstraints::IsA<T, G4VModularPhysicsList>::ok };
+};
+
+#include "QGSP_BERT_TRV.icc"
+typedef TQGSP_BERT_TRV<G4VModularPhysicsList> QGSP_BERT_TRV;
+
+
+#endif
+
+
+
