997d90dedc2f85df6c6b2e8fc9bdf4bcb435f178
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2007-05-11 13:05:53 +0000

    Some code formatting for Soft.Ref.Manual plus disclaimer
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@30148 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/global/HEPNumerics/include/G4SimplexDownhill.hh
+++ source/global/HEPNumerics/include/G4SimplexDownhill.hh
@@ -1,13 +1,42 @@
 //
-// Minimization of a functionof n variables
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
 //
-// Reference 
-// "A Simplex method for function minimization" By J. A. Nelder and R. Mead, Computer Journal, 7, 308 (1965)
-// and also 
-// "Numerical Recipes in C: the art of scientific computing" By William H., Cambridge University Press ISBN 0521437202 (1992)
 //
-// Koi, Tatsumi (SLAC/SCCS)
-// 
+// $Id: G4SimplexDownhill.hh,v 1.2 2007-05-11 13:05:53 gcosmo Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+// Class description:
+//
+// Class implementing minimization of a function of n variables.
+// Reference: "A Simplex method for function minimization"
+//            by J. A. Nelder and R. Mead, Computer Journal, 7, 308 (1965)
+// and also: "Numerical Recipes in C: the art of scientific computing"
+//            by William H., Cambridge University Press ISBN 0521437202 (1992)
+
+// Author: Tatsumi Koi (SLAC/SCCS), 2007
+// --------------------------------------------------------------------------
 
 #ifndef G4SimplexDownhill
 #define G4SimplexDownhill_h
@@ -17,13 +46,14 @@
 #include <vector> 
 #include <algorithm> 
 
-template<class T> class G4SimplexDownhill
+template<class T>
+class G4SimplexDownhill
 {
 
-      T* target;
-   public: 
+   public: // with description
 
-      G4SimplexDownhill( T* tp , G4int n ){ target = tp; init(); numberOfVariable = n; };
+      G4SimplexDownhill( T* tp , G4int n )
+      { target = tp; init(); numberOfVariable = n; }
 
       ~G4SimplexDownhill();
 
@@ -33,9 +63,9 @@ template<class T> class G4SimplexDownhill
 
 
    private:
-      G4double getValue( std::vector< G4double > x ) { return target->GetValueOfMinimizingFunction( x ); };
 
-      G4int numberOfVariable; 
+      G4double getValue( std::vector< G4double > x )
+      { return target->GetValueOfMinimizingFunction( x ); }
 
       void initialize();
       std::vector< std::vector< G4double > > currentSimplex;
@@ -48,13 +78,22 @@ template<class T> class G4SimplexDownhill
 
       G4bool isItGoodEnough();
 
-      std::vector< G4double > getReflectionPoint( std::vector< G4double > , std::vector< G4double > );
-      std::vector< G4double > getExpansionPoint( std::vector< G4double > , std::vector< G4double > );
-      std::vector< G4double > getContractionPoint( std::vector< G4double > , std::vector< G4double > );
+      std::vector< G4double > getReflectionPoint( std::vector< G4double > ,
+                                                  std::vector< G4double > );
+      std::vector< G4double > getExpansionPoint( std::vector< G4double > ,
+                                                 std::vector< G4double > );
+      std::vector< G4double > getContractionPoint( std::vector< G4double > ,
+                                                   std::vector< G4double > );
 
       void doDownhill();
 
       void init();
+
+   private:
+
+      T* target;
+
+      G4int numberOfVariable; 
 
       G4double alpha;
       G4double beta;
