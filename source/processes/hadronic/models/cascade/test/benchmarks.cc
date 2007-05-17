3f4d4e613569cb867516dd9901ebf0b43b950a38
Aatos Heikkinen <miheikki@cern.ch>
2007-05-17 16:56:47 +0000

    integrating testInterface to benchmarks.cc
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@30324 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/models/cascade/test/benchmarks.cc
+++ source/processes/hadronic/models/cascade/test/benchmarks.cc
@@ -52,10 +52,15 @@
 #include "G4BertiniData.hh"
 #include "G4CascadSpecialFunctions.hh"
 
+#include "G4IonTable.hh"
+#include "G4Nucleus.hh"
+#include "G4NucleiModel.hh"
+
 G4int benchmarkAll();
 G4int tTiming();
 G4int tBertiniData();
 G4int tCrossSections();
+G4int tInterface();
 
 int main(int argc, char **argv ) {
 
@@ -66,9 +71,11 @@ int main(int argc, char **argv ) {
   G4cout << "Bertini data:  " ; if (tBertiniData()){ G4cout << "OK";} else {G4cout << "fail" << G4endl;}; G4cout << G4endl;  // test singleton data container 
 
   G4cout << "Cross section:  " ; if (tCrossSections()){ G4cout << "OK";} else {G4cout << "fail" << G4endl;}; G4cout << G4endl;  // test cross sections 
+
+  G4cout << "Interface:  " ; if (tInterface()){ G4cout << "OK";} else {G4cout << "fail" << G4endl;}; G4cout << G4endl;  // test cross sections 
+
+
   return 0;       
-
-
 }
 
 G4int benchmarkAll()  {
@@ -240,3 +247,117 @@ G4int tCrossSections() {   // print cross section data
   }
   return 1;
 }
+
+
+
+
+int tInterface() { // test iterface
+
+typedef std::vector<G4InuclElementaryParticle>::iterator particleIterator;
+typedef std::vector<G4InuclNuclei>::iterator nucleiIterator;
+
+  enum particleType { nuclei = 0, proton = 1, neutron = 2, pionPlus = 3, pionMinus = 5, pionZero = 7, photon = 10 };
+  G4int verboseLevel = 3;
+
+
+
+  G4int bulletType = 0;
+
+  std::vector<G4double>  momentumBullet(4, 0.0);
+  momentumBullet[0] = 1.37126;
+  momentumBullet[3] = 1.5;
+
+  G4InuclParticle *  bullet = new G4InuclElementaryParticle(momentumBullet, 1); 
+
+  G4InuclNuclei*   target  = NULL;
+  G4InuclParticle* targetH = NULL;
+  G4NucleiModel*   model = NULL;
+
+  G4double theNucleusA = 1;
+  std::vector<G4double> targetMomentum(4, 0.0);
+
+  G4CollisionOutput output;
+
+  G4ElementaryParticleCollider*   colep = new G4ElementaryParticleCollider;
+  G4IntraNucleiCascader*            inc = new G4IntraNucleiCascader; // the actual cascade
+  inc->setInteractionCase(1); // Interaction type is particle with nuclei.
+
+  G4NonEquilibriumEvaporator*     noneq = new G4NonEquilibriumEvaporator;
+  G4EquilibriumEvaporator*         eqil = new G4EquilibriumEvaporator;
+  G4Fissioner*                     fiss = new G4Fissioner;
+  G4BigBanger*                     bigb = new G4BigBanger;
+
+  G4InuclCollider*             collider = new G4InuclCollider(colep, inc, noneq, eqil, fiss, bigb);
+
+  for (G4int i = 1; i< 100 ; i++) {
+    if ( theNucleusA < 1.5 ) 
+      {
+	model = new G4NucleiModel(new G4InuclNuclei(targetMomentum, 1, 1));
+	targetH = new G4InuclElementaryParticle((model->generateNucleon(1, 1)).getMomentum(), 1); 
+   
+	//		do
+	  {
+	    G4cout << "+";
+	    output = collider->collide(bullet, targetH); 
+	  } 
+	  //		while(output.getOutgoingParticles().size()<2.5);
+      } 
+    else 
+      {
+	output = collider->collide(bullet, target ); 
+      }
+
+    if (verboseLevel > 1) 
+      {
+	G4cout << " Cascade output: " << G4endl;
+	output.printCollisionOutput();
+      }
+  
+    // Convert cascade data to use hadronics interface
+
+    std::vector<G4InuclNuclei>             nucleiFragments = output.getNucleiFragments();
+    std::vector<G4InuclElementaryParticle> particles =       output.getOutgoingParticles();
+
+    G4int numSecondaries = nucleiFragments.size()+particles.size();
+    G4cout << "num secondaries: " << numSecondaries << G4endl;
+    if(!particles.empty()) { 
+      particleIterator ipart;
+      G4int outgoingParticle;
+
+      for(ipart = particles.begin(); ipart != particles.end(); ipart++) {
+	outgoingParticle = ipart->type();
+	std::vector<G4double> mom = ipart->getMomentum();
+	G4double ekin = ipart->getKineticEnergy() * GeV;
+	G4ThreeVector aMom(mom[1], mom[2], mom[3]);
+	aMom = aMom.unit();
+
+      
+      }
+
+      nucleiIterator ifrag;
+
+      for(ifrag = nucleiFragments.begin(); ifrag != nucleiFragments.end(); ifrag++) 
+	{
+	  G4double eKin = ifrag->getKineticEnergy() * GeV;
+	  std::vector<G4double> mom = ifrag->getMomentum();
+	  G4ThreeVector aMom(mom[1], mom[2], mom[3]);
+	  aMom = aMom.unit();
+
+	  // hpw @@@ ==> Should be zero: G4double fragmentExitation = ifrag->getExitationEnergyInGeV();
+
+	  if (verboseLevel > 2) {
+	    G4cout << " Nuclei fragment: " << G4endl;
+	    ifrag->printParticle();
+	  }
+	  G4int A = G4int(ifrag->getA());
+	  G4int Z = G4int(ifrag->getZ());
+
+	}
+    }
+  }
+  return 1;
+}
+
+
+
+
