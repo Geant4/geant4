f8300de1168ce1186d3d7cacf9243e7ae4de8841
Aatos Heikkinen <miheikki@cern.ch>
2007-05-21 15:46:47 +0000

    adding more plots
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@30479 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/models/cascade/test/scripts/analyze.C
+++ source/processes/hadronic/models/cascade/test/scripts/analyze.C
@@ -4,13 +4,22 @@
   c->Divide(3,2);
 
   c->cd(1);ntuple.Draw("momX");
-  c->cd(2); ntuple.Draw("momX:momZ");
+  c->cd(2); ntuple.Draw("momX:momZ","","surf");
   c->cd(3); ntuple.Draw("momX:momY:momZ","particleId==10");
   c->cd(4); ntuple.Draw("momX:momY:momZ","particleId==1");
 
   c->cd(5);ntuple.Draw("momZ >>hmomz(100,0,0.01)","particleId==1");
   ntuple.Draw("momZ >>+hmomz","particleId==2");
   c->cd(6);ntuple.Draw("fragmentA:fragmentZ >>haz(20,0,20,30,0,30)");
+
+  TCanvas *e =new TCanvas("e","e",0,0,600,400);
+  e->Divide(3,2);
+  e->cd(1); ntuple.Draw("particleId");
+  e->cd(2); ntuple.Draw("momZ:particleId","","box"); //or cont1
+  e->cd(3); ntuple.Draw("kineticEnergy:particleId","","box");   
+
+  e->cd(4); ntuple.Draw("sqrt(momZ*momZ)/sqrt(momX*momX+momY*momY):particleId","","box");   
+
 
   TCanvas *d =new TCanvas("d","d",0,0,600,400);
   d->Divide(3,2);
@@ -26,4 +35,8 @@
   d->cd(4); ntuple->Draw("kineticEnergy "); // now draws only protons
   d->cd(5); ntuple->Draw("kineticEnergy:momX ");
   d->cd(6); ntuple->Draw("kineticEnergy:momZ ");
+
+
+
+
 }
