9e499d1f1c276d5401297e8dcebbdce8858019b9
Vladimir Ivantchenko <Vladimir.Ivantchenko@cern.ch>
2007-05-30 15:05:17 +0000

    return back
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@30811 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ tests/test36/include/StepMax.hh
@@ -0,0 +1,74 @@
+#ifndef StepMax_h
+#define StepMax_h 1
+
+#include "globals.hh"
+#include "G4VDiscreteProcess.hh"
+#include "G4ParticleDefinition.hh"
+#include "G4Step.hh"
+
+//#include "DetectorConstruction.hh"
+
+class StepMaxMessenger;
+
+const G4int MaxAbsor = 3;
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+class StepMax : public G4VDiscreteProcess
+{
+public:
+
+  StepMax(const G4String& processName = "UserStepMax");
+  virtual ~StepMax();
+
+  G4bool   IsApplicable(const G4ParticleDefinition&);
+
+  void     SetStepMax(G4int, G4double);
+
+  G4double GetStepMax(G4int k) {return stepMax[k];};
+
+  G4double PostStepGetPhysicalInteractionLength( const G4Track& track,
+						 G4double previousStepSize,
+						 G4ForceCondition* condition);
+
+  G4VParticleChange* PostStepDoIt(const G4Track&, const G4Step&);
+
+  G4double GetMeanFreePath(const G4Track&, G4double,G4ForceCondition*)
+     {return DBL_MAX;};    
+
+private:
+
+  G4double stepMax[MaxAbsor];
+     
+  StepMaxMessenger* pMess;
+};
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+inline 
+G4double StepMax::PostStepGetPhysicalInteractionLength( const G4Track& aTrack,
+                                    G4double, G4ForceCondition* condition)
+{
+  // condition is set to "Not Forced"
+  *condition = NotForced;
+  
+  G4double limit = DBL_MAX; 
+  G4int n = aTrack.GetVolume()->GetCopyNo();
+  if (n < MaxAbsor) limit = stepMax[n];
+  return limit;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+inline 
+G4VParticleChange* StepMax::PostStepDoIt(const G4Track& aTrack, const G4Step&)
+{
+   // do nothing
+   aParticleChange.Initialize(aTrack);
+   return &aParticleChange;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+#endif
+
