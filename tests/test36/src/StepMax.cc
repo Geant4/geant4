727783f0b31aca0bfdaf9e6c52222207d79cf3d7
Vladimir Ivantchenko <Vladimir.Ivantchenko@cern.ch>
2007-05-30 15:04:44 +0000

    return lost
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@30810 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ tests/test36/src/StepMax.cc
@@ -0,0 +1,30 @@
+#include "StepMax.hh"
+#include "StepMaxMessenger.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+StepMax::StepMax(const G4String& processName)
+ : G4VDiscreteProcess(processName)
+{
+  for (G4int k=0; k<MaxAbsor; k++) stepMax[k] = DBL_MAX;
+  pMess = new StepMaxMessenger(this);
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+StepMax::~StepMax() { delete pMess; }
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+G4bool StepMax::IsApplicable(const G4ParticleDefinition& particle)
+{
+  return (particle.GetPDGCharge() != 0. && !particle.IsShortLived());
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void StepMax::SetStepMax(G4int k,G4double step) {stepMax[k] = step;}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+
