b8434f27807776f99b13f5b4af5965d8cc95cba9
Alexander Howard <alexander.howard@cern.ch>
2007-06-01 10:00:10 +0000

    Renamed classes with New prefix to standard names, AH 1/6/07
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@30921 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/biasing/include/G4GeometrySampler.hh
+++ source/processes/biasing/include/G4GeometrySampler.hh
@@ -24,7 +24,7 @@
 // ********************************************************************
 //
 //
-// $Id: G4GeometrySampler.hh,v 1.9 2007-06-01 09:52:23 ahoward Exp $
+// $Id: G4GeometrySampler.hh,v 1.10 2007-06-01 10:00:10 ahoward Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // ----------------------------------------------------------------------
@@ -47,7 +47,7 @@
 
 class G4ImportanceConfigurator;
 class G4WeightWindowConfigurator;
-//class G4ScoreConfigurator;
+class G4ScoreConfigurator;
 class G4WeightCutOffConfigurator;
 class G4VGCellFinder;
 
@@ -59,7 +59,7 @@ public:  // with description
   explicit G4GeometrySampler(G4VPhysicalVolume *worldvolume, const G4String &particlename);
   virtual ~G4GeometrySampler();
 
-  //  virtual void PrepareScoring(G4VScorer *Scorer);
+  virtual void PrepareScoring(G4VScorer *Scorer);
   virtual void PrepareImportanceSampling(G4VIStore *istore,
                                          const G4VImportanceAlgorithm 
                                          *ialg);
@@ -90,7 +90,7 @@ private:
   G4String fParticleName;
   G4VPhysicalVolume* fWorld;
   G4ImportanceConfigurator *fImportanceConfigurator;
-  //  G4ScoreConfigurator *fScoreConfigurator;
+  G4ScoreConfigurator *fScoreConfigurator;
   G4VGCellFinder *fGCellFinder;
   G4WeightCutOffConfigurator *fWeightCutOffConfigurator;
   G4VIStore *fIStore;
