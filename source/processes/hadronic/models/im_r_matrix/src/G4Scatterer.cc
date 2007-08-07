7d9e1f0ce44f5c18380d5d161b74bea32ec4c767
Dennis Herbert Wright <dennis@cern.ch>
2007-08-07 20:43:19 +0000

    use 2006 version
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@31708 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/models/im_r_matrix/src/G4Scatterer.cc
+++ source/processes/hadronic/models/im_r_matrix/src/G4Scatterer.cc
@@ -1,26 +1,29 @@
 //
 // ********************************************************************
-// * DISCLAIMER                                                       *
+// * License and Disclaimer                                           *
 // *                                                                  *
-// * The following disclaimer summarizes all the specific disclaimers *
-// * of contributors to this software. The specific disclaimers,which *
-// * govern, are listed with their locations in:                      *
-// *   http://cern.ch/geant4/license                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
 // *                                                                  *
 // * Neither the authors of this software system, nor their employing *
 // * institutes,nor the agencies providing financial support for this *
 // * work  make  any representation or  warranty, express or implied, *
 // * regarding  this  software system or assume any liability for its *
-// * use.                                                             *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
 // *                                                                  *
-// * This  code  implementation is the  intellectual property  of the *
-// * GEANT4 collaboration.                                            *
-// * By copying,  distributing  or modifying the Program (or any work *
-// * based  on  the Program)  you indicate  your  acceptance of  this *
-// * statement, and all its terms.                                    *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
 // ********************************************************************
 //
-// $Id: G4Scatterer.cc,v 1.14 2004-12-07 14:50:57 gunter Exp $ //
+// $Id: G4Scatterer.cc,v 1.15 2007-08-07 20:43:19 dennis Exp $ //
 //
 
 #include "globals.hh"
@@ -47,7 +50,8 @@ typedef GROUP2(G4CollisionNN, G4CollisionMesonBaryon) theChannels;
 
 G4Scatterer::G4Scatterer()
 {
-  G4ForEach<theChannels, Register>::Apply(&collisions);
+  Register aR;
+  G4ForEach<theChannels>::Apply(&aR, &collisions);
 }
 
 
@@ -57,36 +61,49 @@ G4Scatterer::~G4Scatterer()
   collisions.clear();
 }
 
-
 G4double G4Scatterer::GetTimeToInteraction(const G4KineticTrack& trk1, 
 					   const G4KineticTrack& trk2)
 {
   G4double time = DBL_MAX;
+    G4double distance_fast;
+  G4LorentzVector mom1 = trk1.GetTrackingMomentum();
+//  G4cout << "zcomp=" << std::abs(mom1.vect().unit().z() -1 ) << G4endl;
+  G4double collisionTime;
   
-  G4double sqrtS = (trk1.Get4Momentum() + trk2.Get4Momentum()).mag();
-
-  // Check whether there is enough energy for elastic scattering 
-  // (to put the particles on to mass shell
+  if ( std::abs(mom1.vect().unit().z() -1 ) < 1e-6 ) 
+  {
+     G4ThreeVector position = trk2.GetPosition() - trk1.GetPosition();
+     G4double deltaz=position.z();
+     G4double velocity = mom1.z()/mom1.e() * c_light;
+     
+     collisionTime=deltaz/velocity;
+     distance_fast=position.x()*position.x() + position.y()*position.y();
+  } else {
   
-//  if (trk1.GetDefinition()->GetPDGMass() + trk2.GetDefinition()->GetPDGMass() < sqrtS)
-    if (trk1.GetActualMass() + trk2.GetActualMass() < sqrtS)
-    {
-      G4LorentzVector mom1 = trk1.GetTrackingMomentum();
-      //  The nucleons of the nucleus are FROZEN, ie. do not move..
-      
-      G4ThreeVector position = trk1.GetPosition() - trk2.GetPosition();    
+    //  The nucleons of the nucleus are FROZEN, ie. do not move..
 
-      // G4ThreeVector velocity = (mom1.boostVector() - mom2.boostVector()) * c_light;
-      if ( mom1.mag2() < -1.*eV )
-      {
-        G4cout << "G4Scatterer::GetTimeToInteraction(): negative m2:" << mom1.mag2() << G4endl;
-      } 
-      G4ThreeVector velocity = mom1.vect()/mom1.e() * c_light; 
-      G4double collisionTime = - (position * velocity) / (velocity * velocity);    // can't divide by /c_light;
-      
+    G4ThreeVector position = trk2.GetPosition() - trk1.GetPosition();    
+
+    G4ThreeVector velocity = mom1.vect()/mom1.e() * c_light;  // mom1.boostVector() will exit on slightly negative mass
+    collisionTime = (position * velocity) / velocity.mag2();    // can't divide by /c_light;
+    position -= velocity * collisionTime;
+    distance_fast=position.mag2();
+    
+//    if ( collisionTime>0 ) G4cout << " dis1/2 square" << dis1 <<" "<< dis2 << G4endl;
+//     collisionTime = GetTimeToClosestApproach(trk1,trk2);
+  }
      if (collisionTime > 0)
 	{ 
+	   static const G4double maxCrossSection = 500*millibarn;
+	   if(0.7*pi*distance_fast>maxCrossSection) return time;
+
+       
            G4LorentzVector mom2(0,0,0,trk2.Get4Momentum().mag());
+
+// 	   G4ThreeVector momLab = mom1.vect();// frozen Nucleus - mom2.vect();
+// 	   G4ThreeVector posLab = trk1.GetPosition() - trk2.GetPosition();
+// 	   G4double disLab=posLab * posLab - (posLab*momLab) * (posLab*momLab) /(momLab.mag2());
+
 	   G4LorentzRotation toCMSFrame((-1)*(mom1 + mom2).boostVector());
 	   mom1 = toCMSFrame * mom1;
 	   mom2 = toCMSFrame * mom2;
@@ -100,9 +117,16 @@ G4double G4Scatterer::GetTimeToInteraction(const G4KineticTrack& trk1,
 
 	  // Calculate the impact parameter
 
-	   G4double distance = pos * pos - (pos*mom) * (pos*mom) / (mom*mom);
+	   G4double distance = pos * pos - (pos*mom) * (pos*mom) / (mom.mag2());
+
+//     G4cout << " disDiff " << distance-disLab << " " << disLab 
+//            << " " << std::abs(distance-disLab)/distance << G4endl
+//	    << " mom/Lab " << mom << " " << momLab << G4endl
+//	    << " pos/Lab " << pos << " " << posLab 
+//	    << G4endl;
+
 	   // global optimization
-	   static const G4double maxCrossSection = 500*millibarn;
+//	   static const G4double maxCrossSection = 500*millibarn;
 	   if(pi*distance>maxCrossSection) return time;
 	   
 	   // charged particles special
@@ -111,6 +135,7 @@ G4double G4Scatterer::GetTimeToInteraction(const G4KineticTrack& trk1,
 	      std::abs(trk2.GetDefinition()->GetPDGCharge())>0.1 &&
 	      pi*distance>maxChargedCrossSection) return time;
 	      
+           G4double sqrtS = (trk1.Get4Momentum() + trk2.Get4Momentum()).mag();
 	   // neutrons special   
 	   if(( trk1.GetDefinition() == G4Neutron::Neutron() ||
 	        trk1.GetDefinition() == G4Neutron::Neutron() ) &&
@@ -148,6 +173,9 @@ G4double G4Scatterer::GetTimeToInteraction(const G4KineticTrack& trk1,
  * 	  if(1)
  * 	    return time;
  */
+	   
+	   if ((trk1.GetActualMass()+trk2.GetActualMass()) > sqrtS) return time;
+
 	    
 	  
 	  G4VCollision* collision = FindCollision(trk1,trk2);
@@ -228,20 +256,6 @@ G4double G4Scatterer::GetTimeToInteraction(const G4KineticTrack& trk1,
 	  // End of debugging
 	      */
 	}
-    }
-  else
-    {
-	      /*
-      // For debugging
-      G4cout << "G4Scatterer - Infinite time to interaction"
-	     << ": sqrtS " << sqrtS
-	     << ", mass1 = " << trk1.GetDefinition()->GetPDGMass()
-	     << ", mass2 = " << trk2.GetDefinition()->GetPDGMass()
-	     << G4endl;
-	throw G4HadronicException(__FILE__, __LINE__, "G4Scatterer TimeToInteraction is INF");
-      // End of debugging
-	      */
-    }
 
   return time;
 }
