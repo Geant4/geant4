b5d34193170019fb7698f8d3388221eb862ebae8
Tsukasa Aso <aso@nc-toyama.ac.jp>
2007-08-14 22:00:40 +0000

    Additional 3D scorers
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@31785 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/digits_hits/utils/include/G4PSPassageCellCurrent3D.hh
@@ -0,0 +1,63 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+// $Id: G4PSPassageCellCurrent3D.hh,v 1.1 2007-08-14 22:00:40 taso Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+
+#ifndef G4PSPassageCellCurrent3D_h
+#define G4PSPassageCellCurrent3D_h 1
+
+#include "G4PSPassageCellCurrent.hh"
+
+////////////////////////////////////////////////////////////////////////////////
+// (Description)
+//   This is a primitive scorer class for scoring a current.
+//   The number of tracks passing through the geometry are taken 
+//  into account.
+//
+// Created: 2007-08-14  Tsukasa ASO
+// 
+///////////////////////////////////////////////////////////////////////////////
+
+
+class G4PSPassageCellCurrent3D : public G4PSPassageCellCurrent
+{
+   public: // with description
+      G4PSPassageCellCurrent3D(G4String name,
+			    G4int ni,G4int nj, G4int nk,
+			    G4int di=2, G4int dj=1, G4int dk=0);
+      virtual ~G4PSPassageCellCurrent3D();
+
+  protected: // with description
+      virtual G4int GetIndex(G4Step*);
+
+  private:
+      G4int fNi, fNj, fNk;
+      G4int fDepthi, fDepthj, fDepthk;
+};
+#endif
+
