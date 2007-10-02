8fcc66e3496de652f740950e84a854fa23f8b22c
Vladimir Ivantchenko <Vladimir.Ivantchenko@cern.ch>
2007-10-02 10:17:12 +0000

    enable AtRest
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@32197 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/electromagnetic/standard/src/G4eplusAnnihilation.cc
+++ source/processes/electromagnetic/standard/src/G4eplusAnnihilation.cc
@@ -23,7 +23,7 @@
 // * acceptance of all terms of the Geant4 Software license.          *
 // ********************************************************************
 //
-// $Id: G4eplusAnnihilation.cc,v 1.26 2006-09-14 10:27:19 maire Exp $
+// $Id: G4eplusAnnihilation.cc,v 1.27 2007-10-02 10:17:12 vnivanch Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // -------------------------------------------------------------------
@@ -66,7 +66,9 @@ using namespace std;
 
 G4eplusAnnihilation::G4eplusAnnihilation(const G4String& name)
   : G4VEmProcess(name), isInitialised(false)
-{}
+{
+  enableAtRestDoIt = true;
+}
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
 
