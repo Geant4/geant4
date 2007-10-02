dd0f3b52f7c52b65f553fe6db3ea4a7635ca015c
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2007-10-02 09:50:46 +0000

    Removed useless debug printout...
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@32188 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/geometry/solids/specific/src/G4Polycone.cc
+++ source/geometry/solids/specific/src/G4Polycone.cc
@@ -24,7 +24,7 @@
 // ********************************************************************
 //
 //
-// $Id: G4Polycone.cc,v 1.38 2007-07-19 08:18:05 gcosmo Exp $
+// $Id: G4Polycone.cc,v 1.39 2007-10-02 09:50:46 gcosmo Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // 
@@ -638,7 +638,6 @@ G4ThreeVector G4Polycone::GetPointOnCone(G4double fRmin1, G4double fRmax1,
                              fRmax2-((zRand-fDz)/(2.*fDz))*(fRmax1-fRmax2)); 
     point =  G4ThreeVector (rRand1*std::cos(startPhi),
                             rRand1*std::sin(startPhi), zRand);
-    G4cout<<"Point3="<<point<<G4endl;
   }
   else
   { 
