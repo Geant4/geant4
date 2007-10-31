6d9b6818e6e9d6ee5afda95460cda79abb6a946d
Alexander Howard <alexander.howard@cern.ch>
2007-10-31 18:05:44 +0000

    Fixed G4ProcessPlacer.cc to put biasing process second for AlongStep, AH 31/10/07
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@32588 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/biasing/include/G4ProcessPlacer.hh
+++ source/processes/biasing/include/G4ProcessPlacer.hh
@@ -24,7 +24,7 @@
 // ********************************************************************
 //
 //
-// $Id: G4ProcessPlacer.hh,v 1.2 2007-06-01 09:16:33 ahoward Exp $
+// $Id: G4ProcessPlacer.hh,v 1.3 2007-10-31 18:05:44 ahoward Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // ----------------------------------------------------------------------
@@ -82,6 +82,8 @@ private:
   void AddProcessAs(G4VProcess *process, SecondOrLast);
 
   void PrintProcVec(G4ProcessVector* processVec);
+  void PrintAlongStepGPILVec();  
+  void PrintAlongStepDoItVec();  
   void PrintPostStepGPILVec();  
   void PrintPostStepDoItVec();  
 
