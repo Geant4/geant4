203f1b16fa93ddcb1ca56dac4d9288aac8a5e647
Ian Mclaren <mclareni@cern.ch>
2007-11-01 15:18:31 +0000

    Add example field04 as test704
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@32592 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- tests/tools/bin/run.sh
+++ tests/tools/bin/run.sh
@@ -423,6 +423,10 @@ if [ $1 = "all" ] ; then
   $G4STTDIR/bin/run.sh test5018
   ${G4STTDIR}/bin/geant4-unix.pl --end-test test5018 72
   
+  ${G4STTDIR}/bin/geant4-unix.pl --start-test test704 73
+  $G4STTDIR/bin/run.sh test704
+  ${G4STTDIR}/bin/geant4-unix.pl --end-test test704 73
+  
 #  ${G4STTDIR}/bin/geant4-unix.pl --start-test test803 61
 #    $G4STTDIR/bin/run.sh test803
 #  ${G4STTDIR}/bin/geant4-unix.pl --end-test test803 61
