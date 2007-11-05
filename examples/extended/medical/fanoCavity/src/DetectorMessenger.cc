b7dbecd82dc1c560a8fac80e55b43c164939426a
Michel Maire <Michel.Maire@cern.ch>
2007-11-05 13:44:18 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@32674 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/medical/fanoCavity/src/DetectorMessenger.cc
+++ examples/extended/medical/fanoCavity/src/DetectorMessenger.cc
@@ -23,7 +23,7 @@
 // * acceptance of all terms of the Geant4 Software license.          *
 // ********************************************************************
 //
-// $Id: DetectorMessenger.cc,v 1.2 2007-10-08 12:05:02 maire Exp $
+// $Id: DetectorMessenger.cc,v 1.3 2007-11-05 13:44:18 maire Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -52,7 +52,7 @@ DetectorMessenger::DetectorMessenger(DetectorConstruction * Det)
   wallMater->SetGuidance("Set material of the wall.");
   wallMater->SetParameterName("wallMat",false);
   
-  wallThick = new G4UIcmdWithADoubleAndUnit("/testem/det/wallTickness",this);
+  wallThick = new G4UIcmdWithADoubleAndUnit("/testem/det/wallThickness",this);
   wallThick->SetGuidance("Set tickness of the wall");
   wallThick->SetParameterName("wallTick",false);
   wallThick->SetRange("wallTick>0.");
@@ -62,7 +62,7 @@ DetectorMessenger::DetectorMessenger(DetectorConstruction * Det)
   cavMater->SetGuidance("Set material of the cavity.");
   cavMater->SetParameterName("cavMat",false);
   
-  cavThick = new G4UIcmdWithADoubleAndUnit("/testem/det/cavityTickness",this);
+  cavThick = new G4UIcmdWithADoubleAndUnit("/testem/det/cavityThickness",this);
   cavThick->SetGuidance("Set tickness of the cavity");
   cavThick->SetParameterName("cavityTick",false);
   cavThick->SetRange("cavityTick>0.");
