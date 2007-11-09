5bfebe4f282e67b767ac0ad591d5e0a3c6fc0831
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2007-11-09 16:06:02 +0000

    Made IdentifyAndPlaceSolid() inline and some cleanup...
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@32802 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/geometry/navigation/include/G4ParameterisedNavigation.hh
+++ source/geometry/navigation/include/G4ParameterisedNavigation.hh
@@ -24,7 +24,7 @@
 // ********************************************************************
 //
 //
-// $Id: G4ParameterisedNavigation.hh,v 1.5 2006-06-29 18:36:10 gunter Exp $
+// $Id: G4ParameterisedNavigation.hh,v 1.6 2007-11-09 16:06:02 gcosmo Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // 
@@ -63,8 +63,8 @@ class G4ParameterisedNavigation : public G4VoxelNavigation
     G4ParameterisedNavigation();
     ~G4ParameterisedNavigation();
 
-    G4SmartVoxelNode* ParamVoxelLocate( G4SmartVoxelHeader* pHead,
-                                  const G4ThreeVector& localPoint );
+    inline G4SmartVoxelNode* ParamVoxelLocate( G4SmartVoxelHeader* pHead,
+                                         const G4ThreeVector& localPoint );
 
     G4bool LevelLocate( G4NavigationHistory& history,
                   const G4VPhysicalVolume* blockedVol,
@@ -102,7 +102,7 @@ class G4ParameterisedNavigation : public G4VoxelNavigation
 
     // Necessary to resolve cases with nested parameterisations
 
-    G4VSolid* IdentifyAndPlaceSolid( G4int num,
+    inline G4VSolid* IdentifyAndPlaceSolid( G4int num,
                                      G4VPhysicalVolume* apparentPhys, 
                                      G4VPVParameterisation* curParam );
        // Call virtual 'Compute' methods, and copy information if nested.
