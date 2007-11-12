1c3dd6c511b953f9ab2456dde3fc9cc633233253
Laurent Garnier <laurent.garnier@univ-rennes1.fr>
2007-11-12 09:37:25 +0000

    Several moc file for different revisions of Qt
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@32864 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/visualization/OpenGL/src/G4OpenGLQtExportDialog_moc_030305.cc
@@ -0,0 +1,120 @@
+#if QT_VERSION < 0x040202
+
+/****************************************************************************
+** G4OpenGLQtExportDialog meta object code from reading C++ file 'G4OpenGLQtExportDialog.hh'
+**
+** Created: Mon Nov 12 10:36:59 2007
+**      by: The Qt MOC ($Id: G4OpenGLQtExportDialog_moc_030305.cc,v 1.1 2007-11-12 09:37:25 lgarnier Exp $)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#undef QT_NO_COMPAT
+#include "../include/G4OpenGLQtExportDialog.hh"
+#include <qmetaobject.h>
+#include <qapplication.h>
+
+#include <private/qucomextra_p.h>
+#if !defined(Q_MOC_OUTPUT_REVISION) || (Q_MOC_OUTPUT_REVISION != 26)
+#error "This file was generated using the moc from 3.3.5. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+const char *G4OpenGLQtExportDialog::className() const
+{
+    return "G4OpenGLQtExportDialog";
+}
+
+QMetaObject *G4OpenGLQtExportDialog::metaObj = 0;
+static QMetaObjectCleanUp cleanUp_G4OpenGLQtExportDialog( "G4OpenGLQtExportDialog", &G4OpenGLQtExportDialog::staticMetaObject );
+
+#ifndef QT_NO_TRANSLATION
+QString G4OpenGLQtExportDialog::tr( const char *s, const char *c )
+{
+    if ( qApp )
+	return qApp->translate( "G4OpenGLQtExportDialog", s, c, QApplication::DefaultCodec );
+    else
+	return QString::fromLatin1( s );
+}
+#ifndef QT_NO_TRANSLATION_UTF8
+QString G4OpenGLQtExportDialog::trUtf8( const char *s, const char *c )
+{
+    if ( qApp )
+	return qApp->translate( "G4OpenGLQtExportDialog", s, c, QApplication::UnicodeUTF8 );
+    else
+	return QString::fromUtf8( s );
+}
+#endif // QT_NO_TRANSLATION_UTF8
+
+#endif // QT_NO_TRANSLATION
+
+QMetaObject* G4OpenGLQtExportDialog::staticMetaObject()
+{
+    if ( metaObj )
+	return metaObj;
+    QMetaObject* parentObject = QDialog::staticMetaObject();
+    static const QUParameter param_slot_0[] = {
+	{ 0, &static_QUType_bool, 0, QUParameter::In }
+    };
+    static const QUMethod slot_0 = {"changeSizeBox", 1, param_slot_0 };
+    static const QUParameter param_slot_1[] = {
+	{ 0, &static_QUType_QString, 0, QUParameter::In }
+    };
+    static const QUMethod slot_1 = {"textWidthChanged", 1, param_slot_1 };
+    static const QUParameter param_slot_2[] = {
+	{ 0, &static_QUType_QString, 0, QUParameter::In }
+    };
+    static const QUMethod slot_2 = {"textHeightChanged", 1, param_slot_2 };
+    static const QMetaData slot_tbl[] = {
+	{ "changeSizeBox(bool)", &slot_0, QMetaData::Public },
+	{ "textWidthChanged(const QString&)", &slot_1, QMetaData::Public },
+	{ "textHeightChanged(const QString&)", &slot_2, QMetaData::Public }
+    };
+    metaObj = QMetaObject::new_metaobject(
+	"G4OpenGLQtExportDialog", parentObject,
+	slot_tbl, 3,
+	0, 0,
+#ifndef QT_NO_PROPERTIES
+	0, 0,
+	0, 0,
+#endif // QT_NO_PROPERTIES
+	0, 0 );
+    cleanUp_G4OpenGLQtExportDialog.setMetaObject( metaObj );
+    return metaObj;
+}
+
+void* G4OpenGLQtExportDialog::qt_cast( const char* clname )
+{
+    if ( !qstrcmp( clname, "G4OpenGLQtExportDialog" ) )
+	return this;
+    return QDialog::qt_cast( clname );
+}
+
+bool G4OpenGLQtExportDialog::qt_invoke( int _id, QUObject* _o )
+{
+    switch ( _id - staticMetaObject()->slotOffset() ) {
+    case 0: changeSizeBox((bool)static_QUType_bool.get(_o+1)); break;
+    case 1: textWidthChanged((const QString&)static_QUType_QString.get(_o+1)); break;
+    case 2: textHeightChanged((const QString&)static_QUType_QString.get(_o+1)); break;
+    default:
+	return QDialog::qt_invoke( _id, _o );
+    }
+    return TRUE;
+}
+
+bool G4OpenGLQtExportDialog::qt_emit( int _id, QUObject* _o )
+{
+    return QDialog::qt_emit(_id,_o);
+}
+#ifndef QT_NO_PROPERTIES
+
+bool G4OpenGLQtExportDialog::qt_property( int id, int f, QVariant* v)
+{
+    return QDialog::qt_property( id, f, v);
+}
+
+bool G4OpenGLQtExportDialog::qt_static_property( QObject* , int , int , QVariant* ){ return FALSE; }
+#endif // QT_NO_PROPERTIES
+
+#endif
---
+++ source/visualization/OpenGL/src/G4OpenGLQtViewer_moc_030305.cc
@@ -0,0 +1,182 @@
+#if QT_VERSION < 0x040202
+
+/****************************************************************************
+** G4OpenGLQtViewer meta object code from reading C++ file 'G4OpenGLQtViewer.hh'
+**
+** Created: Mon Nov 12 10:34:24 2007
+**      by: The Qt MOC ($Id: G4OpenGLQtViewer_moc_030305.cc,v 1.1 2007-11-12 09:37:25 lgarnier Exp $)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#undef QT_NO_COMPAT
+#include "../include/G4OpenGLQtViewer.hh"
+#include <qmetaobject.h>
+#include <qapplication.h>
+
+#include <private/qucomextra_p.h>
+#if !defined(Q_MOC_OUTPUT_REVISION) || (Q_MOC_OUTPUT_REVISION != 26)
+#error "This file was generated using the moc from 3.3.5. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+const char *G4OpenGLQtViewer::className() const
+{
+    return "G4OpenGLQtViewer";
+}
+
+QMetaObject *G4OpenGLQtViewer::metaObj = 0;
+static QMetaObjectCleanUp cleanUp_G4OpenGLQtViewer( "G4OpenGLQtViewer", &G4OpenGLQtViewer::staticMetaObject );
+
+#ifndef QT_NO_TRANSLATION
+QString G4OpenGLQtViewer::tr( const char *s, const char *c )
+{
+    if ( qApp )
+	return qApp->translate( "G4OpenGLQtViewer", s, c, QApplication::DefaultCodec );
+    else
+	return QString::fromLatin1( s );
+}
+#ifndef QT_NO_TRANSLATION_UTF8
+QString G4OpenGLQtViewer::trUtf8( const char *s, const char *c )
+{
+    if ( qApp )
+	return qApp->translate( "G4OpenGLQtViewer", s, c, QApplication::UnicodeUTF8 );
+    else
+	return QString::fromUtf8( s );
+}
+#endif // QT_NO_TRANSLATION_UTF8
+
+#endif // QT_NO_TRANSLATION
+
+QMetaObject* G4OpenGLQtViewer::staticMetaObject()
+{
+    if ( metaObj )
+	return metaObj;
+    QMetaObject* parentObject = QObject::staticMetaObject();
+    static const QUMethod slot_0 = {"actionDrawingWireframe", 0, 0 };
+    static const QUMethod slot_1 = {"actionDrawingLineRemoval", 0, 0 };
+    static const QUMethod slot_2 = {"actionDrawingSurfaceRemoval", 0, 0 };
+    static const QUMethod slot_3 = {"actionDrawingLineSurfaceRemoval", 0, 0 };
+    static const QUMethod slot_4 = {"actionControlPanels", 0, 0 };
+    static const QUMethod slot_5 = {"actionExitG4", 0, 0 };
+    static const QUMethod slot_6 = {"actionCreateEPS", 0, 0 };
+    static const QUParameter param_slot_7[] = {
+	{ 0, &static_QUType_int, 0, QUParameter::In }
+    };
+    static const QUMethod slot_7 = {"toggleDrawingAction", 1, param_slot_7 };
+    static const QUParameter param_slot_8[] = {
+	{ 0, &static_QUType_bool, 0, QUParameter::In }
+    };
+    static const QUMethod slot_8 = {"toggleMouseAction", 1, param_slot_8 };
+    static const QUParameter param_slot_9[] = {
+	{ 0, &static_QUType_bool, 0, QUParameter::In }
+    };
+    static const QUMethod slot_9 = {"toggleRepresentation", 1, param_slot_9 };
+    static const QUParameter param_slot_10[] = {
+	{ 0, &static_QUType_bool, 0, QUParameter::In }
+    };
+    static const QUMethod slot_10 = {"toggleBackground", 1, param_slot_10 };
+    static const QUParameter param_slot_11[] = {
+	{ 0, &static_QUType_bool, 0, QUParameter::In }
+    };
+    static const QUMethod slot_11 = {"toggleTransparency", 1, param_slot_11 };
+    static const QUParameter param_slot_12[] = {
+	{ 0, &static_QUType_bool, 0, QUParameter::In }
+    };
+    static const QUMethod slot_12 = {"toggleAntialiasing", 1, param_slot_12 };
+    static const QUParameter param_slot_13[] = {
+	{ 0, &static_QUType_bool, 0, QUParameter::In }
+    };
+    static const QUMethod slot_13 = {"toggleHaloing", 1, param_slot_13 };
+    static const QUParameter param_slot_14[] = {
+	{ 0, &static_QUType_bool, 0, QUParameter::In }
+    };
+    static const QUMethod slot_14 = {"toggleAux", 1, param_slot_14 };
+    static const QUParameter param_slot_15[] = {
+	{ 0, &static_QUType_bool, 0, QUParameter::In }
+    };
+    static const QUMethod slot_15 = {"toggleFullScreen", 1, param_slot_15 };
+    static const QUMethod slot_16 = {"dialogClosed", 0, 0 };
+    static const QMetaData slot_tbl[] = {
+	{ "actionDrawingWireframe()", &slot_0, QMetaData::Private },
+	{ "actionDrawingLineRemoval()", &slot_1, QMetaData::Private },
+	{ "actionDrawingSurfaceRemoval()", &slot_2, QMetaData::Private },
+	{ "actionDrawingLineSurfaceRemoval()", &slot_3, QMetaData::Private },
+	{ "actionControlPanels()", &slot_4, QMetaData::Private },
+	{ "actionExitG4()", &slot_5, QMetaData::Private },
+	{ "actionCreateEPS()", &slot_6, QMetaData::Private },
+	{ "toggleDrawingAction(int)", &slot_7, QMetaData::Private },
+	{ "toggleMouseAction(bool)", &slot_8, QMetaData::Private },
+	{ "toggleRepresentation(bool)", &slot_9, QMetaData::Private },
+	{ "toggleBackground(bool)", &slot_10, QMetaData::Private },
+	{ "toggleTransparency(bool)", &slot_11, QMetaData::Private },
+	{ "toggleAntialiasing(bool)", &slot_12, QMetaData::Private },
+	{ "toggleHaloing(bool)", &slot_13, QMetaData::Private },
+	{ "toggleAux(bool)", &slot_14, QMetaData::Private },
+	{ "toggleFullScreen(bool)", &slot_15, QMetaData::Private },
+	{ "dialogClosed()", &slot_16, QMetaData::Private }
+    };
+    metaObj = QMetaObject::new_metaobject(
+	"G4OpenGLQtViewer", parentObject,
+	slot_tbl, 17,
+	0, 0,
+#ifndef QT_NO_PROPERTIES
+	0, 0,
+	0, 0,
+#endif // QT_NO_PROPERTIES
+	0, 0 );
+    cleanUp_G4OpenGLQtViewer.setMetaObject( metaObj );
+    return metaObj;
+}
+
+void* G4OpenGLQtViewer::qt_cast( const char* clname )
+{
+    if ( !qstrcmp( clname, "G4OpenGLQtViewer" ) )
+	return this;
+    if ( !qstrcmp( clname, "G4OpenGLViewer" ) )
+	return (G4OpenGLViewer*)this;
+    return QObject::qt_cast( clname );
+}
+
+bool G4OpenGLQtViewer::qt_invoke( int _id, QUObject* _o )
+{
+    switch ( _id - staticMetaObject()->slotOffset() ) {
+    case 0: actionDrawingWireframe(); break;
+    case 1: actionDrawingLineRemoval(); break;
+    case 2: actionDrawingSurfaceRemoval(); break;
+    case 3: actionDrawingLineSurfaceRemoval(); break;
+    case 4: actionControlPanels(); break;
+    case 5: actionExitG4(); break;
+    case 6: actionCreateEPS(); break;
+    case 7: toggleDrawingAction((int)static_QUType_int.get(_o+1)); break;
+    case 8: toggleMouseAction((bool)static_QUType_bool.get(_o+1)); break;
+    case 9: toggleRepresentation((bool)static_QUType_bool.get(_o+1)); break;
+    case 10: toggleBackground((bool)static_QUType_bool.get(_o+1)); break;
+    case 11: toggleTransparency((bool)static_QUType_bool.get(_o+1)); break;
+    case 12: toggleAntialiasing((bool)static_QUType_bool.get(_o+1)); break;
+    case 13: toggleHaloing((bool)static_QUType_bool.get(_o+1)); break;
+    case 14: toggleAux((bool)static_QUType_bool.get(_o+1)); break;
+    case 15: toggleFullScreen((bool)static_QUType_bool.get(_o+1)); break;
+    case 16: dialogClosed(); break;
+    default:
+	return QObject::qt_invoke( _id, _o );
+    }
+    return TRUE;
+}
+
+bool G4OpenGLQtViewer::qt_emit( int _id, QUObject* _o )
+{
+    return QObject::qt_emit(_id,_o);
+}
+#ifndef QT_NO_PROPERTIES
+
+bool G4OpenGLQtViewer::qt_property( int id, int f, QVariant* v)
+{
+    return QObject::qt_property( id, f, v);
+}
+
+bool G4OpenGLQtViewer::qt_static_property( QObject* , int , int , QVariant* ){ return FALSE; }
+#endif // QT_NO_PROPERTIES
+
+#endif
