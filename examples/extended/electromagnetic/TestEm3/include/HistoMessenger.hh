a0b5f7ee00dea45e97ff9332bf00430f4238e625
Michel Maire <Michel.Maire@cern.ch>
2007-11-12 17:04:55 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@32885 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/electromagnetic/TestEm3/include/HistoMessenger.hh
+++ examples/extended/electromagnetic/TestEm3/include/HistoMessenger.hh
@@ -23,7 +23,7 @@
 // * acceptance of all terms of the Geant4 Software license.          *
 // ********************************************************************
 //
-// $Id: HistoMessenger.hh,v 1.4 2006-06-29 16:51:22 gunter Exp $
+// $Id: HistoMessenger.hh,v 1.5 2007-11-12 17:04:55 maire Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -63,6 +63,7 @@ class HistoMessenger: public G4UImessenger
    G4UIcmdWithAString*     typeCmd;
    G4UIcmdWithAString*     optionCmd;         
    G4UIcommand*            histoCmd;
+   G4UIcmdWithAnInteger*   prhistoCmd;      
    G4UIcmdWithAnInteger*   rmhistoCmd;    
  
 };
