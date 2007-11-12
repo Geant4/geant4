866697dd5f3675c7da215a398fcda8aa85368ab4
Michel Maire <Michel.Maire@cern.ch>
2007-11-12 17:48:16 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@32887 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/electromagnetic/TestEm5/include/HistoManager.hh
+++ examples/extended/electromagnetic/TestEm5/include/HistoManager.hh
@@ -23,7 +23,7 @@
 // * acceptance of all terms of the Geant4 Software license.          *
 // ********************************************************************
 //
-// $Id: HistoManager.hh,v 1.12 2007-07-31 16:51:29 maire Exp $
+// $Id: HistoManager.hh,v 1.13 2007-11-12 17:48:16 maire Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -63,7 +63,8 @@ class HistoManager
     void SetHisto (G4int,G4int,G4double,G4double,const G4String& unit="none");  
     void FillHisto(G4int id, G4double e, G4double weight = 1.0);
     void RemoveHisto (G4int);
-
+    void PrintHisto  (G4int);
+    
     G4bool    HistoExist  (G4int id) {return exist[id];}
     G4double  GetHistoUnit(G4int id) {return Unit[id];}
     G4double  GetBinWidth (G4int id) {return Width[id];}
@@ -84,8 +85,13 @@ class HistoManager
     G4double                 Vmax [MaxHisto];
     G4double                 Unit [MaxHisto];
     G4double                 Width[MaxHisto];
+    G4bool                   ascii[MaxHisto];
+        
     G4bool                   factoryOn;
     HistoMessenger*          histoMessenger;
+    
+  private:
+    void saveAscii();       
 };
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
--- examples/extended/electromagnetic/TestEm5/include/HistoMessenger.hh
+++ examples/extended/electromagnetic/TestEm5/include/HistoMessenger.hh
@@ -23,7 +23,7 @@
 // * acceptance of all terms of the Geant4 Software license.          *
 // ********************************************************************
 //
-// $Id: HistoMessenger.hh,v 1.7 2006-06-29 16:54:43 gunter Exp $
+// $Id: HistoMessenger.hh,v 1.8 2007-11-12 17:48:16 maire Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -63,6 +63,7 @@ class HistoMessenger: public G4UImessenger
    G4UIcmdWithAString*     typeCmd;
    G4UIcmdWithAString*     optionCmd;      
    G4UIcommand*            histoCmd;
+   G4UIcmdWithAnInteger*   prhistoCmd;      
    G4UIcmdWithAnInteger*   rmhistoCmd;
 
 };
