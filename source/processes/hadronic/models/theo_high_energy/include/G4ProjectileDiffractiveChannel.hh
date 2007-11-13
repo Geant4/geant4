f44a9b38fe19755ac44157f582004ef2017b5217
Gunter Folger <Gunter.Folger@cern.ch>
2007-11-13 16:01:36 +0000

    add projectile diffractive Channel
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@32948 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/processes/hadronic/models/theo_high_energy/include/G4ProjectileDiffractiveChannel.hh
@@ -0,0 +1,70 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+// $Id: G4ProjectileDiffractiveChannel.hh,v 1.1 2007-11-13 16:01:36 gunter Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+
+// Author : Gunter Folger Nov 2007
+// Class Description
+// Final state production model for theoretical models of hadron inelastic
+// projectile diffractive scattering in geant4;
+// Class Description - End
+//
+// Modified:
+
+#ifndef G4ProjectileDiffractiveChannel_h
+#define G4ProjectileDiffractiveChannel_h
+
+#include "G4Nucleus.hh"
+#include "G4HadProjectile.hh"
+#include "G4KineticTrackVector.hh"
+
+#include "G4QDiffractionRatio.hh"
+
+class G4ProjectileDiffractiveChannel
+{
+  public:
+	G4ProjectileDiffractiveChannel();
+	~G4ProjectileDiffractiveChannel();
+	
+	G4double GetFraction(G4Nucleus &theNucleus,
+				const G4DynamicParticle & thePrimary);
+
+	G4KineticTrackVector * Scatter(G4Nucleus &theNucleus,
+					const G4DynamicParticle & thePrimary);
+					
+  private:
+        G4ProjectileDiffractiveChannel(const G4ProjectileDiffractiveChannel &);
+	const G4ProjectileDiffractiveChannel & operator=(const G4ProjectileDiffractiveChannel &);
+	int operator==(const G4ProjectileDiffractiveChannel &) const;
+	int operator!=(const G4ProjectileDiffractiveChannel &) const;
+
+   private:
+   	G4QDiffractionRatio * theQDiffraction;
+};
+
+#endif
--- source/processes/hadronic/models/theo_high_energy/src/G4TheoFSGenerator.cc
+++ source/processes/hadronic/models/theo_high_energy/src/G4TheoFSGenerator.cc
@@ -24,7 +24,7 @@
 // ********************************************************************
 //
 //
-// $Id: G4TheoFSGenerator.cc,v 1.8 2007-10-24 16:32:48 gunter Exp $
+// $Id: G4TheoFSGenerator.cc,v 1.9 2007-11-13 16:01:36 gunter Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // G4TheoFSGenerator
@@ -36,14 +36,14 @@
 
 G4TheoFSGenerator::G4TheoFSGenerator(const G4String& name)
     : G4HadronicInteraction(name)
-    , theParallelFSGenerator(0)
+    , theQuasielastic(0), theProjectileDiffraction(0)
 {
  theParticleChange = new G4HadFinalState;
 }
 
 G4TheoFSGenerator::G4TheoFSGenerator(const G4TheoFSGenerator &) 
     : G4HadronicInteraction("TheoFSGenerator")
-    , theParallelFSGenerator(0)
+    , theQuasielastic(0), theProjectileDiffraction(0)
 {
 }
 
@@ -86,11 +86,42 @@ G4HadFinalState * G4TheoFSGenerator::ApplyYourself(const G4HadProjectile & thePr
                           thePrimary.Get4Momentum().vect());
   const G4DynamicParticle * aPart = &aTemp;
 
-  if ( theParallelFSGenerator ) {
+  if ( theQuasielastic ) {
   
-     if ( theParallelFSGenerator->GetFraction(theNucleus, *aPart) > G4UniformRand() )
+     if ( theQuasielastic->GetFraction(theNucleus, *aPart) > G4UniformRand() )
      {
-        G4KineticTrackVector *result= theParallelFSGenerator->Scatter(theNucleus, *aPart);
+        G4KineticTrackVector *result= theQuasielastic->Scatter(theNucleus, *aPart);
+	if (result)
+	{
+	    for(unsigned int  i=0; i<result->size(); i++)
+	    {
+	      G4DynamicParticle * aNew = 
+		 new G4DynamicParticle(result->operator[](i)->GetDefinition(),
+                        	       result->operator[](i)->Get4Momentum().e(),
+                        	       result->operator[](i)->Get4Momentum().vect());
+	      theParticleChange->AddSecondary(aNew);
+	      delete result->operator[](i);
+	    }
+	    delete result;
+	   
+	} else 
+	{
+	    theParticleChange->SetStatusChange(isAlive);
+	    theParticleChange->SetEnergyChange(thePrimary.GetKineticEnergy());
+	    theParticleChange->SetMomentumChange(thePrimary.Get4Momentum().vect().unit());
+ 
+	}
+	return theParticleChange;
+     } 
+  }
+  if ( theProjectileDiffraction) {
+  
+     if ( theProjectileDiffraction->GetFraction(theNucleus, *aPart) > G4UniformRand() )
+        // strictly, this returns fraction on inelastic, so quasielastic should 
+	//    already be substracted, ie. quasielastic should always be used
+	//     before diffractive
+     {
+        G4KineticTrackVector *result= theProjectileDiffraction->Scatter(theNucleus, *aPart);
 	if (result)
 	{
 	    for(unsigned int  i=0; i<result->size(); i++)
@@ -132,7 +163,7 @@ G4HadFinalState * G4TheoFSGenerator::ApplyYourself(const G4HadProjectile & thePr
                theTransport->Propagate(theInitialResult, theHighEnergyGenerator->GetWoundedNucleus());
     if ( !theTransportResult ) {
        G4cout << "G4TheoFSGenerator: null ptr from transport propagate " << G4endl;
-       throw G4HadronicException(__FILE__, __LINE__, text);
+       throw G4HadronicException(__FILE__, __LINE__, "Null ptr from transport propagate");
     } 
   }
   else
@@ -140,7 +171,7 @@ G4HadFinalState * G4TheoFSGenerator::ApplyYourself(const G4HadProjectile & thePr
     theTransportResult = theDecay.Propagate(theInitialResult, theHighEnergyGenerator->GetWoundedNucleus());
     if ( !theTransportResult ) {
        G4cout << "G4TheoFSGenerator: null ptr from decay propagate " << G4endl;
-       throw G4HadronicException(__FILE__, __LINE__, text);
+       throw G4HadronicException(__FILE__, __LINE__, "Null ptr from decay propagate");
     }   
   }
 
