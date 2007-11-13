7eb7ba4758403eb2b251d72bb2487a03b14fe8b7
Gunter Folger <Gunter.Folger@cern.ch>
2007-11-13 10:23:24 +0000

    include/QGS_BIC.hh
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@32907 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/physics_lists/lists/include/QGS_BIC.hh
@@ -0,0 +1,67 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+// $Id: QGS_BIC.hh,v 1.1 2007-11-13 10:23:24 gunter Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+//---------------------------------------------------------------------------
+//
+// ClassName:   
+//
+// Author: 2007 Gunter Folger
+//     created from QGSP_BIC  by H.P.Wellisch
+//
+// Modified:
+//
+//----------------------------------------------------------------------------
+//
+#ifndef TQGS_BIC_h
+#define TQGS_BIC_h 1
+
+#include "G4VModularPhysicsList.hh"
+#include "globals.hh"
+#include "CompileTimeConstraints.hh"
+
+template<class T>
+class TQGS_BIC: public T
+{
+public:
+  TQGS_BIC(G4int ver = 1);
+  virtual ~TQGS_BIC();
+  
+public:
+  // SetCuts() 
+  virtual void SetCuts();
+
+private:
+  enum {ok = CompileTimeConstraints::IsA<T, G4VModularPhysicsList>::ok };
+};
+#include "QGS_BIC.icc"
+typedef TQGS_BIC<G4VModularPhysicsList> QGS_BIC;
+
+#endif
+
+
+
