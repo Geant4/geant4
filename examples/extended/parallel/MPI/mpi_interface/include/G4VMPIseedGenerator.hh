ac0d15dba2027153fc1d6cfac5142c22745bee0e
Koichi Murakami <koichi.murakami@kek.jp>
2007-11-16 14:05:41 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@33133 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ examples/extended/parallel/MPI/mpi_interface/include/G4VMPIseedGenerator.hh
@@ -0,0 +1,79 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+// $Id: G4VMPIseedGenerator.hh,v 1.1 2007-11-16 14:05:41 kmura Exp $
+// $Name: not supported by cvs2svn $
+//
+// ====================================================================
+//   G4VMPIseedGenerator.hh
+//
+//                                         2007 Q
+// ====================================================================
+#ifndef G4VMPI_SEED_GENERATOR_H
+#define G4VMPI_SEED_GENERATOR_H
+
+#include "globals.hh"
+#include <vector>
+
+// ====================================================================
+//
+// class definition
+//
+// ====================================================================
+
+class G4VMPIseedGenerator {
+protected:
+  G4long masterSeed;
+  std::vector<G4long> seedList;
+
+  // generate seeds for MPI nodes
+  virtual void GenerateSeeds()=0;
+
+public:
+  G4VMPIseedGenerator();
+  virtual ~G4VMPIseedGenerator();
+
+  // set/get methods
+  void SetMasterSeed(G4long aseed); // trigger GenerateSeeds()
+  G4long GetMasterSeed() const;
+
+  const std::vector<G4long>& GetSeedList() const;
+
+};
+
+// ====================================================================
+//   inline functions
+// ====================================================================
+inline G4long G4VMPIseedGenerator::GetMasterSeed() const
+{
+  return masterSeed;
+}
+
+inline const std::vector<G4long>& G4VMPIseedGenerator::GetSeedList() const  
+{
+  return seedList;
+}
+
+#endif
