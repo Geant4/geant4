38eb86286852eb8d9323b510999acc2d5e0d8164
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2007-11-20 09:42:57 +0000

    Added disclaimer...
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@33237 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/gdml/load_gdml.cc
+++ examples/extended/gdml/load_gdml.cc
@@ -1,3 +1,38 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+// $Id: load_gdml.cc,v 1.3 2007-11-20 09:42:57 gcosmo Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+//
+// --------------------------------------------------------------
+//      GEANT 4 - load_gdml
+//
+// --------------------------------------------------------------
+
 #include "G4RunManager.hh"
 #include "G4UImanager.hh"
 #include "G4UIsession.hh"
@@ -16,7 +51,7 @@ int main(int argc, char **argv) {
 
    if (argc<2) {
    
-      std::cout << "Usage: load_gdml <filename>" << std::endl;
+      G4cout << "Usage: load_gdml <filename>" << G4endl;
       return 0;
    }
 
