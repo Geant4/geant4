792d09de689aa05cf28c4deda476a683b0fa1a5e
Tatsumi Koi <tkoi@slac.stanford.edu>
2007-11-24 19:00:10 +0000

    Reduce standard outputted information
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@33300 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/models/qmd/src/G4QMDNucleus.cc
+++ source/processes/hadronic/models/qmd/src/G4QMDNucleus.cc
@@ -82,8 +82,8 @@ G4double G4QMDNucleus::GetNuclearMass()
       G4double Asym = 23*MeV; 
 
       G4double BE = Av * A 
-                  - As * std::pow ( G4double(A) , 2.0/3.0 ) 
-                  - Ac * Z*Z/std::pow ( G4double(A) , 1.0/3.0 )
+                  - As * std::pow ( A , 2.0/3.0 ) 
+                  - Ac * Z*Z/std::pow ( A , 1.0/3.0 )
                   - Asym * ( N - Z )* ( N - Z ) / A; 
 
       mass = Z * G4Proton::Proton()->GetPDGMass() 
@@ -191,7 +191,7 @@ void G4QMDNucleus::CalEnergyAndAngularMomentumInCM()
    //G4cout << "G4BindingEnergy in GeV " << G4NucleiPropertiesTable::GetBindingEnergy( GetAtomicNumber() , GetMassNumber() )/GeV << G4endl;
 
    excitationEnergy = bindingEnergy + G4NucleiPropertiesTable::GetBindingEnergy( GetAtomicNumber() , GetMassNumber() )/GeV;
-   G4cout << "excitationEnergy in GeV " << excitationEnergy << G4endl;
+   //G4cout << "excitationEnergy in GeV " << excitationEnergy << G4endl;
    if ( excitationEnergy < 0 ) excitationEnergy = 0.0; 
 
 }
