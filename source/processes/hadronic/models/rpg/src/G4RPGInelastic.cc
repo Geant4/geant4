d2ac4e1440dcc9b7f334e7f3e8a2d1d73adaa9cb
Dennis Herbert Wright <dennis@cern.ch>
2008-01-04 23:17:04 +0000

    add quantum number conservation check
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@33622 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/models/rpg/src/G4RPGInelastic.cc
+++ source/processes/hadronic/models/rpg/src/G4RPGInelastic.cc
@@ -23,7 +23,7 @@
 // * acceptance of all terms of the Geant4 Software license.          *
 // ********************************************************************
 //
-// $Id: G4RPGInelastic.cc,v 1.2 2007-08-15 20:38:25 dennis Exp $
+// $Id: G4RPGInelastic.cc,v 1.3 2008-01-04 23:17:04 dennis Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 
@@ -469,5 +469,125 @@ G4RPGInelastic::SetUpChange(G4FastVector<G4ReactionProduct,256> &vec,
     delete vec[i];
   }
 }
- 
+
+
+std::pair<G4int, G4double>
+G4RPGInelastic::interpolateEnergy(G4double e) const
+{
+  G4int index = 29;
+  G4double fraction = 0.0;
+
+  for (G4int i = 1; i < 30; i++) {
+    if (e < energyScale[i]) {
+      index = i-1;
+      fraction = (e - energyScale[index]) / (energyScale[i] - energyScale[index]);
+      break;
+    }
+  }
+  return std::pair<G4int, G4double>(index, fraction);
+}
+
+
+G4int
+G4RPGInelastic::sampleFlat(std::vector<G4double> sigma) const
+{
+  G4int i;
+  G4double sum(0.);
+  for (i = 0; i < G4int(sigma.size()); i++) sum += sigma[i];
+
+  G4double fsum = sum*G4UniformRand();
+  G4double partialSum = 0.0;
+  G4int channel = 0;
+
+  for (i = 0; i < G4int(sigma.size()); i++) {
+    partialSum += sigma[i];
+    if (fsum < partialSum) {
+      channel = i;
+      break;
+    }
+  }
+
+  return channel;
+}
+
+
+void G4RPGInelastic::CheckQnums(G4FastVector<G4ReactionProduct,256> &vec,
+                                G4int &vecLen,
+                                G4ReactionProduct &currentParticle,
+                                G4ReactionProduct &targetParticle,
+                                G4double Q, G4double B, G4double S)
+{
+  G4ParticleDefinition* projDef = currentParticle.GetDefinition();
+  G4ParticleDefinition* targDef = targetParticle.GetDefinition();
+  G4double chargeSum = projDef->GetPDGCharge() + targDef->GetPDGCharge();
+  G4double baryonSum = projDef->GetBaryonNumber() + targDef->GetBaryonNumber();
+  G4double strangenessSum = projDef->GetQuarkContent(3) - 
+                            projDef->GetAntiQuarkContent(3) + 
+                            targDef->GetQuarkContent(3) -
+                            targDef->GetAntiQuarkContent(3);
+
+  G4ParticleDefinition* secDef = 0;
+  for (G4int i = 0; i < vecLen; i++) {
+    secDef = vec[i]->GetDefinition();
+    chargeSum += secDef->GetPDGCharge();
+    baryonSum += secDef->GetBaryonNumber();
+    strangenessSum += secDef->GetQuarkContent(3) 
+                    - secDef->GetAntiQuarkContent(3);
+  }
+
+  G4bool OK = true;
+  if (chargeSum != Q) {
+    G4cout << " Charge not conserved " << G4endl;
+    OK = false;
+  }
+  if (baryonSum != B) {
+    G4cout << " Baryon number not conserved " << G4endl;
+    OK = false;
+  }
+  if (strangenessSum != S) {
+    G4cout << " Strangeness not conserved " << G4endl;
+    OK = false;
+  } 
+
+  if (!OK) {
+    G4cout << " projectile: " << projDef->GetParticleName() 
+           << "  target: " << targDef->GetParticleName() << G4endl;
+    for (G4int i = 0; i < vecLen; i++) {
+      secDef = vec[i]->GetDefinition();
+      G4cout << secDef->GetParticleName() << " " ;
+    }
+    G4cout << G4endl;
+  }
+
+}
+
+
+const G4double G4RPGInelastic::energyScale[30] = {
+  0.0,  0.01, 0.013, 0.018, 0.024, 0.032, 0.042, 0.056, 0.075, 0.1,
+  0.13, 0.18, 0.24,  0.32,  0.42,  0.56,  0.75,  1.0,   1.3,   1.8,
+  2.4,  3.2,  4.2,   5.6,   7.5,   10.0,  13.0,  18.0,  24.0, 32.0 };
+
+G4ParticleDefinition* p0 = G4PionZero::PionZero();
+G4ParticleDefinition* p1 = G4PionPlus::PionPlus();
+G4ParticleDefinition* p2 = G4PionMinus::PionMinus();
+G4ParticleDefinition* p3 = G4KaonPlus::KaonPlus();
+G4ParticleDefinition* p4 = G4KaonMinus::KaonMinus();
+G4ParticleDefinition* p5 = G4KaonZero::KaonZero();
+G4ParticleDefinition* p6 = G4AntiKaonZero::AntiKaonZero();
+G4ParticleDefinition* p7 = G4Proton::Proton();
+G4ParticleDefinition* p8 = G4Neutron::Neutron();
+G4ParticleDefinition* p9 = G4Lambda::Lambda();
+G4ParticleDefinition* p10 = G4SigmaPlus::SigmaPlus();
+G4ParticleDefinition* p11 = G4SigmaZero::SigmaZero();
+G4ParticleDefinition* p12 = G4SigmaMinus::SigmaMinus();
+G4ParticleDefinition* p13 = G4XiZero::XiZero();
+G4ParticleDefinition* p14 = G4XiMinus::XiMinus();
+G4ParticleDefinition* p15 = G4OmegaMinus::OmegaMinus();
+G4ParticleDefinition* p16 = G4AntiProton::AntiProton();
+G4ParticleDefinition* p17 = G4AntiNeutron::AntiNeutron();
+
+G4ParticleDefinition* G4RPGInelastic::particleDef[18] = {
+  p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, 
+  p15, p16, p17 };
+
 /* end of file */
