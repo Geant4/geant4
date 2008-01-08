22722dd651394e59b0a57147caa071a4a596aeb6
Michel Maire <Michel.Maire@cern.ch>
2008-01-08 16:34:38 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@33645 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/electromagnetic/TestEm15/src/HistoManager.cc
+++ examples/extended/electromagnetic/TestEm15/src/HistoManager.cc
@@ -23,7 +23,7 @@
 // * acceptance of all terms of the Geant4 Software license.          *
 // ********************************************************************
 //
-// $Id: HistoManager.cc,v 1.5 2007-11-13 14:13:31 maire Exp $
+// $Id: HistoManager.cc,v 1.6 2008-01-08 16:34:38 maire Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -220,7 +220,7 @@ void HistoManager::RemoveHisto(G4int ih)
 
 void HistoManager::PrintHisto(G4int ih)
 {
- if (ih < MaxHisto) ascii[ih] = true;
+ if (ih < MaxHisto) { ascii[ih] = true; ascii[0] = true; }
  else
     G4cout << "---> warning from HistoManager::PrintHisto() : histo " << ih
            << "does not exist" << G4endl;
@@ -233,6 +233,8 @@ void HistoManager::PrintHisto(G4int ih)
 void HistoManager::saveAscii()
 {
 #ifdef G4ANALYSIS_USE
+
+ if (!ascii[0]) return;
  
  G4String name = fileName[0] + ".ascii";
  std::ofstream File(name, std::ios::out);
