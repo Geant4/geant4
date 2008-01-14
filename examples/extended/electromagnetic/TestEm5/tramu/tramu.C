59b96ffded5f845a2df4153d442b0d673f8f701a
Michel Maire <Michel.Maire@cern.ch>
2008-01-14 13:48:52 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@33702 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/electromagnetic/TestEm5/tramu/tramu.C
+++ examples/extended/electromagnetic/TestEm5/tramu/tramu.C
@@ -5,20 +5,26 @@
    TFile f("./tramu.root");
    TH1D* h1d = (TH1D*) f.Get("10");
    h1d->SetTitle("1 TeV muon in 3 m iron : kinetic energy at exit (GeV)");
-   h1d->GetXaxis()->SetTitle("Ekine(GeV)");
-   h1d->GetYaxis()->SetTitle("1/GeV");
+   h1d->GetXaxis()->SetTitle("Ekine (GeV)");
+   h1d->GetYaxis()->SetTitle("nb/GeV");
    h1d->SetStats(kFALSE);  // Eliminate statistics box
-   h1d->Draw();
+   h1d->Draw("HIST");
+   
+/*
+* muon 1 TeV/c in 3 m Iron
+* Particle Data Group. Physics Letters B 592 (2004) page 251
+* distribution of the muon kinetic energy
+* (from 950 GeV to 1000 GeV by bin of 0.5 GeV --> 100 bins) 
+*/
 
-   // data2.ascii came from Mars14
    ifstream in;
    in.open("mars14.ascii");
 
-   // Create a new histogramm which mars14.acsii values
-   int x_min = 950;
-   int x_max = 1000;
-   int nb_bins = 100;
-   TH1F *h1f = new TH1F("h1f","",nb_bins,x_min,x_max);
+   // Create a new histogram with mars14.acsii values
+   int nb_bins = 100;   
+   float x_min = 950.;
+   float x_max = 1000.;
+   TH1F* h1f = new TH1F("h1f","",nb_bins,x_min,x_max);
 
    Float_t x, y;
    while (1) {
@@ -33,9 +39,9 @@
    h1f->Draw("SAME");
 
    // Print the histograms legend
-   TLegend *legend = new TLegend(0.2,0.55,0.36,0.68);
-   legend->AddEntry(h1d,"G4-9.1","l");
-   legend->AddEntry(h1f,"Mars14","L");
+   TLegend* legend = new TLegend(0.2,0.55,0.45,0.70);
+   legend->AddEntry(h1d,"G4-9.1-ref-00 ","l");
+   legend->AddEntry(h1f,"Mars14 simul ","L");
    legend->Draw();
 
 
