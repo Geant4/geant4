a3a582da58a59036d1f83cdd165114682bc8b8d9
Michel Maire <Michel.Maire@cern.ch>
2008-01-17 13:46:29 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@33762 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/electromagnetic/TestEm12/berger/berger.C
+++ examples/extended/electromagnetic/TestEm12/berger/berger.C
@@ -1,20 +1,23 @@
 {
    gROOT->Reset();
 
-   /* data */
-   // 1MeV-DPK.ascii came from berger
+  /* data 
+   * Seltzer, Appl. Radiat. Isot. 42(1991) page 917  
+   * L. Ferrer et al. Cancer Bio. Rad. 22-1 (2007)
+   */   
+
    ifstream in;
    in.open("./data/1MeV-DPK.ascii");
 
 
-   // Create a new histogramm which 1MeV-DPK.acsii values
+   // Create a new histogram
    double x_min = 0;
    double x_max = 1.2;
+   
    int nbdata = 0;
-
    in >> nbdata;
    
-   TH1D *h1d = new TH1D("h1d","",nbdata,x_min,x_max);
+   TH1D* h1d = new TH1D("h1d","",nbdata,x_min,x_max);
 
    Double_t x, y;   
    for ( int i = 0 ; i < nbdata ; i++ ) {
@@ -23,29 +26,25 @@
       h1d->Fill(x,y);
    }
    in.close();
+   
    // Draw histogram
-   h1d->SetTitle(" 1MeV   Dose point kernel : energy deposition profil ");
+   h1d->SetTitle("Dose point kernel : energy deposition profile, e- 1 MeV");
    h1d->GetXaxis()->SetTitle("d(E/E0)/d(r/r0) along r/r0                             r/r0");
    h1d->GetYaxis()->SetTitle("DPK");
-   h1d->SetStats(kFALSE);  // Eliminate statistics box   
-   
+   h1d->SetStats(kFALSE);  // Eliminate statistics box      
    h1d->SetLineColor(2);
    h1d->Draw("L");
 
 
    // Draw histogram fill by Geant4 TestEm12 simulation
-   TFile f("./1MeV.root");
+   TFile f("./1mev.root");
    TH1D* hroot = (TH1D*) f.Get("8");
    hroot->Draw("SAME HIST");
 
-
-
-
    // Print the histograms legend
    TLegend *legend = new TLegend(0.7,0.65,0.9,0.78);
-   legend->AddEntry(hroot,"G4-9.1","l");
-   legend->AddEntry(h1d,"Berger data","l");
+   legend->AddEntry(hroot,"G4-9.1-ref-00 ","l");
+   legend->AddEntry(h1d,"Etran simul","l");
    legend->Draw();
-
 
 }
