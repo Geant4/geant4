5939c15103ca6ad25f044e73f7fb207af3dcd6ed
Maria Grazia Pia <Maria.Grazia.Pia@cern.ch>
2008-03-02 13:13:37 +0000

    Fixed awful indentation changed by RC
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@34193 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/electromagnetic/lowenergy/include/G4CompositeEMDataSet.hh
+++ source/processes/electromagnetic/lowenergy/include/G4CompositeEMDataSet.hh
@@ -24,7 +24,7 @@
 // ********************************************************************
 //
 //
-// $Id: G4CompositeEMDataSet.hh,v 1.8 2006-06-29 19:33:08 gunter Exp $
+// $Id: G4CompositeEMDataSet.hh,v 1.9 2008-03-02 13:12:52 pia Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // Author: Maria Grazia Pia (Maria.Grazia.Pia@cern.ch)
@@ -44,53 +44,57 @@
 // -------------------------------------------------------------------
 
 #ifndef   G4COMPOSITEEMDATASET_HH
- #define  G4COMPOSITEEMDATASET_HH 1
+#define  G4COMPOSITEEMDATASET_HH 1
 
- #include "globals.hh"
- #include "G4VEMDataSet.hh"
- #include <vector>
+#include "globals.hh"
+#include "G4VEMDataSet.hh"
+#include <vector>
 
- class G4VDataSetAlgorithm;
+class G4VDataSetAlgorithm;
 
- class G4CompositeEMDataSet : public G4VEMDataSet 
- {
-  public:
-                                                G4CompositeEMDataSet(G4VDataSetAlgorithm* argAlgorithm, G4double argUnitEnergies=MeV, G4double argUnitData=barn, G4int argMinZ=1, G4int argMaxZ=99); 
-   virtual                                     ~G4CompositeEMDataSet();
+class G4CompositeEMDataSet : public G4VEMDataSet 
+{
+public:
+  G4CompositeEMDataSet(G4VDataSetAlgorithm* argAlgorithm, 
+		       G4double argUnitEnergies=MeV, 
+		       G4double argUnitData=barn, 
+		       G4int argMinZ=1, 
+		       G4int argMaxZ=99); 
+  virtual ~G4CompositeEMDataSet();
  
-   virtual G4double                             FindValue(G4double argEnergy, G4int argComponentId=0) const;
+  virtual G4double FindValue(G4double argEnergy, G4int argComponentId=0) const;
   
-   virtual void                                 PrintData(void) const;
+  virtual voidPrintData(void) const;
 
-   virtual const G4VEMDataSet *                 GetComponent(G4int argComponentId) const { return components[argComponentId]; }
-   virtual void                                 AddComponent(G4VEMDataSet * argDataSet) { components.push_back(argDataSet); }
-   virtual size_t                               NumberOfComponents(void) const { return components.size(); }
+  virtual const G4VEMDataSet* GetComponent(G4int argComponentId) const { return components[argComponentId]; }
+  virtual void AddComponent(G4VEMDataSet* argDataSet) { components.push_back(argDataSet); }
+  virtual size_t NumberOfComponents(void) const { return components.size(); }
 
-   virtual const G4DataVector &                 GetEnergies(G4int argComponentId) const { return GetComponent(argComponentId)->GetEnergies(0); }
-   virtual const G4DataVector &                 GetData(G4int argComponentId) const { return GetComponent(argComponentId)->GetData(0); }
-   virtual void                                 SetEnergiesData(G4DataVector * argEnergies, G4DataVector * argData, G4int argComponentId);
+  virtual const G4DataVector& GetEnergies(G4int argComponentId) const { return GetComponent(argComponentId)->GetEnergies(0); }
+  virtual const G4DataVector& GetData(G4int argComponentId) const { return GetComponent(argComponentId)->GetData(0); }
+  virtual void etEnergiesData(G4DataVector* argEnergies, G4DataVector* argData, G4int argComponentId);
 
-   virtual G4bool                               LoadData(const G4String & argFileName);
-   virtual G4bool                               SaveData(const G4String & argFileName) const;
+  virtual G4bool LoadData(const G4String& argFileName);
+  virtual G4bool SaveData(const G4String& argFileName) const;
    
-  private:
-   void                                         CleanUpComponents(void);
+private:
+  void CleanUpComponents(void);
   
-   // Hide copy constructor and assignment operator 
-                                                G4CompositeEMDataSet();
-                                                G4CompositeEMDataSet(const G4CompositeEMDataSet & copy);
-   G4CompositeEMDataSet &                       operator=(const G4CompositeEMDataSet & right);
+  // Hide copy constructor and assignment operator 
+  G4CompositeEMDataSet();
+  G4CompositeEMDataSet(const G4CompositeEMDataSet& copy);
+  G4CompositeEMDataSet& operator=(const G4CompositeEMDataSet& right);
 
-   std::vector<G4VEMDataSet *>                  components;          // Owned pointers
+  std::vector<G4VEMDataSet*> components;    // Owned pointers
 
-   G4VDataSetAlgorithm *                        algorithm;           // Owned pointer 
+  G4VDataSetAlgorithm* algorithm;           // Owned pointer 
   
-   G4double                                     unitEnergies;
-   G4double                                     unitData;
+  G4double unitEnergies;
+  G4double unitData;
 
-   G4int                                        minZ;
-   G4int                                        maxZ;
- };
+  G4int minZ;
+  G4int maxZ;
+};
 #endif /* G4COMPOSITEEMDATASET_HH */
 
 
--- source/processes/electromagnetic/lowenergy/src/G4CompositeEMDataSet.cc
+++ source/processes/electromagnetic/lowenergy/src/G4CompositeEMDataSet.cc
@@ -24,7 +24,7 @@
 // ********************************************************************
 //
 //
-// $Id: G4CompositeEMDataSet.cc,v 1.9 2006-06-29 19:38:46 gunter Exp $
+// $Id: G4CompositeEMDataSet.cc,v 1.10 2008-03-02 13:13:37 pia Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // Author: Maria Grazia Pia (Maria.Grazia.Pia@cern.ch)
@@ -41,147 +41,128 @@
 #include <fstream>
 #include <sstream>
 
-                                                G4CompositeEMDataSet :: G4CompositeEMDataSet(G4VDataSetAlgorithm* argAlgorithm, G4double argUnitEnergies, G4double argUnitData, G4int argMinZ, G4int argMaxZ)
-:
- algorithm(argAlgorithm),
- unitEnergies(argUnitEnergies),
- unitData(argUnitData),
- minZ(argMinZ),
- maxZ(argMaxZ)
+G4CompositeEMDataSet::G4CompositeEMDataSet(G4VDataSetAlgorithm* argAlgorithm, 
+					   G4double argUnitEnergies, 
+					   G4double argUnitData, 
+					   G4int argMinZ, 
+					   G4int argMaxZ)
+  :
+  algorithm(argAlgorithm),
+  unitEnergies(argUnitEnergies),
+  unitData(argUnitData),
+  minZ(argMinZ),
+  maxZ(argMaxZ)
 {
- if (algorithm == 0) 
-  G4Exception("G4CompositeEMDataSet::G4CompositeEMDataSet - interpolation == 0");
+  if (algorithm == 0) 
+    G4Exception("G4CompositeEMDataSet::G4CompositeEMDataSet - interpolation == 0");
 }
 
 
 
-                                                G4CompositeEMDataSet :: ~G4CompositeEMDataSet()
+G4CompositeEMDataSet::~G4CompositeEMDataSet()
 {
- CleanUpComponents();
-
- if (algorithm)
-  delete algorithm;
-}
-
-
-
-
-
-
-G4double                                        G4CompositeEMDataSet :: FindValue(G4double argEnergy, G4int argComponentId) const
-{
- const G4VEMDataSet * component(GetComponent(argComponentId));
- 
- if (component)
-  return component->FindValue(argEnergy);
-
- std::ostringstream message;
- message << "G4CompositeEMDataSet::FindValue - component " << argComponentId << " not found";
- 
- G4Exception(message.str().c_str());
- 
- return 0.;
+  CleanUpComponents();
+  if (algorithm) delete algorithm;
 }
 
 
 
 
 
-void                                            G4CompositeEMDataSet :: PrintData(void) const
-{
- const size_t n(NumberOfComponents());
 
- G4cout << "The data set has " << n << " components" << G4endl;
- G4cout << G4endl;
+G4double G4CompositeEMDataSet::FindValue(G4double argEnergy, G4int argComponentId) const
+{
+  const G4VEMDataSet* component(GetComponent(argComponentId));
  
- size_t i(0);
+  if (component) return component->FindValue(argEnergy);
+
+  std::ostringstream message;
+  message << "G4CompositeEMDataSet::FindValue - component " << argComponentId << " not found";
  
- while (i<n)
- {
-  G4cout << "--- Component " << i << " ---" << G4endl;
-  GetComponent(i)->PrintData();
-  i++;
- }
+  G4Exception(message.str().c_str());
+ 
+  return 0.;
+}
+
+void G4CompositeEMDataSet::PrintData(void) const
+{
+  const size_t n(NumberOfComponents());
+
+  G4cout << "The data set has " << n << " components" << G4endl;
+  G4cout << G4endl;
+ 
+  size_t i(0);
+ 
+  while (i<n)
+    {
+      G4cout << "--- Component " << i << " ---" << G4endl;
+      GetComponent(i)->PrintData();
+      i++;
+    }
+}
+
+void G4CompositeEMDataSet::SetEnergiesData(G4DataVector* argEnergies, G4DataVector* argData, G4int argComponentId)
+{
+  G4VEMDataSet * component(components[argComponentId]);
+ 
+  if (component)
+    {
+      component->SetEnergiesData(argEnergies, argData, 0);
+      return;
+    }
+
+  std::ostringstream message;
+  message << "G4CompositeEMDataSet::SetEnergiesData - component " << argComponentId << " not found";
+ 
+  G4Exception(message.str().c_str());
+}
+
+G4boolG4CompositeEMDataSet::LoadData(const G4String& argFileName)
+{
+  CleanUpComponents(); 
+
+  for (G4int z(minZ); z<maxZ; z++)
+    {
+      G4VEMDataSet* component = new G4EMDataSet(z, algorithm->Clone(), unitEnergies, unitData);
+      if (!component->LoadData(argFileName))
+	{
+	  delete component;
+	  return false;
+	}
+      AddComponent(component);
+    }
+  return true;
 }
 
 
 
-
-
-void                                            G4CompositeEMDataSet :: SetEnergiesData(G4DataVector * argEnergies, G4DataVector * argData, G4int argComponentId)
+G4bool G4CompositeEMDataSet::SaveData(const G4String& argFileName) const
 {
- G4VEMDataSet * component(components[argComponentId]);
- 
- if (component)
- {
-  component->SetEnergiesData(argEnergies, argData, 0);
-  return;
- }
-
- std::ostringstream message;
- message << "G4CompositeEMDataSet::SetEnergiesData - component " << argComponentId << " not found";
- 
- G4Exception(message.str().c_str());
-}
-
-
-
-
-
-G4bool                                          G4CompositeEMDataSet :: LoadData(const G4String & argFileName)
-{
- CleanUpComponents(); 
-
- for (G4int z(minZ); z<maxZ; z++)
- {
-  G4VEMDataSet * component=new G4EMDataSet(z, algorithm->Clone(), unitEnergies, unitData);
-  if (!component->LoadData(argFileName))
-  {
-   delete component;
-   return false;
-  }
+  for (G4int z(minZ); z<maxZ; z++)
+    {
+      const G4VEMDataSet* component(GetComponent(z-minZ));
   
-  AddComponent(component);
- }
+      if (!component)
+	{
+	  std::ostringstream message;
+	  message << "G4CompositeEMDataSet::SaveData - component " << (z-minZ) << " not found";
+	  G4Exception(message.str().c_str());
+	}
+
+      if (!component->SaveData(argFileName))
+	return false;
+    }
  
- return true;
+  return true;
 }
 
-
-
-G4bool                                          G4CompositeEMDataSet :: SaveData(const G4String & argFileName) const
+void G4CompositeEMDataSet::CleanUpComponents(void)
 {
- for (G4int z(minZ); z<maxZ; z++)
- {
-  const G4VEMDataSet * component(GetComponent(z-minZ));
-  
-  if (!component)
-  {
-   std::ostringstream message;
-   message << "G4CompositeEMDataSet::SaveData - component " << (z-minZ) << " not found";
- 
-   G4Exception(message.str().c_str());
-  }
-
-  if (!component->SaveData(argFileName))
-   return false;
- }
- 
- return true;
-}
-
-
-
-
-
-void                                            G4CompositeEMDataSet :: CleanUpComponents(void)
-{
- while (!components.empty())
- {
-  if (components.back())
-   delete components.back();
-
-  components.pop_back();
- }
+  while (!components.empty())
+    {
+      if (components.back())
+	delete components.back();
+      components.pop_back();
+    }
 }
 
