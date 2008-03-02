ce5ce88479fbe682852f48997a79262cad7dd97a
Maria Grazia Pia <Maria.Grazia.Pia@cern.ch>
2008-03-02 11:34:00 +0000

    Fixed awful indentation changed by RC
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@34191 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/electromagnetic/lowenergy/include/G4EMDataSet.hh
+++ source/processes/electromagnetic/lowenergy/include/G4EMDataSet.hh
@@ -24,7 +24,7 @@
 // ********************************************************************
 //
 //
-// $Id: G4EMDataSet.hh,v 1.7 2006-06-29 19:35:31 gunter Exp $
+// $Id: G4EMDataSet.hh,v 1.8 2008-03-02 11:34:00 pia Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // Author: Maria Grazia Pia (Maria.Grazia.Pia@cern.ch)
@@ -44,53 +44,53 @@
 // -------------------------------------------------------------------
 
 #ifndef   G4EMDATASET_HH
- #define  G4EMDATASET_HH 1
+#define  G4EMDATASET_HH 1
 
- #include "globals.hh"
- #include "G4VEMDataSet.hh"
+#include "globals.hh"
+#include "G4VEMDataSet.hh"
 
- class G4VDataSetAlgorithm;
+class G4VDataSetAlgorithm;
 
- class G4EMDataSet : public G4VEMDataSet
- {
-  public:
-                                                G4EMDataSet(G4int argZ, G4VDataSetAlgorithm * argAlgorithm, G4double argUnitEnergies=MeV, G4double argUnitData=barn);
-                                                G4EMDataSet(G4int argZ, G4DataVector * argEnergies, G4DataVector * argData, G4VDataSetAlgorithm * argAlgorithm, G4double argUnitEnergues=MeV, G4double argUnitData=barn);
-   virtual                                     ~G4EMDataSet();
+class G4EMDataSet : public G4VEMDataSet
+{
+public:
+  G4EMDataSet(G4int argZ, G4VDataSetAlgorithm* argAlgorithm, G4double argUnitEnergies=MeV, G4double argUnitData=barn);
+  G4EMDataSet(G4int argZ, G4DataVector* argEnergies, G4DataVector* argData, G4VDataSetAlgorithm* argAlgorithm, G4double argUnitEnergies=MeV, G4double argUnitData=barn);
+  virtual ~G4EMDataSet();
  
-   virtual G4double                             FindValue(G4double argEnergy, G4int argComponentId=0) const;
+  virtual G4double FindValue(G4double argEnergy, G4int argComponentId=0) const;
   
-   virtual void                                 PrintData(void) const;
+  virtual void PrintData(void) const;
 
-   virtual const G4VEMDataSet *                 GetComponent(G4int /* argComponentId */) const { return 0; }
-   virtual void                                 AddComponent(G4VEMDataSet * /* argDataSet */) {}
-   virtual size_t                               NumberOfComponents(void) const { return 0; }
+  virtual const G4VEMDataSet* GetComponent(G4int /* argComponentId */) const { return 0; }
+  virtual void AddComponent(G4VEMDataSet* /* argDataSet */) {}
+  virtual size_t NumberOfComponents(void) const { return 0; }
 
-   virtual const G4DataVector &                 GetEnergies(G4int /* argComponentId */) const { return *energies; }
-   virtual const G4DataVector &                 GetData(G4int /* argComponentId */) const { return *data; }
-   virtual void                                 SetEnergiesData(G4DataVector * argEnergies, G4DataVector * argData, G4int argComponentId);
+  virtual const G4DataVector& GetEnergies(G4int /* argComponentId */) const { return *energies; }
+  virtual const G4DataVector& GetData(G4int /* argComponentId */) const { return *data; }
+  virtual void SetEnergiesData(G4DataVector* argEnergies, G4DataVector* argData, G4int argComponentId);
 
-   virtual G4bool                               LoadData(const G4String & argFileName);
-   virtual G4bool                               SaveData(const G4String & argFileName) const;
+  virtual G4bool LoadData(const G4String& argFileName);
+  virtual G4bool SaveData(const G4String& argFileName) const;
    
-  private:
-   size_t                                       FindLowerBound(G4double argEnergy) const;
+private:
+  size_t FindLowerBound(G4double argEnergy) const;
    
-   G4String                                     FullFileName(const G4String & argFileName) const;
+  G4String FullFileName(const G4String& argFileName) const;
 
-   // Hide copy constructor and assignment operator 
-                                                G4EMDataSet();
-                                                G4EMDataSet(const G4EMDataSet & copy);
-   G4EMDataSet &                                operator=(const G4EMDataSet & right);
+  // Hide copy constructor and assignment operator 
+  G4EMDataSet();
+  G4EMDataSet(const G4EMDataSet& copy);
+  G4EMDataSet& operator=(const G4EMDataSet& right);
 
-   G4int                                        z;
+  G4int z;
 
-   G4DataVector *                               energies;            // Owned pointer
-   G4DataVector *                               data;                // Owned pointer
+  G4DataVector* energies;            // Owned pointer
+  G4DataVector* data;                // Owned pointer
 
-   G4VDataSetAlgorithm *                        algorithm;           // Owned pointer 
+  G4VDataSetAlgorithm* algorithm;    // Owned pointer 
   
-   G4double                                     unitEnergies;
-   G4double                                     unitData;
- };
+  G4double unitEnergies;
+  G4double unitData;
+};
 #endif /* G4EMDATASET_HH */
