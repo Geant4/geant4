9d77f1fc576c0f1c24ac958869b2db82ccb1da38
Michel Maire <Michel.Maire@cern.ch>
2008-04-07 17:19:49 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@34617 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/electromagnetic/TestEm15/src/HistoManager.cc
+++ examples/extended/electromagnetic/TestEm15/src/HistoManager.cc
@@ -23,7 +23,7 @@
 // * acceptance of all terms of the Geant4 Software license.          *
 // ********************************************************************
 //
-// $Id: HistoManager.cc,v 1.6 2008-01-08 16:34:38 maire Exp $
+// $Id: HistoManager.cc,v 1.7 2008-04-07 17:19:49 maire Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -248,7 +248,8 @@ void HistoManager::saveAscii()
      
       for (G4int iBin=0; iBin<Nbins[ih]; iBin++) {
          File << "  " << iBin << "\t" 
-              << histo[ih]->binMean(iBin) << "\t"
+              << 0.5*(histo[ih]->axis().binLowerEdge(iBin) +
+	              histo[ih]->axis().binUpperEdge(iBin)) << "\t"	      
 	      << histo[ih]->binHeight(iBin) 
 	      << G4endl;
       } 
