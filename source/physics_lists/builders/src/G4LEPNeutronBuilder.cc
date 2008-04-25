b6f3a691b481b52eef9511506d83e2da876cb079
Gunter Folger <Gunter.Folger@cern.ch>
2008-04-25 15:11:46 +0000

    protect delete against uninitialised ptr
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@34877 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/physics_lists/builders/src/G4LEPNeutronBuilder.cc
+++ source/physics_lists/builders/src/G4LEPNeutronBuilder.cc
@@ -42,21 +42,20 @@
 #include "G4ParticleTable.hh"
 #include "G4ProcessManager.hh"
 
-G4LEPNeutronBuilder::
-G4LEPNeutronBuilder() 
-{
+G4LEPNeutronBuilder::G4LEPNeutronBuilder() : theLENeutronModel(0),
+					     theNeutronFissionModel(0),
+					     theNeutronCaptureModel(0) {
   theMin = 0;
   theIMin = theMin;
   theMax = 20*TeV;
   theIMax = 55*GeV;
 }
 
-G4LEPNeutronBuilder::
-~G4LEPNeutronBuilder() 
+G4LEPNeutronBuilder::~G4LEPNeutronBuilder() 
 {
-  delete theNeutronFissionModel;
-  delete theNeutronCaptureModel;
-  delete theLENeutronModel;
+  if (theNeutronFissionModel) delete theNeutronFissionModel;
+  if (theNeutronCaptureModel) delete theNeutronCaptureModel;
+  if (theLENeutronModel)      delete theLENeutronModel;
 }
 
 void G4LEPNeutronBuilder::
