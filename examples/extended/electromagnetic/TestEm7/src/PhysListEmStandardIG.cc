64b009185ea89c17160e2cdeb9e8418ec4ef9099
Vladimir Ivantchenko <Vladimir.Ivantchenko@cern.ch>
2008-05-07 08:07:45 +0000

    SRN macro updated
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@35057 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/electromagnetic/TestEm7/src/PhysListEmStandardIG.cc
+++ examples/extended/electromagnetic/TestEm7/src/PhysListEmStandardIG.cc
@@ -23,7 +23,7 @@
 // * acceptance of all terms of the Geant4 Software license.          *
 // ********************************************************************
 //
-// $Id: PhysListEmStandardIG.cc,v 1.8 2008-04-21 13:13:30 vnivanch Exp $
+// $Id: PhysListEmStandardIG.cc,v 1.9 2008-05-07 08:07:45 vnivanch Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
