fe2057037a0e67bda24f1e6247f86def4a3ff790
Vladimir Ivantchenko <Vladimir.Ivantchenko@cern.ch>
2008-06-02 16:50:14 +0000

    extended printout
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@35328 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/materials/src/G4Element.cc
+++ source/materials/src/G4Element.cc
@@ -24,7 +24,7 @@
 // ********************************************************************
 //
 //
-// $Id: G4Element.cc,v 1.26 2007-10-18 11:14:33 vnivanch Exp $
+// $Id: G4Element.cc,v 1.27 2008-06-02 16:50:14 vnivanch Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -68,16 +68,25 @@ G4Element::G4Element(const G4String& name, const G4String& symbol,
                      G4double zeff, G4double aeff)
   : fName(name), fSymbol(symbol)		     
 {
-  if (zeff<1.) G4Exception (" ERROR from G4Element::G4Element !"
-			    " It is not allowed to create an Element with Z < 1" );
-
-  if (aeff/(g/mole)<zeff) G4Exception (" ERROR from G4Element::G4Element !"
-				       " Attempt to create an Element with N < Z !!!" );
-
-  if ((zeff-G4int(zeff)) > perMillion)
+  if (zeff<1.) {
+    G4cout << "G4Element ERROR:  " << name << " Z= " << zeff 
+	   << " A= " << aeff/(g/mole) << G4endl; 
+    G4Exception (" ERROR from G4Element::G4Element !"
+		 " It is not allowed to create an Element with Z < 1" );
+  }
+  if (aeff/(g/mole)<zeff) {
+    G4cout << "G4Element ERROR:  " << name << " Z= " << zeff 
+	   << " A= " << aeff/(g/mole) << G4endl; 
+    G4Exception (" ERROR from G4Element::G4Element !"
+		 " Attempt to create an Element with N < Z !!!" );
+  }
+  if ((zeff-G4int(zeff)) > perMillion) {
+    G4cout << "G4Element Warning:  " << name << " Z= " << zeff 
+	   << " A= " << aeff/(g/mole) << G4endl; 
     G4cerr << name << " : WARNING from G4Element::G4Element !"  
       " Trying to define an element as a mixture directly via effective Z."
 	   << G4endl;
+  }
 
   InitializePointers();
 
@@ -116,9 +125,11 @@ G4Element::G4Element(const G4String& name,
 
 void G4Element::AddIsotope(G4Isotope* isotope, G4double abundance)
 {
-  if (theIsotopeVector == 0)
+  if (theIsotopeVector == 0) {
+    G4cout << "G4Element ERROR:  " << fName << G4endl; 
     G4Exception ("ERROR from G4Element::AddIsotope!"
 		 " Trying to add an Isotope before contructing the element.");
+  }
 
   // filling ...
   if ( fNumberOfIsotopes < theIsotopeVector->size() ) {
@@ -132,9 +143,11 @@ void G4Element::AddIsotope(G4Isotope* isotope, G4double abundance)
     (*theIsotopeVector)[fNumberOfIsotopes] = isotope;
     ++fNumberOfIsotopes;
     isotope->increaseCountUse();
-  } 
-  else G4Exception ("ERROR from G4Element::AddIsotope!"  
-		    " Attempt to add more than the declared number of isotopes.");
+  } else {
+    G4cout << "G4Element ERROR:  " << fName << G4endl; 
+    G4Exception ("ERROR from G4Element::AddIsotope!"  
+		 " Attempt to add more than the declared number of isotopes.");
+  }
 
   // filled.
   if ( fNumberOfIsotopes == theIsotopeVector->size() ) {
