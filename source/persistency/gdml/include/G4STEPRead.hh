b120d1481d137891266574c7a1e11dc465041c54
Zoltan Torzsok <ztorzsok@cern.ch>
2008-06-24 08:43:00 +0000

    Geom file parsing added.
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@35676 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/persistency/gdml/include/G4STEPRead.hh
+++ source/persistency/gdml/include/G4STEPRead.hh
@@ -1,8 +1,19 @@
 #ifndef _G4STEPREAD_INCLUDED_
 #define _G4STEPREAD_INCLUDED_
 
+#include "G4TessellatedSolid.hh"
+#include "G4QuadrangularFacet.hh"
+#include "G4TriangularFacet.hh"
+#include "G4LogicalVolume.hh"
+#include "G4Material.hh"
+
+#include <fstream>
+
 class G4STEPRead {
+   G4TessellatedSolid* ReadTessellatedSolid(const std::string&);
+   G4VFacet* ReadFacet(const std::string&);
 public:
+   void ReadGeom(const G4String&);
 };
 
 #endif
--- source/persistency/gdml/src/G4STEPRead.cc
+++ source/persistency/gdml/src/G4STEPRead.cc
@@ -1,1 +1,61 @@
 #include "G4STEPRead.hh"
+
+G4TessellatedSolid* G4STEPRead::ReadTessellatedSolid(const std::string& line) {
+
+   std::istringstream stream(line.substr(2));
+   
+   G4String name;
+   stream >> name;
+   
+   return new G4TessellatedSolid(name);
+}
+
+G4VFacet* G4STEPRead::ReadFacet(const std::string& line) {
+
+   if (line[2]=='3') { // Triangular facet
+
+      G4double x1,y1,z1;
+      G4double x2,y2,z2;
+      G4double x3,y3,z3;
+      
+      std::istringstream stream(line.substr(4));
+      stream >> x1 >> y1 >> z1 >> x2 >> y2 >> z2 >> x3 >> y3 >> z3;
+      return new G4TriangularFacet(G4ThreeVector(x1,y1,z1),G4ThreeVector(x2,y2,z2),G4ThreeVector(x3,y3,z3),ABSOLUTE);
+   }
+
+   if (line[2]=='4') { // Quadrangular facet
+
+      G4double x1,y1,z1;
+      G4double x2,y2,z2;
+      G4double x3,y3,z3;
+      G4double x4,y4,z4;
+
+      std::istringstream stream(line.substr(4));
+      stream >> x1 >> y1 >> z1 >> x2 >> y2 >> z2 >> x3 >> y3 >> z3 >> x4 >> y4 >> z4;
+      return new G4QuadrangularFacet(G4ThreeVector(x1,y1,z1),G4ThreeVector(x2,y2,z2),G4ThreeVector(x3,y3,z3),G4ThreeVector(x4,y4,z4),ABSOLUTE);
+   }
+
+   G4Exception("G4STEPRead: ERROR! Number of vertices per facet should be either 3 or 4!");
+   return 0;
+}
+
+void G4STEPRead::ReadGeom(const G4String& name) {
+
+   G4cout << "G4STEPRead: Reading '" << name << "'..." << G4endl;
+
+   std::ifstream GeomFile(name);
+   
+   if (!GeomFile) G4Exception("G4STEPRead: ERROR! Can not open file: "+name);
+
+   std::string line;
+   
+   G4TessellatedSolid* tessellated = 0;
+   
+   while (getline(GeomFile,line)) {
+   
+      if (line[0] == 'f') { tessellated = ReadTessellatedSolid(line); } else
+      if (line[0] == 'p') { tessellated->AddFacet(ReadFacet(line)); }
+   }
+
+   G4cout << "G4STEPRead: Reading '" << name << "' done." << G4endl;
+}
