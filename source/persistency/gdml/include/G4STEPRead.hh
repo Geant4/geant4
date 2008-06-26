6ef3b1856c560856c3d15d691e77f5c05f5cea2d
Zoltan Torzsok <ztorzsok@cern.ch>
2008-06-26 12:18:01 +0000

    ST Reader completed.
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@35702 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/persistency/gdml/include/G4STEPRead.hh
+++ source/persistency/gdml/include/G4STEPRead.hh
@@ -5,21 +5,32 @@
 #include "G4QuadrangularFacet.hh"
 #include "G4TriangularFacet.hh"
 #include "G4LogicalVolume.hh"
+#include "G4PVPlacement.hh"
 #include "G4Material.hh"
+#include "G4Box.hh"
+#include "G4Orb.hh"
 
 #include <fstream>
+#include <vector>
+#include <map>
 
 class G4STEPRead {
-   std::vector<G4LogicalVolume*> volumeList;
    std::vector<G4TessellatedSolid*> tessellatedList;
+   std::map<G4TessellatedSolid*,G4LogicalVolume*> volumeMap;
 
+   G4Box* world_box;
    G4Material* solid_material;
    G4Material* medium_material;
+   G4LogicalVolume* world_volume;
 
    void tessellatedRead(const std::string&);
    void facetRead(const std::string&);
-public:
+   void physvolRead(const std::string&);
    void ReadGeom(const G4String&);
+   void ReadTree(const G4String&);
+public:
+   void Read(const G4String&);
+   G4VPhysicalVolume* GetWorldVolume();
 };
 
 #endif
--- source/persistency/gdml/src/G4STEPRead.cc
+++ source/persistency/gdml/src/G4STEPRead.cc
@@ -2,18 +2,18 @@
 
 void G4STEPRead::tessellatedRead(const std::string& line) {
 
+   if (tessellatedList.size()>0) tessellatedList.back()->SetSolidClosed(true); // Finish the previous solid at first!
+
    std::istringstream stream(line.substr(2));
    
    G4String name;
    stream >> name;
    
    G4TessellatedSolid* tessellated = new G4TessellatedSolid(name);
-   G4LogicalVolume* volume = new G4LogicalVolume(tessellated,solid_material,name,0,0,0);
+   volumeMap[tessellated] = new G4LogicalVolume(tessellated,solid_material,"volume"+name,0,0,0);
+   tessellatedList.push_back(tessellated);
 
    G4cout << "G4STEPRead: Reading solid: " << name << G4endl;
-
-   tessellatedList.push_back(tessellated);
-   volumeList.push_back(volume);
 }
 
 void G4STEPRead::facetRead(const std::string& line) {
@@ -44,9 +44,39 @@ void G4STEPRead::facetRead(const std::string& line) {
    G4Exception("G4STEPRead: ERROR! Number of vertices per facet should be either 3 or 4!");
 }
 
-void G4STEPRead::ReadGeom(const G4String& name) {
+void G4STEPRead::physvolRead(const std::string& line) {
 
-   solid_material = new G4Material("Aluminium",13,26.98*g/mole,2.7*g/cm3,kStateSolid);
+   G4int level;
+   G4String name;
+   G4double r1,r2,r3;
+   G4double r4,r5,r6;
+   G4double r7,r8,r9;
+   G4double pX,pY,pZ;
+   G4double n1,n2,n3,n4,n5;
+
+   std::istringstream stream(line.substr(2));
+   stream >> level >> name >> r1 >> r2 >> r3 >> n1 >> r4 >> r5 >> r6 >> n2 >> r7 >> r8 >> r9 >> n3 >> pX >> pY >> pZ >> n4 >> n5;
+
+   name.resize(name.rfind("_"));
+
+   G4TessellatedSolid* tessellated = 0;
+
+   for (size_t i=0;i<tessellatedList.size();i++) { // Find the volume for this physvol!
+   
+      if (tessellatedList[i]->GetName() == G4String(name)) {
+      
+         tessellated = tessellatedList[i];
+	 break;
+      }
+   }
+
+   if (tessellated == 0) G4Exception("G4STEPRead: ERROR! Referenced solid '"+name+"' not found!");
+
+   G4RotationMatrix* rot = new G4RotationMatrix(G4ThreeVector(r1,r2,r3),G4ThreeVector(r4,r5,r6),G4ThreeVector(r7,r8,r9));
+   G4PVPlacement(rot,G4ThreeVector(pX,pY,pZ),volumeMap[tessellated],"physvol"+name,world_volume,0,0);
+}
+
+void G4STEPRead::ReadGeom(const G4String& name) {
 
    G4cout << "G4STEPRead: Reading '" << name << "'..." << G4endl;
 
@@ -55,7 +85,7 @@ void G4STEPRead::ReadGeom(const G4String& name) {
    if (!GeomFile) G4Exception("G4STEPRead: ERROR! Can not open file: "+name);
 
    tessellatedList.clear();
-   volumeList.clear();
+   volumeMap.clear();
    std::string line;
    
    while (getline(GeomFile,line)) {
@@ -64,5 +94,51 @@ void G4STEPRead::ReadGeom(const G4String& name) {
       if (line[0] == 'p') facetRead(line);
    }
 
+   if (tessellatedList.size()>0) tessellatedList.back()->SetSolidClosed(true); // Finish the last solid!
+
    G4cout << "G4STEPRead: Reading '" << name << "' done." << G4endl;
+}
+
+void G4STEPRead::ReadTree(const G4String& name) {
+
+   G4cout << "G4STEPRead: Reading '" << name << "'..." << G4endl;
+
+   world_box = new G4Box("WorldBox",100,50,100);
+   medium_material = new G4Material("Aluminium",13,26.98*g/mole,2.7*g/cm3,kStateSolid);
+   solid_material = new G4Material("Aluminium",13,26.98*g/mole,2.7*g/cm3,kStateSolid);
+   world_volume = new G4LogicalVolume(world_box,medium_material,"WorldLogicalVolume",0,0,0);
+
+   std::ifstream TreeFile(name);
+
+   if (!TreeFile) G4Exception("G4STEPRead: ERROR! Can not open file: "+name);
+
+
+   std::string line;
+   
+   while (getline(TreeFile,line)) {
+   
+      if (line[0] == 'g') physvolRead(line);
+   }
+
+   G4cout << "G4STEPRead: Reading '" << name << "' done." << G4endl;
+}
+
+void G4STEPRead::Read(const G4String& name) {
+
+   ReadGeom(name+".geom");
+   ReadTree(name+".tree");
+}
+
+G4VPhysicalVolume* G4STEPRead::GetWorldVolume() {
+
+   world_box = new G4Box("WorldBox",100,50,100);
+   medium_material = new G4Material("Aluminium",13,26.98*g/mole,2.7*g/cm3,kStateSolid);
+   world_volume = new G4LogicalVolume(world_box,medium_material,"WorldLogicalVolume",0,0,0);
+
+   G4Box* box = new G4Box("TheBox",10,30,60);
+   solid_material = new G4Material("Aluminium",13,26.98*g/mole,2.7*g/cm3,kStateSolid);
+   G4LogicalVolume* vol= new G4LogicalVolume(box,solid_material,"volume",0,0,0);
+   G4PVPlacement(0,G4ThreeVector(0,0,0),vol,"physvol",world_volume,0,0);
+
+   return new G4PVPlacement(0,G4ThreeVector(0,0,0),world_volume,"WorldPhysicalVolume",0,0,0);
 }
