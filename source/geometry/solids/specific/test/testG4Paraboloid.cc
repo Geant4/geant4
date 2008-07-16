808814ee6edac84511c412af4a798378cde856ac
Tatiana Nikitina <tnikitin@cern.ch>
2008-07-16 16:28:25 +0000

    Add checks in DistanceToOut(p,v) . This fix the problem report 1015
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@35875 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/geometry/solids/specific/test/testG4Paraboloid.cc
+++ source/geometry/solids/specific/test/testG4Paraboloid.cc
@@ -1,33 +1,9 @@
-//
-// ********************************************************************
-// * License and Disclaimer                                           *
-// *                                                                  *
-// * The  Geant4 software  is  copyright of the Copyright Holders  of *
-// * the Geant4 Collaboration.  It is provided  under  the terms  and *
-// * conditions of the Geant4 Software License,  included in the file *
-// * LICENSE and available at  http://cern.ch/geant4/license .  These *
-// * include a list of copyright holders.                             *
-// *                                                                  *
-// * Neither the authors of this software system, nor their employing *
-// * institutes,nor the agencies providing financial support for this *
-// * work  make  any representation or  warranty, express or implied, *
-// * regarding  this  software system or assume any liability for its *
-// * use.  Please see the license in the file  LICENSE  and URL above *
-// * for the full disclaimer and the limitation of liability.         *
-// *                                                                  *
-// * This  code  implementation is the result of  the  scientific and *
-// * technical work of the GEANT4 collaboration.                      *
-// * By using,  copying,  modifying or  distributing the software (or *
-// * any work based  on the software)  you  agree  to acknowledge its *
-// * use  in  resulting  scientific  publications,  and indicate your *
-// * acceptance of all terms of the Geant4 Software license.          *
-// ********************************************************************
-//
 #include <assert.h>
 
 #include "G4GeometryTolerance.hh"
 #include "G4Paraboloid.hh"
 #include "G4Polyhedron.hh"
+#include "Randomize.hh"
 int main()
 {
   G4ThreeVector testPoint;
@@ -36,7 +12,7 @@ int main()
   G4Paraboloid paraboloid2("Paraboloid2", .01, 2., 3.);
 
   G4double kCarTolerance = G4GeometryTolerance::GetInstance()->GetSurfaceTolerance();
-
+  
   // Test Inside function.
   assert(paraboloid1.Inside(G4ThreeVector(2.598076211, 1.5, 1.)) == kSurface);
   assert(paraboloid1.Inside(G4ThreeVector((2 * kCarTolerance + 3.) * std::sqrt(3.) / 2., (2. * kCarTolerance + 3.) / 2., 1.)) == kOutside);
@@ -54,7 +30,7 @@ int main()
   assert(paraboloid1.Inside(G4ThreeVector(0., 0.,   1. - 2 * kCarTolerance)) == kInside);
   assert(paraboloid1.Inside(G4ThreeVector((- 2 * kCarTolerance) / std::sqrt(2.), ( 2. * kCarTolerance ) / std::sqrt(2.), -1.)) == kOutside);
 
-  assert(paraboloid2.Inside(G4ThreeVector(2.598076211, 1.5, .01)) == kSurface);
+   assert(paraboloid2.Inside(G4ThreeVector(2.598076211, 1.5, .01)) == kSurface);
   assert(paraboloid2.Inside(G4ThreeVector((2 * kCarTolerance + 3.) * std::sqrt(3.) / 2., (2. * kCarTolerance + 3.) / 2., .01)) == kOutside);
   assert(paraboloid2.Inside(G4ThreeVector((- 2 * kCarTolerance + 3.) * std::sqrt(3.) / 2., (- 2. * kCarTolerance + 3.) / 2., .01)) == kSurface);
 
@@ -71,7 +47,7 @@ int main()
   assert(paraboloid2.Inside(G4ThreeVector((- 2. - 2 * kCarTolerance) / std::sqrt(2.), (2. + 2. * kCarTolerance ) / std::sqrt(2.), -.01)) == kOutside);
   assert(paraboloid2.Inside(G4ThreeVector((- 2. + 2 * kCarTolerance) / std::sqrt(2.), (2. - 2. * kCarTolerance ) / std::sqrt(2.), -.01)) == kSurface);
   assert(paraboloid2.Inside(G4ThreeVector((- 2.) / std::sqrt(2.), (2.) / std::sqrt(2.), -.01)) == kSurface);
-
+  
   // Test DistanceToIn(p, v) function.
   assert(std::fabs(paraboloid1.DistanceToIn( G4ThreeVector(3.,2.,10.), 
            G4ThreeVector(-0.099014754299999993558678568206233, -0.099014754299999993558678568206233, -0.99014754279999994679428709787317)) 
@@ -113,7 +89,7 @@ int main()
   assert(std::fabs(paraboloid2.GetCubicVolume() - 0.408407) < 1e-6);
 
   // Test area function.
-  //G4cout<<paraboloid1.GetPolyhedron()->GetSurfaceArea()<<G4endl;
+  // G4cout<<paraboloid1.GetPolyhedron()->GetSurfaceArea()<<G4endl;
   assert(std::fabs(paraboloid1.GetSurfaceArea() - 66.758844) < 1e-6);
   assert(std::fabs(paraboloid2.GetSurfaceArea() - 56.551935) < 1e-6);
 
@@ -127,6 +103,66 @@ int main()
   assert((paraboloid2.SurfaceNormal(G4ThreeVector(1.732050808, 2.449489743, 0.01)) - G4ThreeVector(0.40824829,0.57735027,0.70710678)).r() < 1e-5);
   assert((paraboloid2.SurfaceNormal(G4ThreeVector(1.658312395, 2., 0.001)) - G4ThreeVector(0.013263635,0.015996545,-0.99978407)).r() < 1e-5);
   assert((paraboloid2.SurfaceNormal(G4ThreeVector(0., 0., -.01)) - G4ThreeVector(0., 0., -1.)).r() < kCarTolerance);
+  
+
+  // Add Test Distance To Out for User Problem with Substraction Of Paraboloid :: Problem Report N1015
+   G4Paraboloid paraboloid3("Paraboloid1", 72., 0., 240.);
+  //  G4Paraboloid paraboloid3("Paraboloid1", 72., 210., 240.);//This test is Ok, Intersection with DZ working correctly
+   G4double distOut1;
+   distOut1=paraboloid3.DistanceToOut(G4ThreeVector(200., 0., 72.), G4ThreeVector(0.0,0.,-1.), false, NULL, NULL);
+   assert((distOut1-44.0)<1e-5);
+   //G4cout<<"Test3::distOut="<<distOut1<<G4endl;
+
+   distOut1=paraboloid3.DistanceToOut(G4ThreeVector(200., 0., 72.), G4ThreeVector(0.000000000001,0.,-1.), false, NULL, NULL);
+    assert((distOut1-44.0)<1e-5);
+   //G4cout<<"Test3::distOut="<<distOut1<<G4endl;
+   // Add Rotation On the Surface around "Problem" Point(200.,0.,72.) 
+
+    G4double tolA=0.25e-7; 
+    G4ThreeVector In (200.,0.,72.);
+    EInside in; 
+    G4ThreeVector Dir,vDir,pSurf;
+    Dir=G4ThreeVector(10*tolA,0.,-1.);
+    vDir=Dir.unit(); 
+    in=paraboloid3.Inside(In);
+    if(in!=kSurface)G4cout<<"Problem ::Point p"<<In<<" is NOT On Surface"<<G4endl;
+    
+    for(G4int i=0; i<100; i++){
+     
+      G4double distOut=paraboloid3.DistanceToOut(In,vDir);
+      G4cout.precision(16);
+      pSurf=In+vDir*distOut;
+      in=paraboloid3.Inside(pSurf);
+      if(in!=kSurface) G4cout<<"Problem :: ps is Not on the Surface DistToOut="<<distOut<<" dir="<<vDir<<G4endl;
+      Dir=Dir-G4ThreeVector(tolA,0,0);
+      vDir=Dir.unit();
+
+   }
+    // Rotation from the Lower DZ plane
+     Dir=G4ThreeVector(10*tolA,0.,1.);
+     vDir=Dir.unit(); 
+     In= G4ThreeVector (0.,0.,-72.);
+     for(G4int i=0; i<100; i++){
+     
+      G4double distOut=paraboloid3.DistanceToOut(In,vDir);
+      G4cout.precision(16);
+      pSurf=In+vDir*distOut;
+      in=paraboloid3.Inside(pSurf);
+      if(in!=kSurface) G4cout<<"Problem :: ps is Not on the Surface DistToOut="<<distOut<<" dir="<<vDir<<G4endl;
+      Dir=Dir-G4ThreeVector(tolA,0,0);
+      vDir=Dir.unit();
+     }
+  // Paraboloid with R1!=0 and with Point situated On both Surfaces : Z and Parabolic
+   G4Paraboloid paraboloid5("Paraboloid1", 10., 0., 100.);
+   distOut1=paraboloid5.DistanceToOut(G4ThreeVector(100., 0., 10.), G4ThreeVector(0.0,0.,-1.), false, NULL, NULL);
+   assert((distOut1-0.0)<1e-5);
+  
+    Dir=G4ThreeVector(-0.9,0.,-0.1);
+    vDir=Dir.unit();
+    distOut1=paraboloid5.DistanceToOut(G4ThreeVector(100., 0., 10.), vDir, false, NULL, NULL);
+    pSurf=G4ThreeVector(100.0,0.,10.)+vDir*distOut1;
+    in=paraboloid5.Inside(pSurf);
+    if(in!=kSurface)G4cout<<"Problem :: ps is Not on the Surface "<<pSurf<<" vDir="<<vDir<<G4endl;
 
   return 0;
 }
