95e3c4ad6de921d466f87f7f580e9ac08b784587
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2008-08-27 10:30:18 +0000

    First release.
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@36248 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ examples/extended/persistency/gdml/G02/src/RunAction.cc
@@ -0,0 +1,73 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+// $Id: RunAction.cc,v 1.1 2008-08-27 10:30:18 gcosmo Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+// Class RunAction implementation
+//
+// ----------------------------------------------------------------------------
+
+#include "G4ios.hh"
+#include <iomanip>
+
+#include "globals.hh"
+#include "Randomize.hh"
+#include "RunAction.hh"
+
+#include "G4Run.hh"
+#include "G4UImanager.hh"
+#include "G4VVisManager.hh"
+#include "G4VisAttributes.hh"
+
+// ----------------------------------------------------------------------------
+
+RunAction::RunAction()
+{ 
+}
+
+// ----------------------------------------------------------------------------
+
+RunAction::~RunAction()
+{
+}
+
+// ----------------------------------------------------------------------------
+
+void RunAction::BeginOfRunAction(const G4Run* aRun)
+{  
+  G4cout << "### Run " << aRun->GetRunID() << " start." << G4endl;
+}
+
+// ----------------------------------------------------------------------------
+
+void RunAction::EndOfRunAction(const G4Run*)
+{
+  if (G4VVisManager::GetConcreteInstance())
+  {
+    G4UImanager::GetUIpointer()->ApplyCommand("/vis/viewer/update");
+  } 
+}
