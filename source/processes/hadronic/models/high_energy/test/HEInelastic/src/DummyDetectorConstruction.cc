e8da110de5f5b0fc8fae29c660721bd1d4a5dfe3
Dennis Herbert Wright <dennis@cern.ch>
2008-08-29 17:39:03 +0000

    add required dummy files
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@36306 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/processes/hadronic/models/high_energy/test/HEInelastic/src/DummyDetectorConstruction.cc
@@ -0,0 +1,58 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+// $Id: DummyDetectorConstruction.cc,v 1.1 2008-08-29 17:38:06 dennis Exp $
+//
+
+#include "DummyDetectorConstruction.hh"
+
+#include "G4VSolid.hh"
+#include "G4Box.hh"
+#include "G4Material.hh"
+#include "G4LogicalVolume.hh"
+#include "G4VPhysicalVolume.hh"
+#include "G4PVPlacement.hh"
+
+
+DummyDetectorConstruction::DummyDetectorConstruction()
+{}
+
+DummyDetectorConstruction::~DummyDetectorConstruction()
+{}
+
+G4VPhysicalVolume* DummyDetectorConstruction::Construct()
+{  
+  G4Material* h2gas =
+         new G4Material("hydrogen", 1.0, 1.01*g/mole, 71.0*mg/cm3);
+
+  // experimental hall (world volume)
+  G4VSolid* worldSolid = new G4Box("worldBox",10.*m,3.*m,10.*m);
+  G4LogicalVolume* worldLogical
+    = new G4LogicalVolume(worldSolid,h2gas,"worldLogical",0,0,0);
+  G4VPhysicalVolume* worldPhysical
+    = new G4PVPlacement(0,G4ThreeVector(),worldLogical,"worldPhysical",0,0,0);
+
+  return worldPhysical;
+}
