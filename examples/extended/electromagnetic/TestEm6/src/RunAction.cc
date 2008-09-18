1f91a37b19dfbbaae4fca0139e0281aa24aa6ed7
Michel Maire <Michel.Maire@cern.ch>
2008-09-18 16:42:07 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@36542 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/electromagnetic/TestEm6/src/RunAction.cc
+++ examples/extended/electromagnetic/TestEm6/src/RunAction.cc
@@ -23,7 +23,7 @@
 // * acceptance of all terms of the Geant4 Software license.          *
 // ********************************************************************
 //
-// $Id: RunAction.cc,v 1.12 2006-06-29 16:57:20 gunter Exp $
+// $Id: RunAction.cc,v 1.13 2008-09-18 16:42:07 maire Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 // 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -58,9 +58,9 @@ RunAction::RunAction()
    // Creating a tree mapped to an hbook file.
    G4bool readOnly  = false;
    G4bool createNew = true;
-   G4String options = "--noErrors uncompress";
-   tree = tf->create("testem6.hbook","hbook",readOnly,createNew,options);
-   //tree = tf->create("testem6.root", "root",readOnly,createNew,options);
+   G4String options = "--noErrors export=root uncompress";
+   //tree = tf->create("testem6.hbook","hbook",readOnly,createNew,options);
+   tree = tf->create("testem6.root", "root",readOnly,createNew,options);
    //tree = tf->create("testem6.XML" ,"XML"  ,readOnly,createNew,options);
    delete tf;
    
