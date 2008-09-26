fd97cf6d6bcfab7203f586457aaec957b3c9f2a1
Michel Maire <Michel.Maire@cern.ch>
2008-09-26 20:15:04 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@36629 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/electromagnetic/TestEm17/src/HistoManager.cc
+++ examples/extended/electromagnetic/TestEm17/src/HistoManager.cc
@@ -23,7 +23,7 @@
 // * acceptance of all terms of the Geant4 Software license.          *
 // ********************************************************************
 //
-// $Id: HistoManager.cc,v 1.3 2008-03-18 15:30:33 vnivanch Exp $
+// $Id: HistoManager.cc,v 1.4 2008-09-26 20:15:04 maire Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -53,8 +53,8 @@ HistoManager::HistoManager()
 #endif 
  
   fileName[0] = "testem17";
-  fileType    = "hbook";
-  fileOption  = "--noErrors uncompress";    
+  fileType    = "root";
+  fileOption  = "--noErrors export=root uncompress";    
   // histograms
   for (G4int k=0; k<MaxHisto; k++) {
     histo[k] = 0;
