51819ccafbd5041dbaad8cffcf3a4b4694ef5833
Michel Maire <Michel.Maire@cern.ch>
2008-10-30 19:00:35 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@36953 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/electromagnetic/TestEm11/src/PhysListEmStandard.cc
+++ examples/extended/electromagnetic/TestEm11/src/PhysListEmStandard.cc
@@ -24,7 +24,7 @@
 // ********************************************************************
 //
 //
-// $Id: PhysListEmStandard.cc,v 1.6 2008-10-24 16:12:26 maire Exp $
+// $Id: PhysListEmStandard.cc,v 1.7 2008-10-30 19:00:35 maire Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -104,7 +104,7 @@ void PhysListEmStandard::ConstructProcess()
     } else if (particleName == "mu+" || 
                particleName == "mu-"    ) {
       //muon  
-      pmanager->AddProcess(new G4eMultipleScattering, -1, 1, 1);
+      pmanager->AddProcess(new G4hMultipleScattering, -1, 1, 1);
       pmanager->AddProcess(new G4MuIonisation,        -1, 2, 2);
       pmanager->AddProcess(new G4MuBremsstrahlung,    -1, 3, 3);
       pmanager->AddProcess(new G4MuPairProduction,    -1, 4, 4);
@@ -113,7 +113,7 @@ void PhysListEmStandard::ConstructProcess()
                particleName == "pi-" ||
                particleName == "pi+"    ) {
       //proton  
-      pmanager->AddProcess(new G4eMultipleScattering, -1, 1, 1);
+      pmanager->AddProcess(new G4hMultipleScattering, -1, 1, 1);
       pmanager->AddProcess(new G4hIonisation,         -1, 2, 2);
       pmanager->AddProcess(new G4hBremsstrahlung,     -1, 3, 3);
       pmanager->AddProcess(new G4hPairProduction,     -1, 4, 4);       
@@ -145,21 +145,21 @@ void PhysListEmStandard::ConstructProcess()
   //
   emOptions.SetMinEnergy(100*eV);	//default    
   emOptions.SetMaxEnergy(100*TeV);	//default  
-  emOptions.SetDEDXBinning(60);		//default  
-  emOptions.SetLambdaBinning(60);	//default
+  emOptions.SetDEDXBinning(120);
+  emOptions.SetLambdaBinning(120);
   emOptions.SetSplineFlag(true);	//default
       
   //multiple coulomb scattering
   //
   emOptions.SetMscStepLimitation(fUseDistanceToBoundary);
-  emOptions.SetMscRangeFactor(0.02);	//default
+  emOptions.SetMscRangeFactor(0.04);	//default
   emOptions.SetMscGeomFactor (2.5);	//default       
   emOptions.SetSkin(3.);		//default
       
   //energy loss
   //
   emOptions.SetStepFunction(0.2, 100*um);   
-  emOptions.SetLinearLossLimit(1.e-3);
+  emOptions.SetLinearLossLimit(1.e-2);	//default
    
   //ionization
   //
