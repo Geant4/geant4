7f9c14ea856f06674b76758b00df573ba4c8dc7c
Vladimir Ivantchenko <Vladimir.Ivantchenko@cern.ch>
2008-11-14 13:33:28 +0000

    add cout of LPM flag
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@37160 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/electromagnetic/standard/src/G4eBremsstrahlung.cc
+++ source/processes/electromagnetic/standard/src/G4eBremsstrahlung.cc
@@ -23,7 +23,7 @@
 // * acceptance of all terms of the Geant4 Software license.          *
 // ********************************************************************
 //
-// $Id: G4eBremsstrahlung.cc,v 1.53 2008-11-14 11:17:39 vnivanch Exp $
+// $Id: G4eBremsstrahlung.cc,v 1.54 2008-11-14 13:33:28 vnivanch Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // -------------------------------------------------------------------
@@ -130,6 +130,7 @@ void G4eBremsstrahlung::InitialiseEnergyLossProcess(
   EmModel(2)->SetSecondaryThreshold(eth);
 
   // Only high energy model LMP flag is ON/OFF
+  EmModel(1)->SetLPMFlag(false);
   EmModel(2)->SetLPMFlag(man->LPMFlag());
 }
 
@@ -139,7 +140,8 @@ void G4eBremsstrahlung::PrintInfo()
 {
   G4LossTableManager* man = G4LossTableManager::Instance(); 
   G4double eth = man->BremsstrahlungTh(); 
-  G4cout << "      LPM flag: " << man->LPMFlag();
+  G4cout << "      LPM flag: " << man->LPMFlag() << " for E > " 
+	 << EmModel(1)->HighEnergyLimit()/GeV << " GeV";
   if(eth < DBL_MAX) G4cout << ",  HighEnergyThreshold(GeV)= " << eth/GeV; 
   G4cout << G4endl;
 }
