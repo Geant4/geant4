37f01cb58308a52a8bc608e5d8026a4cd4173ac5
Michel Maire <Michel.Maire@cern.ch>
2008-11-16 12:30:43 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@37209 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/medical/fanoCavity2/src/PhysicsList.cc
+++ examples/extended/medical/fanoCavity2/src/PhysicsList.cc
@@ -24,7 +24,7 @@
 // ********************************************************************
 //
 //
-// $Id: PhysicsList.cc,v 1.5 2008-11-02 10:50:19 maire Exp $
+// $Id: PhysicsList.cc,v 1.6 2008-11-16 12:30:43 maire Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 
@@ -173,8 +173,8 @@ void PhysicsList::ConstructEM()
   //
   emOptions.SetMinEnergy(100*eV);	//default    
   emOptions.SetMaxEnergy(10*GeV);
-  emOptions.SetDEDXBinning(80);		//default=56  
-  emOptions.SetLambdaBinning(80);	//default=56  
+  emOptions.SetDEDXBinning(8*20);	//default=8*7  
+  emOptions.SetLambdaBinning(8*20);	//default=8*7  
   emOptions.SetSplineFlag(true);	//default
       
   //multiple coulomb scattering
@@ -187,14 +187,14 @@ void PhysicsList::ConstructEM()
   //energy loss
   //
   emOptions.SetStepFunction(0.2, 10*um);	//default=(0.2, 1*mm)   
-  emOptions.SetLinearLossLimit(1.e-3);		//default=1.e-2
+  emOptions.SetLinearLossLimit(1.e-2);		//default
    
           
   //build CSDA range
   //
   emOptions.SetBuildCSDARange(true);		//default=false
   emOptions.SetMaxEnergyForCSDARange(10*GeV);  
-  emOptions.SetDEDXBinningForCSDARange(80);      
+  emOptions.SetDEDXBinningForCSDARange(8*20);	//default=8*7        
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
