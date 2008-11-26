575243939b0809d08be7e5555eb450f10bcdf3b1
Victor Diez Gonzalez <vdiez@cern.ch>
2008-11-26 16:28:13 +0000

    fixed for testing in windows
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@37496 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- tests/tools/qmtest/Geant4Test.py
+++ tests/tools/qmtest/Geant4Test.py
@@ -1,17 +1,18 @@
 import os
 import sys
+import urllib
 from qm.test.classes.command import ShellCommandTest
 
 class Geant4Test (ShellCommandTest):
 
   def Run(self, context, result):
     installdir = os.environ['G4INSTALL']
-    workdir = os.environ['G4WORKDIR']
+    bindir = os.environ['G4BIN']
     system = os.environ['G4SYSTEM']
     
     if sys.platform == 'win32':
         self.command = self.command.replace('$G4INSTALL',installdir)
-        self.command = self.command.replace('$G4BIN',installdir)
+        self.command = self.command.replace('$G4BIN',bindir)
         self.command = self.command.replace('$G4SYSTEM',system)
         self.command = self.command.replace(';',' && ')
         if self.command.count('make') > 0:
@@ -21,9 +22,16 @@ class Geant4Test (ShellCommandTest):
             # We are starting a run test
             self.command = self.command.replace('/','\\')
             
-    #TEMPORAL: EVERYTHING TO BE TESTED ON CYGWIN
-    #TODO: JUST BUILD TESTS ON CYGWIN; RUN ON WINDOWS CMD
-    self.command = self.command.replace('\\','/')
+        #TEMPORAL: EVERYTHING TO BE TESTED ON CYGWIN
+        #TODO: JUST BUILD TESTS ON CYGWIN; RUN ON WINDOWS CMD
+        self.command = self.command.replace('/','\\')
     
+    #file = urllib.urlopen('http://geant4.cern.ch/cgi-bin/bonsai/nightly/gettaglist.cgi').read()
+
+    #print "TAGS SELECTED FOR TESTING:"
+    #numbers=file.split('\n')
+    #for line in numbers: print line
+    #print "---------------------------------------------------------------\n"
+
     print self.command
     ShellCommandTest.Run(self, context, result)
