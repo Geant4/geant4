20994bcac0e77cf6957be02f29e8731d6027f3bf
Vladimir Ivantchenko <Vladimir.Ivantchenko@cern.ch>
2008-12-01 16:50:23 +0000

    added Coulomb barrier
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@37547 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/cross_sections/include/G4UPiNuclearCrossSection.hh
+++ source/processes/hadronic/cross_sections/include/G4UPiNuclearCrossSection.hh
@@ -104,6 +104,7 @@ private:
   G4double           aPower;
   G4double           elow;
   G4double           elowest;
+  G4double           APower[92];
 
   const G4ParticleDefinition* piPlus;
   const G4ParticleDefinition* piMinus;
