a21dc1ff25969b60c455b6c729f6bb70bce7d03f
Makoto Asai <asai@slac.stanford.edu>
2008-12-28 20:32:00 +0000

    Fix the volume for parameterized volume
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@37901 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/digits_hits/scorer/src/G4PSPassageCellFlux.cc
+++ source/digits_hits/scorer/src/G4PSPassageCellFlux.cc
@@ -24,7 +24,7 @@
 // ********************************************************************
 //
 //
-// $Id: G4PSPassageCellFlux.cc,v 1.1 2007-07-11 01:31:03 asaim Exp $
+// $Id: G4PSPassageCellFlux.cc,v 1.2 2008-12-28 20:32:00 asaim Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // G4PSPassageCellFlux
@@ -32,6 +32,8 @@
 #include "G4StepStatus.hh"
 #include "G4Track.hh"
 #include "G4VSolid.hh"
+#include "G4VPhysicalVolume.hh"
+#include "G4VPVParameterisation.hh"
 #include "G4UnitsTable.hh"
 ////////////////////////////////////////////////////////////////////////////////
 // (Description)
@@ -58,9 +60,22 @@ G4bool G4PSPassageCellFlux::ProcessHits(G4Step* aStep,G4TouchableHistory*)
 {
 
   if ( IsPassed(aStep) ) {
-    fCellFlux /= 
-      aStep->GetPreStepPoint()->GetPhysicalVolume()
-      ->GetLogicalVolume()->GetSolid()->GetCubicVolume();
+    G4VPhysicalVolume* physVol = aStep->GetPreStepPoint()->GetPhysicalVolume();
+    G4VPVParameterisation* physParam = physVol->GetParameterisation();
+    G4VSolid* solid = 0;
+    if(physParam)
+    { // for parameterized volume
+      G4int idx = ((G4TouchableHistory*)(aStep->GetPreStepPoint()->GetTouchable()))
+                  ->GetReplicaNumber(indexDepth);
+      solid = physParam->ComputeSolid(idx, physVol);
+      solid->ComputeDimensions(physParam,idx,physVol);
+    }
+    else
+    { // for ordinary volume
+      solid = physVol->GetLogicalVolume()->GetSolid();
+    }
+
+    fCellFlux /= solid->GetCubicVolume();
     G4int index = GetIndex(aStep);
     EvtMap->add(index,fCellFlux);
   }
