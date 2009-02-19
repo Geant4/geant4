3045f1fdaaa7c99234d9bbd575d74c66fe901657
Anton Ivantchenko <anton.ivantchenko@mail.ru>
2009-02-19 11:08:58 +0000

    improvements in testStoppingPower
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@38164 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/materials/test/testStoppingPower.cc
+++ source/materials/test/testStoppingPower.cc
@@ -27,11 +27,14 @@
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 //
-// History: Test for calc. Stopping Power of ions
+// History: Test for calculation Stopping Power of ions
+//          G4Stopping classes are in $G4INSTALL/source/materials/
 // 
 //
-// 11.08.08, A. Ivantchenko
-// TESTTARGET should be gived, please look GNUmakefile
+// 11.08.08, Ivantchenko
+//
+//
+// $TESTTARGET should be gived, please look GNUmakefile
 
 
 #include "G4MaterialStoppingICRU73.hh"
@@ -39,42 +42,45 @@
 #include "G4IronStoppingICRU73.hh"
 
 int main(){  
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 G4double E[31] = {.025,.03,.04,.05,.06,.07,.08,.09,.1,.15,.2,.25,.3,.4,.5,.6,.7,.8,.9,1,1.5,2,2.5,3,4,5,6,7,8,9,10};
-
 G4int Z_Ion[16] = {3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18};
-
 G4double A_Ion[16] = {6.941,9.0122,10.811,12.011,14.007,15.999,18.998,20.180,22.990,24.305,26.982,28.086,30.974,32.065,35.453,39.948};
-
+G4int AA_Ion[16] = {7,9,11,12,14,16,19,20,23,24,27,28,31,32,35,40};
 G4String NameMaterial[31]={"G4_A-150_TISSUE","G4_ADIPOSE_TISSUE_ICRP","G4_AIR","G4_ALUMINUM_OXIDE","G4_BONE_COMPACT_ICRU","G4_BONE_CORTICAL_ICRP","G4_C-552","G4_CALCIUM_FLUORIDE","G4_CARBON_DIOXIDE","G4_Pyrex_Glass","G4_KAPTON","G4_LITHIUM_FLUORIDE","G4_LITHIUM_TETRABORATE","G4_METHANE","G4_MUSCLE_STRIATED_ICRU","G4_NYLON-6/6","G4_PHOTO_EMULSION","G4_PLASTIC_SC_VINYLTOLUENE","G4_POLYCARBONATE","G4_POLYETHYLENE","G4_MYLAR","G4_LUCITE","G4_POLYSTYRENE","G4_TEFLON","G4_PROPANE","G4_SILICON_DIOXIDE","G4_SODIUM_IODIDE","G4_TISSUE-METHANE","G4_TISSUE-PROPANE","G4_WATER","G4_WATER_VAPOR"};
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//G4double G4MaterialStoppingICRU73::GetDEDX(G4int ionZ, G4int idxMaterial,G4double kinEnergy)
+//G4double G4SimpleMaterialStoppingICRU73::GetDEDX(G4int ionZ, G4int idxMaterial, G4double kinEnergy)
+//G4double G4IronStoppingICRU73::GetDEDX(G4int idxMaterial, G4double kinEnergy)
 
+  G4int idxmat = 11;
+  G4int idxion = 5;
+  G4double Energy = .5;
   G4MaterialStoppingICRU73 mS;
-  G4double dedx = mS.GetDEDX(3,0,.025*MeV*6.941);
-  G4String matN = mS.GetMaterialName(0);
-  G4double dens = mS.GetDensity(0);
-  G4cout << "DEDX for Z=3 in " << matN << " is "<< dedx*mm/MeV << " MeV/mm,    " 
-  << dedx*gram/(1000*dens*MeV*cm2) << " MeV/g/cm2 " << G4endl;
+  G4double dedx = mS.GetDEDX(Z_Ion[idxion],idxmat,Energy*MeV*AA_Ion[idxion]);
+  G4String matN = mS.GetMaterialName(idxmat);
+  G4double dens = mS.GetDensity(idxmat);
+  G4cout << "Z=" << Z_Ion[idxion] << " in " << matN << " at E = " << Energy << "MeV, DEDX is "<< dedx*mm/MeV << " MeV/mm,  or " 
+  << dedx*gram/(1000*dens*MeV*cm2) << " MeV/g/cm2, Density= " << dens*cm3/gram << " g/cm3 " << G4endl;
 
+  idxmat = 20;
+  idxion = 5;
+  Energy = 1.;
   G4SimpleMaterialStoppingICRU73 mS1;
-  G4double dedx1 = mS1.GetDEDX(4,1,.025*MeV*6.941);
-  G4String matN1 = mS1.GetMaterialName(1);
-  G4double dens1 = mS1.GetDensity(1);
-  G4cout << "DEDX for Z=3 in " << matN1 << " is "<< dedx1*mm/MeV << " MeV/mm,    " 
-	 << dedx1*gram/(1000*dens1*MeV*cm2) << " MeV/g/cm2 " << G4endl;
-   
+  G4double dedx1 = mS1.GetDEDX(Z_Ion[idxion],idxmat,Energy*MeV*AA_Ion[idxion]);
+  G4String matN1 = mS1.GetMaterialName(idxmat);
+  G4double dens1 = mS1.GetDensity(idxmat);
+  G4cout << "Z=" << Z_Ion[idxion] << " in " << matN1 << "               at E = " << Energy << "MeV, DEDX is "
+	 << dedx1*mm/MeV << " MeV/mm, or " 
+	 << dedx1*gram/(1000*dens1*MeV*cm2) << " MeV/g/cm2, Density= " << dens1*cm3/gram << " g/cm3 " << G4endl;
      
+  idxmat = 10;
+  Energy = .5;
   G4IronStoppingICRU73 mS2;
-  G4double dedx2 = mS2.GetDEDX(4,.025*MeV*55.847);
-  G4String matN2 = mS2.GetMaterialName(4);
-  G4double dens2 = mS2.GetDensity(4);
-   G4cout << "DEDX for Z=26 in " << matN2 << " is "<< dedx2*mm/MeV << " MeV/mm, " 
-	  << dedx2*gram/(1000*dens2*MeV*cm2) << " MeV/g/cm2 " << G4endl;
-   //  G4cout << "DEDX for " << matN2 << " is "<< dedx2*mm/MeV << " MeV/mm   Density= "<< dens2*cm3/gram << " g/cm3 " << G4endl;
-   
+  G4double dedx2 = mS2.GetDEDX(idxmat,Energy*MeV*56);
+  G4String matN2 = mS2.GetMaterialName(idxmat);
+  G4double dens2 = mS2.GetDensity(idxmat);
+  G4cout << "Z=26 in " << matN2 << "               at E = " << Energy << "MeV, DEDX =" << dedx2*mm/MeV << " MeV/mm,  or " 
+	  << dedx2*gram/(1000*dens2*MeV*cm2) << " MeV/g/cm2,  Density= " << dens2*cm3/gram << " g/cm3 " << G4endl;
    G4cout << "### End ###" << G4endl; 
    
    return 0;
