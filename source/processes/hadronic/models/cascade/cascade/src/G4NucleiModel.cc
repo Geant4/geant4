595086bd772609a725506d9ffbcfb46b402befb0
Dennis Herbert Wright <dennis@cern.ch>
2009-02-22 01:59:54 +0000

    get high_energy flag from G4ElementaryParticleCollider
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@38204 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/models/cascade/cascade/src/G4NucleiModel.cc
+++ source/processes/hadronic/models/cascade/cascade/src/G4NucleiModel.cc
@@ -53,7 +53,7 @@
 typedef std::vector<G4InuclElementaryParticle>::iterator particleIterator;
 
 G4NucleiModel::G4NucleiModel()
-  : verboseLevel(2) {
+  : verboseLevel(2), high_energy_interaction(false) {
 
   if (verboseLevel > 3) {
     G4cout << " >>> G4NucleiModel::G4NucleiModel" << G4endl;
@@ -759,8 +759,9 @@ partners G4NucleiModel::generateInteractionPartners(G4CascadParticle& cparticle)
   return thePartners;
 }
 
-std::vector<G4CascadParticle> G4NucleiModel::generateParticleFate(G4CascadParticle& cparticle,
-								    G4ElementaryParticleCollider* theElementaryParticleCollider) {
+std::vector<G4CascadParticle> 
+G4NucleiModel::generateParticleFate(G4CascadParticle& cparticle,
+				    G4ElementaryParticleCollider* theElementaryParticleCollider) {
 
   if (verboseLevel > 3) {
     G4cout << " >>> G4NucleiModel::generateParticleFate" << G4endl;
@@ -812,6 +813,7 @@ std::vector<G4CascadParticle> G4NucleiModel::generateParticleFate(G4CascadPartic
 	}
 
 	G4CollisionOutput output = theElementaryParticleCollider->collide(&bullet, &target);
+        high_energy_interaction = theElementaryParticleCollider->HighEnergyFlag();
 
 	if (verboseLevel > 2){
 	  output.printCollisionOutput();
