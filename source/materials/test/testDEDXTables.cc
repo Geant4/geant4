82bec5cec3fc412e11db01002a6d3fbda35bc68d
Anton Lechner <Anton.Lechner@cern.ch>
2009-03-18 10:16:20 +0000

    Added tests for stopping power classes: G4IronStoppingICRU73 G4MaterialStoppingICRU73 G4SimpleMaterialStoppingICRU73 G4ExternalDEDXTable
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@38577 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/materials/test/testDEDXTables.cc
@@ -0,0 +1,220 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+// Anton.Lechner@cern.ch
+//
+// Environment variable TESTTARGET must be specified (see GNUmakefile)
+//
+
+#include "G4MaterialStoppingICRU73.hh"
+#include "G4SimpleMaterialStoppingICRU73.hh"
+#include "G4IronStoppingICRU73.hh"
+#include <sstream>
+
+
+G4bool Compare(G4PhysicsVector* physicsVector,
+               G4double refDEDXValue,
+               G4double kinEnergyPerNucleon, 
+               G4int atomicNumberIon,
+               const G4String& matIdentifier) {
+
+  G4double tableDEDXValue = -1.0;
+
+  if(physicsVector != 0) {
+     size_t nmbBins = physicsVector -> GetVectorLength();
+     G4double lowerEnergyEdge = physicsVector -> GetLowEdgeEnergy(0);
+     G4double upperEnergyEdge = physicsVector -> GetLowEdgeEnergy(nmbBins-1);
+
+     if(kinEnergyPerNucleon <= upperEnergyEdge && 
+        kinEnergyPerNucleon >= lowerEnergyEdge) {
+  	G4bool b;
+ 
+        tableDEDXValue = physicsVector -> GetValue(kinEnergyPerNucleon, b);
+     }
+  }
+
+  G4cout << " E/A1(MeV) = "   << kinEnergyPerNucleon / MeV
+         << ", Z(Ion) = "     << atomicNumberIon
+         << ", Mat = "        << matIdentifier
+         << " -- dE/dx(MeV*cm2/mg) = "    
+         << tableDEDXValue / (MeV * cm2 / (0.001 * g))
+	 << ", Ref(MeV*cm2/mg) = " 
+         << refDEDXValue / (MeV * cm2 / (0.001 * g))
+         << ":";
+
+  G4double eps = 0.0001 * (MeV * cm2 / (0.001 * g));
+
+  if( refDEDXValue < (tableDEDXValue - eps) ||
+      refDEDXValue > (tableDEDXValue + eps) ) {
+
+     G4cout << " Test failed" << G4endl;
+     return false;
+  } 
+  else G4cout << " Test passed" << G4endl; 
+
+  return true;
+}
+
+
+G4bool CompareDEDXValues(G4VIonDEDXTable* table,
+                         G4double refDEDXValue,
+                         G4double kinEnergyPerNucleon, 
+                         G4int atomicNumberIon,
+                         G4int atomicNumberMat) {
+
+  G4PhysicsVector* physicsVector = 
+                            table -> GetPhysicsVector(atomicNumberIon,
+                                                      atomicNumberMat);
+
+  std::stringstream ss;
+  ss << atomicNumberMat;
+
+  return Compare(physicsVector, refDEDXValue, kinEnergyPerNucleon, 
+                 atomicNumberIon, ss.str());
+}
+
+
+G4bool CompareDEDXValues(G4VIonDEDXTable* table,
+                         G4double refDEDXValue,
+                         G4double kinEnergyPerNucleon, 
+                         G4int atomicNumberIon,
+                         const G4String& matName) {
+
+  G4PhysicsVector* physicsVector = 
+                            table -> GetPhysicsVector(atomicNumberIon,
+                                                      matName);
+  return Compare(physicsVector, refDEDXValue, kinEnergyPerNucleon, 
+                 atomicNumberIon, matName);
+}
+
+
+G4bool TestApplicability(G4VIonDEDXTable* table,
+                         G4bool shouldBeApplicable,
+                         G4int atomicNumberIon,
+                         const G4String& matName) {
+
+  G4bool isApplicable = table -> IsApplicable(atomicNumberIon, matName);
+
+  G4cout << " Z(Ion) = " << atomicNumberIon
+         << ", Mat = "   << matName
+         << " -- ";
+
+  if(shouldBeApplicable) {
+     G4cout << " Should be applicable: "; 
+     if(isApplicable) G4cout << "Test passed." << G4endl;
+     else { G4cout << "Test failed." << G4endl; return false; }
+  }
+  else {
+     G4cout << " Should not be applicable: ";   
+     if(!isApplicable) G4cout << "Test passed." << G4endl;
+     else { G4cout << "Test failed." << G4endl; return false; }
+  }
+  return true;
+}
+
+
+int main() {  
+
+  G4double unitDEDX = (MeV * cm2 / (0.001 * g));
+
+  G4SimpleMaterialStoppingICRU73* icru73elem = 
+                                         new G4SimpleMaterialStoppingICRU73();
+
+  G4MaterialStoppingICRU73* icru73comp = new G4MaterialStoppingICRU73();
+
+  G4IronStoppingICRU73* icru73iron = new G4IronStoppingICRU73();
+
+  G4cout << "### Test of DEDX tables (ICRU 73) ###" << G4endl; 
+
+  G4cout << "#### I. Testing Function GetPhysicsVector() ###" << G4endl;
+
+  G4cout << G4endl << "### A. G4SimpleMaterialStoppingICRU73:" << G4endl;
+    
+  // Reference values taken directly from ICRU 73 tables
+  CompareDEDXValues(icru73elem, 8.31  * unitDEDX, 0.025, 3, 1 );
+  CompareDEDXValues(icru73elem, 3.312 * unitDEDX, 1.5,   8, 50);
+  CompareDEDXValues(icru73elem, 10.99 * unitDEDX, 10., 18, 10);
+  CompareDEDXValues(icru73elem, 4.412 * unitDEDX, 10., 18, 92);
+
+  CompareDEDXValues(icru73elem, 8.31  * unitDEDX, 0.025, 3, "G4_H");
+  CompareDEDXValues(icru73elem, 3.312 * unitDEDX, 1.5,   8, "G4_Sn");
+  CompareDEDXValues(icru73elem, 10.99 * unitDEDX, 10., 18, "G4_Ne");
+  CompareDEDXValues(icru73elem, 4.412 * unitDEDX, 10., 18, "G4_U");
+
+  G4cout << G4endl << "### B. G4MaterialStoppingICRU73:" << G4endl;
+    
+  // Reference values taken directly from ICRU 73 tables
+  CompareDEDXValues(icru73comp, 2.748  * unitDEDX, 0.025, 3, "G4_A-150_TISSUE");
+  CompareDEDXValues(icru73comp, 1.909  * unitDEDX, 0.025, 3, "G4_CARBON_DIOXIDE");
+  CompareDEDXValues(icru73comp, 4.225  * unitDEDX, 10.0 , 10, "G4_MYLAR");
+
+  G4cout << G4endl << "### C. G4IronStoppingICRU73:" << G4endl;
+
+  // Reference values taken directly from ICRU 73 tables
+  CompareDEDXValues(icru73iron, 27.6  * unitDEDX, 0.025, 26, "G4_H");
+  CompareDEDXValues(icru73iron, 30.72  * unitDEDX, 1.5, 26, "G4_Al");
+  CompareDEDXValues(icru73iron, 13.75  * unitDEDX, 10.0 , 26, "G4_Kr");
+
+  G4cout << G4endl << "#### II. Testing Function IsApplicable() ###" << G4endl;
+
+  G4cout << G4endl << "### A. G4SimpleMaterialStoppingICRU73:" << G4endl;
+    
+  TestApplicability(icru73elem, true, 3, "G4_H");
+  TestApplicability(icru73elem, true, 8, "G4_Sn");
+  TestApplicability(icru73elem, true, 18, "G4_Ne");
+  TestApplicability(icru73elem, true, 18, "G4_U");
+  TestApplicability(icru73elem, false, 19, "G4_H");
+  TestApplicability(icru73elem, false, 2, "G4_Sn");
+  TestApplicability(icru73elem, false, 3, "G4_Cr");
+
+  G4cout << G4endl << "### B. G4MaterialStoppingICRU73:" << G4endl;
+    
+  TestApplicability(icru73comp, true, 3, "G4_MYLAR");
+  TestApplicability(icru73comp, true, 8, "G4_POLYETHYLENE");
+  TestApplicability(icru73comp, true, 18, "G4_CARBON_DIOXIDE");
+  TestApplicability(icru73comp, true, 18, "G4_A-150_TISSUE");
+  TestApplicability(icru73comp, false, 19, "G4_MYLAR");
+  TestApplicability(icru73comp, false, 2, "G4_MYLAR");
+  TestApplicability(icru73comp, false, 3, "G4_Cr");
+
+  G4cout << G4endl << "### B. G4IronStoppingICRU73:" << G4endl;
+    
+  TestApplicability(icru73iron, true, 26, "G4_H");
+  TestApplicability(icru73iron, true, 26, "G4_Ne");
+  TestApplicability(icru73iron, true, 26, "G4_Kr");
+  TestApplicability(icru73iron, false, 19, "G4_H");
+  TestApplicability(icru73iron, false, 26, "G4_Sn");
+  TestApplicability(icru73iron, false, 3, "G4_Cr");
+
+  G4cout << G4endl << "################# DONE #################" << G4endl;
+
+  delete icru73elem;
+  delete icru73comp;
+  delete icru73iron;
+
+  return 0;
+} 
+
---
+++ source/materials/test/testExternalDEDXTable.cc
@@ -0,0 +1,262 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+// Anton.Lechner@cern.ch
+//
+// Environment variable TESTTARGET must be specified (see GNUmakefile)
+//
+//
+// The following functions of the class G4ExternalDEDXTable are tested:
+//       IsApplicable()
+//       AddPhysicsVector()
+//       RemovePhysicsVector()
+//       GetPhysicsVector()
+//       StorePhysicsTable()
+//       RetrievePhysicsTable()
+//
+// Note: This test creates a file "usertable.dat". You may want to delete it
+//       after running the test.
+
+#include "globals.hh"
+#include "G4ExternalDEDXTable.hh"
+#include "G4LPhysicsFreeVector.hh"
+
+
+void Test(const G4String& fileName) {
+
+  // ########################################################################
+  // # Creating physics vectors
+  // ########################################################################
+
+  G4double factor = MeV * cm2 / (0.001 * g);
+
+  G4double E[31] = {.025,.03,.04,.05,.06,.07,.08,.09,.1,.15,.2,.25,.3,
+                    .4,.5,.6,.7,.8,.9,1,1.5,2,2.5,3,4,5,6,7,8,9,10};
+
+  // Lithium ions in hydrogen
+  G4double H_3[31]={8.31,8.919,9.838,10.46,10.87,11.14,11.3,11.4,11.43,
+                    11.21,10.73,10.21,9.696,8.773,7.989,7.326,6.761,6.275,
+                    5.853,5.485,4.175,3.377,2.843,2.46,1.948,1.62,1.393,
+                    1.225,1.095,0.9921,0.9082};
+
+  G4LPhysicsFreeVector* physVecZ3MatH = 
+                 new G4LPhysicsFreeVector(31, E[0] * MeV, E[30] * MeV);
+  physVecZ3MatH -> SetSpline(true);
+
+  for(size_t i = 0; i < 31; i++) {
+    physVecZ3MatH -> PutValues(i, E[i] * MeV, H_3[i] * factor);
+  }
+
+  // Lithium ions in tissue
+  G4double A_3[31]={2.748,2.992,3.386,3.674,3.877,4.016,4.108,4.166,4.2,
+                    4.186,4.059,3.906,3.751,3.458,3.2,2.974,2.777,2.603,
+                    2.449,2.313,1.807,1.485,1.263,1.1,0.8801,0.7372,0.6368,
+                    0.5623,0.5046,0.4586,0.4209}; 
+
+  G4LPhysicsFreeVector* physVecZ3MatTissue = 
+                 new G4LPhysicsFreeVector(31, E[0] * MeV, E[30] * MeV);
+  physVecZ3MatTissue -> SetSpline(true);
+
+  for(size_t i = 0; i < 31; i++) {
+      physVecZ3MatTissue -> PutValues(i, E[i] * MeV, A_3[i] * factor);
+  }
+
+  // ########################################################################
+  // # Test 1:
+  // ########################################################################
+
+  G4ExternalDEDXTable* tableA = new G4ExternalDEDXTable();
+
+  G4cout << G4endl << "##### Testing AddPhysicsVector() #####" 
+         << G4endl;
+
+  G4cout << "***Stopping power of hydrogen for Li ions***" << G4endl;
+
+  G4cout << "  Vector not available. Is applicable: "; 
+  if( tableA -> IsApplicable(3, "G4_H") ) 
+     G4cout << "Yes. Test failed" << G4endl; 
+  else 
+     G4cout << "No. Test passed" << G4endl; 
+
+  G4cout << "  Adding vector. Success: "; 
+  if( tableA -> AddPhysicsVector(physVecZ3MatH, 3, "G4_H", 1) )
+     G4cout << "Yes. Test passed" << G4endl; 
+  else 
+     G4cout << "No. Test failed" << G4endl; 
+
+  G4cout << "  Vector added. Is applicable: "; 
+  if( tableA -> IsApplicable(3, "G4_H") ) 
+     G4cout << "Yes. Test passed" << G4endl; 
+  else 
+     G4cout << "No. Test failed" << G4endl; 
+
+  G4cout << "  Vector added. Is applicable: "; 
+  if( tableA -> IsApplicable(3, 1) ) 
+     G4cout << "Yes. Test passed" << G4endl; 
+  else 
+     G4cout << "No. Test failed" << G4endl; 
+
+  G4cout << "***Stopping power of tissue for Li ions***" << G4endl;
+  
+  G4cout << "  Vector not available. Is applicable: "; 
+  if( tableA -> IsApplicable(3, "G4_A-150_TISSUE") ) 
+     G4cout << "Yes. Test failed" << G4endl; 
+  else 
+     G4cout << "No. Test passed" << G4endl; 
+
+  G4cout << "  Adding vector. Success: "; 
+  if( tableA -> AddPhysicsVector(physVecZ3MatTissue, 3, "G4_A-150_TISSUE") )
+     G4cout << "Yes. Test passed" << G4endl; 
+  else 
+     G4cout << "No. Test failed" << G4endl; 
+
+  G4cout << "  Vector added. Is applicable: "; 
+  if( tableA -> IsApplicable(3, "G4_A-150_TISSUE") ) 
+     G4cout << "Yes. Test passed" << G4endl; 
+  else 
+     G4cout << "No. Test failed" << G4endl; 
+
+  // ########################################################################
+  // # Test 2:
+  // ########################################################################
+
+  G4cout << G4endl << "##### Testing GetPhysicsVector() #####" 
+         << G4endl;
+
+  G4PhysicsVector* vector = 0;
+
+  vector = tableA -> GetPhysicsVector(3, "G4_H");
+
+  G4cout << "  Vector retrieved. Found: "; 
+  if( physVecZ3MatH == vector ) 
+     G4cout << "Yes. Test passed" << G4endl; 
+  else 
+     G4cout << "No. Test failed" << G4endl; 
+
+  vector = tableA -> GetPhysicsVector(3, 1);
+
+  G4cout << "  Vector retrieved. Found: ";
+  if( physVecZ3MatH == vector) 
+     G4cout << "Yes. Test passed" << G4endl; 
+  else 
+     G4cout << "No. Test failed" << G4endl; 
+
+  vector = tableA -> GetPhysicsVector(3, "G4_A-150_TISSUE");
+ 
+  G4cout << "  Vector retrieved. Found: ";
+  if( physVecZ3MatTissue == vector) 
+     G4cout << "Yes. Test passed" << G4endl; 
+  else 
+     G4cout << "No. Test failed" << G4endl; 
+
+
+  // ########################################################################
+  // # Test 3:
+  // ########################################################################
+
+  G4cout << G4endl << "##### Testing StorePhysicsTable() #####" 
+         << G4endl;
+
+  G4cout << "  Storing table: ";
+  if( tableA -> StorePhysicsTable(fileName) ) 
+     G4cout << " Test passed" << G4endl;
+  else 
+     G4cout << " Test failed" << G4endl; 
+
+  // ########################################################################
+  // # Test 4:
+  // ########################################################################
+
+  G4cout << G4endl << "##### Testing RemovePhysicsVector() #####" 
+         << G4endl;
+
+  G4cout << "  Removing vector. Success "; 
+  if( tableA -> RemovePhysicsVector(3, "G4_H") )
+     G4cout << "Yes. Test passed" << G4endl; 
+  else 
+     G4cout << "No. Test failed" << G4endl; 
+
+  G4cout << "  Vector was removed. Is applicable: "; 
+  if( tableA -> IsApplicable(3, "G4_H") ) 
+     G4cout << "Yes. Test failed" << G4endl; 
+  else 
+     G4cout << "No. Test passed" << G4endl; 
+
+  G4cout << "  Vector was removed. Is applicable: "; 
+  if( tableA -> IsApplicable(3, 1) ) 
+     G4cout << "Yes. Test failed" << G4endl; 
+  else 
+     G4cout << "No. Test passed" << G4endl; 
+
+  delete tableA;
+
+  // ########################################################################
+  // # Test 5:
+  // ########################################################################
+
+  G4ExternalDEDXTable* tableB = new G4ExternalDEDXTable();
+
+  G4cout << G4endl << "##### Testing RetrievePhysicsTable() #####" 
+         << G4endl;
+
+  G4cout << "  Vector not available. Is applicable: "; 
+  if( tableB -> IsApplicable(3, "G4_H") ) 
+     G4cout << "Yes. Test failed" << G4endl; 
+  else 
+     G4cout << "No. Test passed" << G4endl; 
+
+  G4cout << "  Retrieving table: ";
+  if( tableB -> RetrievePhysicsTable(fileName) ) 
+     G4cout << "Test passed" << G4endl;
+  else 
+     G4cout << "Test failed" << G4endl;
+
+  G4cout << "  Vector added. Is applicable: "; 
+  if( tableB -> IsApplicable(3, "G4_H") ) 
+     G4cout << "Yes. Test passed" << G4endl; 
+  else 
+     G4cout << "No. Test failed" << G4endl; 
+
+  G4cout << "  Vector added. Is applicable: "; 
+  if( tableB -> IsApplicable(3, 1) ) 
+     G4cout << "Yes. Test passed" << G4endl; 
+  else 
+     G4cout << "No. Test failed" << G4endl; 
+
+  G4cout << G4endl;
+
+  delete tableB;
+}
+
+
+int main() {  
+
+
+  Test("usertable.dat");
+
+  return EXIT_SUCCESS;
+} 
+
