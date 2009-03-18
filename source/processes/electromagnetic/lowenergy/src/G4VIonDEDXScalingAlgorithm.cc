67fc3ea13e0eeaeb10dfe0bd4b411fb97d8e52bc
Anton Lechner <Anton.Lechner@cern.ch>
2009-03-18 10:48:56 +0000

    Added interface class for scaling algorithms applied to electronic stopping power tables for ions: - Used by G4IonDEDXHandler
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@38581 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/processes/electromagnetic/lowenergy/src/G4VIonDEDXScalingAlgorithm.cc
@@ -0,0 +1,58 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+//
+// ===========================================================================
+// GEANT4 class source file
+//
+// Class:                G4VIonDEDXScalingAlgorithm
+//
+// Author:               Anton Lechner (Anton.Lechner@cern.ch)
+//
+// First implementation: 11. 03. 2009
+//
+// Modifications: 
+//
+//
+// Class description:
+//    Base class for dE/dx scaling algorithms, used by G4IonDEDXHandler
+//
+// Comments:
+//
+// =========================================================================== 
+
+
+#include "G4VIonDEDXScalingAlgorithm.hh"
+
+
+G4VIonDEDXScalingAlgorithm::G4VIonDEDXScalingAlgorithm() {
+
+}
+
+
+G4VIonDEDXScalingAlgorithm::~G4VIonDEDXScalingAlgorithm() {
+
+}
