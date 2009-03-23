33b2a83a5eb31029194ea76e6e6726266868de95
Dennis Herbert Wright <dennis@cern.ch>
2009-03-23 17:06:31 +0000

    restore hadr-casc-V09-02-00 version
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@38634 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/models/cascade/cascade/include/G4NucleiModel.hh
+++ source/processes/hadronic/models/cascade/cascade/include/G4NucleiModel.hh
@@ -132,27 +132,18 @@ public:
 
   G4CascadParticle initializeCascad(G4InuclElementaryParticle* particle);
 
-  std::pair<std::vector<G4CascadParticle>, std::vector<G4InuclElementaryParticle> > 
-  initializeCascad(G4InuclNuclei* bullet, G4InuclNuclei* target);
+  std::pair<std::vector<G4CascadParticle>, std::vector<G4InuclElementaryParticle> > initializeCascad(G4InuclNuclei* bullet, G4InuclNuclei* target);
 
   std::pair<G4int, G4int> getTypesOfNucleonsInvolved() const {
     return std::pair<G4int, G4int>(current_nucl1, current_nucl2);
   };
-
   G4bool worthToPropagate(const G4CascadParticle& cparticle) const; 
     
   G4InuclElementaryParticle generateNucleon(G4int type, 
 					    G4int zone) const;
 
-  G4bool highEnergyInteraction() const {
-    return high_energy_interaction;
-  }
-
-private:
- 
-  G4int verboseLevel;
-  G4bool high_energy_interaction;
-
+private: 
+G4int verboseLevel;
   G4bool passFermi(const std::vector<G4InuclElementaryParticle>& particles, 
 		   G4int zone);
 
