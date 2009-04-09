18fb7d46a77532b4ffd855fa1901966d9eb928d0
Mikhail Kossov <mkossov@cern.ch>
2009-04-09 08:16:22 +0000

    a tag for working QGSC_QGSC/CHIPS physics lists
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@38802 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/physics_lists/builders/include/G4QGSC_QGSCProtonBuilder.hh
@@ -0,0 +1,75 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+#ifndef G4QGSC_QGSCProtonBuilder_h
+#define G4QGSC_QGSCProtonBuilder_h 
+
+#include "globals.hh"
+
+#include "G4HadronElasticProcess.hh"
+#include "G4HadronFissionProcess.hh"
+#include "G4HadronCaptureProcess.hh"
+#include "G4ProtonInelasticProcess.hh"
+#include "G4VProtonBuilder.hh"
+
+#include "G4NeutronInelasticCrossSection.hh"
+#include "G4TheoFSGenerator.hh"
+#include "G4QStringChipsParticleLevelInterface.hh"
+#include "G4QGSModel.hh"
+#include "G4QGSParticipants.hh"
+#include "G4QGSMFragmentation.hh"
+#include "G4ExcitedStringDecay.hh"
+#include "G4QuasiElasticChannel.hh"
+
+#include "G4ProtonInelasticCrossSection.hh"
+
+class G4QGSC_QGSCProtonBuilder : public G4VProtonBuilder
+{
+  public: 
+    G4QGSC_QGSCProtonBuilder(G4bool quasiElastic=false);
+    virtual ~G4QGSC_QGSCProtonBuilder();
+
+  public: 
+    virtual void Build(G4HadronElasticProcess * aP);
+    virtual void Build(G4ProtonInelasticProcess * aP);
+    
+    void SetMinEnergy(G4double aM) {theMin = aM;}
+
+  private:
+    G4ProtonInelasticCrossSection theXSec;
+    G4TheoFSGenerator * theModel;
+    G4QStringChipsParticleLevelInterface * theCascade;
+    G4QGSModel< G4QGSParticipants > * theStringModel;
+    G4ExcitedStringDecay * theStringDecay;
+    G4QuasiElasticChannel * theQuasiElastic;
+
+    G4double theMin;
+
+};
+
+// 2009 by M. Kosov
+
+#endif
+
---
+++ source/physics_lists/builders/src/G4QGSC_CHIPSPiKBuilder.cc
@@ -0,0 +1,116 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+#include "G4QGSC_CHIPSPiKBuilder.hh"
+#include "G4ParticleDefinition.hh"
+#include "G4ParticleTable.hh"
+#include "G4ProcessManager.hh"
+
+G4QGSC_CHIPSPiKBuilder::
+G4QGSC_CHIPSPiKBuilder(G4bool quasiElastic) 
+{
+  theMin = 0*GeV;
+  theModel = new G4TheoFSGenerator("QGSC_CHIPS");
+  
+  theStringModel = new G4QGSModel< G4QGSParticipants >;
+  theStringDecay = new G4ExcitedStringDecay(new G4QGSMFragmentation);
+  theStringModel->SetFragmentationModel(theStringDecay);
+
+  theCascade = new G4StringChipsParticleLevelInterface;
+
+  theModel->SetTransport(theCascade);
+  theModel->SetHighEnergyGenerator(theStringModel);
+  if (quasiElastic)
+  {
+     theQuasiElastic=new G4QuasiElasticChannel;
+     theModel->SetQuasiElasticChannel(theQuasiElastic);
+  } else 
+  {  theQuasiElastic=0;}  
+}
+
+G4QGSC_CHIPSPiKBuilder::
+~G4QGSC_CHIPSPiKBuilder() 
+{
+  delete theCascade;
+  delete theStringDecay;
+  delete theStringModel;
+  if ( theQuasiElastic ) delete theQuasiElastic;
+  delete theModel;
+}
+
+void G4QGSC_CHIPSPiKBuilder::
+Build(G4HadronElasticProcess * ) {}
+
+void G4QGSC_CHIPSPiKBuilder::
+Build(G4PionPlusInelasticProcess * aP)
+{
+  theModel->SetMinEnergy(theMin);
+  theModel->SetMaxEnergy(100*TeV);
+  aP->AddDataSet(&thePiCross);
+  aP->RegisterMe(theModel);
+}
+
+void G4QGSC_CHIPSPiKBuilder::
+Build(G4PionMinusInelasticProcess * aP)
+{
+  theModel->SetMinEnergy(theMin);
+  theModel->SetMaxEnergy(100*TeV);
+  aP->AddDataSet(&thePiCross);
+  aP->RegisterMe(theModel);
+}
+
+void G4QGSC_CHIPSPiKBuilder::
+Build(G4KaonPlusInelasticProcess * aP)
+{
+  theModel->SetMinEnergy(theMin);
+  theModel->SetMaxEnergy(100*TeV);
+  aP->RegisterMe(theModel);
+}
+
+void G4QGSC_CHIPSPiKBuilder::
+Build(G4KaonMinusInelasticProcess * aP)
+{
+  theModel->SetMinEnergy(theMin);
+  theModel->SetMaxEnergy(100*TeV);
+  aP->RegisterMe(theModel);
+}
+
+void G4QGSC_CHIPSPiKBuilder::
+Build(G4KaonZeroLInelasticProcess * aP)
+{
+  theModel->SetMinEnergy(theMin);
+  theModel->SetMaxEnergy(100*TeV);
+  aP->RegisterMe(theModel);
+}
+
+void G4QGSC_CHIPSPiKBuilder::
+Build(G4KaonZeroSInelasticProcess * aP)
+{
+  theModel->SetMinEnergy(theMin);
+  theModel->SetMaxEnergy(100*TeV);
+  aP->RegisterMe(theModel);
+}
+
+// 2009 by M. Kosov
