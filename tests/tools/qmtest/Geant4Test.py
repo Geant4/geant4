ec1004dbd5f4310bfd1f635e4f84b4b9b1ab5fa7
Victor Diez Gonzalez <vdiez@cern.ch>
2009-04-24 13:30:24 +0000

    The geant4 test class now saves the stderr and stdout of the test in text files
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@38942 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- tests/tools/qmtest/Geant4Test.py
+++ tests/tools/qmtest/Geant4Test.py
@@ -9,29 +9,30 @@ class Geant4Test (ShellCommandTest):
     installdir = os.environ['G4INSTALL']
     bindir = os.environ['G4BIN']
     system = os.environ['G4SYSTEM']
-    
+   
     if sys.platform == 'win32':
         self.command = self.command.replace('$G4INSTALL',installdir)
         self.command = self.command.replace('$G4BIN',bindir)
         self.command = self.command.replace('$G4SYSTEM',system)
         self.command = self.command.replace(';',' && ')
-        if self.command.count('make') > 0:
-            # We are starting a build test
-            self.command = self.command.replace('\\','/')
-        else:
-            # We are starting a run test
-            self.command = self.command.replace('/','\\')
-            
-        #TEMPORAL: EVERYTHING TO BE TESTED ON CYGWIN
-        #TODO: JUST BUILD TESTS ON CYGWIN; RUN ON WINDOWS CMD
+       
         self.command = self.command.replace('/','\\')
-    
-    #file = urllib.urlopen('http://geant4.cern.ch/cgi-bin/bonsai/nightly/gettaglist.cgi').read()
+       
+        folder = self.command.split('&&')
+    else:
+        folder = self.command.split(';')
 
-    #print "TAGS SELECTED FOR TESTING:"
-    #numbers=file.split('\n')
-    #for line in numbers: print line
-    #print "---------------------------------------------------------------\n"
-
+    folder = folder[0].split(' ')
+    folder = folder[1]
+   
     print self.command
     ShellCommandTest.Run(self, context, result)
+   
+    os.chdir(folder)
+    stdout_file = open(result.GetId()+'_stdout.txt', 'w')
+    stdout_file.write(result["ExecTest.stdout"])
+    stdout_file.close()
+   
+    stderr_file = open(result.GetId()+'_stderr.txt', 'w')
+    stderr_file.write(result["ExecTest.stderr"])
+    stderr_file.close()
