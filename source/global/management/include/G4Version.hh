bcf8213134a0d399b7ab88b47c1fba3536dbe875
Vladimir Ivantchenko <Vladimir.Ivantchenko@cern.ch>
2009-06-19 14:25:40 +0000

    restore version 1.19
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@39648 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/global/management/include/G4Version.hh
+++ source/global/management/include/G4Version.hh
@@ -24,7 +24,7 @@
 // ********************************************************************
 //
 //
-// $Id: G4Version.hh,v 1.22 2009-06-19 14:23:18 vnivanch Exp $
+// $Id: G4Version.hh,v 1.23 2009-06-19 14:25:40 vnivanch Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // Version information
