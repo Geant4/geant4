01070ade1d9bd47c07c7df7f6db6b77504291a50
Vladimir Ivantchenko <Vladimir.Ivantchenko@cern.ch>
2009-07-02 09:41:31 +0000

    added elastic models following G4HadronicInteraction interface
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@39738 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/processes/hadronic/models/coherent_elastic/include/G4CHIPSElastic.hh
@@ -0,0 +1,65 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+// $Id: G4CHIPSElastic.hh,v 1.1 2009-07-02 09:41:31 vnivanch Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+// Geant4 Header : G4CHIPSElastic
+//
+// Author : V.Ivanchenko 29 June 2009 (redesign old elastic model)
+//  
+// Modified:
+//
+// Class Description
+// Default model for elastic scattering; GHEISHA algorithm is used 
+// Class Description - End
+
+#ifndef G4CHIPSElastic_h
+#define G4CHIPSElastic_h 1
+ 
+#include "G4VHadronElastic.hh"
+#include "globals.hh"
+
+class G4VQCrossSection;
+
+class G4CHIPSElastic : public G4VHadronElastic
+{
+public:
+
+  G4CHIPSElastic();
+
+  virtual ~G4CHIPSElastic();
+ 
+  virtual G4double SampleT(const G4ParticleDefinition* p, G4double plab,
+			   G4int Z, G4int A);
+
+private:
+
+  G4VQCrossSection* qCManager;
+
+};
+
+#endif
