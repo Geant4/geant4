6b43fee41e115cc9190de13c7b776c643420b17d
Vladimir Ivantchenko <Vladimir.Ivantchenko@cern.ch>
2009-09-01 15:43:16 +0000

    fixed access to ion table
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@40312 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/models/de_excitation/photon_evaporation/src/G4NeutronRadCapture.cc
+++ source/processes/hadronic/models/de_excitation/photon_evaporation/src/G4NeutronRadCapture.cc
@@ -23,7 +23,7 @@
 // * acceptance of all terms of the Geant4 Software license.          *
 // ********************************************************************
 //
-// $Id: G4NeutronRadCapture.cc,v 1.1 2009-08-31 13:52:42 vnivanch Exp $
+// $Id: G4NeutronRadCapture.cc,v 1.2 2009-09-01 15:43:16 vnivanch Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 //
@@ -43,6 +43,11 @@
 #include "G4PhotonEvaporation.hh"
 #include "G4DynamicParticle.hh"
 #include "G4ParticleDefinition.hh"
+#include "G4ParticleTable.hh"
+#include "G4Deuteron.hh"
+#include "G4Triton.hh"
+#include "G4He3.hh"
+#include "G4Alpha.hh"
 
 G4NeutronRadCapture::G4NeutronRadCapture() 
   : G4HadronicInteraction("nRadCapture")
@@ -79,7 +84,7 @@ G4HadFinalState* G4NeutronRadCapture::ApplyYourself(
     return &theParticleChange;
   }
 
-  if (verboseLevel >1) {
+  if (verboseLevel > 1) {
     G4cout << "G4NeutronRadCapture::DoIt: Eini(MeV)=" 
 	   << aTrack.GetKineticEnergy()/MeV << "  Eexc(MeV)= " 
 	   << lv1.e() - G4NucleiProperties::GetNuclearMass(A+1, Z) 
@@ -91,27 +96,37 @@ G4HadFinalState* G4NeutronRadCapture::ApplyYourself(
   //
   G4FragmentVector* fv = photonEvaporation->BreakItUp(aNucleus);
   size_t n = fv->size();
-  G4double elocal = 0.0;
+
   if (verboseLevel > 1) {
     G4cout << "G4NeutronRadCapture: " << n << " final particle" << G4endl;
   }
   for(size_t i=0; i<n; ++i) {
-    G4Fragment* f = (*fv)[i];
-    G4double exc = f->GetExcitationEnergy();
-    if(exc != 0.) {
-      elocal += exc;
-      f->SetExcitationEnergy(0.0);
-    }
+    G4Fragment* f = (*fv)[i];    
     G4LorentzVector lvres = f->GetMomentum();   
     lvres.boost(bst);
-    G4ParticleDefinition* theDef = f->GetParticleDefinition();  
+    G4int Z = G4int(f->GetZ() + 0.5);
+    G4int A = G4int(f->GetA() + 0.5);
+
+    G4ParticleDefinition* theDef = 0;
+    if(0 == Z && 0 == A) {theDef =  f->GetParticleDefinition();}
+    else if(Z > 2 || A > 4) 
+      {
+	theDef = G4ParticleTable::GetParticleTable()->FindIon(Z,A,0,Z);
+      }
+    else if (Z == 1 && A == 2) {theDef = G4Deuteron::Deuteron();}
+    else if (Z == 1 && A == 3) {theDef = G4Triton::Triton();}
+    else if (Z == 2 && A == 3) {theDef = G4He3::He3();}
+    else if (Z == 2 && A == 4) {theDef = G4Alpha::Alpha();}
+
     if (verboseLevel > 1) {
-      G4cout << i << ". " << G4endl;
+      G4cout << i << ". " << theDef->GetParticleName()
+	     << "   " << lvres << G4endl;
     }
-    theParticleChange.AddSecondary(new G4DynamicParticle(theDef, lvres));
+    if(theDef) {
+      theParticleChange.AddSecondary(new G4DynamicParticle(theDef, lvres));
+    }
     delete f;
   }
-  if(elocal > 0.0) theParticleChange.SetLocalEnergyDeposit(elocal);
 
   return &theParticleChange;
 }
