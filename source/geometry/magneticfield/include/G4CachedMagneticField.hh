799bda8b10a96ba28aa5f138012b89b197c4dcd3
John Apostolakis <John.Apostolakis@cern.ch>
2009-11-05 11:06:13 +0000

    New Simple class to cache value of Magnetic Field. Speeds up code when calculation of field value is complex.
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@40998 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/geometry/magneticfield/include/G4CachedMagneticField.hh
@@ -0,0 +1,83 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+// $ Id: G4CachedMagneticField.hh,v 1.0 2009/07/20 18:53:00 japost Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+//
+// class G4CachedMagneticField
+//
+// Class description:
+//
+// Caches Magnetic Field value, for field whose evaluation is expensive.
+
+// History:
+// - Created. JA, July 20th, 2009.
+// --------------------------------------------------------------------
+
+#ifndef G4CACHED_MAGNETIC_FIELD_DEF
+#define G4CACHED_MAGNETIC_FIELD_DEF
+
+#include "G4Types.hh"
+#include "G4ThreeVector.hh"
+#include "G4MagneticField.hh"
+
+class G4CachedMagneticField : public G4MagneticField
+{
+  public:  // with description
+
+     G4CachedMagneticField(G4MagneticField *, G4double distanceConst);
+     virtual ~G4CachedMagneticField();
+       // Constructor and destructor. No actions.
+
+     G4CachedMagneticField(const G4CachedMagneticField &r);
+     G4CachedMagneticField& operator = (const G4CachedMagneticField &p);
+       // Copy constructor & assignment operator.
+
+     virtual void  GetFieldValue( const G4double Point[4],
+                                        G4double *Bfield ) const;
+     
+     G4double GetConstDistance() const         { return fDistanceConst; } 
+     void     SetConstDistance( G4double dist ){ fDistanceConst= dist;}
+
+     G4int GetCountCalls() const { return fCountCalls; }
+     G4int GetCountEvaluations() const { return fCountEvaluations; } 
+     void  ClearCounts() { fCountCalls = 0; fCountEvaluations=0; }
+     void  ReportStatistics();
+
+  private: 
+     G4MagneticField *fpMagneticField;
+     // When the field is evaluated within this distance it will not change
+     G4double      fDistanceConst;
+     // Caching state
+     mutable G4ThreeVector fLastLocation;
+     mutable G4ThreeVector fLastValue;
+
+  protected:
+     mutable G4int fCountCalls, fCountEvaluations;  
+};
+
+#endif /* G4CACHED_MAGNETIC_FIELD_DEF */
---
+++ source/geometry/magneticfield/src/G4CachedMagneticField.cc
@@ -0,0 +1,99 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+// $Id: G4CachedMagneticField.cc,v 1.1 2009-11-05 11:06:13 japost Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+// --------------------------------------------------------------------
+
+#include "G4CachedMagneticField.hh"
+
+G4CachedMagneticField::G4CachedMagneticField(G4MagneticField *pMagField, 
+					     G4double         distance)
+  : G4MagneticField(), 
+    fLastLocation(DBL_MAX,DBL_MAX,DBL_MAX),
+    fLastValue(DBL_MAX,DBL_MAX,DBL_MAX),
+    fCountCalls(0),  fCountEvaluations(0)
+{
+  fpMagneticField=     pMagField;
+  fDistanceConst= distance;
+
+  // G4cout << " Cached-B-Field constructor> Distance = " << distance << G4endl;
+  this->ClearCounts(); 
+}
+
+G4CachedMagneticField::~G4CachedMagneticField()
+{
+}
+
+void
+G4CachedMagneticField::ReportStatistics()
+{
+  G4cout << " Cached field: " << this << G4endl
+	 << "   Number of calls:        " << fCountCalls << G4endl
+	 << "   Number of evaluations : " << fCountEvaluations << G4endl;                     
+}
+
+G4CachedMagneticField::G4CachedMagneticField(const G4CachedMagneticField &rightCMF)
+  : G4MagneticField()
+{
+  fpMagneticField=      rightCMF.fpMagneticField;
+  fDistanceConst = rightCMF.fDistanceConst;
+  fLastLocation  = rightCMF.fLastLocation;
+  fLastValue     = rightCMF.fLastValue;
+  this->ClearCounts(); 
+}
+
+G4CachedMagneticField& G4CachedMagneticField::operator = (const G4CachedMagneticField &p)
+{
+  if (&p == this) return *this; *this = p; return *this;
+}
+
+void
+G4CachedMagneticField::GetFieldValue( const G4double Point[4],
+                                            G4double *Bfield ) const
+{
+  G4ThreeVector newLocation( Point[0], Point[1], Point[2] );
+
+  // G4cout << "Cache-B-field called at " << newLocation << G4endl;
+
+  G4double      distSq= (newLocation-fLastLocation).mag2();
+  fCountCalls++;
+  if( distSq < fDistanceConst*fDistanceConst ) { 
+     Bfield[0] = fLastValue.x();
+     Bfield[1] = fLastValue.y();
+     Bfield[2] = fLastValue.z();
+  }else{
+     // G4CachedMagneticField* thisNonC= const_cast<G4CachedMagneticField*>(this);
+     fpMagneticField->GetFieldValue( Point, Bfield );
+     // G4cout << " Evaluating. " << G4endl;
+     fCountEvaluations++;
+     // thisNonC->
+     fLastLocation= G4ThreeVector( Point[0],  Point[1],  Point[2] );
+     // thisNonC->
+     fLastValue=    G4ThreeVector( Bfield[0], Bfield[1], Bfield[2] );
+  }
+}
