9aab74f621598847267fd4807f281b074b4c909e
Vladimir Grichine <Vladimir.Grichine@cern.ch>
2009-11-17 09:53:03 +0000

    new class g4MonopoleEq for rhs of d(pc)/ds propagation in field, SI units
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@41351 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/geometry/magneticfield/include/G4MonopoleEq.hh
@@ -0,0 +1,74 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+// $Id: G4MonopoleEq.hh,v 1.1 2009-11-17 09:53:03 grichine Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+//
+// class G4MonopoleEq
+//
+// Class description:
+//
+// This is the right-hand side of equation of motion 
+// for monopole in a combined
+// electric and magnetic field.
+
+// History:
+// - Created. V.Grichine, 17.11.09
+// -------------------------------------------------------------------
+
+#ifndef G4EQMAGELECTRICFIELD_hh
+#define G4EQMAGELECTRICFIELD_hh
+
+#include "G4EquationOfMotion.hh"
+#include "G4ElectroMagneticField.hh"
+
+class G4MonopoleEq : public G4EquationOfMotion
+{
+  public:  // with description
+
+    G4MonopoleEq(G4ElectroMagneticField *emField )
+      : G4EquationOfMotion( emField ) {;}
+
+    ~G4MonopoleEq() {;} 
+
+    void  SetChargeMomentumMass(G4double particleCharge, // in e+ units
+                                G4double MomentumXc,
+                                G4double mass);
+
+    void EvaluateRhsGivenB(const G4double y[],
+                           const G4double Field[],
+                                 G4double dydx[] ) const;
+      // Given the value of the electromagnetic field, this function 
+      // calculates the value of the derivative dydx.
+
+  private:
+
+    G4double        fElectroMagCof ;
+    G4double        fMassCof;
+};
+
+#endif
