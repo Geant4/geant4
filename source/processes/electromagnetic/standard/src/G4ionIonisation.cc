d373a1d3dc22798cba2527142e362b74f9976282
Vladimir Ivantchenko <Vladimir.Ivantchenko@cern.ch>
2009-11-24 17:25:50 +0000

    return linLossLimit=0.15
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@41752 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/electromagnetic/standard/src/G4ionIonisation.cc
+++ source/processes/electromagnetic/standard/src/G4ionIonisation.cc
@@ -23,7 +23,7 @@
 // * acceptance of all terms of the Geant4 Software license.          *
 // ********************************************************************
 //
-// $Id: G4ionIonisation.cc,v 1.68 2009-11-24 09:57:20 vnivanch Exp $
+// $Id: G4ionIonisation.cc,v 1.69 2009-11-24 17:25:50 vnivanch Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // -------------------------------------------------------------------
@@ -88,7 +88,7 @@ G4ionIonisation::G4ionIonisation(const G4String& name)
     stopDataActive(true),
     nuclearStopping(true)
 {
-  SetLinearLossLimit(0.05);
+  SetLinearLossLimit(0.15);
   SetStepFunction(0.1, 0.1*mm);
   SetIntegral(true);
   SetProcessSubType(fIonisation);
