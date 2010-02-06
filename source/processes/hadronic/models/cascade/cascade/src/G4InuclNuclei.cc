f9469182d6e12321e8aff87a29af7be5b69506ee
Dennis Herbert Wright <dennis@cern.ch>
2010-02-06 09:34:28 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@42349 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/models/cascade/cascade/src/G4InuclNuclei.cc
+++ source/processes/hadronic/models/cascade/cascade/src/G4InuclNuclei.cc
@@ -22,39 +22,7 @@
 // * use  in  resulting  scientific  publications,  and indicate your *
 // * acceptance of all terms of the Geant4 Software license.          *
 // ********************************************************************
-// $Id: G4InuclNuclei.cc,v 1.2 2010-01-26 23:17:47 mkelsey Exp $
+// $Id: G4InuclNuclei.cc,v 1.3 2010-02-06 09:34:16 dennis Exp $
 // Geant4 tag: $Name: not supported by cvs2svn $
 
 #include "G4InuclNuclei.hh"
-#include "G4ParticleDefinition.hh"
-#include "G4ParticleTable.hh"
-#include <assert.h>
-
-
-G4Allocator<G4InuclNuclei> anInuclNucleiAllocator;
-
-// Convert nuclear configuration to standard GEANT4 pointer
-
-// WARNING:  Opposite conventions!  G4InuclNuclei uses (A,Z) everywhere, while
-//	  G4ParticleTable::GetIon() uses (Z,A)!
-
-G4ParticleDefinition* 
-G4InuclNuclei::makeDefinition(G4double a, G4double z, G4double exc) {
-  G4ParticleTable* pTable = G4ParticleTable::GetParticleTable();
-  G4ParticleDefinition *pd = pTable->GetIon(G4int(z), G4int(a), exc);
-  assert(0 != pd);
-  return pd;
-}
-
-G4double G4InuclNuclei::getNucleiMass(G4double a, G4double z) {
-  G4ParticleDefinition* pd = makeDefinition(a,z);
-  return pd ? pd->GetPDGMass()*MeV/GeV : 0.;	// From G4 to Bertini units
-}
-
-// Assignment operator for use with std::sort()
-G4InuclNuclei& G4InuclNuclei::operator=(const G4InuclNuclei& right) {
-  exitationEnergy = right.exitationEnergy;
-  theExitonConfiguration = right.theExitonConfiguration;
-  G4InuclParticle::operator=(right);
-  return *this;
-}
