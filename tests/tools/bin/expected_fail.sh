7c4fd7f3e3b7716703a18017dcdd3647465ee328
Geant4 Testing <geant4.testing@cern.ch>
2010-02-26 09:57:32 +0000

    much less tests failing, mostly XercesC 3.1 on SLC5
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@42432 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- tests/tools/bin/expected_fail.sh
+++ tests/tools/bin/expected_fail.sh
@@ -5,19 +5,6 @@ cat << EoI | grep -v -e "^#"
 Random_FAIL exam-ext-field-field04-run \
 	slc4_amd64_gcc34_dbg	\
 	i686-slc5-gcc43-opt
-
-EXPECTED_FAIL exam-ext-electromagnetic-testem7-build \
-	x86_64-slc5-gcc43-opt	\
-	x86_64-slc5-gcc43-dbg	\
-	i686-slc5-gcc43-opt	\
-	i686-slc5-gcc43-dbg
-
-
-EXPECTED_FAIL exam-ext-electromagnetic-testem8-build \
-	x86_64-slc5-gcc43-opt	\
-	x86_64-slc5-gcc43-dbg	\
-	i686-slc5-gcc43-opt	\
-	i686-slc5-gcc43-dbg
 	
 EXPECTED_FAIL exam-ext-g3tog4-clgeometry-build \
 	osx105_ia32_gcc401	\
@@ -28,6 +15,12 @@ EXPECTED_FAIL exam-ext-g3tog4-cltog4-build \
 	osx105_ia32_gcc401	\
 	osx105_ia32_gcc401_dbg
 
+#  XercesC compile warnings ... fixed using XercesC 3.1 for slc5, 26 Feb 2010
+#EXPECTED_FAIL exam-ext-persistency-gdml-g01-build 
+#EXPECTED_FAIL exam-ext-persistency-gdml-g02-build 
+#EXPECTED_FAIL exam-ext-persistency-gdml-g03-build 
+#EXPECTED_FAIL bench-calo-FullCMS-build \
+
 #9.2.ref05
 #EXPECTED_FAIL test19-build \
 #	x86_64-slc5-gcc43-opt	\
@@ -36,9 +29,9 @@ EXPECTED_FAIL exam-ext-g3tog4-cltog4-build \
 #	i686-slc5-gcc43-dbg
 		
 #28-Apr
-# 3-Nov back for slc4 64 bit opt, sent mail to Pete in October already 
-EXPECTED_FAIL test28-run \
-	slc4_amd64_gcc34
+# 3-Nov back for slc4 64 bit opt, sent mail to Pete in October already, gone in 9.3 
+#EXPECTED_FAIL test28-run \
+#	slc4_amd64_gcc34
 	
 #	slc4_ia32_gcc34	
 
@@ -79,31 +72,34 @@ EXPECTED_FAIL test28-run \
 #	x86_64-slc5-gcc43-dbg	\
 #	i686-slc5-gcc43-dbg
 	
-#new test August 09
+#new test August 09 
 EXPECTED_FAIL test12-FTFBIC-run \
-       slc4_amd64_gcc34        \
-       slc4_amd64_gcc34_dbg    \
-       slc4_ia32_gcc34         \
        slc4_ia32_gcc34_dbg     \
-       x86_64-slc5-gcc43-opt   \
-       x86_64-slc5-gcc43-dbg   \
-       i686-slc5-gcc43-opt     \
-       i686-slc5-gcc43-dbg     \
-       osx105_ia32_gcc401      \
-       osx105_ia32_gcc401_dbg
+       i686-slc5-gcc43-dbg     
+#       i686-slc5-gcc43-opt     ok 26 Feb 2010  
+
+#    ok 26 Feb 2010
+#EXPECTED_FAIL test12-run \
+#       i686-slc5-gcc43-dbg     
+
+#new test Nov 09, i686-slc5 ok 26 Feb 2010
+EXPECTED_FAIL test12-QGSBIC-run \
+       slc4_ia32_gcc34_dbg     
+#       i686-slc5-gcc43-dbg    
 
 #new test Nov 09
-EXPECTED_FAIL test12-QGSBIC-run \
-       slc4_amd64_gcc34        \
-       slc4_amd64_gcc34_dbg    \
-       slc4_ia32_gcc34         \
-       slc4_ia32_gcc34_dbg     \
-       x86_64-slc5-gcc43-opt   \
-       x86_64-slc5-gcc43-dbg   \
-       i686-slc5-gcc43-opt     \
-       i686-slc5-gcc43-dbg     \
-       osx105_ia32_gcc401      \
-       osx105_ia32_gcc401_dbg
+EXPECTED_FAIL bench-calo-HadCalCMS-run1-LBE \
+	slc4_amd64_gcc34	\
+	slc4_amd64_gcc34_dbg	\
+	slc4_ia32_gcc34		\
+	slc4_ia32_gcc34_dbg	\
+	x86_64-slc5-gcc43-opt	\
+	x86_64-slc5-gcc43-dbg	\
+	x86_64-slc5-gcc41-opt   \
+	x86_64-slc5-gcc41-dbg   \
+	i686-slc5-gcc43-opt	\
+	i686-slc5-gcc43-dbg	\
+	osx105_ia32_gcc401_dbg
 
 EoI
 
