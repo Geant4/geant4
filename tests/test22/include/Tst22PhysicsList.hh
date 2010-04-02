730c27e7d6eba462052731d5a9c2d3e75d09bf41
Dennis Herbert Wright <dennis@cern.ch>
2010-04-02 04:21:04 +0000

    remove G4MultipleScattering header file
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@42833 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- tests/test22/include/Tst22PhysicsList.hh
+++ tests/test22/include/Tst22PhysicsList.hh
@@ -58,8 +58,6 @@
 #include "G4GammaConversion.hh"
 #include "G4PhotoElectricEffect.hh"
 
-#include "G4MultipleScattering.hh"
-
 #include "G4eIonisation.hh"
 #include "G4eBremsstrahlung.hh"
 #include "G4eplusAnnihilation.hh"
