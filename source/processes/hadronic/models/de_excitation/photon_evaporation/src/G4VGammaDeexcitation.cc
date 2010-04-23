bd8360a78e64a0c14b6c70879abfa7cb413819e3
Vladimir Ivantchenko <Vladimir.Ivantchenko@cern.ch>
2010-04-23 11:07:36 +0000

    rewritten kinematics of 2-body decay
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@43111 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/models/de_excitation/photon_evaporation/src/G4VGammaDeexcitation.cc
+++ source/processes/hadronic/models/de_excitation/photon_evaporation/src/G4VGammaDeexcitation.cc
@@ -47,6 +47,9 @@
 //        19 April 2010, J. M. Quesada calculations in CM system
 //              pending final boost to lab system  (not critical)
 //
+//        23 April 2010, V.Ivanchenko rewite kinematic part using PDG formula
+//                                    for 2-body decay
+//
 // -------------------------------------------------------------------
 
 #include "G4VGammaDeexcitation.hh"
@@ -146,128 +149,70 @@ void G4VGammaDeexcitation::SetNucleus(const G4Fragment& nucleus)
 
 G4Fragment* G4VGammaDeexcitation::GenerateGamma()
 {
+  // 23/04/10 V.Ivanchenko rewrite complitely
   G4double eGamma = 0.;
   
   if (_transition != 0) {
     _transition->SelectGamma();  // it can be conversion electron too
     eGamma = _transition->GetGammaEnergy(); 
+    if(eGamma <= 0.0) { return 0; }
   }
+  G4double excitation = _nucleus.GetExcitationEnergy() - eGamma;
+  if(excitation < 0.0) { excitation = 0.0; } 
   if (_verbose > 1 && _transition != 0 ) 
     {
       G4cout << "G4VGammaDeexcitation::GenerateGamma - Gamma energy " << eGamma 
-	     << " ** New excitation " << _nucleus.GetExcitationEnergy() - eGamma
+	     << " ** New excitation " << excitation
 	     << G4endl;
     }
   
-  // Photon momentum isotropically generated
-  // the same for electron 
-  
-  if (eGamma > 0.) 
-    {
-      G4double cosTheta = 1. - 2. * G4UniformRand();
-      G4double sinTheta = std::sqrt(1. - cosTheta * cosTheta);
-      G4double phi = twopi * G4UniformRand();
-      G4double pM = eGamma;
-      G4DiscreteGammaTransition* dtransition = 0; 
-      dtransition = dynamic_cast <G4DiscreteGammaTransition*> (_transition);
-      if ( dtransition && !( dtransition->IsAGamma()) ) {
-	G4double eMass = G4Electron::ElectronDefinition()->GetPDGMass();
-	pM =std::sqrt(eGamma*(eGamma + 2.0*eMass));
-	eGamma = eGamma + eMass;
-      }
-      G4ThreeVector pGamma( pM * sinTheta * std::cos(phi),
-			    pM * sinTheta * std::sin(phi),
-			    pM * cosTheta );
-      G4LorentzVector gamma(pGamma, eGamma);
-      //      	gamma.boost(_nucleus.GetMomentum().boostVector() );
-      G4Fragment* gammaFragment ;
-      if ( dtransition && !(dtransition->IsAGamma()) ){
-	gammaFragment = new G4Fragment(gamma,G4Electron::ElectronDefinition());
-      } else {
-	gammaFragment = new G4Fragment(gamma,G4Gamma::GammaDefinition()); 
-      }
-      G4double gammaTime = _transition->GetGammaCreationTime();
-      gammaTime += _nucleus.GetCreationTime();
-      gammaFragment->SetCreationTime(gammaTime);
-      
-      if (_verbose > 1 && dtransition )
-	G4cout << "G4VGammaDeexcitation::GenerateGamma -  Gamma fragment generated:  " 
-	       << (dtransition->IsAGamma() ? " Gamma" : " Electron" ) << G4endl;
-      return gammaFragment;
-    }
-  else
-    {
-      return 0;
-    }
+  // Do complete Lorentz computation 
+
+  G4LorentzVector lv = _nucleus.GetMomentum();
+  G4double Ecm       = lv.m();
+  G4ThreeVector bst  = lv.boostVector();
+  G4double Mass = _nucleus.GetGroundStateMass() + excitation;
+
+  // select secondary
+  G4ParticleDefinition* gamma = G4Gamma::Gamma();
+
+  G4DiscreteGammaTransition* dtransition = 0; 
+  dtransition = dynamic_cast <G4DiscreteGammaTransition*> (_transition);
+  if ( dtransition && !( dtransition->IsAGamma()) ) { gamma = G4Electron::Electron(); }
+
+  // check consistency  
+  G4double eMass = gamma->GetPDGMass();
+  if(Ecm - Mass <= eMass) { return 0; }
+
+  G4double GammaEnergy = 0.5*((Ecm - Mass)*(Ecm + Mass) + eMass*eMass)/Ecm;
+  G4double cosTheta = 1. - 2. * G4UniformRand(); 
+  G4double sinTheta = std::sqrt(1. - cosTheta * cosTheta);
+  G4double phi = twopi * G4UniformRand();
+  G4double mom = sqrt((GammaEnergy - eMass)*(GammaEnergy + eMass));
+  G4LorentzVector Gamma4P(mom * sinTheta * std::cos(phi),
+			  mom * sinTheta * std::sin(phi),
+			  mom * cosTheta,
+			  GammaEnergy);
+  Gamma4P.boost(bst);  
+  G4Fragment * thePhoton = new G4Fragment(Gamma4P,gamma);
+
+  G4double gammaTime = _nucleus.GetCreationTime() + _transition->GetGammaCreationTime();
+  thePhoton->SetCreationTime(gammaTime);
+
+  return thePhoton;
 }
 
 void G4VGammaDeexcitation::UpdateNucleus(const G4Fragment*  gamma)
 {
-  G4LorentzVector p4Gamma = gamma->GetMomentum();
-  G4ThreeVector pGamma(p4Gamma.vect());
-  
-  G4double eGamma = 0.;
-  if (_transition != 0)
-    eGamma = _transition->GetGammaEnergy();
-  G4DiscreteGammaTransition* dtransition = 0; 
-  dtransition = dynamic_cast <G4DiscreteGammaTransition*> (_transition);
-  if (dtransition && !(dtransition->IsAGamma()) )      
-    eGamma += dtransition->GetBondEnergy(); 
-  
-  G4LorentzVector p4Nucleus(_nucleus.GetMomentum() );
-  
-  // New tetravector calculation:
-  
-  //JMQ : for the sake of consistency, mass is calculated from G4G4ParticleTable (as in evaporation and exciton models)
-  //  G4double Mass = G4ParticleTable::GetParticleTable()->GetIonTable()->GetIonMass(_nucleus.GetZ(),_nucleus.GetA());
-  //  G4double m1 = G4ParticleTable::GetParticleTable()->GetIonTable()->GetIonMass(static_cast<G4int>(_nucleus.GetZ()),
-  //									       static_cast<G4int>(_nucleus.GetA()));
-  //  G4double m2 = _nucleus.GetZ() *  G4Proton::Proton()->GetPDGMass() + 
-  //    (_nucleus.GetA()- _nucleus.GetZ())*G4Neutron::Neutron()->GetPDGMass();
-  
-  //  G4double Mass = std::min(m1,m2);
-  G4double Mass = G4ParticleTable::GetParticleTable()->GetIonTable()->GetIonMass(static_cast<G4int>(_nucleus.GetZ()),
-										 static_cast<G4int>(_nucleus.GetA()));
-  
-  
-  
-  G4double newExcitation = p4Nucleus.mag() - Mass - eGamma;
-  if(newExcitation < 0)
-    newExcitation = 0;
-  
-  
-  
-  //JMQ: calculations have been reshaped for full energy-momentum conservation
-  //  Calculations must be done al CM system and later on 4-momenta must be boosted to lab system
-  
-  //  G4ThreeVector p3Residual(p4Nucleus.vect() - pGamma);
-  G4ThreeVector p3Residual(- pGamma);
-  
-  // Now the residual nucleus. 
-  // The energy conservation says that 
-  G4double ResidualEcm = p4Nucleus.m()-eGamma;
-  
-  
-  
-  //JMQ: equivalent, but more compact 
-  //  G4double newEnergy = std::sqrt(p3Residual * p3Residual +
-  //			    (Mass + newExcitation) * (Mass + newExcitation));
-  //  G4LorentzVector p4Residual(p3Residual, newEnergy);
-  G4LorentzVector p4Residual(p3Residual, ResidualEcm);
-  
-  // Update excited nucleus parameters
-  
-  _nucleus.SetMomentum(p4Residual);
-  
+  // 23/04/10 V.Ivanchenko rewrite complitely
+  if( !gamma ) { return; }
+  G4LorentzVector lv = _nucleus.GetMomentum() - gamma->GetMomentum();
+  //  G4double newExcitation = lv.mag() - _nucleus.GetGroundStateMass();
+  //if(newExcitation < 0.0) { newExcitation = 0.0; }
+
+  _nucleus.SetMomentum(lv);
   _nucleus.SetCreationTime(gamma->GetCreationTime());
-  
-  //  if (_transition != 0) 
-  //  {
-  //    G4double excitation =_transition->GetEnergyTo();
-  //    if (excitation < 0.) excitation = 0.0;
-  //    _nucleus.SetExcitationEnergy(excitation);
-  //  }
-  
+
   return;
 }
 
