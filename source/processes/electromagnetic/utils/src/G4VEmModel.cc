53b64962636b74e6e6782fb48cfdd0c10e250fdf
Vladimir Ivantchenko <Vladimir.Ivantchenko@cern.ch>
2010-05-26 17:07:00 +0000

    reduce number of bins
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@43634 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/electromagnetic/utils/src/G4VEmModel.cc
+++ source/processes/electromagnetic/utils/src/G4VEmModel.cc
@@ -23,7 +23,7 @@
 // * acceptance of all terms of the Geant4 Software license.          *
 // ********************************************************************
 //
-// $Id: G4VEmModel.cc,v 1.33 2010-05-26 10:41:34 vnivanch Exp $
+// $Id: G4VEmModel.cc,v 1.34 2010-05-26 17:07:00 vnivanch Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // -------------------------------------------------------------------
@@ -126,7 +126,7 @@ void G4VEmModel::InitialiseElementSelectors(const G4ParticleDefinition* p,
   // two times less bins because probability functon is normalized 
   // so correspondingly is more smooth
   G4int nbins = 
-    man->GetNumberOfBinsPerDecade()*G4int(std::log10(highLimit/lowLimit))/2;
+    man->GetNumberOfBinsPerDecade()*G4int(std::log10(highLimit/lowLimit))/3;
   if(nbins < 5) { nbins = 5; }
 
   G4ProductionCutsTable* theCoupleTable=
