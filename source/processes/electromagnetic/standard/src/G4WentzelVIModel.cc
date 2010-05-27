c27b721836d7833c04f6ac5aa8b446ddcc35e90b
Vladimir Ivantchenko <Vladimir.Ivantchenko@cern.ch>
2010-05-27 17:42:32 +0000

    fixed numerical problem
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@43669 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/electromagnetic/standard/src/G4WentzelVIModel.cc
+++ source/processes/electromagnetic/standard/src/G4WentzelVIModel.cc
@@ -23,7 +23,7 @@
 // * acceptance of all terms of the Geant4 Software license.          *
 // ********************************************************************
 //
-// $Id: G4WentzelVIModel.cc,v 1.49 2010-05-27 14:22:05 vnivanch Exp $
+// $Id: G4WentzelVIModel.cc,v 1.50 2010-05-27 17:42:32 vnivanch Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // -------------------------------------------------------------------
@@ -215,7 +215,10 @@ G4double G4WentzelVIModel::ComputeTruePathLengthLimit(
    
   // cut correction
   G4double rcut = currentCouple->GetProductionCuts()->GetProductionCut(1);
-  if(rcut > rlimit) { rlimit = fG4pow->A13(rcut*rcut*rlimit); }
+  G4cout << "rcut= " << rcut << " rlimit= " << rlimit << " presafety= " << presafety 
+	 << " 1-cosThetaMax= " <<1-cosThetaMax << " 1-cosTetMaxNuc= " << 1-cosTetMaxNuc
+	 << G4endl;
+  if(rcut > rlimit) { rlimit = rcut*fG4pow->A13(rlimit/rcut); }
 
   if(rlimit < tlimit) { tlimit = rlimit; }
 
