88b5081b3f52dd624cf1d788409fceea309615eb
Pablo Cirrone <Pablo.Cirrone@cern.ch>
2010-06-03 23:29:36 +0000

    PhysicsList migrated
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@43938 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/advanced/human_phantom/include/G4HumanPhantomPhysicsList.hh
+++ examples/advanced/human_phantom/include/G4HumanPhantomPhysicsList.hh
@@ -23,14 +23,18 @@
 // * acceptance of all terms of the Geant4 Software license.          *
 // ********************************************************************
 //
-// Authors: S. Guatelli and M. G. Pia, INFN Genova, Italy
-// 
-// Based on code developed by the undergraduate student G. Guerrieri 
-// Note: this is a preliminary beta-version of the code; an improved 
-// version will be distributed in the next Geant4 public release, compliant
-// with the design in a forthcoming publication, and subject to a 
-// design and code review.
 //
+// $Id: G4HumanPhantomPhysicsList.hh,v 1.6 2010-06-03 23:29:36 cirrone Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+// Code review: MGP, 5 November 2006 (still to be completed)
+//
+//    **********************************
+//    *                                *
+//    *      G4HumanPhantomPhysicsList.hh      *
+//    *                                *
+//    **********************************
+
 #ifndef G4HumanPhantomPhysicsList_h
 #define G4HumanPhantomPhysicsList_h 1
 
@@ -53,9 +57,11 @@ protected:
   void SetCuts();
 
 private:
+
   // These methods construct physics processes and register them
   void ConstructEM();
 };
+
 #endif
 
 
