d99d1e20b0083b42da5ecf2b3e11e5febe30db15
Gunter Folger <Gunter.Folger@cern.ch>
2010-06-04 16:05:50 +0000

    removed lists
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@43998 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/physics_lists/test/compile.cc
+++ source/physics_lists/test/compile.cc
@@ -25,44 +25,45 @@
 //
 #include "G4RunManager.hh" 
 
-#include "FTFC.hh"
-#include "FTFP.hh"
+//9.4beta #include "FTFC.hh"
+//9.4beta #include "FTFP.hh"
 #include "FTFP_BERT.hh"
 #include "FTFP_BERT_TRV.hh"
 
 #include "LBE.hh"
-#include "LHEP_BERT_HP.hh"
-#include "LHEP_BERT.hh"
+//9.4beta #include "LHEP_BERT_HP.hh"
+//9.4beta #include "LHEP_BERT.hh"
 #include "LHEP_EMV.hh"
 #include "LHEP.hh"
-#include "LHEP_PRECO_HP.hh"
+//#include "LHEP_PRECO_HP.hh"
 
 #include "QBBC.hh"
 
-#include "QGSC_EMV.hh"
-#include "QGSC.hh"
-#include "QGSC_EFLOW.hh"
+//#include "QGSC_EMV.hh"
+//#include "QGSC.hh"
+//#include "QGSC_EFLOW.hh"
 #include "QGSC_BERT.hh"
-#include "QGSC_QGSC.hh"
+//#include "QGSC_QGSC.hh"
 #include "QGSC_CHIPS.hh"
 #include "CHIPS.hh"
 
 #include "QGSP.hh"
-#include "QGSP_DIF.hh"
+//#include "QGSP_DIF.hh"
 #include "QGSP_BERT_HP.hh"
 #include "QGSP_BERT.hh"
 #include "QGSP_BERT_EMX.hh"
-#include "QGSP_BERT_DIF.hh"
+//#include "QGSP_BERT_DIF.hh"
 #include "QGSP_BIC_HP.hh"
 #include "QGSP_BIC.hh"
 #include "G4IonQMDPhysics.hh"
-#include "QGSP_EMV.hh"
-#include "QGSP_EMX.hh"
-#include "QGSP_QEL.hh"
+//#include "QGSP_EMV.hh"
+//#include "QGSP_EMX.hh"
+//#include "QGSP_QEL.hh"
 //#include "QGSP_CASC.hh"
 #include "QGSP_INCL_ABLA.hh"
 
 #include "QGSP_FTFP_BERT.hh"
+#include "QGSP_BERT_CHIPS.hh"
 
 #include "QGS_BIC.hh"
 #include "FTF_BIC.hh"
@@ -76,43 +77,44 @@ int main(int argc,char** argv) {
   G4RunManager* runManager = new G4RunManager; 
 //  runManager->SetUserInitialization( new StatAccepTestDetectorConstruction ); 
 
-  G4VModularPhysicsList * theFTF1 = new FTFC; 
-  G4VModularPhysicsList * theFTF2 = new FTFP;
+//9.4beta   G4VModularPhysicsList * theFTF1 = new FTFC; 
+//9.4beta   G4VModularPhysicsList * theFTF2 = new FTFP;
   G4VModularPhysicsList * theFTF3 = new FTFP_BERT;
   G4VModularPhysicsList * theFTF3a = new FTFP_BERT_TRV;
 
   G4VModularPhysicsList *theLBE = new LBE; 
 
-  G4VModularPhysicsList *thePL4 = new LHEP_BERT_HP; 
-  G4VModularPhysicsList *thePL5 = new LHEP_BERT; 
+//9.4beta   G4VModularPhysicsList *thePL4 = new LHEP_BERT_HP; 
+//9.4beta   G4VModularPhysicsList *thePL5 = new LHEP_BERT; 
   G4VModularPhysicsList *thePL8 = new LHEP_EMV; 
   G4VModularPhysicsList *thePL10 = new LHEP; 
 
   G4VModularPhysicsList *thePL15 = new QBBC;
 
-  G4VModularPhysicsList *thePL16 = new QGSC_EMV;
-  G4VModularPhysicsList *thePL17 = new QGSC;
+//9.4beta   G4VModularPhysicsList *thePL16 = new QGSC_EMV;
+//9.4beta   G4VModularPhysicsList *thePL17 = new QGSC;
 //replaced  G4VModularPhysicsList *thePL17a = new QGSC_EFLOW;
   G4VModularPhysicsList *thePL18 = new QGSC_BERT;
-  G4VModularPhysicsList *thePL18a = new QGSC_QGSC;
+//9.4beta   G4VModularPhysicsList *thePL18a = new QGSC_QGSC;
   G4VModularPhysicsList *thePL18b = new QGSC_CHIPS;
   G4VModularPhysicsList *thePL19 = new CHIPS;
     
   G4VModularPhysicsList *thePL20 = new QGSP_BERT_HP; 
   G4VModularPhysicsList *thePL21 = new QGSP_BERT;
   G4VModularPhysicsList *thePL21a = new QGSP_BERT_EMX;
-  G4VModularPhysicsList *thePL21b = new QGSP_BERT_DIF;
+  G4VModularPhysicsList *thePL21b = new QGSP_BERT_CHIPS;
+//9.4beta   G4VModularPhysicsList *thePL21b = new QGSP_BERT_DIF;
   G4VModularPhysicsList *thePL22 = new QGSP_BIC_HP; 
   G4VModularPhysicsList *thePL23 = new QGSP_BIC;
   G4VModularPhysicsList *thePL23a= new QGSP_BIC(1);
       G4IonQMDPhysics * ionQMD=new G4IonQMDPhysics("IonQMD",1);
       thePL23a->RegisterPhysics(ionQMD);
       ionQMD->ConstructProcess();
-  G4VModularPhysicsList *thePL24 = new QGSP_EMV;
+//9.4beta   G4VModularPhysicsList *thePL24 = new QGSP_EMV;
 //replaced  G4VModularPhysicsList *thePL25 = new QGSP_EMX;
   G4VModularPhysicsList *thePL27 = new QGSP;
-  G4VModularPhysicsList *thePL27a = new QGSP_DIF;
-  G4VModularPhysicsList *thePL28 = new QGSP_QEL;
+//9.4beta   G4VModularPhysicsList *thePL27a = new QGSP_DIF;
+//9.4beta   G4VModularPhysicsList *thePL28 = new QGSP_QEL;
 //  G4VModularPhysicsList *thePL29 = new QGSP_CASC;
   G4VModularPhysicsList *thePL29 = new QGSP_INCL_ABLA;
   
