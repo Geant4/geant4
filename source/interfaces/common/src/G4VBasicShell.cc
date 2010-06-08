4372994e967b5582b209f307eb453a29b0c25437
Koichi Murakami <koichi.murakami@kek.jp>
2010-06-08 04:39:22 +0000

    back to 1.14 version including correct bug fix
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@44175 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/interfaces/common/src/G4VBasicShell.cc
+++ source/interfaces/common/src/G4VBasicShell.cc
@@ -24,7 +24,7 @@
 // ********************************************************************
 //
 //
-// $Id: G4VBasicShell.cc,v 1.15 2010-01-06 14:04:13 lgarnier Exp $
+// $Id: G4VBasicShell.cc,v 1.16 2010-06-08 04:39:22 kmura Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 
@@ -243,11 +243,11 @@ void G4VBasicShell::ApplyShellCommand (
     G4cout << "Current Working Directory : " 
        << GetCurrentWorkingDirectory() << G4endl; 
 
-  } else if( command(0,2) == "cd" ) { 
+  } else if( command == "cd" || command(0,3) == "cd ") {
 
     ChangeDirectoryCommand ( command );
 
-  } else if( command(0,4) == "help" ) { 
+  } else if( command == "help" || command(0,5) == "help ") {
 
     TerminalHelp( command ); 
 
@@ -255,7 +255,7 @@ void G4VBasicShell::ApplyShellCommand (
 
     ShowCurrent( command );
 
-  } else if( command(0,4) == "hist" ) {
+  } else if( command == "hist" || command == "history") {
 
     G4int nh = UI->GetNumberOfHistory();
     for(G4int i=0;i<nh;i++) { 
@@ -278,7 +278,7 @@ void G4VBasicShell::ApplyShellCommand (
       G4cerr << "history " << vl << " is not found." << G4endl; 
     }
 
-  } else if( command(0,4) == "exit" ) { 
+  } else if( command == "exit" ) { 
 
     if( exitPause == false) { //In a secondary loop.
       G4cout << "You are now processing RUN." << G4endl;
@@ -289,7 +289,7 @@ void G4VBasicShell::ApplyShellCommand (
       exitSession = true;
     }
 
-  } else if( command(0,4) == "cont" ) { 
+  } else if( command == "cont" || command == "continue"){
 
     exitPause = true;
 
