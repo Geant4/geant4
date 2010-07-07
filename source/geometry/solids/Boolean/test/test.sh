52880b8eab96dc35c3dadc0112ea01853b15d731
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2010-07-07 15:50:02 +0000

    Use make instead of gmake ...
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@44607 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/geometry/solids/Boolean/test/test.sh
+++ source/geometry/solids/Boolean/test/test.sh
@@ -4,7 +4,7 @@ echo `date` >> test.out
 for i in *.cc
 do
   j=`basename $i .cc`
-  gmake G4TARGET=$j
+  make G4TARGET=$j
   echo Test output for $j... >> test.out
   $G4WORKDIR/bin/$G4SYSTEM/$j >> test.out 2>&1;
 done
