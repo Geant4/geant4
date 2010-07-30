b3dd3a7a5081362364e06d3cc5450fb69b0f0f9d
Vladimir Ivantchenko <Vladimir.Ivantchenko@cern.ch>
2010-07-30 18:35:49 +0000

    removed dublicated lines
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@44817 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/electromagnetic/utils/src/G4EmCalculator.cc
+++ source/processes/electromagnetic/utils/src/G4EmCalculator.cc
@@ -23,7 +23,7 @@
 // * acceptance of all terms of the Geant4 Software license.          *
 // ********************************************************************
 //
-// $Id: G4EmCalculator.cc,v 1.53 2010-04-13 10:58:03 vnivanch Exp $
+// $Id: G4EmCalculator.cc,v 1.54 2010-07-30 18:35:49 vnivanch Exp $
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
 // -------------------------------------------------------------------
@@ -791,8 +791,8 @@ G4bool G4EmCalculator::UpdateParticle(const G4ParticleDefinition* p,
 	 && currentParticleName != "hydrogen"
 	 ) {
 	isIon = true;
-	massRatio = theGenericIon->GetPDGMass()/p->GetPDGMass();
-        baseParticle = theGenericIon;
+	//massRatio = theGenericIon->GetPDGMass()/p->GetPDGMass();
+        //baseParticle = theGenericIon;
 	//      G4cout << p->GetParticleName()
 	// << " in " << currentMaterial->GetName()
 	//       << "  e= " << kinEnergy << G4endl;
