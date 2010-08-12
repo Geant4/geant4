81f9a27c8b21b3e367e23c6bac286be2d85a4d3f
Geant4 Testing <geant4.testing@cern.ch>
2010-08-12 08:47:40 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@44854 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- tests/tools/bin/expected_fail.sh
+++ tests/tools/bin/expected_fail.sh
@@ -6,20 +6,14 @@ Random_FAIL exam-ext-field-field04-run \
 	slc4_amd64_gcc34_dbg	\
 	i686-slc5-gcc43-opt
 	
-# changed cmt requirements file to compile g4tog4 on mac as well --> works
-#EXPECTED_FAIL exam-ext-g3tog4-clgeometry-build \
-#	osx105_ia32_gcc401	\
-#	osx105_ia32_gcc401_dbg
-#			
-#EXPECTED_FAIL exam-ext-g3tog4-cltog4-build \
-#	osx105_ia32_gcc401	\
-#	osx105_ia32_gcc401_dbg
+EXPECTED_FAIL exam-ext-g3tog4-clgeometry-run \
+       osx106_ia32_gcc401      \
+       osx106_ia32_gcc401_dbg
+		       
+EXPECTED_FAIL exam-ext-g3tog4-cltog4-run  \
+       osx106_ia32_gcc401      \
+       osx106_ia32_gcc401_dbg
 
-#  XercesC compile warnings ... fixed using XercesC 3.1 for slc5, 26 Feb 2010
-#EXPECTED_FAIL exam-ext-persistency-gdml-g01-build 
-#EXPECTED_FAIL exam-ext-persistency-gdml-g02-build 
-#EXPECTED_FAIL exam-ext-persistency-gdml-g03-build 
-#EXPECTED_FAIL bench-calo-FullCMS-build \
 
 #9.2.ref05
 #EXPECTED_FAIL test19-build \
@@ -35,8 +29,10 @@ Random_FAIL exam-ext-field-field04-run \
 	
 #	slc4_ia32_gcc34	
 
-#28 Apr
-#EXPECTED_FAIL test29-run \
+#28 Apr back Jul 10 on slc4_ia32_gcc34
+EXPECTED_FAIL test29-run \
+	slc4_ia32_gcc34
+
 #	slc4_amd64_gcc34_dbg	\
 #	slc4_ia32_gcc34_dbg	\
 #	i686-slc5-gcc43-dbg
@@ -72,19 +68,19 @@ Random_FAIL exam-ext-field-field04-run \
 #	x86_64-slc5-gcc43-dbg	\
 #	i686-slc5-gcc43-dbg
 	
-#new test August 09 
-EXPECTED_FAIL test12-FTFBIC-run \
-       slc4_ia32_gcc34_dbg     \
-       i686-slc5-gcc43-dbg     
+#new test August 09 ; was timelimit fixed Jul 10
+#EXPECTED_FAIL test12-FTFBIC-run \
+#       slc4_ia32_gcc34_dbg     \
+#       i686-slc5-gcc43-dbg     
 #       i686-slc5-gcc43-opt     ok 26 Feb 2010  
 
 #    ok 26 Feb 2010
 #EXPECTED_FAIL test12-run \
 #       i686-slc5-gcc43-dbg     
 
-#new test Nov 09, i686-slc5 ok 26 Feb 2010
-EXPECTED_FAIL test12-QGSBIC-run \
-       slc4_ia32_gcc34_dbg     
+#new test Nov 09, i686-slc5 ok 26 Feb 2010, all ok Jul 2010
+#EXPECTED_FAIL test12-QGSBIC-run \
+#       slc4_ia32_gcc34_dbg     
 #       i686-slc5-gcc43-dbg    
 
 #new test Nov 09, removed from testing
@@ -101,6 +97,52 @@ EXPECTED_FAIL test12-QGSBIC-run \
 #	i686-slc5-gcc43-dbg	\
 #	osx105_ia32_gcc401_dbg  \
 #	x86_64-mac106-gcc42-dbg
+
+#new test Jul 2010, fail to run so far....
+EXPECTED_FAIL exam-ext-radioactivedecay-radioActiv-run \
+	slc4_amd64_gcc34	\
+	slc4_amd64_gcc34_dbg	\
+	slc4_ia32_gcc34		\
+	slc4_ia32_gcc34_dbg	\
+	i686-slc5-gcc43-opt	\
+	i686-slc5-gcc43-dbg	\
+	x86_64-slc5-gcc43-opt	\
+	x86_64-slc5-gcc43-dbg
+
+#newly in  testing Jul 2010, fail to run for graphics so far....
+EXPECTED_FAIL test201-run \
+	slc4_amd64_gcc34	\
+	slc4_amd64_gcc34_dbg	\
+	slc4_ia32_gcc34		\
+	slc4_ia32_gcc34_dbg	\
+	i686-slc5-gcc43-opt	\
+	i686-slc5-gcc43-dbg	\
+	x86_64-slc5-gcc43-opt	\
+	x86_64-slc5-gcc43-dbg	\
+	x86_64-mac106-gcc42-opt	\
+	x86_64-mac106-gcc42-dbg	\
+	i686-winxp-vc9-opt	\
+	i686-winxp-vc9-dbg
+	
+#newly in  testing Jul 2010, fail to run for graphics so far....
+EXPECTED_FAIL test202-build \
+	i686-winxp-vc9-opt	\
+	i686-winxp-vc9-dbg
+	
+#newly in  testing Jul 2010, fail to run for graphics so far....
+EXPECTED_FAIL test202-run \
+	slc4_amd64_gcc34	\
+	slc4_amd64_gcc34_dbg	\
+	slc4_ia32_gcc34		\
+	slc4_ia32_gcc34_dbg	\
+	i686-slc5-gcc43-opt		\
+	i686-slc5-gcc43-dbg	\
+	x86_64-slc5-gcc43-opt	\
+	x86_64-slc5-gcc43-dbg	\
+	x86_64-mac106-gcc42-opt	\
+	x86_64-mac106-gcc42-dbg
+	
+	
 	
 EoI
 
@@ -112,10 +154,10 @@ slc4_amd64_gcc34_dbg
 slc4_ia32_gcc34
 slc4_ia32_gcc34_dbg
 
-i686-slc5-gcc43
+i686-slc5-gcc43-opt
 i686-slc5-gcc43-dbg
 
-x86_64-slc5-gcc43
+x86_64-slc5-gcc43-opt
 x86_64-slc5-gcc43-dbg
 
 osx105_ia32_gcc401
