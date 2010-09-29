7ba1f9f225ad875894b60cac7780a3d1a46ed55f
Benjamin Morgan <Ben.Morgan@warwick.ac.uk>
2010-09-29 18:44:01 +0000

    Initial import of CMake buildfile source/global/HEPGeometry/sources.cmake
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@45547 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/global/HEPGeometry/sources.cmake
@@ -0,0 +1,46 @@
+#------------------------------------------------------------------------------
+# sources.cmake
+# Module : G4hepgeometry
+# Package: Geant4.src.G4global.G4hepgeometry
+#
+# Sources description for a library.
+# Lists the sources and headers of the code explicitely.
+# Lists include paths needed.
+# Lists the internal granular and global dependencies of the library.
+# Source specific properties should be added at the end.
+#
+# Generated on : 24/9/2010
+#
+# $Id: sources.cmake,v 1.1 2010-09-29 18:44:01 bmorgan Exp $
+#
+#------------------------------------------------------------------------------
+
+# List external includes needed.
+include_directories(${CLHEP_INCLUDE_DIRS})
+
+# List internal includes needed.
+include_directories(${CMAKE_SOURCE_DIR}/source/global/management/include)
+
+#
+# Define the Geant4 Module.
+#
+include(Geant4MacroDefineModule)
+GEANT4_DEFINE_MODULE(NAME G4hepgeometry
+    HEADERS
+        G4LorentzRotation.hh
+        G4LorentzVector.hh
+        G4Normal3D.hh
+        G4Plane3D.hh
+        G4Point3D.hh
+        G4Transform3D.hh
+        G4Vector3D.hh
+        geomdefs.hh
+    SOURCES
+    GRANULAR_DEPENDENCIES
+        G4globman
+    GLOBAL_DEPENDENCIES
+    LINK_LIBRARIES
+)
+
+# List any source specific properties here
+
