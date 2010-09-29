c230f9bf5f815d2fafc485bba815256ec1718791
Benjamin Morgan <Ben.Morgan@warwick.ac.uk>
2010-09-29 18:38:24 +0000

    Initial import of CMake buildfile source/digits_hits/digits/sources.cmake
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@45509 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/digits_hits/digits/sources.cmake
@@ -0,0 +1,49 @@
+#------------------------------------------------------------------------------
+# sources.cmake
+# Module : G4digits
+# Package: Geant4.src.G4digits_hits.G4digits
+#
+# Sources description for a library.
+# Lists the sources and headers of the code explicitely.
+# Lists include paths needed.
+# Lists the internal granular and global dependencies of the library.
+# Source specific properties should be added at the end.
+#
+# Generated on : 24/9/2010
+#
+# $Id: sources.cmake,v 1.1 2010-09-29 18:38:24 bmorgan Exp $
+#
+#------------------------------------------------------------------------------
+
+# List external includes needed.
+include_directories(${CLHEP_INCLUDE_DIRS})
+
+# List internal includes needed.
+include_directories(${CMAKE_SOURCE_DIR}/source/global/HEPGeometry/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/global/HEPRandom/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/global/management/include)
+
+#
+# Define the Geant4 Module.
+#
+include(Geant4MacroDefineModule)
+GEANT4_DEFINE_MODULE(NAME G4digits
+    HEADERS
+        G4DCofThisEvent.hh
+        G4TDigiCollection.hh
+        G4VDigi.hh
+        G4VDigiCollection.hh
+    SOURCES
+        G4DCofThisEvent.cc
+        G4TDigiCollection.cc
+        G4VDigi.cc
+        G4VDigiCollection.cc
+    GRANULAR_DEPENDENCIES
+        G4globman
+    GLOBAL_DEPENDENCIES
+        G4global
+    LINK_LIBRARIES
+)
+
+# List any source specific properties here
+
