8b1d1f74d839368945af65e92d67cb8d77462e87
Benjamin Morgan <Ben.Morgan@warwick.ac.uk>
2010-09-29 18:38:41 +0000

    Initial import of CMake buildfile source/digits_hits/hits/sources.cmake
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@45511 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/digits_hits/hits/sources.cmake
@@ -0,0 +1,50 @@
+#------------------------------------------------------------------------------
+# sources.cmake
+# Module : G4hits
+# Package: Geant4.src.G4digits_hits.G4hits
+#
+# Sources description for a library.
+# Lists the sources and headers of the code explicitely.
+# Lists include paths needed.
+# Lists the internal granular and global dependencies of the library.
+# Source specific properties should be added at the end.
+#
+# Generated on : 24/9/2010
+#
+# $Id: sources.cmake,v 1.1 2010-09-29 18:38:41 bmorgan Exp $
+#
+#------------------------------------------------------------------------------
+
+# List external includes needed.
+include_directories(${CLHEP_INCLUDE_DIRS})
+
+# List internal includes needed.
+include_directories(${CMAKE_SOURCE_DIR}/source/global/HEPGeometry/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/global/HEPRandom/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/global/management/include)
+
+#
+# Define the Geant4 Module.
+#
+include(Geant4MacroDefineModule)
+GEANT4_DEFINE_MODULE(NAME G4hits
+    HEADERS
+        G4HCofThisEvent.hh
+        G4THitsCollection.hh
+        G4THitsMap.hh
+        G4VHit.hh
+        G4VHitsCollection.hh
+    SOURCES
+        G4HCofThisEvent.cc
+        G4THitsCollection.cc
+        G4VHit.cc
+        G4VHitsCollection.cc
+    GRANULAR_DEPENDENCIES
+        G4globman
+    GLOBAL_DEPENDENCIES
+        G4global
+    LINK_LIBRARIES
+)
+
+# List any source specific properties here
+
