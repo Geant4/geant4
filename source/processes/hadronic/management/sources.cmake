52dd7c7b1829b2ddbf349fa1b922daacd3374d2c
Benjamin Morgan <Ben.Morgan@warwick.ac.uk>
2010-09-29 18:55:20 +0000

    Initial import of CMake buildfile source/processes/hadronic/management/sources.cmake
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@45630 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/processes/hadronic/management/sources.cmake
@@ -0,0 +1,96 @@
+#------------------------------------------------------------------------------
+# sources.cmake
+# Module : G4hadronic_mgt
+# Package: Geant4.src.G4processes.G4hadronic.G4hadronic_mgt
+#
+# Sources description for a library.
+# Lists the sources and headers of the code explicitely.
+# Lists include paths needed.
+# Lists the internal granular and global dependencies of the library.
+# Source specific properties should be added at the end.
+#
+# Generated on : 24/9/2010
+#
+# $Id: sources.cmake,v 1.1 2010-09-29 18:55:20 bmorgan Exp $
+#
+#------------------------------------------------------------------------------
+
+# List external includes needed.
+include_directories(${CLHEP_INCLUDE_DIRS})
+
+# List internal includes needed.
+include_directories(${CMAKE_SOURCE_DIR}/source/geometry/management/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/geometry/navigation/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/geometry/volumes/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/global/HEPGeometry/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/global/HEPRandom/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/global/management/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/intercoms/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/materials/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/particles/bosons/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/particles/hadrons/barions/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/particles/hadrons/ions/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/particles/hadrons/mesons/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/particles/leptons/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/particles/management/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/processes/hadronic/cross_sections/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/processes/hadronic/models/management/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/processes/hadronic/util/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/processes/management/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/track/include)
+
+#
+# Define the Geant4 Module.
+#
+include(Geant4MacroDefineModule)
+GEANT4_DEFINE_MODULE(NAME G4hadronic_mgt
+    HEADERS
+        G4EnergyRangeManager.hh
+        G4HadLeadBias.hh
+        G4HadronInelasticProcess.hh
+        G4HadronicEPTestMessenger.hh
+        G4HadronicInteractionWrapper.hh
+        G4HadronicProcess.hh
+        G4HadronicProcessStore.hh
+        G4HadronicProcessType.hh
+        G4IsoParticleChange.hh
+        G4NoModelFound.hh
+        G4VLeadingParticleBiasing.hh
+    SOURCES
+        G4EnergyRangeManager.cc
+        G4HadLeadBias.cc
+        G4HadronInelasticProcess.cc
+        G4HadronicEPTestMessenger.cc
+        G4HadronicInteractionWrapper.cc
+        G4HadronicProcess.cc
+        G4HadronicProcessStore.cc
+    GRANULAR_DEPENDENCIES
+        G4baryons
+        G4bosons
+        G4geometrymng
+        G4globman
+        G4had_mod_man
+        G4hadronic_util
+        G4hadronic_xsect
+        G4intercoms
+        G4ions
+        G4leptons
+        G4materials
+        G4mesons
+        G4navigation
+        G4partman
+        G4procman
+        G4track
+        G4volumes
+    GLOBAL_DEPENDENCIES
+        G4geometry
+        G4global
+        G4intercoms
+        G4materials
+        G4particles
+        G4track
+    LINK_LIBRARIES
+)
+
+# List any source specific properties here
+
