3048db4be0e41b62366f23fabe03510f80365700
Benjamin Morgan <Ben.Morgan@warwick.ac.uk>
2010-09-29 19:07:26 +0000

    Initial import of CMake buildfile source/processes/hadronic/models/theo_high_energy/sources.cmake
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@45719 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/processes/hadronic/models/theo_high_energy/sources.cmake
@@ -0,0 +1,95 @@
+#------------------------------------------------------------------------------
+# sources.cmake
+# Module : G4had_theo_max
+# Package: Geant4.src.G4processes.G4hadronic.G4hadronic_models.G4had_theo_max
+#
+# Sources description for a library.
+# Lists the sources and headers of the code explicitely.
+# Lists include paths needed.
+# Lists the internal granular and global dependencies of the library.
+# Source specific properties should be added at the end.
+#
+# Generated on : 24/9/2010
+#
+# $Id: sources.cmake,v 1.1 2010-09-29 19:07:26 bmorgan Exp $
+#
+#------------------------------------------------------------------------------
+
+# List external includes needed.
+include_directories(${CLHEP_INCLUDE_DIRS})
+
+# List internal includes needed.
+include_directories(${CMAKE_SOURCE_DIR}/source/geometry/management/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/geometry/volumes/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/global/HEPGeometry/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/global/HEPNumerics/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/global/HEPRandom/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/global/management/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/materials/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/particles/bosons/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/particles/hadrons/barions/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/particles/hadrons/ions/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/particles/hadrons/mesons/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/particles/leptons/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/particles/management/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/particles/shortlived/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/processes/hadronic/cross_sections/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/processes/hadronic/management/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/processes/hadronic/models/chiral_inv_phase_space/body/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/processes/hadronic/models/chiral_inv_phase_space/cross_sections/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/processes/hadronic/models/im_r_matrix/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/processes/hadronic/models/management/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/processes/hadronic/models/util/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/processes/hadronic/processes/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/processes/hadronic/util/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/processes/management/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/track/include)
+
+#
+# Define the Geant4 Module.
+#
+include(Geant4MacroDefineModule)
+GEANT4_DEFINE_MODULE(NAME G4had_theo_max
+    HEADERS
+        G4ProjectileDiffractiveChannel.hh
+        G4QuasiElasticChannel.hh
+        G4TheoFSGenerator.hh
+    SOURCES
+        G4ProjectileDiffractiveChannel.cc
+        G4QuasiElasticChannel.cc
+        G4TheoFSGenerator.cc
+    GRANULAR_DEPENDENCIES
+        G4baryons
+        G4bosons
+        G4geometrymng
+        G4globman
+        G4had_im_r_matrix
+        G4had_mod_man
+        G4had_mod_util
+        G4hadronic_body_ci
+        G4hadronic_crosec_ci
+        G4hadronic_mgt
+        G4hadronic_proc
+        G4hadronic_util
+        G4hadronic_xsect
+        G4hepnumerics
+        G4ions
+        G4leptons
+        G4materials
+        G4mesons
+        G4partman
+        G4procman
+        G4shortlived
+        G4track
+        G4volumes
+    GLOBAL_DEPENDENCIES
+        G4geometry
+        G4global
+        G4materials
+        G4particles
+        G4track
+    LINK_LIBRARIES
+)
+
+# List any source specific properties here
+
