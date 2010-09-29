fbdded3e0d4dc76b59857a2d375e428e4bd13a09
Benjamin Morgan <Ben.Morgan@warwick.ac.uk>
2010-09-29 18:47:24 +0000

    Initial import of CMake buildfile source/particles/adjoint/sources.cmake
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@45572 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/particles/adjoint/sources.cmake
@@ -0,0 +1,66 @@
+#------------------------------------------------------------------------------
+# sources.cmake
+# Module : G4partadj
+# Package: Geant4.src.G4particles.G4partadj
+#
+# Sources description for a library.
+# Lists the sources and headers of the code explicitely.
+# Lists include paths needed.
+# Lists the internal granular and global dependencies of the library.
+# Source specific properties should be added at the end.
+#
+# Generated on : 24/9/2010
+#
+# $Id: sources.cmake,v 1.1 2010-09-29 18:47:24 bmorgan Exp $
+#
+#------------------------------------------------------------------------------
+
+# List external includes needed.
+include_directories(${CLHEP_INCLUDE_DIRS})
+
+# List internal includes needed.
+include_directories(${CMAKE_SOURCE_DIR}/source/global/HEPGeometry/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/global/HEPRandom/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/global/management/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/materials/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/particles/management/include)
+
+#
+# Define the Geant4 Module.
+#
+include(Geant4MacroDefineModule)
+GEANT4_DEFINE_MODULE(NAME G4partadj
+    HEADERS
+        G4AdjointAlpha.hh
+        G4AdjointDeuteron.hh
+        G4AdjointElectron.hh
+        G4AdjointGamma.hh
+        G4AdjointGenericIon.hh
+        G4AdjointHe3.hh
+        G4AdjointIons.hh
+        G4AdjointPositron.hh
+        G4AdjointProton.hh
+        G4AdjointTriton.hh
+    SOURCES
+        G4AdjointAlpha.cc
+        G4AdjointDeuteron.cc
+        G4AdjointElectron.cc
+        G4AdjointGamma.cc
+        G4AdjointGenericIon.cc
+        G4AdjointHe3.cc
+        G4AdjointIons.cc
+        G4AdjointPositron.cc
+        G4AdjointProton.cc
+        G4AdjointTriton.cc
+    GRANULAR_DEPENDENCIES
+        G4globman
+        G4materials
+        G4partman
+    GLOBAL_DEPENDENCIES
+        G4global
+        G4materials
+    LINK_LIBRARIES
+)
+
+# List any source specific properties here
+
