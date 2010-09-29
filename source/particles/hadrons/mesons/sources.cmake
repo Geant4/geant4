e1316ad86332388628e3f72ae84edd6c1bc17c4e
Benjamin Morgan <Ben.Morgan@warwick.ac.uk>
2010-09-29 18:48:36 +0000

    Initial import of CMake buildfile source/particles/hadrons/mesons/sources.cmake
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@45581 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/particles/hadrons/mesons/sources.cmake
@@ -0,0 +1,96 @@
+#------------------------------------------------------------------------------
+# sources.cmake
+# Module : G4mesons
+# Package: Geant4.src.G4particles..G4mesons
+#
+# Sources description for a library.
+# Lists the sources and headers of the code explicitely.
+# Lists include paths needed.
+# Lists the internal granular and global dependencies of the library.
+# Source specific properties should be added at the end.
+#
+# Generated on : 24/9/2010
+#
+# $Id: sources.cmake,v 1.1 2010-09-29 18:48:36 bmorgan Exp $
+#
+#------------------------------------------------------------------------------
+
+# List external includes needed.
+include_directories(${CLHEP_INCLUDE_DIRS})
+
+# List internal includes needed.
+include_directories(${CMAKE_SOURCE_DIR}/source/global/HEPGeometry/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/global/HEPRandom/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/global/management/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/materials/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/particles/management/include)
+
+#
+# Define the Geant4 Module.
+#
+include(Geant4MacroDefineModule)
+GEANT4_DEFINE_MODULE(NAME G4mesons
+    HEADERS
+        G4AntiBMesonZero.hh
+        G4AntiBsMesonZero.hh
+        G4AntiDMesonZero.hh
+        G4AntiKaonZero.hh
+        G4BMesonMinus.hh
+        G4BMesonPlus.hh
+        G4BMesonZero.hh
+        G4BsMesonZero.hh
+        G4DMesonMinus.hh
+        G4DMesonPlus.hh
+        G4DMesonZero.hh
+        G4DsMesonMinus.hh
+        G4DsMesonPlus.hh
+        G4Eta.hh
+        G4EtaPrime.hh
+        G4JPsi.hh
+        G4KaonMinus.hh
+        G4KaonPlus.hh
+        G4KaonZero.hh
+        G4KaonZeroLong.hh
+        G4KaonZeroShort.hh
+        G4MesonConstructor.hh
+        G4PionMinus.hh
+        G4PionPlus.hh
+        G4PionZero.hh
+    SOURCES
+        G4AntiBMesonZero.cc
+        G4AntiBsMesonZero.cc
+        G4AntiDMesonZero.cc
+        G4AntiKaonZero.cc
+        G4BMesonMinus.cc
+        G4BMesonPlus.cc
+        G4BMesonZero.cc
+        G4BsMesonZero.cc
+        G4DMesonMinus.cc
+        G4DMesonPlus.cc
+        G4DMesonZero.cc
+        G4DsMesonMinus.cc
+        G4DsMesonPlus.cc
+        G4Eta.cc
+        G4EtaPrime.cc
+        G4JPsi.cc
+        G4KaonMinus.cc
+        G4KaonPlus.cc
+        G4KaonZero.cc
+        G4KaonZeroLong.cc
+        G4KaonZeroShort.cc
+        G4MesonConstructor.cc
+        G4PionMinus.cc
+        G4PionPlus.cc
+        G4PionZero.cc
+    GRANULAR_DEPENDENCIES
+        G4globman
+        G4materials
+        G4partman
+    GLOBAL_DEPENDENCIES
+        G4global
+        G4materials
+    LINK_LIBRARIES
+)
+
+# List any source specific properties here
+
