fa24ff2fbeb332e5626a95588655cc940d2aaef7
Benjamin Morgan <Ben.Morgan@warwick.ac.uk>
2010-09-29 18:47:00 +0000

    Initial import of CMake buildfile source/parameterisations/gflash/sources.cmake
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@45569 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/parameterisations/gflash/sources.cmake
@@ -0,0 +1,113 @@
+#------------------------------------------------------------------------------
+# sources.cmake
+# Module : G4gflash
+# Package: Geant4.src.G4parmodels.G4gflash
+#
+# Sources description for a library.
+# Lists the sources and headers of the code explicitely.
+# Lists include paths needed.
+# Lists the internal granular and global dependencies of the library.
+# Source specific properties should be added at the end.
+#
+# Generated on : 24/9/2010
+#
+# $Id: sources.cmake,v 1.1 2010-09-29 18:47:00 bmorgan Exp $
+#
+#------------------------------------------------------------------------------
+
+# List external includes needed.
+include_directories(${CLHEP_INCLUDE_DIRS})
+
+# List internal includes needed.
+include_directories(${CMAKE_SOURCE_DIR}/source/digits_hits/detector/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/digits_hits/hits/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/geometry/management/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/geometry/navigation/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/geometry/volumes/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/global/HEPGeometry/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/global/HEPNumerics/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/global/HEPRandom/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/global/management/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/graphics_reps/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/intercoms/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/materials/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/particles/bosons/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/particles/hadrons/barions/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/particles/hadrons/ions/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/particles/hadrons/mesons/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/particles/leptons/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/particles/management/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/processes/cuts/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/processes/electromagnetic/muons/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/processes/electromagnetic/utils/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/processes/management/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/processes/parameterisation/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/track/include)
+
+#
+# Define the Geant4 Module.
+#
+include(Geant4MacroDefineModule)
+GEANT4_DEFINE_MODULE(NAME G4gflash
+    HEADERS
+        G4GFlashSpot.hh
+        G4VGFlashSensitiveDetector.hh
+        GFlashEnergySpot.hh
+        GFlashHitMaker.hh
+        GFlashHomoShowerParameterisation.hh
+        GFlashParticleBounds.hh
+        GFlashSamplingShowerParameterisation.hh
+        GFlashSamplingShowerTuning.hh
+        GFlashShowerModel.hh
+        GFlashShowerModelMessenger.hh
+        GVFlashHomoShowerTuning.hh
+        GVFlashShowerParameterisation.hh
+        Gamma.hh
+    SOURCES
+        GFlashEnergySpot.cc
+        GFlashHitMaker.cc
+        GFlashHomoShowerParameterisation.cc
+        GFlashParticleBounds.cc
+        GFlashSamplingShowerParameterisation.cc
+        GFlashShowerModel.cc
+        GFlashShowerModelMessenger.cc
+        GVFlashShowerParameterisation.cc
+        Gamma.cc
+    GRANULAR_DEPENDENCIES
+        G4baryons
+        G4bosons
+        G4cuts
+        G4detector
+        G4emutils
+        G4geometrymng
+        G4globman
+        G4graphics_reps
+        G4hepnumerics
+        G4hits
+        G4intercoms
+        G4ions
+        G4leptons
+        G4materials
+        G4mesons
+        G4muons
+        G4navigation
+        G4parameterisation
+        G4partman
+        G4procman
+        G4track
+        G4volumes
+    GLOBAL_DEPENDENCIES
+        G4digits_hits
+        G4geometry
+        G4global
+        G4graphics_reps
+        G4intercoms
+        G4materials
+        G4particles
+        G4processes
+        G4track
+    LINK_LIBRARIES
+)
+
+# List any source specific properties here
+
