4cc5e110c3456036ce6c2763dc24856cbbdb8f79
Benjamin Morgan <Ben.Morgan@warwick.ac.uk>
2010-09-29 19:10:00 +0000

    Initial import of CMake buildfile source/processes/transportation/sources.cmake
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@45737 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/processes/transportation/sources.cmake
@@ -0,0 +1,87 @@
+#------------------------------------------------------------------------------
+# sources.cmake
+# Module : G4transportation
+# Package: Geant4.src.G4processes.G4transportation
+#
+# Sources description for a library.
+# Lists the sources and headers of the code explicitely.
+# Lists include paths needed.
+# Lists the internal granular and global dependencies of the library.
+# Source specific properties should be added at the end.
+#
+# Generated on : 24/9/2010
+#
+# $Id: sources.cmake,v 1.1 2010-09-29 19:10:00 bmorgan Exp $
+#
+#------------------------------------------------------------------------------
+
+# List external includes needed.
+include_directories(${CLHEP_INCLUDE_DIRS})
+
+# List internal includes needed.
+include_directories(${CMAKE_SOURCE_DIR}/source/geometry/biasing/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/geometry/magneticfield/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/geometry/management/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/geometry/navigation/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/geometry/volumes/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/global/HEPGeometry/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/global/HEPRandom/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/global/management/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/intercoms/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/materials/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/particles/management/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/processes/cuts/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/processes/electromagnetic/utils/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/processes/management/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/track/include)
+
+#
+# Define the Geant4 Module.
+#
+include(Geant4MacroDefineModule)
+GEANT4_DEFINE_MODULE(NAME G4transportation
+    HEADERS
+        G4CoupledTransportation.hh
+        G4CoupledTransportation.icc
+        G4NeutronKiller.hh
+        G4NeutronKillerMessenger.hh
+        G4StepLimiter.hh
+        G4TrackTerminator.hh
+        G4Transportation.hh
+        G4Transportation.icc
+        G4UserSpecialCuts.hh
+        G4VTrackTerminator.hh
+    SOURCES
+        G4CoupledTransportation.cc
+        G4NeutronKiller.cc
+        G4NeutronKillerMessenger.cc
+        G4StepLimiter.cc
+        G4Transportation.cc
+        G4UserSpecialCuts.cc
+        G4VTrackTerminator.cc
+    GRANULAR_DEPENDENCIES
+        G4cuts
+        G4emutils
+        G4geombias
+        G4geometrymng
+        G4globman
+        G4intercoms
+        G4magneticfield
+        G4materials
+        G4navigation
+        G4partman
+        G4procman
+        G4track
+        G4volumes
+    GLOBAL_DEPENDENCIES
+        G4geometry
+        G4global
+        G4intercoms
+        G4materials
+        G4particles
+        G4track
+    LINK_LIBRARIES
+)
+
+# List any source specific properties here
+
