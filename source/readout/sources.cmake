dd2d78b3476480c52defc77122bba72827f634f9
Benjamin Morgan <Ben.Morgan@warwick.ac.uk>
2010-09-29 19:10:25 +0000

    Initial import of CMake buildfile source/readout/sources.cmake
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@45739 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/readout/sources.cmake
@@ -0,0 +1,89 @@
+#------------------------------------------------------------------------------
+# sources.cmake
+# Module : G4readout
+# Package: Geant4.src.G4readout
+#
+# Sources description for a library.
+# Lists the sources and headers of the code explicitely.
+# Lists include paths needed.
+# Lists the internal granular and global dependencies of the library.
+# Source specific properties should be added at the end.
+#
+# Generated on : 24/9/2010
+#
+# $Id: sources.cmake,v 1.1 2010-09-29 19:10:25 bmorgan Exp $
+#
+#------------------------------------------------------------------------------
+
+# List external includes needed.
+include_directories(${CLHEP_INCLUDE_DIRS})
+
+# List internal includes needed.
+include_directories(${CMAKE_SOURCE_DIR}/source/digits_hits/detector/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/digits_hits/digits/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/digits_hits/hits/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/event/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/geometry/management/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/geometry/navigation/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/geometry/volumes/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/global/HEPGeometry/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/global/HEPRandom/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/global/management/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/intercoms/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/materials/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/particles/management/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/processes/electromagnetic/utils/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/processes/management/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/run/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/track/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/tracking/include)
+
+#
+# Define the Geant4 Module.
+#
+include(Geant4MacroDefineModule)
+GEANT4_DEFINE_MODULE(NAME G4readout
+    HEADERS
+        G4DCtable.hh
+        G4DMmessenger.hh
+        G4DigiManager.hh
+        G4VDigitizerModule.hh
+    SOURCES
+        G4DCtable.cc
+        G4DMmessenger.cc
+        G4DigiManager.cc
+        G4VDigitizerModule.cc
+    GRANULAR_DEPENDENCIES
+        G4detector
+        G4digits
+        G4emutils
+        G4event
+        G4geometrymng
+        G4globman
+        G4hits
+        G4intercoms
+        G4materials
+        G4navigation
+        G4partman
+        G4procman
+        G4run
+        G4track
+        G4tracking
+        G4volumes
+    GLOBAL_DEPENDENCIES
+        G4digits_hits
+        G4event
+        G4geometry
+        G4global
+        G4intercoms
+        G4materials
+        G4particles
+        G4processes
+        G4run
+        G4track
+        G4tracking
+    LINK_LIBRARIES
+)
+
+# List any source specific properties here
+
