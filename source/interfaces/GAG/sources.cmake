a1808527626e7ad9ecd67abe307554dcfc732095
Benjamin Morgan <Ben.Morgan@warwick.ac.uk>
2010-09-29 18:45:47 +0000

    Initial import of CMake buildfile source/interfaces/GAG/sources.cmake
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@45560 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ source/interfaces/GAG/sources.cmake
@@ -0,0 +1,48 @@
+#------------------------------------------------------------------------------
+# sources.cmake
+# Module : G4UIGAG
+# Package: Geant4.src.G4interfaces.G4UIGAG
+#
+# Sources description for a library.
+# Lists the sources and headers of the code explicitely.
+# Lists include paths needed.
+# Lists the internal granular and global dependencies of the library.
+# Source specific properties should be added at the end.
+#
+# Generated on : 24/9/2010
+#
+# $Id: sources.cmake,v 1.1 2010-09-29 18:45:47 bmorgan Exp $
+#
+#------------------------------------------------------------------------------
+
+# List external includes needed.
+include_directories(${CLHEP_INCLUDE_DIRS})
+
+# List internal includes needed.
+include_directories(${CMAKE_SOURCE_DIR}/source/global/management/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/intercoms/include)
+include_directories(${CMAKE_SOURCE_DIR}/source/interfaces/common/include)
+
+#
+# Define the Geant4 Module.
+#
+include(Geant4MacroDefineModule)
+GEANT4_DEFINE_MODULE(NAME G4UIGAG
+    HEADERS
+        G4UIGAG.hh
+        G4UIGainServer.hh
+    SOURCES
+        G4UIGAG.cc
+        G4UIGainServer.cc
+    GRANULAR_DEPENDENCIES
+        G4UIcommon
+        G4globman
+        G4intercoms
+    GLOBAL_DEPENDENCIES
+        G4global
+        G4intercoms
+    LINK_LIBRARIES
+)
+
+# List any source specific properties here
+
