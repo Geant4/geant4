0b70d63fbb00f518ffaf4468bee033238ebe4fdd
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2010-10-19 15:20:18 +0000

    Implemented copy-ctr and assignment operator; implemented Clone() method
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@46080 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/geometry/management/src/G4ReflectedSolid.cc
+++ source/geometry/management/src/G4ReflectedSolid.cc
@@ -24,7 +24,7 @@
 // ********************************************************************
 //
 //
-// $Id: G4ReflectedSolid.cc,v 1.12 2010-07-05 09:22:58 gcosmo Exp $
+// $Id: G4ReflectedSolid.cc,v 1.13 2010-10-19 15:20:18 gcosmo Exp $
 //
 // GEANT4 tag $Name: not supported by cvs2svn $
 //
@@ -91,6 +91,49 @@ G4ReflectedSolid::~G4ReflectedSolid()
   }
   delete fpPolyhedron;
 }
+
+///////////////////////////////////////////////////////////////////
+//
+
+G4ReflectedSolid::G4ReflectedSolid(const G4ReflectedSolid& rhs)
+  : G4VSolid(rhs), fPtrSolid(rhs.fPtrSolid), fpPolyhedron(0)
+{
+  fPtrTransform      = new G4AffineTransform(*rhs.fPtrTransform);
+  fDirectTransform   = new G4AffineTransform(*rhs.fDirectTransform);
+  fPtrTransform3D    = new G4Transform3D(*rhs.fPtrTransform3D);
+  fDirectTransform3D = new G4Transform3D(*rhs.fDirectTransform3D);
+}
+
+///////////////////////////////////////////////////////////////////
+//
+
+G4ReflectedSolid& G4ReflectedSolid::operator=(const G4ReflectedSolid& rhs)
+{
+  // Check assignment to self
+  //
+  if (this == &rhs)  { return *this; }
+
+  // Copy base class data
+  //
+  G4VSolid::operator=(rhs);
+
+  // Copy data
+  //
+  fPtrSolid= rhs.fPtrSolid; fpPolyhedron= 0;
+  delete fPtrTransform;
+  fPtrTransform= new G4AffineTransform(*rhs.fPtrTransform);
+  delete fDirectTransform;
+  fDirectTransform= new G4AffineTransform(*rhs.fDirectTransform);
+  delete fPtrTransform3D;
+  fPtrTransform3D= new G4Transform3D(*rhs.fPtrTransform3D);
+  delete fDirectTransform3D;
+  fDirectTransform3D= new G4Transform3D(*rhs.fDirectTransform3D);
+
+  return *this;
+}
+
+///////////////////////////////////////////////////////////////////
+//
 
 G4GeometryType G4ReflectedSolid::GetEntityType() const 
 {
@@ -494,7 +537,7 @@ G4ReflectedSolid::ComputeDimensions(       G4VPVParameterisation*,
                                      const G4VPhysicalVolume* ) 
 {
   DumpInfo();
-  G4Exception("G4BooleanSolid::ComputeDimensions()",
+  G4Exception("G4ReflectedSolid::ComputeDimensions()",
                "NotApplicable", FatalException,
                "Method not applicable in this context!");
 }
@@ -511,6 +554,16 @@ G4ThreeVector G4ReflectedSolid::GetPointOnSurface() const
 
   return G4ThreeVector(newPoint.x(),newPoint.y(),newPoint.z());
 }
+
+//////////////////////////////////////////////////////////////////////////
+//
+// Make a clone of this object
+
+G4VSolid* G4ReflectedSolid::Clone() const
+{
+  return new G4ReflectedSolid(*this);
+}
+
 
 //////////////////////////////////////////////////////////////////////////
 //
