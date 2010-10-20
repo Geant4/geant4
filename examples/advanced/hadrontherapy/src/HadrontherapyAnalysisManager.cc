f65a3b16b4928a03396149aae0a5087eff35a56d
Pablo Cirrone <Pablo.Cirrone@cern.ch>
2010-10-20 10:25:36 +0000

    Fixed a compiler error due to a misplaced #ifdef in the constructor of H.AnalysisManager
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@46114 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/advanced/hadrontherapy/src/HadrontherapyAnalysisManager.cc
+++ examples/advanced/hadrontherapy/src/HadrontherapyAnalysisManager.cc
@@ -48,10 +48,10 @@ theROOTIonTuple(0),
 fragmentNtuple(0),
 metaData(0),
 eventCounter(0)
+#endif
 {
     fMess = new HadrontherapyAnalysisFileMessenger(this);
 }
-#endif
 /////////////////////////////////////////////////////////////////////////////
 
 HadrontherapyAnalysisManager::~HadrontherapyAnalysisManager()
