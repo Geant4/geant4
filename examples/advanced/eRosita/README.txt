9ecbdebcff412d6cb38bc19f613e828cc422e4c8
Maria Grazia Pia <Maria.Grazia.Pia@cern.ch>
2010-11-23 16:15:19 +0000

    GNUmakefile
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@47366 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/advanced/eRosita/README.txt
+++ examples/advanced/eRosita/README.txt
@@ -1,15 +1,64 @@
-$Id: README.txt,v 1.2 2010-11-19 18:04:46 pia Exp $
+$Id: README.txt,v 1.3 2010-11-23 16:15:19 pia Exp $
 -------------------------------------------------------------------
 
-eROSITA fluorescence
---------------------
+     =========================================================
+     Geant4 - an Object-Oriented Toolkit for Simulation in HEP
+     =========================================================
 
-Authors
+                       eROSITA fluorescence
+                       --------------------
 
-References
-Web sites
+Authors:
 
-Contact person
+Dieter Schlosser (pnSensor, Munich), Georg Weidenspointner (MPE Garching
+and MPI Halbleiterlabor, Munich), and Maria Grazia Pia (INFN Genova)
+
+References:
+
+M.G. Pia et al., 2009, "PIXE Simulation With Geant4", 
+IEEE Trans. Nucl. Sci., vol. 56, no. 6, pp. 3614-3649
+
+N. Meidinger et al., 2010, "Development of the focal plane PNCCD
+camera system for the X-ray space telescope eROSITA", Nuclear
+Instruments and Methods in Physics Research A 624, 321–329
+
+Web sites:
+
+????
+
+Contact persons:
+
+Dieter Schlosser     : dis@hll.mpg.de
+Georg Weidenspointner: ggw@hll.mpg.de
+Maria Grazia Pia     : Maria.Grazia.Pia@cern.ch
+
+
+Example description:
+
+This is example is based on simulations of the instrumental background
+of the eROSITA X-ray telescope, in particular the strength of
+fluorescence lines inside the passive graded Z shield. The set-up
+considered in this example consists of a Cu block that is irradiated
+with protons. Impact ionization of Cu atoms generates vacancies in
+atomic shells. These are then filled by atomic de-excitation,
+resulting in the emission of fluorescence photons and Auger electrons
+- the PIXE (particle induced X-ray emission) process. In particular
+fluorescence photons are then detected with a Si CCD positioned next to
+the Cu block. 
+
+
+Instructions on how to build and run the example:
+
+
+Description of output:
+
+
+
+
+
+
+
+ which are then filledVia the PIXE process, flourescence photons and Auger 
 
 Brief documentation of the simulation code and instructions how to build 
 and run the example go here
---
+++ examples/advanced/eRosita/eRosita.cc
@@ -0,0 +1,134 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+// $Id: eRosita.cc,v 1.1 2010-11-23 16:15:19 pia Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+// 
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+#include "eRositaDetectorConstruction.hh"
+#include "eRositaPhysicsList.hh"
+#include "eRositaPrimaryGeneratorAction.hh"
+#include "eRositaRunAction.hh"
+#include "eRositaEventAction.hh"
+#include "eRositaSteppingAction.hh"
+#include "eRositaSteppingVerbose.hh"
+
+#include "G4RunManager.hh"
+#include "G4UImanager.hh"
+#include "G4UIterminal.hh"
+#include "G4UItcsh.hh"
+
+#ifdef G4VIS_USE
+#include "G4VisExecutive.hh"
+#endif
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+int main(int argc,char** argv)
+{
+  // User Verbose output class
+  //
+  G4VSteppingVerbose* verbosity = new eRositaSteppingVerbose;
+  G4VSteppingVerbose::SetInstance(verbosity);
+  
+  // Run manager
+  //
+  G4RunManager * runManager = new G4RunManager;
+
+  // User Initialization classes (mandatory)
+  //
+  eRositaDetectorConstruction* detector = new eRositaDetectorConstruction;
+  runManager->SetUserInitialization(detector);
+  //
+  G4VUserPhysicsList* physics = new eRositaPhysicsList;
+  runManager->SetUserInitialization(physics);
+   
+  // User Action classes
+  //
+  G4VUserPrimaryGeneratorAction* gen_action = new eRositaPrimaryGeneratorAction(detector);
+  runManager->SetUserAction(gen_action);
+  //
+  G4UserRunAction* run_action = new eRositaRunAction;
+  runManager->SetUserAction(run_action);
+  //
+  G4UserEventAction* event_action = new eRositaEventAction;
+  runManager->SetUserAction(event_action);
+  //
+  G4UserSteppingAction* stepping_action = new eRositaSteppingAction;
+  runManager->SetUserAction(stepping_action);
+
+  // Initialize G4 kernel
+  //
+  runManager->Initialize();
+      
+  // Get the pointer to the User Interface manager
+  //
+  G4UImanager * UI = G4UImanager::GetUIpointer();  
+
+  if (argc!=1)   // batch mode  
+    {
+     G4String command = "/control/execute ";
+     G4String fileName = argv[1];
+     UI->ApplyCommand(command+fileName);
+    }
+    
+  else           // interactive mode : define visualization and UI terminal
+    { 
+#ifdef G4VIS_USE
+      G4VisManager* visManager = new G4VisExecutive;
+      visManager->Initialize();
+#endif    
+     
+      G4UIsession * session = 0;
+#ifdef G4UI_USE_TCSH
+      session = new G4UIterminal(new G4UItcsh);      
+#else
+      session = new G4UIterminal();
+#endif
+      UI->ApplyCommand("/control/execute vis.mac");     
+      session->SessionStart();
+      delete session;
+     
+#ifdef G4VIS_USE
+      delete visManager;
+#endif     
+    }
+
+  // Free the store: user actions, physics_list and detector_description are
+  //                 owned and deleted by the run manager, so they should not
+  //                 be deleted in the main() program !
+
+  delete runManager;
+  delete verbosity;
+
+  return 0;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
