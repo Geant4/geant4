bd3f935fc16468f7d4cc405ff001fc8dd0a61728
Pablo Cirrone <Pablo.Cirrone@cern.ch>
2010-11-26 00:17:52 +0000

    fixed errors
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@47519 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ examples/advanced/medical_linac/include/ML2EventAction.hh
@@ -0,0 +1,61 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+//
+// The code was written by :
+//	^Claudio Andenna  claudio.andenna@ispesl.it, claudio.andenna@iss.infn.it
+//      *Barbara Caccia barbara.caccia@iss.it
+//      with the support of Pablo Cirrone (LNS, INFN Catania Italy)
+//	with the contribute of Alessandro Occhigrossi*
+//
+// ^INAIL DIPIA - ex ISPESL and INFN Roma, gruppo collegato Sanità, Italy
+// *Istituto Superiore di Sanità and INFN Roma, gruppo collegato Sanità, Italy
+//  Viale Regina Elena 299, 00161 Roma (Italy)
+//  tel (39) 06 49902246
+//  fax (39) 06 49387075
+//
+// more information:
+// http://g4advancedexamples.lngs.infn.it/Examples/medical-linac
+//
+//*******************************************************//
+
+
+#ifndef CML2EventActionH
+#define CML2EventActionH
+
+#include "G4UserEventAction.hh"
+#include "globals.hh"
+
+class CML2EventAction: public G4UserEventAction
+{
+public:
+	CML2EventAction(void);
+	~CML2EventAction(void);
+  public:
+    void BeginOfEventAction(const G4Event*);
+    void EndOfEventAction(const G4Event*);
+
+private:
+  G4String drawFlag; //Visualisation flag
+};
+
+
+#endif
---
+++ examples/advanced/medical_linac/src/ML2EventAction.cc
@@ -0,0 +1,68 @@
+//
+// ********************************************************************
+// * DISCLAIMER                                                       *
+// *                                                                  *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.                                                             *
+// *                                                                  *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
+// ********************************************************************
+//
+// The code was written by :
+//	^Claudio Andenna  claudio.andenna@ispesl.it, claudio.andenna@iss.infn.it
+//      *Barbara Caccia barbara.caccia@iss.it
+//      with the support of Pablo Cirrone (LNS, INFN Catania Italy)
+//	with the contribute of Alessandro Occhigrossi*
+//
+// ^INAIL DIPIA - ex ISPESL and INFN Roma, gruppo collegato Sanità, Italy
+// *Istituto Superiore di Sanità and INFN Roma, gruppo collegato Sanità, Italy
+//  Viale Regina Elena 299, 00161 Roma (Italy)
+//  tel (39) 06 49902246
+//  fax (39) 06 49387075
+//
+// more information:
+// http://g4advancedexamples.lngs.infn.it/Examples/medical-linac
+//
+//*******************************************************//
+
+
+#include "ML2EventAction.hh"
+
+#include "G4Event.hh"
+#include "G4EventManager.hh"
+#include "G4HCofThisEvent.hh"
+#include "G4VHitsCollection.hh"
+#include "G4TrajectoryContainer.hh"
+#include "G4Trajectory.hh"
+#include "G4VVisManager.hh"
+#include "G4SDManager.hh"
+#include "G4UImanager.hh"
+#include "G4ios.hh"
+
+
+CML2EventAction::CML2EventAction() :
+  drawFlag("all" )
+{}
+
+ 
+CML2EventAction::~CML2EventAction()
+{}
+ 
+void CML2EventAction::BeginOfEventAction(const G4Event*)
+{}
+
+ 
+void CML2EventAction::EndOfEventAction(const G4Event* evt)
+{}
