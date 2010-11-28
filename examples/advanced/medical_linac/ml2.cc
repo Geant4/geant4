9ca836839d30c31af150a98fbbb500dbec0be0ad
Pablo Cirrone <Pablo.Cirrone@cern.ch>
2010-11-28 23:33:52 +0000

    file updating
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@47564 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/advanced/medical_linac/ml2.cc
+++ examples/advanced/medical_linac/ml2.cc
@@ -1,35 +1,33 @@
 //
 // ********************************************************************
-// * License and Disclaimer                                           *
+// * DISCLAIMER                                                       *
 // *                                                                  *
-// * The  Geant4 software  is  copyright of the Copyright Holders  of *
-// * the Geant4 Collaboration.  It is provided  under  the terms  and *
-// * conditions of the Geant4 Software License,  included in the file *
-// * LICENSE and available at  http://cern.ch/geant4/license .  These *
-// * include a list of copyright holders.                             *
+// * The following disclaimer summarizes all the specific disclaimers *
+// * of contributors to this software. The specific disclaimers,which *
+// * govern, are listed with their locations in:                      *
+// *   http://cern.ch/geant4/license                                  *
 // *                                                                  *
 // * Neither the authors of this software system, nor their employing *
 // * institutes,nor the agencies providing financial support for this *
 // * work  make  any representation or  warranty, express or implied, *
 // * regarding  this  software system or assume any liability for its *
-// * use.  Please see the license in the file  LICENSE  and URL above *
-// * for the full disclaimer and the limitation of liability.         *
+// * use.                                                             *
 // *                                                                  *
-// * This  code  implementation is the result of  the  scientific and *
-// * technical work of the GEANT4 collaboration.                      *
-// * By using,  copying,  modifying or  distributing the software (or *
-// * any work based  on the software)  you  agree  to acknowledge its *
-// * use  in  resulting  scientific  publications,  and indicate your *
-// * acceptance of all terms of the Geant4 Software license.          *
+// * This  code  implementation is the  intellectual property  of the *
+// * GEANT4 collaboration.                                            *
+// * By copying,  distributing  or modifying the Program (or any work *
+// * based  on  the Program)  you indicate  your  acceptance of  this *
+// * statement, and all its terms.                                    *
 // ********************************************************************
 //
 // The code was written by :
-//	^Claudio Andenna claudio.andenna@iss.infn.it, claudio.andenna@ispesl.it
+//	^Claudio Andenna  claudio.andenna@ispesl.it, claudio.andenna@iss.infn.it
 //      *Barbara Caccia barbara.caccia@iss.it
 //      with the support of Pablo Cirrone (LNS, INFN Catania Italy)
+//	with the contribute of Alessandro Occhigrossi*
 //
-// ^ISPESL and INFN Roma, gruppo collegato Sanità, Italy
-// *Istituto Superiore di Sanità and INFN Roma, gruppo collegato Sanità, Italy
+// ^INAIL DIPIA - ex ISPESL  and INFN Roma, gruppo collegato SanitÃ , Italy
+// *Istituto Superiore di SanitÃ  and INFN Roma, gruppo collegato SanitÃ , Italy
 //  Viale Regina Elena 299, 00161 Roma (Italy)
 //  tel (39) 06 49902246
 //  fax (39) 06 49387075
@@ -40,56 +38,71 @@
 //*******************************************************//
 
 
-
 #include "ML2Main.hh"
 
 #include "ML2PrimaryGenerationAction.hh"
 #include "ML2WorldConstruction.hh"
 #include "ML2PhysicsList.hh"
 #include "ML2SteppingAction.hh"
+#include "ML2EventAction.hh"
 #include "ML2TrackingAction.hh"
 
 #include "G4UImanager.hh"
 #include "G4Timer.hh"
 	
 
+#include "G4UIterminal.hh"
+#ifdef G4UI_USE_XM
+	#include "G4UIXm.hh"
+#endif
 #ifdef G4VIS_USE
 	#include "G4VisExecutive.hh"
 #endif
 
-#ifdef G4UI_USE
-	#include "G4UIExecutive.hh"
-#endif
 
 
-
-void visio(int argc, char* argv[])
+#ifdef G4VIS_USE
+void visio()
 {
-#ifdef G4VIS_USE
-        G4VisManager * visManager=new G4VisExecutive;
-	visManager->Initialize();
-#endif
+	G4UIsession *session=0;
+	session=new G4UIterminal();
 
-	G4UImanager *UImanager=G4UImanager::GetUIpointer();
-#ifdef G4UI_USE
-	G4UIExecutive* ui = new G4UIExecutive(argc, argv);
-#ifdef G4VIS_USE
-	UImanager->ApplyCommand("/control/execute vis.mac");     
-#endif
-	ui->SessionStart();
-	delete ui;
-#endif
+		G4VisManager * visManager=new G4VisExecutive;
+		visManager->Initialize();
 
-#ifdef G4VIS_USE
-  delete visManager;
-#endif
+		G4UImanager *UI=G4UImanager::GetUIpointer();
+		if (session)
+		{
+			UI->ApplyCommand("/control/execute vis.mac");
+			session->SessionStart();
+			delete session;
+		}
+    delete visManager;
 }
 
+#endif
 
 int main(int argc, char* argv[])
 {
-        CML2CInputData *myInputData;
+	// instantiate the world class
+	CML2WorldConstruction *myWorld=CML2WorldConstruction::GetInstance(); 
+
+	G4RunManager *runManager=new G4RunManager();
+	ML2PhysicsList *physics=new ML2PhysicsList();
+	runManager->SetUserInitialization(physics);
+	
+	// build the primary generator
+	CML2PrimaryGenerationAction *gun;
+	gun = CML2PrimaryGenerationAction::GetInstance();
+
+	// build the main messenger class for the input data
+	CML2CInputData *myInputData;
 	myInputData=new CML2CInputData();
+
+	// initialize the primary generator variables
+	gun->inizialize(&myInputData->inputData.primaryParticleData);
+
+	// according to the number of the launching line
 	if (argc==1)
 	{
 		myInputData->inputData.generalData.seed=1;
@@ -106,76 +119,89 @@ int main(int argc, char* argv[])
 		myInputData->inputData.generalData.StartFileInputData=(G4String)argv[1];
 	}
 
-		G4RunManager *runManager=new G4RunManager();
-		ML2PhysicsList *physics=new ML2PhysicsList();
-		runManager->SetUserInitialization(physics);
-		CML2MainMessenger *ML2MainMessenger;
-		ML2MainMessenger=new CML2MainMessenger(myInputData);
+	// read the main mac file and execute the commands
+	G4UImanager* UI = G4UImanager::GetUIpointer();
+	G4String command = "/control/execute ";
+	UI->ApplyCommand(command+myInputData->inputData.generalData.StartFileInputData); 
 
-		CML2WorldConstruction *myWorld=CML2WorldConstruction::GetInstance(); 
+
+	// set and initialize the random generator
+	CLHEP::HepRandom::setTheEngine(new CLHEP::RanecuEngine);
+	CLHEP::HepRandom :: setTheSeed(myInputData->inputData.generalData.seed);
+
+	// create the world class
+	if (!myWorld->create(&myInputData->inputData, myInputData->getbOnlyVisio()))
+	{
+		return 1; // if it fails to create the world
+	}
+	else
+	{
+		// initialize the primary generator according to the choosen particle source
+		gun->design(myWorld->getCML2AcceleratorConstruction()->getAcceleratorIsoCentre());
+	}
+
+	// instantiate the convergence control class and assign it to myStepAction
+	CML2Convergence *convergence=new CML2Convergence(myInputData->inputData.generalData.seed, myInputData->inputData.generalData.saving_in_Selected_Voxels_every_events, myInputData->inputData.generalData.fileExperimentalData, myInputData->inputData.generalData.fileExperimentalDataOut, myInputData->inputData.generalData.bCompareExp, myInputData->inputData.generalData.maxNumberOfEvents, gun->getNrecycling(), myInputData->inputData.generalData.nMaxLoop);
+
+
+	// build the ML2RunAction to assign the single phantom name at each run 
+	CML2RunAction *myRunAction=new CML2RunAction(convergence, myInputData->inputData.generalData.nBeam, myInputData->bOnlyVisio);
 	
-		CML2PrimaryGenerationAction *gun;
-		gun = new CML2PrimaryGenerationAction(&myInputData->inputData.primaryParticleData);
+	CML2SteppingAction *myStepAction=new CML2SteppingAction(convergence);
+	CML2EventAction *ML2EventAction = new CML2EventAction();
 
-		G4UImanager* UImanager = G4UImanager::GetUIpointer();
-		G4String command = "/control/execute ";
-		UImanager->ApplyCommand(command+myInputData->inputData.generalData.StartFileInputData); 
-// initialize the primary generator according to the choosen particle source
-		gun->design();
+	runManager->SetUserInitialization(myWorld);
+	runManager->SetUserAction(myRunAction);
+	runManager->SetUserAction(gun);
+	runManager->SetUserAction(myStepAction);
+	runManager->SetUserAction(ML2EventAction);
+	runManager->SetUserAction(new CML2TrackingAction);
+	runManager->Initialize();
 
-		CLHEP::HepRandom::setTheEngine(new CLHEP::RanecuEngine);
-		CLHEP::HepRandom :: setTheSeed(myInputData->inputData.generalData.seed);
-
-		myWorld->create(&myInputData->inputData);
-		
-		runManager->SetUserInitialization(myWorld);
-
-
-		runManager->SetUserAction(gun);
-
-// instantiate the convergence control class and assign it to myStepAction
-		CML2Convergence *convergence=new CML2Convergence(myInputData->inputData.generalData.seed, myInputData->inputData.generalData.saving_in_Selected_Voxels_every_events, myInputData->inputData.generalData.fileExperimentalData, myInputData->inputData.generalData.bCompareExp, myInputData->inputData.generalData.minNumberOfEvents);
-		CML2SteppingAction *myStepAction=new CML2SteppingAction(convergence);
-		runManager->SetUserAction(myStepAction);
-
-		runManager->SetUserAction(new CML2TrackingAction);
-
-		runManager->Initialize();
-
-		int nLoop=0;
-		G4Timer MyTime, MyTimeStop;
-		G4double loopElapsedTime;
-
-		G4bool bAgain=true;
-		if (myInputData->bOnlyVisio)
+	// performances info 
+	int nLoop=0;
+	G4Timer MyFullTime;
+	G4double loopElapsedTime;
+	G4bool bStopRun=false;
+	G4bool bNewGeometry=true;
+	if (myInputData->bOnlyVisio)
+	{
+#ifdef G4VIS_USE
+		// visualization
+		myWorld->newGeometry();
+		convergence->setNewGeometry();
+		visio();
+#endif
+	}
+	else
+	{
+		MyFullTime.Start();
+		// compute 
+		while (bNewGeometry)
 		{
-		  visio(argc, argv);
-		}
-		else
-		{
-			MyTimeStop.Start();
-			while (bAgain)
+			bNewGeometry=myWorld->newGeometry();
+			convergence->setNewGeometry();
+			if (bNewGeometry)
 			{
-				std::cout<<"*********************************************"<<'\n';
-				std::cout << "loop n. "<<++nLoop<<'\n';
-					MyTime.Start();
-					std::cout << "Launched "<< myInputData->inputData.generalData.nBeam <<" random primary particles" << '\n';
-
+				if (CML2PhantomConstruction::GetInstance()->getPhantomName()!="Dicom1")
+				{CML2WorldConstruction::GetInstance()->checkVolumeOverlap();}
+				std::cout<<"################ START NEW GEOMETRY ########################"<<'\n';
+				myRunAction->setActualLoop(nLoop);
+				while (!bStopRun)
+				{
 					runManager->BeamOn(myInputData->inputData.generalData.nBeam);
-
-					MyTime.Stop();
-					loopElapsedTime=MyTime.GetUserElapsed();
-					std::cout << "loop elapsed time [s] : "<< loopElapsedTime << '\n';
-					std::cout <<'\n';
-// check if the run have to be reapeted
-				bAgain=convergence->runAgain();
+					// check if the run has to be reapeted
+					bStopRun=convergence->stopRun(); 
+				}
+				nLoop=0;
+			std::cout<<"################ END NEW GEOMETRY ########################"<<'\n';
 			}
+			bStopRun=false;
 		}
-		//job termination
-		delete myInputData;
-//		delete G4MainMessenger;
-		delete myWorld;
-//		delete runManager;
-	return 0;
+		MyFullTime.Stop();
+		loopElapsedTime=MyFullTime.GetUserElapsed();
+		std::cout << "loop elapsed time [s] : "<< loopElapsedTime << '\n';
+		std::cout <<'\n';
+	}
 }
 
