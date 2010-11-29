e483d59d9e37d8be8c5cfe0de6778c89f85d1f7f
Benjamin Morgan <Ben.Morgan@warwick.ac.uk>
2010-11-29 17:15:46 +0000

    Added CVS Name keyword to ensure tags are recorded in file.
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@47597 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/materials/sources.cmake
+++ source/materials/sources.cmake
@@ -11,7 +11,8 @@
 #
 # Generated on : 24/9/2010
 #
-# $Id: sources.cmake,v 1.2 2010-11-26 13:18:10 bmorgan Exp $
+# $Id: sources.cmake,v 1.3 2010-11-29 17:15:46 bmorgan Exp $
+# GEANT4 Tag $Name: not supported by cvs2svn $
 #
 #------------------------------------------------------------------------------
 
