2245a5ff6dcd2456993b645a2945713188e75787
Benjamin Morgan <Ben.Morgan@warwick.ac.uk>
2010-12-02 13:07:28 +0000

    Renamed ConfigConfigure script to better reflect its functionality.
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@47699 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ cmake/Modules/Geant4ConfigureConfigScript.cmake
@@ -0,0 +1,13 @@
+# - Script for configuring and installing geant4-config script
+#
+# The geant4-config script provides an sh based interface to provide
+# information on the Geant4 installation, including installation prefix,
+# version number, compiler and linker flags.
+#
+# The script is generated froma template file and then installed to the
+# known bindir as an executable.
+#
+# $Id: Geant4ConfigureConfigScript.cmake,v 1.1 2010-12-02 13:07:28 bmorgan Exp $
+# GEANT4 Tag $Name: not supported by cvs2svn $
+#
+
