8d948089995c7034c89714e7a6d67eeb9e043d90
Benjamin Morgan <Ben.Morgan@warwick.ac.uk>
2010-12-02 16:59:12 +0000

    Added setting of expansion variables for GDML and Qt.
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@47705 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- cmake/Modules/Geant4ConfigureConfigScript.cmake
+++ cmake/Modules/Geant4ConfigureConfigScript.cmake
@@ -7,7 +7,42 @@
 # The script is generated froma template file and then installed to the
 # known bindir as an executable.
 #
-# $Id: Geant4ConfigureConfigScript.cmake,v 1.1 2010-12-02 13:07:28 bmorgan Exp $
+# $Id: Geant4ConfigureConfigScript.cmake,v 1.2 2010-12-02 16:59:12 bmorgan Exp $
 # GEANT4 Tag $Name: not supported by cvs2svn $
 #
+
+#-----------------------------------------------------------------------------
+# Only create script if we have a global shared library build...
+#
+if(NOT GEANT4_BUILD_GRANULAR_LIBS AND BUILD_SHARED_LIBS AND UNIX)
+    # Setup variables needed for expansion in configuration file
+    # - GDML
+    if(GEANT4_USE_GDML)
+        set(G4_BUILTWITH_GDML "yes")
+    else()
+        set(G4_BUILTWITH_GDML "no")
+    endif()
+
+    # - Qt
+    if(GEANT4_USE_QT)
+        set(G4_BUILTWITH_QT "yes")
+    else()
+        set(G4_BUILTWITH_QT "no")
+    endif()
+
+    # Configure the script
+    configure_file(${CMAKE_SOURCE_DIR}/cmake/Templates/geant4-config.in
+        ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/geant4-config
+        @ONLY)
+
+    # Install it
+    install(FILES ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/geant4-config
+        DESTINATION ${GEANT4_BINDIR}/geant4-config
+        PERMISSIONS
+            OWNER_READ OWNER_WRITE OWNER_EXECUTE
+            GROUP_READ GROUP_EXECUTE
+            WORLD_READ WORLD_EXECUTE)
+else()
+    message(WARNING "geant4-config script will not be generated")
+endif()
 
