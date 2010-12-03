bd1c1cf74704afff70ac61cb76caacc1b118bfab
Benjamin Morgan <Ben.Morgan@warwick.ac.uk>
2010-12-03 15:50:28 +0000

    Corrected softlink install to work correctly with DESTDIR.
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/trunk/geant4@47744 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- cmake/Modules/Geant4ToolchainBackwardCompatibility.cmake
+++ cmake/Modules/Geant4ToolchainBackwardCompatibility.cmake
@@ -30,7 +30,7 @@
 # users to work with the new CMake built libraries transparently if
 # their application relies on the old style toolchain.
 #
-# $Id: Geant4ToolchainBackwardCompatibility.cmake,v 1.7 2010-12-03 15:11:09 bmorgan Exp $
+# $Id: Geant4ToolchainBackwardCompatibility.cmake,v 1.8 2010-12-03 15:50:28 bmorgan Exp $
 # GEANT4 Tag $Name: not supported by cvs2svn $
 #
 
@@ -136,11 +136,11 @@ if(UNIX)
     install(CODE
         "execute_process(COMMAND
         ${CMAKE_COMMAND} -E make_directory
-        ${GEANT4_LIBDIR}/geant4-${geant4_VERSION})")
+        \$ENV{DESTDIR}${GEANT4_LIBDIR}/geant4-${geant4_VERSION})")
     install(CODE
         "execute_process(COMMAND 
         ${CMAKE_COMMAND} -E create_symlink .. ${GEANT4_SYSTEM}-${GEANT4_COMPILER}
-        WORKING_DIRECTORY ${GEANT4_LIBDIR}/geant4-${geant4_VERSION})")
+        WORKING_DIRECTORY \$ENV{DESTDIR}${GEANT4_LIBDIR}/geant4-${geant4_VERSION})")
 
 endif()
 
