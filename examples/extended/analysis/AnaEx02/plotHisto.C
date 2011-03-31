e431e09ef0c705554b48f707850af2d3be2fa555
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2011-03-31 10:21:28 +0000

    Imported from main trunk...
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/09-04-ref-03_branch@49035 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ examples/extended/analysis/AnaEx02/plotHisto.C
@@ -0,0 +1,20 @@
+{
+  gROOT->Reset();
+  
+  // Draw histos filled by Geant4 simulation 
+  //   
+  TFile f = TFile("AnaEx02.root");  
+  TCanvas* c1 = new TCanvas("c1", "  ");
+  
+  TH1D* hist1 = (TH1D*)f.Get("1");
+  hist1->Draw("HIST");
+  
+  TH1D* hist2 = (TH1D*)f.Get("2");
+  hist2->Draw("HIST");
+  
+  TH1D* hist3 = (TH1D*)f.Get("3");
+  c1->SetLogy(1);
+  c1->cd();
+  c1->Update(); 
+  hist3->Draw("HIST");    
+}  
