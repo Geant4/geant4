3256e3072e2198900c84b37a4ae238ef50d111a6
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2011-04-01 06:54:17 +0000

    Merged from main trunk...
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/09-04-ref-03_branch@49049 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ examples/advanced/dnaphysics/include/HistoManager.hh
@@ -0,0 +1,76 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+// $Id: HistoManager.hh,v 1.1 2010/10/06 14:39:41 sincerti Exp $
+// GEANT4 tag $Name:  $
+//
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+#ifndef HistoManager_h
+#define HistoManager_h 1
+
+#include "globals.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+namespace AIDA {
+ class IAnalysisFactory;
+ class ITree;
+ class ITuple;
+}
+
+const G4int MaxNtupl = 1;
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+class HistoManager
+{
+  public:
+
+    HistoManager();
+   ~HistoManager();
+
+    void book();
+    void save();   
+    void FillNtuple(G4int id, G4int column, G4double value);
+    void AddRowNtuple(G4int id);
+
+  private:
+
+    G4String                 fileName[2];
+    G4String                 fileType;
+    G4String                 fileOption;    
+    AIDA::IAnalysisFactory*  af;
+    AIDA::ITree*             tree;
+    AIDA::ITuple*      	     ntupl0;    
+ 
+    G4bool                   factoryOn;
+    
+};
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+#endif
+
