07a17d06d38b56519415786370898d9090dff163
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2011-04-27 09:18:31 +0000

    Tag geant4-09-04-ref-04
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/09-04-ref-04_branch@49342 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/global/management/include/G4Version.hh
+++ source/global/management/include/G4Version.hh
@@ -50,14 +50,14 @@
 #endif
 
 #ifndef G4VERSION_TAG
-#define G4VERSION_TAG "$Name: geant4-09-04-ref-03 $"
+#define G4VERSION_TAG "$Name: geant4-09-04-ref-04 $"
 #endif
 
 // as variables
 
 #include "G4String.hh"
 
-static const G4String G4Version = "$Name: geant4-09-04-ref-03 $";
-static const G4String G4Date    = "(31-March-2011)";
+static const G4String G4Version = "$Name: geant4-09-04-ref-04 $";
+static const G4String G4Date    = "(30-April-2011)";
 
 #endif
