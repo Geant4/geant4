190347adae11d336e480a52fbc6476a49396ce35
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2011-06-21 09:54:12 +0000

    Merged from tag particles-V09-04-09
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/09-04-ref-06_branch@50983 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/particles/management/src/G4PrimaryParticle.cc
+++ source/particles/management/src/G4PrimaryParticle.cc
@@ -40,7 +40,7 @@ G4PrimaryParticle::G4PrimaryParticle()
 :PDGcode(0),G4code(0),
  direction(0.,0.,1.),kinE(0.),
  nextParticle(0),daughterParticle(0),trackID(-1),
- mass(-1.),charge(DBL_MAX),polX(0.),polY(0.),polZ(0.),
+ mass(-1.),charge(0.),polX(0.),polY(0.),polZ(0.),
  Weight0(1.0),properTime(0.0),userInfo(0)
 {;}
 
@@ -48,12 +48,13 @@ G4PrimaryParticle::G4PrimaryParticle(G4int Pcode)
 :PDGcode(Pcode),
  direction(0.,0.,1.),kinE(0.),
  nextParticle(0),daughterParticle(0),trackID(-1),
- mass(-1.),charge(DBL_MAX),polX(0.),polY(0.),polZ(0.),
+ mass(-1.),charge(0.),polX(0.),polY(0.),polZ(0.),
  Weight0(1.0),properTime(0.0),userInfo(0)
 { 
   G4code = G4ParticleTable::GetParticleTable()->FindParticle(Pcode); 
   if (G4code !=0) {
-    mass = G4code->GetPDGMass();
+    mass   = G4code->GetPDGMass();
+    charge = G4code->GetPDGCharge();
   } 
 }
 
@@ -62,12 +63,13 @@ G4PrimaryParticle::G4PrimaryParticle(G4int Pcode,
 :PDGcode(Pcode),
  direction(0.,0.,1.),kinE(0.),
  nextParticle(0),daughterParticle(0),trackID(-1),
- mass(-1.),charge(DBL_MAX),polX(0.),polY(0.),polZ(0.),
+ mass(-1.),charge(0.),polX(0.),polY(0.),polZ(0.),
  Weight0(1.0),properTime(0.0),userInfo(0)
 { 
   G4code = G4ParticleTable::GetParticleTable()->FindParticle(Pcode); 
   if (G4code !=0) {
-    mass = G4code->GetPDGMass();
+    mass   = G4code->GetPDGMass();
+    charge = G4code->GetPDGCharge();
   } 
   SetMomentum( px, py, pz);
 }
@@ -77,10 +79,14 @@ G4PrimaryParticle::G4PrimaryParticle(G4int Pcode,
 :PDGcode(Pcode),
  direction(0.,0.,1.),kinE(0.),
  nextParticle(0),daughterParticle(0),trackID(-1),
- charge(DBL_MAX),polX(0.),polY(0.),polZ(0.),
+ charge(0.),polX(0.),polY(0.),polZ(0.),
  Weight0(1.0),properTime(0.0),userInfo(0)
 {
  G4code = G4ParticleTable::GetParticleTable()->FindParticle(Pcode); 
+ if (G4code !=0) {
+    mass = G4code->GetPDGMass();
+    charge = G4code->GetPDGCharge();
+  } 
  Set4Momentum( px, py, pz, E); 
 }
 
@@ -88,12 +94,13 @@ G4PrimaryParticle::G4PrimaryParticle(const G4ParticleDefinition* Gcode)
 :G4code(Gcode),
  direction(0.,0.,1.),kinE(0.),
  nextParticle(0),daughterParticle(0),trackID(-1),
- mass(-1.),charge(DBL_MAX),polX(0.),polY(0.),polZ(0.),
+ mass(-1.),charge(0.),polX(0.),polY(0.),polZ(0.),
  Weight0(1.0),properTime(0.0),userInfo(0)
 { 
   if (G4code !=0) {
-    PDGcode = G4code->GetPDGEncoding(); 
+    PDGcode = Gcode->GetPDGEncoding(); 
     mass = G4code->GetPDGMass();
+    charge = G4code->GetPDGCharge();
   } 
 }
 
@@ -102,12 +109,13 @@ G4PrimaryParticle::G4PrimaryParticle(const G4ParticleDefinition* Gcode,
 :G4code(Gcode),
  direction(0.,0.,1.),kinE(0.),
  nextParticle(0),daughterParticle(0),trackID(-1),
- mass(-1.),charge(DBL_MAX),polX(0.),polY(0.),polZ(0.),
+ mass(-1.),charge(0.),polX(0.),polY(0.),polZ(0.),
  Weight0(1.0),properTime(0.0),userInfo(0)
 { 
   if (G4code !=0) {
-    PDGcode = G4code->GetPDGEncoding(); 
+    PDGcode = Gcode->GetPDGEncoding(); 
     mass = G4code->GetPDGMass();
+    charge = G4code->GetPDGCharge();
   } 
   SetMomentum( px, py, pz);
 }
@@ -117,24 +125,82 @@ G4PrimaryParticle::G4PrimaryParticle(const G4ParticleDefinition* Gcode,
 :G4code(Gcode),
  direction(0.,0.,1.),kinE(0.),
  nextParticle(0),daughterParticle(0),trackID(-1),
- charge(DBL_MAX),polX(0.),polY(0.),polZ(0.),
+ charge(0.),polX(0.),polY(0.),polZ(0.),
  Weight0(1.0),properTime(0.0),userInfo(0)
 {
   if (G4code !=0) {
-    PDGcode = G4code->GetPDGEncoding(); 
+    PDGcode = Gcode->GetPDGEncoding(); 
     mass = G4code->GetPDGMass();
+    charge = G4code->GetPDGCharge();
   } 
- Set4Momentum( px, py, pz, E); 
+  Set4Momentum( px, py, pz, E); 
 }
+
+G4PrimaryParticle::G4PrimaryParticle(const G4PrimaryParticle& right)
+:PDGcode(0),G4code(0),
+ direction(0.,0.,1.),kinE(0.),
+ nextParticle(0),daughterParticle(0),trackID(-1),
+ mass(-1.),charge(0.),polX(0.),polY(0.),polZ(0.),
+ Weight0(1.0),properTime(0.0),userInfo(0)
+{
+  *this = right;
+}
+
+const G4PrimaryParticle & G4PrimaryParticle::operator=(const G4PrimaryParticle & right)
+{ 
+  if (this != &right) {
+    PDGcode      = right.PDGcode;
+    G4code       = right.G4code;
+    direction    = right.direction;
+    kinE         = right.kinE;
+    if (nextParticle !=0) delete nextParticle;
+    if ( right.nextParticle ==0 ){
+      nextParticle = 0;
+    } else {
+      nextParticle = new G4PrimaryParticle(*right.nextParticle);
+    }
+    if (daughterParticle !=0) delete daughterParticle;
+    if ( right.daughterParticle ==0 ){
+      daughterParticle = 0;
+    } else {
+      daughterParticle = new G4PrimaryParticle(*right.daughterParticle);
+    }
+    trackID      = right.trackID;
+    mass         = right.mass;
+    charge       = right.charge;
+    polX         = right.polX;
+    polY         = right.polY;
+    polZ         = right.polZ;
+    Weight0      = right.Weight0;
+    properTime   = right.properTime;
+
+    // userInfo can not be copied
+    userInfo = 0;
+  }
+  
+  return *this; 
+}
+
+G4int G4PrimaryParticle::operator==(const G4PrimaryParticle &right) const
+{ return (this==&right); }
+
+G4int G4PrimaryParticle::operator!=(const G4PrimaryParticle &right) const
+{ return (this!=&right); }
 
 G4PrimaryParticle::~G4PrimaryParticle()
 {
-  if(nextParticle != 0)
-  { delete nextParticle; }
-  if(daughterParticle != 0)
-  { delete daughterParticle; }
-  if(userInfo!=0)
-  { delete userInfo; }
+  if(nextParticle != 0){ 
+    delete nextParticle;
+    nextParticle = 0;
+  }
+  if(daughterParticle != 0){
+    delete daughterParticle; 
+    daughterParticle =0;
+  }
+  if(userInfo!=0) {
+    delete userInfo; 
+    userInfo=0;
+  }
 }
 
 void G4PrimaryParticle::SetMomentum(G4double px, G4double py, G4double pz)
@@ -142,18 +208,18 @@ void G4PrimaryParticle::SetMomentum(G4double px, G4double py, G4double pz)
   if ((mass<0.)&&(G4code!=0)){ 
     mass =  G4code->GetPDGMass(); 
   }
-  G4double pmom =  sqrt(px*px+py*py+pz*pz);
+  G4double pmom =  std::sqrt(px*px+py*py+pz*pz);
   if (pmom>0.0) {
     direction.setX(px/pmom);
     direction.setY(py/pmom);
     direction.setZ(pz/pmom);
   }
-  kinE = sqrt(px*px+py*py+pz*pz+mass*mass)-mass;
+  kinE = std::sqrt(px*px+py*py+pz*pz+mass*mass)-mass;
 }
 
 void G4PrimaryParticle::Set4Momentum(G4double px, G4double py, G4double pz, G4double E)
 { 
-  G4double pmom =  sqrt(px*px+py*py+pz*pz);
+  G4double pmom =  std::sqrt(px*px+py*py+pz*pz);
   if (pmom>0.0) {
     direction.setX(px/pmom);
     direction.setY(py/pmom);
@@ -166,7 +232,7 @@ void G4PrimaryParticle::Set4Momentum(G4double px, G4double py, G4double pz, G4do
     if (G4code!=0){ 
       mass =  G4code->GetPDGMass(); 
     }
-    E = sqrt(pmom*pmom+mass*mass);
+    E = std::sqrt(pmom*pmom+mass*mass);
   }
   kinE = E - mass;
 }
@@ -177,54 +243,19 @@ void G4PrimaryParticle::SetPDGcode(G4int Pcode)
   G4code = G4ParticleTable::GetParticleTable()->FindParticle(Pcode);
   if (G4code!=0){ 
     mass =  G4code->GetPDGMass(); 
+    charge = G4code->GetPDGCharge();
   }
-}
-
-void G4PrimaryParticle::SetG4code(const G4ParticleDefinition* Gcode)
-{
-  SetParticleDefinition(Gcode);
 }
 
 void G4PrimaryParticle::SetParticleDefinition(const G4ParticleDefinition* Gcode)
 {
   G4code = Gcode;
   if (G4code!=0){ 
-    PDGcode = G4code->GetPDGEncoding();
+    PDGcode = Gcode->GetPDGEncoding();
     mass =  G4code->GetPDGMass(); 
+    charge = G4code->GetPDGCharge();
   }
 }
-
-G4double G4PrimaryParticle::GetMass() const
-{
-  if (mass <0.0 ) {
-    if (G4code != 0) {
-      // return PDG mass if dynamical mass has not be specified 
-      return G4code->GetPDGMass();
-    }
-  }
-  return mass; 
-}
-
-G4double G4PrimaryParticle::GetCharge() const
-{
-  if ( charge <DBL_MAX ) {
-    return charge;
-  } else {
-    if (G4code != 0) {
-      // return PDG charge if dynamical mass has not be specified 
-      return G4code->GetPDGCharge();
-    }
-  }
-  return charge; 
-}
-
-const G4PrimaryParticle & 
-G4PrimaryParticle::operator=(const G4PrimaryParticle &)
-{ return *this; }
-G4int G4PrimaryParticle::operator==(const G4PrimaryParticle &right) const
-{ return (this==&right); }
-G4int G4PrimaryParticle::operator!=(const G4PrimaryParticle &right) const
-{ return (this!=&right); }
 
 void G4PrimaryParticle::Print() const
 {
@@ -233,37 +264,36 @@ void G4PrimaryParticle::Print() const
   { G4cout << G4code->GetParticleName() << G4endl; }
   else
   { G4cout << " is not defined in G4." << G4endl; }
-  if(charge<DBL_MAX)
-  { G4cout << " Assigned charge : " << charge/eplus  << G4endl; }
-  else
-  { G4cout << " Charge will be taken from PDG charge." << G4endl; }
+  G4cout << " Assigned charge : " << charge/eplus  << G4endl; 
   G4cout << "     Momentum ( " 
 	 << GetTotalMomentum()*direction.x()/GeV << "[GeV/c], " 
 	 << GetTotalMomentum()*direction.y()/GeV << "[GeV/c], " 
 	 << GetTotalMomentum()*direction.z()/GeV << "[GeV/c] )" << G4endl;
   G4cout << "     kinetic Energy : " << kinE/GeV  << " [GeV]" << G4endl;
-  if(mass>=0.)
-    { G4cout << "     Mass : " << mass/GeV << " [GeV]" << G4endl; }
-  else
-    { G4cout << "     Nominal mass will be taken from particle definition." << G4endl; }
+  if(mass>=0.){ 
+    G4cout << "     Mass : " << mass/GeV << " [GeV]" << G4endl; 
+  } else { 
+    G4cout << "     Mass is not assigned " << G4endl; 
+  }
   G4cout << "     Polarization ( " 
 	 << polX << ", " 
 	 << polY << ", "
 	 << polZ << " )" 
 	 << G4endl;
   G4cout << "     Weight : " << Weight0 << G4endl;
-  if(properTime>0.0)
-  { G4cout << "     PreAssigned proper decay time : " << properTime/ns << " [ns] " << G4endl; }
-  if(userInfo != 0) userInfo->Print();
-  if(daughterParticle != 0)
-  {
+  if(properTime>0.0) { 
+    G4cout << "     PreAssigned proper decay time : " << properTime/ns << " [ns] " << G4endl; 
+  }
+  if(userInfo != 0) { userInfo->Print(); }
+  if(daughterParticle != 0) {
     G4cout << ">>>> Daughters" << G4endl;
     daughterParticle->Print();
   }
-  if(nextParticle != 0)
-  { nextParticle->Print(); }
-  else
-  { G4cout << "<<<< End of link" << G4endl; }
+  if(nextParticle != 0) { 
+    nextParticle->Print(); 
+  } else { 
+    G4cout << "<<<< End of link" << G4endl; 
+  }
 }
 
 
