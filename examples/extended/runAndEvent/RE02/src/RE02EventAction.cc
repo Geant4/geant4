e460e80b30c2635b342a0e31cf931d73c39f6864
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2011-10-28 11:50:16 +0000

    Merged from tag exampleRE02-V09-04-00
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/09-04-ref-10_branch@53559 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/runAndEvent/RE02/src/RE02EventAction.cc
+++ examples/extended/runAndEvent/RE02/src/RE02EventAction.cc
@@ -52,18 +52,19 @@ void RE02EventAction::EndOfEventAction(const G4Event* evt)
 {
   G4int event_id = evt->GetEventID();
   
-  // get number of stored trajectories
-  //
-  G4TrajectoryContainer* trajectoryContainer = evt->GetTrajectoryContainer();
-  G4int n_trajectories = 0;
-  if (trajectoryContainer) n_trajectories = trajectoryContainer->entries();
-  
   // periodic printing
   //
   if (event_id < 100 || event_id%100 == 0) {
     G4cout << ">>> Event " << evt->GetEventID() << G4endl;
-    //G4cout << "    " << n_trajectories 
-    //       << " trajectories stored in this event." << G4endl;
+#ifdef print_stored_trajectories
+    // get number of stored trajectories
+    //
+    G4TrajectoryContainer* trajectoryContainer = evt->GetTrajectoryContainer();
+    G4int n_trajectories = 0;
+    if (trajectoryContainer) n_trajectories = trajectoryContainer->entries();
+    G4cout << "    " << n_trajectories 
+           << " trajectories stored in this event." << G4endl;
+#endif
   }
 
 }
