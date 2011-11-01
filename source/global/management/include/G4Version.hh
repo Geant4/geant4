1c9acdd7359dfa9e525b1dc19f624a73dd76edef
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2011-11-01 14:07:48 +0000

    Updated tag in banner to ref-10
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/09-04-ref-10_branch@53892 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/global/management/include/G4Version.hh
+++ source/global/management/include/G4Version.hh
@@ -50,14 +50,14 @@
 #endif
 
 #ifndef G4VERSION_TAG
-#define G4VERSION_TAG "$Name: geant4-09-04-ref-09 $"
+#define G4VERSION_TAG "$Name: geant4-09-04-ref-10 $"
 #endif
 
 // as variables
 
 #include "G4String.hh"
 
-static const G4String G4Version = "$Name: geant4-09-04-ref-09 $";
-static const G4String G4Date    = "(7-October-2011)";
+static const G4String G4Version = "$Name: geant4-09-04-ref-10 $";
+static const G4String G4Date    = "(4-November-2011)";
 
 #endif
