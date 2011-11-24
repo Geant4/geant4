4c7bba663cf091b73f9785645f482fb2bf744c82
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2011-11-24 15:15:15 +0000

    Merged from tag cmake-V09-04-39
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/geant4-09-05_branch@55511 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- cmake/Modules/Geant4InstallData.cmake
+++ cmake/Modules/Geant4InstallData.cmake
@@ -16,13 +16,14 @@ option(GEANT4_INSTALL_EXAMPLES "Install source code for Geant4 examples" OFF)
 mark_as_advanced(GEANT4_INSTALL_EXAMPLES)
 
 if(GEANT4_INSTALL_EXAMPLES)
-    install(DIRECTORY examples
-        DESTINATION ${CMAKE_INSTALL_DATAROOTDIR}/Geant4-${Geant4_VERSION}
-        COMPONENT Examples
-        PATTERN "CVS" EXCLUDE
-        PATTERN ".svn" EXCLUDE)
+  install(DIRECTORY examples
+    DESTINATION ${CMAKE_INSTALL_DATAROOTDIR}/Geant4-${Geant4_VERSION}
+    COMPONENT Examples
+    PATTERN "CVS" EXCLUDE
+    PATTERN ".svn" EXCLUDE)
 endif()
 
+GEANT4_ADD_FEATURE(GEANT4_INSTALL_EXAMPLES "Will install source code for Geant4 examples")
 
 #----------------------------------------------------------------------------
 # Install data libraries if requested
@@ -37,10 +38,10 @@ if(${CMAKE_VERSION} VERSION_GREATER 2.7)
     include(ExternalProject)
     set(_urlprefix "http://geant4.cern.ch/support/source")
     set(GEANT4_DATASETS
-      G4NDL/3.16/G4NDL/tar.gz/G4NEUTRONHPDATA
+      G4NDL/4.0/G4NDL/tar.gz/G4NEUTRONHPDATA
       G4EMLOW/6.23/G4EMLOW/tar.gz/G4LEDATA
       PhotonEvaporation/2.2/PhotonEvaporation/tar.gz/G4LEVELGAMMADATA
-      RadioactiveDecay/3.3/G4RadioactiveDecay/tar.gz/G4RADIOACTIVEDATA
+      RadioactiveDecay/3.4/G4RadioactiveDecay/tar.gz/G4RADIOACTIVEDATA
       G4ABLA/3.0/G4ABLA/tar.gz/G4ABLADATA
       G4NEUTRONXS/1.1/G4NEUTRONXS/tar.gz/G4NEUTRONXSDATA
       G4PII/1.3/G4PII/tar.gz/G4PIIDATA
@@ -67,8 +68,11 @@ if(${CMAKE_VERSION} VERSION_GREATER 2.7)
         DESTINATION ${CMAKE_INSTALL_DATAROOTDIR}/Geant4-${Geant4_VERSION}/data
         COMPONENT Data
       )
+
     endforeach()
   endif()
+
+  GEANT4_ADD_FEATURE(GEANT4_INSTALL_DATA "Will download and install data libraries")
 endif()
 
 
