1df92967fc167e24f0c1ce4a05832eff7193befa
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2011-12-01 14:10:09 +0000

    Corrected text...
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/geant4-09-05_branch@55716 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- cmake/Modules/Geant4CPackBase.cmake
+++ cmake/Modules/Geant4CPackBase.cmake
@@ -37,7 +37,6 @@ Its areas of application include high energy, nuclear and accelerator physics,
 as well as studies in medical and space science (http://www.geant4.org).
 
 This installer has been created using CPack (http://www.cmake.org). 
-No additional installation instructions provided at present.
 ")
 
 #----------------------------------------------------------------------------
