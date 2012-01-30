b99fbd11003d7b08c1879d1f19ac3c3d44e4dcaf
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2012-01-30 16:46:28 +0000

    Changed date and tag to ref-01
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/09-05-ref-01_branch@56396 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/global/management/include/G4Version.hh
+++ source/global/management/include/G4Version.hh
@@ -50,14 +50,14 @@
 #endif
 
 #ifndef G4VERSION_TAG
-#define G4VERSION_TAG "$Name: geant4-09-05-ref-00 $"
+#define G4VERSION_TAG "$Name: geant4-09-05-ref-01 $"
 #endif
 
 // as variables
 
 #include "G4String.hh"
 
-static const G4String G4Version = "$Name: geant4-09-05-ref-00 $";
-static const G4String G4Date    = "(2-December-2011)";
+static const G4String G4Version = "$Name: geant4-09-05-ref-01 $";
+static const G4String G4Date    = "(31-January-2012)";
 
 #endif
