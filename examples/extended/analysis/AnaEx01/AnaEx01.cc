9f5983f16149d68ce708dc5e8a500494c47146c5
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2012-04-24 16:25:43 +0000

    Merged from tag AnEx01-V09-05-04
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/09-05-ref-04_branch@58138 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/analysis/AnaEx01/AnaEx01.cc
+++ examples/extended/analysis/AnaEx01/AnaEx01.cc
@@ -89,7 +89,7 @@ int main(int argc,char** argv)
   // Initialize G4 kernel
   //
   runManager->Initialize();
-  
+
   // Get the pointer to the User Interface manager
   //
   G4UImanager* UImanager = G4UImanager::GetUIpointer();
@@ -105,11 +105,13 @@ int main(int argc,char** argv)
 #ifdef G4VIS_USE
       G4VisManager* visManager = new G4VisExecutive;
       visManager->Initialize();
+#ifdef G4UI_USE
+      G4UIExecutive* ui = new G4UIExecutive(argc, argv);
+#endif
       UImanager->ApplyCommand("/control/execute vis.mac");     
 #endif
 
 #ifdef G4UI_USE
-      G4UIExecutive* ui = new G4UIExecutive(argc, argv);
       ui->SessionStart();
       delete ui;
 #endif
