e345bb489a8e9edf2f74c89416231a981664b0d9
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2012-04-24 16:47:48 +0000

    Merged from tag test74-V09-05-00
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/09-05-ref-04_branch@58175 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ tests/test74/src/DetectorConstruction.cc
@@ -0,0 +1,100 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+// -------------------------------------------------------------------
+// $Id: DetectorConstruction.cc,v 1.0 2012-04-04 mraine $
+// -------------------------------------------------------------------
+
+#include "DetectorConstruction.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+DetectorConstruction::DetectorConstruction()
+:physiWorld(NULL), logicWorld(NULL), solidWorld(NULL)
+{}  
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+DetectorConstruction::~DetectorConstruction()
+{}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+G4VPhysicalVolume* DetectorConstruction::Construct()
+
+{
+  DefineMaterials();
+  return ConstructDetector();
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+void DetectorConstruction::DefineMaterials()
+{ 
+  // Silicon is defined from NIST material database
+  G4NistManager * man = G4NistManager::Instance();
+  G4Material * Si = man->FindOrBuildMaterial("G4_Si");
+
+  // Default materials in setup.
+  SiMaterial = Si;
+  aMaterial = Si;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+G4VPhysicalVolume* DetectorConstruction::ConstructDetector()
+{
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+  // WORLD VOLUME
+  
+  WorldSizeX  = 1*m; 
+  WorldSizeY  = 1*m; 
+  WorldSizeZ  = 1*m;
+
+  solidWorld = new G4Box("World",				     //its name
+			   WorldSizeX/2,WorldSizeY/2,WorldSizeZ/2);  //its size
+  
+
+  logicWorld = new G4LogicalVolume(solidWorld,	        //its solid
+				   SiMaterial,	//its material
+				   "World");		//its name
+  
+  physiWorld = new G4PVPlacement(0,			//no rotation
+  				 G4ThreeVector(),	//at (0,0,0)
+                                 "World",		//its name
+                                 logicWorld,		//its logical volume
+                                 NULL,			//its mother  volume
+                                 false,			//no boolean operation
+                                 0);			//copy number
+
+ 
+  // Visualization attributes
+  G4VisAttributes* worldVisAtt= new G4VisAttributes(G4Colour(1.0,1.0,1.0)); //White
+  worldVisAtt->SetVisibility(true);
+  logicWorld->SetVisAttributes(worldVisAtt);
+
+  return physiWorld;
+}
---
+++ tests/test74/src/PhysicsList.cc
@@ -0,0 +1,169 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+// -------------------------------------------------------------------
+// $Id: PhysicsList.cc,v 1.0 2012-04-04 mraine $
+// -------------------------------------------------------------------
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+#include "PhysicsList.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+PhysicsList::PhysicsList():  G4VUserPhysicsList()
+{
+  defaultCutValue = 1*nanometer;
+  cutForGamma     = defaultCutValue;
+  cutForElectron  = defaultCutValue;
+  cutForPositron  = defaultCutValue;
+  cutForProton    = defaultCutValue;
+  
+  SetVerboseLevel(1);
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+PhysicsList::~PhysicsList()
+{}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+void PhysicsList::ConstructParticle()
+{
+  ConstructBosons();
+  ConstructLeptons();
+  ConstructBarions();
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+void PhysicsList::ConstructBosons()
+{ 
+
+  // gamma
+  G4Gamma::GammaDefinition();
+
+  // optical photon
+  G4OpticalPhoton::OpticalPhotonDefinition();
+}
+ //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+void PhysicsList::ConstructLeptons()
+{
+  // leptons
+  G4Electron::ElectronDefinition();
+  G4Positron::PositronDefinition();
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+void PhysicsList::ConstructBarions()
+{
+  //  baryons
+  G4Proton::ProtonDefinition();
+  G4AntiProton::AntiProtonDefinition();
+  G4GenericIon::GenericIonDefinition();
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+void PhysicsList::ConstructProcess()
+{
+  AddTransportation();
+  ConstructEM();
+  ConstructGeneral();
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+// Geant4-MuElec MODELS
+
+#include "G4MuElecElastic.hh"
+#include "G4MuElecInelastic.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+void PhysicsList::ConstructEM()
+{
+  theParticleIterator->reset();
+
+  while( (*theParticleIterator)() )
+  {
+
+    G4ParticleDefinition* particle = theParticleIterator->value();
+    G4ProcessManager* pmanager = particle->GetProcessManager();
+    G4String particleName = particle->GetParticleName();
+
+// Geant4-MuElec
+
+    if (particleName == "e-") {
+
+      pmanager->AddDiscreteProcess(new G4MuElecElastic("e-_G4MuElecElastic"));
+      pmanager->AddDiscreteProcess(new G4MuElecInelastic("e-_G4MuElecInelastic"));
+     	    
+    } else if ( particleName == "proton" ) {
+
+      pmanager->AddDiscreteProcess(new G4MuElecInelastic("p_G4MuElecInelastic"));
+
+    }  else if ( particleName == "GenericIon" ) {
+
+      pmanager->AddDiscreteProcess(new G4MuElecInelastic("ion_G4MuElecInelastic"));
+
+    }
+
+  } // Loop on particles
+
+
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+void PhysicsList::ConstructGeneral()
+{ }
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
+
+void PhysicsList::SetCuts()
+{
+  if (verboseLevel >0)
+  {
+    G4cout << "PhysicsList::SetCuts:";
+    G4cout << "CutLength : " << G4BestUnit(defaultCutValue,"Length") << G4endl;
+  }  
+  
+  // set cut values for gamma at first and for e- second and next for e+,
+  // because some processes for e+/e- need cut values for gamma 
+  SetCutValue(cutForGamma, "gamma");
+  SetCutValue(cutForElectron, "e-");
+  SetCutValue(cutForPositron, "e+");
+  
+  // set cut values for proton and anti_proton before all other hadrons
+  // because some processes for hadrons need cut values for proton/anti_proton 
+  SetCutValue(cutForProton, "proton");
+  SetCutValue(cutForProton, "anti_proton");
+  
+  if (verboseLevel>0) DumpCutValuesTable();
+}
