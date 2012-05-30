00ef2731daa4a5cc39f8a58830db568794943566
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2012-05-30 13:57:23 +0000

    Merged from tag geommng-V09-05-01
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/09-05-ref-05_branch@58978 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/geometry/management/src/G4ErrorPlaneSurfaceTarget.cc
+++ source/geometry/management/src/G4ErrorPlaneSurfaceTarget.cc
@@ -44,8 +44,8 @@
 //---------------------------------------------------------------------
 
 G4ErrorPlaneSurfaceTarget::
-G4ErrorPlaneSurfaceTarget(G4double a, G4double b, G4double c, G4double d)
-  : G4Plane3D( a, b, c, d ) 
+G4ErrorPlaneSurfaceTarget(G4double aa, G4double ab, G4double ac, G4double ad)
+  : G4Plane3D( aa, ab, ac, ad ) 
 {
   theType = G4ErrorTarget_PlaneSurface;
 
@@ -60,8 +60,8 @@ G4ErrorPlaneSurfaceTarget(G4double a, G4double b, G4double c, G4double d)
 //---------------------------------------------------------------------
 
 G4ErrorPlaneSurfaceTarget::
-G4ErrorPlaneSurfaceTarget(const G4Normal3D &n, const G4Point3D &p)
-  : G4Plane3D( n, p ) 
+G4ErrorPlaneSurfaceTarget(const G4Normal3D &norm, const G4Point3D &pt)
+  : G4Plane3D( norm, pt ) 
 {
   theType = G4ErrorTarget_PlaneSurface;
 
