20086672d883012276c9ac34795f2e29c0cd8347
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2012-06-15 09:45:17 +0000

    Merged from tag op-V09-05-03
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/09-05-ref-06_branch@59732 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/optical/src/G4OpWLS.cc
+++ source/processes/optical/src/G4OpWLS.cc
@@ -313,12 +313,12 @@ void G4OpWLS::BuildThePhysicsTable()
 	    // loop over all (photon energy, intensity)
 	    // pairs stored for this material
 
-            for (size_t i = 1;
-                 i < theWLSVector->GetVectorLength();
-                 i++)	    
+            for (size_t j = 1;
+                 j < theWLSVector->GetVectorLength();
+                 j++)	    
 	      {
-		currentPM = theWLSVector->Energy(i);
-		currentIN = (*theWLSVector)[i];
+		currentPM = theWLSVector->Energy(j);
+		currentIN = (*theWLSVector)[j];
 		
 		currentCII = 0.5 * (prevIN + currentIN);
 		
