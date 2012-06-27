8c04e39693c487c23fc902f97626162764c2f056
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2012-06-27 16:21:53 +0000

    Merged from tag radioactive_decay-V09-05-03
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/09-05-ref-06_branch@60070 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/models/radioactive_decay/src/G4BetaDecayCorrections.cc
+++ source/processes/hadronic/models/radioactive_decay/src/G4BetaDecayCorrections.cc
@@ -31,7 +31,8 @@
 G4BetaDecayCorrections::G4BetaDecayCorrections(G4int theZ, G4int theA)
  : Z(theZ), A(theA)
 {
-  alphaZ = fine_structure_const*std::abs(Z);
+  // alphaZ = fine_structure_const*std::abs(Z);
+  alphaZ = fine_structure_const*Z;
 
   // Nuclear radius in units of hbar/m_e/c
   Rnuc = 0.5*fine_structure_const*std::pow(A, 0.33333);
@@ -62,7 +63,8 @@ G4double G4BetaDecayCorrections::FermiFunction(const G4double& W)
     Wprime = W + V0;
   } else {
     Wprime = W - V0;
-    if (Wprime < 1.) Wprime = W;
+//    if (Wprime < 1.) Wprime = W;
+    if (Wprime <= 1.00001) Wprime = 1.00001;
   }
 
   G4double p_e = std::sqrt(Wprime*Wprime - 1.);
