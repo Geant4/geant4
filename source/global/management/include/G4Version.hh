c161260da162d5baaaa8f0099f13eaebebf9d6c6
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2012-08-09 07:29:57 +0000

    Corrected tag name to ref-07
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/09-05-ref-07_branch@60932 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/global/management/include/G4Version.hh
+++ source/global/management/include/G4Version.hh
@@ -50,14 +50,14 @@
 #endif
 
 #ifndef G4VERSION_TAG
-#define G4VERSION_TAG "$Name: geant4-09-05-ref-06 $"
+#define G4VERSION_TAG "$Name: geant4-09-05-ref-07 $"
 #endif
 
 // as variables
 
 #include "G4String.hh"
 
-static const G4String G4Version = "$Name: geant4-09-05-ref-06 $";
+static const G4String G4Version = "$Name: geant4-09-05-ref-07 $";
 static const G4String G4Date    = "(10-August-2012)";
 
 #endif
