a56b82b7ddfb3f03a53c4979f91a0c20d297e160
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2012-10-30 15:39:16 +0000

    Merged from tag testem3-V09-05-05
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/09-05-ref-10_branch@64080 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/electromagnetic/TestEm3/src/HistoManager.cc
+++ examples/extended/electromagnetic/TestEm3/src/HistoManager.cc
@@ -65,7 +65,7 @@ void HistoManager::Book()
   
   const G4String id[] = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
                          "10","11","12","13","14","15","16","17","18","19",
-			 "20","21","22"};
+                         "20","21","22"};
   G4String title;
 
   // Default values (to be reset via /analysis/h1/set command)
