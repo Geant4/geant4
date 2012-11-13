b4596116e10e1e18ff5fd96d50ee4618821c36fd
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2012-11-13 08:30:28 +0000

    Merged from tag nanobeam-V09-05-05
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/09-06-cand-00_branch@65081 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/advanced/nanobeam/include/DetectorConstruction.hh
+++ examples/advanced/nanobeam/include/DetectorConstruction.hh
@@ -78,6 +78,10 @@ public:
   G4float G1, G2, G3, G4;
   G4int model, coef, profile, grid;
 
+  G4LogicalVolume* GetLogicalWorld() {return logicWorld;};
+  G4LogicalVolume* GetLogicalVol() {return logicVol;};
+  G4LogicalVolume* GetLogicalGrid() {return logicControlVol_GridShadow;};
+
 private:
    
   void DefineMaterials();
