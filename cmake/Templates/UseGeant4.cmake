c6953de2be123dd89ea31d2e5821475e33fd909c
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2012-11-21 16:28:10 +0000

    Merged from tag cmake-V09-05-75
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/09-06-cand-00_branch@65559 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- cmake/Templates/UseGeant4.cmake
+++ cmake/Templates/UseGeant4.cmake
@@ -1,25 +1,31 @@
 # - Use file for Geant4
 # This file should be included after a find_package call has successfully
-# located Geant4.
+# located Geant4. If Geant4 has been located via the Geant4Config.cmake
+# config file, this will have set the following variable:
 #
 #  Geant4_USE_FILE : Point to the location of the use file for the found
 #                    Geant4 installation.
 #
-# Inclusion of this file results in the addition of the Geant4 compile
-# definitions and include directories to those of the directory in which
-# this files is included.
+# Inclusion of this file, e.g. via
+# 
+#  include(${Geant4_USE_FILE})
+#
+# results in the addition of the Geant4 compile definitions and 
+# include directories to those of the directory in which this file is 
+# included.
 #
 # The recommended Geant4 compiler flags are also added to CMAKE_CXX_FLAGS
 # but duplicated flags are NOT removed.
-# Advanced users requiring special sets of flags, or the removal of duplicate
-# flags should therefore *not* use this file, preferring the direct use of the 
-# Geant4_XXXX variables set by the Geant4Config file.
+# Advanced users requiring special sets of flags, or the removal of 
+# duplicate flags should therefore *not* use this file, preferring the 
+# direct use of the Geant4_XXXX variables set by the Geant4Config file.
 #
 # The use file also defines a simple macro to help with collating sources
 # for simple user applications.
 #
 #  macro GEANT4_COLLATE_APPLICATION_SOURCES(sources)
-#        Create the list of sources in a standard Geant4 application example.
+#        Create the list of sources in a standard Geant4 application 
+#        example.
 #        If the project is organized as
 #        +- project
 #           +- CMakeLists.txt
@@ -62,12 +68,13 @@
 # macro GEANT4_COLLATE_SOURCES(source_dest_var)
 #
 macro(GEANT4_COLLATE_APPLICATION_SOURCES source_dest_var)
-    file(GLOB_RECURSE 
-        ${source_dest_var} 
-        ${CMAKE_CURRENT_SOURCE_DIR}/*.hh 
-        ${CMAKE_CURRENT_SOURCE_DIR}/*.cc
+  message(" WARNING: macro geant4_collate_application sources is deprecated and will be removed in GEANT4 version 10.0.")
+  file(GLOB_RECURSE 
+    ${source_dest_var} 
+    ${CMAKE_CURRENT_SOURCE_DIR}/*.hh 
+    ${CMAKE_CURRENT_SOURCE_DIR}/*.cc
     )
-    include_directories(${CMAKE_CURRENT_SOURCE_DIR}/include)
+  include_directories(${CMAKE_CURRENT_SOURCE_DIR}/include)
 endmacro()
 
 #-----------------------------------------------------------------------
