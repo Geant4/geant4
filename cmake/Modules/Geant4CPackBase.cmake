53999e6efdc724d4898200c3d876dd0d56ac4bea
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2012-11-27 11:00:06 +0000

    Merged from tag cmake-V09-05-78
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/09-06-cand-00_branch@65688 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- cmake/Modules/Geant4CPackBase.cmake
+++ cmake/Modules/Geant4CPackBase.cmake
@@ -33,8 +33,8 @@ Geant4
 
 A toolkit for the simulation of the passage of particles through matter. 
 
-Its areas of application include high energy, nuclear and accelerator physics, 
-as well as studies in medical and space science (http://www.geant4.org).
+Its areas of application include high energy, nuclear and accelerator physics, as well as studies in medical and space science (http://www.geant4.org).
+The two main reference papers for Geant4 are published in Nuclear Instruments and Methods in Physics Research A 506 (2003) 250-303, and IEEE Transactions on Nuclear Science 53 No. 1 (2006) 270-278.
 
 This installer has been created using CPack (http://www.cmake.org). 
 ")
