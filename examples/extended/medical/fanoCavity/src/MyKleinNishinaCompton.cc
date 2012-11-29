7489ee7f6e67f0851209ea00407bee4d19854ad8
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2012-11-29 11:03:43 +0000

    Merged from tag fano-V09-05-09
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/09-06-cand-00_branch@65846 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/medical/fanoCavity/src/MyKleinNishinaCompton.cc
+++ examples/extended/medical/fanoCavity/src/MyKleinNishinaCompton.cc
@@ -71,10 +71,9 @@ G4double MyKleinNishinaCompton::CrossSectionPerVolume(
                                              G4double GammaEnergy,
                                              G4double, G4double)
 {
-  G4double CrossSection = 
-  G4VEmModel::CrossSectionPerVolume(mat,part,GammaEnergy);
+  G4double xsection = G4VEmModel::CrossSectionPerVolume(mat,part,GammaEnergy);
 
-  return CrossSection*fCrossSectionFactor;
+  return xsection*fCrossSectionFactor;
 }
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
 
@@ -101,18 +100,18 @@ void MyKleinNishinaCompton::SampleSecondaries(
 
   G4double epsilon, epsilonsq, onecost, sint2, greject ;
 
-  G4double epsilon0   = 1./(1. + 2.*E0_m);
-  G4double epsilon0sq = epsilon0*epsilon0;
-  G4double alpha1     = - log(epsilon0);
-  G4double alpha2     = 0.5*(1.- epsilon0sq);
+  G4double eps0   = 1./(1. + 2.*E0_m);
+  G4double eps0sq = eps0*eps0;
+  G4double alpha1     = - log(eps0);
+  G4double alpha2     = 0.5*(1.- eps0sq);
 
   do {
     if ( alpha1/(alpha1+alpha2) > G4UniformRand() ) {
-      epsilon   = exp(-alpha1*G4UniformRand());   // epsilon0**r
+      epsilon   = exp(-alpha1*G4UniformRand());   // eps0**r
       epsilonsq = epsilon*epsilon; 
 
     } else {
-      epsilonsq = epsilon0sq + (1.- epsilon0sq)*G4UniformRand();
+      epsilonsq = eps0sq + (1.- eps0sq)*G4UniformRand();
       epsilon   = sqrt(epsilonsq);
     };
 
