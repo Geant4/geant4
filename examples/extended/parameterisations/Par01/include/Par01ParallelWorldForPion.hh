9372b10e41c778805070b4efa157eadde30bef64
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2012-12-18 10:02:51 +0000

    Merged from tag exParameterisations-V09-06-00
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/09-06-ref-01_branch@66389 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ examples/extended/parameterisations/Par01/include/Par01ParallelWorldForPion.hh
@@ -0,0 +1,44 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+// $Id: Par01ParallelWorldForPion.hh,v 1.1 2006-11-03 17:58:49 mverderi Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+#ifndef Par01ParallelWorldForPion_hh
+#define Par01ParallelWorldForPion_hh
+
+#include "G4VUserParallelWorld.hh"
+
+class Par01ParallelWorldForPion : public G4VUserParallelWorld {
+public:
+  Par01ParallelWorldForPion(G4String worldName);
+  ~Par01ParallelWorldForPion();
+  
+private:
+  void Construct();
+};
+
+#endif
---
+++ examples/extended/parameterisations/Par01/src/Par01RunAction.cc
@@ -0,0 +1,61 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+// $Id: Par01RunAction.cc,v 1.8 2006-06-29 17:53:42 gunter Exp $
+// GEANT4 tag $Name: not supported by cvs2svn $
+//
+
+// Make this appear first!
+#include "G4Timer.hh"
+
+#include "Par01RunAction.hh"
+#include "G4Run.hh"
+#include "G4UImanager.hh"
+#include "G4ios.hh"
+
+Par01RunAction::Par01RunAction()
+{
+  timer = new G4Timer;
+}
+
+Par01RunAction::~Par01RunAction()
+{
+  delete timer;
+}
+
+void Par01RunAction::BeginOfRunAction(const G4Run* aRun)
+{
+  G4cout << "### Run " << aRun->GetRunID() << " start." << G4endl;
+  //  timer->Start();
+}
+
+void Par01RunAction::EndOfRunAction(const G4Run* aRun)
+{
+  timer->Stop();
+  G4cout << "number of event = " << aRun->GetNumberOfEvent() << G4endl;
+  //       << " " << *timer << G4endl;
+}
+
