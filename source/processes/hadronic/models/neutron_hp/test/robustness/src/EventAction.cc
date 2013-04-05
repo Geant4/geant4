485cbf4a1836034197da4088a72c5b84e030914c
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2013-04-05 09:39:22 +0000

    Merged from tag hadr-hpn-V09-06-05
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/09-06-ref-04_branch@68730 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/models/neutron_hp/test/robustness/src/EventAction.cc
+++
@@ -1,9 +0,0 @@
-#include "EventAction.hh"
-    EventAction::EventAction() { nEvent = 0;}
-    EventAction::~EventAction(){}
-
-    void   EventAction::BeginOfEventAction(const G4Event*)
-    {
-      nEvent++;
-      G4cout << "Processing event "<<nEvent<<G4endl;
-    }
