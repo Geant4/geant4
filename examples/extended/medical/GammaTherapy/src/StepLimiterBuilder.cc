c849163ab74e1c0c543315a1b22b45213e3a38c9
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2013-04-05 12:45:26 +0000

    Merged from tag gtherapy-V09-06-02
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/09-06-ref-04_branch@68771 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/medical/GammaTherapy/src/StepLimiterBuilder.cc
+++ examples/extended/medical/GammaTherapy/src/StepLimiterBuilder.cc
@@ -70,9 +70,9 @@ void StepLimiterBuilder::ConstructProcess()
 {
   StepLimiter* stepMax = new StepLimiter();
 
-  theParticleIterator->reset();
-  while( (*theParticleIterator)() ){
-    G4ParticleDefinition* particle = theParticleIterator->value();
+  aParticleIterator->reset();
+  while( (*aParticleIterator)() ){
+    G4ParticleDefinition* particle = aParticleIterator->value();
     G4ProcessManager* pmanager = particle->GetProcessManager();
 
     if (stepMax->IsApplicable(*particle) && !particle->IsShortLived()) {
