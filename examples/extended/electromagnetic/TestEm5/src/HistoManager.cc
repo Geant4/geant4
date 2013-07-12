224e8f37b057fc42d0ab4e8da489ef6fe20abe31
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2013-07-12 08:37:53 +0000

    Merged from tag testem5-V09-06-07
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/09-06-ref-07_branch@72235 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/electromagnetic/TestEm5/src/HistoManager.cc
+++ examples/extended/electromagnetic/TestEm5/src/HistoManager.cc
@@ -114,6 +114,6 @@ void HistoManager::Book()
   // as we have not yet set nbins, vmin, vmax
   for (G4int k=0; k<kMaxHisto; k++) {
     G4int ih = analysisManager->CreateH1(id[k], title[k], nbins, vmin, vmax);
-    analysisManager->SetActivation(G4VAnalysisManager::kH1, ih, false);
+    analysisManager->SetH1Activation(ih, false);
   }
 }
