67bfefa495a1e48254906d438a9634b2a72d4a95
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2013-07-12 08:45:36 +0000

    Merged from tag particleGunExample-V09-06-06
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/09-06-ref-07_branch@72243 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/eventgenerator/particleGun/src/HistoManager.cc
+++ examples/extended/eventgenerator/particleGun/src/HistoManager.cc
@@ -85,7 +85,7 @@ void HistoManager::Book()
   // as we have not yet set nbins, vmin, vmax
   for (G4int k=0; k<kMaxHisto; k++) {
     G4int ih = analysisManager->CreateH1(id[k], title[k], nbins, vmin, vmax);
-    analysisManager->SetActivation(G4VAnalysisManager::kH1, ih, false);
+    analysisManager->SetH1Activation(ih, false);
   }
 }
 
