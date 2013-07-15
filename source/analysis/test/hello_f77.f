ec89e44bcc4ed4a2106ea05457731813df74b7f9
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2013-07-15 15:59:09 +0000

    Merged from tag analysis-V09-06-14
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/09-06-ref-07_branch@72310 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/analysis/test/hello_f77.f
+++
@@ -1,8 +0,0 @@
-C Copyright (C) 2010, Guy Barrand. All rights reserved.
-C See the file tools.license for terms.
-
-      PROGRAM TEST
-      IMPLICIT NONE
-      PRINT *,'HELLO F77'
-      STOP
-      END
