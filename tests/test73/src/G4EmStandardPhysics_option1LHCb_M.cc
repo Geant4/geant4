5482cdcd3842a3c2ccced357471be64e8cb88573
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2013-08-22 13:14:31 +0000

    Merged from tag test73-V09-06-02
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/09-06-ref-08_branch@73247 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- tests/test73/src/G4EmStandardPhysics_option1LHCb_M.cc
+++ tests/test73/src/G4EmStandardPhysics_option1LHCb_M.cc
@@ -31,7 +31,8 @@
 //
 // Author:      V.Ivanchenko 29.06.2011
 //
-// Modified:	M. Reid 30.09.2011: To set step function for G4hIonisation->SetStepFunction(0.2, 50*um) to mesons and baryons
+// Modified:	M. Reid 30.09.2011: To set step function for 
+//   G4hIonisation->SetStepFunction(0.2, 50*um) to mesons and baryons
 // 				    Global use of fUseDistanceToBoundary
 //
 //----------------------------------------------------------------------------
@@ -53,7 +54,7 @@
 #include "G4hMultipleScattering.hh"
 #include "G4CoulombScattering.hh"
 #include "G4WentzelVIModel.hh"
-#include "G4UrbanMscModel93.hh"
+#include "G4UrbanMscModel.hh"
 #include "G4eCoulombScatteringModel.hh"
 
 #include "G4eIonisation.hh"
@@ -89,10 +90,12 @@
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-G4EmStandardPhysics_option1LHCb_M::G4EmStandardPhysics_option1LHCb_M(G4int ver, bool apply, bool newForE )
-	: G4VPhysicsConstructor("G4EmStandardPhysics_option1LHCb_M"), verbose(ver), applyCut(apply), newForElectrons(newForE)
+G4EmStandardPhysics_option1LHCb_M::G4EmStandardPhysics_option1LHCb_M(G4int ver, 
+   bool apply, bool newForE )
+  : G4VPhysicsConstructor("G4EmStandardPhysics_option1LHCb_M"), 
+    verbose(ver), applyCut(apply), newForElectrons(newForE)
 {
-	G4LossTableManager::Instance();
+  G4LossTableManager::Instance();
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -104,31 +107,31 @@ G4EmStandardPhysics_option1LHCb_M::~G4EmStandardPhysics_option1LHCb_M()
 
 void G4EmStandardPhysics_option1LHCb_M::ConstructParticle()
 {
-	// gamma
-	G4Gamma::Gamma();
+  // gamma
+  G4Gamma::Gamma();
 
-	// leptons
-	G4Electron::Electron();
-	G4Positron::Positron();
-	G4MuonPlus::MuonPlus();
-	G4MuonMinus::MuonMinus();
+  // leptons
+  G4Electron::Electron();
+  G4Positron::Positron();
+  G4MuonPlus::MuonPlus();
+  G4MuonMinus::MuonMinus();
 
-	// mesons
-	G4PionPlus::PionPlusDefinition();
-	G4PionMinus::PionMinusDefinition();
-	G4KaonPlus::KaonPlusDefinition();
-	G4KaonMinus::KaonMinusDefinition();
+  // mesons
+  G4PionPlus::PionPlusDefinition();
+  G4PionMinus::PionMinusDefinition();
+  G4KaonPlus::KaonPlusDefinition();
+  G4KaonMinus::KaonMinusDefinition();
 
-	// barions
-	G4Proton::Proton();
-	G4AntiProton::AntiProton();
+  // barions
+  G4Proton::Proton();
+  G4AntiProton::AntiProton();
 
-	// ions
-	G4Deuteron::Deuteron();
-	G4Triton::Triton();
-	G4He3::He3();
-	G4Alpha::Alpha();
-	G4GenericIon::GenericIonDefinition();
+  // ions
+  G4Deuteron::Deuteron();
+  G4Triton::Triton();
+  G4He3::He3();
+  G4Alpha::Alpha();
+  G4GenericIon::GenericIonDefinition();
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -136,178 +139,180 @@ void G4EmStandardPhysics_option1LHCb_M::ConstructParticle()
 void G4EmStandardPhysics_option1LHCb_M::ConstructProcess()
 {
 
-	std::cout << "Physics List " << GetPhysicsName() 
-		<< ", apply cut " << applyCut
-		<< ", new for electrons " << newForElectrons
-		<< std::endl;
+  std::cout << "Physics List " << GetPhysicsName() 
+	    << ", apply cut " << applyCut
+	    << ", new for electrons " << newForElectrons
+	    << std::endl;
 
-	// Add standard EM Processes
-	G4double mscEnergyLimit = 1.05*GeV;
+  // Add standard EM Processes
+  G4double mscEnergyLimit = 1.05*GeV;
 
-	aParticleIterator->reset();
-	while( (*aParticleIterator)() ){
-		G4ParticleDefinition* particle = aParticleIterator->value();
-		G4ProcessManager* pmanager = particle->GetProcessManager();
-		G4String particleName = particle->GetParticleName();
-		if(verbose > 1)
-			G4cout << "### " << GetPhysicsName() << " instantiates for " 
-				<< particleName << G4endl;
+  aParticleIterator->reset();
+  while( (*aParticleIterator)() ){
+    G4ParticleDefinition* particle = aParticleIterator->value();
+    G4ProcessManager* pmanager = particle->GetProcessManager();
+    G4String particleName = particle->GetParticleName();
+    if(verbose > 1)
+      G4cout << "### " << GetPhysicsName() << " instantiates for " 
+	     << particleName << G4endl;
 
-		if (particleName == "gamma") {
+    if (particleName == "gamma") {
 
-			pmanager->AddDiscreteProcess(new G4PhotoElectricEffect);
-			pmanager->AddDiscreteProcess(new G4ComptonScattering);
-			pmanager->AddDiscreteProcess(new G4GammaConversion);
+      pmanager->AddDiscreteProcess(new G4PhotoElectricEffect);
+      pmanager->AddDiscreteProcess(new G4ComptonScattering);
+      pmanager->AddDiscreteProcess(new G4GammaConversion);
 
-		} else if (particleName == "e-") {
+    } else if (particleName == "e-") {
 
-			G4eIonisation* eioni = new G4eIonisation();
-			eioni->SetStepFunction(0.8, 1.0*mm);
-			G4eMultipleScattering* msc = new G4eMultipleScattering;
-			msc->SetStepLimitType(fUseDistanceToBoundary); // Call for minimum of 3 steps within any sized volume
-			if (newForElectrons) {
-				std::cout << "new model for electrons" << std::endl;
-				G4UrbanMscModel93* msc93 = new G4UrbanMscModel93();
-				G4WentzelVIModel* wvi = new G4WentzelVIModel();
-				msc93->SetHighEnergyLimit(mscEnergyLimit);
-				wvi->SetLowEnergyLimit(mscEnergyLimit);
-				msc->AddEmModel(0, msc93);
-				msc->AddEmModel(0, wvi);
-				pmanager->AddProcess(msc,                   -1, 1, 1);
-				pmanager->AddProcess(eioni,                 -1, 2, 2);
-				pmanager->AddProcess(new G4eBremsstrahlung, -1,-3, 3);
-				G4CoulombScattering* sc = new G4CoulombScattering();
-				G4eCoulombScatteringModel* mod = new G4eCoulombScatteringModel();
-				mod->SetActivationLowEnergyLimit(mscEnergyLimit);
-				sc->AddEmModel(0, mod);
-				pmanager->AddDiscreteProcess(sc);
-			} else {
-				std::cout << "simple model for electrons" << std::endl;
-				pmanager->AddProcess(msc,                   -1, 1, 1);
-				pmanager->AddProcess(eioni,                 -1, 2, 2);
-				pmanager->AddProcess(new G4eBremsstrahlung, -1,-3, 3);
-			}
+      G4eIonisation* eioni = new G4eIonisation();
+      eioni->SetStepFunction(0.8, 1.0*mm);
+      G4eMultipleScattering* msc = new G4eMultipleScattering;
+      msc->SetStepLimitType(fUseDistanceToBoundary); 
+      // Call for minimum of 3 steps within any sized volume
+      if (newForElectrons) {
+	std::cout << "new model for electrons" << std::endl;
+	G4UrbanMscModel* mscMod = new G4UrbanMscModel();
+	G4WentzelVIModel* wvi = new G4WentzelVIModel();
+	mscMod->SetHighEnergyLimit(mscEnergyLimit);
+	wvi->SetLowEnergyLimit(mscEnergyLimit);
+	msc->AddEmModel(0, mscMod);
+	msc->AddEmModel(0, wvi);
+	pmanager->AddProcess(msc,                   -1, 1, 1);
+	pmanager->AddProcess(eioni,                 -1, 2, 2);
+	pmanager->AddProcess(new G4eBremsstrahlung, -1,-3, 3);
+	G4CoulombScattering* sc = new G4CoulombScattering();
+	G4eCoulombScatteringModel* mod = new G4eCoulombScatteringModel();
+	mod->SetActivationLowEnergyLimit(mscEnergyLimit);
+	sc->AddEmModel(0, mod);
+	pmanager->AddDiscreteProcess(sc);
+      } else {
+	std::cout << "simple model for electrons" << std::endl;
+	pmanager->AddProcess(msc,                   -1, 1, 1);
+	pmanager->AddProcess(eioni,                 -1, 2, 2);
+	pmanager->AddProcess(new G4eBremsstrahlung, -1,-3, 3);
+      }
 
-		} else if (particleName == "e+") {
+    } else if (particleName == "e+") {
 
-			G4eIonisation* eioni = new G4eIonisation();
-			eioni->SetStepFunction(0.8, 1.0*mm);
-			G4eMultipleScattering* msc = new G4eMultipleScattering;
-			msc->SetStepLimitType(fUseDistanceToBoundary); // Call for minimum of 3 steps within any sized volume
-			if (newForElectrons ) {
-				std::cout << "new model for positrons" << std::endl;
-				G4UrbanMscModel93* msc93 = new G4UrbanMscModel93();
-				G4WentzelVIModel* wvi = new G4WentzelVIModel();
-				msc93->SetHighEnergyLimit(mscEnergyLimit);
-				wvi->SetLowEnergyLimit(mscEnergyLimit);
-				msc->AddEmModel(0, msc93);
-				msc->AddEmModel(0, wvi);
-				pmanager->AddProcess(msc,                     -1, 1, 1);
-				pmanager->AddProcess(eioni,                   -1, 2, 2);
-				pmanager->AddProcess(new G4eBremsstrahlung,   -1,-3, 3);
-				pmanager->AddProcess(new G4eplusAnnihilation,  0,-1, 4);
-				G4CoulombScattering* sc = new G4CoulombScattering();
-				G4eCoulombScatteringModel* mod = new G4eCoulombScatteringModel();
-				mod->SetActivationLowEnergyLimit(mscEnergyLimit);
-				sc->AddEmModel(0, mod);
-				pmanager->AddDiscreteProcess(sc);
-			}
-			else {
-				std::cout << "simple model for electrons" << std::endl;
-				pmanager->AddProcess(msc,                     -1, 1, 1);
-				pmanager->AddProcess(eioni,                   -1, 2, 2);
-				pmanager->AddProcess(new G4eBremsstrahlung,   -1,-3, 3);
-				pmanager->AddProcess(new G4eplusAnnihilation,  0,-1, 4);
-			}
+      G4eIonisation* eioni = new G4eIonisation();
+      eioni->SetStepFunction(0.8, 1.0*mm);
+      G4eMultipleScattering* msc = new G4eMultipleScattering;
+      msc->SetStepLimitType(fUseDistanceToBoundary); 
+      // Call for minimum of 3 steps within any sized volume
+      if (newForElectrons ) {
+	std::cout << "new model for positrons" << std::endl;
+	G4UrbanMscModel* mscMod = new G4UrbanMscModel();
+	G4WentzelVIModel* wvi = new G4WentzelVIModel();
+	mscMod->SetHighEnergyLimit(mscEnergyLimit);
+	wvi->SetLowEnergyLimit(mscEnergyLimit);
+	msc->AddEmModel(0, mscMod);
+	msc->AddEmModel(0, wvi);
+	pmanager->AddProcess(msc,                     -1, 1, 1);
+	pmanager->AddProcess(eioni,                   -1, 2, 2);
+	pmanager->AddProcess(new G4eBremsstrahlung,   -1,-3, 3);
+	pmanager->AddProcess(new G4eplusAnnihilation,  0,-1, 4);
+	G4CoulombScattering* sc = new G4CoulombScattering();
+	G4eCoulombScatteringModel* mod = new G4eCoulombScatteringModel();
+	mod->SetActivationLowEnergyLimit(mscEnergyLimit);
+	sc->AddEmModel(0, mod);
+	pmanager->AddDiscreteProcess(sc);
+      }
+      else {
+	std::cout << "simple model for electrons" << std::endl;
+	pmanager->AddProcess(msc,                     -1, 1, 1);
+	pmanager->AddProcess(eioni,                   -1, 2, 2);
+	pmanager->AddProcess(new G4eBremsstrahlung,   -1,-3, 3);
+	pmanager->AddProcess(new G4eplusAnnihilation,  0,-1, 4);
+      }
 
-		} else if (particleName == "mu+" ||
-				particleName == "mu-"    ) {
+    } else if (particleName == "mu+" ||
+	       particleName == "mu-"    ) {
+      
+      G4MuMultipleScattering* msc = new G4MuMultipleScattering();
+      msc->AddEmModel(0, new G4WentzelVIModel());
+      msc->SetStepLimitType(fUseDistanceToBoundary);
+      pmanager->AddProcess(msc,                     -1, 1, 1);
+      pmanager->AddProcess(new G4MuIonisation,      -1, 2, 2);
+      pmanager->AddProcess(new G4MuBremsstrahlung,  -1,-3, 3);
+      pmanager->AddProcess(new G4MuPairProduction,  -1,-4, 4);
+      pmanager->AddDiscreteProcess(new G4CoulombScattering());
 
-			G4MuMultipleScattering* msc = new G4MuMultipleScattering();
-			msc->AddEmModel(0, new G4WentzelVIModel());
-			msc->SetStepLimitType(fUseDistanceToBoundary);
-			pmanager->AddProcess(msc,                     -1, 1, 1);
-			pmanager->AddProcess(new G4MuIonisation,      -1, 2, 2);
-			pmanager->AddProcess(new G4MuBremsstrahlung,  -1,-3, 3);
-			pmanager->AddProcess(new G4MuPairProduction,  -1,-4, 4);
-			pmanager->AddDiscreteProcess(new G4CoulombScattering());
+    } else if (particleName == "alpha" ||
+	       particleName == "He3") {
 
-		} else if (particleName == "alpha" ||
-				particleName == "He3") {
+      pmanager->AddProcess(new G4hMultipleScattering, -1, 1, 1);
+      pmanager->AddProcess(new G4ionIonisation,       -1, 2, 2);
 
-			pmanager->AddProcess(new G4hMultipleScattering, -1, 1, 1);
-			pmanager->AddProcess(new G4ionIonisation,       -1, 2, 2);
+    } else if (particleName == "GenericIon") {
 
-		} else if (particleName == "GenericIon") {
+      pmanager->AddProcess(new G4hMultipleScattering, -1, 1, 1);
+      pmanager->AddProcess(new G4ionIonisation,       -1, 2, 2);
 
-			pmanager->AddProcess(new G4hMultipleScattering, -1, 1, 1);
-			pmanager->AddProcess(new G4ionIonisation,       -1, 2, 2);
+    } else if (particleName == "pi+" ||
+	       particleName == "pi-" ||
+	       particleName == "kaon+" ||
+	       particleName == "kaon-" ||
+	       particleName == "proton" ) {
 
-		} else if (particleName == "pi+" ||
-				particleName == "pi-" ||
-				particleName == "kaon+" ||
-				particleName == "kaon-" ||
-				particleName == "proton" ) {
+      G4hMultipleScattering* msc = new G4hMultipleScattering();
+      msc->AddEmModel(0, new G4WentzelVIModel());
+      msc->SetStepLimitType(fUseDistanceToBoundary);
+      pmanager->AddProcess(msc,                   -1, 1, 1);
 
-			G4hMultipleScattering* msc = new G4hMultipleScattering();
-			msc->AddEmModel(0, new G4WentzelVIModel());
-			msc->SetStepLimitType(fUseDistanceToBoundary);
-			pmanager->AddProcess(msc,                   -1, 1, 1);
+      G4hIonisation* hIoni = new G4hIonisation();
+      hIoni->SetStepFunction(0.2, 50*um);
+      pmanager->AddProcess(hIoni,                     -1, 2, 2);
+      pmanager->AddProcess(new G4hBremsstrahlung,     -1,-3, 3);
+      pmanager->AddProcess(new G4hPairProduction,     -1,-4, 4);
 
-			G4hIonisation* hIoni = new G4hIonisation();
-			hIoni->SetStepFunction(0.2, 50*um);
-			pmanager->AddProcess(hIoni,                     -1, 2, 2);
-			pmanager->AddProcess(new G4hBremsstrahlung,     -1,-3, 3);
-			pmanager->AddProcess(new G4hPairProduction,     -1,-4, 4);
+    } else if (particleName == "B+" ||
+	       particleName == "B-" ||
+	       particleName == "D+" ||
+	       particleName == "D-" ||
+	       particleName == "Ds+" ||
+	       particleName == "Ds-" ||
+	       particleName == "anti_He3" ||
+	       particleName == "anti_alpha" ||
+	       particleName == "anti_deuteron" ||
+	       particleName == "anti_lambda_c+" ||
+	       particleName == "anti_omega-" ||
+	       particleName == "anti_proton" ||
+	       particleName == "anti_sigma_c+" ||
+	       particleName == "anti_sigma_c++" ||
+	       particleName == "anti_sigma+" ||
+	       particleName == "anti_sigma-" ||
+	       particleName == "anti_triton" ||
+	       particleName == "anti_xi_c+" ||
+	       particleName == "anti_xi-" ||
+	       particleName == "deuteron" ||
+	       particleName == "lambda_c+" ||
+	       particleName == "omega-" ||
+	       particleName == "sigma_c+" ||
+	       particleName == "sigma_c++" ||
+	       particleName == "sigma+" ||
+	       particleName == "sigma-" ||
+	       particleName == "tau+" ||
+	       particleName == "tau-" ||
+	       particleName == "triton" ||
+	       particleName == "xi_c+" ||
+	       particleName == "xi-" ) {
 
-		} else if (particleName == "B+" ||
-				particleName == "B-" ||
-				particleName == "D+" ||
-				particleName == "D-" ||
-				particleName == "Ds+" ||
-				particleName == "Ds-" ||
-				particleName == "anti_He3" ||
-				particleName == "anti_alpha" ||
-				particleName == "anti_deuteron" ||
-				particleName == "anti_lambda_c+" ||
-				particleName == "anti_omega-" ||
-				particleName == "anti_proton" ||
-				particleName == "anti_sigma_c+" ||
-				particleName == "anti_sigma_c++" ||
-				particleName == "anti_sigma+" ||
-				particleName == "anti_sigma-" ||
-				particleName == "anti_triton" ||
-				particleName == "anti_xi_c+" ||
-				particleName == "anti_xi-" ||
-				particleName == "deuteron" ||
-				particleName == "lambda_c+" ||
-				particleName == "omega-" ||
-				particleName == "sigma_c+" ||
-				particleName == "sigma_c++" ||
-				particleName == "sigma+" ||
-				particleName == "sigma-" ||
-				particleName == "tau+" ||
-				particleName == "tau-" ||
-				particleName == "triton" ||
-				particleName == "xi_c+" ||
-				particleName == "xi-" ) {
-
-					G4MuMultipleScattering* msc = new G4MuMultipleScattering();
-					msc->AddEmModel(0, new G4WentzelVIModel());
-					msc->SetStepLimitType(fUseDistanceToBoundary);
-					pmanager->AddProcess(msc, -1, 1, 1);
-					G4hIonisation* hIoni = new G4hIonisation();
-					hIoni->SetStepFunction(0.2, 50*um);
-					pmanager->AddProcess(hIoni,                     -1, 2, 2);
-				}
-	}
-	G4EmProcessOptions opt;
-	opt.SetVerbose(verbose);
-	opt.SetPolarAngleLimit(CLHEP::pi);
-	if (applyCut) {  
-		opt.SetApplyCuts(true);
-		std::cout << GetPhysicsName() << " applying cut" << std::endl;
-	}
+      G4MuMultipleScattering* msc = new G4MuMultipleScattering();
+      msc->AddEmModel(0, new G4WentzelVIModel());
+      msc->SetStepLimitType(fUseDistanceToBoundary);
+      pmanager->AddProcess(msc, -1, 1, 1);
+      G4hIonisation* hIoni = new G4hIonisation();
+      hIoni->SetStepFunction(0.2, 50*um);
+      pmanager->AddProcess(hIoni,                     -1, 2, 2);
+    }
+  }
+  G4EmProcessOptions opt;
+  opt.SetVerbose(verbose);
+  opt.SetPolarAngleLimit(CLHEP::pi);
+  if (applyCut) {  
+    opt.SetApplyCuts(true);
+    std::cout << GetPhysicsName() << " applying cut" << std::endl;
+  }
 
 }
 
