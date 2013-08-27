a22dc11940e923d2a9720ad861afb31fbb280c14
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2013-08-27 11:28:21 +0000

    Merged from tag exampleRE05-V09-06-07
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/09-06-ref-08_branch@73444 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/runAndEvent/RE05/include/RE05TrackerParametrisation.hh
+++ examples/extended/runAndEvent/RE05/include/RE05TrackerParametrisation.hh
@@ -44,6 +44,7 @@ class G4Trap;
 class G4Cons;
 class G4Orb;
 class G4Sphere;
+class G4Ellipsoid;
 class G4Torus;
 class G4Para;
 class G4Hype;
@@ -56,26 +57,28 @@ class RE05TrackerParametrisation : public G4VPVParameterisation
   public:
   
     RE05TrackerParametrisation();
-    virtual ~RE05TrackerParametrisation();
-    virtual void ComputeTransformation(const G4int copyNo,
-                                     G4VPhysicalVolume *physVol) const;
-    virtual void ComputeDimensions(G4Tubs & trackerLayer,
+   ~RE05TrackerParametrisation();
+
+    void ComputeTransformation(const G4int copyNo,
+                               G4VPhysicalVolume *physVol) const;
+    void ComputeDimensions(G4Tubs & trackerLayer,
                            const G4int copyNo,
                            const G4VPhysicalVolume * physVol) const;
 
   private:  // Dummy declarations to get rid of warnings ...
 
-    virtual void ComputeDimensions (G4Trd&,const G4int,const G4VPhysicalVolume*) const {}
-    virtual void ComputeDimensions (G4Trap&,const G4int,const G4VPhysicalVolume*) const {}
-    virtual void ComputeDimensions (G4Cons&,const G4int,const G4VPhysicalVolume*) const {}
-    virtual void ComputeDimensions (G4Orb&,const G4int,const G4VPhysicalVolume*) const {}
-    virtual void ComputeDimensions (G4Sphere&,const G4int,const G4VPhysicalVolume*) const {}
-    virtual void ComputeDimensions (G4Torus&,const G4int,const G4VPhysicalVolume*) const {}
-    virtual void ComputeDimensions (G4Para&,const G4int,const G4VPhysicalVolume*) const {}
-    virtual void ComputeDimensions (G4Hype&,const G4int,const G4VPhysicalVolume*) const {}
-    virtual void ComputeDimensions (G4Box&,const G4int,const G4VPhysicalVolume*) const {}
-    virtual void ComputeDimensions (G4Polycone&,const G4int,const G4VPhysicalVolume*) const {}
-    virtual void ComputeDimensions (G4Polyhedra&,const G4int,const G4VPhysicalVolume*) const {}
+    void ComputeDimensions (G4Trd&,const G4int,const G4VPhysicalVolume*) const {}
+    void ComputeDimensions (G4Trap&,const G4int,const G4VPhysicalVolume*) const {}
+    void ComputeDimensions (G4Cons&,const G4int,const G4VPhysicalVolume*) const {}
+    void ComputeDimensions (G4Orb&,const G4int,const G4VPhysicalVolume*) const {}
+    void ComputeDimensions (G4Sphere&,const G4int,const G4VPhysicalVolume*) const {}
+    void ComputeDimensions (G4Ellipsoid&,const G4int,const G4VPhysicalVolume*) const {}
+    void ComputeDimensions (G4Torus&,const G4int,const G4VPhysicalVolume*) const {}
+    void ComputeDimensions (G4Para&,const G4int,const G4VPhysicalVolume*) const {}
+    void ComputeDimensions (G4Hype&,const G4int,const G4VPhysicalVolume*) const {}
+    void ComputeDimensions (G4Box&,const G4int,const G4VPhysicalVolume*) const {}
+    void ComputeDimensions (G4Polycone&,const G4int,const G4VPhysicalVolume*) const {}
+    void ComputeDimensions (G4Polyhedra&,const G4int,const G4VPhysicalVolume*) const {}
 
   private:
 
