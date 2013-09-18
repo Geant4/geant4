41d5e10a1c42ab51887753fc1c1daceddf7a4f34
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2013-09-18 14:16:30 +0000

    Merged from tag test202-V09-06-04
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/09-06-ref-09_branch@73971 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- tests/test202/xvfb.sh
+++ tests/test202/xvfb.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 export DISPLAY_TMP=$DISPLAY
 XV_CMD="Xvfb :99 -screen 0 1024x768x24 -nolisten tcp"
-$XV_CMD&
+$XV_CMD 2>&1 &
 export DISPLAY=:99
-echo "launch"
+echo "launching $1"
 $1 $2
+rc=$?
+
 export DISPLAY=$DISPLAY_TMP
-echo "fin"
+echo "fin: RC=$rc"
 XVFB_PID="`pgrep -f "$XV_CMD"`"
 kill -9 $XVFB_PID
-exit 0
+exit $rc
