5e4f525f946d4e2c8f79cbe4ffa4bd456c3bf557
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2013-11-27 08:39:08 +0000

    Merged from tag test18-V09-06-11
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/09-06-ref-11_branch@77648 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- tests/test18/test18.cc
+++ tests/test18/test18.cc
@@ -55,7 +55,7 @@ int main(int argc,char** argv)
   // Run manager
 #ifdef G4MULTITHREADED
   G4MTRunManager* runManager = new G4MTRunManager;
-  runManager->SetNumberOfThreads(4);
+  runManager->SetNumberOfThreads(1);
 #else
   G4RunManager* runManager = new G4RunManager;
 #endif
