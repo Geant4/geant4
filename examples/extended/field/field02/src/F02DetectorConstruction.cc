dbf8324a3a67f583d9099c5c8399c36d438982c5
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2013-11-27 08:51:29 +0000

    Merged from tag fieldex02-V09-06-05
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/09-06-ref-11_branch@77654 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/field/field02/src/F02DetectorConstruction.cc
+++ examples/extended/field/field02/src/F02DetectorConstruction.cc
@@ -252,25 +252,22 @@ G4VPhysicalVolume* F02DetectorConstruction::ConstructCalorimeter()
                                   0);                      // copy number
   // Absorber
 
-  if (fAbsorberThickness > 0.)
-  {
-      fSolidAbsorber = new G4Tubs("Absorber",0.,
-                                  fAbsorberRadius,
-                                  fAbsorberThickness/2.,
-                                  0.0,twopi);
+  fSolidAbsorber = new G4Tubs("Absorber",0.,
+                              fAbsorberRadius,
+                              fAbsorberThickness/2.,
+                              0.0,twopi);
 
-      fLogicAbsorber = new G4LogicalVolume(fSolidAbsorber,
-                                           fAbsorberMaterial,
-                                           "Absorber");
+  fLogicAbsorber = new G4LogicalVolume(fSolidAbsorber,
+                                       fAbsorberMaterial,
+                                       "Absorber");
 
-      fPhysiAbsorber = new G4PVPlacement(0,
-                                         G4ThreeVector(0.,0.,fZAbsorber),
-                                         "Absorber",
-                                         fLogicAbsorber,
-                                         fPhysiWorld,
-                                         false,
-                                         0);
-  }
+  fPhysiAbsorber = new G4PVPlacement(0,
+                                     G4ThreeVector(0.,0.,fZAbsorber),
+                                     "Absorber",
+                                     fLogicAbsorber,
+                                     fPhysiWorld,
+                                     false,
+                                     0);
 
   return fPhysiWorld;
 }
@@ -389,9 +386,9 @@ void F02DetectorConstruction::ConstructSDandField()
 
   if (!fCalorimeterSD.Get()) {
     F02CalorimeterSD* calorimeterSD = new F02CalorimeterSD("CalorSD",this);
-    SetSensitiveDetector(fLogicAbsorber, calorimeterSD);
     fCalorimeterSD.Put(calorimeterSD);
   }  
+  SetSensitiveDetector(fLogicAbsorber, fCalorimeterSD.Get());
 
   // Construct the field creator - this will register the field it creates
 
