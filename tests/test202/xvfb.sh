01178aeeaadabb682be51597970957855091ddcd
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2014-01-21 16:16:49 +0000

    Merged from tag test202-V10-00-00
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/10-00-ref-01_branch@78749 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- tests/test202/xvfb.sh
+++ tests/test202/xvfb.sh
@@ -1,14 +1,52 @@
-#!/bin/sh
+#!/bin/sh 
+
 export DISPLAY_TMP=$DISPLAY
-XV_CMD="Xvfb :99 -screen 0 1024x768x24 -nolisten tcp"
-$XV_CMD 2>&1 &
-export DISPLAY=:99
+id=89
+Njobs=0
+
+# choose a free display number
+
+while test $Njobs -ne 1; do
+  (( id++ ))
+
+  if test $id -gt 99 ; then
+     echo "Cannot find a free display number, all /tmp/test202-[90-99].lock in use"
+     exit 1
+  fi
+
+  /usr/bin/lockfile -l 3600 -r1 /tmp/test202-${id}.lock || continue
+
+# check if there is a lock from Xserver for this display, if so try another
+  [ -r /tmp/.X${id}-lock ] && continue
+  pid=`cat /tmp/.X${id}-lock`
+  ps -p $pid && continue
+
+  XV_CMD="Xvfb :${id} -screen 0 1024x768x24 -nolisten tcp"
+  $XV_CMD 2>&1 &
+  XVFB_PID=$!
+  sleep 1
+  Njobs=`/bin/ps -f -p ${XVFB_PID} | /bin/grep -e " :${id} " | /usr/bin/wc -l `
+  if  ( [ $Njobs -eq 1 ] ) ; then
+# success quit search loop
+     break
+	  
+  else  
+     echo "Error: No Xserver for display :${id} started!"
+     echo " is there a problem with lockfile/pipe still existing?"
+	  echo "  Check for /tmp/.X${id}-lock and /tmp/.X11-unix/X${id}"
+     rm -f /tmp/test202-${id}.lock
+  fi	
+
+done
+
+
+export DISPLAY=:${id}
 echo "launching $1"
 $1 $2
 rc=$?
 
-export DISPLAY=$DISPLAY_TMP
-echo "fin: RC=$rc"
-XVFB_PID="`pgrep -f "$XV_CMD"`"
-kill -9 $XVFB_PID
+kill  $XVFB_PID
+rm -f /tmp/test202-${id}.lock
+
+echo "end of test202 executable: RC=$rc"
 exit $rc
