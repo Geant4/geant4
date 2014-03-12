6f09b30d125189f8929049d88d502a2398df9ca4
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2014-03-12 12:59:30 +0000

    Merged from tag hadr-inclxx-V10-00-02
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/10-00-ref-03_branch@79692 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/models/inclxx/utils/include/G4INCLVersion.hh
+++ source/processes/hadronic/models/inclxx/utils/include/G4INCLVersion.hh
@@ -34,5 +34,5 @@
 
 #include "globals.hh"
 
-#define G4INCL_VERSION_ID "v5.1.14.6-clean"
-#define G4INCL_GIT_HASH "433ceab7536b88315645cfb2c535c50862d62c17"
+#define G4INCL_VERSION_ID "v5.1.14.7-clean"
+#define G4INCL_GIT_HASH "1d0c4632a06bd31c430e24a0a1e6dd63214f32bc"
