0224134f86337845c08fd40748ea5ec0843309b2
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2014-04-10 12:12:28 +0000

    Merged from tag hadr-cohe-V10-00-03
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/10-00-ref-04_branch@80308 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/hadronic/models/coherent_elastic/src/G4NuclNuclDiffuseElastic.cc
+++ source/processes/hadronic/models/coherent_elastic/src/G4NuclNuclDiffuseElastic.cc
@@ -1960,7 +1960,7 @@ G4complex G4NuclNuclDiffuseElastic::GammaLogarithm(G4complex zz)
   const G4double cof[6] = { 76.18009172947146,     -86.50532032941677,
                              24.01409824083091,      -1.231739572450155,
                               0.1208650973866179e-2, -0.5395239384953e-5  } ;
-  register G4int j;
+  G4int j;
   G4complex z = zz - 1.0;
   G4complex tmp = z + 5.5;
   tmp -= (z + 0.5) * std::log(tmp);
