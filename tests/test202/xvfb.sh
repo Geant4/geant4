d8928b2ef767e42059a0e0ec4cd32924fbdbaaad
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2014-04-22 08:25:51 +0000

    Merged from tag test202-V10-00-09
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/10-00-ref-04_branch@80444 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- tests/test202/xvfb.sh
+++ tests/test202/xvfb.sh
@@ -1,15 +1,35 @@
 #!/bin/sh
+
+# Test file name :
+testFile="visSceneTest_0.eps"
+
+# reference folder :
+reference_folder=$2-reference
+
+# output folder : 
+output_folder=$2-output
+
 export DISPLAY_TMP=$DISPLAY
 XV_CMD="Xvfb :99 -screen 0 1024x768x24 -nolisten tcp"
+
 # is GLX extension present ?
 rm -f XvfbExtensions
 $XV_CMD 2>&1 &>XvfbExtensions &
 sleep 1
+
 # if GLX extension not present, return ok, but not run test
 if grep "GLX" XvfbExtensions; then
+
+  # OGLxx specific
+  macro=$2.mac
+  rm -rf $output_folder
+  mkdir -p $output_folder
+
+  # Launching
+  echo "Launching application..."
   export DISPLAY=:99
   echo "launching $1"
-  $1 $2
+  $1 $macro
   rc=$?
 
   export DISPLAY=$DISPLAY_TMP
@@ -17,7 +37,25 @@ if grep "GLX" XvfbExtensions; then
   if [ $XVFB_PID ]; then
     kill -9 $XVFB_PID
   fi
-  exit $rc
+
+  # check if all ok
+  echo "Check if output and reference are similar... "
+  if ! test -f $output_folder/$testFile; then
+    echo "ERROR: $output_folder/$testFile not found. No output produced!"
+    exit 1
+  fi
+
+  if [`diff $reference_folder/$testFile $output_folder/$testFile | wc -l` == 0 ]; then
+    echo "All OK"
+    exit 0
+  else
+    echo "ERROR: Output and reference are different!"
+    echo "======================================================"
+    echo `diff $reference_folder/$testFile $output_folder/$testFile`
+    echo "======================================================"
+    exit 1
+  fi
+
 else
   echo "No GLX extension on Xvfb server"
   XVFB_PID="`pgrep -f "$XV_CMD"`"
