68fa17462f29ca10c2096cacbdc7e22cba1dc78d
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2014-04-24 13:41:59 +0000

    Merged from tag externals-V10-00-02
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/10-00-ref-04_branch@80491 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/externals/clhep/src/Random.cc
+++ source/externals/clhep/src/Random.cc
@@ -61,7 +61,6 @@ struct defaults {
   shared_ptr<HepRandomEngine>  theEngine;
 };  // defaults
 
-  inline
   defaults &  theDefaults()  {
     static  CLHEP_THREAD_LOCAL HepRandom       theDefaultGenerator;
     static  CLHEP_THREAD_LOCAL HepJamesRandom  theDefaultEngine;
