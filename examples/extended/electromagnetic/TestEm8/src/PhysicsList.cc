5aecb372ed3af7171a48743f272b6e9c32e16ed8
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2014-04-29 07:49:21 +0000

    Merged from tag testem8-V10-00-02
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/10-00-ref-04_branch@80580 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/electromagnetic/TestEm8/src/PhysicsList.cc
+++ examples/extended/electromagnetic/TestEm8/src/PhysicsList.cc
@@ -52,6 +52,7 @@
 #include "G4EmStandardPhysics_option4.hh"
 #include "G4EmLivermorePhysics.hh"
 #include "G4EmPenelopePhysics.hh"
+#include "G4EmLowEPPhysics.hh"
 #include "G4DecayPhysics.hh"
 
 #include "G4PAIModel.hh"
@@ -180,6 +181,12 @@ void PhysicsList::AddPhysicsList(const G4String& name)
     fEmName = name;
     delete fEmPhysicsList;
     fEmPhysicsList = new G4EmPenelopePhysics();
+
+  } else if (name == "emlowenergy") {
+
+    fEmName = name;
+    delete fEmPhysicsList;
+    fEmPhysicsList = new G4EmLowEPPhysics();
 
   } else if (name == "pai") {
 
