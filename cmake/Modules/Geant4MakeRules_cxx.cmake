44f80ec08f3cd4017d4bd073d8f8b6e4acc3e564
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2014-05-28 14:07:04 +0000

    Merged from tag cmake-V10-00-16
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/10-00-ref-05_branch@81438 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- cmake/Modules/Geant4MakeRules_cxx.cmake
+++ cmake/Modules/Geant4MakeRules_cxx.cmake
@@ -125,6 +125,20 @@ function(__configure_cxxstd_intel)
   endforeach()
 endfunction()
 
+#-----------------------------------------------------------------------
+# function __configure_tls_models()
+#          Set available thread local storage models. Valid for GNU,
+#          Clang and Intel compilers.
+#
+function(__configure_tls_models)
+  # available models, default first
+  set(_TLSMODELS initial-exec local-exec global-dynamic local-dynamic)
+
+  set(TLSMODEL_IS_AVAILABLE ${_TLSMODELS} PARENT_SCOPE)
+  foreach(_s ${_TLSMODELS})
+    set(${_s}_FLAGS "-ftls-model=${_s}" PARENT_SCOPE)
+  endforeach()
+endfunction()
 
 #-----------------------------------------------------------------------
 # DEFAULT FLAG SETTING
@@ -158,7 +172,8 @@ if(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
   endif()
 
   # - Multithreading
-  set(GEANT4_MULTITHREADED_CXX_FLAGS "-ftls-model=initial-exec -pthread")
+  __configure_tls_models()
+  set(GEANT4_MULTITHREADED_CXX_FLAGS "-pthread")
 endif()
 
 
@@ -202,7 +217,8 @@ if(CMAKE_CXX_COMPILER MATCHES "icpc.*|icc.*")
   __configure_cxxstd_intel()
 
   # - Multithreading
-  set(GEANT4_MULTITHREADED_CXX_FLAGS "-ftls-model=initial-exec -pthread")
+  __configure_tls_models()
+  set(GEANT4_MULTITHREADED_CXX_FLAGS "-pthread")
 
   # Linker flags
   set(CMAKE_EXE_LINKER_FLAGS "-i-dynamic -limf")
