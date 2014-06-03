ad71a676370082eb8bb8dcf17f5c2fa8ced37f7e
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2014-06-03 10:51:46 +0000

    Merged from tag zlib-V10-00-01
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/geant4-10-00-patches_branch@81586 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/externals/zlib/src/inftrees.cc
+++ source/externals/zlib/src/inftrees.cc
@@ -8,8 +8,8 @@
 
 #define MAXBITS 15
 
-const char inflate_copyright[] =
-   " inflate 1.2.7 Copyright 1995-2012 Mark Adler ";
+// const char inflate_copyright[] =
+//    " inflate 1.2.7 Copyright 1995-2012 Mark Adler ";
 /*
   If you use the zlib library in a product, an acknowledgment is welcome
   in the documentation of your product. If for some reason you cannot
