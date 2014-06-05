36e315304054fee6cd832cc99203a7eaa581b94e
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2014-06-05 09:10:33 +0000

    Merged from tag exgflash-V10-00-00
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/10-00-ref-05_branch@81783 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/parameterisations/gflash/src/ExGflashActionInitialization.cc
+++ examples/extended/parameterisations/gflash/src/ExGflashActionInitialization.cc
@@ -37,12 +37,20 @@
 
 ExGflashActionInitialization::ExGflashActionInitialization()
  : G4VUserActionInitialization()
-{}
+{
+    //AND->3June2014, temporary to take into account new GPS
+    //Create an instance of GPS in master so shared resources and messenger
+    //exist in master.
+    masterGPS = new G4GeneralParticleSource();
+    //AND<-3June2014
+}
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 ExGflashActionInitialization::~ExGflashActionInitialization()
-{;}
+{
+  delete masterGPS;
+}
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
