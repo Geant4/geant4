8a4ec3902120fd9ea964a5a1435a3c0b876712ed
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2014-06-06 09:53:57 +0000

    Merged from tag procuts-V10-00-02
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/geant4-10-00-patches_branch@81850 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/cuts/src/G4VRangeToEnergyConverter.cc
+++ source/processes/cuts/src/G4VRangeToEnergyConverter.cc
@@ -72,9 +72,6 @@ G4VRangeToEnergyConverter & G4VRangeToEnergyConverter::operator=(const G4VRangeT
     theLossTable=0;
  }
 
-  LowestEnergy = right.LowestEnergy;
-  HighestEnergy = right.HighestEnergy;
-  MaxEnergyCut = right.MaxEnergyCut;
   fMaxEnergyCut = right.fMaxEnergyCut;
   NumberOfElements = right.NumberOfElements;
   theParticle = right.theParticle;
@@ -353,8 +350,11 @@ void G4VRangeToEnergyConverter::BuildRangeVector(const G4Material* aMaterial,
   }
    
   // Integrate with Simpson formula with logarithmic binning
-  G4double ltt = std::log(MaxEnergyCut/LowestEnergy);
-  G4double dltau = ltt/TotBin;
+  G4double dltau = 1.0;
+  if (LowestEnergy>0.) {
+      G4double ltt =std::log(MaxEnergyCut/LowestEnergy);
+      dltau = ltt/TotBin;
+  }
 
   G4double s0 = 0.;
   G4double Value;
