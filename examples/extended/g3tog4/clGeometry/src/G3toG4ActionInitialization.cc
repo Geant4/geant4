ff7ec5080a531a93d947d6b848b7afadbcd27d2f
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2014-06-12 09:43:33 +0000

    Merged from tag g3tog4Tests-V10-00-01
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/10-00-ref-06_branch@82227 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ examples/extended/g3tog4/clGeometry/src/G3toG4ActionInitialization.cc
@@ -0,0 +1,65 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+// $Id: G3toG4ActionInitialization.cc 68058 2013-03-13 14:47:43Z gcosmo $
+//
+/// \file G3toG4ActionInitialization.cc
+/// \brief Implementation of the G3toG4ActionInitialization class
+
+#include "G3toG4ActionInitialization.hh"
+
+// common package includes
+#include "ExG4EventAction01.hh"
+#include "ExG4RunAction01.hh"
+#include "ExG4PrimaryGeneratorAction01.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+G3toG4ActionInitialization::G3toG4ActionInitialization()
+ : G4VUserActionInitialization()
+{}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+G3toG4ActionInitialization::~G3toG4ActionInitialization()
+{}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void G3toG4ActionInitialization::BuildForMaster() const
+{
+  SetUserAction(new ExG4RunAction01);
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void G3toG4ActionInitialization::Build() const
+{
+  SetUserAction(new ExG4PrimaryGeneratorAction01);
+  SetUserAction(new ExG4RunAction01);
+  SetUserAction(new ExG4EventAction01);
+}  
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
