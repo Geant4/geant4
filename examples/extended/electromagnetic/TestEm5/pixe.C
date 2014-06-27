e0e5e41966eb27288d94f6dead82f589455ad5e1
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2014-06-27 12:33:19 +0000

    Merged from tag testem5-V10-00-08
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/10-00-ref-07_branch@82562 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/electromagnetic/TestEm5/pixe.C
+++ examples/extended/electromagnetic/TestEm5/pixe.C
@@ -9,15 +9,15 @@
   c1->cd();
   c1->Update();
      
-  TH1D* hist1 = (TH1D*)f.Get("3");
+  TH1D* hist1 = (TH1D*)f.Get("h3");
   hist1->Draw("HIST");
      
-  TH1D* hist2 = (TH1D*)f.Get("5");
+  TH1D* hist2 = (TH1D*)f.Get("h5");
   hist2->Draw("HIST");
     
-  TH1D* hist3 = (TH1D*)f.Get("20");
+  TH1D* hist3 = (TH1D*)f.Get("h20");
   hist3->Draw("HIST");
   
-  TH1D* hist4 = (TH1D*)f.Get("40");
+  TH1D* hist4 = (TH1D*)f.Get("h40");
   hist4->Draw("HIST");      
 }  
