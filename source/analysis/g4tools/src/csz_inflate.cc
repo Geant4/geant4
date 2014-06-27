b61cb465c9cd47457636c213dba49e06ec4bbea4
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2014-06-27 12:59:08 +0000

    Merged from tag analysis-V10-00-15
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/10-00-ref-07_branch@82569 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/analysis/g4tools/src/csz_inflate.cc
+++ source/analysis/g4tools/src/csz_inflate.cc
@@ -446,17 +446,17 @@ int csz__huft_build(unsigned *b, unsigned n, unsigned s, ush *d, ush *e, struct 
   unsigned f;                   /* i repeats in table every f entries */
   int g;                        /* maximum code length */
   int h;                        /* table level */
-  register unsigned i;          /* counter, current code */
-  register unsigned j;          /* counter */
-  register int k;               /* number of bits in current code */
+  unsigned i;          /* counter, current code */
+  unsigned j;          /* counter */
+  int k;               /* number of bits in current code */
   int lx[BMAX+1];               /* memory for l[-1..BMAX-1] */
   int *l = lx+1;                /* stack of bits per table */
-  register unsigned *p;         /* pointer into c[], b[], or v[] */
-  register struct huft *q;      /* points to current table */
+  unsigned *p;         /* pointer into c[], b[], or v[] */
+  struct huft *q;      /* points to current table */
   struct huft r;                /* table entry for structure assignment */
   struct huft *u[BMAX];         /* table stack */
   static unsigned v[N_MAX];     /* values in order of bit length */
-  register int w;               /* bits before this table == (l * h) */
+  int w;               /* bits before this table == (l * h) */
   unsigned x[BMAX+1];           /* bit offsets, then code stack */
   unsigned *xp;                 /* pointer into x */
   int y;                        /* number of dummy codes added */
@@ -627,7 +627,7 @@ int csz__huft_free(struct huft *t)
    list of the tables it made, with the links in a dummy first entry of
    each table. */
 {
-  register struct huft *p, *q;
+  struct huft *p, *q;
 
 
   /* Go through linked list, freeing from the malloced (t[-1]) address. */
@@ -656,13 +656,13 @@ int csz__Inflate_codes(struct huft *tl, struct huft *td, int bl, int bd)
 /* inflate (decompress) the codes in a deflated (compressed) block.
    Return an error code or zero if it all goes ok. */
 {
-  register unsigned e;  /* table entry flag/number of extra bits */
+  unsigned e;  /* table entry flag/number of extra bits */
   unsigned n, d;        /* length and index for copy */
   unsigned w;           /* current window position */
   struct huft *t;       /* pointer to table entry */
   unsigned ml, md;      /* masks for bl and bd bits */
-  register ulg b;       /* bit buffer */
-  register unsigned k;  /* number of bits in bit buffer */
+  ulg b;       /* bit buffer */
+  unsigned k;  /* number of bits in bit buffer */
 
 
   /* make local copies of globals */
@@ -765,8 +765,8 @@ int csz__Inflate_stored()
 {
   unsigned n;           /* number of bytes in block */
   unsigned w;           /* current window position */
-  register ulg b;       /* bit buffer */
-  register unsigned k;  /* number of bits in bit buffer */
+  ulg b;       /* bit buffer */
+  unsigned k;  /* number of bits in bit buffer */
 
 
   /* make local copies of globals */
@@ -886,8 +886,8 @@ int csz__Inflate_dynamic()
 #else
   static unsigned ll[286+30]; /* literal/length and distance code lengths */
 #endif
-  register ulg b;       /* bit buffer */
-  register unsigned k;  /* number of bits in bit buffer */
+  ulg b;       /* bit buffer */
+  unsigned k;  /* number of bits in bit buffer */
 
   static int qflag = 0; /*G.Barrand*/
 
@@ -1037,8 +1037,8 @@ int csz__Inflate_block(int *e)
 /* decompress an inflated block */
 {
   unsigned t;           /* block type */
-  register ulg b;       /* bit buffer */
-  register unsigned k;  /* number of bits in bit buffer */
+  ulg b;       /* bit buffer */
+  unsigned k;  /* number of bits in bit buffer */
 
 
   /* make local bit buffer */
