5e0427f88273ad4bb6d421c556673d7820215f7c
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2014-07-01 09:41:33 +0000

    Merged from tag channelingExample-V10-00-14
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/10-00-ref-07_branch@82629 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/exoticphysics/channeling/src/XCrystalPlanarMoliereTempPotential.cc
+++ examples/extended/exoticphysics/channeling/src/XCrystalPlanarMoliereTempPotential.cc
@@ -69,9 +69,9 @@ ComputeECForSinglePlane(G4double vX,
     for(unsigned int i=0;i<3;i++){
         G4double vTemp = 0.;
         vTemp += ( exp(-vX/ aTF * fBeta[i] ) *
-                  (1.-erf((vTVA / aTF * fBeta[i] - vX/ vTVA) / pow(2.,0.5))) );
+                  (1.-CLHEP::HepStat::erfQ((vTVA / aTF * fBeta[i] - vX/ vTVA) / pow(2.,0.5))) );
         vTemp += ( exp( vX/ aTF * fBeta[i] ) *
-                  (1.-erf((vTVA / aTF * fBeta[i] + vX/ vTVA) / pow(2.,0.5))) );
+                  (1.-CLHEP::HepStat::erfQ((vTVA / aTF * fBeta[i] + vX/ vTVA) / pow(2.,0.5))) );
         vValueForSinglePlane += ( vTemp * fGamma[i] * exp( vTau[i] ) /2.0);
     }
     
