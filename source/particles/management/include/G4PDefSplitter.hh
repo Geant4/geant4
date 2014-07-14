36b9321555183bd7fe671b56da367e10b09f535b
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2014-07-14 09:16:37 +0000

    Merged from tag particles-V10-00-16
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/10-00-ref-07_branch@82858 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/particles/management/include/G4PDefSplitter.hh
+++ source/particles/management/include/G4PDefSplitter.hh
@@ -47,21 +47,29 @@
 
 #include "globals.hh"
 #include "pwdefs.hh"
+#include "G4AutoLock.hh"
 
 template <class T>  // T is the private data from the object to be split
 class G4PDefSplitter
 {
   public:
 
-    G4PDefSplitter() : totalobj(0) {}
+    G4PDefSplitter() : totalobj(0) {
+    	G4MUTEXINIT(mutex);
+    }
 
     G4int CreateSubInstance()
       // Invoked by the master or work thread to create a new subinstance
       // whenever a new split class instance is created. For each worker
       // thread, ions are created dynamically.
     {
+      G4AutoLock l(&mutex);
       totalobj++;
-      if (totalobj > slavetotalspace)  { NewSubInstances(); }
+      if (totalobj > slavetotalspace)  {
+    	  l.unlock();
+    	  NewSubInstances();
+    	  l.lock();
+      }
       return (totalobj - 1);
     }
 
@@ -70,6 +78,7 @@ class G4PDefSplitter
       // initialize each new subinstance using a particular method defined
       // by the subclass.
     {
+      G4AutoLock l(&mutex);
       if (slavetotalspace  >= totalobj)  { return; }
       G4int originaltotalspace = slavetotalspace;
       slavetotalspace = totalobj + 512;
@@ -103,6 +112,7 @@ class G4PDefSplitter
   private:
 
     G4int totalobj;
+    G4Mutex mutex;
 };
 
 #endif
---
+++ source/particles/test/storeParticleTable/storeParticles.cc
@@ -0,0 +1,103 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+// $Id$
+//
+// 
+// ---------------------------------------------------------------
+#include "G4ios.hh"
+#include "globals.hh"
+#include "tst2ParticleConstructor.hh"
+#include "G4ParticlePropertyTable.hh"
+#include "G4StateManager.hh"
+#include "G4TextPPReporter.hh"
+#include <fstream>
+#include <iomanip>
+
+int main() 
+{
+   // set the initial application state
+  G4StateManager::GetStateManager()->SetNewState(G4State_PreInit);
+
+  // create all particles
+  tst2ParticleConstructor pConstructor;
+  pConstructor.ConstructParticle();
+
+  // particleContainer 
+  G4VParticlePropertyReporter* aPPR;
+
+  G4VParticlePropertyReporter* pLepton = new G4TextPPReporter();
+  G4VParticlePropertyReporter* pMeson  = new G4TextPPReporter();
+  G4VParticlePropertyReporter* pBaryon = new G4TextPPReporter();
+  G4VParticlePropertyReporter* pIon    = new G4TextPPReporter();
+  G4VParticlePropertyReporter* pQuark  = new G4TextPPReporter();
+  G4VParticlePropertyReporter* pOther  = new G4TextPPReporter();
+
+  // pointer to the particle table
+  G4ParticleTable* theParticleTable = G4ParticleTable::GetParticleTable();
+  G4ParticleTable::G4PTblDicIterator* theParticleIterator;
+  theParticleIterator = theParticleTable->GetIterator();
+
+  G4ParticlePropertyTable* thePPTable = G4ParticlePropertyTable::GetParticlePropertyTable();
+
+  // loop over all particles in G4ParticleTable 
+  theParticleIterator->reset();
+  while( (*theParticleIterator)() ){
+    G4ParticleDefinition* particle = theParticleIterator->value();
+    G4String type = particle->GetParticleType();
+    if  (type=="lepton") {
+      aPPR = pLepton;
+    } else  if  (type=="meson") {
+      aPPR = pMeson;
+
+    } else  if  (type=="baryon") {
+      aPPR = pBaryon;
+
+    } else  if  (type=="nucleus") {
+      aPPR = pIon;
+
+    } else  if  ((type=="quarks")||(type=="diquarks")||(type=="gluons")) {
+      aPPR = pQuark;
+
+    } else    {
+      aPPR = pOther;
+    } 
+    // add property data to PPDcontainer
+    aPPR->FillList(particle->GetParticleName()); 
+  }
+  pLepton->Print("leptons");
+  pMeson->Print("mesons");
+  pBaryon->Print("baryons");
+  pIon->Print("ions");
+  pQuark->Print("quarks");
+  pOther->Print("others");
+
+  return EXIT_SUCCESS;
+}
+
+
+
+
