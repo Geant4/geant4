84f98045cb5c49f420e023a7ee21bae33de98ab5
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2014-07-24 10:27:23 +0000

    Merged from tag emhighenergy-V10-00-03
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/10-00-ref-07_branch@83000 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/processes/electromagnetic/highenergy/include/G4Vee2hadrons.hh
+++ source/processes/electromagnetic/highenergy/include/G4Vee2hadrons.hh
@@ -36,12 +36,13 @@
 //
 // Creation date: 12.08.2004
 //
-// Modifications:
+// Modifications: 14 July 2014 N. Chikuma revised interfaces  
 //
 
 //
-// Class Description:
-//
+// Class Description: base class to compute partial cross sections
+//                    of e+e- annihilation into hadrons and 
+//                    sample of final state in the centre mass frame
 
 // -------------------------------------------------------------------
 //
@@ -64,12 +65,10 @@ class G4Vee2hadrons
 
 public:
 
-  //G4Vee2hadrons() : lowEnergy(0.0), highEnergy(1.1*CLHEP::GeV) {}; //nchikuma
   G4Vee2hadrons(G4eeCrossSections* cr,
 		G4double vlowEnergy,
 		G4double vhighEnergy,
-		G4double vdelta) 
-  :cross(cr)
+		G4double vdelta) : cross(cr)
   {
 	lowEnergy  = vlowEnergy;
 	highEnergy = vhighEnergy;
@@ -78,16 +77,13 @@ public:
 
   virtual ~G4Vee2hadrons() {};
 
-  //virtual G4double ThresholdEnergy() const = 0;
-
   virtual G4double PeakEnergy() const = 0;
 
   virtual G4double ComputeCrossSection(G4double) const = 0;
 
-  //virtual G4PhysicsVector* PhysicsVector(G4double, G4double) const = 0;
   G4PhysicsVector* PhysicsVector() const
   {
-    G4int nbins = (G4int)( highEnergy - lowEnergy )/delta;
+    G4int nbins = std::max(3, G4int((highEnergy - lowEnergy)/delta) );
     G4PhysicsVector* pp = new G4PhysicsLinearVector(lowEnergy,highEnergy,nbins);
     return pp;
   };
@@ -95,18 +91,9 @@ public:
   virtual void SampleSecondaries(std::vector<G4DynamicParticle*>*,
 				 G4double, const G4ThreeVector&) = 0;
 
-  //void SetLowEnergy(G4double val) {lowEnergy = val;};  //nchikuma
-
   G4double LowEnergy() const {return lowEnergy;};
 
-  //void SetHighEnergy(G4double val) {highEnergy = val;}; //nchikuma
-
   G4double HighEnergy() const {return highEnergy;};
-
-/*  inline G4double GetCrossSection(G4double energy){
-	return physVec->Value(energy);
-  };
-*/
   
 private:
 
@@ -114,15 +101,14 @@ private:
   G4Vee2hadrons & operator=(const  G4Vee2hadrons &right);
   G4Vee2hadrons(const  G4Vee2hadrons&);
 
+  // parameters of the table
   G4double lowEnergy;
   G4double highEnergy;
-  G4double delta;  //nchikuma
-
-  //G4PhysicsVector* physVec; //nchikuma
+  G4double delta;       
 
 protected:
 
-   G4eeCrossSections* cross;  //nchikuma
+   G4eeCrossSections* cross;  // class to compute cross section
 
 };
 
