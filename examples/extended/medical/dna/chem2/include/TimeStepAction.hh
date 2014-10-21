1f302027894927bda7f4c070aec91b930f1f081e
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2014-10-21 12:26:40 +0000

    Merged from tag chem2-V10-00-00
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/10-00-ref-09_branch@84823 82ad68bf-922b-4865-b672-c5fc3b8c97f6

---
+++ examples/extended/medical/dna/chem2/include/TimeStepAction.hh
@@ -0,0 +1,63 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+// This example is provided by the Geant4-DNA collaboration
+// Any report or published results obtained using the Geant4-DNA software
+// shall cite the following Geant4-DNA collaboration publication:
+// Med. Phys. 37 (2010) 4692-4708
+// J. Comput. Phys. 274 (2014) 841-882
+// The Geant4-DNA web site is available at http://geant4-dna.org
+//
+// $Id$
+//
+/// \file TimeStepAction.hh
+/// \brief Definition of the TimeStepAction class
+
+#ifndef ITACTION_H
+#define ITACTION_H
+
+#include "G4UserTimeStepAction.hh"
+
+class TimeStepAction : public G4UserTimeStepAction
+{
+public:
+  TimeStepAction();
+  virtual ~TimeStepAction();
+  TimeStepAction(const TimeStepAction& other);
+  TimeStepAction& operator=(const TimeStepAction& other);
+
+  virtual void StartProcessing();
+
+  virtual void UserPreTimeStepAction();
+  virtual void UserPostTimeStepAction();
+
+  virtual void UserReactionAction(const G4Track&,
+                                  const G4Track&,
+                                  const std::vector<G4Track*>&);
+
+  virtual void EndProcessing();
+};
+
+#endif // ITACTION_H
