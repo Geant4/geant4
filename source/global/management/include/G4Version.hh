c1acb8e8b1e150ea14d0a74c9b4eb94ad21a431b
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2014-10-28 10:27:22 +0000

    Tag geant4-10-00-ref-09
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/10-00-ref-09_branch@85364 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/global/management/include/G4Version.hh
+++ source/global/management/include/G4Version.hh
@@ -50,7 +50,7 @@
 #endif
 
 #ifndef G4VERSION_TAG
-#define G4VERSION_TAG "$Name: geant4-10-00-ref-08 $"
+#define G4VERSION_TAG "$Name: geant4-10-00-ref-09 $"
 #endif
 
 // as variables
@@ -58,10 +58,10 @@
 #include "G4String.hh"
 
 #ifdef G4MULTITHREADED
-static const G4String G4Version = "$Name: geant4-10-00-ref-08 [MT]$";
+static const G4String G4Version = "$Name: geant4-10-00-ref-09 [MT]$";
 #else
-static const G4String G4Version = "$Name: geant4-10-00-ref-08 $";
+static const G4String G4Version = "$Name: geant4-10-00-ref-09 $";
 #endif
-static const G4String G4Date    = "(26-September-2014)";
+static const G4String G4Date    = "(31-October-2014)";
 
 #endif
