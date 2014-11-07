2ce5e5779003585aee24750ca2c2bbe0208b0e75
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2014-11-07 11:07:29 +0000

    Merged from tag exhadr03-V10-00-07
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/geant4-10-00-ref-10_branch@86125 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- examples/extended/hadronic/Hadr03/src/PhysicsList.cc
+++ examples/extended/hadronic/Hadr03/src/PhysicsList.cc
@@ -36,11 +36,11 @@
 #include "G4UnitsTable.hh"
 
 #include "G4HadronElasticPhysicsHP.hh"
-#include "G4IonElasticPhysics.hh"
 #include "G4HadronPhysicsFTFP_BERT_HP.hh"
 #include "G4HadronPhysicsQGSP_BIC_HP.hh"
 #include "G4HadronInelasticQBBC.hh"
 #include "G4HadronPhysicsINCLXX.hh"
+#include "G4IonElasticPhysics.hh"
 #include "G4IonPhysics.hh"
 #include "G4IonINCLXXPhysics.hh"
 #include "GammaPhysics.hh"
@@ -69,20 +69,27 @@ PhysicsList::PhysicsList()
   new G4UnitDefinition( "um2/mg", "um2/mg","Surface/Mass", um*um/mg);  
   
   // Hadron Elastic scattering
-  RegisterPhysics( new G4HadronElasticPhysicsHP(verb) );
-  RegisterPhysics( new G4IonElasticPhysics(verb) );  
+  //
+  RegisterPhysics( new G4HadronElasticPhysicsHP(verb));
 
-  // Hadron Inelastic Physics
+  // Hadron Inelastic physics
+  //
   RegisterPhysics( new G4HadronPhysicsFTFP_BERT_HP(verb));
   ////RegisterPhysics( new G4HadronPhysicsQGSP_BIC_HP(verb));
   ////RegisterPhysics( new G4HadronInelasticQBBC(verb));        
   ////RegisterPhysics( new G4HadronPhysicsINCLXX(verb));
   
-  // Ion Physics
+  // Ion Elastic scattering
+  //
+  RegisterPhysics( new G4IonElasticPhysics(verb));  
+  
+  // Ion Inelastic physics
+  //
   RegisterPhysics( new G4IonPhysics(verb));
   ////RegisterPhysics( new G4IonINCLXXPhysics(verb));
     
-  // Gamma Physics
+  // Gamma physics
+  //
   RegisterPhysics( new GammaPhysics("gamma"));
 }
 
