867208f05c0acd52ebf8f6756cce2ed255328a43
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2014-11-10 14:44:45 +0000

    Merged from tag cmake-V10-00-33
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/geant4-10-00-ref-10_branch@86389 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- cmake/Modules/Geant4DatasetDefinitions.cmake
+++ cmake/Modules/Geant4DatasetDefinitions.cmake
@@ -38,11 +38,11 @@ geant4_add_dataset(
 # - Radioisotopes
 geant4_add_dataset(
   NAME      RadioactiveDecay
-  VERSION   4.1
+  VERSION   4.2
   FILENAME  G4RadioactiveDecay
   EXTENSION tar.gz
   ENVVAR    G4RADIOACTIVEDATA
-  MD5SUM    be4bc5ad366f259c8ccc51d8698721d1
+  MD5SUM    2eade8116387ef67d4cd6f14b9cb1512
   )
 
 # - Neutron XS
