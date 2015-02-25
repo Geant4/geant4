5b6ee6e9b8274d9fa4c271b82261ae3f65526b26
Gabriele Cosmo <Gabriele.Cosmo@cern.ch>
2015-02-25 15:03:25 +0000

    Release 10.0.p04
    git-svn-id: svn+ssh://svn.cern.ch/reps/geant4/branches/geant4/_symbols/geant4-10-00-patches_branch@88524 82ad68bf-922b-4865-b672-c5fc3b8c97f6

--- source/global/management/include/G4Version.hh
+++ source/global/management/include/G4Version.hh
@@ -46,11 +46,11 @@
 //     |--> patch number
 
 #ifndef G4VERSION_NUMBER
-#define G4VERSION_NUMBER  1003
+#define G4VERSION_NUMBER  1004
 #endif
 
 #ifndef G4VERSION_TAG
-#define G4VERSION_TAG "$Name: geant4-10-00-patch-03 $"
+#define G4VERSION_TAG "$Name: geant4-10-00-patch-04 $"
 #endif
 
 // as variables
@@ -58,10 +58,10 @@
 #include "G4String.hh"
 
 #ifdef G4MULTITHREADED
-static const G4String G4Version = "$Name: geant4-10-00-patch-03 [MT]$";
+static const G4String G4Version = "$Name: geant4-10-00-patch-04 [MT]$";
 #else
-static const G4String G4Version = "$Name: geant4-10-00-patch-03 $";
+static const G4String G4Version = "$Name: geant4-10-00-patch-04 $";
 #endif
-static const G4String G4Date    = "(31-October-2014)";
+static const G4String G4Date    = "(6-March-2015)";
 
 #endif
