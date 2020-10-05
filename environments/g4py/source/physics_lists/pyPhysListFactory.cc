82a01e67ef76fd2bff5ff573610d2879e1d873fb
Daniel Hellfeld <dhellfeld@lbl.gov>
2020-10-05 09:23:52 -0700

    Expose PhysListFactory in g4py

--- environments/g4py/source/physics_lists/pyPhysListFactory.cc
+++ environments/g4py/source/physics_lists/pyPhysListFactory.cc
@@ -24,22 +24,30 @@
 // ********************************************************************
 //
 // ====================================================================
-//   pymodG4physicslists.cc [Geant4Py module]
+//   pyPhysListFactory.cc
 //
-//                                         2005 Q
+//                                         2020 Q
 // ====================================================================
 #include <boost/python.hpp>
+#include "G4PhysListFactory.hh"
 
 using namespace boost::python;
 
 // ====================================================================
 // module definition
 // ====================================================================
-void export_PhysicsLists();
-void export_PhysListFactory();
-
-BOOST_PYTHON_MODULE(G4physicslists)
+void export_PhysListFactory()
 {
-  export_PhysicsLists();
-  export_PhysListFactory();
+  class_<G4PhysListFactory, G4PhysListFactory*>
+    ("G4PhysListFactory", "phys list factory")
+    .def("GetReferencePhysList", &G4PhysListFactory::GetReferencePhysList,
+         return_internal_reference<>())
+    .def("ReferencePhysList", &G4PhysListFactory::ReferencePhysList,
+         return_internal_reference<>())
+    .def("IsReferencePhysList", &G4PhysListFactory::IsReferencePhysList)
+    .def("AvailablePhysLists", &G4PhysListFactory::AvailablePhysLists,
+	 return_value_policy<reference_existing_object>())
+    .def("AvailablePhysListsEM", &G4PhysListFactory::AvailablePhysListsEM,
+	 return_value_policy<reference_existing_object>())
+    ;
 }
