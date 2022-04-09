e5eda430f1db6eab31a4fa39ef8e2fd42e1c5c75
Alois Kabelschacht <andrii.verbytskyi@mpp.mpg.de>
2022-04-09 19:15:58 +0200

    OK

--- .github/workflows/fedora35/entrypoint.sh
+++ .github/workflows/fedora35/entrypoint.sh
@@ -5,6 +5,7 @@ cat /etc/issue
 yum -y install dnf-plugins-core
 yum -y install  gcc gcc-c++ gcc-gfortran make which cmake cmake-data cmake-filesystem
 yum -y install  HepMC3 HepMC3-devel HepMC HepMC-devel
+yum -y install  expat-devel  xerces-c-devel xerces-c
 yum -y install yum-plugin-copr
 yum -y copr enable averbyts/HEPrpms
 yum -y install geant4 geant4-devel  clhep clhep-devel PTL-devel expat expat-devel zlib zlib-devel pythia6 lhapdf lhapdf-devel
@@ -14,7 +15,7 @@ export FC=gfortran
 export FCFLAGS=
 mkdir -p test
 cd test
-cmake -B. -S ../examples/extended/eventgenerator -DGeant4_DIR=/usr/lib64/Geant4-10.7.0  -DHepMC3_DIR=/usr/share/HepMC3/cmake -DCMAKE_Fortran_FLAGS=-fPIC -DCMAKE_C_FLAGS=-fPIC -DCMAKE_CXX_FLAGS=-fPIC
+cmake -B. -S ../examples/extended/eventgenerator -DGeant4_DIR=/usr/lib64/Geant4-11.0.0  -DHepMC3_DIR=/usr/share/HepMC3/cmake -DCMAKE_Fortran_FLAGS=-fPIC -DCMAKE_C_FLAGS=-fPIC -DCMAKE_CXX_FLAGS=-fPIC
 make -j 2
 
 
