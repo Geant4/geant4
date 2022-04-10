83ab3e2d2c9c9edcf506798f1237a50996c910c6
Alois Kabelschacht <andrii.verbytskyi@mpp.mpg.de>
2022-04-10 14:23:53 +0200

    More examples

--- .github/workflows/fedora35/entrypoint.sh
+++ .github/workflows/fedora35/entrypoint.sh
@@ -6,6 +6,7 @@ yum -y install dnf-plugins-core
 yum -y install  gcc gcc-c++ gcc-gfortran make which cmake cmake-data cmake-filesystem
 yum -y install  HepMC3 HepMC3-devel HepMC HepMC-devel redhat-rpm-config
 yum -y install  expat-devel  xerces-c-devel xerces-c expat expat-devel zlib zlib-devel lhapdf lhapdf-devel
+yum -y install  pythia8-devel pythia8 pythia8-data
 yum -y install yum-plugin-copr
 yum -y copr enable averbyts/HEPrpms
 yum -y install geant4 geant4-devel  clhep clhep-devel PTL-devel  pythia6 
@@ -15,7 +16,7 @@ export FC=gfortran
 export FCFLAGS=
 mkdir -p test
 cd test
-cmake -B. -S ../examples/extended/eventgenerator -DGeant4_DIR=/usr/lib64/Geant4-11.0.0  -DPythia6_DIR=/usr -DHEPMC_DIR=/usr -DCMAKE_Fortran_FLAGS=-fPIC -DCMAKE_C_FLAGS=-fPIC -DCMAKE_CXX_FLAGS=-fPIC
+cmake -B. -S ../examples/extended/eventgenerator -DGeant4_DIR=/usr/lib64/Geant4-11.0.0  -DPYTHIA6_ROOT_DIR=/usr -DHEPMC_DIR=/usr -DCMAKE_Fortran_FLAGS=-fPIC -DCMAKE_C_FLAGS=-fPIC -DCMAKE_CXX_FLAGS=-fPIC
 make -j 2
 
 
