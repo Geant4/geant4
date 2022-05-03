a05c2bb133702a67944df3fa2692fc9b5996d6d0
Andrii Verbytskyi <andriish@pcatlas18.mpp.mpg.de>
2022-05-03 17:02:50 +0200

    Try 2

--- .github/workflows/osx/entrypoint.sh
+++ .github/workflows/osx/entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/sh -l
 set -x
 export TOP=$(pwd)
-mkdir LOCAL
-cd LOCAL
 brew tap davidchall/hep
-brew install hepmc lhapdf wget
+brew install hepmc lhapdf wget cmake coreutils  
 
 which gfortran-11
 if [ "$?" = "0" ]; then 
@@ -24,10 +22,11 @@ export CXX=clang++
 export CC=clang
 
 wget https://geant4-data.web.cern.ch/releases/lib_11.0.1/Darwin-clang13.0.0-Monterey.tar.gz
+tar -xzvf Darwin-clang13.0.0-Monterey.tar.gz
+ls -lah
 
-TOP=$(pwd)
 mkdir -p test
 cd test
-cmake -B. -S ../examples/extended/eventgenerator  -DCMAKE_INSTALL_PREFIX=$TOP/INSTALL -DGeant4_DIR=/usr/lib64/Geant4-11.0.0  -DPYTHIA6_ROOT_DIR=/usr -DHEPMC_DIR=/usr -DCMAKE_Fortran_FLAGS=-fPIC -DCMAKE_C_FLAGS=-fPIC -DCMAKE_CXX_FLAGS=-fPIC
+cmake -B. -S ../examples/extended/eventgenerator  -DCMAKE_INSTALL_PREFIX=$TOP/INSTALL -DGeant4_DIR=$TOP/Darwin-clang13.0.0-Monterey/Geant4-11.0.1-Darwin/lib64/Geant4-11.0.1  -DPYTHIA6_ROOT_DIR=/usr/local -DHEPMC_DIR=/usr/local -DCMAKE_Fortran_FLAGS=-fPIC -DCMAKE_C_FLAGS=-fPIC -DCMAKE_CXX_FLAGS=-fPIC
 cmake --build . -j 2
 cmake --install .
