5dc38005574527e94def4c6f23868cbdd717d4af
Andrii Verbytskyi <andriish@pcatlas18.mpp.mpg.de>
2022-05-04 14:21:04 +0200

    OK

--- .github/workflows/osx/entrypoint.sh
+++ .github/workflows/osx/entrypoint.sh
@@ -2,13 +2,43 @@
 set -x
 export TOP=$(pwd)
 brew tap davidchall/hep
-brew install hepmc lhapdf wget cmake coreutils  
+brew install lhapdf wget cmake coreutils  
 brew install gnu-sed
 brew install gcc
 brew install expat
 brew install pythia
 brew install libxmu
-brew install --cask xquartz
+#brew install --cask xquartz
+
+cat <<EOF > hepmc2.rb
+class Hepmc2 < Formula
+  desc "C++ event record for Monte Carlo generators"
+  homepage "https://hepmc.web.cern.ch/"
+  url "https://gitlab.cern.ch/hepmc/HepMC/-/archive/2.06.11/HepMC-2.06.11.tar.gz"
+  sha256 "ceaced62d39e4e2a1469fa2f20662d4d370279b3209930250766db02f44ae8de"
+
+  option "with-test", "Test during installation"
+
+  depends_on "cmake" => :build
+
+  def install
+    mkdir "../build" do
+      system "cmake", buildpath, "-Dmomentum:STRING=GEV", "-Dlength:STRING=MM", *std_cmake_args
+      system "make"
+      system "make", "test" if build.with? "test"
+      system "make", "install"
+    end
+  end
+
+  test do
+    cp_r share/"HepMC/examples/.", testpath
+    system "make", "example_BuildEventFromScratch.exe"
+    system "./example_BuildEventFromScratch.exe"
+  end
+end
+EOF
+
+brew install --build-from-source hepmc2.rb
 
 which gfortran-11
 if [ "$?" = "0" ]; then 
